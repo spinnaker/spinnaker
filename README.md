a2093b3538e2762b51859373dbff1b953a45eb10
Andrew Glover <aglover@netflix.com>
2014-04-08 17:56:59 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Java Application project template
+------------------------------------
+
+You have just created a basic Java application. There is a standard project
+structure for source code and tests.
+
+In this project you get:
+
+* A Gradle build file
+* A standard project structure:
+
+    <proj>
+      |
+      +- src
+          |
+          +- main
+          |     |
+          |     +- java
+          |
+          +- test
+          |   |
+          |   +- java
---
+++ src/test/groovy/com/netflix/front50/SimpleDBExploreTest.groovy
@@ -0,0 +1,50 @@
+package com.netflix.front50
+
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.simpledb.AmazonSimpleDB
+import com.amazonaws.services.simpledb.AmazonSimpleDBClient
+import com.amazonaws.services.simpledb.model.Item
+import com.amazonaws.services.simpledb.model.SelectRequest
+import spock.lang.Specification
+
+/**
+ * Created by aglover on 4/8/14.
+ */
+class SimpleDBExploreTest extends Specification {
+    static AmazonSimpleDB client
+    static String domain = "RESOURCE_REGISTRY"
+
+    def setupSpec() {
+        client = new AmazonSimpleDBClient(new BasicAWSCredentials(
+                System.properties["aws.key"], System.properties["aws.secret"]
+        ))
+    }
+
+    def 'should list items in a table'() {
+        String qry = "select * from `" + domain + "`";
+        SelectRequest selectRequest = new SelectRequest(qry);
+
+        expect:
+        client.select(selectRequest).getItems() != null
+    }
+
+    def 'should count items in a table'() {
+        String qry = "select count(*) from `" + domain + "`";
+        SelectRequest selectRequest = new SelectRequest(qry);
+
+        List<Item> items = client.select(selectRequest).getItems()
+
+        expect:
+        items != null
+        items.size() == 1
+
+        def item = items[0]
+        item.attributes != null
+        item.attributes.size() == 1
+
+        def attribute = item.attributes[0]
+        attribute.name == "Count"
+        attribute.value.toInteger() > 0
+    }
+
+}
