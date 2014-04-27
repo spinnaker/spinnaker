704afedbad8f9c4375bb172ade22ccd50f5c67fb
Daniel Woods <daniel.p.woods@gmail.com>
2014-04-27 10:04:45 -0700

    java docs on user data provider interface

--- src/main/groovy/com/netflix/kato/deploy/aws/userdata/UserDataProvider.groovy
+++ src/main/groovy/com/netflix/kato/deploy/aws/userdata/UserDataProvider.groovy
@@ -1,5 +1,20 @@
 package com.netflix.kato.deploy.aws.userdata
 
+/**
+ * Implementations of this interface will provide user data to instances during the deployment process
+ *
+ * @author Dan Woods
+ */
 public interface UserDataProvider {
+  /**
+   * Returns user data that will be applied to a new instance. The launch configuration will not have been created at
+   * this point in the workflow, but the name is provided, as it may be needed when building user data detail.
+   *
+   * @param asgName
+   * @param launchConfigName
+   * @param region
+   *
+   * @return user data string
+   */
   String getUserData(String asgName, String launchConfigName, String region)
 }
