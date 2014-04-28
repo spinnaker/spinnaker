f2af9c4eeae1df7eaa0f566883ab9b44dc86c210
Daniel Woods <daniel.p.woods@gmail.com>
2014-04-28 13:22:49 -0700

    tuning for cluster view

--- src/main/groovy/com/netflix/oort/controllers/ClusterController.groovy
+++ src/main/groovy/com/netflix/oort/controllers/ClusterController.groovy
@@ -33,12 +33,17 @@ class ClusterController {
     def map = DeployableController.Cacher.get().get(deployable)
     if (map.clusters.containsKey(clusterName)) {
       def cluster = map.clusters."$clusterName"
-      cluster.collect { String region, List<Map> asgs ->
+      def asgDetails = cluster.collect { String region, List<Map> asgs ->
         def found = asgs.find { it.autoScalingGroupName == asgName }
-        def converted = convertAsgMap(clusterName, region, found)
-        converted.asg = found
-        converted
-      }?.getAt(0)
+        if (found) {
+          def converted = convertAsgMap(clusterName, region, found)
+          converted.asg = found
+          converted.instances = converted.instances.collect { getInstance(region, it.instanceId) }
+          converted
+        } else { null }
+      }
+      asgDetails.removeAll([null])
+      asgDetails?.getAt(0)
     } else {
       response.sendError 404
     }
@@ -57,9 +62,13 @@ class ClusterController {
   }
 
   def convertAsgMap(String cluster, String region, Map asg) {
-    def resp = [name: asg.autoScalingGroupName, region: region, cluster: cluster, instances: asg.instances, created: asg.createdTime]
+    def extra = getExtraDetails(asg.launchConfigurationName, region)
+    def launchConfig = extra.launchConfig
+    def image = extra.image
 
-    def image = getImage(asg.launchConfigurationName, region)
+    def resp = [name: asg.autoScalingGroupName, region: region, cluster: cluster, instances: asg.instances,
+                created: asg.createdTime, launchConfig: launchConfig]
+
     def buildVersion = image.tags.find { it.key == "appversion" }?.value
     if (buildVersion) {
       def props = AppVersion.parseName(buildVersion)?.properties
@@ -73,8 +82,15 @@ class ClusterController {
     resp
   }
 
-  def getImage(String launchConfigName, String region) {
+  def getExtraDetails(String launchConfigName, String region) {
     def launchConfig = restTemplate.getForEntity("http://entrypoints-v2.${region}.test.netflix.net:7001/REST/v2/aws/launchConfigurations/$launchConfigName", Map).body
-    restTemplate.getForEntity("http://entrypoints-v2.${region}.test.netflix.net:7001/REST/v2/aws/images/$launchConfig.imageId", Map).body
+    def image = restTemplate.getForEntity("http://entrypoints-v2.${region}.test.netflix.net:7001/REST/v2/aws/images/$launchConfig.imageId", Map).body
+    [launchConfig: launchConfig, image: image]
+  }
+
+  def getInstance(String region, String instanceId) {
+    try {
+      restTemplate.getForEntity("http://entrypoints-v2.${region}.test.netflix.net:7001/REST/v2/view/instances/$instanceId", Map).body
+    } catch (IGNORE) { [instanceId: instanceId, state: [name: "offline"]] }
   }
 }
