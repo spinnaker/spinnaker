f84689583664c3390d264d199c29980885409e3e
Rob Fletcher <rfletcher@netflix.com>
2014-05-16 17:29:11 +0100

    broke out a base spec class for doing the embedded HTTP server

--- src/test/groovy/com/netflix/bluespar/orca/bakery/api/BakeryServiceSpec.groovy
+++ src/test/groovy/com/netflix/bluespar/orca/bakery/api/BakeryServiceSpec.groovy
@@ -1,21 +1,15 @@
 package com.netflix.bluespar.orca.bakery.api
 
-import com.sun.net.httpserver.HttpServer
-import groovy.json.JsonBuilder
+import com.netflix.bluespar.orca.test.HttpServerSpecification
 import retrofit.RestAdapter
-import spock.lang.Specification
 import spock.lang.Subject
 
-import static com.google.common.net.HttpHeaders.CONTENT_TYPE
 import static com.google.common.net.HttpHeaders.LOCATION
 import static java.net.HttpURLConnection.*
-import static java.util.Collections.EMPTY_MAP
 import static retrofit.RestAdapter.LogLevel.FULL
 
-class BakeryServiceSpec extends Specification {
+class BakeryServiceSpec extends HttpServerSpecification {
 
-    String address
-    HttpServer server
     RestAdapter restAdapter
 
     @Subject
@@ -27,16 +21,11 @@ class BakeryServiceSpec extends Specification {
     final statusId = "s-123456789"
 
     def setup() {
-        address = startServer()
         restAdapter = new RestAdapter.Builder()
-            .setEndpoint(address)
+            .setEndpoint(baseURI)
             .setLogLevel(FULL)
             .build()
         bakery = restAdapter.create(BakeryService)
-    }
-
-    def cleanup() {
-        server.stop(3)
     }
 
     def "should return status of newly created bake"() {
@@ -44,8 +33,8 @@ class BakeryServiceSpec extends Specification {
         expect("POST", bakeURI, HTTP_ACCEPTED) {
             state "PENDING"
             progress 0
-            resource_uri "$address$bakeURI/$bakeId"
-            uri "$address$statusURI/$statusId"
+            resource_uri "$baseURI$bakeURI/$bakeId"
+            uri "$baseURI$statusURI/$statusId"
             id "s-123456789"
             attempts 0
             ctime 1382310109766
@@ -65,15 +54,15 @@ class BakeryServiceSpec extends Specification {
         def responseContent = {
             state "RUNNING"
             progress 1
-            resource_uri "$address$bakeURI/$bakeId"
-            uri "$address$statusURI/$statusId"
+            resource_uri "$baseURI$bakeURI/$bakeId"
+            uri "$baseURI$statusURI/$statusId"
             id "s-123456789"
             attempts 1
             ctime 1382310109766
             mtime 1382310109766
             messages(["on instance i-66f5913d runnning: aminate ..."])
         }
-        expect "POST", bakeURI, HTTP_SEE_OTHER, [(LOCATION): "$address$statusURI/$statusId"], responseContent
+        expect "POST", bakeURI, HTTP_SEE_OTHER, [(LOCATION): "$baseURI$statusURI/$statusId"], responseContent
         expect "GET", "$statusURI/$statusId", HTTP_OK, responseContent
 
         expect: "createBake should return the status of the bake"
@@ -83,42 +72,5 @@ class BakeryServiceSpec extends Specification {
         }
     }
 
-    private void expect(String method, String path, int responseStatus, Closure responseContent) {
-        expect method, path, responseStatus, EMPTY_MAP, responseContent
-    }
-
-    private void expect(String method, String path, Integer responseStatus, Map<String, ?> headers, Closure responseContent) {
-        server.createContext path, { exchange ->
-            if (exchange.requestMethod == method) {
-                def json = new JsonBuilder()
-                json(responseContent)
-                def response = json.toString()
-                exchange.with {
-                    responseHeaders[CONTENT_TYPE] = "application/json"
-                    headers.each { key, value ->
-                        responseHeaders[key] = value
-                    }
-                    sendResponseHeaders responseStatus, response.length()
-                    responseBody.write response.bytes
-                    close()
-                }
-            } else {
-                exchange.with {
-                    sendResponseHeaders HTTP_BAD_METHOD, 0
-                    close()
-                }
-            }
-        }
-    }
-
-    private String startServer() {
-        def address = new InetSocketAddress(0)
-        server = HttpServer.create(address, 0)
-        server.with {
-            executor = null
-            start()
-        }
-        return "http://localhost:$server.address.port"
-    }
 
 }
---
+++ src/test/groovy/com/netflix/bluespar/orca/test/HttpServerSpecification.groovy
@@ -0,0 +1,118 @@
+package com.netflix.bluespar.orca.test
+
+import com.google.common.base.Optional
+import com.google.common.net.HttpHeaders
+import com.sun.net.httpserver.HttpServer
+import groovy.json.JsonBuilder
+import groovy.transform.CompileStatic
+import spock.lang.Specification
+
+/**
+ * A base {@link Specification} class for specs that need to connect to a REST server or similar endpoint.
+ *
+ * Expectations for HTTP requests are set up using {@link HttpServerSpecification#expect}. You can set up multiple
+ * expectations in a single specification method. Order is not enforced
+ */
+abstract class HttpServerSpecification extends Specification {
+
+    private static final int SERVER_SHUTDOWN_TIMEOUT = 3
+    private String baseURI
+    private HttpServer server
+
+    @CompileStatic
+    def setup() {
+        startServer()
+    }
+
+    @CompileStatic
+    def cleanup() {
+        server.stop SERVER_SHUTDOWN_TIMEOUT
+    }
+
+    /**
+     * @return the URI of the root of the web server.
+     */
+    @CompileStatic
+    protected final String getBaseURI() {
+        baseURI
+    }
+
+    /**
+     * @see #expect(java.lang.String, java.lang.String, int, java.util.Map, com.google.common.base.Optional)
+     */
+    protected final void expect(String method, String path, int responseStatus) {
+        expect method, path, responseStatus, Collections.EMPTY_MAP, Optional.absent()
+    }
+
+    /**
+     * @see #expect(java.lang.String, java.lang.String, int, java.util.Map, com.google.common.base.Optional)
+     */
+    protected final void expect(String method, String path, int responseStatus, Closure responseContent) {
+        expect method, path, responseStatus, Collections.EMPTY_MAP, Optional.of(responseContent)
+    }
+
+    /**
+     * @see #expect(java.lang.String, java.lang.String, int, java.util.Map, com.google.common.base.Optional)
+     */
+    protected final void expect(String method, String path, int responseStatus, Map<String, ?> headers) {
+        expect method, path, responseStatus, headers, Optional.absent()
+    }
+
+    /**
+     * @see #expect(java.lang.String, java.lang.String, int, java.util.Map, com.google.common.base.Optional)
+     */
+    protected
+    final void expect(String method, String path, int responseStatus, Map<String, ?> headers, Closure responseContent) {
+        expect method, path, responseStatus, headers, Optional.of(responseContent)
+    }
+
+    /**
+     * Sets up an expectation for an HTTP request. If a request to {@code path} is made using the specified
+     * {@code method} then the server will respond according to the parameters supplied to this method. If a
+     * request is made to {@code path} using a different HTTP method the server will respond with
+     * {@value HttpURLConnection#HTTP_BAD_METHOD}.
+     *
+     * @param method the HTTP method expected
+     * @param path the literal path expected relative to the base URI of the server. Note this cannot use wildcards or any other clever things.
+     * @param responseStatus the HTTP status of the response.
+     * @param headers any HTTP headers that should be sent with the response.
+     * @param responseContent a <em>Closure</em> used to construct a response using {@link JsonBuilder}.
+     */
+    protected
+    final void expect(String method, String path, int responseStatus, Map<String, ?> headers, Optional<Closure> responseContent) {
+        server.createContext path, { exchange ->
+            if (exchange.requestMethod == method) {
+                def response = ""
+                if (responseContent.present) {
+                    def json = new JsonBuilder()
+                    json(responseContent.get())
+                    response = json.toString()
+                }
+                exchange.with {
+                    responseHeaders[HttpHeaders.CONTENT_TYPE] = "application/json"
+                    headers.each { key, value ->
+                        responseHeaders[key] = value
+                    }
+                    sendResponseHeaders responseStatus, response.length()
+                    responseBody.write response.bytes
+                    close()
+                }
+            } else {
+                exchange.with {
+                    sendResponseHeaders HttpURLConnection.HTTP_BAD_METHOD, 0
+                    close()
+                }
+            }
+        }
+    }
+
+    private void startServer() {
+        def address = new InetSocketAddress(0)
+        server = HttpServer.create(address, 0)
+        server.with {
+            executor = null
+            start()
+        }
+        baseURI = "http://localhost:$server.address.port"
+    }
+}
