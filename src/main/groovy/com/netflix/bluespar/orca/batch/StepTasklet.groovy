854f88f66d4cbfe5b1a861f8457fc776efe83ed1
Rob Fletcher <rfletcher@netflix.com>
2014-05-19 18:03:47 +0100

    Explicit constructor for StepTasklet
    TupleConstructor doesn't do exactly what I thought it did

--- src/main/groovy/com/netflix/bluespar/orca/batch/StepTasklet.groovy
+++ src/main/groovy/com/netflix/bluespar/orca/batch/StepTasklet.groovy
@@ -2,7 +2,6 @@ package com.netflix.bluespar.orca.batch
 
 import com.netflix.bluespar.orca.Step
 import groovy.transform.CompileStatic
-import groovy.transform.TupleConstructor
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.tasklet.Tasklet
@@ -12,11 +11,14 @@ import static com.netflix.bluespar.orca.StepResult.COMPLETE
 import static org.springframework.batch.repeat.RepeatStatus.CONTINUABLE
 import static org.springframework.batch.repeat.RepeatStatus.FINISHED
 
-@TupleConstructor
 @CompileStatic
 class StepTasklet implements Tasklet {
 
-    final Step step
+    private final Step step
+
+    StepTasklet(Step step) {
+        this.step = step
+    }
 
     @Override
     RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
