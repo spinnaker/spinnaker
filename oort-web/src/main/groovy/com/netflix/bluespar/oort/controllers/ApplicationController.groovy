76a2a912ea73c6e2d073fc3fba3518b54137b83e
Daniel Woods <daniel.p.woods@gmail.com>
2014-05-26 19:36:31 -0700

    app controller refactoring an testing

--- oort-web/src/main/groovy/com/netflix/bluespar/oort/controllers/ApplicationController.groovy
+++ oort-web/src/main/groovy/com/netflix/bluespar/oort/controllers/ApplicationController.groovy
@@ -19,69 +19,108 @@ package com.netflix.bluespar.oort.controllers
 import com.netflix.bluespar.oort.clusters.Cluster
 import com.netflix.bluespar.oort.deployables.Application
 import com.netflix.bluespar.oort.deployables.ApplicationProvider
+import groovy.transform.Canonical
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.MessageSource
+import org.springframework.context.i18n.LocaleContextHolder
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/applications")
 class ApplicationController {
 
   @Autowired
-  List<ApplicationProvider> deployableProviders
+  MessageSource messageSource
+
+  @Autowired
+  List<ApplicationProvider> applicationProviders
 
   @RequestMapping(method = RequestMethod.GET)
-  def list() {
-    Map<String, Application> deployables = [:]
-    deployableProviders.each {
-      it.list().each { Application deployable ->
-        if (deployables.containsKey(deployable.name)) {
-          def existing = deployables[deployable.name]
-          def merged = Application.merge(existing, deployable)
-          deployables[deployable.name] = merged
-        } else {
-          deployables[deployable.name] = deployable
-        }
+  Map<String, ApplicationViewModel> list() {
+    List<Application> applicationProviderResults = applicationProviders.collectMany { it.list() ?: [] }
+    def nameKeyedApplicationList = getMergedApplications(applicationProviderResults)
+
+    nameKeyedApplicationList.inject(new HashMap<>()) { Map<String, ApplicationViewModel> viewModels, String name, Application application ->
+      if (!viewModels.containsKey(name)) {
+        viewModels[name] = convertToModel(name, application)
       }
-    }
-    deployables.inject(new HashMap()) { Map map, String name, Application deployable ->
-      if (!map.containsKey(name)) {
-        map[name] = [clusterCount: 0, instanceCount: 0, serverGroupCount: 0, attributes: deployable.attributes]
+      application.clusters.list().each { Cluster cluster ->
+        viewModels[name].clusters = (viewModels[name].clusters ?: new HashSet()) << cluster.name
+        viewModels[name].clusterCount += 1
+        viewModels[name].serverGroupCount += cluster.serverGroups?.size()
+        viewModels[name].instanceCount += cluster.serverGroups?.collect { it.getInstanceCount() }?.sum() ?: 0
       }
-      deployable.clusters.list().each { Cluster cluster ->
-        map[name].clusters = (map[name].clusters ?: new HashSet()) << cluster.name
-        map[name].clusterCount += 1
-        map[name].serverGroupCount += cluster.serverGroups?.size()
-        map[name].instanceCount += cluster.serverGroups?.collect { it.getInstanceCount() }?.sum() ?: 0
-      }
-      map
+      viewModels
     }
   }
 
   @RequestMapping(value = "/{name}")
-  def get(@PathVariable("name") String name) {
-    def deployables = deployableProviders.collect { it.get(name) }.findAll { it }
-    Application deployable = deployables.inject(new HashMap()) { Map map, Application deployable ->
-      if (map.containsKey(deployable.name)) {
-        def existing = map[deployable.name]
-        def merged = Application.merge(existing, deployable)
-        map[deployable.name] = merged
-      } else {
-        map[deployable.name] = deployable
-      }
-      map
-    }?.getAt(name)
-
-    if (!deployable) {
-      return null
+  ApplicationViewModel get(@PathVariable("name") String name) {
+    List<Application> applicationProviderResults = applicationProviders.collectMany { [it.get(name)] ?: [] }.findAll { Application application -> application } as List<Application>
+    def nameKeyedApplicationList = getMergedApplications(applicationProviderResults)
+    if (!nameKeyedApplicationList.containsKey(name)) {
+      throw new ApplicationNotFoundException(name)
     }
+    def viewModel = convertToModel name, nameKeyedApplicationList[name]
+    return viewModel
+  }
 
-    def clusters = deployable.clusters.list()
-    def serverGroupCount = clusters.collect { it.serverGroups.size() }?.sum()
+  @ExceptionHandler(ApplicationNotFoundException)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  Map handleApplicationNotFoundException(ApplicationNotFoundException ex) {
+    def message = messageSource.getMessage("application.not.found", [ex.name] as String[], LocaleContextHolder.locale)
+    [error: "Application not found", messages: [message], status: HttpStatus.NOT_FOUND]
+  }
+
+  private static Map<String, Application> getMergedApplications(List<Application> applications) {
+    Map<String, Application> map = [:]
+    applications.each { Application application ->
+      if (map.containsKey(application.name)) {
+        Application existing = map[application.name]
+        def merged = Application.merge(existing, application)
+        map[application.name] = merged
+      } else {
+        map[application.name] = application
+      }
+    }
+    map
+  }
+
+  @Canonical
+  static class ApplicationNotFoundException extends RuntimeException {
+    String name
+  }
+
+  static ApplicationViewModel convertToModel(String name, Application application) {
+    def clusters = application.clusters.list()
+    def serverGroupCount = (clusters.collect { it.serverGroups.size() }?.sum() ?: 0) as int
     def instanceCount = clusters.inject(0) { int c, Cluster cluster -> c += (cluster.serverGroups?.collect { it.getInstanceCount() }?.sum() ?: 0); c }
 
-    [clusterCount: clusters.size(), instanceCount: instanceCount, serverGroupCount: serverGroupCount, attributes: deployable.attributes]
+    def model = new ApplicationViewModel(name: name)
+    model.clusterCount = clusters.size()
+    model.instanceCount = instanceCount
+    model.serverGroupCount = serverGroupCount
+    model.instanceCount = instanceCount
+    model.attributes = application.attributes
+    model.clusters = (model.clusters ?: new HashSet())
+    model.clusters.addAll(clusters.collect { it.name })
+    model
+  }
+
+  static class ApplicationViewModel {
+    String name
+    int clusterCount
+    int instanceCount
+    int serverGroupCount
+    Map attributes
+    Set<String> clusters
   }
 }
