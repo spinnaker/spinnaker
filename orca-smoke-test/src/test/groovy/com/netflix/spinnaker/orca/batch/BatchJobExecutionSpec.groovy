1c872393c6544d967257d073410a066abd1e6204
Rob Fletcher <rfletcher@netflix.com>
2014-05-28 12:40:58 +0100

    couple of failure scenario tests

--- orca-smoke-test/src/test/groovy/com/netflix/spinnaker/orca/batch/BatchJobExecutionSpec.groovy
+++ orca-smoke-test/src/test/groovy/com/netflix/spinnaker/orca/batch/BatchJobExecutionSpec.groovy
@@ -3,14 +3,13 @@ package com.netflix.spinnaker.orca.batch
 import com.netflix.spinnaker.orca.DefaultTaskResult
 import com.netflix.spinnaker.orca.Task
 import com.netflix.spinnaker.orca.smoke.BatchTestConfiguration
-import org.springframework.batch.core.BatchStatus
 import org.springframework.batch.core.ExitStatus
+import org.springframework.batch.core.Job
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.test.JobLauncherTestUtils
-import org.springframework.batch.test.MetaDataInstanceFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
@@ -27,6 +26,9 @@ class BatchJobExecutionSpec extends Specification {
     @Autowired StepBuilderFactory steps
     def jobLauncherTestUtils = new JobLauncherTestUtils()
 
+    def startTask = Stub(Task)
+    def monitorTask = Mock(Task)
+
     def setup() {
         jobLauncherTestUtils.jobLauncher = jobLauncher
         jobLauncherTestUtils.jobRepository = jobRepository
@@ -34,22 +36,10 @@ class BatchJobExecutionSpec extends Specification {
 
     def "can start an external service and monitor until completed"() {
         given:
-        def startTask = Stub(Task) {
-            execute(_) >> new DefaultTaskResult(SUCCEEDED)
-        }
-        def monitorTask = Mock(Task)
+        startTask.execute(_) >> new DefaultTaskResult(SUCCEEDED)
 
         and:
-        def step1 = steps.get("StartStep")
-            .tasklet(TaskTaskletAdapter.decorate(startTask))
-            .build()
-        def step2 = steps.get("MonitorStep")
-            .tasklet(TaskTaskletAdapter.decorate(monitorTask))
-            .build()
-        jobLauncherTestUtils.job = jobs.get("StartAndMonitorJob")
-            .start(step1)
-            .next(step2)
-            .build()
+        jobLauncherTestUtils.job = createStartAndMonitorWorkflow(startTask, monitorTask)
 
         when:
         def jobExecution = jobLauncherTestUtils.launchJob()
@@ -61,4 +51,52 @@ class BatchJobExecutionSpec extends Specification {
         jobExecution.exitStatus == ExitStatus.COMPLETED
     }
 
+    def "abandons monitoring if the monitor task fails"() {
+        given:
+        startTask.execute(_) >> new DefaultTaskResult(SUCCEEDED)
+
+        and:
+        jobLauncherTestUtils.job = createStartAndMonitorWorkflow(startTask, monitorTask)
+
+        when:
+        def jobExecution = jobLauncherTestUtils.launchJob()
+
+        then:
+        2 * monitorTask.execute(_) >> new DefaultTaskResult(RUNNING) >> {
+            throw new RuntimeException("something went wrong")
+        }
+
+        and:
+        jobExecution.exitStatus == ExitStatus.FAILED
+    }
+
+    def "does not start monitoring if the start task fails"() {
+        given:
+        startTask.execute(_) >> { throw new RuntimeException("something went wrong") }
+
+        and:
+        jobLauncherTestUtils.job = createStartAndMonitorWorkflow(startTask, monitorTask)
+
+        when:
+        def jobExecution = jobLauncherTestUtils.launchJob()
+
+        then:
+        0 * monitorTask._
+
+        and:
+        jobExecution.exitStatus == ExitStatus.FAILED
+    }
+
+    private Job createStartAndMonitorWorkflow(Task startTask, Task monitorTask) {
+        def step1 = steps.get("StartStep")
+            .tasklet(TaskTaskletAdapter.decorate(startTask))
+            .build()
+        def step2 = steps.get("MonitorStep")
+            .tasklet(TaskTaskletAdapter.decorate(monitorTask))
+            .build()
+        jobs.get("StartAndMonitorJob")
+            .start(step1)
+            .next(step2)
+            .build()
+    }
 }
