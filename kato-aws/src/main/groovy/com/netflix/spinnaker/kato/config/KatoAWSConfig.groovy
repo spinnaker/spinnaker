68561abf475c180068725d05b8f58f4e0382b0c4
Daniel Woods <daniel.p.woods@gmail.com>
2014-05-30 05:21:20 -0700

    this spel has been wrong all along.

--- kato-aws/src/main/groovy/com/netflix/spinnaker/kato/config/KatoAWSConfig.groovy
+++ kato-aws/src/main/groovy/com/netflix/spinnaker/kato/config/KatoAWSConfig.groovy
@@ -62,7 +62,7 @@ class KatoAWSConfig {
   }
 
   @Configuration
-  @ConditionalOnExpression('${bastion.enabled:false}')
+  @ConditionalOnExpression('#{${bastion.enabled}}')
   static class BastionCredentialsInitializer {
     @Autowired
     NamedAccountCredentialsHolder namedAccountCredentialsHolder
@@ -82,7 +82,7 @@ class KatoAWSConfig {
   }
 
   @Configuration
-  @ConditionalOnExpression('${!bastion.enabled:false}')
+  @ConditionalOnExpression('#{!${bastion.enabled}}')
   static class AmazonCredentialsInitializer {
     @Autowired
     AWSCredentialsProvider awsCredentialsProvider
