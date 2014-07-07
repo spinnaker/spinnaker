f7b3268c2a69dee892ddf884946070a2285c5451
Daniel Woods <daniel.p.woods@gmail.com>
2014-07-07 15:57:29 -0700

    account hostname filter

---
+++ front50-web/src/main/groovy/com/netflix/spinnaker/front50/filters/AccountHostnameFilter.groovy
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.front50.filters
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class AccountHostnameFilter implements Filter {
+
+  @Value('${front50.prefix:front50}')
+  private String front50Prefix
+
+  @Value('${front50.domain:netflix.net}')
+  private String front50Domain
+
+  void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest request = (HttpServletRequest) req
+    def host = request.requestURL.toURL().host
+
+    // in the format: front50.<account>.netflix.net
+    if (host.startsWith("${front50Prefix}.") && host.endsWith(front50Domain)) {
+      def hostParts = host.tokenize('.')
+      def account = hostParts[1]
+      def reqParts = request.requestURI.tokenize('/')
+      def reqAccount = reqParts[0]
+      if (account != reqAccount) {
+        req.getRequestDispatcher("/${account}/${reqParts.join('/')}").forward(req, res)
+        return
+      }
+    }
+    chain.doFilter(req, res)
+  }
+
+  void init(FilterConfig filterConfig) throws ServletException {}
+
+  void destroy() {}
+}
---
+++ front50-web/src/test/groovy/com/netflix/spinnaker/front50/filters/AccountHostnameFilterSpec.groovy
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.front50.filters
+
+import spock.lang.Specification
+
+import javax.servlet.FilterChain
+import javax.servlet.RequestDispatcher
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class AccountHostnameFilterSpec extends Specification {
+
+  void "if hostname matches vip pattern, internal forward appropriately"() {
+    setup:
+    def req = Mock(HttpServletRequest)
+    def res = Mock(HttpServletResponse)
+    def chain = Mock(FilterChain)
+    def filter = new AccountHostnameFilter(front50Domain: "netflix.net", front50Prefix: "front50")
+
+    when:
+    filter.doFilter(req, res, chain)
+
+    then:
+    1 * req.getRequestURL() >> new StringBuffer("http://front50.test.netflix.net")
+    1 * req.getRequestURI() >> "/applications"
+    1 * req.getRequestDispatcher("/test/applications") >> Mock(RequestDispatcher)
+  }
+
+  void "hostanme matches that already have the account pattner don't forward"() {
+    setup:
+    def req = Mock(HttpServletRequest)
+    def res = Mock(HttpServletResponse)
+    def chain = Mock(FilterChain)
+    def filter = new AccountHostnameFilter(front50Domain: "netflix.net", front50Prefix: "front50")
+
+    when:
+    filter.doFilter(req, res, chain)
+
+    then:
+    1 * req.getRequestURL() >> new StringBuffer("http://front50.test.netflix.net")
+    1 * req.getRequestURI() >> "/test/applications"
+    0 * req.getRequestDispatcher("/test/applications") >> Mock(RequestDispatcher)
+  }
+}
