bde09d8bccccda4a487439af307496e29b4bb011
Daniel Woods <daniel.p.woods@gmail.com>
2014-07-10 20:22:56 -0700

    ResizeAsgStageBuilder

---
+++ orca-kato/src/main/groovy/com/netflix/spinnaker/orca/kato/pipeline/ResizeAsgStageBuilder.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.kato.pipeline
+
+import groovy.transform.CompileStatic
+import com.netflix.spinnaker.orca.kato.tasks.MonitorKatoTask
+import com.netflix.spinnaker.orca.kato.tasks.ResizeAsgTask
+import com.netflix.spinnaker.orca.kato.tasks.WaitForCapacityMatchTask
+import com.netflix.spinnaker.orca.pipeline.LinearStageBuilder
+import org.springframework.batch.core.Step
+import org.springframework.stereotype.Component
+
+@Component
+@CompileStatic
+class ResizeAsgStageBuilder extends LinearStageBuilder {
+
+  @Override
+  protected List<Step> buildSteps() {
+    def step1 = steps.get("ResizeAsgStep")
+        .tasklet(buildTask(ResizeAsgTask))
+        .build()
+    def step2 = steps.get("MonitorAsgStep")
+        .tasklet(buildTask(MonitorKatoTask))
+        .build()
+    def step3 = steps.get("WaitForCapacityMatchStep")
+        .tasklet(buildTask(WaitForCapacityMatchTask))
+        .build()
+
+    [step1, step2, step3]
+  }
+}
