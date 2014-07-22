e71557b6d52072a5f401adc88500b894fc2b1914
Clay McCoy <git@claymccoy.com>
2014-07-22 15:47:16 -0700

    add operations for suspending and resuming ASG processes

---
+++ kato-aws/src/main/groovy/com/netflix/spinnaker/kato/deploy/aws/converters/SuspendAsgProcessesAtomicOperationConverter.groovy
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spinnaker.kato.deploy.aws.converters
+
+import com.netflix.spinnaker.kato.deploy.aws.description.SuspendAsgProcessesDescription
+import com.netflix.spinnaker.kato.deploy.aws.ops.SuspendAsgProcessesAtomicOperation
+import com.netflix.spinnaker.kato.orchestration.AtomicOperation
+import com.netflix.spinnaker.kato.security.AbstractAtomicOperationsCredentialsSupport
+import org.springframework.stereotype.Component
+
+@Component("suspendAsgProcessesDescription")
+class SuspendAsgProcessesAtomicOperationConverter extends AbstractAtomicOperationsCredentialsSupport {
+  @Override
+  AtomicOperation convertOperation(Map input) {
+    new SuspendAsgProcessesAtomicOperation(convertDescription(input))
+  }
+
+  @Override
+  SuspendAsgProcessesDescription convertDescription(Map input) {
+    input.credentials = getCredentialsObject(input.credentials as String)
+    new SuspendAsgProcessesDescription(input)
+  }
+}
