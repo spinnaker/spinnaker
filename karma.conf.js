cee16bded4871d9365281f664027253578bdf63d
chrisb <chrisb@netflix.com>
2014-07-28 08:44:24 -0700

    include template cache in test config

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,8 @@ module.exports = function(config) {
       'app/bower_components/angular-spinner/angular-spinner.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
-      'test/spec/**/*.js'
+      'test/spec/**/*.js',
+      '.tmp/scripts/templates.js'
     ],
 
     // list of files / patterns to exclude
