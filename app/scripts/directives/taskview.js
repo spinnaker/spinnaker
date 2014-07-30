795ae9be0d01a84f08c3a8fe2cf3f9cb5ae13a3c
Benjamin Malley <bmalley@netflix.com>
2014-07-30 19:48:55 +0000

    [wip] task details

---
+++ app/scripts/directives/taskview.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('deckApp')
+  .directive('taskView', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'views/task.html',
+      scope: {
+        task: '=',
+      },
+      controller: function($scope) {
+        $scope.open = false;
+        $scope.toggleDetails = function() {
+          $scope.open = !$scope.open;
+        };
+
+      },
+    };
+
+  })
