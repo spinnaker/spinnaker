7c0b3829f6ae6df8c99c1878a3e605cd3f666bc4
chrisb <chrisb@netflix.com>
2014-07-31 13:36:07 -0700

    instance not found handling

--- app/scripts/controllers/InstanceCtrl.js
+++ app/scripts/controllers/InstanceCtrl.js
@@ -9,11 +9,23 @@ angular.module('deckApp')
           return serverGroup.instances.some(function (possibleInstance) {
             if (possibleInstance.name === instance.name) {
               $scope.instance = possibleInstance;
+              // bind healthStatus from asg instance
+              var asgInstances = serverGroup.asg.instances.filter(function(asgInstance) {
+                return asgInstance.instanceId === instance.name;
+              });
+              if (asgInstances.length) {
+                possibleInstance.healthStatus = asgInstances[0].healthStatus;
+              }
               return true;
             }
           });
         });
       });
+      if (!$scope.instance) {
+        instance.notFound = true;
+        instance.healthStatus = 'Unhealthy';
+        $scope.instance = instance;
+      }
     }
 
     if (application.data.clusters && application.data.clusters.length) {
