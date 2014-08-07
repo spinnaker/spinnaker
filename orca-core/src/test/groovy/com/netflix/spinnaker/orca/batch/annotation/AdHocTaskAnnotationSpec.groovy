63c329cf2e05539eb90cee6a4785c9ac96d8a90b
Rob Fletcher <rfletcher@netflix.com>
2014-08-07 15:13:26 +0100

    #42 annotation allowing ad-hoc tasks to be declared as spring components

---
+++ orca-core/src/test/groovy/com/netflix/spinnaker/orca/batch/annotation/AdHocTaskAnnotationSpec.groovy
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.batch.annotation
+
+import groovy.transform.CompileStatic
+import com.netflix.spinnaker.orca.DefaultTaskResult
+import com.netflix.spinnaker.orca.Task
+import com.netflix.spinnaker.orca.TaskContext
+import com.netflix.spinnaker.orca.TaskResult
+import com.netflix.spinnaker.orca.annotation.AdHocTask
+import org.springframework.beans.factory.NoSuchBeanDefinitionException
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+import static com.netflix.spinnaker.orca.TaskResult.Status.SUCCEEDED
+import static org.hamcrest.Matchers.not
+import static org.hamcrest.Matchers.sameInstance
+import static spock.util.matcher.HamcrestSupport.that
+
+@ContextConfiguration(classes = [TestTask])
+class AdHocTaskAnnotationSpec extends Specification {
+
+  @Autowired ApplicationContext applicationContext
+
+  def "ad-hoc tasks can be wired by name"() {
+    expect:
+    applicationContext.containsBean(beanName)
+
+    when:
+    applicationContext.getBean(beanName)
+
+    then:
+    notThrown NoSuchBeanDefinitionException
+
+    where:
+    beanName = "testTask"
+  }
+
+  def "ad-hoc tasks are prototype scoped"() {
+    expect:
+    that applicationContext.getBean(TestTask), not(sameInstance(applicationContext.getBean(TestTask)))
+  }
+}
+
+@CompileStatic
+@AdHocTask
+class TestTask implements Task {
+  @Override
+  TaskResult execute(TaskContext context) {
+    new DefaultTaskResult(SUCCEEDED)
+  }
+}
