e9a10e85c67c369886652db0c1a5a956ed2cc50a
Tomas Lin <clin@netflix.com>
2014-08-12 14:34:51 -0700

    formatting

---
+++ echo-cassandra/src/main/groovy/com/netflix/spinnaker/echo/events/CassandraEventListener.groovy
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.echo.events
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.netflix.spinnaker.echo.cassandra.HistoryRepository
+import com.netflix.spinnaker.echo.model.Event
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+/**
+ * Event listener for echo events
+ */
+@Component
+class CassandraEventListener implements EchoEventListener {
+
+    @Autowired
+    HistoryRepository repository
+
+    ObjectMapper mapper = new ObjectMapper()
+
+    @Override
+    void processEvent(Event event) {
+        String key = '${event.metaData.source::${event.metaData.type}::${event.metaData.date}'
+        repository.saveHistory(key, mapper.writeValueAsString(event))
+    }
+
+}
