ef60bc54e573c51412f994db58f04da224fb2560
Benjamin Malley <bmalley@netflix.com>
2014-08-21 07:03:25 +0000

    use arrow keys

--- app/scripts/controllers/globalsearch.js
+++ app/scripts/controllers/globalsearch.js
@@ -3,18 +3,41 @@
 var angular = require('angular');
 
 angular.module('deckApp')
-  .controller('GlobalSearchCtrl', function($scope, $element, infrastructureSearch) {
-    var search = infrastructureSearch();
-    $scope.$watch('query', function(query) {
-      search.query(query).then(function(result) {
-        $scope.categories = result;
-
-      });
-    });
+  .controller('GlobalSearchCtrl', function($scope, $element, infrastructureSearch, $) {
+    var search = infrastructureSearch(),
+        elem = $($element[0]);
 
     $scope.showSearchResults = false;
 
+    this.dispatchQueryInput = function(event) {
+      if (event.keyIdentifier === 'Down' && $scope.showSearchResults) {
+        elem.find('.dropdown-menu').find('a')[0].focus();
+      } else {
+        search.query($scope.query).then(function(result) {
+          $scope.categories = result;
+          $scope.showSearchResults = true;
+        });
+      }
+    };
+
     this.showSearchResults = function() {
       $scope.showSearchResults = true;
+    };
+
+    this.navigateResults = function(event) {
+      if (event.keyIdentifier === 'Down') {
+        $(event.srcElement)
+          .parent()
+          .nextAll('.result')[0]
+          .children[0]
+          .focus();
+      }
+      if (event.keyIdentifier === 'Up') {
+        $(event.srcElement)
+          .parent()
+          .prevAll('.result')[0]
+          .children[0]
+          .focus();
+      }
     };
   });
