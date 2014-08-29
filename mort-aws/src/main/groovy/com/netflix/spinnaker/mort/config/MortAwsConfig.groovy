419cb6084f8cef051b149fe58d181901aadf222c
Daniel Woods <daniel.p.woods@gmail.com>
2014-08-29 03:04:18 -0700

    basic struct

---
+++ mort-aws/src/main/groovy/com/netflix/spinnaker/mort/config/MortAwsConfig.groovy
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.mort.config
+
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.netflix.amazoncomponents.security.AmazonClientProvider
+import com.netflix.spinnaker.amos.AccountCredentialsRepository
+import com.netflix.spinnaker.amos.aws.AmazonCredentials
+import com.netflix.spinnaker.amos.aws.NetflixAssumeRoleAamzonCredentials
+import com.netflix.spinnaker.mort.aws.security.BastionCredentialsProvider
+import javax.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+
+@Configuration
+class MortAWSConfig {
+
+  @Bean
+  AmazonClientProvider amazonClientProvider() {
+    new AmazonClientProvider()
+  }
+
+  @Component
+  @ConfigurationProperties("bastion")
+  static class BastionConfiguration {
+    Boolean enabled
+    String host
+    String user
+    Integer port
+    String proxyCluster
+    String proxyRegion
+  }
+
+  @Configuration
+  @ConditionalOnExpression('${bastion.enabled:false}')
+  static class BastionCredentialsInitializer {
+    @Autowired
+    AccountCredentialsRepository accountCredentialsRepository
+
+    @Autowired
+    BastionConfiguration bastionConfiguration
+
+    @Autowired
+    AwsConfigurationProperties awsConfigurationProperties
+
+    @PostConstruct
+    void init() {
+      def provider = new BastionCredentialsProvider(bastionConfiguration.user, bastionConfiguration.host, bastionConfiguration.port, bastionConfiguration.proxyCluster,
+          bastionConfiguration.proxyRegion, awsConfigurationProperties.accountIamRole)
+
+      for (account in awsConfigurationProperties.accounts) {
+        account.credentialsProvider = provider
+        accountCredentialsRepository.save(account.name, account)
+      }
+    }
+  }
+
+  @Configuration
+  @ConditionalOnExpression('!${bastion.enabled:false}')
+  static class AmazonCredentialsInitializer {
+    @Autowired
+    AWSCredentialsProvider awsCredentialsProvider
+
+    @Autowired
+    AccountCredentialsRepository accountCredentialsRepository
+
+    @Autowired
+    AwsConfigurationProperties awsConfigurationProperties
+
+    @Value('${default.account.env:default}')
+    String defaultEnv
+
+    @PostConstruct
+    void init() {
+      if (!awsConfigurationProperties.accounts) {
+        accountCredentialsRepository.save(defaultEnv, new AmazonCredentials(name: defaultEnv))
+      } else {
+        for (account in awsConfigurationProperties.accounts) {
+          account.credentialsProvider = awsCredentialsProvider
+          accountCredentialsRepository.save(account.name, account)
+        }
+      }
+    }
+  }
+
+  @Component
+  @ConfigurationProperties("aws")
+  static class AwsConfigurationProperties {
+    // This is the IAM Role that Mort will operate under
+    String accountIamRole
+    // These are accounts that have been configured with permissions under the above assumeRole for Kato to perform operations
+    List<NetflixAssumeRoleAamzonCredentials> accounts
+  }
+}
