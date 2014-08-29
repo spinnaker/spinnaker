4ffe2969d6d9691120c5fa0cd75e915b9219d77a
Benjamin Malley <bmalley@netflix.com>
2014-08-29 18:44:44 +0000

    applications sorting fix

---
+++ app/scripts/controllers/sorttoggle.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var angular = require('angular');
+
+angular.module('deckApp')
+  .controller('SortToggleCtrl', function($scope) {
+    var ctrl = this;
+
+    if (angular.isObject($scope.$parent.sortModel)) {
+      $scope.model = $scope.$parent.sortModel;
+    } else {
+      $scope.model = {
+        reverse: false,
+      };
+      $scope.$parent.sortModel = $scope.model;
+    }
+
+    if ($scope.default === 'true') {
+      $scope.model.sortKey = $scope.key;
+    }
+
+    ctrl.setSortKey = function (key) {
+      $scope.model.sortKey = key;
+      $scope.model.reverse = ctrl.isSortKey(key) ? !$scope.model.reverse : false;
+      console.log(key, $scope.model, $scope.$parent.sortModel);
+    };
+
+    ctrl.isSortKey = function (key) {
+      return $scope.$parent.sortModel.sortKey === key;
+    };
+
+  });
--- app/scripts/directives/sorttoggle.js
+++ app/scripts/directives/sorttoggle.js
@@ -19,27 +19,9 @@ angular.module('deckApp')
         label: '@',
         'default': '@',
         onChange: '&',
-        model: '=sortingModel'
       },
       restrict: 'A',
-      controller: function ($scope) {
-        if ($scope.default === 'true') {
-          $scope.model.sortKey = $scope.key;
-        }
-
-        $scope.onChange = $scope.onChange || angular.noop;
-
-        $scope.model.reverse = false;
-        $scope.setSortKey = function (key) {
-          $scope.model.sortKey = key;
-          $scope.model.reverse = $scope.isSortKey(key) ? !$scope.model.reverse : false;
-          $scope.onChange();
-        };
-
-        $scope.isSortKey = function (key) {
-          return $scope.model.sortKey === key;
-        };
-      }
+      controller: 'SortToggleCtrl as ctrl'
     };
   }
 );
