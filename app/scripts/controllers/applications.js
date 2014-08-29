2ed7b2ae0b0e216d597920bbf39f6e3aef37ec4a
Benjamin Malley <bmalley@netflix.com>
2014-08-29 17:19:45 +0000

    remove pagination from applications view

--- app/scripts/controllers/applications.js
+++ app/scripts/controllers/applications.js
@@ -52,42 +52,8 @@ angular.module('deckApp')
       }
     ];
 
-    this.filterApplications = function filterApplications() {
-      var filtered = $filter('filter')($scope.applications, {name: $scope.applicationFilter}),
-          sorted = $filter('orderBy')(filtered, $scope.sorting.sortKey, $scope.sorting.reverse);
-      $scope.filteredApplications = sorted;
-      this.resetPaginator();
-    };
-
-    this.resultPage = function resultPage() {
-      var pagination = $scope.pagination,
-          allFiltered = $scope.filteredApplications,
-          start = (pagination.currentPage - 1) * pagination.itemsPerPage,
-          end = pagination.currentPage * pagination.itemsPerPage;
-      if (!allFiltered || !allFiltered.length) {
-        return [];
-      }
-      if (allFiltered.length < pagination.itemsPerPage) {
-        return allFiltered;
-      }
-      if (allFiltered.length < end) {
-        return allFiltered.slice(start);
-      }
-      return allFiltered.slice(start, end);
-    };
-
-    this.resetPaginator = function resetPaginator() {
-      $scope.pagination = {
-        currentPage: 1,
-        itemsPerPage: 10,
-        maxSize: 10
-      };
-    };
-
-    var ctrl = this;
     oortService.listApplications().then(function(applications) {
       $scope.applications = applications;
-      ctrl.filterApplications();
       $scope.applicationsLoaded = true;
     });
 
