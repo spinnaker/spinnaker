15d42ced1da548c4d8261f3f3424a348cebaea13
Benjamin Malley <bmalley@netflix.com>
2014-09-02 17:49:58 +0000

    use state.href

--- app/scripts/services/urlbuilder.js
+++ app/scripts/services/urlbuilder.js
@@ -4,38 +4,55 @@ var angular = require('angular');
 
 angular.module('deckApp')
   .factory('urlBuilder', function($state) {
-    var getClusters = function(input) {
-      return ['#',
-        'applications',
-        input.application,
-        'clusters'].join('/');
-    };
-
-    var getCluster = function(input) {
-      return [getClusters(input),
-        input.account,
-        input.cluster].join('/');
-    };
-
     var lookup = {
+      // url for a single serverGroup
       'serverGroups': function(input) {
-        return [
-          getCluster(input),
-          'serverGroupDetails'
-        ].join('/')+'?serverGroup='+input.serverGroup+'&accountId='+input.account+'&region='+input.region;
+        return $state.href(
+          'home.applications.application.insight.clusters.cluster.serverGroup',
+          {
+            application: input.application,
+            cluster: input.cluster,
+            account: input.account,
+            accountId: input.account,
+            region: input.region,
+            serverGroup: input.serverGroup,
+          }
+        );
       },
+      // url for a single instance
       'serverGroupInstances': function(input) {
-        return [
-          getCluster(input),
-          'instanceDetails'
-        ].join('/')+'?instanceId='+input.instanceId;
+        return $state.href(
+          'home.applications.application.insight.clusters.cluster.instanceDetails',
+          {
+            application: input.application,
+            cluster: input.cluster,
+            accountId: input.account,
+            instanceId: input.instanceId,
+            account: input.account,
+          }
+        );
       },
+      // url for a single cluster
       'clusters': function(input) {
-        return getCluster(input);
+        return $state.href(
+          'home.applications.application.insight.clusters.cluster',
+          {
+            application: input.application,
+            cluster: input.cluster,
+            account: input.account,
+          }
+        );
       },
+      // url for a single application
       'applications': function(input) {
-        return getClusters(input);
+        return $state.href(
+          'home.applications.application.insight.clusters',
+          {
+            application: input.application,
+          }
+        );
       },
+      // url for a single load balancer
       'loadBalancerServerGroups': function(input) {
         return $state.href(
           'home.applications.application.insight.loadBalancers.loadBalancerDetails',
