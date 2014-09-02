88a66d18ee1ae1e17334591a25e6e11cf9a91e95
Benjamin Malley <benjamin.malley@gmail.com>
2014-09-02 23:20:39 +0000

    use computed properties

--- app/scripts/services/pond.js
+++ app/scripts/services/pond.js
@@ -77,14 +77,22 @@ angular.module('deckApp')
 
       RestangularConfigurer.addElementTransformer('tasks', true, function(taskCollection) {
         taskCollection.forEach(filterTask);
-        taskCollection.runningCount = function() {
-          return taskCollection.reduce(function(acc, current) {
-            return current.status === 'STARTED' ? acc + 1 : acc;
-          }, 0);
-        };
-        taskCollection.hasActive = function() {
-          return taskCollection.runningCount > 0 ? true : false;
-        };
+
+        Object.defineProperties(taskCollection, {
+          runningCount: {
+            get: function() {
+              return taskCollection.reduce(function(acc, current) {
+                return current.status === 'STARTED' ? acc + 1 : acc;
+              }, 0);
+            },
+          },
+          hasActive: {
+            get: function() {
+              return taskCollection.runningCount > 0 ? true : false;
+            },
+          },
+        });
+
         return taskCollection;
       });
 
