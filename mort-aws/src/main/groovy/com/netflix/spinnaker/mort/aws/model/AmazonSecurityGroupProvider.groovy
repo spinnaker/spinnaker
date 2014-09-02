44314fafed14729eb6d23d059fe832bbd3f3483c
Tomas Lin <clin@netflix.com>
2014-09-02 16:30:37 -0700

    controller endpoints for security groups

--- mort-aws/src/main/groovy/com/netflix/spinnaker/mort/aws/model/AmazonSecurityGroupProvider.groovy
+++ mort-aws/src/main/groovy/com/netflix/spinnaker/mort/aws/model/AmazonSecurityGroupProvider.groovy
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 /**
- *
+ * A provider for amazon security groups
  */
 @Component
 class AmazonSecurityGroupProvider implements SecurityGroupProvider {
@@ -19,27 +19,53 @@ class AmazonSecurityGroupProvider implements SecurityGroupProvider {
   @Override
   Set<SecurityGroup> getAll() {
     def keys = cacheService.keysByType(Keys.Namespace.SECURITY_GROUPS)
-    (List<AmazonSecurityGroup>) keys.collect { cacheService.retrieve(it, AmazonSecurityGroup) }
+    (List<AmazonSecurityGroup>) keys.collect { retrieve(it) }
   }
 
   @Override
   Set<SecurityGroup> getAllByRegion(String region) {
-    return null
+    def keys = cacheService.keysByType(Keys.Namespace.SECURITY_GROUPS).findAll { key ->
+      def parts = Keys.parse(key)
+      parts.region == region
+    }
+    keys.collect { key ->
+      retrieve(key)
+    }
   }
 
   @Override
   Set<SecurityGroup> getAllByAccount(String account) {
-    return null
+    def keys = cacheService.keysByType(Keys.Namespace.SECURITY_GROUPS).findAll { key ->
+      def parts = Keys.parse(key)
+      parts.account == account
+    }
+    keys.collect { key ->
+      retrieve(key)
+    }
   }
 
   @Override
   Set<SecurityGroup> getAllByAccountAndRegion(String account, String region) {
-    return null
+    def keys = cacheService.keysByType(Keys.Namespace.SECURITY_GROUPS).findAll { key ->
+      def parts = Keys.parse(key)
+      parts.account == account && parts.region == region
+    }
+    keys.collect { key ->
+      retrieve(key)
+    }
   }
 
   @Override
   SecurityGroup get(String account, String name) {
-    return null
+    def keys = cacheService.keysByType(Keys.Namespace.SECURITY_GROUPS).findAll { key ->
+      def parts = Keys.parse(key)
+      parts.account == account && parts.id == name
+    }
+    keys.empty ? null : retrieve(keys.first())
+  }
+
+  private SecurityGroup retrieve(String key) {
+    cacheService.retrieve(key, AmazonSecurityGroup)
   }
 
 }
