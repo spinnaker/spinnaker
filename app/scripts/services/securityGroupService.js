a7c82636e57b24a898107d8422a088838de9d3c9
chrisb <chrisb@netflix.com>
2014-09-14 17:54:18 -0700

    safely attach security groups to asg

--- app/scripts/services/securityGroupService.js
+++ app/scripts/services/securityGroupService.js
@@ -48,6 +48,7 @@ angular.module('deckApp')
         }
       });
       application.serverGroups.forEach(function(serverGroup) {
+        if (serverGroup.launchConfig) {
           serverGroup.launchConfig.securityGroups.forEach(function (securityGroupId) {
             var securityGroup = indexedSecurityGroups[serverGroup.account][serverGroup.region][securityGroupId];
             if (!securityGroup) {
@@ -60,6 +61,7 @@ angular.module('deckApp')
               applicationSecurityGroups.push(securityGroup);
             }
           });
+        }
       });
 
       application.securityGroups = _.unique(applicationSecurityGroups);
