b6f37dcfa714a7bfeb5aa9b95b84ce613efe2433
chrisb <chrisb@netflix.com>
2014-09-22 21:18:11 -0700

    two-stage application list loading, adding pagination back

--- app/scripts/controllers/applications.js
+++ app/scripts/controllers/applications.js
@@ -4,22 +4,13 @@ require('../app');
 var angular = require('angular');
 
 angular.module('deckApp')
-  .controller('ApplicationsCtrl', function($scope, $exceptionHandler, $modal, $log, $filter, RxService, front50, notifications, oortService) {
+  .controller('ApplicationsCtrl', function($scope, $exceptionHandler, $modal, $log, $filter, RxService, front50, notifications, oortService, searchService) {
 
     $scope.applicationsLoaded = false;
 
     $scope.sorting = {
       sortKey: 'name',
       reverse: false
-    };
-
-    this.demoMultiPageModal = function() {
-      $modal.open(
-        {
-          templateUrl: 'views/modal/multiPageWrapper.html',
-          scope: $scope
-        }
-      );
     };
 
     $scope.applicationFilter = '';
@@ -52,9 +43,57 @@ angular.module('deckApp')
       }
     ];
 
+    searchService.search({q:'', type: 'applications', pageSize:100000}).then(function(response) {
+      if (!$scope.applications) {
+        $scope.applications = response.data[0].results.map(function (app) {
+          return {name: app.application};
+        });
+        ctrl.filterApplications();
+        $scope.partiallyLoaded = true;
+        $scope.applicationsLoaded = true;
+      }
+    });
+
+    this.filterApplications = function filterApplications() {
+      var filtered = $filter('filter')($scope.applications, {name: $scope.applicationFilter}),
+        sorted = $filter('orderBy')(filtered, $scope.sorting.sortKey, $scope.sorting.reverse);
+      $scope.filteredApplications = sorted;
+      this.resetPaginator();
+    };
+
+    this.resultPage = function resultPage() {
+      var pagination = $scope.pagination,
+        allFiltered = $scope.filteredApplications,
+        start = (pagination.currentPage - 1) * pagination.itemsPerPage,
+        end = pagination.currentPage * pagination.itemsPerPage;
+      if (!allFiltered || !allFiltered.length) {
+        return [];
+      }
+      if (allFiltered.length < pagination.itemsPerPage) {
+        return allFiltered;
+      }
+      if (allFiltered.length < end) {
+        return allFiltered.slice(start);
+      }
+      return allFiltered.slice(start, end);
+    };
+
+    this.resetPaginator = function resetPaginator() {
+      $scope.pagination = {
+        currentPage: 1,
+        itemsPerPage: 12,
+        maxSize: 12
+      };
+    };
+
+    var ctrl = this;
+
     oortService.listApplications().then(function(applications) {
       $scope.applications = applications;
+      ctrl.filterApplications();
       $scope.applicationsLoaded = true;
+      $scope.partiallyLoaded = false;
+      $scope.fullyLoaded = true;
     });
 
   }
