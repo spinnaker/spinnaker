08a0fda28279cdc2e1db81a1d888efac5d367860
Dan Woods <daniel.p.woods@gmail.com>
2014-09-24 11:59:19 -0700

    Update settings.js

--- app/scripts/settings/settings.js
+++ app/scripts/settings/settings.js
@@ -2,10 +2,10 @@
 
 angular.module('deckApp')
   .constant('settings', {
-    front50Url: 'http://front50.test.netflix.net',
+    front50Url: 'http://front50.prod.netflix.net',
     oortUrl: 'http://oort.prod.netflix.net',
     mortUrl: 'http://mort.prod.netflix.net',
-    pondUrl: 'http://pond.test.netflix.net',
+    pondUrl: 'http://pond.prod.netflix.net',
     awsMetadataUrl: 'http://spinnaker.test.netflix.net/aws',
     credentialsUrl: 'http://kato.prod.netflix.net/',
     accounts: {
