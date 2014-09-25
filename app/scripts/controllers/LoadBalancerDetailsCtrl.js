ec35a262c80b6a0b7a84fb09868d32ff7f5395cf
chrisb <chrisb@netflix.com>
2014-09-25 13:09:08 -0700

    consistent display of security groups

--- app/scripts/controllers/LoadBalancerDetailsCtrl.js
+++ app/scripts/controllers/LoadBalancerDetailsCtrl.js
@@ -4,11 +4,22 @@ require('../app');
 var angular = require('angular');
 
 angular.module('deckApp')
-  .controller('LoadBalancerDetailsCtrl', function ($scope, $rootScope, loadBalancer, application, securityGroupService, $modal) {
+  .controller('LoadBalancerDetailsCtrl', function ($scope, $rootScope, loadBalancer, application, securityGroupService, $modal, _) {
 
     $scope.loadBalancer = application.loadBalancers.filter(function (test) {
       return test.name === loadBalancer.name && test.region === loadBalancer.region && test.account === loadBalancer.accountId;
     })[0];
+
+    if ($scope.loadBalancer && $scope.loadBalancer.elb && $scope.loadBalancer.elb.securityGroups) {
+      var securityGroups = [];
+      $scope.loadBalancer.elb.securityGroups.forEach(function(securityGroupId) {
+        var match = securityGroupService.getApplicationSecurityGroup(application, loadBalancer.accountId, loadBalancer.region, securityGroupId);
+        if (match) {
+          securityGroups.push(match);
+        }
+      });
+      $scope.securityGroups = _.sortBy(securityGroups, 'name');
+    }
 
     this.editLoadBalancer = function editLoadBalancer() {
       $modal.open({
