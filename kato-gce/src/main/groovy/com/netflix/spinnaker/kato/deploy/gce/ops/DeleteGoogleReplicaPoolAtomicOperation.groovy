fcc28dd62c77368e5f83ed9c5417c5fb7bfb7968
duftler <duftler@google.com>
2014-09-25 09:21:27 -0400

    Initial support for 'deleteGoogleReplicaPoolDescription'.

---
+++ kato-gce/src/main/groovy/com/netflix/spinnaker/kato/deploy/gce/ops/DeleteGoogleReplicaPoolAtomicOperation.groovy
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2014 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.deploy.gce.ops
+
+import com.google.api.services.replicapool.model.PoolsDeleteRequest
+import com.google.api.services.replicapool.ReplicapoolScopes
+import com.netflix.spinnaker.kato.data.task.Task
+import com.netflix.spinnaker.kato.data.task.TaskRepository
+import com.netflix.spinnaker.kato.deploy.gce.description.DeleteGoogleReplicaPoolDescription
+import com.netflix.spinnaker.kato.orchestration.AtomicOperation
+
+class DeleteGoogleReplicaPoolAtomicOperation implements AtomicOperation<Void> {
+  // TODO(duftler): This should move to a common location.
+  private static final String APPLICATION_NAME = "Spinnaker"
+  private static final String BASE_PHASE = "DELETE_REPLICA_POOL"
+
+  private static Task getTask() {
+    TaskRepository.threadLocalTask.get()
+  }
+
+  private final DeleteGoogleReplicaPoolDescription description
+  private final ReplicaPoolBuilder replicaPoolBuilder
+
+  DeleteGoogleReplicaPoolAtomicOperation(DeleteGoogleReplicaPoolDescription description,
+                                         ReplicaPoolBuilder replicaPoolBuilder) {
+    this.description = description
+    this.replicaPoolBuilder = replicaPoolBuilder
+  }
+
+  @Override
+  Void operate(List priorOutputs) {
+    // TODO(duftler): Implement proper sequential naming and tests for same. Best done when this class is reconciled
+    // with logic in BasicGoogleDeployHandler.
+    def replicaPoolName = "${description.application}-${description.stack}-v000"
+
+    task.updateStatus BASE_PHASE, "Initializing delete of replica pool $replicaPoolName in ${description.zone}..."
+
+    def project = description.credentials.project
+
+    def credentialBuilder = description.credentials.createCredentialBuilder(ReplicapoolScopes.REPLICAPOOL)
+
+    def replicapool = replicaPoolBuilder.buildReplicaPool(credentialBuilder, APPLICATION_NAME);
+
+    replicapool.pools().delete(project, description.zone, replicaPoolName, new PoolsDeleteRequest()).execute()
+
+    task.updateStatus BASE_PHASE, "Done deleting replica pool $replicaPoolName in ${description.zone}."
+    null
+  }
+}
---
+++ kato-gce/src/test/groovy/com/netflix/spinnaker/kato/deploy/gce/ops/DeleteGoogleReplicaPoolAtomicOperationUnitSpec.groovy
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2014 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.deploy.gce.ops
+
+import com.google.api.services.compute.Compute
+import com.google.api.services.replicapool.Replicapool
+import com.netflix.spinnaker.kato.data.task.Task
+import com.netflix.spinnaker.kato.data.task.TaskRepository
+import com.netflix.spinnaker.kato.deploy.gce.description.DeleteGoogleReplicaPoolDescription
+import com.netflix.spinnaker.kato.security.gce.GoogleCredentials
+import spock.lang.Specification
+import spock.lang.Subject
+
+class DeleteGoogleReplicaPoolAtomicOperationUnitSpec extends Specification {
+  private static final ACCOUNT_NAME = "auto"
+  private static final PROJECT_NAME = "my_project"
+  private static final APPLICATION = "spinnaker"
+  private static final STACK = "spinnaker-test"
+  private static final ZONE = "us-central1-b"
+
+  def setupSpec() {
+    TaskRepository.threadLocalTask.set(Mock(Task))
+  }
+
+  void "should delete replica pool"() {
+    setup:
+      def computeMock = Mock(Compute)
+      def replicaPoolBuilderMock = Mock(ReplicaPoolBuilder)
+      def replicaPoolMock = Mock(Replicapool)
+      def poolsMock = Mock(Replicapool.Pools)
+      def poolsDeleteMock = Mock(Replicapool.Pools.Delete)
+      def credentials = new GoogleCredentials(PROJECT_NAME, computeMock)
+      def description = new DeleteGoogleReplicaPoolDescription(application: APPLICATION,
+                                                               stack: STACK,
+                                                               zone: ZONE,
+                                                               accountName: ACCOUNT_NAME,
+                                                               credentials: credentials)
+      @Subject def operation = new DeleteGoogleReplicaPoolAtomicOperation(description, replicaPoolBuilderMock)
+
+    when:
+      operation.operate([])
+
+    then:
+      1 * replicaPoolBuilderMock.buildReplicaPool(_, _) >> replicaPoolMock
+      1 * replicaPoolMock.pools() >> poolsMock
+      1 * poolsMock.delete(PROJECT_NAME, ZONE, _, _) >> poolsDeleteMock
+      1 * poolsDeleteMock.execute()
+  }
+}
