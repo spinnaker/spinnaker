99769c637297c820530a851d26c106c7b99aa7b8
Benjamin Malley <benjamin.malley@gmail.com>
2014-09-25 10:06:21 -0700

    pass application to view

--- app/scripts/controllers/tasks.js
+++ app/scripts/controllers/tasks.js
@@ -6,10 +6,7 @@ var angular = require('angular');
 angular.module('deckApp')
   .controller('TasksCtrl', function ($scope, application) {
     $scope.taskStateFilter = 'All';
-
-    $scope.subscribed = {
-      data: application.tasks,
-    };
+    $scope.application = application;
 
   }
 );
