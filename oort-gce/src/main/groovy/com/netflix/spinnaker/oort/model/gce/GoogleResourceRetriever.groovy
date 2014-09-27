62ca4a914e0fd9639b50029879b097111fa0ce2c
duftler <duftler@google.com>
2014-09-27 15:31:49 -0400

    Improve handling of account names in GoogleResourceRetriever.

--- oort-gce/src/main/groovy/com/netflix/spinnaker/oort/model/gce/GoogleResourceRetriever.groovy
+++ oort-gce/src/main/groovy/com/netflix/spinnaker/oort/model/gce/GoogleResourceRetriever.groovy
@@ -38,8 +38,8 @@ class GoogleResourceRetriever {
   // TODO(duftler): Need to query all zones.
   private static final String ZONE = "us-central1-b"
 
-  private static final String ACCOUNT_NAME = "default"
   private static final String GOOGLE_SERVER_GROUP_TYPE = "gce"
+  private static final String GOOGLE_INSTANCE_TYPE = "gce"
 
   // The value of this field is always assigned atomically and the map is never modified after assignment.
   private static appMap = new HashMap<String, GoogleApplication>()
@@ -67,61 +67,67 @@ class GoogleResourceRetriever {
     def tempAppMap = new HashMap<String, GoogleApplication>()
     def simpleDateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
-    for (GoogleCredentials credentials : getAllGoogleCredentialsObjects(accountCredentialsProvider)) {
-      def project = credentials.project
-      def credentialBuilder = credentials.createCredentialBuilder(ReplicapoolScopes.REPLICAPOOL)
-      def replicapool = new ReplicaPoolBuilder().buildReplicaPool(credentialBuilder, APPLICATION_NAME);
-      def poolsListResult = replicapool.pools().list(project, ZONE).execute()
+    getAllGoogleCredentialsObjects(accountCredentialsProvider).each {
+      def accountName = it.key
+      def credentialsSet = it.value
 
-      for (def pool : poolsListResult.resources) {
-        def names = Names.parseName(pool.name)
-        def appName = names.app.toLowerCase()
+      for (GoogleCredentials credentials : credentialsSet) {
+        def project = credentials.project
+        def credentialBuilder = credentials.createCredentialBuilder(ReplicapoolScopes.REPLICAPOOL)
+        def replicapool = new ReplicaPoolBuilder().buildReplicaPool(credentialBuilder, APPLICATION_NAME);
+        def poolsListResult = replicapool.pools().list(project, ZONE).execute()
 
-        if (appName) {
-          if (!tempAppMap[appName]) {
-            tempAppMap[appName] = new GoogleApplication(name: appName)
-            tempAppMap[appName].clusterNames[ACCOUNT_NAME] = new HashSet<String>()
-            tempAppMap[appName].clusters[ACCOUNT_NAME] = new HashMap<String, GoogleCluster>()
+        for (def pool : poolsListResult.resources) {
+          def names = Names.parseName(pool.name)
+          def appName = names.app.toLowerCase()
+
+          if (appName) {
+            if (!tempAppMap[appName]) {
+              tempAppMap[appName] = new GoogleApplication(name: appName)
+              tempAppMap[appName].clusterNames[accountName] = new HashSet<String>()
+              tempAppMap[appName].clusters[accountName] = new HashMap<String, GoogleCluster>()
+            }
+
+            if (!tempAppMap[appName].clusters[accountName][names.cluster]) {
+              tempAppMap[appName].clusters[accountName][names.cluster] = new GoogleCluster(name: names.cluster, accountName: accountName)
+            }
+
+            def cluster = tempAppMap[appName].clusters[accountName][names.cluster]
+
+            // pool.name == names.group
+            def googleServerGroup = new GoogleServerGroup(pool.name, GOOGLE_SERVER_GROUP_TYPE, REGION)
+            googleServerGroup.zones << ZONE
+
+            def instanceIdSet = new HashSet<>()
+            def earliestReplicaTimestamp = Long.MAX_VALUE
+            def replicasListResult = replicapool.replicas().list(project, ZONE, pool.name).execute()
+            for (def replica : replicasListResult.resources) {
+              long replicaTimestamp = simpleDateFormat.parse(replica.status.vmStartTime).getTime()
+              // Use earliest replica launchTime as createdTime of replica pool for now.
+              earliestReplicaTimestamp = Math.min(earliestReplicaTimestamp, replicaTimestamp)
+
+              def googleInstance = new GoogleInstance(replica.name)
+              googleInstance.setProperty("isHealthy", replica.status.state == "RUNNING")
+              // oort.aws puts a com.amazonaws.services.ec2.model.Instance here. More importantly, deck expects it.
+              googleInstance.setProperty("instance", [instanceId: replica.name,
+                                                      instanceType: pool.template.vmParams.machineType,
+                                                      providerType: GOOGLE_INSTANCE_TYPE,
+                                                      launchTime: replicaTimestamp,
+                                                      placement : [availabilityZone: ZONE]])
+              googleServerGroup.instances << googleInstance
+
+              instanceIdSet << [instanceId: replica.name]
+            }
+
+            // oort.aws puts a com.amazonaws.services.autoscaling.model.AutoScalingGroup here. More importantly, deck expects it.
+            googleServerGroup.setProperty("asg", [createdTime: earliestReplicaTimestamp,
+                                                  instances  : instanceIdSet,
+                                                  minSize    : pool.currentNumReplicas,
+                                                  maxSize    : pool.currentNumReplicas,
+                                                  desiredCapacity: pool.currentNumReplicas])
+
+            cluster.serverGroups << googleServerGroup
           }
-
-          if (!tempAppMap[appName].clusters[ACCOUNT_NAME][names.cluster]) {
-            tempAppMap[appName].clusters[ACCOUNT_NAME][names.cluster] = new GoogleCluster(name: names.cluster, accountName: ACCOUNT_NAME)
-          }
-
-          def cluster = tempAppMap[appName].clusters[ACCOUNT_NAME][names.cluster]
-
-          // pool.name == names.group
-          def googleServerGroup = new GoogleServerGroup(pool.name, GOOGLE_SERVER_GROUP_TYPE, REGION)
-          googleServerGroup.zones << ZONE
-
-          def instanceIdSet = new HashSet<>()
-          def earliestReplicaTimestamp = Long.MAX_VALUE
-          def replicasListResult = replicapool.replicas().list(project, ZONE, pool.name).execute()
-          for (def replica : replicasListResult.resources) {
-            long replicaTimestamp = simpleDateFormat.parse(replica.status.vmStartTime).getTime()
-            // Use earliest replica launchTime as createdTime of replica pool for now.
-            earliestReplicaTimestamp = Math.min(earliestReplicaTimestamp, replicaTimestamp)
-
-            def googleInstance = new GoogleInstance(replica.name)
-            googleInstance.setProperty("isHealthy", replica.status.state == "RUNNING")
-            // oort.aws puts a com.amazonaws.services.ec2.model.Instance here. More importantly, deck expects it.
-            googleInstance.setProperty("instance", [instanceId: replica.name,
-                                                    instanceType: pool.template.vmParams.machineType,
-                                                    launchTime: replicaTimestamp,
-                                                    placement: [availabilityZone: ZONE]])
-            googleServerGroup.instances << googleInstance
-
-            instanceIdSet << [instanceId: replica.name]
-          }
-
-          // oort.aws puts a com.amazonaws.services.autoscaling.model.AutoScalingGroup here. More importantly, deck expects it.
-          googleServerGroup.setProperty("asg", [createdTime: earliestReplicaTimestamp,
-                                                instances: instanceIdSet,
-                                                minSize: pool.currentNumReplicas,
-                                                maxSize: pool.currentNumReplicas,
-                                                desiredCapacity: pool.currentNumReplicas])
-
-          cluster.serverGroups << googleServerGroup
         }
       }
     }
@@ -131,20 +137,26 @@ class GoogleResourceRetriever {
     log.info "Finished loading GCE resources."
   }
 
-  private static Set<GoogleCredentials> getAllGoogleCredentialsObjects(AccountCredentialsProvider accountCredentialsProvider) {
-    def allGoogleCredentialsSet = new HashSet<GoogleCredentials>()
+  static Map<String, Set<GoogleCredentials>> getAllGoogleCredentialsObjects(AccountCredentialsProvider accountCredentialsProvider) {
+    def accountNameToSetOfGoogleCredentialsMap = new HashMap<String, Set<GoogleCredentials>>()
 
     for (def accountCredentials : accountCredentialsProvider.getAll()) {
       try {
         if (accountCredentials.credentials instanceof GoogleCredentials) {
-          allGoogleCredentialsSet.add(accountCredentials.credentials)
+          def accountName = accountCredentials.getName()
+
+          if (!accountNameToSetOfGoogleCredentialsMap.containsKey(accountName)) {
+            accountNameToSetOfGoogleCredentialsMap[accountName] = new HashSet<GoogleCredentials>()
+          }
+
+          accountNameToSetOfGoogleCredentialsMap[accountName] << accountCredentials.credentials
         }
       } catch (e) {
         log.info "Squashed exception ${e.getClass().getName()} thrown by $accountCredentials."
       }
     }
 
-    allGoogleCredentialsSet
+    accountNameToSetOfGoogleCredentialsMap
   }
 
   Map<String, GoogleApplication> getApplicationsMap() {
