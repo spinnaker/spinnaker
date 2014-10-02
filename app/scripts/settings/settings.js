17378ebdb62ae5e6d81b54768a5610bc424799ff
chrisb <chrisb@netflix.com>
2014-10-02 10:00:10 -0700

    settings to prestaging

--- app/scripts/settings/settings.js
+++ app/scripts/settings/settings.js
@@ -3,11 +3,11 @@
 angular.module('deckApp')
   .constant('settings', {
     front50Url: 'http://front50-staging.prod.netflix.net',
-    oortUrl: 'http://oort-staging.prod.netflix.net',
-    mortUrl: 'http://mort-staging.prod.netflix.net',
-    pondUrl: 'http://pond-staging.prod.netflix.net',
+    oortUrl: 'http://oort-prestaging.prod.netflix.net',
+    mortUrl: 'http://mort-prestaging.prod.netflix.net',
+    pondUrl: 'http://pond-prestaging.prod.netflix.net',
     awsMetadataUrl: 'http://spinnaker.test.netflix.net/aws',
-    credentialsUrl: 'http://kato-staging.prod.netflix.net/',
+    credentialsUrl: 'http://kato-prestaging.prod.netflix.net/',
     accounts: {
       prod: {
         challengeDestructiveActions: true
