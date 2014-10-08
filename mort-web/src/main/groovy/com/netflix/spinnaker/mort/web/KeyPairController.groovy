4e526f76ca6cdc93da8b2f5942ea289288708407
Clay McCoy <git@claymccoy.com>
2014-10-08 14:50:40 -0700

    resource should be plural for consistency

--- mort-web/src/main/groovy/com/netflix/spinnaker/mort/web/KeyPairController.groovy
+++ mort-web/src/main/groovy/com/netflix/spinnaker/mort/web/KeyPairController.groovy
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/keyPair")
+@RequestMapping("/keyPairs")
 @RestController
 class KeyPairController {
 
