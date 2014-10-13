4861119fbe677c2c01278d7aa2440c3161ff2adf
Benjamin Malley <benjamin.malley@gmail.com>
2014-10-13 16:17:41 -0700

    load deck without initializing cache

--- test/spec/services/pondKato.js
+++ test/spec/services/pondKato.js
@@ -22,7 +22,7 @@ describe('Service: Pond - task complete, task force refresh', function() {
 
   var service, $http, config, scope, timeout, task;
 
-  beforeEach(module('deckApp'));
+  beforeEach(loadDeckWithoutCacheInitializer);
 
   beforeEach(inject(function(settings, pond, $httpBackend, $rootScope, $timeout) {
 
