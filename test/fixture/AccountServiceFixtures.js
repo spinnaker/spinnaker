e782f4d0c223935ce42ea04e02624e678b661aef
chrisb <chrisb@netflix.com>
2014-10-16 10:28:36 -0700

    use preferred availability zones by default; fixes #169

---
+++ test/fixture/AccountServiceFixtures.js
@@ -0,0 +1,38 @@
+var AccountServiceFixture = {};
+AccountServiceFixture.regionsKeyedByAccount = {
+  'test': {
+    regions: [
+      {
+        name: 'us-east-1',
+        availabilityZones: ['a','b','c']
+      },
+      {
+        name: 'us-west-1',
+        availabilityZones: ['b','c','d']
+      }
+    ]
+  },
+  'prod': {
+    regions: [
+      {
+        name: 'us-east-1',
+        availabilityZones: ['a','b','c','d','e']
+      },
+      {
+        name: 'us-west-1',
+        availabilityZones: ['a','b','c','g','h','i']
+      }
+    ]
+  }
+};
+
+AccountServiceFixture.preferredZonesByAccount = {
+  test: {
+    'us-east-1': ['a','b','c'],
+    'us-west-1': ['c','d']
+  },
+  prod: {
+    'us-east-1': ['d','e'],
+    'us-west-1': ['g','h','i']
+  }
+};
---
+++ test/fixture/SecurityGroupServiceFixtures.js
@@ -0,0 +1,15 @@
+var SecurityGroupServiceFixture = {};
+SecurityGroupServiceFixture.allSecurityGroups = {
+  'test': {
+    aws: {
+      'us-east-1': [],
+      'us-west-1': []
+    }
+  },
+  'prod': {
+    aws: {
+      'us-east-1': [],
+      'us-west-1': []
+    }
+  }
+};
