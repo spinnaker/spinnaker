26cc1a19af8a7326660a6a2ff9b2523d38b74b31
Benjamin Malley <benjamin.malley@gmail.com>
2014-10-20 22:37:04 -0700

    use jasmine async support

--- test/e2e/applications.js
+++ test/e2e/applications.js
@@ -5,26 +5,58 @@ var newApplicationModal = require('../pages/newApplicationModal.js');
 var Q = require('q');
 
 describe('applications view', function() {
+  beforeEach(function() {
+    this.openMenu = function() {
+      browser.get(page.url);
+      return page.menu.click();
+    };
+  });
+
   it('should redirect to /applications from /', function() {
     browser.get('/');
     expect(page.header.getText()).toEqual('Applications');
   });
 
-  it('should create an application', function() {
-    browser.get(page.url);
-    page.menu.click().then(function() {
-      expect(page.createApplicationMenuItem.isDisplayed()).toBe(true);
-      page.createApplicationMenuItem.click().then(function() {
-        browser.sleep(200);
-        expect(newApplicationModal.header.isDisplayed()).toBe(true);
+  describe('the Actions menu', function() {
+
+    describe('Create Application', function () {
+      it('is accessed from the Actions menu', function(done) {
+        this.openMenu().then(function() {
+          done();
+        });
+      });
+
+      it('launches the Create Application modal', function(done) {
+        browser.wait(page.createApplicationMenuItem.isDisplayed,
+          undefined,
+          'Failed to open the Actions menu').then(function() {
+          page.createApplicationMenuItem.click().then(function() {
+            browser.wait(newApplicationModal.header.isDisplayed,
+              undefined,
+              'Failed to launch the new application modal').then(function() {
+                done(); // done() fails when provided with an argument :(
+              }, console.log);
+          }, console.log);
+        }, console.log);
+      });
+
+      it('takes a name, email, description and account', function(done) {
+        // TODO: write validation tests
         Q.all([
           newApplicationModal.name.sendKeys('deck-e2e-test'),
           newApplicationModal.description.sendKeys('a deck test'),
           newApplicationModal.email.sendKeys('delivery-engineering+deck-e2e@netflix.com'),
           newApplicationModal.prod.click(),
         ]).then(function() {
-          newApplicationModal.submit.click();
-        });
+          done();
+        }, console.log);
+      });
+
+      it('creates the application', function(done) {
+        // TODO: application creation is currently busted in prestaging
+        newApplicationModal.submit.click().then(function() {
+          done();
+        }, console.log);
       });
     });
   });
