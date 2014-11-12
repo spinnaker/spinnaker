32e52718f3b9d4d9a5f63cb30bc781288cb3a85c
Daniel Woods <daniel.p.woods@gmail.com>
2014-11-12 02:29:38 -0800

    Fixes #138 - Push seq deriving from groups outside the deploy region

--- kato-aws/src/main/groovy/com/netflix/spinnaker/kato/deploy/aws/AutoScalingWorker.groovy
+++ kato-aws/src/main/groovy/com/netflix/spinnaker/kato/deploy/aws/AutoScalingWorker.groovy
@@ -219,7 +219,7 @@ class AutoScalingWorker {
     }
     asgs.findAll { AutoScalingGroup asg ->
       def names = Names.parseName(asg.autoScalingGroupName)
-      names.sequence >= 0 && application == names.app && (stack ? stack == names.stack : true)
+      names.sequence >= 0 && application == names.app && (stack || names.stack ? stack == names.stack : true)
     }?.max({ a, b -> a.autoScalingGroupName <=> b.autoScalingGroupName }) ?: null
   }
 
