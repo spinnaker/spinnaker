c05e34c78e5980fea3cdf57e7edf51c36e80b210
chrisb <chrisb@netflix.com>
2014-11-21 16:55:55 -0800

    add deployment strategy to clone operation

--- app/scripts/services/awsServerGroupService.js
+++ app/scripts/services/awsServerGroupService.js
@@ -64,6 +64,7 @@ angular.module('deckApp')
 
         var command = {
           application: application.name,
+          strategy: '',
           stack: serverGroupName.stack,
           freeFormDetails: serverGroupName.freeFormDetails,
           credentials: serverGroup.account,
