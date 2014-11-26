8a640605f950a25ab2faf24074a107ccf595c786
Benjamin Klingher <bklingher@google.com>
2014-11-26 15:32:45 -0500

    Adds support for HTTP load balancer creation in GCE.

---
+++ kato-gce/src/main/groovy/com/netflix/spinnaker/kato/deploy/gce/ops/CreateGoogleHttpLoadBalancerAtomicOperation.groovy
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2014 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.deploy.gce.ops
+
+import com.google.api.services.compute.model.Backend
+import com.google.api.services.compute.model.BackendService
+import com.google.api.services.compute.model.ForwardingRule
+import com.google.api.services.compute.model.HostRule
+import com.google.api.services.compute.model.PathMatcher
+import com.google.api.services.compute.model.PathRule
+import com.google.api.services.compute.model.TargetHttpProxy
+import com.google.api.services.compute.model.UrlMap
+import com.netflix.spinnaker.kato.data.task.Task
+import com.netflix.spinnaker.kato.data.task.TaskRepository
+import com.netflix.spinnaker.kato.deploy.DeploymentResult
+import com.netflix.spinnaker.kato.deploy.gce.GCEUtil
+import com.netflix.spinnaker.kato.deploy.gce.description.CreateGoogleHttpLoadBalancerDescription
+import com.netflix.spinnaker.kato.orchestration.AtomicOperation
+
+class CreateGoogleHttpLoadBalancerAtomicOperation  implements AtomicOperation<DeploymentResult> {
+  private static final String BASE_PHASE = "CREATE_HTTP_LB"
+  private static final String HEALTH_CHECK_NAME_PREFIX = "health-check"
+  private static final String BACKEND_SERVICE_NAME_PREFIX = "backend-service"
+  private static final String URL_MAP_NAME_PREFIX = "url-map"
+  private static final String TARGET_HTTP_PROXY_NAME_PREFIX = "target-http-proxy"
+  private static final String BACKEND_PORT_NAME = "http"
+  private static final String IP_PROTOCOL = "HTTP"
+  private static final String DEFAULT_PORT_RANGE = "80"
+
+  private static Task getTask() {
+    TaskRepository.threadLocalTask.get()
+  }
+
+  private final CreateGoogleHttpLoadBalancerDescription description
+
+  CreateGoogleHttpLoadBalancerAtomicOperation(CreateGoogleHttpLoadBalancerDescription description) {
+    this.description = description
+  }
+
+  /**
+   * curl -X POST -H "Content-Type: application/json" -d '[ { "createGoogleHttpLoadBalancerDescription": {"credentials": "my-account-name", "loadBalancerName": "spin-lb"}} ]' localhost:8501/ops
+   *
+   * @param description
+   * @param priorOutputs
+   * @return
+   */
+  @Override
+  DeploymentResult operate(List priorOutputs) {
+    task.updateStatus BASE_PHASE, "Initializing create of HTTP load balancer $description.loadBalancerName..."
+
+    if (!description.credentials) {
+      throw new IllegalArgumentException("Unable to resolve credentials for Google account '${description.accountName}'.")
+    }
+
+    def compute = description.credentials.compute
+    def project = description.credentials.project
+
+    // Make health check.
+    def healthCheckName = String.format("%s-%s-%d", description.loadBalancerName, HEALTH_CHECK_NAME_PREFIX, System.currentTimeMillis())
+
+    task.updateStatus BASE_PHASE, "Creating health check $healthCheckName..."
+
+    def httpHealthChecksResourceLinks = new ArrayList<String>();
+    def httpHealthCheck = GCEUtil.makeHttpHealthCheck(healthCheckName, description.healthCheck)
+    def httpHealthCheckResourceLink = compute.httpHealthChecks().insert(project, httpHealthCheck).execute().getTargetLink()
+    httpHealthChecksResourceLinks.add(httpHealthCheckResourceLink)
+
+    // Make backend service.
+    def backends = new ArrayList<Backend>();
+    for (backend in description.backends) {
+      backends.add(new Backend(
+          group: backend.group,
+          balancingMode: backend.balancingMode,
+          maxUtilization: backend.maxUtilization,
+          capacityScaler: backend.capacityScaler
+      ))
+    }
+
+    def backendServiceName = String.format("%s-%s-%d", description.loadBalancerName, BACKEND_SERVICE_NAME_PREFIX, System.currentTimeMillis())
+
+    task.updateStatus BASE_PHASE, "Creating backend service $backendServiceName..."
+
+    def backendService = new BackendService(
+        name: backendServiceName,
+        backends: backends,
+        timeoutSec: description.backendTimeoutSec,
+        healthChecks: httpHealthChecksResourceLinks,
+        port: description.backendPort,
+        portName: BACKEND_PORT_NAME,
+        protocol: IP_PROTOCOL,
+    )
+    def backendServiceLink = compute.backendServices().insert(project, backendService).execute().getTargetLink()
+
+    // Make URL map.
+    def pathMatchers = new ArrayList<PathMatcher>();
+    for (pathMatcher in description.pathMatchers) {
+      def pathRules = new ArrayList<PathRule>();
+      for (pathRule in pathMatcher.pathRules) {
+        pathRules.add(
+            paths: pathRule.paths,
+            service: pathRule.service
+        )
+      }
+      pathMatchers.add(
+          name: pathMatcher.name,
+          defaultService: pathMatcher.defaultService,
+          pathRules: pathRules
+      )
+    }
+
+    def hostRules = new ArrayList<HostRule>();
+    for (hostRule in description.hostRules) {
+      hostRules.add(
+          hosts: hostRule.hosts,
+          pathMatcher: hostRule.pathMatcher
+      )
+    }
+
+    def urlMapName = String.format("%s-%s-%d", description.loadBalancerName, URL_MAP_NAME_PREFIX, System.currentTimeMillis())
+
+    task.updateStatus BASE_PHASE, "Creating URL map $urlMapName..."
+
+    def urlMap = new UrlMap(
+        name: urlMapName,
+        pathMatchers: pathMatchers,
+        hostRules: hostRules,
+        defaultService: backendServiceLink
+    )
+    def urlMapLink = compute.urlMaps().insert(project, urlMap).execute().getTargetLink()
+
+    // Make target HTTP proxy.
+    def targetHttpProxyName = String.format("%s-%s-%d", description.loadBalancerName, TARGET_HTTP_PROXY_NAME_PREFIX, System.currentTimeMillis())
+
+    task.updateStatus BASE_PHASE, "Creating target HTTP proxy $targetHttpProxyName..."
+
+    def targetHttpProxy = new TargetHttpProxy(
+        name: targetHttpProxyName,
+        urlMap: urlMapLink
+    )
+    def targetHttpProxyLink = compute.targetHttpProxies().insert(project, targetHttpProxy).execute().getTargetLink()
+
+    // Finally, make forwarding rule.
+    task.updateStatus BASE_PHASE, "Creating forwarding rule $description.loadBalancerName..."
+
+    def portRange
+    if (!description.portRange) {
+      portRange = DEFAULT_PORT_RANGE
+    } else {
+      portRange = description.portRange
+    }
+
+    def forwardingRule = new ForwardingRule(
+        name: description.loadBalancerName,
+        iPAddress: description.ipAddress,
+        portRange: portRange,
+        target: targetHttpProxyLink
+    )
+    compute.globalForwardingRules().insert(project, forwardingRule).execute()
+
+    task.updateStatus BASE_PHASE, "Done."
+    null
+  }
+}
---
+++ kato-gce/src/test/groovy/com/netflix/spinnaker/kato/deploy/gce/validators/CreateGoogleHttpLoadBalancerDescriptionValidatorSpec.groovy
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2014 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.deploy.gce.validators
+
+import com.netflix.spinnaker.amos.DefaultAccountCredentialsProvider
+import com.netflix.spinnaker.amos.MapBackedAccountCredentialsRepository
+import com.netflix.spinnaker.kato.deploy.gce.description.CreateGoogleHttpLoadBalancerDescription
+import com.netflix.spinnaker.kato.security.gce.GoogleCredentials
+import com.netflix.spinnaker.kato.security.gce.GoogleNamedAccountCredentials
+import org.springframework.validation.Errors
+import spock.lang.Shared
+import spock.lang.Specification
+
+class CreateGoogleHttpLoadBalancerDescriptionValidatorSpec extends Specification {
+  private static final LOAD_BALANCER_NAME = "spinnaker-test-v000"
+  private static final ACCOUNT_NAME = "auto"
+  private static final INSTANCE_GROUP = "group"
+  private static final BALANCING_MODE = "super_balanced"
+  private static final IP_ADDRESS = "1.1.1.1"
+  private static final PORT_RANGE = "80-82"
+  private static final SERVICE = "service"
+  private static final PATH = "/path"
+  private static final HOST = "*"
+  private static final MATCHER = "matcher"
+
+  @Shared
+  CreateGoogleHttpLoadBalancerDescriptionValidator validator
+
+  void setupSpec() {
+    validator = new CreateGoogleHttpLoadBalancerDescriptionValidator()
+    def credentialsRepo = new MapBackedAccountCredentialsRepository()
+    def credentialsProvider = new DefaultAccountCredentialsProvider(credentialsRepo)
+    def credentials = Mock(GoogleNamedAccountCredentials)
+    credentials.getName() >> ACCOUNT_NAME
+    credentials.getCredentials() >> new GoogleCredentials()
+    credentialsRepo.save(ACCOUNT_NAME, credentials)
+    validator.accountCredentialsProvider = credentialsProvider
+  }
+
+  void "pass validation with proper description inputs"() {
+    setup:
+      def description = new CreateGoogleHttpLoadBalancerDescription([
+          loadBalancerName: LOAD_BALANCER_NAME,
+          accountName: ACCOUNT_NAME,
+          backendTimeoutSec: 30,
+          healthCheck: [
+            port: 8080,
+            checkIntervalSec: 5,
+            healthyThreshold: 2,
+            unhealthyThreshold: 2,
+            timeoutSec: 5,
+            requestPath: "/"
+          ],
+          backends: [[group: INSTANCE_GROUP, balancingMode: BALANCING_MODE]],
+          ipAddress: IP_ADDRESS,
+          portRange: PORT_RANGE,
+          hostRules: [[hosts: [HOST], pathMatcher: MATCHER]],
+          pathMatchers: [[name: MATCHER, defaultService: SERVICE, pathRules: [[paths: [PATH], service: SERVICE]]]]
+      ])
+      def errors = Mock(Errors)
+
+    when:
+      validator.validate([], description, errors)
+
+    then:
+      0 * errors._
+  }
+
+  void "fail validation with pathMatchers and no hostRules"() {
+    setup:
+    def description = new CreateGoogleHttpLoadBalancerDescription([
+        loadBalancerName: LOAD_BALANCER_NAME,
+        accountName: ACCOUNT_NAME,
+        backendTimeoutSec: 30,
+        backends: [[group: INSTANCE_GROUP, balancingMode: BALANCING_MODE]],
+        ipAddress: IP_ADDRESS,
+        portRange: PORT_RANGE,
+        pathMatchers: [[name: MATCHER, defaultService: SERVICE, pathRules: [[paths: [PATH], service: SERVICE]]]]
+    ])
+    def errors = Mock(Errors)
+
+    when:
+    validator.validate([], description, errors)
+
+    then:
+    1 * errors.rejectValue('hostRules', _)
+  }
+
+  void "fail validation with hostRules and no pathMatchers"() {
+    setup:
+    def description = new CreateGoogleHttpLoadBalancerDescription([
+        loadBalancerName: LOAD_BALANCER_NAME,
+        accountName: ACCOUNT_NAME,
+        backendTimeoutSec: 30,
+        backends: [[group: INSTANCE_GROUP, balancingMode: BALANCING_MODE]],
+        ipAddress: IP_ADDRESS,
+        portRange: PORT_RANGE,
+        hostRules: [[hosts: [HOST], pathMatcher: MATCHER]],
+    ])
+    def errors = Mock(Errors)
+
+    when:
+    validator.validate([], description, errors)
+
+    then:
+    1 * errors.rejectValue('pathMatchers', _)
+  }
+
+  void "fail validation with more pathMatchers than hostRules"() {
+    setup:
+    def description = new CreateGoogleHttpLoadBalancerDescription([
+        loadBalancerName: LOAD_BALANCER_NAME,
+        accountName: ACCOUNT_NAME,
+        backendTimeoutSec: 30,
+        backends: [[group: INSTANCE_GROUP, balancingMode: BALANCING_MODE]],
+        ipAddress: IP_ADDRESS,
+        portRange: PORT_RANGE,
+        hostRules: [[hosts: [HOST], pathMatcher: MATCHER]],
+        pathMatchers: [[name: MATCHER, defaultService: SERVICE, pathRules: [[paths: [PATH], service: SERVICE]]],
+                       [name: MATCHER, defaultService: SERVICE, pathRules: [[paths: [PATH], service: SERVICE]]]]
+    ])
+    def errors = Mock(Errors)
+
+    when:
+    validator.validate([], description, errors)
+
+    then:
+    1 * errors.rejectValue('pathMatchers', _)
+  }
+
+  void "fail validation with no name for pathMatcher and no pathMacher field in hostRule"() {
+    setup:
+    def description = new CreateGoogleHttpLoadBalancerDescription([
+        loadBalancerName: LOAD_BALANCER_NAME,
+        accountName: ACCOUNT_NAME,
+        backendTimeoutSec: 30,
+        healthCheck: [
+            port: 8080,
+            checkIntervalSec: 5,
+            healthyThreshold: 2,
+            unhealthyThreshold: 2,
+            timeoutSec: 5,
+            requestPath: "/"
+        ],
+        backends: [[group: INSTANCE_GROUP, balancingMode: BALANCING_MODE]],
+        ipAddress: IP_ADDRESS,
+        portRange: PORT_RANGE,
+        hostRules: [[hosts: [HOST]]],
+        pathMatchers: [[defaultService: SERVICE, pathRules: [[paths: [PATH], service: SERVICE]]]]
+    ])
+    def errors = Mock(Errors)
+
+    when:
+    validator.validate([], description, errors)
+
+    then:
+    1 * errors.rejectValue('pathMatchers', _)
+    1 * errors.rejectValue('hostRules', _)
+  }
+
+  void "pass validation with minimal description"() {
+    setup:
+      def description = new CreateGoogleHttpLoadBalancerDescription(
+          loadBalancerName: LOAD_BALANCER_NAME,
+          accountName: ACCOUNT_NAME)
+      def errors = Mock(Errors)
+
+    when:
+      validator.validate([], description, errors)
+
+    then:
+      0 * errors._
+  }
+
+  void "null input fails validation"() {
+    setup:
+      def description = new CreateGoogleHttpLoadBalancerDescription()
+      def errors = Mock(Errors)
+
+    when:
+      validator.validate([], description, errors)
+
+    then:
+      1 * errors.rejectValue('credentials', _)
+      1 * errors.rejectValue('loadBalancerName', _)
+  }
+}
