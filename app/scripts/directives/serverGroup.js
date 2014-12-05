dcf06d1d825a5ad221eeb243d168e311fa9d9800
chrisb <chrisb@netflix.com>
2014-12-04 23:07:22 -0800

    allow clicking anywhere in server group pod to load details; fixes #308

--- app/scripts/directives/serverGroup.js
+++ app/scripts/directives/serverGroup.js
@@ -2,7 +2,7 @@
 
 
 angular.module('deckApp')
-  .directive('serverGroup', function ($rootScope) {
+  .directive('serverGroup', function ($rootScope, $timeout) {
     return {
       restrict: 'E',
       replace: true,
@@ -12,8 +12,29 @@ angular.module('deckApp')
         serverGroup: '=',
         displayOptions: '='
       },
-      link: function (scope) {
+      link: function (scope, el) {
+        // stolen from uiSref directive
+        var base = el.parent().inheritedData('$uiView').state;
+
         scope.$state = $rootScope.$state;
+
+        scope.loadDetails = function(e) {
+          $timeout(function() {
+            var serverGroup = scope.serverGroup;
+            // anything handled by ui-sref or actual links should be ignored
+            if (e.isDefaultPrevented() || (e.originalEvent && e.originalEvent.target.href)) {
+              return;
+            }
+            var params = {
+              region: serverGroup.region,
+              accountId: serverGroup.account,
+              serverGroup: serverGroup.name
+            };
+            // also stolen from uiSref directive
+            scope.$state.go('.serverGroup', params, {relative: base, inherit: true});
+          });
+        };
+
         if (scope.serverGroup.isDisabled) {
           scope.serverGroup.instances.forEach(function(instance) {
             instance.healthStatus = 'Disabled';
