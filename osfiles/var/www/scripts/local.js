aac4f3fa58daa29353a6fac3e995192edbce380a
Daniel Woods <daniel.p.woods@gmail.com>
2014-12-09 07:24:09 -0800

    update configuration for local settings

---
+++ osfiles/var/www/scripts/local.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('deckApp')
+  .constant('settings', {
+    front50Url: '/front50',
+    oortUrl: '/oort',
+    mortUrl: '/mort',
+    pondUrl: '/pond',
+    katoUrl: '/kato',
+    credentialsUrl: '/kato',
+    feedbackUrl: '/hootch',
+    gateUrl: '/gate',
+    accounts: {
+      prod: {
+        challengeDestructiveActions: true
+      }
+    },
+    pollSchedule: 30000,
+    providers: ['aws'],
+    primaryAccounts: ['default'],
+    primaryRegions: ['eu-west-1','us-east-1','us-west-1','us-west-2'],
+    defaults: {
+      account: 'default',
+      region: 'us-east-1'
+    }
+  });
+
