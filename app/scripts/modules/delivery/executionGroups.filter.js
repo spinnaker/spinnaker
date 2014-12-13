5adf7f429ad259b13e26afe123b89839e4f2a74a
chrisb <chrisb@netflix.com>
2014-12-12 17:32:10 -0800

    move delivery tab location; handle no pipelines/executions scenarios

--- app/scripts/modules/delivery/executionGroups.filter.js
+++ app/scripts/modules/delivery/executionGroups.filter.js
@@ -3,7 +3,6 @@
 angular.module('deckApp.delivery')
   .filter('executionGroups', function(timeBoundaries) {
     return function(executions, filter) {
-      executions = executions || [];
       switch (filter.execution.groupBy) {
         case 'timeBoundary':
           return Object.keys(timeBoundaries.groupByTimeBoundary(executions));
