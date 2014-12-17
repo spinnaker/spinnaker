96e44ff32c200a959f9471200e3fe72ccea341ab
chrisb <chrisb@netflix.com>
2014-12-17 12:28:59 -0800

    remove redundant settings to old endpoints

--- app/scripts/settings/settings.js
+++ app/scripts/settings/settings.js
@@ -2,14 +2,6 @@
 
 angular.module('deckApp.settings', [])
   .constant('settings', {
-    front50Url: 'http://front50-prestaging.prod.netflix.net',
-    oortUrl: 'http://oort.prod.netflix.net',
-    mortUrl: 'http://mort-staging.prod.netflix.net',
-    pondUrl: 'http://pond-prestaging.prod.netflix.net',
-    katoUrl: 'http://kato-prestaging.prod.netflix.net',
-    igorUrl: 'http://igor-prestaging.prod.netflix.net',
-    mayoUrl: 'http://mayo-prestaging.prod.netflix.net',
-    credentialsUrl: 'http://kato-prestaging.prod.netflix.net/',
     feedbackUrl: 'http://hootch.test.netflix.net/submit',
     gateUrl: 'http://spinnaker-api-prestaging.prod.netflix.net',
     accounts: {
