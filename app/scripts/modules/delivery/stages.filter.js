fa17274b9e81429994f98a2217d8abe1ed7e46a5
chrisb <chrisb@netflix.com>
2014-12-17 16:03:47 -0800

    hack for broken start times; normalized status

--- app/scripts/modules/delivery/stages.filter.js
+++ app/scripts/modules/delivery/stages.filter.js
@@ -5,9 +5,9 @@ angular.module('deckApp.delivery')
     return function(stages, filter) {
       return stages.filter(function(stage) {
         return filter.stage.name[stage.name] &&
-          filter.stage.status[stage.status.toLowerCase()] ||
+          filter.stage.status[stage.normalizedStatus.toLowerCase()] ||
           (filter.stage.scale === 'fixed' &&
-             stage.status.toLowerCase() === 'not_started' &&
+             stage.normalizedStatus.toLowerCase() === 'not_started' &&
              filter.stage.name[stage.name]);
       });
     };
