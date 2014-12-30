fb3cedd758c03c2f9269566c437d7b1907041c55
chrisb <chrisb@netflix.com>
2014-12-29 22:34:20 -0800

    remove more dead artifacts

--- app/scripts/controllers/BuildTimelinesCtrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('deckApp')
-  .controller('BuildTimelinesCtrl', function($scope, pipelines) {
-    pipelines.getAll().then(function(p) {
-      $scope.pipelines = p;
-    });
-  });
