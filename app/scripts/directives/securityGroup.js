fec5b6d2618668b9fc88fac225046664fe1b6d68
chrisb <chrisb@netflix.com>
2015-01-09 15:15:49 -0800

    fix hover color on security groups, load balancers

--- app/scripts/directives/securityGroup.js
+++ app/scripts/directives/securityGroup.js
@@ -2,7 +2,7 @@
 
 
 angular.module('deckApp')
-  .directive('securityGroup', function ($rootScope) {
+  .directive('securityGroup', function ($rootScope, $timeout) {
     return {
       restrict: 'E',
       replace: true,
@@ -11,8 +11,28 @@ angular.module('deckApp')
         securityGroup: '=',
         displayOptions: '='
       },
-      link: function (scope) {
+      link: function (scope, el) {
+        var base = el.parent().inheritedData('$uiView').state;
+
         scope.$state = $rootScope.$state;
+
+        scope.loadDetails = function(e) {
+          $timeout(function() {
+            var securityGroup = scope.securityGroup;
+            // anything handled by ui-sref or actual links should be ignored
+            if (e.isDefaultPrevented() || (e.originalEvent && e.originalEvent.target.href)) {
+              return;
+            }
+            var params = {
+              region: securityGroup.region,
+              accountId: securityGroup.accountName,
+              name: securityGroup.name,
+            };
+            // also stolen from uiSref directive
+            scope.$state.go('.securityGroupDetails', params, {relative: base, inherit: true});
+          });
+        };
+
       }
     };
   }
