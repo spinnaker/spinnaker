87b1170678002dca6530647a7fa4e8d787959029
Daniel Woods <daniel.p.woods@gmail.com>
2015-01-18 11:44:14 -0800

    Modify Scaling Process stage

--- app/scripts/settings/helpContents.js
+++ app/scripts/settings/helpContents.js
@@ -47,6 +47,7 @@ angular.module('deckApp')
     'loadBalancer.advancedSettings.healthyThreshold': '<p>Configures the number of healthy observations before reinstituting an instance into the ELB’s traffic rotation.</p><p>Default: <b>10</b></p>',
     'loadBalancer.advancedSettings.unhealthyThreshold': '<p>Configures the number of unhealthy observations before deservicing an instance from the ELB.</p><p>Default: <b>2</b></p>',
     'pipeline.config.resizeAsg.cluster': '<p>Configures the cluster upon which this resize operation will act. The <em>target</em> specifies what ASG to resolve for the operation.</p>',
+    'pipeline.config.modifyScalingProcess.cluster': '<p>Configures the cluster upon which this resize operation will act. The <em>target</em> specifies what ASG to resolve for the operation.</p>',
 
     'serverGroup.description': '<p>A server group is a collection of instances managed together. </p>' +
       '<ul>' +
