42a9b7a75c55ba1810bd81d43c2492e8a5ce4609
chrisb <chrisb@netflix.com>
2015-01-27 15:04:23 -0800

    directives to toggle display of elements based on provider count

---
+++ app/scripts/modules/account/providerToggles.directive.js
@@ -0,0 +1,32 @@
+'use strict';
+
+
+angular.module('deckApp.account')
+  .directive('ifMultipleProviders', function(accountService) {
+    return {
+      restrict: 'A',
+      link: function(scope, elem) {
+        accountService.listProviders().then(function(providers) {
+          if (providers && providers.length && providers.length > 1) {
+            elem.show();
+          } else {
+            elem.hide();
+          }
+        });
+      }
+    };
+  })
+  .directive('ifSingleProvider', function(accountService) {
+    return {
+      restrict: 'A',
+      link: function(scope, elem) {
+        accountService.listProviders().then(function(providers) {
+          if (!providers || !providers.length || providers.length === 1) {
+            elem.show();
+          } else {
+            elem.hide();
+          }
+        });
+      }
+    };
+  });
