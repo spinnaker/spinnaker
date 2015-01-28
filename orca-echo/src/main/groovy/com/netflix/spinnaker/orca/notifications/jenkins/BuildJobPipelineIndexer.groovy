98a33a00024ffcb89bbd0cad79253f0b6c41e4f3
Rob Fletcher <rfletcher@netflix.com>
2015-01-28 10:33:00 +0000

    Use RxJava to background polling for pipelines in trigger mechanism

--- orca-echo/src/main/groovy/com/netflix/spinnaker/orca/notifications/jenkins/BuildJobPipelineIndexer.groovy
+++ orca-echo/src/main/groovy/com/netflix/spinnaker/orca/notifications/jenkins/BuildJobPipelineIndexer.groovy
@@ -1,14 +1,20 @@
 package com.netflix.spinnaker.orca.notifications.jenkins
 
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
+import groovy.util.logging.Log4j
 import javax.annotation.PostConstruct
+import javax.annotation.PreDestroy
+import com.google.common.annotations.VisibleForTesting
 import com.google.common.collect.ImmutableMap
 import com.netflix.spinnaker.orca.mayo.services.PipelineConfigurationService
 import com.netflix.spinnaker.orca.notifications.PipelineIndexer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import rx.Scheduler
+import rx.Subscription
+import rx.schedulers.Schedulers
+import static java.util.concurrent.TimeUnit.SECONDS
 
 /**
  * Indexes pipelines known by mayo for consumption by a
@@ -16,7 +22,8 @@ import org.springframework.stereotype.Component
  */
 @Component
 @CompileStatic
-class BuildJobPipelineIndexer implements PipelineIndexer, Runnable {
+@Log4j
+class BuildJobPipelineIndexer implements PipelineIndexer {
 
   private static final String TRIGGER_KEY = "job"
   private static final String TRIGGER_MASTER = "master"
@@ -24,6 +31,8 @@ class BuildJobPipelineIndexer implements PipelineIndexer, Runnable {
   final long pollingInterval = 60
   private final PipelineConfigurationService pipelineConfigurationService
   private Map<Trigger, Collection<Map>> interestingPipelines = [:]
+  private Scheduler scheduler = Schedulers.io()
+  private Subscription subscription
 
   @Autowired
   BuildJobPipelineIndexer(PipelineConfigurationService pipelineConfigurationService) {
@@ -31,8 +40,22 @@ class BuildJobPipelineIndexer implements PipelineIndexer, Runnable {
   }
 
   @PostConstruct
+  @CompileDynamic
   void init() {
-    Executors.newSingleThreadScheduledExecutor().schedule(this, pollingInterval, TimeUnit.SECONDS)
+    subscription = rx.Observable.interval(pollingInterval, SECONDS, scheduler).map {
+      try {
+        poll()
+      } catch (e) {
+        log.error "Caught exception polling for pipelines", e
+      }
+    } subscribe { Map<Trigger, Collection<Map>> pipelines ->
+      interestingPipelines = pipelines
+    }
+  }
+
+  @PreDestroy
+  void shutdown() {
+    subscription.unsubscribe()
   }
 
   @Override
@@ -40,25 +63,25 @@ class BuildJobPipelineIndexer implements PipelineIndexer, Runnable {
     ImmutableMap.copyOf(interestingPipelines)
   }
 
-  @Override
-  void run() {
-    try {
-      Map<Trigger, Collection<Map>> _interestingPipelines = [:]
-      for (Map pipeline in pipelineConfigurationService.pipelines) {
-        def triggers = pipeline.triggers as List<Map>
-        for (trigger in triggers) {
-          if (trigger.type == BuildJobNotificationHandler.TRIGGER_TYPE) {
-            def key = new Trigger(trigger[TRIGGER_MASTER] as String, trigger[TRIGGER_KEY] as String)
-            if (!_interestingPipelines.containsKey(key)) {
-              _interestingPipelines[key] = []
-            }
-            _interestingPipelines[key] << pipeline
+  private Map<Trigger, Collection<Map>> poll() {
+    Map<Trigger, Collection<Map>> _interestingPipelines = [:]
+    for (pipeline in pipelineConfigurationService.pipelines) {
+      def triggers = pipeline.triggers as List<Map>
+      for (trigger in triggers) {
+        if (trigger.type == BuildJobNotificationHandler.TRIGGER_TYPE) {
+          def key = new Trigger(trigger[TRIGGER_MASTER] as String, trigger[TRIGGER_KEY] as String)
+          if (!_interestingPipelines.containsKey(key)) {
+            _interestingPipelines[key] = []
           }
+          _interestingPipelines[key] << pipeline
         }
       }
-      this.interestingPipelines = _interestingPipelines
-    } catch (e) {
-      e.printStackTrace()
     }
+    _interestingPipelines
+  }
+
+  @VisibleForTesting
+  void setScheduler(Scheduler scheduler) {
+    this.scheduler = scheduler
   }
 }
--- orca-echo/src/test/groovy/com/netflix/spinnaker/orca/notifications/jenkins/BuildJobPipelineIndexerSpec.groovy
+++ orca-echo/src/test/groovy/com/netflix/spinnaker/orca/notifications/jenkins/BuildJobPipelineIndexerSpec.groovy
@@ -1,15 +1,20 @@
 package com.netflix.spinnaker.orca.notifications.jenkins
 
 import com.netflix.spinnaker.orca.mayo.services.PipelineConfigurationService
+import retrofit.MockHttpException
+import rx.schedulers.Schedulers
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
+import static java.util.concurrent.TimeUnit.SECONDS
 
 class BuildJobPipelineIndexerSpec extends Specification {
 
-  def pipelineConfigurationService = Stub(PipelineConfigurationService)
+  def pipelineConfigurationService = Mock(PipelineConfigurationService)
 
   @Subject
   def pipelineIndexer = new BuildJobPipelineIndexer(pipelineConfigurationService)
+  @Shared def scheduler = Schedulers.test()
 
   def pipeline1 = [
       name    : "pipeline1",
@@ -37,15 +42,48 @@ class BuildJobPipelineIndexerSpec extends Specification {
       stages  : []
   ]
 
-  void "should add multiple pipeline targets to single trigger type"() {
-    setup:
+  def setup() {
+    pipelineIndexer.scheduler = scheduler
+  }
+
+  def "should add multiple pipeline targets to single trigger type"() {
+    given:
     pipelineConfigurationService.getPipelines() >> [pipeline1, pipeline2, pipeline3]
 
+    and:
+    pipelineIndexer.init()
+
     when:
-    pipelineIndexer.run()
+    scheduler.advanceTimeBy(pipelineIndexer.pollingInterval, SECONDS)
 
     then:
     pipelineIndexer.pipelines[key].name == ["pipeline1", "pipeline2"]
+
+    cleanup:
+    pipelineIndexer.shutdown()
+
+    where:
+    key = new Trigger("master1", "SPINNAKER-package-pond")
+  }
+
+  def "should continue polling despite errors"() {
+    given: "we'll get an error the second time we poll"
+    pipelineConfigurationService.getPipelines() >> [] >> {
+      throw MockHttpException.newInternalError(null)
+    }
+
+    and: "we have polled twice"
+    pipelineIndexer.init()
+    scheduler.advanceTimeBy(pipelineIndexer.pollingInterval * 2, SECONDS)
+
+    when: "the next poll interval occurs"
+    scheduler.advanceTimeBy(pipelineIndexer.pollingInterval, SECONDS)
+
+    then: "the polling thread hasn't died"
+    1 * pipelineConfigurationService.getPipelines() >> []
+
+    cleanup:
+    pipelineIndexer.shutdown()
 
     where:
     key = new Trigger("master1", "SPINNAKER-package-pond")
