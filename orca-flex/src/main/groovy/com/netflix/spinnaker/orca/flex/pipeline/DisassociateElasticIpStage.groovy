cb364cfc34b14ba79733dd2cba655561a4f3824c
Adam Jordens <ajordens@netflix.com>
2015-02-10 23:29:40 -0800

    Elastic IP tasks (associate/disassociate)
    - RetrofitErrorHandler now does a better job of dealing with UNEXPECTED RetrofitErrors

---
+++ orca-flex/src/main/groovy/com/netflix/spinnaker/orca/flex/pipeline/DisassociateElasticIpStage.groovy
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.flex.pipeline
+
+import com.netflix.spinnaker.orca.flex.tasks.DisassociateElasticIpTask
+import com.netflix.spinnaker.orca.pipeline.LinearStage
+import com.netflix.spinnaker.orca.pipeline.model.Stage
+import groovy.transform.CompileStatic
+import org.springframework.batch.core.Step
+import org.springframework.stereotype.Component
+
+@Component
+@CompileStatic
+class DisassociateElasticIpStage extends LinearStage {
+  public static final String MAYO_CONFIG_TYPE = "disassociateElasticIp"
+
+  DisassociateElasticIpStage() {
+    super(MAYO_CONFIG_TYPE)
+  }
+
+  @Override
+  protected List<Step> buildSteps(Stage stage) {
+    def step1 = buildStep(stage, "disassociateElasticIp", DisassociateElasticIpTask)
+    [step1]
+  }
+}
