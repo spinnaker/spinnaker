360412285ee5a62cf696754e84a84acc6d393758
Zan Thrash <zanthrash@gmail.com>
2015-02-12 12:36:00 -0800

    tweeking protracter config

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,6 @@
 exports.config = {
   seleniumServerJar: './node_modules/selenium-server/lib/runner/selenium-server-standalone-2.43.1.jar',
+  chromeDriver: './node_modules/chromedriver/lib/chromedriver/chromedriver',
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://0.0.0.0:9000',
   specs: 'test/e2e/**/*.js',
