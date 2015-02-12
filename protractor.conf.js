92d3fa2280e35f13e801af906ed6830971f83137
Zan Thrash <zanthrash@gmail.com>
2015-02-12 14:25:52 -0800

    e2e testing

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  seleniumServerJar: './node_modules/selenium-server/lib/runner/selenium-server-standalone-2.43.1.jar',
+  seleniumServerJar: './node_modules/selenium-server/lib/runner/selenium-server-standalone-2.44.0.jar',
   chromeDriver: './node_modules/chromedriver/lib/chromedriver/chromedriver',
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://0.0.0.0:9000',
