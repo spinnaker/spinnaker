0e501b6fb0465a8ea4323fe29527ded570dc8ed0
Adam Jordens <ajordens@netflix.com>
2015-02-12 23:10:52 -0800

    Re-swagger the credentials controller

--- front50-web/src/main/groovy/com/netflix/spinnaker/front50/controllers/CredentialsController.groovy
+++ front50-web/src/main/groovy/com/netflix/spinnaker/front50/controllers/CredentialsController.groovy
@@ -16,8 +16,11 @@
 
 package com.netflix.spinnaker.front50.controllers
 
+import com.netflix.spectator.api.ExtendedRegistry
 import com.netflix.spinnaker.amos.AccountCredentialsProvider
 import com.netflix.spinnaker.front50.model.application.GlobalAccountCredentials
+import com.wordnik.swagger.annotations.Api
+import com.wordnik.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -25,13 +28,17 @@ import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/credentials")
 @RestController
+@Api(value = "credential", description = "Credential API")
 class CredentialsController {
-
   @Autowired
   AccountCredentialsProvider accountCredentialsProvider
 
+  @Autowired
+  ExtendedRegistry extendedRegistry
+
+  @ApiOperation(value = "", notes = "Fetch all account details")
   @RequestMapping(method = RequestMethod.GET)
   List<Map> list() {
-    accountCredentialsProvider.all.collect { [name: it.name, global: it instanceof GlobalAccountCredentials] }
+    return accountCredentialsProvider.all.collect { [name: it.name, global: it instanceof GlobalAccountCredentials] }
   }
 }
