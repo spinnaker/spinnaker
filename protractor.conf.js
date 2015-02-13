7adb9b06968ef9286a3f3a6e88d59edcddd6f496
Zan Thrash <zanthrash@gmail.com>
2015-02-12 16:24:59 -0800

    e2e testing

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,6 +2,9 @@ exports.config = {
   seleniumServerJar: './node_modules/selenium-server/lib/runner/selenium-server-standalone-2.44.0.jar',
   chromeDriver: './node_modules/chromedriver/lib/chromedriver/chromedriver',
   //seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  framework: 'jasmine',
+
   baseUrl: 'http://0.0.0.0:9000',
   specs: 'test/e2e/**/*.js',
   getPageTimeout: 20000,
@@ -10,6 +13,11 @@ exports.config = {
     'browserName': 'chrome',
   },
   onPrepare: function() {
-    browser.driver.manage().window().setSize(1500, 800);
+    //browser.driver.manage().window().setSize(1500, 800);
+
+    require('jasmine-reporters');
+    jasmine.getEnv().addReporter(
+      new jasmine.JUnitXmlReporter('xmloutput', true, true)
+    );
   }
 };
