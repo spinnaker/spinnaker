8d46765ab464529ea80f75e5914f5c5f32cbdfc9
Adam Jordens <ajordens@netflix.com>
2015-02-13 17:12:35 -0800

    Migrate MetricsInterceptor to the korkWeb equivalent

--- front50-web/src/main/groovy/com/netflix/spinnaker/front50/config/Front50WebConfig.groovy
+++ front50-web/src/main/groovy/com/netflix/spinnaker/front50/config/Front50WebConfig.groovy
@@ -17,7 +17,7 @@
 package com.netflix.spinnaker.front50.config
 
 import com.netflix.spectator.api.ExtendedRegistry
-import com.netflix.spinnaker.front50.interceptors.MetricsInterceptor
+import com.netflix.spinnaker.kork.web.interceptors.MetricsInterceptor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
@@ -31,7 +31,11 @@ public class Front50WebConfig extends WebMvcConfigurerAdapter {
   ExtendedRegistry extendedRegistry
 
   @Override
-  public void addInterceptors(InterceptorRegistry registry){
-    registry.addInterceptor(new MetricsInterceptor(extendedRegistry));
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(
+        new MetricsInterceptor(
+            extendedRegistry, "controller.invocations", ["account", "application"], ["BasicErrorController"]
+        )
+    )
   }
 }
