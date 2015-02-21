320dfaddd4f55314188d7964b7b73813f31f87cb
chrisb <chrisb@netflix.com>
2015-02-20 16:48:00 -0800

    pod scroll behavior tweaks

--- app/scripts/modules/utils/scrollTriggerService.js
+++ app/scripts/modules/utils/scrollTriggerService.js
@@ -4,7 +4,6 @@
 angular.module('deckApp.utils.scrollTrigger', ['deckApp.utils.jQuery'])
   .factory('scrollTriggerService', function($window, $timeout, $) {
     var eventRegistry = Object.create(null),// creates {} with no prototype; ES6 Maps would be preferable (available in Chrome 38?),
-        waypointRegistry = Object.create(null),
         registryCounter = 0,
         scrollEventActive = Object.create(null),
         $$window = $($window);
@@ -40,73 +39,6 @@ angular.module('deckApp.utils.scrollTrigger', ['deckApp.utils.jQuery'])
       activateScrollEvent(scrollTarget);
     }
 
-    function registerWaypointContainer(elementScope, element, key, offset) {
-      waypointRegistry[key] = waypointRegistry[key] || Object.create(null);
-      waypointRegistry[key].container = element;
-      waypointRegistry[key].offset = offset;
-      enableWaypointEvent(element, key);
-      elementScope.$on('$destroy', function() {
-        waypointRegistry[key] = null;
-        disableWaypointEvent(key);
-      });
-    }
-
-    function enableWaypointEvent(element, key) {
-      var registryEntry = waypointRegistry[key];
-      if (!registryEntry.scrollEnabled) {
-        // because they do not affect rendering directly, we can debounce this pretty liberally
-        element.bind('scroll.waypointEvents resize.waypointEvents', _.debounce(function() {
-          var containerRect = element.get(0).getBoundingClientRect(),
-              topThreshold = containerRect.top + registryEntry.offset,
-              waypoints = element.find('[waypoint]'),
-              inView = [];
-          waypoints.each(function(idx, waypoint) {
-            var waypointRect = waypoint.getBoundingClientRect();
-            if (waypointRect.bottom >= topThreshold && waypointRect.top <= containerRect.bottom) {
-              inView.push({ top: waypointRect.top, elem: waypoint.getAttribute('waypoint') });
-            }
-          });
-
-          waypointRegistry[key].lastWindow = _(inView).sortBy('top').pluck('elem').valueOf();
-
-        }, 300));
-        registryEntry.scrollEnabled = true;
-      }
-    }
-
-    function disableWaypointEvent(key) {
-      var registry = waypointRegistry[key];
-      if (registry) {
-        registry.container.unbind('scroll.waypointEvents resize.waypointEvents');
-        registry.scrollEnabled = false;
-      }
-    }
-
-    function restoreToWaypoint(key) {
-      $timeout(function() {
-        var registry = waypointRegistry[key];
-        if (!registry) {
-          return;
-        }
-
-        var candidates = registry.lastWindow || [],
-            container = registry.container,
-            containerScrollTop = container.scrollTop(),
-            containerTop = container.offset().top;
-
-        candidates.every(function(candidate) {
-          var elem = $('[waypoint="' + candidate + '"]', container);
-          if (elem.length) {
-            container.scrollTop(containerScrollTop + elem.offset().top - containerTop - registry.offset);
-            container.trigger('scroll');
-            return false;
-          }
-          return true;
-
-        });
-      }, 50);
-    }
-
     function deregister(scrollTarget, registeredEventId) {
       if (eventRegistry[scrollTarget] && eventRegistry[scrollTarget][registeredEventId]) {
         delete eventRegistry[scrollTarget][registeredEventId];
@@ -117,8 +49,7 @@ angular.module('deckApp.utils.scrollTrigger', ['deckApp.utils.jQuery'])
     }
 
     function eventIsInView(registeredEvent, scrollBottom) {
-      var elementRect = registeredEvent.element.get(0).getBoundingClientRect(),
-          elementTop = elementRect.bottom - elementRect.height;
+      var elementTop = registeredEvent.element.get(0).getBoundingClientRect().top;
       if (elementTop < 0) {
         return false;
       }
@@ -156,10 +87,6 @@ angular.module('deckApp.utils.scrollTrigger', ['deckApp.utils.jQuery'])
 
     return {
       register: register,
-      registerWaypointContainer: registerWaypointContainer,
-      enableWaypointEvent: enableWaypointEvent,
-      disableWaypointEvent: disableWaypointEvent,
-      restoreToWaypoint: restoreToWaypoint,
     };
   }
 );
