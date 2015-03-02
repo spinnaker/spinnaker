ac2b3328e98c699db26564e4c25a61952e7bc185
chrisb <chrisb@netflix.com>
2015-03-02 08:52:13 -0800

    set provider on bake execution details

--- app/scripts/modules/pipelines/config/stages/bake/bakeStage.module.js
+++ app/scripts/modules/pipelines/config/stages/bake/bakeStage.module.js
@@ -1,3 +1,7 @@
 'use strict';
 
-angular.module('deckApp.pipelines.stage.bake', ['deckApp.pipelines.stage', 'deckApp.pipelines.stage.core']);
+angular.module('deckApp.pipelines.stage.bake', [
+  'deckApp.pipelines.stage',
+  'deckApp.pipelines.stage.core',
+  'deckApp.pipelines.stage.bake.executionDetails.controller',
+]);
