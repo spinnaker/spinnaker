c2d404d4dbd7b43219cb9f7bc5d28a52f74f33b8
Daniel Zapata <dzapata@netflix.com>
2015-03-03 14:35:11 -0800

    fix passing of request params map

--- src/main/groovy/com/netflix/spinnaker/igor/jenkins/BuildController.groovy
+++ src/main/groovy/com/netflix/spinnaker/igor/jenkins/BuildController.groovy
@@ -19,6 +19,7 @@ package com.netflix.spinnaker.igor.jenkins
 import com.netflix.spinnaker.igor.jenkins.client.JenkinsClient
 import com.netflix.spinnaker.igor.jenkins.client.JenkinsMasters
 import com.netflix.spinnaker.igor.jenkins.client.model.Build
+import com.netflix.spinnaker.igor.jenkins.client.model.JobConfig
 import groovy.transform.InheritConstructors
 import groovy.util.logging.Slf4j
 
@@ -57,7 +58,7 @@ class BuildController {
             throw new MasterNotFoundException()
         }
         try {
-            def poller = new BuildJobPoller(job, masters.map[master])
+            def poller = new BuildJobPoller(job, masters.map[master], requestParams)
             executor.submit(poller).get(30, TimeUnit.SECONDS)
             poller.build
         } catch (RuntimeException e) {
@@ -65,22 +66,6 @@ class BuildController {
             throw e
         }
     }
-/*
-    @RequestMapping(value = '/masters/{name}/jobs/{job}', method = RequestMethod.PUT)
-    Build build(@PathVariable("name") String master, @PathVariable String job, @RequestParam Map<String,String> requestParams) {
-        if (!masters.map.containsKey(master)) {
-            throw new MasterNotFoundException()
-        }
-        try {
-            def poller = new BuildJobPoller(job, masters.map[master])
-            executor.submit(poller).get(30, TimeUnit.SECONDS)
-            poller.build
-        } catch (RuntimeException e) {
-            log.error("Unable to build job `${job}`", e)
-            throw e
-        }
-    }
-*/
 
     static class BuildJobPoller implements Runnable {
         private final String job
@@ -94,7 +79,7 @@ class BuildController {
             this.client = client
         }
         
-        BuildJobPoller(String job, JenkinsClient clientm, Map<String,String> requestParams) {
+        BuildJobPoller(String job, JenkinsClient client, Map<String,String> requestParams) {
             this.job = job
             this.client = client
             this.requestParams = requestParams
@@ -102,13 +87,19 @@ class BuildController {
 
         void run() {
             def response
-            
-            if(requestParams) {
+            // fetch the build configuration and make sure that it's configured as we expect
+            JobConfig jobConfig = client.getJobConfig(job)
+
+            if(requestParams && jobConfig.parameterDefinitionList?.size() > 0) {
                 response = client.buildWithParameters(job, requestParams)
-            } else {
+            } else if(!requestParams && (!jobConfig.parameterDefinitionList || jobConfig.parameterDefinitionList.size() == 0)){
                 response = client.build(job, requestParams)
+            } else { // Jenkins will reject the build, so don't even try
+                log.error("job : ${job}, either not passing params to a build job which needs them or passing params to a job which doesn't need them")
+                // we should throw a BuildJobError, but I get a bytecode error : java.lang.VerifyError: Bad <init> method call from inside of a branch
+                throw new RuntimeException()
             }
-            
+
             if (response.status != 201) {
                 throw new BuildJobError()
             }
