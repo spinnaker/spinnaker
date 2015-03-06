e3cf8089a14d6e2cd53331fd2f67d3f3fc86b6cd
chrisb <chrisb@netflix.com>
2015-03-06 12:23:15 -0800

    use consistent readable time for all timestamps

--- app/scripts/filters/timeFormatters.js
+++ app/scripts/filters/timeFormatters.js
@@ -2,26 +2,13 @@
 
 
 angular.module('deckApp')
-  .filter('relativeTime', function(momentService) {
+  .filter('timestamp', function(momentService) {
     return function(input) {
-      input = input || '';
       var moment = momentService(isNaN(parseInt(input)) ? input : parseInt(input));
-      return moment.isValid()? moment.calendar() : 'n/a';
+      return moment.isValid() ? moment.format('YYYY-MM-DD HH:mm:ss') : 'n/a';
     };
   })
   .filter('duration', function(momentService) {
-    return function(input) {
-      var moment = momentService(isNaN(parseInt(input)) ? input : parseInt(input));
-      return moment.isValid()? moment.fromNow() : 'n/a';
-    };
-  })
-  .filter('simpleTime', function(momentService) {
-    return function(input) {
-      var moment = momentService(isNaN(parseInt(input)) ? input : parseInt(input));
-      return moment.isValid() ? moment.format('MM/DD/YY, h:mm:ss a') : 'n/a';
-    };
-  })
-  .filter('numericDuration', function(momentService) {
     return function(input) {
       var moment = momentService.utc(isNaN(parseInt(input)) ? input : parseInt(input));
       var format = moment.hours() ? 'HH:mm:ss' : 'mm:ss';
