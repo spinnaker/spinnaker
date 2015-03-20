33d9d6a3f4fa7b655f184e70fe5e21f1de68add4
chrisb <chrisb@netflix.com>
2015-03-19 23:02:56 -0700

    provide hipchat link on osx

--- app/scripts/modules/feedback/feedback.directive.js
+++ app/scripts/modules/feedback/feedback.directive.js
@@ -6,6 +6,17 @@ angular.module('deckApp.feedback.directive', [])
       restrict: 'E',
       templateUrl: 'scripts/modules/feedback/feedback.html',
       controller: function($scope, $modal) {
+
+        $scope.state = {
+          showMenu: false,
+          isMac: navigator.platform.toLowerCase().indexOf('mac') !== -1,
+        };
+
+        $scope.toggleMenu = function() {
+          $scope.state.showMenu = !$scope.state.showMenu;
+          console.warn('gottle eh', $scope.state.showMenu);
+        };
+
         $scope.openFeedback = function() {
           $modal.open({
             templateUrl: 'scripts/modules/feedback/feedback.modal.html',
