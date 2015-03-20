d6b2051711a820df52b529f13c8628daf1283ce7
Eric Wiseblatt <ewiseblatt@google.com>
2015-03-20 16:18:47 -0400

    Added Reset operation for GCE to reboot instances

---
+++ kato-gce/src/test/groovy/com/netflix/spinnaker/kato/gce/deploy/validators/ResetGoogleInstancesDescriptionValidatorSpec.groovy
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2015 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.netflix.spinnaker.kato.gce.deploy.validators
+
+import com.netflix.spinnaker.amos.DefaultAccountCredentialsProvider
+import com.netflix.spinnaker.amos.MapBackedAccountCredentialsRepository
+import com.netflix.spinnaker.amos.gce.GoogleCredentials
+import com.netflix.spinnaker.amos.gce.GoogleNamedAccountCredentials
+import com.netflix.spinnaker.kato.gce.deploy.description.ResetGoogleInstancesDescription
+import org.springframework.validation.Errors
+import spock.lang.Shared
+import spock.lang.Specification
+
+class ResetGoogleInstancesDescriptionValidatorSpec extends Specification {
+  private static final ACCOUNT_NAME = "auto"
+  private static final ZONE = "us-central1-b"
+  private static final INSTANCE_IDS = ["my-app7-dev-v000-instance1", "my-app7-dev-v000-instance2"]
+
+  @Shared
+  ResetGoogleInstancesDescriptionValidator validator
+
+  void setupSpec() {
+    validator = new ResetGoogleInstancesDescriptionValidator()
+    def credentialsRepo = new MapBackedAccountCredentialsRepository()
+    def credentialsProvider = new DefaultAccountCredentialsProvider(credentialsRepo)
+    def credentials = Mock(GoogleNamedAccountCredentials)
+    credentials.getName() >> ACCOUNT_NAME
+    credentials.getCredentials() >> new GoogleCredentials()
+    credentialsRepo.save(ACCOUNT_NAME, credentials)
+    validator.accountCredentialsProvider = credentialsProvider
+  }
+
+  void "pass validation with proper description inputs"() {
+    setup:
+      def description = new ResetGoogleInstancesDescription(zone: ZONE,
+                                                            instanceIds: INSTANCE_IDS,
+                                                            accountName: ACCOUNT_NAME)
+      def errors = Mock(Errors)
+
+    when:
+      validator.validate([], description, errors)
+
+    then:
+      0 * errors._
+  }
+
+  void "invalid instanceIds fail validation"() {
+    setup:
+      def description = new ResetGoogleInstancesDescription(instanceIds: [""])
+      def errors = Mock(Errors)
+
+    when:
+      validator.validate([], description, errors)
+
+    then:
+      1 * errors.rejectValue("instanceIds", "resetGoogleInstancesDescription.instanceId.invalid")
+  }
+
+  void "null input fails validation"() {
+    setup:
+      def description = new ResetGoogleInstancesDescription()
+      def errors = Mock(Errors)
+
+    when:
+      validator.validate([], description, errors)
+
+    then:
+      1 * errors.rejectValue('credentials', _)
+      1 * errors.rejectValue('zone', _)
+      1 * errors.rejectValue('instanceIds', _)
+  }
+}
