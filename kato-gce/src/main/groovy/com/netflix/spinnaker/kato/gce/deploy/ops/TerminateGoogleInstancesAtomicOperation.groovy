28fabba558d1aaa20b4a1679ad0475f9dbd33fa7
Eric Wiseblatt <ewiseblatt@google.com>
2015-03-27 13:41:24 -0400

    Make TerminateGoogleInstances operation eager.

--- kato-gce/src/main/groovy/com/netflix/spinnaker/kato/gce/deploy/ops/TerminateGoogleInstancesAtomicOperation.groovy
+++ kato-gce/src/main/groovy/com/netflix/spinnaker/kato/gce/deploy/ops/TerminateGoogleInstancesAtomicOperation.groovy
@@ -49,6 +49,15 @@ class TerminateGoogleInstancesAtomicOperation implements AtomicOperation<Void> {
   }
 
   /**
+   * Attempt to terminate each of the specified instanceIds.
+   *
+   * This will attempt to terminate each of the id's independent of one another. Should any of them throw
+   * an exception, the first one will be propagated from this method, but the other attempts will be allowed
+   * to complete first.
+   *
+   * Currently, if others also throw an exception then those exceptions will be lost (however their failures will
+   * be logged in the status)
+   *
    * curl -X POST -H "Content-Type: application/json" -d '[ { "terminateGoogleInstancesDescription": { "instanceIds": ["myapp-dev-v000-abcd"], "zone": "us-central1-b", "credentials": "my-account-name" }} ]' localhost:8501/ops
    */
   @Override
@@ -59,14 +68,30 @@ class TerminateGoogleInstancesAtomicOperation implements AtomicOperation<Void> {
     def project = description.credentials.project
     def zone = description.zone
 
-    // TODO(duftler): Should we attempt to terminate each instance, even in the case of individual failures?
-    for (def instanceId : description.instanceIds) {
-      task.updateStatus BASE_PHASE, "Attempting termination of instance ${instanceId}..."
+    def firstFailure
+    def okIds = []
+    def failedIds = []
 
-      compute.instances().delete(project, zone, instanceId).execute()
+    for (def instanceId : description.instanceIds) {
+      task.updateStatus BASE_PHASE, "Attempting to terminate id=${instanceId}..."
+
+      try {
+        compute.instances().delete(project, zone, instanceId).execute()
+        okIds.add(instanceId)
+      } catch (Exception e) {
+        task.updateStatus BASE_PHASE, "Failed to terminate id=${instanceId} in zone=${zone}: ${e.message}."
+        failedIds.add(instanceId)
+        if (!firstFailure) {
+          firstFailure = e
+        }
+      }
     }
 
-    task.updateStatus BASE_PHASE, "Done executing termination of instances (${description.instanceIds.join(", ")})."
+    if (firstFailure) {
+      task.updateStatus BASE_PHASE, "Failed to terminate instances=${failedIds.join(", ")} but sucessfully terminated instances=${okIds.join(", ")}."
+      throw firstFailure
+    }
+    task.updateStatus BASE_PHASE, "Successfully terminated all instances=(${description.instanceIds.join(", ")})."
     null
   }
 }
--- kato-gce/src/test/groovy/com/netflix/spinnaker/kato/gce/deploy/ops/TerminateGoogleInstancesAtomicOperationUnitSpec.groovy
+++ kato-gce/src/test/groovy/com/netflix/spinnaker/kato/gce/deploy/ops/TerminateGoogleInstancesAtomicOperationUnitSpec.groovy
@@ -26,10 +26,15 @@ import spock.lang.Specification
 import spock.lang.Subject
 
 class TerminateGoogleInstancesAtomicOperationUnitSpec extends Specification {
+  private static final ID_GOOD_PREFIX = "my-app7-dev-v000-good";
+  private static final ID_BAD_PREFIX = "my-app7-dev-v000-bad";
   private static final ACCOUNT_NAME = "auto"
   private static final PROJECT_NAME = "my_project"
   private static final ZONE = "us-central1-b"
-  private static final INSTANCE_IDS = ["my-app7-dev-v000-instance1", "my-app7-dev-v000-instance2"]
+  private static final GOOD_INSTANCE_IDS = ["${ID_GOOD_PREFIX}1", "${ID_GOOD_PREFIX}2"]
+  private static final BAD_INSTANCE_IDS = ["${ID_BAD_PREFIX}1", "${ID_BAD_PREFIX}2"]
+  private static final ALL_INSTANCE_IDS = ["${ID_GOOD_PREFIX}1", "${ID_BAD_PREFIX}1",
+                                           "${ID_GOOD_PREFIX}2", "${ID_BAD_PREFIX}2"]
 
   def setupSpec() {
     TaskRepository.threadLocalTask.set(Mock(Task))
@@ -42,7 +47,7 @@ class TerminateGoogleInstancesAtomicOperationUnitSpec extends Specification {
       def deleteMock = Mock(Compute.Instances.Delete)
       def credentials = new GoogleCredentials(PROJECT_NAME, computeMock)
       def description = new TerminateGoogleInstancesDescription(zone: ZONE,
-                                                                instanceIds: INSTANCE_IDS,
+                                                                instanceIds: GOOD_INSTANCE_IDS,
                                                                 accountName: ACCOUNT_NAME,
                                                                 credentials: credentials)
       @Subject def operation = new TerminateGoogleInstancesAtomicOperation(description)
@@ -52,10 +57,40 @@ class TerminateGoogleInstancesAtomicOperationUnitSpec extends Specification {
 
     then:
       0 * computeMock._
-      INSTANCE_IDS.each {
+      GOOD_INSTANCE_IDS.each {
         1 * computeMock.instances() >> instancesMock
         1 * instancesMock.delete(PROJECT_NAME, ZONE, it) >> deleteMock
         1 * deleteMock.execute()
       }
+  }
+
+  void "should terminate all known instances and fail on all unknown instances"() {
+    setup:
+      def computeMock = Mock(Compute)
+      def instancesMock = Mock(Compute.Instances)
+      def deleteMock = Mock(Compute.Instances.Delete)
+      def credentials = new GoogleCredentials(PROJECT_NAME, computeMock)
+      def description = new TerminateGoogleInstancesDescription(zone: ZONE,
+                                                                instanceIds: ALL_INSTANCE_IDS,
+                                                                accountName: ACCOUNT_NAME,
+                                                                credentials: credentials)
+      @Subject def operation = new TerminateGoogleInstancesAtomicOperation(description)
+
+    when:
+      operation.operate([])
+
+    then:
+      0 * computeMock._
+      GOOD_INSTANCE_IDS.each {
+        1 * computeMock.instances() >> instancesMock
+        1 * instancesMock.delete(PROJECT_NAME, ZONE, it) >> deleteMock
+        1 * deleteMock.execute()
+      }
+      BAD_INSTANCE_IDS.each {
+        1 * computeMock.instances() >> instancesMock
+        1 * instancesMock.delete(PROJECT_NAME, ZONE, it) >> deleteMock
+        1 * deleteMock.execute() >> { throw new IOException() }
+      }
+      thrown IOException
   }
 }
