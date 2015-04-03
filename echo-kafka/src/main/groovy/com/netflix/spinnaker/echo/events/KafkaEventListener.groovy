fdfc8bb651681b437d887a39d002f76bf0dd04c8
Tomas Lin <clin@netflix.com>
2015-04-03 00:17:57 -0700

    make kafka connection optional

--- echo-kafka/src/main/groovy/com/netflix/spinnaker/echo/events/KafkaEventListener.groovy
+++ echo-kafka/src/main/groovy/com/netflix/spinnaker/echo/events/KafkaEventListener.groovy
@@ -23,6 +23,7 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 
 /**
@@ -30,6 +31,7 @@ import org.springframework.stereotype.Component
  */
 @Component
 @Slf4j
+@ConditionalOnProperty('kafka.enabled')
 @SuppressWarnings('GStringExpressionWithinString')
 class KafkaEventListener implements EchoEventListener {
 
@@ -50,24 +52,21 @@ class KafkaEventListener implements EchoEventListener {
     @Value('${kafka.topic:spinnaker_events}')
     String topic
 
-    @Autowired(required=false)
+    @Autowired
     KafkaProducer producer
 
     @Override
     void processEvent(Event event) {
-        if (KafkaProducer) {
-            Map eventAsMap = mapper.convertValue(event, Map)
-            eventAsMap."${prefix}_ts" = new Date().time
-            eventAsMap."${prefix}_app" = app
-            eventAsMap."${prefix}_hostname" = hostname
-            eventAsMap."${prefix}_rowid" = UUID.randomUUID()
-            try {
-                ProducerRecord data = new ProducerRecord(topic, mapper.writeValueAsBytes(eventAsMap))
-                producer.send(data).get()
-            } catch (e) {
-                log.error("Cannot send event", event, e)
-            }
+        Map eventAsMap = mapper.convertValue(event, Map)
+        eventAsMap."${prefix}_ts" = new Date().time
+        eventAsMap."${prefix}_app" = app
+        eventAsMap."${prefix}_hostname" = hostname
+        eventAsMap."${prefix}_rowid" = UUID.randomUUID()
+        try {
+            ProducerRecord data = new ProducerRecord(topic, mapper.writeValueAsBytes(eventAsMap))
+            producer.send(data).get()
+        } catch (e) {
+            log.error("Cannot send kafka event", event, e)
         }
     }
-
 }
