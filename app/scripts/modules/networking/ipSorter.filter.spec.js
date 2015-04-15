bf367df6785a676efa78dae2d30d4f5c8973a688
chrisb <chrisb@netflix.com>
2015-04-15 10:30:43 -0700

    sort ip addresses in Networking section

---
+++ app/scripts/modules/networking/ipSorter.filter.spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('Filter: ipSort', function() {
+  var filter;
+
+  beforeEach(
+    module('deckApp.networking.ip.sort.filter')
+  );
+
+  beforeEach(
+    inject(
+      function($filter) {
+        filter = $filter('ipSorter');
+      }
+    )
+  );
+
+  it('sorts ip addresses, preserving original array', function() {
+    var a = { address: '1.1.1.1' },
+      b = { address: '1.1.1.0' },
+      c = { address: '1.1.0.0' },
+      d = { address: '1.0.1.0' },
+      e = { address: '1.0.0.1' },
+      f = { address: '1.0.0.0' },
+      g = { address: '2.0.0.0' };
+
+    var original = [a,b,c,d,e,f,g];
+
+    var sorted = filter(original);
+
+    expect(sorted).toEqual([f,e,d,c,b,a,g]);
+    expect(original).toEqual([a,b,c,d,e,f,g]);
+
+  });
+
+});
