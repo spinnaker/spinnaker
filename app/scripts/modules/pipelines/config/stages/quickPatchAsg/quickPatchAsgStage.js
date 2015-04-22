0b297df4fd05cce8d59ee30169e751597d35c853
Daniel Zapata <dzapata@netflix.com>
2015-04-22 16:28:23 -0700

    quick patch stage ui changes part 1

---
+++ app/scripts/modules/pipelines/config/stages/quickPatchAsg/quickPatchAsgStage.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.module('deckApp.pipelines.stage.quickPatchAsg')
+  .config(function(pipelineConfigProvider) {
+    pipelineConfigProvider.registerStage({
+      label: 'Quick Patch ASG',
+      description: 'Quick Patches an ASG',
+      key: 'quickPatch',
+      controller: 'QuickPatchAsgStageCtrl',
+      controllerAs: 'QuickPatchAsgStageCtrl',
+      templateUrl: 'scripts/modules/pipelines/config/stages/quickPatchAsg/quickPatchAsgStage.html',
+      executionDetailsUrl: 'scripts/modules/pipelines/config/stages/quickPatchAsg/quickPatchAsgExecutionDetails.html'
+    });
+  }).controller('QuickPatchAsgStageCtrl', function($scope, stage, bakeryService, accountService) {
+    $scope.stage = stage;
+    $scope.baseOsOptions = ['ubuntu', 'centos'];
+
+    $scope.state = {
+      accounts: false,
+      regionsLoaded: false
+    };
+
+    accountService.listAccounts().then(function (accounts) {
+      $scope.accounts = accounts;
+      $scope.state.accounts = true;
+    });
+
+    $scope.accountUpdated = function() {
+      accountService.getRegionsForAccount($scope.stage.credentials).then(function(regions) {
+        $scope.regions = _.map(regions, function(v) { return v.name; });
+        $scope.regionsLoaded = true;
+      });
+    };
+
+    (function() {
+      if ($scope.stage.credentials) {
+        $scope.accountUpdated();
+      }
+    })();
+
+    $scope.$watch('stage.credentials', $scope.accountUpdated);
+  });
+
---
+++ app/scripts/modules/pipelines/config/stages/quickPatchAsg/quickPatchAsgStage.module.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('deckApp.pipelines.stage.quickPatchAsg', [
+  'deckApp.pipelines.stage',
+  'deckApp.pipelines.stage.core',
+  'deckApp.account',
+  'deckApp.pipelines.stage.quickPatchAsg.executionDetails.controller',
+]);
