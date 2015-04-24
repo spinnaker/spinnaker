919e51baf9ab703679bed2ef787fe1c140462c54
Tomas Lin <clin@netflix.com>
2015-04-24 12:37:12 -0700

    corrected typo in enable in discovery stage

--- orca-kato/src/main/groovy/com/netflix/spinnaker/orca/kato/pipeline/EnableInstancesInDiscoveryStage.groovy
+++ orca-kato/src/main/groovy/com/netflix/spinnaker/orca/kato/pipeline/EnableInstancesInDiscoveryStage.groovy
@@ -46,7 +46,7 @@ class EnableInstancesInDiscoveryStage extends LinearStage {
 
   @Override
   public List<Step> buildSteps(Stage stage) {
-    def step1 = buildStep(stage, "disableInstances", EnableInstancesInDiscoveryTask)
+    def step1 = buildStep(stage, "enableInstances", EnableInstancesInDiscoveryTask)
     def step2 = buildStep(stage, "monitorInstances", MonitorKatoTask)
     def step3 = buildStep(stage, "waitForDiscoveryState", WaitForUpInstanceHealthTask)
     [step1, step2, step3]
