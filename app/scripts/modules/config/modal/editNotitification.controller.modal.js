08d516f2f17880034a6b4548c8264c27a8cdd72a
Tomas Lin <clin@netflix.com>
2015-05-06 13:22:55 -0700

    enable save only when there is a valid notification config

--- app/scripts/modules/config/modal/editNotitification.controller.modal.js
+++ app/scripts/modules/config/modal/editNotitification.controller.modal.js
@@ -3,7 +3,7 @@
 angular
   .module('deckApp.editNotification.modal.controller',[
   ])
-  .controller('EditNotificationController', function ($modalInstance, notification) {
+  .controller('EditNotificationController', function ($scope, $modalInstance, notification) {
     var vm = this;
     vm.notification = angular.copy(notification);
 
@@ -11,7 +11,8 @@ angular
       'email', 'hipchat'
     ];
 
-    vm.selectedWhenOptions = {};
+    vm.hasSelectedWhen = false;
+    $scope.selectedWhenOptions = {};
 
     vm.whenOptions = [
       'pipeline.started',
@@ -19,12 +20,31 @@ angular
       'pipeline.failed'
     ];
 
+
+    vm.updateSelectedWhen = function(){
+      var selected = false;
+      _.each(vm.whenOptions, function(option){
+        if($scope.selectedWhenOptions[option] === true){
+          selected = true;
+        }
+      });
+      vm.hasSelectedWhen = selected;
+    };
+
     if(vm.notification !== undefined) {
       _.each(vm.whenOptions, function (option) {
         if (vm.notification.when.indexOf(option) > -1) {
-          vm.selectedWhenOptions[option] = true;
+          $scope.selectedWhenOptions[option] = true;
         }
       });
+      vm.updateSelectedWhen();
+
+      if(vm.notification.type ==='email'){
+        vm.email = vm.notification.address;
+      } else {
+        vm.hipchat = vm.notification.address;
+      }
+
     } else {
       vm.notification = {};
       vm.notification.address = '';
@@ -36,12 +56,23 @@ angular
     vm.submit = function() {
       vm.notification.when = [];
       _.each(vm.whenOptions, function(option){
-        if(vm.selectedWhenOptions[option] === true){
+        if($scope.selectedWhenOptions[option] === true){
           vm.notification.when.push(option);
         }
       });
+      if(vm.notification.type ==='email'){
+        vm.notification.address = vm.email;
+      } else {
+        vm.notification.address = vm.hipchat;
+      }
       $modalInstance.close([notification, vm.notification]);
     };
+
+    $scope.$watch('selectedWhenOptions', function (a,b) {
+      if(a!==b) {
+        vm.updateSelectedWhen();
+      }
+    }, true);
 
     return vm;
   });
