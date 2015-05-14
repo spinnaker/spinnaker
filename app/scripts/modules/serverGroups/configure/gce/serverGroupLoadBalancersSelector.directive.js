4c1ebeedf68909eb3a5b58bd0bdf0249d7e62f8a
chrisb <chrisb@netflix.com>
2015-05-14 08:16:23 -0700

    allow users to refresh load balancers when configuring a new server group (GCE)

--- app/scripts/modules/serverGroups/configure/gce/serverGroupLoadBalancersSelector.directive.js
+++ app/scripts/modules/serverGroups/configure/gce/serverGroupLoadBalancersSelector.directive.js
@@ -1,12 +1,25 @@
 'use strict';
 
 angular.module('deckApp.serverGroup.configure.gce')
-  .directive('gceServerGroupLoadBalancersSelector', function() {
+  .directive('gceServerGroupLoadBalancersSelector', function(gceServerGroupConfigurationService, infrastructureCaches) {
     return {
       restrict: 'E',
       scope: {
         command: '=',
       },
-      templateUrl: 'scripts/modules/serverGroups/configure/gce/serverGroupLoadBalancersDirective.html'
+      templateUrl: 'scripts/modules/serverGroups/configure/gce/serverGroupLoadBalancersDirective.html',
+      link: function(scope) {
+
+        scope.getLoadBalancerRefreshTime = function() {
+          return infrastructureCaches.loadBalancers.getStats().ageMax;
+        };
+
+        scope.refreshLoadBalancers = function() {
+          scope.refreshing = true;
+          gceServerGroupConfigurationService.refreshLoadBalancers(scope.command).then(function() {
+            scope.refreshing = false;
+          });
+        };
+      }
     };
   });
