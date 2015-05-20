c4c84ca46923c65a5f63a4bbe9625b398d27ca77
Cameron Fieber <cfieber@netflix.com>
2015-05-20 09:15:42 -0700

    validator/converter, file rename for case mismatch

---
+++ kato-aws/src/main/groovy/com/netflix/spinnaker/kato/aws/deploy/validators/ModifyAsgLaunchConfigurationDescriptionValidator.groovy
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.aws.deploy.validators
+
+import com.netflix.spinnaker.kato.aws.deploy.description.ModifyAsgLaunchConfigurationDescription
+import org.springframework.stereotype.Component
+import org.springframework.validation.Errors
+
+@Component("modifyAsgLaunchConfigurationDescriptionValidator")
+class ModifyAsgLaunchConfigurationDescriptionValidator extends AmazonDescriptionValidationSupport<ModifyAsgLaunchConfigurationDescription> {
+  @Override
+  void validate(List priorDescriptions, ModifyAsgLaunchConfigurationDescription description, Errors errors) {
+    def key = ModifyAsgLaunchConfigurationDescription.class.simpleName
+    validateRegion(description, description.region, key, errors)
+  }
+}
