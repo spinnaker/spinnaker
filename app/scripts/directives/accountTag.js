bd617c4fe8026fca2e65f2cfebd814b5c9aa18c0
Zan Thrash <zanthrash@gmail.com>
2015-06-04 12:21:13 -0700

    - Adding the red color treatment for the accountTag for the detailsPane

--- app/scripts/directives/accountTag.js
+++ app/scripts/directives/accountTag.js
@@ -5,11 +5,24 @@ angular.module('spinnaker')
   .directive('accountTag', function () {
     return {
       restrict: 'E',
-      template: '<span class="label label-default account-label account-label-{{account}} {{pad}}">{{account}}</span>',
+      template: '<span class="label label-default account-label account-label-{{getAccountType()}} {{pad}}">{{account}}</span>',
       scope: {
         account: '=',
+        provider: '=?',
         pad: '@?'
-      }
+      },
+      controller: function($scope, settings ) {
+
+        $scope.getAccountType = function() {
+          if($scope.provider) {
+            var prodAccounts = settings.providers[$scope.provider].challengeDestructiveActions || [];
+            return prodAccounts.indexOf($scope.account) > -1 ? 'prod' : $scope.account;
+          } else {
+            return $scope.account;
+          }
+        };
+
+      },
     };
   }
 );
