78b53365234e7e28e087874fdcb55ff80fd6a2ae
Satyajit Thadeshwar <sthadeshwar@netflix.com>
2015-06-09 11:21:10 -0700

    Adding ancestor server group name in the task objects

--- kato-aws/src/main/groovy/com/netflix/spinnaker/kato/aws/deploy/AWSServerGroupNameResolver.groovy
+++ kato-aws/src/main/groovy/com/netflix/spinnaker/kato/aws/deploy/AWSServerGroupNameResolver.groovy
@@ -18,6 +18,8 @@ package com.netflix.spinnaker.kato.aws.deploy
 import com.amazonaws.services.autoscaling.model.AutoScalingGroup
 import com.netflix.spinnaker.kato.aws.services.AsgService
 import com.netflix.spinnaker.kato.aws.services.RegionScopedProviderFactory.RegionScopedProvider
+import com.netflix.spinnaker.kato.data.task.Task
+import com.netflix.spinnaker.kato.data.task.TaskRepository
 import com.netflix.spinnaker.kato.helpers.AbstractServerGroupNameResolver
 import groovy.transform.CompileStatic
 /**
@@ -26,17 +28,32 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class AWSServerGroupNameResolver extends AbstractServerGroupNameResolver {
 
+  private static final String AWS_PHASE = "AWS_DEPLOY"
+
+  private final String region
   private final RegionScopedProvider regionScopedProvider
 
   AWSServerGroupNameResolver(RegionScopedProvider regionScopedProvider, Boolean ignoreSequence) {
     super(ignoreSequence)
+    this.region = region
     this.regionScopedProvider = regionScopedProvider
+  }
+
+  private static Task getTask() {
+    TaskRepository.threadLocalTask.get()
   }
 
   @Override
   String getPreviousServerGroupName(String clusterName) {
     AsgService asgService = regionScopedProvider.asgService
     AutoScalingGroup ancestorAsg = asgService.getAncestorAsg(clusterName)
-    return ancestorAsg ? ancestorAsg.autoScalingGroupName : null
+    String previousServerGroupName = ancestorAsg ? ancestorAsg.autoScalingGroupName : null
+    if (previousServerGroupName) {
+      task.updateStatus AWS_PHASE, "Found ancestor ASG, parsing details (name: ${previousServerGroupName})"
+      def ancestorServerGroupNames = [ancestorServerGroupNames : "${region}:${previousServerGroupName}"]
+      def ancestorServerGroupNameByRegion = [ancestorServerGroupNameByRegion: [(region): previousServerGroupName]]
+      task.addResultObjects([ancestorServerGroupNames, ancestorServerGroupNameByRegion])
+    }
+    return previousServerGroupName
   }
 }
