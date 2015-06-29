753e003493d267d11f8e645c980bae8861dce1cc
Zan Thrash <zanthrash@gmail.com>
2015-06-29 13:03:24 -0700

    SPIN-492:  Manual Deploy pipeline executions with that have a deploy stage have been missing the build info and link to the build job.  This fixes that.

--- app/scripts/modules/delivery/executionStatus.directive.js
+++ app/scripts/modules/delivery/executionStatus.directive.js
@@ -10,5 +10,23 @@ angular.module('spinnaker.delivery.executionStatus.directive', [])
       },
       templateUrl: 'scripts/modules/delivery/executionStatus.html',
       controller: 'executionStatus as ctrl',
+      link: function(scope) {
+
+        function findDeployStageList() {
+          return _(scope.execution.stages)
+            .chain()
+            .find(function(stage) {
+              return stage.type === 'deploy';
+            })
+            .get('context')
+            .get('deploymentDetails')
+            .first()
+            .get('jenkins')
+            .value();
+        }
+
+        scope.execution.buildInfo = findDeployStageList();
+
+      }
     };
   });
