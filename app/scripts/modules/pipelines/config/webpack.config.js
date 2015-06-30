b4da05241307761f1d13a906ba8be7f28bdc7da7
Benjamin Malley <benjamin.malley@gmail.com>
2015-06-30 10:32:12 -0700

    more wip on utils & pipelines

---
+++ app/scripts/modules/pipelines/config/webpack.config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var IgnorePlugin = require("webpack/lib/IgnorePlugin");
+var path = require('path');
+
+module.exports = {
+  entry: './config.module.js',
+  devtool: 'eval',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    noParse: [
+      /\.spec\.js$/,
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel!eslint',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        loader: 'style!css!less',
+      },
+      {
+        test: /\.woff(2)?(.*)?$/,
+        loader: 'url?limit=10000&minetype=application/font-woff',
+      },
+      {
+        test: /\.(otf|ttf|eot|svg)(.*)?$/,
+        loader: 'file',
+      },
+      {
+        test: /\.html$/,
+        loader: 'ngtemplate!html'
+      },
+    ],
+  },
+  plugins: [
+    new IgnorePlugin(
+      /\.spec/
+    ),
+    new HtmlWebpackPlugin({
+      title: 'Spinnaker',
+    }),
+  ],
+};
