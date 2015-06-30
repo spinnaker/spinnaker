6b0a210cff430b6d61c9d38a9f0ff13747df5cf8
Benjamin Malley <benjamin.malley@gmail.com>
2015-06-30 12:46:45 -0700

    remove old utils module

--- app/scripts/modules/utils/lodash.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-let _ = require('lodash');
-let angular = require('angular');
-
-module.exports = angular.module('spinnaker.utils.lodash', [])
-  .factory('_', function() {
-    return _;
-  });
--- app/scripts/modules/utils/moment.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let moment = require('moment');
-let angular = require('angular');
-
-module.exports = angular.module('spinnaker.utils.moment', [])
-  .factory('momentService', function() {
-    return moment;
-  });
