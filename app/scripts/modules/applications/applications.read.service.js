f8f0ebfc41fb86f4a78f24b1d9940bf84bc44cc6
chrisb <chrisb@netflix.com>
2015-07-02 19:22:06 -0700

    remove executions before stringifying server group

--- app/scripts/modules/applications/applications.read.service.js
+++ app/scripts/modules/applications/applications.read.service.js
@@ -240,7 +240,7 @@ module.exports = angular
                 .then(
                   function() {
                     application.serverGroups.forEach(function(sg) {
-                      sg.stringVal = angular.toJson(sg);
+                      sg.stringVal = JSON.stringify(sg, jsonReplacer);
                     });
                     return application;
                   },
@@ -253,6 +253,23 @@ module.exports = angular
             });
         });
     }
+
+    // strips out Angular bits (see angular.js#toJsonReplacer), as well as executions, which often
+    // have circular references because of the horrible things we do in executions.transformer.js
+    function jsonReplacer(key, value) {
+      var val = value;
+
+      if (typeof key === 'string' && key.charAt(0) === '$' && key.charAt(1) === '$') {
+        val = undefined;
+      }
+
+      if (key === 'executions') {
+        val = undefined;
+      }
+
+      return val;
+    }
+
 
 
     return {
