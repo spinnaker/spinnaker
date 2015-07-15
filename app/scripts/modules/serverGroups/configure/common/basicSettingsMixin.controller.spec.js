9caeb9f943827606912de7702b5a27a2cf53f777
chrisb <chrisb@netflix.com>
2015-07-14 17:48:23 -0700

    tests for basic settings patterns

---
+++ app/scripts/modules/serverGroups/configure/common/basicSettingsMixin.controller.spec.js
@@ -0,0 +1,132 @@
+'use strict';
+
+describe('Basic Settings Mixin Controller:', function () {
+  var controller, $scope;
+
+  beforeEach(module('spinnaker.serverGroups.basicSettings.controller'));
+
+  beforeEach(inject(function($controller, $rootScope, RxService, namingService) {
+    $scope = $rootScope.$new();
+    $scope.application = { name: 'app', serverGroups: [] };
+    $scope.command = { viewState: { }};
+    controller = $controller('BasicSettingsMixin',{
+      $scope: $scope,
+      RxService: RxService,
+      namingService: namingService,
+      $modalStack: { dismissAll: angular.noop },
+    });
+  }));
+
+  describe('pattern testing: templating not enabled', function () {
+
+    beforeEach(function() {
+      $scope.command.viewState = { };
+    });
+
+    it('stack should accept underscores, letters, numbers, and nothing', function () {
+      var test = controller.getStackPattern.test;
+      expect(test('a')).toBe(true);
+      expect(test('ab')).toBe(true);
+      expect(test('a_b')).toBe(true);
+      expect(test('9')).toBe(true);
+      expect(test('9aA')).toBe(true);
+      expect(test(null)).toBe(true);
+      expect(test('')).toBe(true);
+    });
+
+    it('stack should fail on dashes or various other special characters', function () {
+      var test = controller.getStackPattern.test;
+      expect(test('-a')).toBe(false);
+      expect(test('a-')).toBe(false);
+      expect(test('-')).toBe(false);
+      expect(test('$')).toBe(false);
+      expect(test('9*')).toBe(false);
+      expect(test('${a}')).toBe(false);
+    });
+
+    it('detail should accept underscores, letters, numbers, dashes, and nothing', function() {
+      var test = controller.getDetailPattern.test;
+      expect(test('a')).toBe(true);
+      expect(test('ab')).toBe(true);
+      expect(test('a_b')).toBe(true);
+      expect(test('9')).toBe(true);
+      expect(test('9aA')).toBe(true);
+      expect(test(null)).toBe(true);
+      expect(test('')).toBe(true);
+      expect(test('-a')).toBe(true);
+      expect(test('a-')).toBe(true);
+      expect(test('-')).toBe(true);
+    });
+
+    it('detail should fail on various special characters', function () {
+      var test = controller.getDetailPattern.test;
+      expect(test('$')).toBe(false);
+      expect(test('9*')).toBe(false);
+      expect(test('#9')).toBe(false);
+      expect(test('1@9')).toBe(false);
+      expect(test('${a}')).toBe(false);
+    });
+  });
+
+  describe('pattern testing: templating enabled', function () {
+    beforeEach(function() {
+      $scope.command.viewState = { templatingEnabled: true };
+    });
+
+    it('unfortunately is greedy and accepts invalid placeholders', function () {
+      expect(controller.getStackPattern.test('${not valid - trailing closing curly brackets}}}')).toBe(true);
+      expect(controller.getDetailPattern.test('${not valid - trailing closing curly brackets}}}')).toBe(true);
+    });
+
+
+    it('stack should accept underscores, letters, numbers, and nothing', function () {
+      var test = controller.getStackPattern.test;
+      expect(test('a')).toBe(true);
+      expect(test('ab')).toBe(true);
+      expect(test('a_b')).toBe(true);
+      expect(test('9')).toBe(true);
+      expect(test('9aA')).toBe(true);
+      expect(test(null)).toBe(true);
+      expect(test('')).toBe(true);
+    });
+
+    it('stack should accept template placeholders', function () {
+      var test = controller.getStackPattern.test;
+      expect(test('${a}')).toBe(true);
+      expect(test('b${a}')).toBe(true);
+      expect(test('${a}b')).toBe(true);
+      expect(test('c${a}b')).toBe(true);
+      expect(test('c${a}b${d}')).toBe(true);
+      expect(test('c_${a}b${d}')).toBe(true);
+
+      // still fails on invalid characters
+      expect(test('c-${a}b${d}')).toBe(false);
+    });
+
+    it('detail should accept underscores, letters, numbers, dashes, and nothing', function() {
+      var test = controller.getDetailPattern.test;
+      expect(test('a')).toBe(true);
+      expect(test('ab')).toBe(true);
+      expect(test('a_b')).toBe(true);
+      expect(test('9')).toBe(true);
+      expect(test('9aA')).toBe(true);
+      expect(test(null)).toBe(true);
+      expect(test('')).toBe(true);
+      expect(test('-a')).toBe(true);
+      expect(test('a-')).toBe(true);
+      expect(test('-')).toBe(true);
+    });
+
+    it('detail should accept template placeholders', function () {
+      var test = controller.getDetailPattern.test;
+      expect(test('${a}')).toBe(true);
+      expect(test('b${a}')).toBe(true);
+      expect(test('${a}b')).toBe(true);
+      expect(test('c${a}b')).toBe(true);
+      expect(test('c-${a}b${d}')).toBe(true);
+      expect(test('c_${a}b${d}')).toBe(true);
+    });
+
+  });
+
+});
