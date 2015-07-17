53c6a5b172c80bddb6107ed69d72740f03400076
chrisb <chrisb@netflix.com>
2015-07-17 13:42:03 -0700

    short-term cron trigger for pipelines

--- app/scripts/modules/pipelines/config/triggers/cron/cronTrigger.js
+++ app/scripts/modules/pipelines/config/triggers/cron/cronTrigger.js
@@ -3,8 +3,8 @@
 angular.module('spinnaker.pipelines.trigger.cron')
   .config(function(pipelineConfigProvider) {
     pipelineConfigProvider.registerTrigger({
-      label: 'Cron',
-      description: 'Executes the pipeline on a Cron schedule',
+      label: 'CRON',
+      description: 'Executes the pipeline on a CRON schedule',
       key: 'cron',
       controller: 'CronTriggerCtrl',
       controllerAs: 'cronTriggerCtrl',
@@ -15,26 +15,33 @@ angular.module('spinnaker.pipelines.trigger.cron')
   .controller('CronTriggerCtrl', function($scope, trigger) {
 
     $scope.trigger = trigger;
-    trigger.id = trigger.id || randomUUID();
 
     function getRandom(max) {
       return Math.random() * max;
     }
 
-    function randomUUID() {
+    function v4() {
       var id = '', i;
-      for(i = 0; i < 36; i++) {
+
+      for(i = 0; i < 36; i++)
+      {
         if (i === 14) {
           id += '4';
-        } else if (i === 19) {
+        }
+        else if (i === 19) {
           id += '89ab'.charAt(getRandom(4));
-        } else if(i === 8 || i === 13 || i === 18 || i === 23) {
+        }
+        else if(i === 8 || i === 13 || i === 18 || i === 23) {
           id += '-';
-        } else {
+        }
+        else
+        {
           id += '0123456789abcdef'.charAt(getRandom(16));
         }
       }
       return id;
     }
+
+    trigger.id = trigger.id || v4();
 
   });
