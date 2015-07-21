0cbf3280259b880de588aaf4a2f1c567ee2b01d7
chrisb <chrisb@netflix.com>
2015-07-21 14:16:48 -0700

    initial tide integration

---
+++ orca-tide/src/main/groovy/com/netflix/spinnaker/orca/tide/config/TideConfiguration.groovy
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.tide.config
+
+import com.netflix.spinnaker.orca.retrofit.logging.RetrofitSlf4jLog
+import groovy.transform.CompileStatic
+import com.google.gson.Gson
+import com.netflix.spinnaker.orca.tide.TideService
+import com.netflix.spinnaker.orca.retrofit.RetrofitConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import retrofit.Endpoint
+import retrofit.RestAdapter
+import retrofit.client.Client
+import retrofit.converter.GsonConverter
+import static retrofit.Endpoints.newFixedEndpoint
+
+@Configuration
+@Import(RetrofitConfiguration)
+@ComponentScan([
+    "com.netflix.spinnaker.orca.tide.pipeline",
+    "com.netflix.spinnaker.orca.tide.tasks"
+])
+@CompileStatic
+class TideConfiguration {
+
+  @Autowired Client retrofitClient
+  @Autowired RestAdapter.LogLevel retrofitLogLevel
+
+  @Bean Endpoint tideEndpoint(
+      @Value('${tide.baseUrl}') String front50BaseUrl) {
+    newFixedEndpoint(front50BaseUrl)
+  }
+
+  @Bean TideService tideService(Endpoint tideEndpoint, Gson gson) {
+    new RestAdapter.Builder()
+        .setEndpoint(tideEndpoint)
+        .setClient(retrofitClient)
+        .setLogLevel(retrofitLogLevel)
+        .setLog(new RetrofitSlf4jLog(TideService))
+        .setConverter(new GsonConverter(gson))
+        .build()
+        .create(TideService)
+  }
+}
