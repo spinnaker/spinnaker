35df9a4f75f572270b3ce61a23bc3dd3536578dc
Adam Jordens <ajordens@netflix.com>
2015-07-28 21:19:43 -0700

    Enable challenge mode for 'mgmt' account
    - Specify the default regions for mgmt/mgmttest

--- app/scripts/settings/settings.js
+++ app/scripts/settings/settings.js
@@ -15,7 +15,7 @@ angular.module('spinnaker.settings', [])
         },
         primaryAccounts: ['prod', 'test'],
         primaryRegions: ['eu-west-1','us-east-1','us-west-1','us-west-2'],
-        challengeDestructiveActions: ['prod', 'mceprod', 'cpl'],
+        challengeDestructiveActions: ['mgmt', 'prod', 'mceprod', 'cpl'],
         preferredZonesByAccount: {
           prod: {
             'us-east-1': ['us-east-1c', 'us-east-1d', 'us-east-1e'],
@@ -28,6 +28,26 @@ angular.module('spinnaker.settings', [])
             'sa-east-1': ['sa-east-1a', 'sa-east-1b']
           },
           test: {
+            'us-east-1': ['us-east-1c', 'us-east-1d', 'us-east-1e'],
+            'us-west-1': ['us-west-1a', 'us-west-1c'],
+            'us-west-2': ['us-west-2a', 'us-west-2b', 'us-west-2c'],
+            'eu-west-1': ['eu-west-1a', 'eu-west-1b', 'eu-west-1c'],
+            'ap-northeast-1': ['ap-northeast-1a', 'ap-northeast-1b', 'ap-northeast-1c'],
+            'ap-southeast-1': ['ap-southeast-1a', 'ap-southeast-1b'],
+            'ap-southeast-2': ['ap-southeast-2a', 'ap-southeast-2b'],
+            'sa-east-1': ['sa-east-1a', 'sa-east-1b']
+          },
+          mgmt: {
+            'us-east-1': ['us-east-1c', 'us-east-1d', 'us-east-1e'],
+            'us-west-1': ['us-west-1a', 'us-west-1c'],
+            'us-west-2': ['us-west-2a', 'us-west-2b', 'us-west-2c'],
+            'eu-west-1': ['eu-west-1a', 'eu-west-1b', 'eu-west-1c'],
+            'ap-northeast-1': ['ap-northeast-1a', 'ap-northeast-1b', 'ap-northeast-1c'],
+            'ap-southeast-1': ['ap-southeast-1a', 'ap-southeast-1b'],
+            'ap-southeast-2': ['ap-southeast-2a', 'ap-southeast-2b'],
+            'sa-east-1': ['sa-east-1a', 'sa-east-1b']
+          },
+          mgmttest: {
             'us-east-1': ['us-east-1c', 'us-east-1d', 'us-east-1e'],
             'us-west-1': ['us-west-1a', 'us-west-1c'],
             'us-west-2': ['us-west-2a', 'us-west-2b', 'us-west-2c'],
