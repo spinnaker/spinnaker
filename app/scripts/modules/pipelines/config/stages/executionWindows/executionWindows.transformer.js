829f48f0688a0ec8d45f2646ac1e9171a23df46f
chrisb <chrisb@netflix.com>
2015-08-07 13:08:47 -0700

    override execution status based on restrict window; show active executions in bubbles

--- app/scripts/modules/pipelines/config/stages/executionWindows/executionWindows.transformer.js
+++ app/scripts/modules/pipelines/config/stages/executionWindows/executionWindows.transformer.js
@@ -5,14 +5,26 @@ module.exports =  angular.module('spinnaker.pipelines.stage.executionWindows.tra
 
     //Overrides "running" status, setting it to "suspended"
     function transformRunningExecutionWindows(execution) {
+      var hasRunningStage = false,
+          inRestrictedWindow = false;
       execution.stages
         .filter(function (stage) {
           return stage.type === 'restrictExecutionDuringTimeWindow' && stage.status === 'RUNNING';
         })
         .forEach(function (stage) {
+          inRestrictedWindow = true;
           stage.status = 'SUSPENDED';
           stage.tasks.forEach(function(task) { task.status = 'SUSPENDED'; });
         });
+
+      if (inRestrictedWindow && execution.status === 'RUNNING') {
+        hasRunningStage = execution.stages.some(function (stage) {
+          return stage.status === 'RUNNING';
+        });
+      }
+      if (inRestrictedWindow && !hasRunningStage) {
+        execution.status = 'SUSPENDED';
+      }
     }
     this.transform = function(application, execution) {
       transformRunningExecutionWindows(execution);
