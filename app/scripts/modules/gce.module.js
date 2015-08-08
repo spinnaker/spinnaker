036ce87f7f4b20209c85864fa40ff9ccf8ea8ab4
chrisb <chrisb@netflix.com>
2015-08-08 11:15:27 -0700

    register provider images via service

--- app/scripts/modules/gce.module.js
+++ app/scripts/modules/gce.module.js
@@ -2,11 +2,18 @@
 
 let angular = require('angular');
 
-require('file!../../images/providers/logo_gce.png');
-
 module.exports = angular.module('spinnaker.gce', [
   require('./serverGroups/details/gce/serverGroupDetails.gce.controller.js'),
   require('./serverGroups/configure/gce/ServerGroupCommandBuilder.js'),
   require('./serverGroups/configure/gce/wizard/CloneServerGroupCtrl.js'),
   require('./serverGroups/configure/gce/serverGroup.configure.gce.module.js'),
-]).name;
+  require('./providerSelection/provider.image.service.provider.js'),
+])
+  .config(function(providerImageServiceProvider) {
+    providerImageServiceProvider.registerImage({
+      provider: 'gce',
+      key: 'logo',
+      path: require('../../images/providers/logo_gce.png')
+    });
+  }).name;
+
