765487dc851ee12acb42f7ec35671c547fb3d3a0
chrisb <chrisb@netflix.com>
2015-08-12 14:36:59 -0700

    restore overrideTimeout directive

--- app/scripts/modules/pipelines/pipelines.module.js
+++ app/scripts/modules/pipelines/pipelines.module.js
@@ -6,7 +6,7 @@ module.exports = angular.module('spinnaker.pipelines', [
   require('exports?"restangular"!imports?_=lodash!restangular'),
   require('exports?"ui.sortable"!angular-ui-sortable'),
   require('utils/lodash.js'),
-  require('./config/config.module.js'),
+  require('./config/pipelineConfig.module.js'),
   require('../caches/viewStateCache.js'),
   require('../authentication/authentication.module.js'),
   require('../caches/deckCacheFactory.js'),
