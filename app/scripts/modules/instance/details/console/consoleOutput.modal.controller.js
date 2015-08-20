2a7c81d657c353b7be7820011d5c7963f2474028
chrisb <chrisb@netflix.com>
2015-08-19 17:29:23 -0700

    instance console modal

---
+++ app/scripts/modules/instance/details/console/consoleOutput.modal.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let angular = require('angular');
+
+require('./consoleOutput.modal.html');
+
+module.exports = angular.module('spinnaker.core.instance.details.console.controller', [
+  require('../../instance.read.service.js'),
+])
+  .controller('ConsoleOutputCtrl', function($scope, $modalInstance, instanceReader, instance) {
+    $scope.vm = {
+      loading: true,
+      instanceId: instance.instanceId,
+    };
+
+    instanceReader.getConsoleOutput(instance.account, instance.region, instance.instanceId, instance.provider).then(
+      function(response) {
+        $scope.vm.consoleOutput = response.output;
+        $scope.vm.loading = false;
+      },
+      function(exception) {
+        $scope.vm.exception = exception;
+        $scope.vm.loading = false;
+      }
+    );
+
+    $scope.close = $modalInstance.dismiss;
+
+  }).name;
--- app/scripts/modules/instance/instance.module.js
+++ app/scripts/modules/instance/instance.module.js
@@ -4,6 +4,7 @@ let angular = require('angular');
 
 module.exports = angular
   .module('spinnaker.instance', [
+    require('./details/console/consoleOutput.modal.controller.js'),
     require('./details/aws/instance.details.controller.js'),
     require('./details/gce/instance.details.controller.js'),
     require('./loadBalancer/instanceLoadBalancerHealth.js'),
