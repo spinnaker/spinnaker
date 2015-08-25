2c6b48bbd9a41752f7d2f399ff028666a3d6dca3
Zan Thrash <zanthrash@gmail.com>
2015-08-25 13:10:32 -0700

    making the keyboard shortcuts context aware

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -377,46 +377,6 @@ module.exports = angular.module('spinnaker', [
         description: 'Data',
         callback: () => $state.go('home.data'),
       },
-      {
-        combo: 'ctrl+alt+0',
-        description: 'Pipeline Config',
-        callback: () => $state.go('home.applications.application.pipelineConfig'),
-      },
-      {
-        combo: 'ctrl+alt+1',
-        description: 'Pipelines',
-        callback: () => $state.go('home.applications.application.executions'),
-      },
-      {
-        combo: 'ctrl+alt+2',
-        description: 'Clusters',
-        callback: () => $state.go('home.applications.application.insight.clusters')
-      },
-      {
-        combo: 'ctrl+alt+3',
-        description: 'Load Balancer',
-        callback: () => $state.go('home.applications.application.insight.loadBalancers')
-      },
-      {
-        combo: 'ctrl+alt+4',
-        description: 'Security Groups',
-        callback: () => $state.go('home.applications.application.insight.securityGroups')
-      },
-      {
-        combo: 'ctrl+alt+5',
-        description: 'Properties',
-        callback: () => $state.go('home.applications.application.properties')
-      },
-      {
-        combo: 'ctrl+alt+6',
-        description: 'Tasks',
-        callback: () => $state.go('home.applications.application.tasks')
-      },
-      {
-        combo: 'ctrl+alt+7',
-        description: 'Config',
-        callback: () => $state.go('home.applications.application.config')
-      },
 
     ];
 
