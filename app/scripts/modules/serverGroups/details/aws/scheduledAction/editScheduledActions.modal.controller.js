926c722597f2eece126c349bd3ffb35773b9295a
chrisb <chrisb@netflix.com>
2015-08-27 16:43:10 -0700

    allow editing of scheduled actions

---
+++ app/scripts/modules/serverGroups/details/aws/scheduledAction/editScheduledActions.modal.controller.js
@@ -0,0 +1,65 @@
+'use strict';
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.serverGroup.details.aws.scheduledActions.editScheduledActions.modal.controller', [
+  require('utils/lodash.js'),
+  require('../../../../tasks/monitor/taskMonitor.module.js'),
+  require('../../../../../services/taskExecutor.js'),
+])
+  .controller('EditScheduledActionsCtrl', function($scope, $modalInstance, taskMonitorService, taskExecutor, _,
+                                                     application, serverGroup) {
+    $scope.command = {
+      scheduledActions: serverGroup.scheduledActions.map((action) => {
+        return {
+          recurrence: action.recurrence,
+          minSize: action.minSize,
+          maxSize: action.maxSize,
+          desiredCapacity: action.desiredCapacity,
+        };
+      }),
+    };
+
+    $scope.serverGroup = serverGroup;
+
+    this.addScheduledAction = () => {
+      $scope.command.scheduledActions.push({});
+    };
+
+    this.removeScheduledAction = (index) => {
+      $scope.command.scheduledActions.splice(index, 1);
+    };
+
+    this.submit = () => {
+      var job = [
+        {
+          type: 'upsertAsgScheduledActions',
+          asgs: [{asgName: serverGroup.name, region: serverGroup.region}],
+          scheduledActions: $scope.command.scheduledActions,
+          credentials: serverGroup.account,
+        }
+      ];
+
+      var submitMethod = function() {
+        return taskExecutor.executeTask({
+          job: job,
+          application: application,
+          description: 'Update Scheduled Actions for ' + serverGroup.name
+        });
+      };
+
+      var taskMonitorConfig = {
+        modalInstance: $modalInstance,
+        application: application,
+        title: 'Update Scheduled Actions for ' + serverGroup.name,
+        submitMethod: submitMethod,
+        forceRefreshEnabled: true,
+      };
+
+      $scope.taskMonitor = taskMonitorService.buildTaskMonitor(taskMonitorConfig);
+
+      $scope.taskMonitor.submit(submitMethod);
+    };
+
+    this.cancel = $modalInstance.dismiss;
+  }).name;
