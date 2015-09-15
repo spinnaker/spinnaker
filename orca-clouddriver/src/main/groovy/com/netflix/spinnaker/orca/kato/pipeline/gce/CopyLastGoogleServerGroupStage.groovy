20a0133aa5d1a95cf9399f85791261d90da3a9c7
duftler <duftler@google.com>
2015-09-15 14:59:11 -0400

    Kill off GoogleServerGroupCacheForceRefreshTask in favor of generic ServerGroupCacheForceRefreshTask.

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/kato/pipeline/gce/CopyLastGoogleServerGroupStage.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/kato/pipeline/gce/CopyLastGoogleServerGroupStage.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Google, Inc.
+ * Copyright 2015 Google, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,13 +16,13 @@
 
 package com.netflix.spinnaker.orca.kato.pipeline.gce
 
-import groovy.transform.CompileStatic
 import com.netflix.spinnaker.orca.clouddriver.tasks.MonitorKatoTask
+import com.netflix.spinnaker.orca.clouddriver.tasks.ServerGroupCacheForceRefreshTask
 import com.netflix.spinnaker.orca.kato.tasks.WaitForUpInstancesTask
 import com.netflix.spinnaker.orca.kato.tasks.gce.CreateCopyLastGoogleServerGroupTask
-import com.netflix.spinnaker.orca.kato.tasks.gce.GoogleServerGroupCacheForceRefreshTask
 import com.netflix.spinnaker.orca.pipeline.LinearStage
 import com.netflix.spinnaker.orca.pipeline.model.Stage
+import groovy.transform.CompileStatic
 import org.springframework.batch.core.Step
 import org.springframework.stereotype.Component
 
@@ -40,7 +40,7 @@ class CopyLastGoogleServerGroupStage extends LinearStage {
   public List<Step> buildSteps(Stage stage) {
     def step1 = buildStep(stage, "copyLastServerGroup", CreateCopyLastGoogleServerGroupTask)
     def step2 = buildStep(stage, "monitorDeploy", MonitorKatoTask)
-    def step3 = buildStep(stage, "forceCacheRefresh", GoogleServerGroupCacheForceRefreshTask)
+    def step3 = buildStep(stage, "forceCacheRefresh", ServerGroupCacheForceRefreshTask)
     def step4 = buildStep(stage, "waitForUpInstances", WaitForUpInstancesTask)
     [step1, step2, step3, step4]
   }
