13acac04db074c8b34ec882dd1e29793e46717ac
Cameron Fieber <cfieber@netflix.com>
2015-09-16 15:16:34 -0700

    a failed canary should be TERMINAL

--- orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/tasks/CompleteCanaryTask.groovy
+++ orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/tasks/CompleteCanaryTask.groovy
@@ -33,7 +33,7 @@ class CompleteCanaryTask implements Task {
     } else if (canary.canaryResult?.overallResult == 'SUCCESS') {
       new DefaultTaskResult(ExecutionStatus.SUCCEEDED)
     } else {
-      new DefaultTaskResult(ExecutionStatus.FAILED)
+      new DefaultTaskResult(ExecutionStatus.TERMINAL)
     }
   }
 }
