303cfa80391e651e17d406b7dbc4a354fd0b54f4
Adam Jordens <ajordens@netflix.com>
2015-09-16 00:02:44 -0700

    Very simple Project crud stages and tasks

---
+++ orca-front50/src/main/groovy/com/netflix/spinnaker/orca/front50/pipeline/DeleteProjectStage.groovy
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.netflix.spinnaker.orca.front50.pipeline
+
+import com.netflix.spinnaker.orca.DefaultTaskResult
+import com.netflix.spinnaker.orca.ExecutionStatus
+import com.netflix.spinnaker.orca.Task
+import com.netflix.spinnaker.orca.TaskResult
+import com.netflix.spinnaker.orca.front50.Front50Service
+import com.netflix.spinnaker.orca.pipeline.LinearStage
+import com.netflix.spinnaker.orca.pipeline.model.Stage
+import groovy.transform.CompileStatic
+import org.springframework.batch.core.Step
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+@CompileStatic
+class DeleteProjectStage extends LinearStage {
+  public static final String PIPELINE_CONFIG_TYPE = "deleteProject"
+
+  DeleteProjectStage() {
+    super(PIPELINE_CONFIG_TYPE)
+  }
+
+  @Override
+  public List<Step> buildSteps(Stage stage) {
+    [buildStep(stage, "deleteProject", DeleteProjectTask)]
+  }
+
+  @Component
+  static class DeleteProjectTask implements Task {
+    @Autowired
+    Front50Service front50Service
+
+    @Override
+    TaskResult execute(Stage stage) {
+      def projectId = stage.mapTo("/project", Front50Service.Project)
+      front50Service.deleteProject(projectId.id)
+
+      def outputs = [
+        "notification.type": "deleteproject"
+      ]
+
+      return new DefaultTaskResult(ExecutionStatus.SUCCEEDED, outputs)
+    }
+  }
+}
---
+++ orca-front50/src/main/groovy/com/netflix/spinnaker/orca/front50/pipeline/UpsertProjectStage.groovy
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.netflix.spinnaker.orca.front50.pipeline
+
+import com.google.common.annotations.VisibleForTesting
+import com.netflix.spinnaker.orca.DefaultTaskResult
+import com.netflix.spinnaker.orca.ExecutionStatus
+import com.netflix.spinnaker.orca.Task
+import com.netflix.spinnaker.orca.TaskResult
+import com.netflix.spinnaker.orca.front50.Front50Service
+import com.netflix.spinnaker.orca.pipeline.LinearStage
+import com.netflix.spinnaker.orca.pipeline.model.Stage
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import org.springframework.batch.core.Step
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+@CompileStatic
+class UpsertProjectStage extends LinearStage {
+  public static final String PIPELINE_CONFIG_TYPE = "upsertProject"
+
+  UpsertProjectStage() {
+    super(PIPELINE_CONFIG_TYPE)
+  }
+
+  @Override
+  public List<Step> buildSteps(Stage stage) {
+    [buildStep(stage, "upsertProject", UpsertProjectTask)]
+  }
+
+  @Slf4j
+  @Component
+  @VisibleForTesting
+  public static class UpsertProjectTask implements Task {
+    @Autowired
+    Front50Service front50Service
+
+    UpsertProjectTask() {}
+
+    @Override
+    TaskResult execute(Stage stage) {
+      def projectId = stage.mapTo("/project", Front50Service.Project)
+      def project = stage.mapTo("/project", Map)
+
+      if (projectId.id) {
+        front50Service.updateProject(projectId.id, project)
+      } else {
+        front50Service.createProject(project)
+      }
+
+      def outputs = [
+        "notification.type": "upsertproject"
+      ]
+
+      return new DefaultTaskResult(ExecutionStatus.SUCCEEDED, outputs)
+    }
+  }
+}
