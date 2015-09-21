8a4b3ac2f39502d213a58e966e568cd153e060b0
chrisb <chrisb@netflix.com>
2015-09-21 14:43:27 -0700

    surface ability to toggle azrebalance flag

--- app/scripts/modules/amazon/serverGroup/configure/wizard/CloneServerGroup.aws.controller.js
+++ app/scripts/modules/amazon/serverGroup/configure/wizard/CloneServerGroup.aws.controller.js
@@ -175,6 +175,20 @@ module.exports = angular.module('spinnaker.aws.cloneServerGroup.controller', [
       $modalInstance.dismiss();
     };
 
+    this.toggleSuspendedProcess = function(process) {
+      $scope.command.suspendedProcesses = $scope.command.suspendedProcesses || [];
+      var processIndex = $scope.command.suspendedProcesses.indexOf(process);
+      if (processIndex === -1) {
+        $scope.command.suspendedProcesses.push(process);
+      } else {
+        $scope.command.suspendedProcesses.splice(processIndex, 1);
+      }
+    };
+
+    this.processIsSuspended = function(process) {
+      return $scope.command.suspendedProcesses.indexOf(process) !== -1;
+    };
+
     if (!$scope.state.requiresTemplateSelection) {
       configureCommand();
     } else {
