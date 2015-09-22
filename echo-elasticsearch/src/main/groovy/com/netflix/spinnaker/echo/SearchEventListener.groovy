06e4a011d3d289d732d375d4ebd82e3b1768bd5f
tomaslin <clin@netflix.com>
2015-09-22 12:48:13 -0700

    make elastic search optional

--- echo-elasticsearch/src/main/groovy/com/netflix/spinnaker/echo/SearchEventListener.groovy
+++ echo-elasticsearch/src/main/groovy/com/netflix/spinnaker/echo/SearchEventListener.groovy
@@ -19,12 +19,14 @@ package com.netflix.spinnaker.echo
 import com.netflix.spinnaker.echo.events.EchoEventListener
 import com.netflix.spinnaker.echo.model.Event
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 
 /**
  * Adds events received into elastic search
  */
 @Service
+@ConditionalOnProperty('search.enabled')
 class SearchEventListener implements EchoEventListener {
 
     @Autowired
