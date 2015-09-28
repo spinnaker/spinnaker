85699424ea56364d0a55db3c40063ff6cc078167
duftler <duftler@google.com>
2015-09-28 10:43:43 -0400

    Remove extraneous deletions from serverGroup.transformer.js.

--- app/scripts/modules/google/serverGroup/serverGroup.transformer.js
+++ app/scripts/modules/google/serverGroup/serverGroup.transformer.js
@@ -18,25 +18,12 @@ module.exports = angular
       if (base.viewState.mode !== 'clone') {
         delete command.source;
       }
-      if (base.viewState.useAllImageSelection) {
-        command.amiName = base.viewState.allImageSelection;
-      }
-      command.availabilityZones = {};
-      command.availabilityZones[command.region] = [base.zone];
       command.account = command.credentials;
-      if (!command.ramdiskId) {
-        delete command.ramdiskId; // TODO: clean up in kato? - should ignore if empty string
-      }
       delete command.region;
       delete command.viewState;
       delete command.backingData;
       delete command.selectedProvider;
-      delete command.instanceProfile;
-      delete command.vpcId;
 
-      if (!command.subnetType) {
-        delete command.subnetType;
-      }
       return command;
     }
 
