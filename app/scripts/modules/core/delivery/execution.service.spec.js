b8508a3863fcac5fa6dd4d38d56994e25ed97c4f
Zan Thrash <zanthrash@gmail.com>
2015-10-08 07:17:35 -0700

    Putting in fix in deck to deal with the 429 error code (TOO_MANY_REQUESTS) coming from gate if the Hystrix circits are tripped.
    This will provide a solution for a legitimate empty execution or task list coming back, and for when we are having issues with orca response times.

--- app/scripts/modules/core/delivery/execution.service.spec.js
+++ app/scripts/modules/core/delivery/execution.service.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Service: executionService', function () {
+fdescribe('Service: executionService', function () {
 
   //NOTE: This is only testing the service dependencies. Please add more tests.
 
