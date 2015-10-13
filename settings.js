c4ee2520439e7a26bd746576c3f4299ff4ca4177
chrisb <chrisb@netflix.com>
2015-10-12 20:45:27 -0700

    filter stage types by available providers

--- settings.js
+++ settings.js
@@ -8,6 +8,7 @@ let authEndpoint = process.env.AUTH_ENDPOINT || 'spinnaker-api-prestaging.prod.n
 let protocol = process.env.PROTOCOL || 'https';
 
 window.spinnakerSettings = {
+  defaultProvider: 'aws',
   feedbackUrl: feedbackUrl,
   gateUrl: `${protocol}://${gateHost}`,
   bakeryDetailUrl: bakeryDetailUrl,
