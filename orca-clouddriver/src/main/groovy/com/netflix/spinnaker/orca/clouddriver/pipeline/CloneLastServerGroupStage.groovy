bcdeeab727f5a5de7b8d101286e18e996da338a5
Satyajit Thadeshwar <sthadeshwar@netflix.com>
2015-10-14 18:20:03 -0700

    Adding CloneLastServerGroupStage and marking CopyLastAsgStage as deprecated

---
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/pipeline/CloneLastServerGroupStage.groovy
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.clouddriver.pipeline
+
+import com.netflix.spinnaker.orca.clouddriver.tasks.MonitorKatoTask
+import com.netflix.spinnaker.orca.clouddriver.tasks.ServerGroupCacheForceRefreshTask
+import com.netflix.spinnaker.orca.clouddriver.tasks.WaitForUpInstancesTask
+import com.netflix.spinnaker.orca.kato.pipeline.strategy.DeployStrategyStage
+import com.netflix.spinnaker.orca.kato.tasks.CloneLastServerGroupTask
+import com.netflix.spinnaker.orca.kato.tasks.DiffTask
+import com.netflix.spinnaker.orca.pipeline.model.Stage
+import groovy.transform.CompileStatic
+import org.springframework.batch.core.Step
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+/**
+ * @author sthadeshwar
+ */
+@Component
+@CompileStatic
+class CloneLastServerGroupStage extends DeployStrategyStage {
+
+  public static final String PIPELINE_CONFIG_TYPE = "cloneLastServerGroup"
+
+  @Autowired(required = false)
+  List<DiffTask> diffTasks
+
+  CloneLastServerGroupStage() {
+    super(PIPELINE_CONFIG_TYPE)
+  }
+
+  CloneLastServerGroupStage(String type) {
+    super(type)
+  }
+
+  @Override
+  List<Step> basicSteps(Stage stage) {
+    def steps = []
+
+    steps << buildStep(stage, "cloneLastServerGroup", CloneLastServerGroupTask)
+    steps << buildStep(stage, "monitorDeploy", MonitorKatoTask)
+    steps << buildStep(stage, "forceCacheRefresh", ServerGroupCacheForceRefreshTask)
+    steps << buildStep(stage, "waitForUpInstances", WaitForUpInstancesTask)
+    steps << buildStep(stage, "forceCacheRefresh", ServerGroupCacheForceRefreshTask)
+
+    if (diffTasks) {
+      diffTasks.each { DiffTask diffTask ->
+        steps << buildStep(stage, getDiffTaskName(diffTask.class.simpleName), diffTask.class)
+      }
+    }
+
+    return steps
+  }
+
+  private String getDiffTaskName(String className) {
+    try {
+      className = className[0].toLowerCase() + className.substring(1)
+      className = className.replaceAll("Task", "")
+    } catch (e) {}
+    return className
+  }
+}
