a24001f6938d425d0e7504bdf5d27fc866a85c3d
Eric Wiseblatt <ewiseblatt@google.com>
2015-10-28 17:42:04 +0000

    Migration of runtime and development scripts.
    This is a single large PR adding all the current build/install/run scripting
    support.  There are a few noteworthy caveats:
    
    1) The primary build script (build_release.py) is an interim python script.
       The intent is to replace this with a gradle build.
    
    2) The main installation script (install_spinnaker.py) pulls the artifacts
       from a directory or storage bucket (e.g. S3 or GCS). The expectation is
       to run apt-get install spinnaker instead where the spinnaker debian package
       does similar things but with more standard packaging.
    
    3) There are a pair of scripts to install a development environment on a
       machine (install_development.py) and set one up for a user
       (bootstrap_dev.sh). This too is interim and should become
       an apt-get install spinnaker-dev debian package.
    
    4) There is a script to build a Google Image. It uses packer so should be
       easy to add AMIs and other platform image support. In the end, it uses
       the install script, which is independent of platform anyway.
    
    5) There are runtime scripts for managing an instance (start/stop).
       The validate script is minimal at the moment. I'll add rules back
       in future PRs. For now it is representative. The reconfigure script
       is intended to add customizations into the settings.js that have to be
       static.
    
    6) The pylib/yaml directory is a copy of the standard python YAML library
       that is not included by default. It is here to avoid complexity managing
       python libraries (since pip is not installed by default either) when
       there is no other need for python here.
    
    The dev/ directory is only intended for developers (stuff available in the
    spinnaker-dev packaging). The others are intended for operational runtime
    environments.
    
    The scripts support running a standard installation (in the runtime directory)
    or as a developer (straight out of gradle). The developer scripts are
    generally separate so they can inject a different configuration for where
    to locate components.
    
    Minimal usage as a developer[*] would be:
      mkdir build
      cd build
      ../spinnaker/dev/refresh_source.sh --github_user=<user> --pull_origin
      ../spinnaker/dev/run_dev.sh  (will build and run as a developer)
    
    [*] Assuming you already have a machine setup.
    If you dont then you could run
      spinnaker/dev/install_development.py  (to set up a machine)
      spinnaker/dev/bootstrap_dev.sh  (to setup user environments)
    
    The bootstrap_dev.sh will create the build directory and refresh the sources
    leaving you in ./build.
    
    To use this in a production environment, you'd need to build a release.
      RELEASE_PATH=<path or storage bucket>
      ../spinnaker/dev/refresh_source.sh --pull_origin
      ../spinnaker/dev/build_release --release_path=$RELEASE_PATH
    
    That will create and write to the RELEASE_PATH and also a
    install_spinnaker.py.zip file. You can then copy that .py.zip onto
    the machine you want ot install spinnaker on, then
       python install_spinnaker.py.zip \
           --package_manager \
           --release_path=$RELEASE_PATH
    
    To complete the installation create a /root/.spinnaker/spinnaker-local.yml
    file (from /opt/spinnaker/config/default-spinnaker-local.yml) and fine tune
    it with your credentials and so forth. Be sure to enable a provider and
    set credentials.
    
    Then start spinnaker
       sudo /opt/spinnaker/scripts/start_spinnaker.sh

---
+++ dev/dev_runner.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+#
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import re
+import shutil
+import signal
+import stat
+import subprocess
+import sys
+import time
+
+from spinnaker.fetch import fetch
+from spinnaker.configurator import InstallationParameters
+from spinnaker import spinnaker_runner
+
+
+class DevInstallationParameters(InstallationParameters):
+  """Specialization of the normal production InstallationParameters.
+
+  This is a developer deployment where the paths are setup to run directly
+  out of this repository rather than a standard system installation.
+
+  Also, custom configuration parameters come from the $HOME/.spinnaker
+  rather than the normal installation location.
+  """
+  DEV_SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
+  SUBSYSTEM_ROOT_DIR = os.getcwd()
+
+  USER_CONFIG_DIR = os.path.join(os.environ['HOME'], '.spinnaker')
+  LOG_DIR = os.path.join(SUBSYSTEM_ROOT_DIR, 'logs')
+
+  SPINNAKER_INSTALL_DIR = os.path.abspath(
+      os.path.join(DEV_SCRIPT_DIR, '..'))
+  INSTALLED_CONFIG_DIR = os.path.abspath(
+      os.path.join(DEV_SCRIPT_DIR, '../config'))
+
+  UTILITY_SCRIPT_DIR = os.path.abspath(
+      os.path.join(DEV_SCRIPT_DIR, '../runtime'))
+  EXTERNAL_DEPENDENCY_SCRIPT_DIR = os.path.abspath(
+      os.path.join(DEV_SCRIPT_DIR, '../runtime'))
+
+  DECK_INSTALL_DIR = os.path.join(SUBSYSTEM_ROOT_DIR, 'deck')
+  HACK_DECK_SETTINGS_FILENAME = 'settings.js'
+  DECK_PORT = 9000
+
+
+class DevRunner(spinnaker_runner.Runner):
+  """Specialization of the normal spinnaker runner for development use.
+
+  This class has different behaviors than the normal runner.
+  It follows similar heuristics for launching and stopping jobs,
+  however, the details differ in fundamental ways.
+
+    * The subsystems are run from their source (using gradle)
+      and will attempt to rebuild before running.
+
+    * Spinnaker will be reconfigured on each invocation.
+
+  The runner will display all the events to the subsystem error logs
+  to the console for as long as this script is running. When the script
+  terminates, the console will no longer show the error log, but the processes
+  will remain running, and continue logging to the logs directory.
+  """
+
+  def __init__(self, installation_parameters=None):
+    installation = installation_parameters or DevInstallationParameters
+    super(DevRunner, self).__init__(installation)
+
+  def start_subsystem(self, subsystem, environ=None):
+    """Starts the specified subsystem.
+
+    Args:
+      subsystem [string]: The repository name of the subsystem to run.
+    """
+    print 'Starting {subsystem}'.format(subsystem=subsystem)
+    command = os.path.join(
+        self.installation.SUBSYSTEM_ROOT_DIR,
+        subsystem,
+        'start_dev.sh')
+    return self.run_daemon(command, [command], environ=environ)
+
+  def tail_error_logs(self):
+    """Start a background tail job of all the component error logs."""
+    log_dir = self.installation.LOG_DIR
+    try:
+      os.makedirs(log_dir)
+    except OSError:
+      pass
+
+    tail_jobs = []
+    for subsystem in self.get_all_subsystem_names():
+      path = os.path.join(log_dir, subsystem + '.err')
+      open(path, 'w').close()
+      tail_jobs.append(self.start_tail(path))
+
+    return tail_jobs
+
+  def get_deck_pid(self):
+    """Return the process id for deck, or None."""
+    program='node ./node_modules/webpack-dev-server/bin/webpack-dev-server.js'
+    stdout, stderr = subprocess.Popen(
+        'ps -fwwwC node', stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        shell=True, close_fds=True).communicate()
+    match = re.search('(?m)^[^ ]+ +([0-9]+) .* {program}'.format(
+        program=program), stdout)
+    return int(match.group(1)) if match else None
+
+  def start_deck(self):
+    """Start subprocess for deck."""
+    pid = self.get_deck_pid()
+    if pid:
+      print 'Deck is already running as pid={pid}'.format(pid=pid)
+      return pid
+
+    path = os.path.join(self.installation.SUBSYSTEM_ROOT_DIR,
+                        'deck/start_dev.sh')
+    return self.run_daemon(path, [path])
+
+  def stop_deck(self):
+    """Stop subprocess for deck."""
+    pid = self.get_deck_pid()
+    if pid:
+      print 'Terminating deck in pid={pid}'.format(pid=pid)
+      os.kill(pid, signal.SIGTERM)
+
+  def start_all(self, options):
+    """Starts all the components then logs stderr to the console forever.
+
+    The subsystems are in forked processes disassociated from this, so will
+    continue running even after this process exists. Only the stderr logging
+    to console will stop once this process is terminated. However, the
+    logging will still continue into the LOG_DIR.
+    """
+    self.configurator.update_deck_settings()
+
+    ignore_tail_jobs = self.tail_error_logs()
+    super(DevRunner, self).start_all(options)
+
+    deck_port = self.installation.DECK_PORT
+    print 'Waiting for deck to start on port {port}'.format(port=deck_port)
+
+    # Tail the log file while we wait and run.
+    # But the log file might not yet exist if deck hasnt started yet.
+    # So wait for the log file to exist before starting to tail it.
+    # Deck cant be ready yet if it hasnt started yet anyawy.
+    deck_log_path = os.path.join(self.installation.LOG_DIR, 'deck.log')
+    while not os.path.exists(deck_log_path):
+      time.sleep(0.1)
+    ignore_tail_jobs.append(self.start_tail(deck_log_path))
+
+    # Dont just wait for port to be ready,  but for deck to respond
+    # because it takes a long time to startup once port is ready.
+    while True:
+      code, ignore = fetch('http://localhost:{port}/'.format(port=deck_port))
+      if code == 200:
+        break
+      else:
+        time.sleep(0.1)
+
+    print """Spinnaker is now ready on port {port}.
+
+You can ^C (ctrl-c) to finish the script, which will stop emitting errors.
+Spinnaker will continue until you run scripts/release/stop_spinnaker.sh
+""".format(port=deck_port)
+
+    while True:
+      time.sleep(3600)
+
+  def program_to_subsystem(self, program):
+    return program
+
+  def subsystem_to_program(self, subsystem):
+    return subsystem
+
+
+if __name__ == '__main__':
+  if not os.path.exists('deck'):
+     sys.stderr.write('This script needs to be run from the root of'
+                      ' your build directory.\n')
+     sys.exit(-1)
+
+  DevRunner.main()
---
+++ install/first_google_boot.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+#
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is specific to preparing a Google-hosted virtual machine
+# for running Spinnaker when the instance was created with metadata
+# holding configuration information.
+
+set -e
+set -u
+
+# We're running as root, but HOME might not be defined.
+HOME=${HOME:-"/root"}
+SPINNAKER_INSTALL_DIR=/opt/spinnaker
+CONFIG_DIR=$HOME/.spinnaker
+
+# This status prefix provides a hook to inject output signals with status
+# messages for consumers like the Google Deployment Manager Coordinator.
+# Normally this isnt needed. Callers will populate it as they need
+# using --status_prefix.
+STATUS_PREFIX="*"
+
+METADATA_URL="http://metadata.google.internal/computeMetadata/v1"
+INSTANCE_METADATA_URL="$METADATA_URL/instance"
+if full_zone=$(curl -s -H "Metadata-Flavor: Google" "$INSTANCE_METADATA_URL/zone"); then
+  MY_ZONE=$(basename $full_zone)
+else
+  echo "Not running on Google Cloud Platform."
+  MY_ZONE=""
+fi
+
+function get_instance_metadata_attribute() {
+  local name="$1"
+  local value=$(curl -s -f -H "Metadata-Flavor: Google" \
+                     $INSTANCE_METADATA_URL/attributes/$name)
+  if [[ $? -eq 0 ]]; then
+    echo "$value"
+  else
+    echo ""
+  fi
+}
+
+function write_instance_metadata() {
+  gcloud compute instances add-metadata `hostname` \
+      --zone $MY_ZONE \
+      --metadata "$@"
+  return $?
+}
+
+function clear_metadata_to_file() {
+  local key="$1"
+  local path="$2"
+  local value=$(get_instance_metadata_attribute "$key")
+
+  if [[ "$value" != "" ]]; then
+     echo "$value" > $path
+     clear_instance_metadata "$key"
+     if [[ $? -ne 0 ]]; then
+       die "Could not clear metadata from $key"
+     fi
+     return 0
+  fi
+
+  return 1
+}
+
+function clear_instance_metadata() {
+  gcloud compute instances remove-metadata `hostname` \
+      --zone $MY_ZONE \
+      --keys "$1"
+  return $?
+}
+
+function replace_startup_script() {
+  # Keep the original around for reference.
+  # From now on, all we need to do is start_spinnaker
+  local original=$(get_instance_metadata_attribute "startup-script")
+  echo "$original" > "$SPINNAKER_INSTALL_DIR/scripts/original_startup_script.sh"
+  write_instance_metadata \
+      "startup-script=$SPINNAKER_INSTALL_DIR/scripts/start_spinnaker.sh"
+}
+
+function extract_spinnaker_local_yaml() {
+  local value=$(get_instance_metadata_attribute "spinnaker_local")
+  if [[ "$value" == "" ]]; then
+    return 1
+  fi
+
+  local config="$CONFIG_DIR/spinnaker-local.yml"
+  mkdir -p $(dirname $config)
+  echo "$value" > $config
+  chmod 600 $config
+
+  clear_instance_metadata "spinnaker_local"
+  return 0
+}
+
+function extract_spinnaker_credentials() {
+    extract_spinnaker_google_credentials
+    extract_spinnaker_aws_credentials
+}
+
+function extract_spinnaker_google_credentials() {
+  local json_path="$CONFIG_DIR/ManagedProjectCredentials.json"
+  mkdir -p $(dirname $json_path)
+  if clear_metadata_to_file "managed_project_credentials" $json_path; then
+    # This is a workaround for difficulties using the Google Deployment Manager
+    # to express no value. We'll use the value "None". But we dont want
+    # to officially support this, so we'll just strip it out of this first
+    # time boot if we happen to see it, and assume the Google Deployment Manager
+    # got in the way.
+    sed -i s/^None$//g $json_path
+    if [[ -s $json_path ]]; then
+      chmod 400 $json_path
+      echo "Extracted google credentials to $json_path"
+    else
+       rm $json_path
+    fi
+  else
+    clear_instance_metadata "managed_project_credentials"
+    json_path=""
+  fi
+
+  # This cant be configured when we create the instance because
+  # the path is local within this instance (file transmitted in metadata)
+  # Remove the old line, if one existed, and replace it with a new one.
+  # This way it does not matter whether the user supplied it or not
+  # (and might have had it point to something client side).
+  if [[ -f "$CONFIG_DIR/spinnaker-local.yml" ]]; then
+      sed -i "s/\( \+jsonPath:\).\+/\1 ${json_path//\//\\\/}/g" \
+          $CONFIG_DIR/spinnaker-local.yml
+  fi
+}
+
+function extract_spinnaker_aws_credentials() {
+  local credentials_path="$HOME/.aws/credentials"
+  mkdir -p $(dirname $credentials_path)
+  if clear_metadata_to_file "aws_credentials" $credentials_path; then
+    # This is a workaround for difficulties using the Google Deployment Manager
+    # to express no value. We'll use the value "None". But we dont want
+    # to officially support this, so we'll just strip it out of this first
+    # time boot if we happen to see it, and assume the Google Deployment Manager
+    # got in the way.
+    sed -i s/^None$//g $credentials_path
+    if [[ -s $credentials_path ]]; then
+      chmod 400 $credentials_path
+      echo "Extracted aws credentials to $credentials_path"
+    else
+       rm $credentials_path
+    fi
+  else
+    clear_instance_metadata "aws_credentials"
+  fi
+}
+
+function process_args() {
+  while [[ $# > 0 ]]
+  do
+    local key="$1"
+    case $key in
+    --status_prefix)
+      STATUS_PREFIX="$2"
+      shift
+      ;;
+
+    *)
+      echo "ERROR: unknown option '$key'."
+      exit -1
+      ;;
+    esac
+    shift
+  done
+}
+
+# apply outstanding updates since time of image creation
+apt-get -y update
+apt-get -y dist-upgrade
+
+process_args
+mkdir -p /root/.spinnaker
+
+echo "$STATUS_PREFIX  Extracting Configuration Info"
+extract_spinnaker_local_yaml
+
+echo "$STATUS_PREFIX  Extracting Credentials"
+extract_spinnaker_credentials
+
+echo "$STATUS_PREFIX  Configuring Spinnaker"
+$SPINNAKER_INSTALL_DIR/scripts/reconfigure_spinnaker.sh
+
+
+# Replace this first time boot with the normal startup script
+# that just starts spinnaker (and its dependencies) without configuring anymore.
+echo "$STATUS_PREFIX  Cleaning Up"
+replace_startup_script
+
+echo "$STATUS_PREFIX  Starting Spinnaker"
+$SPINNAKER_INSTALL_DIR/scripts/start_spinnaker.sh
+echo "$STATUS_PREFIX  Spinnaker is now ready"
