fd60cfc26f6b45cc67b20d901dea196b7a6e8eb2
Zan Thrash <zanthrash@gmail.com>
2015-10-30 10:45:50 -0700

    Adding Hystrix around FastPropertyService

--- gate-core/src/main/groovy/com/netflix/spinnaker/gate/services/commands/HystrixFactory.groovy
+++ gate-core/src/main/groovy/com/netflix/spinnaker/gate/services/commands/HystrixFactory.groovy
@@ -59,6 +59,14 @@ class HystrixFactory {
     new MapCommand(groupKey, commandKey, false, propagate(work, false), fallback)
   }
 
+  public static StringCommand newStringCommand(String groupKey,
+                                        String commandKey,
+                                        boolean withLastKnownGoodFallback,
+                                        Closure<? extends String> work,
+                                        Closure<? extends String> fallback = { null }) {
+
+    new StringCommand(groupKey, commandKey, withLastKnownGoodFallback, propagate(work, false), fallback )
+  }
   private static class ListCommand extends AbstractHystrixCommand<List> {
     ListCommand(String groupKey, String commandKey, boolean withLastKnownGoodFallback, Closure work, Closure fallback) {
       super(groupKey, commandKey, withLastKnownGoodFallback, [], work, fallback)
@@ -68,6 +76,12 @@ class HystrixFactory {
   private static class MapCommand extends AbstractHystrixCommand<Map> {
     MapCommand(String groupKey, String commandKey, boolean withLastKnownGoodFallback, Closure work, Closure fallback) {
       super(groupKey, commandKey, withLastKnownGoodFallback, [:], work, fallback)
+    }
+  }
+
+  private static class StringCommand extends AbstractHystrixCommand<String> {
+    StringCommand(String groupKey, String commandKey, boolean withLastKnownGoodFallback, Closure work, Closure fallback) {
+      super(groupKey, commandKey, withLastKnownGoodFallback, "", work, fallback)
     }
   }
 }
