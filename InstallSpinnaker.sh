691c4ebd3b399bb8293923837098870d6ec9e301
David Peterka <dpeterka@ziji-media.com>
2015-11-05 22:47:21 -0500

    Add some safety

--- InstallSpinnaker.sh
+++ InstallSpinnaker.sh
@@ -4,6 +4,41 @@
 # To you put this file in the root of a web server
 # curl -L https://foo.com/InstallSpinnaker.sh| sudo bash
 
+# We can only currently support limited releases
+# First guess what sort of operating system
+
+if [ -f /etc/lsb-release ]; then
+    . /etc/lsb-release
+    DISTRO=$DISTRIB_ID
+elif [ -f /etc/debian_version ]; then
+    DISTRO=Debian
+    # XXX or Ubuntu
+elif [ -f /etc/redhat-release ]; then
+    if grep -iq cent /etc/redhat-release; then
+      DISTRO="CentOS"
+    elif grep -iq red /etc/redhat-release; then
+      DISTRO="RedHat"
+    fi
+    
+    else
+    DISTRO=$(uname -s)
+fi
+
+# If not Ubuntu 14.xx.x or higher
+
+if [[ "$DISTRO" == "Ubuntu" ]]; then
+  if [ "${DISTRIB_RELEASE%%.*}" -ne 14 ]; then
+  echo "Not a supported version of Ubuntu"
+  echo "Version is $DISTRIB_RELEASE we require 14.02 or higher"
+  exit 1
+  fi
+else
+  echo "Not a supported operating system"
+  echo "Recommend you use Ubuntu 14.10 or higher"
+  exit 1
+fi
+
+
 
 ## PPAs ##
 # Add PPAs for software that is not necessarily in sync with Ubuntu releases
