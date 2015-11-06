5bf9d06fb6dbb1db736da5ddc5e6c4e190317bd2
Rohit Agarwal <ragarwal@kenzanmedia.com>
2015-11-06 21:27:25 +0530

    removed if condition from pre-postInstall scripts

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,16 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-dist=`gawk -F= '/^NAME/{print $2}' /etc/os-release`
 
-if [[ "$dist" == *Ubuntu* ]]; then
-  # Create spinnaker user
-  /usr/sbin/useradd spinnaker
-
-  # test dir created to test block execution
-  mkdir -p /home/ubuntu/2
-
-fi
-
-if [[ "$dist" == *"Amazon Linux AMI"* ]]; then
-  echo "Amazon Linux AMI"
-fi
+# Create spinnaker user
+/usr/sbin/useradd spinnaker
