9845792ce403131785ec60bc4c87cf69330cc774
David Peterka <dpeterka@ziji-media.com>
2015-11-09 11:55:56 -0500

    Accept enter as default

--- InstallSpinnaker.sh
+++ InstallSpinnaker.sh
@@ -81,7 +81,7 @@ nodetool enablethrift
 apt-get install -y --force-yes --allow-unauthenticated spinnaker
 
 read -p "Enable Amazon AWS? (Y|n)" enableAws
-if [[ " ${enableAws,,}" == "y" ]]; then
+if [[ " ${enableAws,,}" == "y" || -z "$enableAws" ]]; then
     setEnableAws="true"
     read -p "Default region: " defaultRegion
     sed -i.bak -e "s/false/$setEnableAws" -e "s/us-west-2/$defaultRegion" /etc/default/spinnaker
