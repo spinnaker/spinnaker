0d437e44f20b419db2ec65ad8d0a5553ed4fbf0d
chrisb <chrisb@netflix.com>
2015-11-08 16:46:47 -0800

    show reservation counts when performing AWS resize

---
+++ app/scripts/modules/amazon/serverGroup/report/reservationReport.directive.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const angular = require('angular');
+
+require('./reservationReport.directive.less');
+
+module.exports = angular
+  .module('spinnaker.amazon.serverGroup.report.reservationReport.directive', [
+    require('./reservationReport.read.service.js'),
+  ])
+  .directive('reservationReport', function() {
+    return {
+      restrict: 'E',
+      templateUrl: require('./reservationReport.directive.html'),
+      scope: {},
+      bindToController: {
+        account: '=',
+        region: '=',
+        instanceType: '=',
+        zones: '=',
+      },
+      controller: 'ReservationReportCtrl',
+      controllerAs: 'vm',
+    };
+  })
+  .controller('ReservationReportCtrl', function (reservationReportReader) {
+    this.viewState = {
+      loading: true,
+      error: false,
+    };
+
+    reservationReportReader.getReservationsFor(this.account, this.region, this.instanceType)
+      .then(
+      (report) => {
+        this.report = report.filter((row) => this.zones.indexOf(row.availabilityZone) > -1);
+        this.viewState.loading = false;
+      },
+      () => {
+        this.viewState.loading = false;
+        this.viewState.error = true;
+      }
+    );
+  })
+  .name;
---
+++ app/scripts/modules/amazon/serverGroup/report/reservationReport.read.service.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let angular = require('angular');
+
+module.exports = angular
+  .module('spinnaker.amazon.instance.report.reservation.read.service', [
+    require('exports?"restangular"!imports?_=lodash!restangular'),
+    require('../../../core/utils/lodash.js'),
+  ])
+  .factory('reservationReportReader', function (Restangular) {
+    function getReservationsFor(account, region, instanceType) {
+      return Restangular.one('reports', 'reservation')
+        .get()
+        .then((result) => {
+          return extractReservations(result.reservations, account, region, instanceType);
+        });
+    }
+
+    function extractReservations(reservations, account, region, instanceType) {
+      return reservations
+        .filter((reservation) => reservation.region === region &&
+          reservation.instanceType === instanceType &&
+          reservation.accounts[account])
+        .map((reservation) => {
+          return {
+            availabilityZone: reservation.availabilityZone,
+            os: reservation.os,
+            reservations: reservation.accounts[account],
+          };
+        });
+    }
+
+    return {
+      getReservationsFor: getReservationsFor
+    };
+  }).name;
