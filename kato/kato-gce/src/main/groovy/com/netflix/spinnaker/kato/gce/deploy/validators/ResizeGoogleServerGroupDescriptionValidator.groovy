90ff6e2c45139e1497d2f589e3c74f0a9ec374b4
duftler <duftler@google.com>
2015-11-10 09:49:09 -0500

    Add validation check that targetSize is explicitly specified, instead of defaulting to a value of zero.

--- kato/kato-gce/src/main/groovy/com/netflix/spinnaker/kato/gce/deploy/validators/ResizeGoogleServerGroupDescriptionValidator.groovy
+++ kato/kato-gce/src/main/groovy/com/netflix/spinnaker/kato/gce/deploy/validators/ResizeGoogleServerGroupDescriptionValidator.groovy
@@ -37,7 +37,8 @@ class ResizeGoogleServerGroupDescriptionValidator extends DescriptionValidator<R
 
     helper.validateCredentials(description.accountName, accountCredentialsProvider)
     helper.validateServerGroupName(description.serverGroupName)
-    helper.validateNonNegativeLong(description.targetSize, "targetSize")
+    helper.validateNotEmpty(description.targetSize, "targetSize")
+    helper.validateNonNegativeLong(description.targetSize ?: 0, "targetSize")
     helper.validateZone(description.zone)
   }
 }
