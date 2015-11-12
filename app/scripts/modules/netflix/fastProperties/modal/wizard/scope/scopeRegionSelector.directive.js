666753604ae9ae5cd5e7441d1bc6fc4bd30d9351
Zan Thrash <zanthrash@gmail.com>
2015-11-12 14:35:58 -0800

    Allowing for the "none" selection to be removed form the scope region selector and scope cluster selector.
    This is is for the ACA target selection where we want the users to pick a valid region and cluster

--- app/scripts/modules/netflix/fastProperties/modal/wizard/scope/scopeRegionSelector.directive.js
+++ app/scripts/modules/netflix/fastProperties/modal/wizard/scope/scopeRegionSelector.directive.js
@@ -4,19 +4,25 @@
 let angular = require('angular');
 
 module.exports = angular
-  .module('spinnaker.fastProperties.scope.regionSelector.directive', [])
+  .module('spinnaker.fastProperties.scope.regionSelector.directive', [
+    require('../../../../../core/utils/lodash')
+  ])
   .directive('scopeRegionSelector', function() {
     return {
       restrict: 'E',
-      scope: {},
+      scope: {
+        allowNone: '=?',
+      },
       bindToController: {
         model: '=',
         regions: '=',
-        onChange: '&'
+        onChange: '&',
       },
       controllerAs: 'fp',
-      controller: function controller() {
+      controller: function controller($scope, _) {
         var vm = this;
+
+        vm.allowNone = _.isBoolean($scope.allowNone) ? $scope.allowNone : true;
 
         vm.regionChanged = () => {
           vm.onChange({region: vm.model});
