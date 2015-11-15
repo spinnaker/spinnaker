791bcd1592828d9d5d16e83f3a825fb08b0ba22d
Matt Duftler <duftler@google.com>
2015-11-15 12:46:10 -0500

    Don't need to rename the service.

--- InstallSpinnaker.sh
+++ InstallSpinnaker.sh
@@ -217,7 +217,7 @@ fi
 service clouddriver start
 service orca start
 service gate start
-service spinnaker-rush start
+service rush start
 service rosco start
 service front50 start
 service echo start
