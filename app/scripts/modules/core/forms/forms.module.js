8ccfbe329fd9c96e8c4f7a328ad80243d995c5d1
chrisb <chrisb@netflix.com>
2015-11-15 00:11:19 -0800

    sort of prevent bad delete key behavior in ui-select

--- app/scripts/modules/core/forms/forms.module.js
+++ app/scripts/modules/core/forms/forms.module.js
@@ -6,4 +6,5 @@ module.exports = angular.module('spinnaker.core.forms', [
   require('./autofocus/autofocus.directive.js'),
   require('./checklist/checklist.directive.js'),
   require('./checkmap/checkmap.directive.js'),
+  require('./ignoreEmptyDelete.directive.js'),
 ]).name;
