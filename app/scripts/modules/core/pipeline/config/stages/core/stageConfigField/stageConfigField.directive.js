eeba76c7f6ab01148a46e5bae1d7ef21b80fe907
chrisb <chrisb@netflix.com>
2015-11-22 22:53:56 -0800

    introduce stageConfigField directive

---
+++ app/scripts/modules/core/pipeline/config/stages/core/stageConfigField/stageConfigField.directive.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const angular = require('angular');
+
+require('./stageConfigField.directive.less');
+
+module.exports = angular
+  .module('spinnaker.core.pipeline.config.stages.core.stageField.directive', [
+    require('../../../../../help/helpField.directive.js'),
+  ])
+  .directive('stageConfigField', function () {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {},
+      controllerAs: 'vm',
+      templateUrl: require('./stageConfigField.directive.html'),
+      bindToController: {
+        label: '@',
+        helpKey: '@',
+        fieldColumns: '@',
+      },
+      controller: function() {
+        this.fieldColumns = this.fieldColumns || 6;
+      }
+    };
+  }).name;
---
+++ app/scripts/modules/core/pipeline/config/stages/core/stageConfigField/stageConfigField.directive.spec.js
@@ -0,0 +1,66 @@
+'use strict';
+
+describe('Directives: stageConfigField', function () {
+
+  var scope, compile, helpContents;
+
+  require('./stageConfigField.directive.html');
+
+  beforeEach(
+    window.module(
+      require('./stageConfigField.directive.js')
+    )
+  );
+
+  beforeEach(window.inject(function ($rootScope, $compile, _helpContents_) {
+    scope = $rootScope.$new();
+    compile = $compile;
+    helpContents = _helpContents_;
+  }));
+
+  it('initializes label', function () {
+    var field = compile('<stage-config-field label="The Label"></stage-config-field>')(scope);
+
+    scope.$digest();
+
+    expect(field.find('.label-text').html()).toEqual('The Label');
+
+  });
+
+  it('updates label', function () {
+    scope.foo = true;
+
+    var field = compile('<stage-config-field label="{{foo ? \'foo\' : \'bar\'}}"></stage-config-field>')(scope);
+
+    scope.$digest();
+    expect(field.find('.label-text').html()).toEqual('foo');
+
+    scope.foo = false;
+    scope.$digest();
+    expect(field.find('.label-text').html()).toEqual('bar');
+  });
+
+  it('includes help text if provided', function () {
+    helpContents['foo.bar'] = 'Some help';
+
+    var field = compile('<stage-config-field label="Label" help-key="foo.bar"></stage-config-field>')(scope);
+    scope.$digest();
+    expect(field.find('a.help-field').size()).toBe(1);
+
+    field = compile('<stage-config-field label="Label" help-key="foo.bar.baz"></stage-config-field>')(scope);
+    scope.$digest();
+    expect(field.find('a.help-field').size()).toBe(0);
+  });
+
+  it('transcludes content, defaulting to 6-columns', function () {
+    var field = compile('<stage-config-field label="Label"><h3>The content</h3></stage-config-field>')(scope);
+    scope.$digest();
+    expect(field.find('.col-md-6 h3').html()).toBe('The content');
+  });
+
+  it('allows columns to be overridden for field', function () {
+    var field = compile('<stage-config-field label="Label" field-columns="3"><h3>The content</h3></stage-config-field>')(scope);
+    scope.$digest();
+    expect(field.find('.col-md-3 h3').html()).toBe('The content');
+  });
+});
