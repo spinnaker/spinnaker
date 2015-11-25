f5b595326f4d98e844248d989d015dade259e11f
layered-esell <eujon@layered.com>
2015-11-25 09:13:41 -0700

    backup everything under /opt/rosco/config/packer

--- rosco-web/pkg_scripts/preInstall.sh
+++ rosco-web/pkg_scripts/preInstall.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 # backup install_packages.sh
-if [ -f /opt/rosco/config/packer/install_packages.sh ]; then
-	cp /opt/rosco/config/packer/install_packages.sh /opt/rosco/config/packer/install_packages.sh.orig
+if [ -d /opt/rosco/config/packer ]; then
+  if [ -d /opt/rosco/config/packer/backup ]; then
+    rm -rf /opt/rosco/config/packer/backup/*
+  else
+    mkdir /opt/rosco/config/packer/backup
+  fi
+  find /opt/rosco/config/packer -type f -not -iname 'backup' -print0 | xargs -0 cp -t /opt/rosco/config/packer/backup/
 fi
