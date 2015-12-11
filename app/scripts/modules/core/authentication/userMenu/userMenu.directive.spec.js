7954bab0dbacc3481f46a3019fff448010b1a18a
Samuel Toriel <samuel@fullcontact.com>
2015-12-11 10:10:47 -0700

    Make usermenu work on large and small screens
    The usermenu will display the name on large screens and become an icon
    on med-smaller screens which can be clicked on to show a dropdown of the username

--- app/scripts/modules/core/authentication/userMenu/userMenu.directive.spec.js
+++ app/scripts/modules/core/authentication/userMenu/userMenu.directive.spec.js
@@ -17,30 +17,46 @@ describe('Directives: userMenu', function () {
     })
   );
 
+  function createUserMenu(givenScope) {
+    var domNode;
+
+    domNode = $compile('<user-menu></user-menu>')(givenScope);
+    givenScope.$digest();
+
+    // ng-if creates a sibling if used on the root element in the directive
+    // so grab the sibling with .next()
+    return domNode.next();
+  }
+
   describe('user menu rendering', function() {
     it('displays nothing when auth is not enabled', function () {
       var domNode;
 
       settings.authEnabled = false;
-      spyOn(authenticationService, 'getAuthenticatedUser').and.returnValue({'name': 'sam mulligan'});
+      domNode = createUserMenu($scope);
 
-      domNode = $compile('<user-menu></user-menu>')($scope);
-      $scope.$digest();
-
-      expect(domNode.hasClass('ng-hide')).toBe(true);
+      expect(domNode.size()).toBe(0);
     });
 
     it('displays the user menu when auth is enabled', function () {
-      var domNode;
+      var domNode, templateElement;
 
       settings.authEnabled = true;
       spyOn(authenticationService, 'getAuthenticatedUser').and.returnValue({'name': 'sam mulligan'});
+      domNode = createUserMenu($scope);
 
-      domNode = $compile('<user-menu></user-menu>')($scope);
-      $scope.$digest();
+      expect(domNode.size()).toBe(1);
+    });
 
-      expect(domNode.hasClass('ng-hide')).toBe(false);
-      expect(domNode.find('.user-name').text()).toBe('sam mulligan');
+    it('displays the user name for both large and small screens', function () {
+      var domNode, templateElement;
+
+      settings.authEnabled = true;
+      spyOn(authenticationService, 'getAuthenticatedUser').and.returnValue({'name': 'sam mulligan'});
+      domNode = createUserMenu($scope);
+
+      expect(domNode.find('.user-name-small').text()).toBe('sam mulligan');
+      expect(domNode.find('.user-name-large').text()).toBe('sam mulligan');
     });
   });
 });
