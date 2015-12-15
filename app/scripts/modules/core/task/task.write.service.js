b5b0fff44c66fcfaa06e72f811b31d0f3fe593f9
chrisb <chrisb@netflix.com>
2015-12-15 14:43:28 -0800

    wait until task state is recorded, then refresh the view

--- app/scripts/modules/core/task/task.write.service.js
+++ app/scripts/modules/core/task/task.write.service.js
@@ -5,8 +5,9 @@ let angular = require('angular');
 module.exports = angular
   .module('spinnaker.core.task.write.service', [
     require('exports?"restangular"!imports?_=lodash!restangular'),
+    require('./task.read.service.js'),
   ])
-  .factory('taskWriter', function(Restangular) {
+  .factory('taskWriter', function(Restangular, taskReader, $q, $timeout) {
 
     var endpoint = Restangular.all('applications');
 
@@ -19,11 +20,46 @@ module.exports = angular
     }
 
     function cancelTask(application, taskId) {
-      return getEndpoint(application).one(taskId).one('cancel').put();
+      return getEndpoint(application).one(taskId).one('cancel').put().then(() =>
+        waitUntilTaskMatches(application, taskId, (task) => task.status === 'CANCELED')
+      );
+    }
+
+    function waitUntilTaskMatches(application, taskId, closure) {
+      return taskReader.getOneTaskForApplication(application, taskId).then((task) => {
+        if (closure(task)) {
+          return task;
+        }
+        return $timeout(() => waitUntilTaskMatches(application, taskId, closure), 1000);
+      });
+    }
+
+    function waitUntilTaskIsDeleted(taskId) {
+      // wait until the task is gone, i.e. the promise from the get() is rejected, before succeeding
+      var deferred = $q.defer();
+      Restangular.one('tasks', taskId).get().then(
+        () => {
+          $timeout(() => {
+            // task is still present, try again
+            waitUntilTaskIsDeleted(taskId).then(deferred.resolve);
+          }, 1000, false);
+        },
+        (resp) => {
+          if (resp.status === 404) {
+            // task is no longer present
+            deferred.resolve();
+          } else {
+            $timeout(() => {
+              // task is maybe still present, try again
+              waitUntilTaskIsDeleted(taskId).then(deferred.resolve);
+            }, 1000, false);
+          }
+        });
+      return deferred.promise;
     }
 
     function deleteTask(taskId) {
-      return Restangular.all('tasks').one(taskId).remove();
+      return Restangular.one('tasks', taskId).remove().then(() => waitUntilTaskIsDeleted(taskId));
     }
 
     return {
