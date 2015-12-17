5e3de210bdd5b41a1134e90f7187e1b843f9796d
AMeng <alexbmeng@gmail.com>
2015-12-17 14:32:59 -0700

    Update gate Jenkins endpoints for /v2/ changes

--- app/scripts/modules/core/ci/jenkins/igor.service.js
+++ app/scripts/modules/core/ci/jenkins/igor.service.js
@@ -7,21 +7,26 @@ module.exports = angular.module('spinnaker.core.ci.jenkins.igor.service', [
   require('exports?"restangular"!imports?_=lodash!restangular'),
 ])
   .factory('igorService', function (settings, Restangular) {
+    var RestangularNoEncoding;
+
+    RestangularNoEncoding = Restangular.withConfig(function(RestangularConfigurer) {
+      RestangularConfigurer.setEncodeIds(false);
+    });
 
     function listMasters() {
-      return Restangular.one('builds').getList();
+      return Restangular.one('v2').one('builds').getList();
     }
 
     function listJobsForMaster(master) {
-      return Restangular.one('builds', master).all('jobs').getList();
+      return Restangular.one('v2').one('builds', master).all('jobs').getList();
     }
 
     function listBuildsForJob(master, job) {
-      return Restangular.one('builds', master).one('jobs', job).all('builds').getList();
+      return RestangularNoEncoding.one('v2').one('builds', master).one('builds').one(job).getList();
     }
 
     function getJobConfig(master, job){
-      return Restangular.one('builds', master).one('jobs', job).get();
+      return RestangularNoEncoding.one('v2').one('builds', master).one('jobs', job).get();
     }
 
     return {
