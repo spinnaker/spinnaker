bf2be709b23748eae69bd53fc2d6bc109db38fac
chrisb <chrisb@netflix.com>
2015-12-30 22:46:01 -0800

    enhanced application name validation - pattern per provider

--- app/scripts/modules/core/application/service/applications.write.service.spec.js
+++ app/scripts/modules/core/application/service/applications.write.service.spec.js
@@ -1,8 +1,9 @@
 'use strict';
 
-describe('Servcie: applicationWriter', function () {
+describe('Service: applicationWriter', function () {
   var applicationWriter;
   var taskExecutor;
+  var $q;
 
   beforeEach(
     window.module(
@@ -11,9 +12,10 @@ describe('Servcie: applicationWriter', function () {
   );
 
   beforeEach(
-    window.inject(function(_applicationWriter_, _taskExecutor_) {
+    window.inject(function(_applicationWriter_, _taskExecutor_, _$q_) {
       applicationWriter = _applicationWriter_;
       taskExecutor = _taskExecutor_;
+      $q = _$q_;
     })
   );
 
@@ -39,8 +41,9 @@ describe('Servcie: applicationWriter', function () {
       expect(taskExecutor.executeTask.calls.count()).toEqual(1);
     });
 
-    it('should execute a task for each account in a application', function () {
-      spyOn(taskExecutor, 'executeTask');
+    it('should execute a single task with multiple jobs for an application with multiple accounts', function () {
+      var jobs = null;
+      spyOn(taskExecutor, 'executeTask').and.callFake((task) => jobs = task.job);
 
       var application = {
         name: 'foo',
@@ -56,11 +59,12 @@ describe('Servcie: applicationWriter', function () {
       applicationWriter.updateApplication(application);
 
       expect(taskExecutor.executeTask).toHaveBeenCalled();
-      expect(taskExecutor.executeTask.calls.count()).toEqual(2);
+      expect(taskExecutor.executeTask.calls.count()).toEqual(1);
+      expect(jobs.length).toBe(2);
     });
 
     it('should join cloud providers into a single string', function () {
-      var job;
+      var job = null;
       spyOn(taskExecutor, 'executeTask').and.callFake((task) => job = task.job[0]);
 
       var application = {
@@ -76,6 +80,7 @@ describe('Servcie: applicationWriter', function () {
 
       applicationWriter.updateApplication(application);
 
+      expect(job).not.toBe(null);
       expect(job.application.cloudProviders).toBe('titan,cf');
 
     });
@@ -83,7 +88,7 @@ describe('Servcie: applicationWriter', function () {
 
   describe('delete an application', function () {
     it('should execute one task if the application has one account', function () {
-      spyOn(taskExecutor, 'executeTask');
+      spyOn(taskExecutor, 'executeTask').and.returnValue($q.when({}));
 
       var application = {
         name: 'foo',
@@ -94,6 +99,25 @@ describe('Servcie: applicationWriter', function () {
 
       expect(taskExecutor.executeTask).toHaveBeenCalled();
       expect(taskExecutor.executeTask.calls.count()).toEqual(1);
+    });
+
+    it('should execute a single task with multiple jobs for an application with multiple accounts', function () {
+      var jobs = null;
+      spyOn(taskExecutor, 'executeTask').and.callFake((task) => {
+        jobs = task.job;
+        return $q.when(task);
+      });
+
+      var application = {
+        name: 'foo',
+        accounts: 'test,prod',
+      };
+
+      applicationWriter.deleteApplication(application);
+
+      expect(taskExecutor.executeTask).toHaveBeenCalled();
+      expect(taskExecutor.executeTask.calls.count()).toEqual(1);
+      expect(jobs.length).toBe(2);
     });
   });
 
