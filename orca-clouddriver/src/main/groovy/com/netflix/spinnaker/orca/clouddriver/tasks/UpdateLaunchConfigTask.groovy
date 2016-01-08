673fe07f07f861a43ecff31c42a94f84a91668a4
Travis Tomsu <ttomsu@google.com>
2016-01-08 15:02:49 -0500

    Makes update launch config stage/task generic.

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/UpdateLaunchConfigTask.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/UpdateLaunchConfigTask.groovy
@@ -1,7 +1,7 @@
 /*
- * Copyright 2015 Netflix, Inc.
+ * Copyright 2016 Google, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.netflix.spinnaker.orca.kato.tasks
+package com.netflix.spinnaker.orca.clouddriver.tasks
 
 import com.netflix.spinnaker.orca.DefaultTaskResult
 import com.netflix.spinnaker.orca.ExecutionStatus
@@ -22,6 +22,7 @@ import com.netflix.spinnaker.orca.Task
 import com.netflix.spinnaker.orca.TaskResult
 import com.netflix.spinnaker.orca.clouddriver.KatoService
 import com.netflix.spinnaker.orca.clouddriver.utils.CloudProviderAware
+import com.netflix.spinnaker.orca.kato.tasks.DeploymentDetailsAware
 import com.netflix.spinnaker.orca.pipeline.model.Stage
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
@@ -30,7 +31,10 @@ import org.springframework.stereotype.Component
 
 @Component
 @Slf4j
-class ModifyAsgLaunchConfigurationTask implements Task, DeploymentDetailsAware {
+class UpdateLaunchConfigTask implements Task, DeploymentDetailsAware, CloudProviderAware {
+
+  public static final String OPERATION = "updateLaunchConfig"
+
   @Autowired
   KatoService kato
 
@@ -39,29 +43,43 @@ class ModifyAsgLaunchConfigurationTask implements Task, DeploymentDetailsAware {
 
   @Override
   TaskResult execute(Stage stage) {
-    def operationConfig = new HashMap(stage.context)
-    operationConfig.amiName = getImage(stage)
+    def ops
+    def cloudProvider = getCloudProvider(stage)
+    if (cloudProvider == "aws") {
+      // Since AWS is the only provider that needs to do anything here, it felt like overkill to do the
+      // provider-specific rigmarole here.
+      ops = getAwsOps(stage)
+    } else {
+      ops = [[(OPERATION): stage.context]]
+    }
+
+    def taskId = kato.requestOperations(cloudProvider, ops)
+        .toBlocking()
+        .first()
+    new DefaultTaskResult(ExecutionStatus.SUCCEEDED, [
+        "notification.type"                        : "modifyasglaunchconfiguration",
+        "modifyasglaunchconfiguration.account.name": getCredentials(stage),
+        "modifyasglaunchconfiguration.region"      : stage.context.region,
+        "kato.last.task.id"                        : taskId,
+        "deploy.server.groups"                     : [(stage.context.region): [stage.context.serverGroupName ?: stage.context.asgName]]
+    ])
+  }
+
+  private getAwsOps(Stage stage) {
+    def operation = new HashMap(stage.context)
+    operation.amiName = getImage(stage)
 
     def ops = []
     if (stage.context.credentials != defaultBakeAccount) {
-      ops << [allowLaunchDescription: convertAllowLaunch(stage.context.credentials, defaultBakeAccount,
-                                                         stage.context.region, operationConfig.amiName)]
+      ops << [allowLaunchDescription: convertAllowLaunch(getCredentials(stage),
+                                                         defaultBakeAccount,
+                                                         stage.context.region as String,
+                                                         operation.amiName as String)]
       log.info("Generated `allowLaunchDescription` (allowLaunchDescription: ${ops})")
     }
 
-    ops << [modifyAsgLaunchConfigurationDescription: operationConfig]
-
-    def taskId = kato.requestOperations(CloudProviderAware.DEFAULT_CLOUD_PROVIDER, ops)
-                     .toBlocking()
-                     .first()
-    new DefaultTaskResult(ExecutionStatus.SUCCEEDED, [
-      "notification.type"                        : "modifyasglaunchconfiguration",
-      "modifyasglaunchconfiguration.account.name": stage.context.credentials,
-      "modifyasglaunchconfiguration.region"      : stage.context.region,
-      "kato.last.task.id"                        : taskId,
-      "kato.task.id"                             : taskId, // TODO retire this.
-      "deploy.server.groups"                     : [(stage.context.region): [stage.context.asgName]]
-    ])
+    ops << [(OPERATION): operation]
+    ops
   }
 
   private String getImage(Stage stage) {
---
+++ orca-clouddriver/src/test/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/UpdateLaunchConfigTaskSpec.groovy
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2016 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.clouddriver.tasks
+
+import com.netflix.spinnaker.orca.clouddriver.KatoService
+import com.netflix.spinnaker.orca.clouddriver.model.TaskId
+import com.netflix.spinnaker.orca.pipeline.model.Pipeline
+import com.netflix.spinnaker.orca.pipeline.model.PipelineStage
+import rx.Observable
+import spock.lang.Specification
+import spock.lang.Subject
+
+class UpdateLaunchConfigTaskSpec extends Specification {
+
+  KatoService katoService = Mock(KatoService)
+
+  @Subject
+  UpdateLaunchConfigTask task = new UpdateLaunchConfigTask(kato: katoService, defaultBakeAccount: "default")
+
+  void "should populate deploy.server.groups to enable force cache refresh"() {
+    setup:
+      def taskConfig = [
+          credentials: "test",
+          region     : region,
+          asgName    : asgName
+      ]
+      def stage = new PipelineStage(new Pipeline(), "whatever", taskConfig)
+
+    when:
+      def result = task.execute(stage)
+
+    then:
+      1 * katoService.requestOperations("aws", _) >> rx.Observable.just(new TaskId("blerg"))
+      result.stageOutputs."deploy.server.groups" == [(region): [asgName]]
+
+    where:
+      region = "us-east-1"
+      asgName = "myasg-v001"
+  }
+
+  void "should add amiName from upstream task if not specified"() {
+    setup:
+      def taskConfig = [
+          credentials      : "test",
+          region           : region,
+          asgName          : asgName,
+          deploymentDetails: [
+              [ami: deploymentDetailsAmi, region: region]
+          ],
+          amiName          : contextAmi
+      ]
+      def stage = new PipelineStage(new Pipeline(), "whatever", taskConfig)
+
+    when:
+      def result = task.execute(stage)
+
+    then:
+      1 * katoService.requestOperations("aws", _) >> { cloudProvider, ops ->
+        def opConfig = ops.last().updateLaunchConfig
+
+        assert opConfig.amiName == expectedAmi
+        rx.Observable.just(new TaskId("blerg"))
+      }
+
+    where:
+      deploymentDetailsAmi | contextAmi | expectedAmi
+      "ami-dd"             | "ami-cc"   | "ami-cc"
+      "ami-dd"             | null       | "ami-dd"
+      region = "us-east-1"
+      asgName = "myasg-v001"
+  }
+
+  def "prefers the ami from an upstream stage to one from deployment details"() {
+    given:
+      def taskConfig = [
+          credentials      : "test",
+          region           : region,
+          asgName          : asgName,
+          deploymentDetails: [
+              ["ami": "not-my-ami", "region": region],
+              ["ami": "also-not-my-ami", "region": region]
+          ]
+      ]
+      def stage = new PipelineStage(new Pipeline(), "whatever", taskConfig)
+      stage.context.amiName = null
+      stage.context.deploymentDetails = [
+          ["ami": "not-my-ami", "region": region],
+          ["ami": "also-not-my-ami", "region": region]
+      ]
+
+
+    and:
+      def operations = []
+      interaction {
+        def taskId = new TaskId(UUID.randomUUID().toString())
+        katoService.requestOperations(*_) >> { cloudProvider, ops ->
+          operations.addAll(ops.flatten())
+          Observable.from(taskId)
+        }
+      }
+
+    and:
+      def bakeStage1 = new PipelineStage(stage.execution, "bake")
+      bakeStage1.id = UUID.randomUUID()
+      bakeStage1.refId = "1a"
+      stage.execution.stages << bakeStage1
+
+      def bakeSynthetic1 = new PipelineStage(stage.execution, "bake in $region", [ami: amiName, region: region])
+      bakeSynthetic1.id = UUID.randomUUID()
+      bakeSynthetic1.parentStageId = bakeStage1.id
+      stage.execution.stages << bakeSynthetic1
+
+      def bakeStage2 = new PipelineStage(stage.execution, "bake")
+      bakeStage2.id = UUID.randomUUID()
+      bakeStage2.refId = "2a"
+      stage.execution.stages << bakeStage2
+
+      def bakeSynthetic2 = new PipelineStage(stage.execution, "bake in $region",
+                                             [ami: "parallel-branch-ami", region: region])
+      bakeSynthetic2.id = UUID.randomUUID()
+      bakeSynthetic2.parentStageId = bakeStage2.id
+      stage.execution.stages << bakeSynthetic2
+
+      def intermediateStage = new PipelineStage(stage.execution, "whatever")
+      intermediateStage.id = UUID.randomUUID()
+      intermediateStage.refId = "1b"
+      stage.execution.stages << intermediateStage
+
+    and:
+      intermediateStage.requisiteStageRefIds = [bakeStage1.refId]
+      stage.requisiteStageRefIds = [intermediateStage.refId]
+
+    when:
+      task.execute(stage.asImmutable())
+
+    then:
+      operations.find {
+        it.containsKey("updateLaunchConfig")
+      }.updateLaunchConfig.amiName == amiName
+
+    where:
+      amiName = "ami-name-from-bake"
+      region = "us-east-1"
+      asgName = "myasg-v001"
+  }
+
+  void "should inject allowLaunch if deploy account does not match bake account"() {
+    setup:
+      def taskConfig = [
+          credentials: credentials,
+          region     : region,
+          asgName    : asgName,
+          amiName    : "ami-abcdef"
+      ]
+      def stage = new PipelineStage(new Pipeline(), "whatever", taskConfig)
+
+    when:
+      task.execute(stage)
+
+    then:
+      1 * katoService.requestOperations("aws", _) >> { cloudProvider, ops ->
+        assert ops.size() == expectedOpsSize
+        if (expectedOpsSize == 2) {
+          def allowLaunch = ops.first().allowLaunchDescription
+          assert allowLaunch.account == credentials
+          assert allowLaunch.credentials == "default"
+          assert allowLaunch.region == region
+          assert allowLaunch.amiName == "ami-abcdef"
+        }
+        rx.Observable.just(new TaskId("blerg"))
+      }
+
+    where:
+      credentials | expectedOpsSize
+      "default"   | 1
+      "test"      | 2
+      region = "us-east-1"
+      asgName = "myasg-v001"
+  }
+
+  def "should pass context unchanged with non-AWS cloud provider"() {
+    setup:
+      def taskConfig = [
+          credentials  : "default",
+          region       : "north-pole",
+          foo          : "bar",
+          cloudProvider: "abc"
+      ]
+      def stage = new PipelineStage(new Pipeline(), "whatever", taskConfig)
+
+    when:
+      task.execute(stage)
+
+    then:
+      1 * katoService.requestOperations("abc", _) >> { cloudProvider, ops ->
+        assert ops.size() == 1
+        ops.find {
+          it.containsKey("updateLaunchConfig")
+        }.updateLaunchConfig == taskConfig
+        rx.Observable.just(new TaskId("blerg"))
+      }
+  }
+}
