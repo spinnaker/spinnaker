a03c1294a5e046255f340147b693d6f099f67bf1
tomaslin <clin@netflix.com>
2016-02-01 15:35:22 -0800

    surface webhooks endpoint from echo and remove deprecated events endpoint

--- gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/EventController.groovy
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/EventController.groovy
@@ -19,8 +19,6 @@ package com.netflix.spinnaker.gate.controllers
 import com.netflix.spinnaker.gate.services.EventService
 import groovy.transform.CompileStatic
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
 import org.springframework.web.bind.annotation.*
 
 @CompileStatic
@@ -30,19 +28,8 @@ class EventController {
   @Autowired
   EventService eventService
 
-  @RequestMapping(value = "/events", method = RequestMethod.GET)
-  HttpEntity<List<Map>> all(@RequestParam(value = "offset", required = false, defaultValue = "0") Integer offset,
-                            @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
-    def events = eventService.getAll(offset, size)
-    def headers = new HttpHeaders()
-    headers.add("X-Result-Total", Integer.valueOf(events.total as String).toString())
-    headers.add("X-Result-Offset", offset.toString())
-    headers.add("X-Result-Size", Integer.valueOf(events.paginationSize as String).toString())
-    new HttpEntity(events.hits, headers)
-  }
-
-  @RequestMapping(value = "/applications/{app}/events", method = RequestMethod.GET)
-  List getForApp(@PathVariable("app") String app) {
-    eventService.getForApplication(app)?.hits as List<Map>
+  @RequestMapping(value = "/webhooks/{type}/{source}", method = RequestMethod.POST)
+  void webhooks(@PathVariable("type") String type, @PathVariable("source") String source, @RequestBody Map event) {
+    eventService.webhooks(type, source, event)
   }
 }
