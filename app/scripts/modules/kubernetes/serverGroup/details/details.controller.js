4c738add2f8c3f07a00eee73fd1f9541b9234b49
Lars Wander <lwander@google.com>
2016-02-06 10:50:53 -0500

    provider/kubernetes: Render basic server group information on "cluster" screen

---
+++ app/scripts/modules/kubernetes/serverGroup/details/details.controller.js
@@ -0,0 +1,118 @@
+'use strict';
+/* jshint camelcase:false */
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.serverGroup.details.kubernetes.controller', [
+  require('angular-ui-router'),
+  require('../../../core/serverGroup/serverGroup.read.service.js'),
+  require('../../../core/serverGroup/details/serverGroupWarningMessage.service.js'),
+  require('../../../core/serverGroup/serverGroup.write.service.js'),
+  require('../../../core/serverGroup/configure/common/runningExecutions.service.js'),
+  require('../../../core/utils/lodash.js'),
+  require('../../../core/insight/insightFilterState.model.js'),
+  require('../../../core/utils/selectOnDblClick.directive.js'),
+])
+  .controller('kubernetesDetailsController', function ($scope, $state, app, serverGroup, InsightFilterStateModel,
+                                                       serverGroupReader, $uibModal, serverGroupWriter,
+                                                       runningExecutionsService, serverGroupWarningMessageService) {
+
+    let application = app;
+
+    $scope.state = {
+      loading: true
+    };
+
+    $scope.InsightFilterStateModel = InsightFilterStateModel;
+
+    function extractServerGroupSummary() {
+      var summary = _.find(application.serverGroups, function (toCheck) {
+        return toCheck.name === serverGroup.name && toCheck.account === serverGroup.accountId && toCheck.region === serverGroup.region;
+      });
+      if (!summary) {
+        application.loadBalancers.some(function (loadBalancer) {
+          if (loadBalancer.account === serverGroup.accountId && loadBalancer.region === serverGroup.region) {
+            return loadBalancer.serverGroups.some(function (possibleServerGroup) {
+              if (possibleServerGroup.name === serverGroup.name) {
+                summary = possibleServerGroup;
+                return true;
+              }
+            });
+          }
+        });
+      }
+      return summary;
+    }
+
+    function retrieveServerGroup() {
+      var summary = extractServerGroupSummary();
+      return serverGroupReader.getServerGroup(application.name, serverGroup.accountId, serverGroup.region, serverGroup.name).then(function(details) {
+        cancelLoader();
+
+        var restangularlessDetails = details.plain();
+        angular.extend(restangularlessDetails, summary);
+
+        $scope.serverGroup = restangularlessDetails;
+        $scope.runningExecutions = function() {
+          return runningExecutionsService.filterRunningExecutions($scope.serverGroup.executions);
+        };
+      },
+        autoClose
+      );
+    }
+
+    function autoClose() {
+      if ($scope.$$destroyed) {
+        return;
+      }
+      $state.params.allowModalToStayOpen = true;
+      $state.go('^', null, {location: 'replace'});
+    }
+
+    function cancelLoader() {
+      $scope.state.loading = false;
+    }
+
+    retrieveServerGroup().then(() => {
+      // If the user navigates away from the view before the initial retrieveServerGroup call completes,
+      // do not bother subscribing to the autoRefreshStream
+      if (!$scope.$$destroyed) {
+        let refreshWatcher = app.autoRefreshStream.subscribe(retrieveServerGroup);
+        $scope.$on('$destroy', () => refreshWatcher.dispose());
+      }
+    });
+
+    this.destroyServerGroup = () => {
+    };
+
+    this.getBodyTemplate = (serverGroup, application) => {
+      if (this.isLastServerGroupInRegion(serverGroup, application)) {
+        return serverGroupWarningMessageService.getMessage(serverGroup);
+      }
+    };
+
+    this.isLastServerGroupInRegion = function (serverGroup, application ) {
+      try {
+        var cluster = _.find(application.clusters, {name: serverGroup.cluster, account:serverGroup.account});
+        return _.filter(cluster.serverGroups, {region: serverGroup.region}).length === 1;
+      } catch (error) {
+        return false;
+      }
+    };
+
+    this.disableServerGroup = function disableServerGroup() {
+    };
+
+    this.enableServerGroup = function enableServerGroup() {
+    };
+
+    this.rollbackServerGroup = function rollbackServerGroup() {
+    };
+
+    this.resizeServerGroup = function resizeServerGroup() {
+    };
+
+    this.cloneServerGroup = function cloneServerGroup() {
+    };
+  }
+);
