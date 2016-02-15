4379236302c643a2456b72d136c75ed2ca892cf1
Adam Jordens <ajordens@netflix.com>
2016-02-15 12:48:34 -0800

    No sense attempting to resize an ASG that no longer exists (or is in the process of being destroyed)

--- clouddriver-aws/src/test/groovy/com/netflix/spinnaker/clouddriver/aws/deploy/ops/ResizeAsgAtomicOperationUnitSpec.groovy
+++ clouddriver-aws/src/test/groovy/com/netflix/spinnaker/clouddriver/aws/deploy/ops/ResizeAsgAtomicOperationUnitSpec.groovy
@@ -16,6 +16,9 @@
 
 package com.netflix.spinnaker.clouddriver.aws.deploy.ops
 import com.amazonaws.services.autoscaling.AmazonAutoScaling
+import com.amazonaws.services.autoscaling.model.AutoScalingGroup
+import com.amazonaws.services.autoscaling.model.DescribeAutoScalingGroupsRequest
+import com.amazonaws.services.autoscaling.model.DescribeAutoScalingGroupsResult
 import com.amazonaws.services.autoscaling.model.UpdateAutoScalingGroupRequest
 import com.netflix.spinnaker.clouddriver.aws.security.AmazonClientProvider
 import com.netflix.spinnaker.clouddriver.data.task.Task
@@ -23,6 +26,7 @@ import com.netflix.spinnaker.clouddriver.data.task.TaskRepository
 import com.netflix.spinnaker.clouddriver.aws.TestCredential
 import com.netflix.spinnaker.clouddriver.aws.deploy.description.ResizeAsgDescription
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class ResizeAsgAtomicOperationUnitSpec extends Specification {
 
@@ -30,7 +34,8 @@ class ResizeAsgAtomicOperationUnitSpec extends Specification {
     TaskRepository.threadLocalTask.set(Mock(Task))
   }
 
-  void "operation invokes update to autoscaling group"() {
+  @Unroll
+  void "should update ASG iff it exists and is not in the process of being deleted"() {
     setup:
     def mockAutoScaling = Mock(AmazonAutoScaling)
     def mockAmazonClientProvider = Mock(AmazonClientProvider)
@@ -47,11 +52,23 @@ class ResizeAsgAtomicOperationUnitSpec extends Specification {
     operation.operate([])
 
     then:
-    1 * mockAutoScaling.updateAutoScalingGroup(_) >> { UpdateAutoScalingGroupRequest request ->
+    1 * mockAutoScaling.describeAutoScalingGroups(_) >> { DescribeAutoScalingGroupsRequest request ->
+      assert request.autoScalingGroupNames == ["myasg-stack-v000"]
+      return new DescribeAutoScalingGroupsResult().withAutoScalingGroups(
+        new AutoScalingGroup().withAutoScalingGroupName("myasg-stack-v0001").withStatus(status)
+      )
+    }
+    expectedUpdateCount * mockAutoScaling.updateAutoScalingGroup(_) >> { UpdateAutoScalingGroupRequest request ->
       assert request.autoScalingGroupName == "myasg-stack-v000"
       assert request.minSize == 1
       assert request.maxSize == 2
       assert request.desiredCapacity == 5
     }
+    0 * mockAutoScaling._
+
+    where:
+    status     || expectedUpdateCount
+    "Deleting" || 0
+    null       || 1
   }
 }
