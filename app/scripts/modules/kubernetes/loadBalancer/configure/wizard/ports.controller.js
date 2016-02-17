82b14e479790b15d828f9a6ad42b9e0d8659def4
Lars Wander <lwander@google.com>
2016-02-17 14:25:05 -0500

    provider/kubernetes: Display load balancer dialog

---
+++ app/scripts/modules/kubernetes/loadBalancer/configure/wizard/ports.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.loadBalancer.configure.kubernetes.ports', [])
+  .controller('kubernetesLoadBalancerPortsController', function($scope) {
+    this.addPort = function() {
+      $scope.loadBalancer.ports.push({});
+    };
+
+    this.removePort = function(index) {
+      $scope.loadBalancer.ports.splice(index, 1);
+    };
+
+    this.protocols = ['TCP', 'UDP'];
+    this.maxPort = 65535;
+  });
---
+++ app/scripts/modules/kubernetes/loadBalancer/configure/wizard/upsert.controller.js
@@ -0,0 +1,165 @@
+'use strict';
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.loadBalancer.kubernetes.create.controller', [
+  require('angular-ui-router'),
+  require('../../../../core/loadBalancer/loadBalancer.write.service.js'),
+  require('../../../../core/loadBalancer/loadBalancer.read.service.js'),
+  require('../../../../core/account/account.service.js'),
+  require('../../../../core/modal/wizard/v2modalWizard.service.js'),
+  require('../../../../core/task/monitor/taskMonitorService.js'),
+  require('../../../../core/search/search.service.js'),
+  require('../../../namespace/selectField.directive.js'),
+  require('../../transformer.js'),
+])
+  .controller('kubernetesUpsertLoadBalancerController', function($scope, $modalInstance, $state,
+                                                                 application, loadBalancer, isNew, loadBalancerReader,
+                                                                 accountService, kubernetesLoadBalancerTransformer,
+                                                                 _, searchService, v2modalWizardService, loadBalancerWriter, taskMonitorService) {
+
+    var ctrl = this;
+    $scope.isNew = isNew;
+
+    $scope.pages = {
+      basicSettings: require('./basicSettings.html'),
+      ports: require('./ports.html'),
+      advancedSettings: require('./advancedSettings.html'),
+    };
+
+    $scope.state = {
+      accountsLoaded: false,
+      loadBalancerNamesLoaded: false,
+      submitting: false
+    };
+
+    function onApplicationRefresh() {
+      // If the user has already closed the modal, do not navigate to the new details view
+      if ($scope.$$destroyed) {
+        return;
+      }
+      $modalInstance.close();
+      var newStateParams = {
+        name: $scope.loadBalancer.name,
+        accountId: $scope.loadBalancer.credentials,
+        region: $scope.loadBalancer.region,
+        provider: 'kubernetes',
+      };
+      if (!$state.includes('**.loadBalancerDetails')) {
+        $state.go('.loadBalancerDetails', newStateParams);
+      } else {
+        $state.go('^.loadBalancerDetails', newStateParams);
+      }
+    }
+
+    function onTaskComplete() {
+      application.loadBalancers.refresh();
+      application.loadBalancers.onNextRefresh($scope, onApplicationRefresh);
+    }
+
+    $scope.taskMonitor = taskMonitorService.buildTaskMonitor({
+      application: application,
+      title: (isNew ? 'Creating ' : 'Updating ') + 'your load balancer',
+      modalInstance: $modalInstance,
+      onTaskComplete: onTaskComplete,
+    });
+
+    var allLoadBalancerNames = {};
+
+    function initializeEditMode() {
+    }
+
+    function initializeCreateMode() {
+      accountService.listAccounts('kubernetes').then(function (accounts) {
+        $scope.accounts = accounts;
+        $scope.state.accountsLoaded = true;
+
+        var accountNames = _.pluck($scope.accounts, 'name');
+        if (accountNames.length && accountNames.indexOf($scope.loadBalancer.credentials) === -1) {
+          $scope.loadBalancer.credentials = accountNames[0];
+        }
+
+        ctrl.accountUpdated();
+      });
+    }
+
+    function initializeLoadBalancerNames() {
+      loadBalancerReader.listLoadBalancers('kubernetes').then(function (loadBalancers) {
+        loadBalancers.forEach((loadBalancer) => {
+          let account = loadBalancer.account;
+          if (!allLoadBalancerNames[account]) {
+            allLoadBalancerNames[account] = {};
+          }
+          let namespace = loadBalancer.namespace;
+          if (!allLoadBalancerNames[account][namespace]) {
+            allLoadBalancerNames[account][namespace] = [];
+          }
+          allLoadBalancerNames[account][namespace].push(loadBalancer.name);
+        });
+
+        updateLoadBalancerNames();
+        $scope.state.loadBalancerNamesLoaded = true;
+      });
+    }
+
+    function updateLoadBalancerNames() {
+      var account = $scope.loadBalancer.credentials;
+
+      if (allLoadBalancerNames[account]) {
+        $scope.existingLoadBalancerNames = _.flatten(_.map(allLoadBalancerNames[account]));
+      } else {
+        $scope.existingLoadBalancerNames = [];
+      }
+    }
+
+    // initialize controller
+    if (loadBalancer) {
+      $scope.loadBalancer = kubernetesLoadBalancerTransformer.convertLoadBalancerForEditing(loadBalancer);
+      initializeEditMode();
+    } else {
+      $scope.loadBalancer = kubernetesLoadBalancerTransformer.constructNewLoadBalancerTemplate();
+      initializeLoadBalancerNames();
+      initializeCreateMode();
+    }
+
+    // Controller API
+    this.updateName = function() {
+      $scope.loadBalancer.name = this.getName();
+    };
+
+    this.getName = function() {
+      var loadBalancer = $scope.loadBalancer;
+      var loadBalancerName = [application.name, (loadBalancer.stack || ''), (loadBalancer.detail || '')].join('-');
+      return _.trimRight(loadBalancerName, '-');
+    };
+
+    this.accountUpdated = function() {
+      accountService.getAccountDetails($scope.loadBalancer.credentials).then(function(details) {
+        $scope.namespaces = details.namespaces;
+        ctrl.namespaceUpdated();
+      });
+    };
+
+    this.namespaceUpdated = function() {
+      updateLoadBalancerNames();
+      ctrl.updateName();
+    };
+
+    this.submit = function () {
+      var descriptor = isNew ? 'Create' : 'Update';
+
+      $scope.taskMonitor.submit(
+        function() {
+          let params = {
+            cloudProvider: 'kubernetes',
+            loadBalancerName: $scope.loadBalancer.name,
+          };
+          return loadBalancerWriter.upsertLoadBalancer($scope.loadBalancer, application, descriptor, params);
+        }
+      );
+    };
+
+    this.cancel = function () {
+      $modalInstance.dismiss();
+    };
+  });
