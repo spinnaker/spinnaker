94dedc2034ebad30607b8c76aaa72b7528c7d209
chrisb <chrisb@netflix.com>
2016-02-21 08:51:13 -0800

    fix linter warnings in cf module

--- app/scripts/modules/cloudfoundry/serverGroup/configure/wizard/ServerGroupAdvanced.controller.js
+++ app/scripts/modules/cloudfoundry/serverGroup/configure/wizard/ServerGroupAdvanced.controller.js
@@ -5,7 +5,7 @@ let angular = require('angular');
 module.exports = angular.module('spinnaker.serverGroup.configure.cf.advanced.controller', [
         require('../../../../core/modal/wizard/v2modalWizard.service.js'),
     ])
-    .controller('cfServerGroupAdvancedCtrl', function($scope, v2modalWizardService) {
+    .controller('cfServerGroupAdvancedCtrl', function(/*$scope, v2modalWizardService*/) {
 
         // TODO(GLT): Fix roles after Find/Bake updates are rolled in.
 
--- app/scripts/modules/cloudfoundry/serverGroup/configure/wizard/ServerGroupLoadBalancers.controller.js
+++ app/scripts/modules/cloudfoundry/serverGroup/configure/wizard/ServerGroupLoadBalancers.controller.js
@@ -5,7 +5,7 @@ let angular = require('angular');
 module.exports = angular.module('spinnaker.serverGroup.configure.cf.loadBalancers.controller', [
         require('../../../../core/modal/wizard/v2modalWizard.service.js'),
     ])
-    .controller('cfServerGroupLoadBalancersCtrl', function($scope, v2modalWizardService) {
+    .controller('cfServerGroupLoadBalancersCtrl', function(/*$scope, v2modalWizardService*/) {
 
         // TODO(GLT): Fix roles after Find/Bake updates are rolled in.
 
