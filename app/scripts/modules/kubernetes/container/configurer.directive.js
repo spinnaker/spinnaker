fb358ec1eb8bac52393ed464090f8d1b7260cc54
Lars Wander <lwander@google.com>
2016-02-22 12:48:03 -0500

    provider/kubernetes: Pick SG ports & LBs

--- app/scripts/modules/kubernetes/container/configurer.directive.js
+++ app/scripts/modules/kubernetes/container/configurer.directive.js
@@ -9,10 +9,22 @@ module.exports = angular.module('spinnaker.kubernetes.container.configurer.direc
       templateUrl: require('./configurer.directive.html'),
       scope: {
         container: '=',
+        index: '=',
       }
     };
   })
-  .controller('kubernetesContainerConfigurerController', function() {
+  .controller('kubernetesContainerConfigurerController', function($scope) {
     this.cpuPattern = /^\d+(m)?$/;
     this.memoryPattern = /^\d+(Mi|Gi)?$/;
+
+    this.removePort = function(index) {
+      $scope.container.ports.splice(index, 1);
+    };
+
+    this.addPort = function() {
+      $scope.container.ports.push({ protocol: 'TCP' });
+    };
+
+    this.protocols = ['TCP', 'UDP'];
+    this.maxPort = 65535;
   });
---
+++ app/scripts/modules/kubernetes/serverGroup/configure/wizard/loadBalancers.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.serverGroup.configure.kubernetes.loadBalancers', [
+  require('../configuration.service.js')
+])
+  .controller('kubernetesServerGroupLoadBalancersController', function(kubernetesServerGroupConfigurationService, infrastructureCaches, $scope) {
+    this.refreshLoadBalancers = function() {
+      kubernetesServerGroupConfigurationService.refreshLoadBalancers($scope.command, false);
+    };
+
+    this.getLoadBalancerRefreshTime = function() {
+      return infrastructureCaches.loadBalancers.getStats().ageMax;
+    };
+  });
