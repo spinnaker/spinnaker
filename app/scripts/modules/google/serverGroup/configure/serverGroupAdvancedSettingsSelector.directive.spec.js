7a4c4f4e02c0fbec610ada154967f4e80abd6734
duftler <duftler@google.com>
2016-02-24 10:26:31 -0500

    Use fancy new map-editor instead of hand-rolling GCE Instance Metadata. Define addButtonLabel on map-editor to override default 'Add Field' text.

--- app/scripts/modules/google/serverGroup/configure/serverGroupAdvancedSettingsSelector.directive.spec.js
+++ app/scripts/modules/google/serverGroup/configure/serverGroupAdvancedSettingsSelector.directive.spec.js
@@ -20,40 +20,6 @@ describe('Directive: GCE Group Advanced Settings Selector', function() {
     this.scope.$digest();
   }));
 
-  it('should correctly add a metadata key/value to the command', function() {
-    expect(this.scope.command.instanceMetadata.length).toEqual(0);
-
-    this.elem.find('table.metadata button').trigger('click');
-    this.scope.$apply();
-    expect(this.scope.command.instanceMetadata.length).toEqual(1);
-
-    var inputs = this.elem.find('table.metadata input');
-    expect(inputs.length).toEqual(2);
-    $(inputs[0]).val('myKey').trigger('input');
-    $(inputs[1]).val('myVal').trigger('input');
-    this.scope.$apply();
-
-    expect(this.scope.command.instanceMetadata.length).toEqual(1);
-    expect(this.scope.command.instanceMetadata[0].key).toEqual('myKey');
-    expect(this.scope.command.instanceMetadata[0].value).toEqual('myVal');
-  });
-
-  it('should correctly remove a metadata key/value from the command', function() {
-    this.scope.command.instanceMetadata.push({'key': 'myKey1', 'value': 'myVal1'},
-      {'key': 'myKey2', 'value': 'myVal2'});
-    this.scope.$apply();
-
-    var removeLinks = this.elem.find('table.metadata a');
-    expect(removeLinks.length).toEqual(2);
-
-    $(removeLinks[0]).trigger('click');
-    this.scope.$apply();
-
-    expect(this.scope.command.instanceMetadata.length).toEqual(1);
-    expect(this.scope.command.instanceMetadata[0].key).toEqual('myKey2');
-    expect(this.scope.command.instanceMetadata[0].value).toEqual('myVal2');
-  });
-
   it('should correctly add tags to the command', function() {
     expect(this.scope.command.tags.length).toEqual(0);
 
