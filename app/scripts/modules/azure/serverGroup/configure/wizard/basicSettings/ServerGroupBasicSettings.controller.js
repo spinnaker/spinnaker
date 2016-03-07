4bd030f34d6af0c8e134d4d678677521a5e26094
JargoonPard <larry.guger@hotmail.com>
2016-03-07 15:29:37 -0800

    Add region filter for image list

--- app/scripts/modules/azure/serverGroup/configure/wizard/basicSettings/ServerGroupBasicSettings.controller.js
+++ app/scripts/modules/azure/serverGroup/configure/wizard/basicSettings/ServerGroupBasicSettings.controller.js
@@ -5,10 +5,12 @@ let angular = require('angular');
 module.exports = angular.module('spinnaker.azure.serverGroup.configure.basicSettings', [
   require('angular-ui-router'),
   require('angular-ui-bootstrap'),
+  require('./image.regional.filter.js'),
   require('../../../../../core/serverGroup/configure/common/basicSettingsMixin.controller.js'),
   require('../../../../../core/modal/wizard/v2modalWizard.service.js'),
   require('../../../../../core/image/image.reader.js'),
   require('../../../../../core/naming/naming.service.js'),
+  require('../../../../../core/utils/lodash.js'),
 ])
   .controller('azureServerGroupBasicSettingsCtrl', function($scope, $controller, $uibModalStack, $state,
                                                           v2modalWizardService, imageReader, namingService) {
