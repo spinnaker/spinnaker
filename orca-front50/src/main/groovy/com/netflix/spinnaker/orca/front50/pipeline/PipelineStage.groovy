69520694dc35124e0a23618a9edc13e726d4f944
jcusa <jianic@gmail.com>
2016-03-24 16:07:45 -0700

    Making pipeline stage restartable.

--- orca-front50/src/main/groovy/com/netflix/spinnaker/orca/front50/pipeline/PipelineStage.groovy
+++ orca-front50/src/main/groovy/com/netflix/spinnaker/orca/front50/pipeline/PipelineStage.groovy
@@ -16,17 +16,21 @@
 
 package com.netflix.spinnaker.orca.front50.pipeline
 
+import com.netflix.spinnaker.orca.ExecutionStatus
+import com.netflix.spinnaker.orca.batch.RestartableStage
 import com.netflix.spinnaker.orca.front50.tasks.MonitorPipelineTask
 import com.netflix.spinnaker.orca.front50.tasks.StartPipelineTask
 import com.netflix.spinnaker.orca.pipeline.LinearStage
 import com.netflix.spinnaker.orca.pipeline.model.Stage
+import com.netflix.spinnaker.orca.pipeline.model.Task
 import groovy.transform.CompileStatic
 import org.springframework.batch.core.Step
 import org.springframework.stereotype.Component
 
+
 @Component
 @CompileStatic
-class PipelineStage extends LinearStage {
+class PipelineStage extends LinearStage implements RestartableStage {
   public static final String PIPELINE_CONFIG_TYPE = "pipeline"
 
   PipelineStage() {
@@ -39,5 +43,24 @@ class PipelineStage extends LinearStage {
       buildStep(stage, "startPipeline", StartPipelineTask),
       buildStep(stage, "monitorPipeline", MonitorPipelineTask)
     ]
+  }
+
+  @Override
+  Stage prepareStageForRestart(Stage stage) {
+    stage = super.prepareStageForRestart(stage)
+    stage.startTime = null
+    stage.endTime = null
+
+    stage.context.remove("status")
+    stage.context.remove("executionName")
+    stage.context.remove("executionId")
+
+    stage.tasks.each { Task task ->
+      task.startTime = null
+      task.endTime = null
+      task.status = ExecutionStatus.NOT_STARTED
+    }
+
+    return stage
   }
 }
