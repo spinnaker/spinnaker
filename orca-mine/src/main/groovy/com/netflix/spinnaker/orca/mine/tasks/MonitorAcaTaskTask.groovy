f7c947151bb5073003b002fd3bda1d2632121583
Zan Thrash <zanthrash@gmail.com>
2016-04-21 13:04:17 -0700

    Add Stage for Generic ACA Task Stage.

---
+++ orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/tasks/MonitorAcaTaskTask.groovy
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.mine.tasks
+
+import com.netflix.spinnaker.orca.DefaultTaskResult
+import com.netflix.spinnaker.orca.ExecutionStatus
+import com.netflix.spinnaker.orca.RetryableTask
+import com.netflix.spinnaker.orca.TaskResult
+import com.netflix.spinnaker.orca.clouddriver.KatoService
+import com.netflix.spinnaker.orca.clouddriver.tasks.AbstractCloudProviderAwareTask
+import com.netflix.spinnaker.orca.mine.MineService
+import com.netflix.spinnaker.orca.pipeline.model.Stage
+import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import retrofit.RetrofitError
+
+import java.util.concurrent.TimeUnit
+
+@Component
+@Slf4j
+class MonitorAcaTaskTask extends AbstractCloudProviderAwareTask implements RetryableTask {
+  long backoffPeriod = 10000
+  long timeout = TimeUnit.DAYS.toMillis(2)
+
+  @Autowired
+  MineService mineService
+
+  @Override
+  TaskResult execute(Stage stage) {
+    Map context = stage.context
+    Map outputs = [
+      canary : context.canary
+    ]
+
+    try {
+      outputs << [
+        canary : mineService.getCanary(context.canary.id)
+      ]
+    } catch (RetrofitError e) {
+      log.error("Exception occurred while getting canary with id ${context.canary.id} from mine service", e)
+      return new DefaultTaskResult(ExecutionStatus.RUNNING, outputs)
+    }
+
+    if (outputs.canary.status?.complete) {
+      log.info("Canary $stage.id complete")
+      return new DefaultTaskResult(ExecutionStatus.SUCCEEDED, outputs, outputs)
+    }
+
+    log.info("Canary in progress: ${outputs.canary}")
+    return new DefaultTaskResult(ExecutionStatus.RUNNING, outputs)
+  }
+}
---
+++ orca-mine/src/main/groovy/com/netflix/spinnaker/orca/mine/tasks/RegisterAcaTaskTask.groovy
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2015 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.mine.tasks
+
+import com.netflix.spinnaker.orca.DefaultTaskResult
+import com.netflix.spinnaker.orca.ExecutionStatus
+import com.netflix.spinnaker.orca.Task
+import com.netflix.spinnaker.orca.TaskResult
+import com.netflix.spinnaker.orca.mine.MineService
+import com.netflix.spinnaker.orca.mine.pipeline.DeployCanaryStage
+import com.netflix.spinnaker.orca.pipeline.model.Stage
+import groovy.util.logging.Slf4j
+import org.apache.commons.lang.StringUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import retrofit.client.Response
+
+@Component
+@Slf4j
+class RegisterAcaTaskTask implements Task {
+
+  @Autowired
+  MineService mineService
+
+  @Override
+  TaskResult execute(Stage stage) {
+    String app = stage.context.application ?: stage.execution.application
+
+    Map c = buildCanary(app, stage)
+
+    log.info("Registering Canary (executionId: ${stage.execution.id}, canary: ${c})")
+
+    Response response = mineService.registerCanary(c)
+    String canaryId
+
+    if (response.status == 200 && response.body.mimeType().startsWith('text/plain')) {
+      canaryId = response.body.in().text
+    } else {
+      throw new IllegalStateException("Unable to handle $response")
+    }
+
+    def canary = mineService.getCanary(canaryId)
+    def outputs = [
+      canary: canary,
+      stageTimeoutMs: getMonitorTimeout(canary),
+    ]
+
+    return new DefaultTaskResult(ExecutionStatus.SUCCEEDED, outputs)
+  }
+
+  Map buildCanary(String app, Stage stage) {
+    Map c = stage.context.canary
+    c.application = c.application ?: app
+    c.canaryConfig.canaryHealthCheckHandler = c.canaryConfig.canaryHealthCheckHandler ?: [:]
+    c.canaryConfig.canaryHealthCheckHandler['@class'] = c.canaryConfig.canaryHealthCheckHandler['@class'] ?: 'com.netflix.spinnaker.mine.CanaryResultHealthCheckHandler'
+    c.canaryConfig.name = c.canaryConfig.name ?: stage.execution.id
+    c.canaryConfig.application = c.canaryConfig.application ?: c.application ?: app
+    return c
+  }
+
+  private static Long getMonitorTimeout(Map canary) {
+    String configuredTimeout = (canary.canaryConfig.lifetimeHours.toString() ?: "46")
+    int timeoutHours = StringUtils.isNumeric(configuredTimeout) ? Integer.parseInt(configuredTimeout) + 2: 48
+    return timeoutHours * 60 * 60 * 1000
+  }
+}
