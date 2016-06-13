53d0ab682062905df357ef975d86561d212d14f5
dpeach <dpeach@google.com>
2016-06-13 10:14:43 -0400

    * added support for global provider settings  * added useAmiBlockDeviceMappings as global config

--- app/scripts/modules/core/application/modal/applicationProviderFields.component.spec.js
+++ app/scripts/modules/core/application/modal/applicationProviderFields.component.spec.js
@@ -5,7 +5,9 @@ describe('Controller: ApplicationProviderFieldsCtrl', function () {
   let controller,
     scope,
     cloudProviderRegistry,
-    angular = require('angular');
+    settings,
+    angular = require('angular'),
+    _ = require('lodash');
 
   beforeEach(
     window.module(
@@ -15,11 +17,18 @@ describe('Controller: ApplicationProviderFieldsCtrl', function () {
 
   beforeEach(
     window.inject(function ($rootScope, $controller, _cloudProviderRegistry_) {
-      scope = $rootScope.$new();
-      cloudProviderRegistry = _cloudProviderRegistry_;
+      scope = $rootScope.$new(),
+      cloudProviderRegistry = _cloudProviderRegistry_,
+      settings = {
+        providers: {
+          aws: {
+            defaultPath: '/path/to/somewhere',
+          }
+        },
+      };
 
       let application = {
-        cloudProviders: []
+        cloudProviders: [],
       };
 
       let cloudProviders = [
@@ -33,6 +42,8 @@ describe('Controller: ApplicationProviderFieldsCtrl', function () {
       controller = $controller('ApplicationProviderFieldsCtrl', {
         $scope: scope,
         cloudProviderRegistry: cloudProviderRegistry,
+        settings,
+        _,
       }, {
         application,
         cloudProviders,
@@ -68,6 +79,43 @@ describe('Controller: ApplicationProviderFieldsCtrl', function () {
           expect(spy).toHaveBeenCalledWith('gce', 'applicationProviderFields.templateUrl');
           expect(spy).not.toHaveBeenCalledWith('aws', 'applicationProviderFields.templateUrl');
         });
+
+    });
+
+  });
+
+  describe('initializeApplicationFields', function() {
+
+    it('does not mutate the application if the setting has been already defined within the application', function() {
+
+      _.set(controller.application, 'providerSettings.aws.defaultPath', '/path/to/somewhere/else');
+      let applicationBeforeFunctionCall = _.cloneDeep(controller.application);
+
+      controller.initializeApplicationField('aws.defaultPath');
+
+      expect(_.isEqual(controller.application, applicationBeforeFunctionCall)).toEqual(true);
+      expect(_.get(controller.application, 'providerSettings.aws.defaultPath')).toEqual('/path/to/somewhere/else');
+
+    });
+
+    it('does not mutate the application if the path does not exist within the global provider settings', function() {
+
+      _.set(settings, 'providers.aws', undefined);
+      let applicationBeforeFunctionCall = _.cloneDeep(controller.application);
+
+      controller.initializeApplicationField('aws.defaultPath');
+
+      expect(_.isEqual(controller.application, applicationBeforeFunctionCall)).toEqual(true);
+
+    });
+
+    it(`mutates the application to match the global provider settings 
+      if the setting has not been defined within the application`,
+      function() {
+
+      controller.initializeApplicationField('aws.defaultPath');
+
+      expect(_.get(controller.application, 'providerSettings.aws.defaultPath')).toEqual('/path/to/somewhere');
 
     });
 
