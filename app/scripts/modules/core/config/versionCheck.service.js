5d36e31f61ba3ac45761004f355f9a0023d67389
Chris Berry <chrisb@netflix.com>
2016-06-17 17:57:50 -0700

    (core) wait until new version has been seen for three minutes before prompting user to refresh (#2343)

--- app/scripts/modules/core/config/versionCheck.service.js
+++ app/scripts/modules/core/config/versionCheck.service.js
@@ -10,23 +10,30 @@ module.exports = angular
   .factory('versionCheckService', function ($http, $timeout, notifierService, $log, $filter) {
 
     let currentVersion = require('../../../../../version.json');
+    let newVersionSeenCount = 0;
 
     $log.debug('Deck version', currentVersion.version, 'created', $filter('timestamp')(currentVersion.created));
 
     let checkVersion = () => {
       let url = '/version.json?_=' + new Date().getTime();
-      $timeout(() => $http.get(url).then(versionRetrieved, checkVersion), 15000);
+      $timeout(() => $http.get(url).then(versionRetrieved, checkVersion), 30000);
     };
 
     let versionRetrieved = (response) => {
       let data = response.data;
       if (data.version === currentVersion.version) {
+        newVersionSeenCount = 0;
         checkVersion();
       } else {
-        $log.debug('New Deck version:', data.version, 'created', $filter('timestamp')(data.created));
-        notifierService.publish(
-          `A new version of Spinnaker is available
-            <a role="button" class="action" onclick="document.location.reload(true)">Refresh</a>`);
+        newVersionSeenCount++;
+        if (newVersionSeenCount < 6) {
+          checkVersion();
+        } else {
+          $log.debug('New Deck version:', data.version, 'created', $filter('timestamp')(data.created));
+          notifierService.publish(
+            `A new version of Spinnaker is available
+              <a role="button" class="action" onclick="document.location.reload(true)">Refresh</a>`);
+        }
       }
     };
 
