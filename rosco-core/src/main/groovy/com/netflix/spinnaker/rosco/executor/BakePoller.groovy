df8d619bb0cc11a82a4616d341dce318c49a3a65
Matt Duftler <duftler@google.com>
2016-06-22 13:46:20 -0400

    Add comment. (#110)

--- rosco-core/src/main/groovy/com/netflix/spinnaker/rosco/executor/BakePoller.groovy
+++ rosco-core/src/main/groovy/com/netflix/spinnaker/rosco/executor/BakePoller.groovy
@@ -116,6 +116,8 @@ class BakePoller implements ApplicationListener<ContextRefreshedEvent> {
                           long lastUpdatedTimeSeconds = TimeUnit.MILLISECONDS.toSeconds(bakeStatus.updatedTimestamp)
                           long eTimeMinutes = TimeUnit.SECONDS.toMinutes(currentTimeSeconds - lastUpdatedTimeSeconds)
 
+                          // This can only be true if the rosco instance that owns this bake has not been updating the status.
+                          // Note that this only applies to bakes owned by _other_ rosco instances, not _this_ one.
                           if (eTimeMinutes >= orphanedJobTimeoutMinutes) {
                             log.info("The staleness of bake $statusId ($eTimeMinutes minutes) has met or exceeded the " +
                                      "value of orphanedJobTimeoutMinutes ($orphanedJobTimeoutMinutes minutes).")
