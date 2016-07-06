e909bf29ab4951614f1ae67387d7f0ccb8e95aef
tomaslin <clin@netflix.com>
2016-07-06 12:03:07 -0700

    continueOnUnhealthy outside of canaryConfig so it doesn't get swallowed by mine

--- orca-mine/src/test/groovy/com/netflix/spinnaker/orca/mine/tasks/CompleteCanaryTaskSpec.groovy
+++ orca-mine/src/test/groovy/com/netflix/spinnaker/orca/mine/tasks/CompleteCanaryTaskSpec.groovy
@@ -17,32 +17,9 @@
 package com.netflix.spinnaker.orca.mine.tasks
 
 import com.netflix.spinnaker.orca.ExecutionStatus
-import com.netflix.spinnaker.orca.clouddriver.model.TaskId
 import com.netflix.spinnaker.orca.pipeline.model.Pipeline
 import com.netflix.spinnaker.orca.pipeline.model.PipelineStage
 import spock.lang.Specification
-
-import static com.netflix.spinnaker.orca.mine.tasks.CleanupCanaryTask.Action.DISABLE
-import static com.netflix.spinnaker.orca.mine.tasks.CleanupCanaryTask.Action.DISABLE
-import static com.netflix.spinnaker.orca.mine.tasks.CleanupCanaryTask.Action.TERMINATE
-import static com.netflix.spinnaker.orca.mine.tasks.CleanupCanaryTask.Health.UNHEALTHY
-
-
-/*
- * Copyright 2016 Netflix, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 class CompleteCanaryTaskSpec extends Specification {
 
@@ -89,9 +66,7 @@ class CompleteCanaryTaskSpec extends Specification {
         health: [
             health: "UNHEALTHY"
         ],
-        canaryConfig: [
-          continueOnUnhealthy: true
-        ]
+        continueOnUnhealthy: true
       ]
     ])
 
@@ -109,9 +84,7 @@ class CompleteCanaryTaskSpec extends Specification {
         health: [
           health: "UNHEALTHY"
         ],
-        canaryConfig: [
-          continueOnUnhealthy: false
-        ]
+        continueOnUnhealthy: false
       ]
     ])
 
