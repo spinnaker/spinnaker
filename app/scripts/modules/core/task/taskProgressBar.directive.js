d9619ec52d068acd7243338c1dcd3183899a800d
Chris Berry <chrisb@netflix.com>
2016-07-11 08:59:30 -0700

    (core) guard against NPE when creating tooltip for task progress bar (#2430)

--- app/scripts/modules/core/task/taskProgressBar.directive.js
+++ app/scripts/modules/core/task/taskProgressBar.directive.js
@@ -20,24 +20,21 @@ module.exports = angular.module('spinnaker.core.task.progressBar.directive', [])
         scope.progressStyle = { width: stepsComplete.length / task.steps.length * 100 + '%' };
 
         if (task.isRunning) {
-          var currentStep = task.steps.filter(function(step) {
-            return step.hasNotStarted || step.isRunning;
-          });
-
+          let [currentStep] = task.steps.filter(step => step.hasNotStarted || step.isRunning);
           var currentStepIndex = task.steps.indexOf(currentStep[0]) + 1;
 
-          scope.tooltip = $sce.trustAsHtml('Step ' + currentStepIndex + ' of ' + task.steps.length + ': ' + $filter('robotToHuman')(currentStep[0].name));
+          if (currentStep) {
+            scope.tooltip = $sce.trustAsHtml('Step ' + currentStepIndex + ' of ' + task.steps.length + ': ' + $filter('robotToHuman')(currentStep.name));
+          }
         }
 
         if (task.isFailed) {
-          var failedStep = task.steps.filter(function(step) {
-            return step.isFailed || step.isSuspended;
-          });
+          var [failedStep] = task.steps.filter(step => step.isFailed || step.isSuspended);
 
-          if (failedStep.length && task.failureMessage) {
-            var failedStepIndex = task.steps.indexOf(failedStep[0]) + 1;
+          if (failedStep && task.failureMessage) {
+            var failedStepIndex = task.steps.indexOf(failedStep) + 1;
             var clipped = task.failureMessage.length > 400 ? task.failureMessage.substring(0, 400) + '&hellip;' : task.failureMessage;
-            scope.tooltip = $sce.trustAsHtml('Failed on Step ' + failedStepIndex + ' of ' + task.steps.length + ':<br>' + $filter('robotToHuman')(failedStep[0].name) +
+            scope.tooltip = $sce.trustAsHtml('Failed on Step ' + failedStepIndex + ' of ' + task.steps.length + ':<br>' + $filter('robotToHuman')(failedStep.name) +
               '<br><br><strong>Exception:</strong><p>' + clipped + '</p>');
           } else {
             scope.tooltip = $sce.trustAsHtml('Task failed; sorry, no reason provided');
