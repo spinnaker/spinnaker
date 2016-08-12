5e8adc0ca0701a586dabc8b544c3e2c6e3365b37
Chris Berry <chrisb@netflix.com>
2016-08-12 11:37:58 -0700

    include disableAsg controller (#2563)

--- app/scripts/modules/core/pipeline/config/stages/disableAsg/disableAsgStage.js
+++ app/scripts/modules/core/pipeline/config/stages/disableAsg/disableAsgStage.js
@@ -5,7 +5,8 @@ let angular = require('angular');
 module.exports = angular.module('spinnaker.core.pipeline.stage.disableAsgStage', [
   require('../../../../utils/lodash.js'),
   require('../stageConstants.js'),
-  require('../../pipelineConfigProvider.js')
+  require('../../pipelineConfigProvider.js'),
+  require('./templates/disableAsgExecutionDetails.controller'),
 ])
   .config(function(pipelineConfigProvider) {
     pipelineConfigProvider.registerStage({
