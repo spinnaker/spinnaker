2714eac9a1adc9b6f4305ec769fa9882e08daadf
Matt Duftler <duftler@google.com>
2016-08-19 12:50:12 -0400

    core/bake: Support dynamic properties on BakeRequest so orca doesn't have to be modified when newly-added properties need to be propagated to Rosco. (#944)

--- orca-bakery/src/main/groovy/com/netflix/spinnaker/orca/bakery/api/BakeRequest.groovy
+++ orca-bakery/src/main/groovy/com/netflix/spinnaker/orca/bakery/api/BakeRequest.groovy
@@ -16,19 +16,28 @@
 
 package com.netflix.spinnaker.orca.bakery.api
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter
+import com.fasterxml.jackson.annotation.JsonAnySetter
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import groovy.transform.CompileStatic
 import groovy.transform.Immutable
 
 /**
- * A request to bake a new AMI.
+ * A request to bake a new image.
  *
  * @see BakeryService#createBake
  */
 @Immutable(copyWith = true)
 @CompileStatic
 class BakeRequest {
-  static final Default = new BakeRequest(System.getProperty("user.name"), null, null, null, null, null, null, CloudProviderType.aws, Label.release, "ubuntu", null, null, null, null, null, null, null, null, null, null)
+  private static final PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy lowerCaseWithUnderscoresStrategy =
+    new PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy()
+
+  static final Default = new BakeRequest(user: System.getProperty("user.name"),
+                                         cloudProviderType: CloudProviderType.aws,
+                                         baseLabel: Label.release,
+                                         baseOs: "ubuntu")
 
   String user
   @JsonProperty("package") String packageName
@@ -48,9 +57,17 @@ class BakeRequest {
   String amiName
   String amiSuffix
 
-  String templateFileName
-  Map extendedAttributes
-  String instanceType
+  private Map<String, Object> other = new HashMap<String, Object>()
+
+  @JsonAnyGetter
+  public Map<String, Object> other() {
+    return other
+  }
+
+  @JsonAnySetter
+  public void set(String name, Object value) {
+    other.put(lowerCaseWithUnderscoresStrategy.translate(name), value)
+  }
 
   static enum CloudProviderType {
     aws, azure, docker, gce, openstack
