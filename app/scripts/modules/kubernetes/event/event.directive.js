fcb59e691690c515886c7b01d29b631ddccf84fe
Lars Wander <lwander@users.noreply.github.com>
2016-08-30 16:00:26 -0400

    (kubernetes): Render kubenretes events (#2641)

---
+++ app/scripts/modules/kubernetes/event/event.directive.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.kubernetes.event.event.directive', [])
+  .directive('kubernetesEvent', function () {
+    return {
+      restrict: 'E',
+      templateUrl: require('./event.directive.html'),
+      scope: {
+        event: '=',
+      }
+    };
+  }).controller('kubernetesEventController', function ($scope, $uibModal) {
+    if ($scope.event.message) {
+      this.displayMessage = $scope.event.message.substring(0, 25);
+    }
+
+    this.type = $scope.event.type;
+    this.count = $scope.event.count;
+    this.first = $scope.event.firstOccurrence;
+    this.last = $scope.event.lastOccurrence;
+    this.reason = $scope.event.reason;
+
+    this.showMessage = function showMessage() {
+      $scope.userDataModalTitle = 'Message';
+      $scope.userData = $scope.event.message;
+      $uibModal.open({
+        templateUrl: require('../../core/serverGroup/details/userData.html'),
+        controller: 'CloseableModalCtrl',
+        scope: $scope
+      });
+    };
+  });
--- app/scripts/modules/kubernetes/kubernetes.module.js
+++ app/scripts/modules/kubernetes/kubernetes.module.js
@@ -22,6 +22,7 @@ module.exports = angular.module('spinnaker.kubernetes', [
   require('./cluster/cluster.kubernetes.module.js'),
   require('./container/configurer.directive.js'),
   require('./container/probe.directive.js'),
+  require('./event/event.directive.js'),
   require('./instance/details/details.kubernetes.module.js'),
   require('./loadBalancer/configure/configure.kubernetes.module.js'),
   require('./loadBalancer/details/details.kubernetes.module.js'),
