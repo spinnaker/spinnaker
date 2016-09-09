7ce6675ae020c36d12e5dc7a371e078f9561d89d
Zan Thrash <zanthrash@gmail.com>
2016-09-09 12:40:52 -0700

    SpEL / ui-select toggle widget (#2672)
    This was created for SPIN-1956, which allows the Pipeline Parameters in a Pipeline stage to use and show SpEL for paramgers that have mulple options.
    
    This simply a component <spel-select></spel-select> tha can wrap a ui-select or regular select field.

--- app/scripts/modules/core/pipeline/config/stages/pipeline/pipelineStage.module.js
+++ app/scripts/modules/core/pipeline/config/stages/pipeline/pipelineStage.module.js
@@ -12,4 +12,5 @@ module.exports = angular.module('spinnaker.core.pipeline.stage.pipeline', [
   require('../../services/pipelineConfigService.js'),
   require('./pipelineExecutionDetails.controller.js'),
   require('../../../../application/service/applications.read.service.js'),
+  require('../../../../../core/widgets/spelText/spelSelect.component'),
 ]);
---
+++ app/scripts/modules/core/widgets/spelText/spelSelect.component.js
@@ -0,0 +1,86 @@
+'use strict';
+
+let angular = require('angular');
+
+module.exports = angular
+  .module('spinnaker.widgets.numberInput.component', [])
+  .component('spelSelect', {
+    template:`
+      <div class="navbar-form" style="padding: 0 ;">
+        <div class="button-input" ng-class="{select: $ctrl.selectActive, text: $ctrl.textActive, focus: $ctrl.isGlowing}">
+          <span class="btn-group btn-group-xs" role="group">
+            <button type="button"
+              class="btn btn-default"
+              ng-click="$ctrl.toggleSelect()"
+              ng-class="{active: $ctrl.selectActive}"
+              ng-focus="$ctrl.glow(true)"
+              ng-blur="$ctrl.glow(false)"
+              uib-tooltip="Toggle for select">
+              <span class="glyphicon glyphicon-triangle-bottom" style="font-size: 75%"></span>
+              </button>
+            <button type="button"
+              class="btn btn-default"
+              ng-click="$ctrl.toggleText()"
+              ng-class="{active: $ctrl.expressionActive}"
+              ng-focus="$ctrl.glow(true)"
+              ng-blur="$ctrl.glow(false)"
+              uib-tooltip="Toggle to enter expression">
+              $\{…\}
+              </button>
+          </span>
+          <input
+            ng-if="$ctrl.expressionActive"
+            type="text" class="form-control borderless"
+            ng-model="$ctrl.model" ng-focus="$ctrl.glow(true)"
+            ng-blur="$ctrl.glow(false)"
+          />
+          <div
+            ng-transclude
+            ng-if="$ctrl.selectActive"
+            class="borderless input"
+            ng-focus="$ctrl.glow(true)"
+            ng-blur="$ctrl.glow(false)"
+            >
+          </div>
+
+        </div>
+      </div>
+      `,
+    bindings: {
+      model: '=',
+    },
+    transclude: true,
+    controller: function() {
+      let ctrl = this;
+      let select = 'select';
+      let textType = 'text';
+
+      let setState = (inputType) => {
+        if(inputType == textType) {
+          ctrl.expressionActive = true;
+          ctrl.selectActive = false;
+        } else {
+          ctrl.selectActive = true;
+          ctrl.expressionActive = false;
+        }
+      };
+
+      ctrl.$onInit = () => {
+        console.log('model:', ctrl.model);
+        ctrl.inputType = ctrl.model && ctrl.model.indexOf('${') > -1 ? textType : select;
+        setState(ctrl.inputType);
+      };
+
+      ctrl.toggleSelect = () => {
+        setState(select);
+      };
+
+      ctrl.toggleText = () => {
+        setState(textType);
+      };
+
+      ctrl.glow = (isGlowing) => {
+        ctrl.isGlowing = isGlowing;
+      };
+    }
+  });
