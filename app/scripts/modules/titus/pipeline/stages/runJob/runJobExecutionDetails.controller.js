c44b0817e35f1e0b3c341180234b0e294e3f75f8
Chris Berry <chrisb@netflix.com>
2016-09-14 16:41:11 -0700

    (titus) guard against missing fields on failed job run (#2697)

--- app/scripts/modules/titus/pipeline/stages/runJob/runJobExecutionDetails.controller.js
+++ app/scripts/modules/titus/pipeline/stages/runJob/runJobExecutionDetails.controller.js
@@ -4,10 +4,11 @@ let angular = require('angular');
 
 module.exports = angular.module('spinnaker.core.pipeline.stage.runJob.titus.executionDetails.controller', [
   require('angular-ui-router'),
+  require('../../../../core/utils/lodash'),
   require('../../../../core/delivery/details/executionDetailsSection.service.js'),
   require('../../../../core/delivery/details/executionDetailsSectionNav.directive.js'),
 ])
-  .controller('titusRunJobExecutionDetailsCtrl', function ($scope, $stateParams, executionDetailsSectionService, accountService) {
+  .controller('titusRunJobExecutionDetailsCtrl', function ($scope, $stateParams, executionDetailsSectionService, accountService, _) {
 
     $scope.configSections = ['runJobConfig', 'taskStatus'];
 
@@ -19,12 +20,14 @@ module.exports = angular.module('spinnaker.core.pipeline.stage.runJob.titus.exec
     initialize();
     $scope.$on('$stateChangeSuccess', initialize, true);
 
-    $scope.job = $scope.stage.context['deploy.jobs'][$scope.stage.context.cluster.region][0];
+    let [job] = _.get($scope.stage.context['deploy.jobs'], $scope.stage.context.cluster.region, []);
+
+    $scope.job = job;
 
     accountService.getAccountDetails($scope.stage.context.credentials).then((details) => {
       $scope.apiEndpoint = _.where(details.regions, {name: $scope.stage.context.cluster.region})[0].endpoint;
       $scope.titusUiEndpoint = $scope.apiEndpoint.replace('titusapi', 'titus-ui').replace('http', 'https').replace('7101', '7001');
-      if ($scope.stage.context.jobStatus.completionDetails) {
+      if (_.has($scope.stage.context, 'jobStatus.completionDetails')) {
         $scope.task = $scope.stage.context.jobStatus.completionDetails.taskId;
       }
     });
