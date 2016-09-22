06fc3b7584d50dbc44a50d03e1766fb67e24638c
Chris Berry <chrisb@netflix.com>
2016-09-22 10:16:27 -0700

    (core) show instance counts in load balancers menu (#2733)

--- app/scripts/modules/core/loadBalancer/loadBalancersTag.directive.js
+++ app/scripts/modules/core/loadBalancer/loadBalancersTag.directive.js
@@ -9,11 +9,20 @@ module.exports = angular.module('spinnaker.core.loadBalancer.tag.directive', [])
       replace: false,
       templateUrl: require('./loadBalancer/loadBalancersTag.html'),
       scope: {
+        application: '=',
         serverGroup: '=',
         maxDisplay: '='
       },
       link: function(scope) {
         scope.popover = { show: false };
+        scope.application.loadBalancers.ready().then(() => {
+          scope.loadBalancers = scope.serverGroup.loadBalancers.map(lbName => {
+            let serverGroup = scope.serverGroup;
+            let [match] = scope.application.loadBalancers.data
+              .filter(lb => lb.name === lbName && lb.account === serverGroup.account && lb.region === serverGroup.region);
+            return match;
+          });
+        });
       }
     };
   }
