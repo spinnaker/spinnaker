a61757af17d70020faacf6339c257b88a165901a
jeyrs <jeyrschabu@gmail.com>
2016-09-26 16:10:24 -0700

    Refactor Project;s API
    - Converted Hal returns to POJOs for consistency

--- front50-web/src/main/groovy/com/netflix/spinnaker/front50/controllers/v2/ProjectsController.groovy
+++ front50-web/src/main/groovy/com/netflix/spinnaker/front50/controllers/v2/ProjectsController.groovy
@@ -21,15 +21,12 @@ import com.netflix.spectator.api.Registry
 import com.netflix.spinnaker.front50.exception.NotFoundException
 import com.netflix.spinnaker.front50.model.project.Project
 import com.netflix.spinnaker.front50.model.project.ProjectDAO
-import com.netflix.spinnaker.front50.resources.project.ProjectResource
-import com.netflix.spinnaker.front50.resources.project.ProjectResourceAssembler
 import groovy.transform.InheritConstructors
 import groovy.util.logging.Slf4j
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.MessageSource
-import org.springframework.hateoas.Resources
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
@@ -39,7 +36,6 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*
 
 import javax.servlet.http.HttpServletResponse
 
@@ -57,47 +53,41 @@ public class ProjectsController {
   @Autowired
   Registry registry
 
-  private final ProjectResourceAssembler assembler = new ProjectResourceAssembler()
-
   @RequestMapping(value = "/search", method = RequestMethod.GET)
   @ApiOperation(value = "", notes = """Search for projects given one or more attributes.
 
 - /search?q=ProjectName
 - /search?q=ApplicationName
 """)
-  Resources<ProjectResource> search(@RequestParam("q") String query) {
+  Set<Project> search(@RequestParam("q") String query) {
     def projects = projectDAO.all().findAll { Project project ->
       project.name.toLowerCase().contains(query.toLowerCase()) || project.config.applications.find {
         it.toLowerCase().contains(query.toLowerCase())
       }
     }
 
-    def resources = assembler.toResources(projects)
-    def link = linkTo(ProjectsController).slash("/search?q=${query}").withSelfRel()
-    return new Resources<ProjectResource>(resources, link)
+    return projects
   }
 
   @ApiOperation(value = "", notes = "Fetch all projects")
   @RequestMapping(method = RequestMethod.GET)
-  Resources<ProjectResource> projects() {
-    def resources = assembler.toResources(projectDAO.all())
-    def link = linkTo(ProjectsController).slash('/').withSelfRel()
-    return new Resources<ProjectResource>(resources, link)
+  Set<Project> projects() {
+    return  projectDAO.all()
   }
 
   @ApiOperation(value = "", notes = "Fetch a single project")
   @RequestMapping(method = RequestMethod.GET, value = "/{projectId}")
-  ProjectResource project(@PathVariable String projectId) {
+  Project project(@PathVariable String projectId) {
     try {
-      return assembler.toResource(projectDAO.findByName(projectId))
+      return projectDAO.findByName(projectId)
     } catch (NotFoundException e) {
-      return assembler.toResource(projectDAO.findById(projectId))
+      return projectDAO.findById(projectId)
     }
   }
 
   @ApiOperation(value = "", notes = "Update an existing project")
   @RequestMapping(method = RequestMethod.PUT, value = "/{projectId}")
-  ProjectResource put(@PathVariable final String projectId, @RequestBody final Project project) {
+  Project put(@PathVariable final String projectId, @RequestBody final Project project) {
     def existingProject = projectDAO.findById(projectId)
 
     project.id = existingProject.id
@@ -112,12 +102,12 @@ public class ProjectsController {
     } catch (NotFoundException ignored) {}
 
     projectDAO.update(projectId, project)
-    return assembler.toResource(project)
+    return project
   }
 
   @ApiOperation(value = "", notes = "Create a project")
   @RequestMapping(method = RequestMethod.POST)
-  ProjectResource create(@RequestBody final Project project) {
+  Project create(@RequestBody final Project project) {
     project.createTs = System.currentTimeMillis()
     project.updateTs = System.currentTimeMillis()
 
@@ -126,7 +116,7 @@ public class ProjectsController {
       throw new ProjectAlreadyExistsException()
     } catch (NotFoundException ignored) {}
 
-    return assembler.toResource(projectDAO.create(project.id, project))
+    return projectDAO.create(project.id, project)
   }
 
   @ApiOperation(value = "", notes = "Delete a project")
