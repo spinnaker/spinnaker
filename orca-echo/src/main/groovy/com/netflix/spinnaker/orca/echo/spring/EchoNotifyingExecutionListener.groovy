d55d7365b1b7cbbdba257d1f395cf7542fae9103
Rob Fletcher <rfletcher@netflix.com>
2016-11-01 15:00:31 -0700

    stop sending events for tasks

--- orca-echo/src/main/groovy/com/netflix/spinnaker/orca/echo/spring/EchoNotifyingExecutionListener.groovy
+++ orca-echo/src/main/groovy/com/netflix/spinnaker/orca/echo/spring/EchoNotifyingExecutionListener.groovy
@@ -7,6 +7,7 @@ import com.netflix.spinnaker.orca.echo.EchoService
 import com.netflix.spinnaker.orca.listeners.ExecutionListener
 import com.netflix.spinnaker.orca.listeners.Persister
 import com.netflix.spinnaker.orca.pipeline.model.Execution
+import com.netflix.spinnaker.orca.pipeline.model.Pipeline
 
 @Slf4j
 @CompileStatic
@@ -20,22 +21,24 @@ class EchoNotifyingExecutionListener implements ExecutionListener {
 
   @Override
   void beforeExecution(Persister persister, Execution execution) {
-    try {
-      if (execution.status != ExecutionStatus.SUSPENDED) {
-        echoService.recordEvent(
-          details: [
-            source     : "orca",
-            type       : "orca:pipeline:starting",
-            application: execution.application,
-          ],
-          content: [
-            execution  : execution,
-            executionId: execution.id
-          ]
-        )
+    if (execution instanceof Pipeline) {
+      try {
+        if (execution.status != ExecutionStatus.SUSPENDED) {
+          echoService.recordEvent(
+            details: [
+              source     : "orca",
+              type       : "orca:pipeline:starting",
+              application: execution.application,
+            ],
+            content: [
+              execution  : execution,
+              executionId: execution.id
+            ]
+          )
+        }
+      } catch (Exception e) {
+        log.error("Failed to send pipeline start event: ${execution?.id}")
       }
-    } catch (Exception e) {
-      log.error("Failed to send pipeline start event: ${execution?.id}")
     }
   }
 
@@ -44,22 +47,24 @@ class EchoNotifyingExecutionListener implements ExecutionListener {
                       Execution execution,
                       ExecutionStatus executionStatus,
                       boolean wasSuccessful) {
-    try {
-      if (execution.status != ExecutionStatus.SUSPENDED) {
-        echoService.recordEvent(
-          details: [
-            source     : "orca",
-            type       : "orca:pipeline:${wasSuccessful ? "complete" : "failed"}".toString(),
-            application: execution.application,
-          ],
-          content: [
-            execution  : execution,
-            executionId: execution.id
-          ]
-        )
+    if (execution instanceof Pipeline) {
+      try {
+        if (execution.status != ExecutionStatus.SUSPENDED) {
+          echoService.recordEvent(
+            details: [
+              source     : "orca",
+              type       : "orca:pipeline:${wasSuccessful ? "complete" : "failed"}".toString(),
+              application: execution.application,
+            ],
+            content: [
+              execution  : execution,
+              executionId: execution.id
+            ]
+          )
+        }
+      } catch (Exception e) {
+        log.error("Failed to send pipeline end event: ${execution?.id}")
       }
-    } catch (Exception e) {
-      log.error("Failed to send pipeline end event: ${execution?.id}")
     }
   }
 }
