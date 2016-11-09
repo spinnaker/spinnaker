2b7251f069bf848815353e1f7b1279815c1d65a5
tomaslin <clin@netflix.com>
2016-11-09 15:38:49 -0800

    (titus) - enables the following stages for spinnaker pipelines:  * enable Asg  * disable Asg  * resize Asg  * destroy Asg  * disable cluster  * clone servergroup  * shrink cluster

---
+++ app/scripts/modules/titus/pipeline/stages/cloneServerGroup/titusCloneServerGroupStage.js
@@ -0,0 +1,89 @@
+'use strict';
+
+import _ from 'lodash';
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.core.pipeline.stage.titus.cloneServerGroupStage', [
+  require('core/application/modal/platformHealthOverride.directive.js'),
+  require('core/account/account.service.js'),
+  require('core/naming/naming.service.js'),
+  require('./cloneServerGroupExecutionDetails.controller.js'),
+])
+  .config(function(pipelineConfigProvider) {
+    pipelineConfigProvider.registerStage({
+      provides: 'cloneServerGroup',
+      cloudProvider: 'titus',
+      templateUrl: require('./cloneServerGroupStage.html'),
+      executionDetailsUrl: require('./cloneServerGroupExecutionDetails.html'),
+      executionStepLabelUrl: require('./cloneServerGroupStepLabel.html'),
+      validators: [
+        { type: 'requiredField', fieldName: 'targetCluster', fieldLabel: 'cluster' },
+        { type: 'requiredField', fieldName: 'target' },
+        { type: 'requiredField', fieldName: 'region', },
+        { type: 'requiredField', fieldName: 'credentials', fieldLabel: 'account'}
+      ],
+    });
+  }).controller('titusCloneServerGroupStageCtrl', function($scope, accountService, namingService, stageConstants) {
+
+    let stage = $scope.stage;
+
+    $scope.viewState = {
+      accountsLoaded: false,
+    };
+
+    accountService.listAccounts('titus').then((accounts) => {
+      $scope.accounts = accounts;
+      $scope.viewState.accountsLoaded = true;
+    });
+
+    this.cloneTargets = stageConstants.targetList;
+    stage.target = stage.target || this.cloneTargets[0].val;
+    stage.application = $scope.application.name;
+    stage.cloudProvider = 'titus';
+    stage.cloudProviderType = 'titus';
+
+    if (stage.isNew && $scope.application.attributes.platformHealthOnlyShowOverride && $scope.application.attributes.platformHealthOnly) {
+      stage.interestingHealthProviderNames = ['Titus'];
+    }
+
+    if (!stage.credentials && $scope.application.defaultCredentials.titus) {
+      stage.credentials = $scope.application.defaultCredentials.titus;
+    }
+
+    this.targetClusterUpdated = () => {
+      if (stage.targetCluster) {
+        let clusterName = namingService.parseServerGroupName(stage.targetCluster);
+        stage.stack = clusterName.stack;
+        stage.freeFormDetails = clusterName.freeFormDetails;
+      } else {
+        stage.stack = '';
+        stage.freeFormDetails = '';
+      }
+    };
+
+    $scope.$watch('stage.targetCluster', this.targetClusterUpdated);
+
+    this.removeCapacity = () => {
+      delete stage.capacity;
+    };
+
+    if (!_.has(stage, 'useSourceCapacity')) {
+      stage.useSourceCapacity = true;
+    }
+
+    this.toggleSuspendedProcess = (process) => {
+      stage.suspendedProcesses = stage.suspendedProcesses || [];
+      var processIndex = stage.suspendedProcesses.indexOf(process);
+      if (processIndex === -1) {
+        stage.suspendedProcesses.push(process);
+      } else {
+        stage.suspendedProcesses.splice(processIndex, 1);
+      }
+    };
+
+    this.processIsSuspended = (process) => {
+      return stage.suspendedProcesses && stage.suspendedProcesses.includes(process);
+    };
+  });
+
---
+++ app/scripts/modules/titus/pipeline/stages/destroyAsg/titusDestroyAsgStage.js
@@ -0,0 +1,60 @@
+'use strict';
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.core.pipeline.stage.titus.destroyAsgStage', [
+  require('core/pipeline/config/stages/stageConstants.js'),
+])
+  .config(function(pipelineConfigProvider) {
+    pipelineConfigProvider.registerStage({
+      provides: 'destroyServerGroup',
+      alias: 'destroyAsg',
+      cloudProvider: 'titus',
+      templateUrl: require('./destroyAsgStage.html'),
+      executionDetailsUrl: require('core/pipeline/config/stages/destroyAsg/templates/destroyAsgExecutionDetails.template.html'),
+      executionStepLabelUrl: require('./destroyAsgStepLabel.html'),
+      validators: [
+        {
+          type: 'targetImpedance',
+          message: 'This pipeline will attempt to destroy a server group without deploying a new version into the same cluster.'
+        },
+        { type: 'requiredField', fieldName: 'cluster' },
+        { type: 'requiredField', fieldName: 'target', },
+        { type: 'requiredField', fieldName: 'regions', },
+        { type: 'requiredField', fieldName: 'credentials', fieldLabel: 'account'},
+      ],
+    });
+  }).controller('titusDestroyAsgStageCtrl', function($scope, accountService, stageConstants) {
+
+    let stage = $scope.stage;
+
+    $scope.state = {
+      accounts: false,
+      regionsLoaded: false
+    };
+
+    accountService.listAccounts('titus').then(function (accounts) {
+      $scope.accounts = accounts;
+      $scope.state.accounts = true;
+    });
+
+    $scope.regions = ['us-east-1', 'us-west-1', 'eu-west-1', 'us-west-2'];
+
+    $scope.targets = stageConstants.targetList;
+
+    stage.regions = stage.regions || [];
+    stage.cloudProvider = 'titus';
+
+    if (!stage.credentials && $scope.application.defaultCredentials.titus) {
+      stage.credentials = $scope.application.defaultCredentials.titus;
+    }
+    if (!stage.regions.length && $scope.application.defaultRegions.titus) {
+      stage.regions.push($scope.application.defaultRegions.titus);
+    }
+
+    if (!stage.target) {
+      stage.target = $scope.targets[0].val;
+    }
+
+  });
+
