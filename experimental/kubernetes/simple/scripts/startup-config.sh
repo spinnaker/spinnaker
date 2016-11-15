5aff0d45cdcafb8b4bf562f64519b366e1cef4d5
Ricky Pai <ricky.pai@airbnb.com>
2016-11-14 23:33:43 -0800

    just generate secret from whole directory since all config files are needed.

--- experimental/kubernetes/simple/scripts/startup-config.sh
+++ experimental/kubernetes/simple/scripts/startup-config.sh
@@ -2,21 +2,7 @@ cp ../../../config/*.yml config
 rm config/default-spinnaker-local.yml
 
 kubectl create secret generic spinnaker-config \
-    --from-file=./config/clouddriver.yml \
-    --from-file=./config/clouddriver-local.yml \
-    --from-file=./config/echo.yml \
-    --from-file=./config/echo-local.yml \
-    --from-file=./config/front50.yml \
-    --from-file=./config/front50-local.yml \
-    --from-file=./config/gate.yml \
-    --from-file=./config/gate-local.yml \
-    --from-file=./config/igor.yml \
-    --from-file=./config/igor-local.yml \
-    --from-file=./config/orca.yml \
-    --from-file=./config/orca-local.yml \
-    --from-file=./config/settings.js \
-    --from-file=./config/spinnaker.yml \
-    --from-file=./config/spinnaker-local.yml \
+    --from-file=./config/. \
     --namespace=spinnaker
 
 GENERIC_CREDS="--from-file=$HOME/.kube/config"
