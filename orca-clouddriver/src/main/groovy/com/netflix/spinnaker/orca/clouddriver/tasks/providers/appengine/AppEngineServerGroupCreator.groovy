66520ad09cbf39535cd5814f29e9c4ae65c513c3
Daniel Peach <dpeach@google.com>
2016-11-22 16:33:26 -0500

    provider/appengine server group creator

---
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/providers/appengine/AppEngineServerGroupCreator.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.clouddriver.tasks.providers.appengine
+
+import com.netflix.spinnaker.orca.clouddriver.tasks.servergroup.ServerGroupCreator
+import com.netflix.spinnaker.orca.pipeline.model.Stage
+import groovy.util.logging.Slf4j
+import org.springframework.stereotype.Component
+
+@Slf4j
+@Component
+class AppEngineServerGroupCreator implements ServerGroupCreator {
+  boolean katoResultExpected = false
+  String cloudProvider = 'appengine'
+
+  @Override
+  List<Map> getOperations(Stage stage) {
+    def operation = [:]
+
+    // If this stage was synthesized by a parallel deploy stage, the operation properties will be under 'cluster'.
+    if (stage.context.containsKey("cluster")) {
+      operation.putAll(stage.context.cluster as Map)
+    } else {
+      operation.putAll(stage.context)
+    }
+
+    return [[(OPERATION): operation]]
+  }
+
+  @Override
+  Optional<String> getHealthProviderName() {
+    return Optional.empty()
+  }
+}
