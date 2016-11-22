b93fa8fd4047dbe49bf3b377f6d39243cae43e8b
Travis Tomsu <ttomsu@google.com>
2016-11-22 10:33:26 -0500

    Merged Azure LB (AppGateway) Controller/Provider

--- clouddriver-azure/src/main/groovy/com/netflix/spinnaker/clouddriver/azure/resources/appgateway/view/AzureAppGatewayProvider.groovy
+++ clouddriver-azure/src/main/groovy/com/netflix/spinnaker/clouddriver/azure/resources/appgateway/view/AzureAppGatewayProvider.groovy
@@ -16,25 +16,34 @@
 
 package com.netflix.spinnaker.clouddriver.azure.resources.appgateway.view
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.netflix.spinnaker.cats.cache.Cache
 import com.netflix.spinnaker.cats.cache.CacheData
 import com.netflix.spinnaker.cats.cache.RelationshipCacheFilter
 import com.netflix.spinnaker.clouddriver.azure.AzureCloudProvider
+import com.netflix.spinnaker.clouddriver.azure.common.AzureUtilities
 import com.netflix.spinnaker.clouddriver.azure.resources.appgateway.model.AzureAppGatewayDescription
 import com.netflix.spinnaker.clouddriver.azure.resources.common.cache.Keys
 import com.netflix.spinnaker.clouddriver.azure.resources.loadbalancer.model.AzureLoadBalancer
 import com.netflix.spinnaker.clouddriver.model.LoadBalancerProvider
+import com.netflix.spinnaker.clouddriver.model.LoadBalancerProviderTempShim
 import com.netflix.spinnaker.clouddriver.model.LoadBalancerServerGroup
+import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class AzureAppGatewayProvider implements LoadBalancerProvider<AzureLoadBalancer> {
+class AzureAppGatewayProvider implements LoadBalancerProvider<AzureLoadBalancer>, LoadBalancerProviderTempShim {
+
+  final String cloudProvider = AzureCloudProvider.ID
 
   private final AzureCloudProvider azureCloudProvider
   private final Cache cacheView
   final ObjectMapper objectMapper
+
+  @Autowired
+  AccountCredentialsProvider accountCredentialsProvider
 
   @Autowired
   AzureAppGatewayProvider(AzureCloudProvider azureCloudProvider, Cache cacheView, ObjectMapper objectMapper) {
@@ -107,4 +116,113 @@ class AzureAppGatewayProvider implements LoadBalancerProvider<AzureLoadBalancer>
     cacheData? objectMapper.convertValue(cacheData.attributes['appgateway'], AzureAppGatewayDescription) : null
   }
 
+  List<AzureAppGatewaySummary> list() {
+    getSummaryForAppGateways().values() as List
+  }
+
+  private Map<String, AzureAppGatewaySummary> getSummaryForAppGateways() {
+    Map<String, AzureAppGatewaySummary> map = [:]
+    def loadBalancers = getApplicationLoadBalancers('*')
+
+    loadBalancers?.each() { lb ->
+      def summary = map.get(lb.name)
+
+      if (!summary) {
+        summary = new AzureAppGatewaySummary(name: lb.name)
+        map.put lb.name, summary
+      }
+
+      def loadBalancerDetail = new AzureAppGatewayAccountRegionDetail(account: lb.account, name: lb.name, region: lb.region)
+
+      summary.getOrCreateAccount(lb.account).getOrCreateRegion(lb.region).loadBalancers << loadBalancerDetail
+    }
+    map
+  }
+
+  @Override
+  LoadBalancerProviderTempShim.Item get(String name) {
+    throw new UnsupportedOperationException("TODO: Implement single getter.")
+  }
+
+  List<Map> byAccountAndRegionAndName(String account, String region, String name) {
+    String appName = AzureUtilities.getAppNameFromAzureResourceName(name)
+    AzureAppGatewayDescription description = getAppGatewayDescription(account, appName, region, name)
+
+    if (description) {
+      def lbDetail = [
+          name: description.loadBalancerName
+      ]
+
+      lbDetail.account = account
+      lbDetail.region = region
+      lbDetail.application = appName
+      lbDetail.stack = description.stack
+      lbDetail.detail = description.detail
+      lbDetail.createdTime = description.createdTime
+      lbDetail.cluster = description.cluster ?: "unassigned"
+      lbDetail.serverGroups = description.serverGroups?.join(" ") ?: "unassigned"
+      lbDetail.securityGroup = description.securityGroup ?: "unassigned"
+      lbDetail.vnet = description.vnet ?: "vnet-unassigned"
+      lbDetail.subnet = description.subnet ?: "subnet-unassigned"
+      lbDetail.dnsName = description.dnsName ?: "dnsname-unassigned"
+
+      lbDetail.probes = description.probes
+      lbDetail.loadBalancingRules = description.loadBalancingRules
+      lbDetail.tags = description.tags
+
+      lbDetail.sku = description.sku
+      lbDetail.tier = description.tier
+      lbDetail.capacity = description.capacity
+
+      return [lbDetail]
+    }
+
+    return []
+  }
+
+  static class AzureAppGatewaySummary implements LoadBalancerProviderTempShim.Item {
+    private Map<String, AzureAppGatewayAccount> mappedAccounts = [:]
+    String name
+
+    AzureAppGatewayAccount getOrCreateAccount(String name) {
+      if (!mappedAccounts[name]) {
+        mappedAccounts[name] = new AzureAppGatewayAccount(name:name)
+      }
+      mappedAccounts[name]
+    }
+
+    @JsonProperty("accounts")
+    List<AzureAppGatewayAccount> getByAccounts() {
+      mappedAccounts.values() as List
+    }
+  }
+
+  static class AzureAppGatewayAccount implements LoadBalancerProviderTempShim.ByAccount {
+    private Map<String, AzureAppGatewayAccountRegion> mappedRegions = [:]
+    String name
+
+    AzureAppGatewayAccountRegion getOrCreateRegion(String name) {
+      if (!mappedRegions[name]) {
+        mappedRegions[name] =  new AzureAppGatewayAccountRegion(name: name, loadBalancers: [])
+      }
+      mappedRegions[name];
+    }
+
+    @JsonProperty("regions")
+    List<AzureAppGatewayAccountRegion> getByRegions() {
+      mappedRegions.values() as List
+    }
+  }
+
+  static class AzureAppGatewayAccountRegion implements LoadBalancerProviderTempShim.Details {
+    String name
+    List<AzureAppGatewayAccountRegionDetail> loadBalancers = []
+  }
+
+  static class AzureAppGatewayAccountRegionDetail implements LoadBalancerProviderTempShim.Details {
+    String type="azure"
+    String account
+    String region
+    String name
+  }
 }
