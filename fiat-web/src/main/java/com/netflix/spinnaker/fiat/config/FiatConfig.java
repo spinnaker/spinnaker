18588b15d62ce9d9b2807dd737b5c78ac7ca385d
Travis Tomsu <ttomsu@google.com>
2016-11-30 15:40:37 -0500

    Adds spectator integration for metrics collection. (#122)

--- fiat-web/src/main/java/com/netflix/spinnaker/fiat/config/FiatConfig.java
+++ fiat-web/src/main/java/com/netflix/spinnaker/fiat/config/FiatConfig.java
@@ -1,16 +1,21 @@
 package com.netflix.spinnaker.fiat.config;
 
+import com.google.common.collect.ImmutableList;
+import com.netflix.spectator.api.Registry;
 import com.netflix.spinnaker.fiat.model.resources.Role;
 import com.netflix.spinnaker.fiat.roles.UserRolesProvider;
+import com.netflix.spinnaker.kork.web.interceptors.MetricsInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,7 +23,21 @@ import java.util.Map;
 @Configuration
 @Import(RetrofitConfig.class)
 @EnableConfigurationProperties(FiatServerConfigurationProperties.class)
-public class FiatConfig {
+public class FiatConfig extends WebMvcConfigurerAdapter {
+
+  @Autowired
+  private Registry registry;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    List<String> pathVarsToTag = ImmutableList.of("accountName", "applicationName", "resourceName");
+    List<String> exclude = ImmutableList.of("BasicErrorController");
+    MetricsInterceptor interceptor = new MetricsInterceptor(this.registry,
+                                                            "controller.invocations",
+                                                            pathVarsToTag,
+                                                            exclude);
+    registry.addInterceptor(interceptor);
+  }
 
   @Bean
   @ConditionalOnMissingBean(UserRolesProvider.class)
