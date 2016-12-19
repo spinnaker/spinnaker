6e700af9159ecd2d5f3e824b15deda8507a7e67a
Daniel Peach <dpeach@google.com>
2016-12-19 13:03:59 -0500

    (appengine) terminate instance (#3105)

--- app/scripts/modules/appengine/instance/details/details.controller.ts
+++ app/scripts/modules/appengine/instance/details/details.controller.ts
@@ -1,6 +1,6 @@
 import {module, IScope, IPromise, IQService} from 'angular';
 import {IStateService} from 'angular-ui-router';
-import {flattenDeep} from 'lodash';
+import {flattenDeep, cloneDeep} from 'lodash';
 
 import {Application} from 'core/application/application.model';
 import {IAppengineInstance} from 'appengine/domain/index';
@@ -22,13 +22,17 @@ class AppengineInstanceDetailsController {
   public outOfServiceToolTip: string = `
     An App Engine instance is 'Out Of Service' if no load balancers are directing traffic to its server group.`;
 
-  static get $inject() { return ['$scope', '$state', '$q', 'app', 'instanceReader', 'instance']; }
+  static get $inject() {
+    return ['$scope', '$state', '$q', 'app', 'instanceReader', 'instanceWriter', 'confirmationModalService', 'instance'];
+  }
 
   constructor(private $scope: IScope,
               private $state: IStateService,
               private $q: IQService,
               private app: Application,
               private instanceReader: any,
+              private instanceWriter: any,
+              private confirmationModalService: any,
               instance: InstanceFromStateParams) {
     this.app.ready()
       .then(() => this.retrieveInstance(instance))
@@ -37,6 +41,38 @@ class AppengineInstanceDetailsController {
         this.state.loading = false;
       })
       .catch(() => this.autoClose());
+  }
+
+  public terminateInstance(): void {
+    let instance = cloneDeep(this.instance) as any;
+    let shortName = `${this.instance.name.substring(0, 10)}...`;
+    instance.placement = {};
+    instance.instanceId = instance.name;
+
+    let taskMonitor = {
+      application: this.app,
+      title: 'Terminating ' + shortName,
+      forceRefreshMessage: 'Refreshing application...',
+      forceRefreshEnabled: true,
+      onApplicationRefresh: function() {
+        if (this.$state.includes('**.instanceDetails', {instanceId: instance.name})) {
+          this.$state.go('^');
+        }
+      }
+    };
+
+    let submitMethod = () => {
+      return this.instanceWriter.terminateInstance(instance, this.app, {cloudProvider: 'appengine'});
+    };
+
+    this.confirmationModalService.confirm({
+      header: 'Really terminate ' + shortName + '?',
+      buttonText: 'Terminate ' + shortName,
+      account: instance.account,
+      provider: 'appengine',
+      taskMonitorConfig: taskMonitor,
+      submitMethod: submitMethod
+    });
   }
 
   private autoClose(): void {
@@ -77,4 +113,6 @@ export const APPENGINE_INSTANCE_DETAILS_CTRL = 'spinnaker.appengine.instanceDeta
 
 module(APPENGINE_INSTANCE_DETAILS_CTRL, [
   require('core/instance/instance.read.service.js'),
+  require('core/instance/instance.write.service.js'),
+  require('core/confirmationModal/confirmationModal.service.js'),
 ]).controller('appengineInstanceDetailsCtrl', AppengineInstanceDetailsController);
