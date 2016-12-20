e8a3e38e83fbc4cf7c3111383cb58cec93fecc48
Tomas Lin <clin@netflix.com>
2016-12-19 16:54:59 -0800

    (titus) simplified UI for bakery

---
+++ app/scripts/modules/titus/pipeline/stages/bake/bakeExecutionDetails.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import {EXECUTION_DETAILS_SECTION_SERVICE} from 'core/delivery/details/executionDetailsSection.service';
+
+let angular = require('angular');
+
+module.exports = angular.module('spinnaker.core.pipeline.stage.bake.titus.executionDetails.controller', [
+  require('angular-ui-router'),
+  EXECUTION_DETAILS_SECTION_SERVICE,
+  require('core/delivery/details/executionDetailsSectionNav.directive.js'),
+])
+  .controller('titusBakeExecutionDetailsCtrl', function ($scope, $stateParams, executionDetailsSectionService,
+                                                          $interpolate, settings) {
+
+    $scope.configSections = ['bakeConfig', 'taskStatus'];
+
+    let initialized = () => {
+      $scope.detailsSection = $stateParams.details;
+      $scope.provider = $scope.stage.context.cloudProviderType || 'titus';
+      $scope.bakeryDetailUrl = $interpolate(settings.bakeryDetailUrl);
+    };
+
+    let initialize = () => executionDetailsSectionService.synchronizeSection($scope.configSections, initialized);
+
+    initialize();
+
+    $scope.$on('$stateChangeSuccess', initialize);
+
+  });
