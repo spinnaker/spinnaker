07ffcb1c8d69121470eb6d318c87c8df51ab59b1
Daniel Peach <dpeach@google.com>
2017-01-11 11:14:57 -0500

    (appengine) scaling policy details (#3161)

--- app/scripts/modules/appengine/common/conditionalDescriptionListItem.component.ts
+++ app/scripts/modules/appengine/common/conditionalDescriptionListItem.component.ts
@@ -1,12 +1,32 @@
-import {module, IComponentOptions} from 'angular';
+import {module, IComponentOptions, IFilterService, IComponentController} from 'angular';
+
+class AppengineConditionalDescriptionListItemCtrl implements IComponentController {
+  public label: string;
+  public key: string;
+  public component: any;
+
+  static get $inject() { return ['$filter']; }
+
+  constructor(private $filter: IFilterService) {}
+
+  public $onInit(): void {
+    if (!this.label) {
+      this.label = this.$filter<Function>('robotToHuman')(this.key);
+    }
+  }
+}
 
 class AppengineConditionalDescriptionListItem implements IComponentOptions {
   public bindings: any = {label: '@', key: '@', component: '<'};
-  public transclude: boolean = true;
+  public transclude: any = {
+    keyLabel: '?keyText',
+    valueLabel: '?valueLabel',
+  };
   public template: string = `
-    <dt ng-if="$ctrl.component[$ctrl.key]">{{$ctrl.label}}<ng-transclude></ng-transclude></dt>
-    <dd>{{$ctrl.component[$ctrl.key]}}</dd>
+    <dt ng-if="$ctrl.component[$ctrl.key]">{{$ctrl.label}}<span ng-transclude="keyLabel"></span></dt>
+    <dd ng-if="$ctrl.component[$ctrl.key]">{{$ctrl.component[$ctrl.key]}}<span ng-transclude="valueLabel"></span></dd>
   `;
+  public controller: any = AppengineConditionalDescriptionListItemCtrl;
 }
 
 export const APPENGINE_CONDITIONAL_DESCRIPTION_LIST_ITEM = 'spinnaker.appengine.conditionalDescriptionListItem';
