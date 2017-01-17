f2a66b989b7ed9368291247869229cfb3b3d7593
Lars Wander <lwander@google.com>
2017-01-17 11:03:33 -0500

    Switch from deploying replication controllers to replica sets

--- experimental/kubernetes/simple/scripts/startup-redis.sh
+++ experimental/kubernetes/simple/scripts/startup-redis.sh
@@ -4,5 +4,5 @@ echo
 # Create a service to track the servers
 kubectl create -f svcs/data-redis-server.yaml
 
-# Create a replication controller for redis servers
-kubectl create -f rcs/data-redis-master.yaml
+# Create a replica set for redis servers
+kubectl create -f rs/data-redis-master.yaml
