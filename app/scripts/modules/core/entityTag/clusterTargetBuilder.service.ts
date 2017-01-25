ace813edea8e111d1407938bfbb6e3b1884dcec8
anotherchrisberry <anotherchrisberry@gmail.com>
2017-01-25 10:13:55 -0800

    (titus) add alert/notice support

---
+++ app/scripts/modules/core/entityTag/clusterTargetBuilder.service.ts
@@ -0,0 +1,51 @@
+import {module} from 'angular';
+
+import {NAMING_SERVICE, NamingService} from 'core/naming/naming.service';
+import {ServerGroup} from '../domain/serverGroup';
+import {IOwnerOption} from './entityTagEditor.controller';
+
+export class ClusterTargetBuilder {
+
+  static get $inject() { return ['namingService']; }
+
+  constructor(private namingService: NamingService) {}
+
+  public buildClusterTargets(serverGroup: ServerGroup): IOwnerOption[] {
+    const clusterName = this.namingService.getClusterNameFromServerGroupName(serverGroup.name);
+    return [
+      {
+        type: 'serverGroup',
+        label: `just ${serverGroup.name}`,
+        owner: serverGroup,
+        isDefault: true,
+      },
+      {
+        type: 'cluster',
+        label: `all server groups in **${serverGroup.region}** in ${clusterName}`,
+        owner: {
+          name: clusterName,
+          cloudProvider: 'aws',
+          region: serverGroup.region,
+          account: serverGroup.account,
+        },
+        isDefault: false,
+      },
+      {
+        type: 'cluster',
+        label: `all server groups in **all regions** in ${clusterName}`,
+        owner: {
+          name: clusterName,
+          cloudProvider: 'aws',
+          region: '*',
+          account: serverGroup.account,
+        },
+        isDefault: false,
+      },
+    ];
+  }
+}
+
+export const CLUSTER_TARGET_BUILDER = 'spinnaker.core.entityTag.clusterTargetBuilder';
+module(CLUSTER_TARGET_BUILDER, [
+  NAMING_SERVICE,
+]).service('clusterTargetBuilder', ClusterTargetBuilder);
