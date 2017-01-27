bda95a0895671f97ba99f0f831758a9f89d86e26
Lars Wander <lwander@users.noreply.github.com>
2017-01-27 15:44:27 -0500

    Project structure (#1)

---
+++ kayenta-web/pkg_scripts/postInstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# ubuntu
+# check that owner group exists
+if [ -z `getent group spinnaker` ]; then
+  groupadd spinnaker
+fi
+
+# check that user exists
+if [ -z `getent passwd spinnaker` ]; then
+  useradd --gid spinnaker spinnaker -m --home-dir /home/spinnaker
+fi
+
+install --mode=755 --owner=spinnaker --group=spinnaker --directory  /var/log/spinnaker/kayenta 
