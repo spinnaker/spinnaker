595eb26a0abc34928299059a48eb1c69950e041f
Lars Wander <lwander@users.noreply.github.com>
2017-02-13 18:51:43 -0500

    Create halconfig (#3298)

---
+++ halconfig/settings.js
@@ -0,0 +1,123 @@
+'use strict';
+
+var feedbackUrl = '';
+var gateHost = '{%gate.baseUrl%}';
+var bakeryDetailUrl = (gateHost + '/bakery/logs/{{context.region}}/{{context.status.resourceId}}');
+var authEndpoint = (gateHost + '/auth/user');
+var authEnabled = {%features.auth%};
+var chaosEnabled = {%features.chaos%};
+var fiatEnabled = {%features.fiat%};
+var jobsEnabled = {%features.jobs%};
+var timezone = '{%timezone%}';
+var gce = {
+  defaults: {
+    account: '{%google.default.account%}',
+    region: '{%google.default.region%}',
+    zone: '{%google.default.zone%}'
+  }
+};
+var kubernetes = {
+  defaults: {
+    account: '{%kubernetes.default.account%}',
+    namespace: '{%kubernetes.default.namespace%}',
+    proxy: '{%kubernetes.default.proxy%}'
+  }
+};
+var appengine = {
+  defaults: {
+    account: '{%appengine.default.account%}',
+    editLoadBalancerStageEnabled: {%appengine.enabled%}
+  }
+};
+var entityTagsEnabled = false;
+var netflixMode = false;
+
+window.spinnakerSettings = {
+  checkForUpdates: false,
+  defaultProviders: ['aws', 'gce', 'azure', 'cf', 'kubernetes', 'titus', 'openstack'],
+  feedbackUrl: feedbackUrl,
+  gateUrl: gateHost,
+  bakeryDetailUrl: bakeryDetailUrl,
+  authEndpoint: authEndpoint,
+  pollSchedule: 30000,
+  defaultTimeZone: timezone, // see http://momentjs.com/timezone/docs/#/data-utilities/
+  defaultCategory: 'serverGroup',
+  defaultInstancePort: 80,
+  providers: {
+    azure: {
+      defaults: {
+        account: 'azure-test',
+        region: 'westus'
+      },
+    },
+    aws: {
+      defaults: {
+        account: 'test',
+        region: 'us-east-1',
+        iamRole: 'BaseIAMRole',
+      },
+      defaultSecurityGroups: [],
+      loadBalancers: {
+        // if true, VPC load balancers will be created as internal load balancers if the selected subnet has a purpose
+        // tag that starts with "internal"
+        inferInternalFlagFromSubnet: false,
+      },
+      useAmiBlockDeviceMappings: false,
+    },
+    gce: gce,
+    titus: {
+      defaults: {
+        account: 'titustestvpc',
+        region: 'us-east-1',
+        iamProfile: '{{application}}InstanceProfile',
+      },
+    },
+    openstack: {
+      defaults: {
+        account: 'test',
+        region: 'us-west-1'
+      },
+    },
+    kubernetes: kubernetes,
+    appengine: appengine, 
+  },
+  whatsNew: {
+    gistId: '32526cd608db3d811b38',
+    fileName: 'news.md',
+  },
+  notifications: {
+    email: {
+      enabled: true,
+    },
+    hipchat: {
+      enabled: true,
+      botName: 'Skynet T-800'
+    },
+    sms: {
+      enabled: true,
+    },
+    slack: {
+      enabled: true,
+      botName: 'spinnakerbot'
+    }
+  },
+  authEnabled: authEnabled,
+  authTtl: 600000,
+  gitSources: ['stash', 'github'],
+  triggerTypes: ['git', 'pipeline', 'docker', 'cron', 'jenkins'],
+  feature: {
+    entityTags: entityTagsEnabled,
+    fiatEnabled: fiatEnabled,
+    netflixMode: netflixMode,
+    chaosMonkey: chaosEnabled,
+    jobs: jobsEnabled,
+    pipelines: true,
+    notifications: false,
+    fastProperty: true,
+    vpcMigrator: true,
+    clusterDiff: false,
+    roscoMode: true,
+    infrastructureStages: false,
+    snapshots: false,
+  },
+};
