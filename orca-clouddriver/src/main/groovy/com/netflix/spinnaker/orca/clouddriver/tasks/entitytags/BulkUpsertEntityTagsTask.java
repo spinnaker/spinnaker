81c794d8f722c03d2553e375ac7cda855da6049a
anotherchrisberry <anotherchrisberry@gmail.com>
2017-02-14 11:48:13 -0800

    add bulkUpsertEntityTags stage

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/entitytags/BulkUpsertEntityTagsTask.java
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/entitytags/BulkUpsertEntityTagsTask.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 Netflix, Inc.
+ * Copyright 2017 Netflix, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,9 +21,7 @@ import com.netflix.spinnaker.orca.ExecutionStatus;
 import com.netflix.spinnaker.orca.RetryableTask;
 import com.netflix.spinnaker.orca.TaskResult;
 import com.netflix.spinnaker.orca.clouddriver.KatoService;
-import com.netflix.spinnaker.orca.clouddriver.OortService;
 import com.netflix.spinnaker.orca.clouddriver.model.TaskId;
-import com.netflix.spinnaker.orca.clouddriver.tasks.AbstractCloudProviderAwareTask;
 import com.netflix.spinnaker.orca.pipeline.model.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -34,11 +32,11 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Component
-public class UpsertEntityTagsTask extends AbstractCloudProviderAwareTask implements RetryableTask {
+public class BulkUpsertEntityTagsTask implements RetryableTask {
   private final KatoService kato;
 
   @Autowired
-  public UpsertEntityTagsTask(KatoService kato) {
+  public BulkUpsertEntityTagsTask(KatoService kato) {
     this.kato = kato;
   }
 
@@ -46,12 +44,12 @@ public class UpsertEntityTagsTask extends AbstractCloudProviderAwareTask impleme
   public TaskResult execute(Stage stage) {
     TaskId taskId = kato.requestOperations(Collections.singletonList(
       new HashMap<String, Map>() {{
-        put("upsertEntityTags", stage.getContext());
+        put("bulkUpsertEntityTagsDescription", stage.getContext());
       }})
     ).toBlocking().first();
 
     return new DefaultTaskResult(ExecutionStatus.SUCCEEDED, new HashMap<String, Object>() {{
-      put("notification.type", "upsertentitytags");
+      put("notification.type", "bulkupsertentitytags");
       put("kato.last.task.id", taskId);
     }});
   }
