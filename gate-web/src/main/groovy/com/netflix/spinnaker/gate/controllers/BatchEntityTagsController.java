b4cdb3ac45e219bc12ec96faa53c8d8aace5ce19
anotherchrisberry <anotherchrisberry@gmail.com>
2017-02-14 12:01:24 -0800

    add batchUpdate endpoint for tagging

---
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/BatchEntityTagsController.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.gate.controllers;
+
+import com.netflix.spinnaker.gate.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/batch/tags")
+public class BatchEntityTagsController {
+
+  private TaskService taskService;
+
+  @Autowired
+  public BatchEntityTagsController(TaskService taskService) {
+    this.taskService = taskService;
+  }
+
+  @RequestMapping(value = "/batch", method = RequestMethod.POST)
+  @ResponseStatus(value = HttpStatus.ACCEPTED)
+  public Map batchUpdate(@RequestBody List<Map> entityTags) {
+    Map<String, Object> job = new HashMap<>();
+    job.put("type", "bulkUpsertEntityTags");
+    job.put("entityTags", entityTags);
+
+    List<Map<String, Object>> jobs = new ArrayList<>();
+    jobs.add(job);
+
+    Map<String, Object> operation = new HashMap<>();
+    operation.put("description", "Bulk upsert Tags");
+    operation.put("job", jobs);
+    return taskService.createAppTask("ad-hoc", operation);
+  }
+}
