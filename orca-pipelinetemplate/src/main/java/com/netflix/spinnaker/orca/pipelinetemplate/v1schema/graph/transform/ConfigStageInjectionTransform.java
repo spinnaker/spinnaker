05c27cdc11b7871fa8120dcdfacfb99131a209e2
jeyrs <jeyrschabu@gmail.com>
2017-02-16 13:09:25 -0800

    Refactoring & adding cycle detection in DAG
    - Updated injectFirst to add directly to the beginning of the list since graph is topologically sorted
    - Updated injectLast. Now finding leaf nodes through dfs as well
    - Ensuring no cycle is introduced during injection
    
    Completed injection schemes

--- orca-pipelinetemplate/src/main/java/com/netflix/spinnaker/orca/pipelinetemplate/v1schema/graph/transform/ConfigStageInjectionTransform.java
+++ orca-pipelinetemplate/src/main/java/com/netflix/spinnaker/orca/pipelinetemplate/v1schema/graph/transform/ConfigStageInjectionTransform.java
@@ -23,8 +23,7 @@ import com.netflix.spinnaker.orca.pipelinetemplate.v1schema.model.TemplateConfig
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -73,32 +72,67 @@ public class ConfigStageInjectionTransform implements PipelineTemplateVisitor {
 
   private void injectStages(PipelineTemplate pipelineTemplate) {
     // Create initial graph via dependsOn.
-    createDag(pipelineTemplate.getStages());
+    pipelineTemplate.setStages(
+      createGraph(pipelineTemplate.getStages())
+    );
 
     // Handle stage injections.
     injectStages(pipelineTemplate.getStages(), pipelineTemplate.getStages());
     injectStages(templateConfiguration.getStages(), pipelineTemplate.getStages());
   }
 
-  private static void createDag(List<StageDefinition> stages) {
-    stages
+  private enum Status {
+    VISITING,
+    VISITED
+  }
+
+  private static void dfs(String stageId,
+                          Set<StageDefinition> result,
+                          Map<String, Status> state,
+                          Map<String, StageDefinition> graph,
+                          Map<String, Integer> outOrder) {
+
+    state.put(stageId, Status.VISITING);
+    StageDefinition stage = graph.get(stageId);
+
+    for (String n : stage.getRequisiteStageRefIds()) {
+      Status status = state.get(n);
+      if (status == Status.VISITING) throw new IllegalStateException("Cycle detected in graph");
+      if (status == Status.VISITED) continue;
+      dfs(n, result, state, graph, outOrder);
+    }
+
+    result.add(stage);
+    state.put(stage.getId(), Status.VISITED);
+    stage.getRequisiteStageRefIds().forEach( s -> {
+      int order = outOrder.getOrDefault(s, 0);
+      outOrder.put(s, order + 1);
+    });
+  }
+
+  private static List<StageDefinition> createGraph(List<StageDefinition> stages) {
+    Set<StageDefinition> sorted = new LinkedHashSet<>();
+    Map<String, Status> state = new HashMap<>();
+
+    stages.forEach(s -> s.setRequisiteStageRefIds(s.getDependsOn()));
+
+    Map<String, StageDefinition> graph = stages
       .stream()
-      .filter(s -> !s.getDependsOn().isEmpty() && s.getInject() == null)
-      .forEach(s -> s.setRequisiteStageRefIds(
-        stages
-          .stream()
-          .filter(parentCandidate -> s.getDependsOn().contains(parentCandidate.getId()))
-          .map(StageDefinition::getId)
-          .collect(Collectors.toList())
-      ));
+      .collect(
+        Collectors.toMap(StageDefinition::getId, i -> i)
+      );
+
+    graph.forEach((k, v) -> dfs(k, sorted, state, graph, new HashMap<>()));
+
+    return sorted
+      .stream()
+      .collect(Collectors.toList());
   }
 
   private static void injectStages(List<StageDefinition> stages, List<StageDefinition> templateStages) {
     stages.stream()
       .filter(s -> s.getInject() != null)
       .forEach(s -> {
-        templateStages.add(s);
-
         InjectionRule rule = s.getInject();
 
         if (rule.getFirst() != null && rule.getFirst()) {
@@ -126,44 +160,100 @@ public class ConfigStageInjectionTransform implements PipelineTemplateVisitor {
   }
 
   private static void injectFirst(StageDefinition stage, List<StageDefinition> allStages) {
-    allStages
-      .stream()
-      .filter(pts -> pts.getRequisiteStageRefIds().isEmpty())
-      .forEach(pts -> pts.getRequisiteStageRefIds().add(stage.getId()));
+    allStages.get(0).getRequisiteStageRefIds().add(stage.getId());
+    allStages.add(0, stage);
   }
 
   private static void injectLast(StageDefinition stage, List<StageDefinition> allStages) {
-    int numStages = allStages.size();
-    allStages
+    Map<String, Integer> outOrder = new HashMap<>();
+    Set<StageDefinition> sorted = new LinkedHashSet<>();
+    Map<String, Status> state = new HashMap<>();
+
+    Map<String, StageDefinition> graph = allStages
       .stream()
-      .filter(s -> !s.getId().equals(stage.getId()))
-      .filter(candidate -> allStages
-        .stream()
-        .filter(s -> !s.getRequisiteStageRefIds().contains(candidate.getId()))
-        .count() == numStages)
-      .forEach(parent -> stage.getRequisiteStageRefIds().add(parent.getId()));
+      .collect(
+        Collectors.toMap(StageDefinition::getId, i -> i)
+      );
+
+    // leaf nodes are stages with outOrder 0
+    graph.keySet().forEach(k -> outOrder.put(k, 0));
+    graph.forEach((k, v) -> dfs(k, sorted, state, graph, outOrder));
+    sorted
+      .stream()
+      .filter(i -> outOrder.get(i.getId()) == 0)
+      .forEach(i -> stage.getRequisiteStageRefIds().add(i.getId()));
+
+    allStages.add(stage);
+    graph.put(stage.getId(), stage);
+    ensureNoCyclesInDAG(allStages, graph);
   }
 
   private static void injectBefore(StageDefinition stage, String targetId, List<StageDefinition> allStages) {
     StageDefinition target = getInjectionTarget(stage.getId(), targetId, allStages);
+    int index = allStages.indexOf(target);
 
     stage.getRequisiteStageRefIds().addAll(target.getRequisiteStageRefIds());
     target.getRequisiteStageRefIds().clear();
-    target.getRequisiteStageRefIds().add(stage.getId());
+    target.setRequisiteStageRefIds(Collections.singletonList(stage.getId()));
+    allStages.add(index + 1, stage);
+
+    Map<String, StageDefinition> graph = allStages
+      .stream()
+      .collect(
+        Collectors.toMap(StageDefinition::getId, i -> i)
+      );
+
+    ensureNoCyclesInDAG(allStages, graph);
+  }
+
+  private static void ensureNoCyclesInDAG(List<StageDefinition> allStages, Map<String, StageDefinition> graph) {
+    Map<String, Status> state = new HashMap<>();
+    allStages
+      .stream()
+      .collect(
+        Collectors.toMap(StageDefinition::getId, i -> i)
+      ).forEach((k, v) -> dfs(k, new LinkedHashSet<>(), state, graph, new HashMap<>()));
   }
 
   private static void injectAfter(StageDefinition stage, String targetId, List<StageDefinition> allStages) {
-    StageDefinition target = getInjectionTarget(stage.getId(), targetId, allStages);
+    Map<String, Integer> outOrder = new HashMap<>();
+    Set<StageDefinition> sorted = new LinkedHashSet<>();
+    Map<String, Status> state = new HashMap<>();
+
+    Map<String, StageDefinition> graph = allStages
+      .stream()
+      .collect(
+        Collectors.toMap(StageDefinition::getId, i -> i)
+      );
+
+    graph.forEach((k, v) -> dfs(k, sorted, state, graph, outOrder));
+
+    StageDefinition target = graph.get(targetId);
+    if (target == null) {
+      throw new RuntimeException(String.format("could not inject '%s' stage: unknown target stage id '%s'", stage.getId(), targetId));
+    }
 
     stage.getRequisiteStageRefIds().add(target.getId());
-    allStages
+    int index = allStages.indexOf(target);
+
+    // 1. find edges to target stage
+    Set<StageDefinition> edges = graph.entrySet()
       .stream()
-      .filter(childCandidate -> !childCandidate.getId().equals(stage.getId()) &&
-        childCandidate.getRequisiteStageRefIds().contains(target.getId()))
-      .forEach(child -> {
-        child.getRequisiteStageRefIds().removeAll(Collections.singleton(target.getId()));
-        child.getRequisiteStageRefIds().add(stage.getId());
-      });
+      .filter(s -> s.getValue().getRequisiteStageRefIds().contains(targetId))
+      .map(Map.Entry::getValue)
+      .collect(Collectors.toSet());
+
+    // 2. swap target with stage being inserted
+    edges
+      .stream()
+      .filter(e -> e.getRequisiteStageRefIds().removeIf(targetId::equals))
+      .forEach(e -> e.getRequisiteStageRefIds().add(stage.getId()));
+
+    // 3. add in the sorted graph
+    allStages.add(index + 1, stage);
+    graph.put(stage.getId(), stage);
+
+    ensureNoCyclesInDAG(allStages, graph);
   }
 
   private static StageDefinition getInjectionTarget(String stageId, String targetId, List<StageDefinition> allStages) {
