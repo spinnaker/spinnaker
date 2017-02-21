32673fe576714fa299bc2d5b22d870c1d906f5a5
Zan Thrash <zanthrash@gmail.com>
2017-02-21 17:24:09 -0600

    Enhance fast property execution details (#3320)
    * Enhance fast property execution details
    
    - Added the action type (CREATE, UPDATE, DELETE) to the execution label and added it in the status tab.
    - When pipeline fails it will now let the user know that the properties were rolled back and displays the rolled back state.
    - Properties that where deleted were not displaying any key/value pairs in the details tab.
    
    * fix typo

--- app/scripts/modules/netflix/pipeline/stage/properties/create/propertyExecutionDetails.controller.js
+++ app/scripts/modules/netflix/pipeline/stage/properties/create/propertyExecutionDetails.controller.js
@@ -16,11 +16,14 @@ module.exports = angular.module('spinnaker.netflix.pipeline.stage.property.detai
 
     let initialized = () => {
       $scope.detailsSection = $stateParams.details;
-      $scope.properties = $scope.stage.context.persistedProperties;
+      $scope.properties = getPropertiesForAction($scope.stage.context);
+      $scope.originalProperties = extractProperties($scope.stage.context.originalProperties);
       $scope.rolledBack = $scope.stage.context.rollbackProperties;
       $scope.notificationEmail = $scope.stage.context.email;
       $scope.cmcTicket = $scope.stage.context.cmcTicket;
       $scope.scope = $scope.stage.context.scope;
+      $scope.propertyAction = $scope.stage.context.propertyAction;
+      $scope.pipelineStatus = $scope.execution.status;
     };
 
     this.propertyScopeForDisplay = () => {
@@ -32,10 +35,30 @@ module.exports = angular.module('spinnaker.netflix.pipeline.stage.property.detai
       return $scope.stage.context.exception.details.error;
     };
 
+    this.isExecutionTerminalOrCanceled = () => {
+      return $scope.pipelineStatus === 'TERMINAL' || $scope.pipelineStatus === 'CANCELED';
+    };
+
+    this.wasCreateAction = () => {
+      return $scope.propertyAction === 'CREATE';
+    };
+
     let initialize = () => executionDetailsSectionService.synchronizeSection($scope.configSections, initialized);
 
     initialize();
 
     $scope.$on('$stateChangeSuccess', initialize);
+
+    let getPropertiesForAction = (stageContext) => {
+      if (stageContext.propertyAction === 'DELETE' && stageContext.originalProperties) {
+        return extractProperties(stageContext.originalProperties);
+      }
+      return stageContext.persistedProperties;
+    };
+
+    let extractProperties = (propertyList) => {
+      if (propertyList.length) return propertyList.map( prop => prop.property );
+    };
+
 
   });
