47273753a7fe7eb6804b83ddbaf47554f50ce3f8
Jacob Kiefer <jacobkiefer@google.com>
2017-03-02 16:15:46 -0500

    feat(install): Install packer when rosco is installed. (#189)

--- rosco-web/pkg_scripts/postInstall.sh
+++ rosco-web/pkg_scripts/postInstall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # ubuntu
 # check that owner group exists
@@ -9,6 +9,20 @@ fi
 # check that user exists
 if [ -z `getent passwd spinnaker` ]; then
   useradd --gid spinnaker spinnaker -m --home-dir /home/spinnaker
+fi
+
+# install packer
+PACKER_VERSION="0.12.1"
+packer_version=$(packer --version)
+packer_status=$?
+if [ $packer_status -ne 0 ] || [ "$packer_version" -ne "$PACKER_VERSION" ]; then
+  pushd .
+  cd /usr/bin
+  wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+  apt-get install unzip -y
+  unzip -o "packer_${PACKER_VERSION}_linux_amd64.zip"
+  rm "packer_${PACKER_VERSION}_linux_amd64.zip"
+  popd
 fi
 
 install --mode=755 --owner=spinnaker --group=spinnaker --directory  /var/log/spinnaker/rosco
