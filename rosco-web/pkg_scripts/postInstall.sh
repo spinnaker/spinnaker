f061cce102271d439600d8d6d649435947a479cc
Jacob Kiefer <jacobkiefer@google.com>
2017-03-03 13:04:17 -0500

    fix(install): Use tempdir instead of /usr/bin. (#191)

--- rosco-web/pkg_scripts/postInstall.sh
+++ rosco-web/pkg_scripts/postInstall.sh
@@ -11,19 +11,19 @@ if [ -z `getent passwd spinnaker` ]; then
   useradd --gid spinnaker spinnaker -m --home-dir /home/spinnaker
 fi
 
-# install packer
 function install_packer() {
   PACKER_VERSION="0.12.1"
   local packer_version=$(packer --version)
   local packer_status=$?
   if [[ $packer_status -ne 0 ]] || [[ "$packer_version" != "$PACKER_VERSION" ]]; then
+    TEMPDIR=$(mktemp -d installrosco.XXXX)
     pushd .
-    cd /usr/bin
+    cd $TEMPDIR
     wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
     apt-get install unzip -y
-    unzip -o "packer_${PACKER_VERSION}_linux_amd64.zip"
-    rm "packer_${PACKER_VERSION}_linux_amd64.zip"
+    unzip -o "packer_${PACKER_VERSION}_linux_amd64.zip" -d /usr/bin
     popd
+    rm -rf $TEMPDIR
   fi
 }
 
