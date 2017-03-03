3dc7a466a78ea735056a77205f51d2d12c063cdf
Jacob Kiefer <jacobkiefer@google.com>
2017-03-03 11:37:47 -0500

    fix(install): Wrap packer failure in function. (#190)

--- rosco-web/pkg_scripts/postInstall.sh
+++ rosco-web/pkg_scripts/postInstall.sh
@@ -12,17 +12,20 @@ if [ -z `getent passwd spinnaker` ]; then
 fi
 
 # install packer
-PACKER_VERSION="0.12.1"
-packer_version=$(packer --version)
-packer_status=$?
-if [ $packer_status -ne 0 ] || [ "$packer_version" -ne "$PACKER_VERSION" ]; then
-  pushd .
-  cd /usr/bin
-  wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
-  apt-get install unzip -y
-  unzip -o "packer_${PACKER_VERSION}_linux_amd64.zip"
-  rm "packer_${PACKER_VERSION}_linux_amd64.zip"
-  popd
-fi
+function install_packer() {
+  PACKER_VERSION="0.12.1"
+  local packer_version=$(packer --version)
+  local packer_status=$?
+  if [[ $packer_status -ne 0 ]] || [[ "$packer_version" != "$PACKER_VERSION" ]]; then
+    pushd .
+    cd /usr/bin
+    wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
+    apt-get install unzip -y
+    unzip -o "packer_${PACKER_VERSION}_linux_amd64.zip"
+    rm "packer_${PACKER_VERSION}_linux_amd64.zip"
+    popd
+  fi
+}
 
+install_packer
 install --mode=755 --owner=spinnaker --group=spinnaker --directory  /var/log/spinnaker/rosco
