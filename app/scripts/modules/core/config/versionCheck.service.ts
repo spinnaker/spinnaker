1733768d2b7f05dcdee8b53cc7325da930d180d0
anotherchrisberry <anotherchrisberry@gmail.com>
2017-03-04 10:30:22 -0800

    refactor(version check): convert to TS, use scheduler

---
+++ app/scripts/modules/core/config/versionCheck.service.ts
@@ -0,0 +1,69 @@
+import {module} from 'angular';
+
+interface IDeckVersion {
+  version: string;
+  created: number;
+}
+
+class VersionCheckService {
+  private currentVersion: IDeckVersion = require('../../../../../version.json');
+  private newVersionSeenCount = 0;
+  private scheduler: any;
+
+  static get $inject(): string[] {
+    return ['$http', 'notifierService', 'schedulerFactory', '$log', '$filter'];
+  }
+
+  constructor(private $http: ng.IHttpService,
+              private notifierService: any,
+              private schedulerFactory: any,
+              private $log: ng.ILogService,
+              private $filter: any) {}
+
+  public initialize(): void {
+    this.$log.debug(
+      'Deck version',
+      this.currentVersion.version,
+      'created',
+      this.$filter('timestamp')(this.currentVersion.created)
+    );
+    this.scheduler = this.schedulerFactory.createScheduler();
+    this.scheduler.subscribe(() => this.checkVersion());
+  }
+
+  private checkVersion(): void {
+    const url = `/version.json?_=${Date.now()}`;
+    this.$http.get(url).then((resp) => this.versionRetrieved(resp));
+  }
+
+  private versionRetrieved(response: any): void {
+    const data: IDeckVersion = response.data;
+    if (data.version === this.currentVersion.version) {
+      this.newVersionSeenCount = 0;
+    } else {
+      this.newVersionSeenCount++;
+      if (this.newVersionSeenCount > 5) {
+        this.$log.debug('New Deck version:', data.version, 'created', this.$filter('timestamp')(data.created));
+        this.notifierService.publish({
+          key: 'newVersion',
+          position: 'bottom',
+          body: `A new version of Spinnaker is available
+              <a role="button" class="action" onclick="document.location.reload(true)">Refresh</a>`
+        });
+        this.scheduler.unsubscribe();
+      }
+    }
+  }
+}
+
+export const VERSION_CHECK_SERVICE = 'spinnaker.core.config.versionCheck.service';
+module(VERSION_CHECK_SERVICE, [
+  require('./settings'),
+  require('../widgets/notifier/notifier.service'),
+  require('core/scheduler/scheduler.factory'),
+]).service('versionCheckService', VersionCheckService)
+  .run((versionCheckService: VersionCheckService, settings: any) => {
+    if (settings.checkForUpdates) {
+      versionCheckService.initialize();
+    }
+  });
