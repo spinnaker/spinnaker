2d3a749a620faab67b478da2c9e10c8e1908ca4f
Jacob Kiefer <jacobkiefer@google.com>
2017-03-08 16:17:13 -0500

    feat(build): Generate Docker images during build script. (#1465)

--- dev/generate_bom.py
+++ dev/generate_bom.py
@@ -27,6 +27,21 @@ from spinnaker.run import run_quick
 SERVICES = 'services'
 VERSION = 'version'
 
+GOOGLE_CONTAINER_BUILDER_BASE_CONFIG = {
+  'steps': [
+    {
+      'name': 'java:8',
+      'env': ['GRADLE_USER_HOME=cache'],
+      'args': []
+    },
+    {
+      'name': 'gcr.io/cloud-builders/docker',
+      'args': []
+    }
+  ],
+  'images': []
+}
+
 class BomGenerator(Annotator):
   """Provides facilities for generating the Bill of Materials file for the
   Spinnaker product release.
@@ -49,6 +64,7 @@ class BomGenerator(Annotator):
 
   def __init__(self, options):
     self.__base_dir = options.base_dir
+    self.__docker_registry = options.docker_registry
     self.__bom_file = ''
     self.__component_versions = {}
     super(BomGenerator, self).__init__(options)
@@ -56,9 +72,58 @@ class BomGenerator(Annotator):
   @classmethod
   def init_argument_parser(cls, parser):
     """Initialize command-line arguments."""
-    parser.add_argument('--base_dir', default='.',
+    parser.add_argument('--base_dir', default='.', required=True,
                         help="Base directory containing the component's git repositories as subdirectories.")
+    parser.add_argument('--container_builder', default='gcb',
+                        help="Type of builder to use. Currently, the supported options are {'gcb', 'docker'}.")
+    parser.add_argument('--docker_registry', default='',
+                        help="Docker registry to push the container images to.")
     super(BomGenerator, cls).init_argument_parser(parser)
+
+  def write_container_builder_gcr_config(self):
+    """Write a configuration file for producing Container Images with Google Container Builder for each microservice.
+    """
+    for comp in self.__component_versions:
+      config = dict(GOOGLE_CONTAINER_BUILDER_BASE_CONFIG)
+      version_bump = self.__component_versions[comp]
+
+      next_tag_with_build = '{0}-{1}'.format(version_bump.version_str,
+                                             self.build_number)
+      first_dash_idx = next_tag_with_build.index('-')
+      gradle_version = next_tag_with_build[first_dash_idx + 1:]
+
+      gradle_cmd = ''
+      if comp == 'deck':
+        gradle_cmd = './gradlew build -PskipTests'
+      else:
+        gradle_cmd = './gradlew {0}-web:installDist -x test'.format(comp)
+      config['steps'][0]['args'] = ['bash', '-c', gradle_cmd]
+      versioned_image = '{reg}/{repo}:{tag}'.format(reg=self.__docker_registry,
+                                                    repo=comp,
+                                                    tag=gradle_version)
+      config['steps'][1]['args'] = ['build', '-t', versioned_image, '-f', 'Dockerfile.slim', '.']
+      config['images'] = [versioned_image]
+      config_file = '{0}-gcb.yml'.format(comp)
+      with open(config_file, 'w') as cfg:
+        yaml.dump(config, cfg, default_flow_style=True)
+
+  def write_docker_version_files(self):
+    """Write a file containing the full tag for each microservice for Docker.
+    """
+    for comp in self.__component_versions:
+      version_bump = self.__component_versions[comp]
+
+      next_tag_with_build = '{0}-{1}'.format(version_bump.version_str,
+                                             self.build_number)
+      first_dash_idx = next_tag_with_build.index('-')
+      gradle_version = next_tag_with_build[first_dash_idx + 1:]
+      docker_tag = '{reg}/{comp}:{tag}'.format(reg=self.__docker_registry,
+                                               comp=comp,
+                                               tag=gradle_version)
+
+      config_file = '{0}-docker.yml'.format(comp)
+      with open(config_file, 'w') as cfg:
+        cfg.write(docker_tag)
 
   def write_bom(self):
     output_yaml = {SERVICES: {}}
@@ -169,6 +234,10 @@ class BomGenerator(Annotator):
 
     bom_generator = cls(options)
     bom_generator.determine_and_tag_versions()
+    if options.container_builder == 'gcb':
+      bom_generator.write_container_builder_gcr_config()
+    elif options.container_builder == 'docker':
+      bom_generator.write_docker_version_files()
     bom_generator.write_bom()
     bom_generator.publish_microservice_configs()
 
