56cf04ca153bf97818de927402a2ae91a91a2f55
Justin Reynolds <jreynolds@netflix.com>
2017-04-11 20:27:23 -0700

    refactor(core/delivery): Convert executionBuildNumber to a react component and bring in react-ga

--- app/scripts/react.module.ts
+++ app/scripts/react.module.ts
@@ -1,7 +1,15 @@
 import {module} from 'angular';
 import 'ngimport';
+import * as ReactGA from 'react-ga';
+
+import {SETTINGS} from './modules/core/config/settings';
 
 export const REACT_MODULE = 'spinnaker.react';
+
+// Initialize React Google Analytics
+if (SETTINGS.analytics.ga) {
+  ReactGA.initialize(SETTINGS.analytics.ga, {});
+}
 
 module(REACT_MODULE, [
   'bcherny/ngimport',
