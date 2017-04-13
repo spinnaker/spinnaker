45ba3d48bbc80a7b8bc2dd1fce6f3b5a0527eecb
anotherchrisberry <anotherchrisberry@gmail.com>
2017-04-13 13:37:22 -0700

    chore(modal): convert modal close component back to ng1

--- app/scripts/modules/core/index.ts
+++ app/scripts/modules/core/index.ts
@@ -3,17 +3,12 @@ import {Type} from '@angular/core';
 import {IDowngradeItem} from 'core/domain/IDowngradeItem';
 import {AUTHENTICATION_MODULE_DOWNGRADES} from './authentication';
 import {HELP_DIRECTIVE_UPGRADES} from './help';
-import {MODAL_COMPONENT_MODULE_DOWNGRADES} from './modal';
 import {UPGRADE_MODULE_DOWNGRADES} from './upgrade';
 import {WIDGET_DIRECTIVE_UPGRADES} from './widgets';
 
 export const CORE_MODULE_DOWNGRADES: IDowngradeItem[] = [
   ...AUTHENTICATION_MODULE_DOWNGRADES,
   ...UPGRADE_MODULE_DOWNGRADES
-];
-
-export const CORE_COMPONENT_MODULE_DOWNGRADES: IDowngradeItem[] = [
-  ...MODAL_COMPONENT_MODULE_DOWNGRADES
 ];
 
 export const CORE_DIRECTIVE_UPGRADES: Type<any>[] = [
--- app/scripts/modules/core/modal/index.ts
+++
@@ -1,6 +0,0 @@
-import {IDowngradeItem} from 'core/domain/IDowngradeItem';
-import {BUTTON_COMPONENT_MODULE_DOWNGRADES} from './buttons';
-
-export const MODAL_COMPONENT_MODULE_DOWNGRADES: IDowngradeItem[] = [
-  ...BUTTON_COMPONENT_MODULE_DOWNGRADES
-];
