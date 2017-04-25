1a68e565a3e682b4f64d05147a0313c17a4e7138
Ethan Rogers <ethanfrogers@users.noreply.github.com>
2017-04-25 11:29:07 -0400

    feat(kubernetes): Allow Run Job trigger tag (#3570)
    Support resolving Run Job container tags from triggers. There are a
    couple of things that can be improved here but this is basic
    implementation. One thing to think about is what happens if there are 2
    docker triggers and the pipeline is triggered by one that isn't used for
    this job. What happens?
    
    Changes include:
    
    * Checkbox for selecting if you want to resolve from trigger
    * If the box is checked, a list of Docker triggers is presented
    * The Run Job Execution Details will not print the tag is it's from a
      trigger

--- app/scripts/modules/kubernetes/pipeline/stages/runJob/runJobStage.js
+++ app/scripts/modules/kubernetes/pipeline/stages/runJob/runJobStage.js
@@ -23,13 +23,44 @@ module.exports = angular.module('spinnaker.core.pipeline.stage.kubernetes.runJob
       validators: [
         { type: 'requiredField', fieldName: 'account' },
         { type: 'requiredField', fieldName: 'namespace' },
-        { type: 'requiredField', fieldName: 'container.imageDescription.tag', fieldLabel: 'tag' },
+        { type: 'custom', validate: (_pipeline, stage) => {
+          let response = null;
+
+          if (stage.container.imageDescription.fromTrigger === false && !stage.container.imageDescription.tag) {
+            response = '<strong>Tag</strong> is a required field for Run Job stages.';
+          } else if (stage.container.imageDescription.fromTrigger === true && !stage.container.imageDescription.registry) {
+            response = '<strong>Trigger</strong> is a required field for Run Job stages.';
+          }
+
+          return response;
+        }},
       ]
     });
   }).controller('kubernetesRunJobStageCtrl', function($scope, accountService) {
+
     this.stage = $scope.stage;
+    this.pipeline = $scope.pipeline;
+    this.container = null;
+
+    const buildImageDescriptor = (image) => {
+      let descriptor = `${image.account}/${image.repository}`;
+      if (image.tag) {
+        descriptor += `:${image.tag}`;
+      }
+      return descriptor;
+    };
+
     if (!_.has(this.stage, 'container.name')) {
       _.set(this.stage, 'container.name', Date.now().toString());
+    }
+
+    if (!_.has(this.stage, 'container.imageDescription.fromTrigger')) {
+      _.set(this.stage, 'container.imageDescription.fromTrigger', false);
+    }
+
+
+    if (this.stage.container.imageDescription.fromTrigger === true) {
+      this.container = buildImageDescriptor(this.stage.container.imageDescription);
     }
 
     accountService.getUniqueAttributeForAllAccounts('kubernetes', 'namespaces')
@@ -52,4 +83,39 @@ module.exports = angular.module('spinnaker.core.pipeline.stage.kubernetes.runJob
     this.onChange = (changes) => {
       this.stage.container.imageDescription.registry = changes.registry;
     };
+
+    this.triggerImages = () => {
+
+      if (this.pipeline.triggers.length <= 0) {
+        return [];
+      }
+
+      return this.pipeline.triggers.filter((trigger) => {
+        return trigger.type === 'docker' && trigger.enabled === true;
+      }).map((trigger) => {
+        return buildImageDescriptor(trigger);
+      });
+    };
+
+    this.hasDockerPipelineTriggers = () => {
+      return this.triggerImages().length > 0;
+    };
+
+    this.updateContainerImage = () => {
+
+      const trigger = this.pipeline.triggers.find((trigger) => buildImageDescriptor(trigger) === this.container);
+
+      if (trigger) {
+        this.stage.container.imageDescription.account = trigger.account;
+        this.stage.container.imageDescription.organization = trigger.organization;
+        this.stage.container.imageDescription.repository = trigger.repository;
+        this.stage.container.imageDescription.registry = trigger.registry;
+        if (trigger.tag) {
+          this.stage.container.imageDescription.tag = trigger.tag;
+        } else {
+          this.stage.container.imageDescription.tag = null;
+        }
+      }
+    };
+
   });
