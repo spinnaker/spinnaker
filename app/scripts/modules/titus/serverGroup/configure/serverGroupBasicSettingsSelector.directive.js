a5731aa61eb46ed771e88847b3ca497f5d241393
Cameron Fieber <cfieber@netflix.com>
2017-04-26 14:59:54 -0700

    allow ~ in titus server group detail

--- app/scripts/modules/titus/serverGroup/configure/serverGroupBasicSettingsSelector.directive.js
+++ app/scripts/modules/titus/serverGroup/configure/serverGroupBasicSettingsSelector.directive.js
@@ -29,8 +29,8 @@ module.exports = angular.module('spinnaker.serverGroup.configure.titus.basicSett
     this.detailPattern = {
       test: function (detail) {
         var pattern = $scope.command.viewState.templatingEnabled ?
-          /^([a-zA-Z_0-9._$-{}\\\^]*(\${.+})*)*$/ :
-          /^[a-zA-Z_0-9._$-{}\\\^]*$/;
+          /^([a-zA-Z_0-9._$-{}\\\^~]*(\${.+})*)*$/ :
+          /^[a-zA-Z_0-9._$-{}\\\^~]*$/;
 
         return isNotExpressionLanguage(detail) ? pattern.test(detail) : true;
       }
