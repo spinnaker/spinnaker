b7bd270852dccc18be4c5b864fd0ffd79adee73f
Michael Graff <explorer-github@flame.org>
2017-05-17 10:59:03 -0400

    add SSE responses to the sample Atlas controller (#19)

--- kayenta-atlas/src/main/java/com/netflix/kayenta/controllers/SampleAtlasController.java
+++ kayenta-atlas/src/main/java/com/netflix/kayenta/controllers/SampleAtlasController.java
@@ -20,6 +20,7 @@ import com.google.common.base.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,24 +28,47 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This is a temporary stand-in for the /fetch endpoint that will be exposed by OSS Atlas.
  */
 @RestController
-@RequestMapping("/api/v1/fetch")
+@RequestMapping("/api/v2/fetch")
 public class SampleAtlasController {
 
   @Autowired
-  ResourceLoader resourceLoader;
+  private ResourceLoader resourceLoader;
 
-  @RequestMapping(method = RequestMethod.GET)
-  public String queryMetrics(@RequestParam final String q) throws IOException {
-    return getJsonContent("com/netflix/kayenta/controllers/sample-atlas-response.json");
+  private static final Map<String, String> queryMap = createQueryMap();
+
+  private static Map<String, String> createQueryMap()
+  {
+    Map<String,String> temp = new HashMap<String,String>();
+
+    temp.put("nf.app,app_mocha,:eq,name,CpuRawSystem,:eq,:and,(,nf.cluster,),:by",
+             "com/netflix/kayenta/controllers/sample00.sse");
+
+    temp.put("nf.app,app_wesley,:eq,name,CpuRawSystem,:eq,:and",
+            "com/netflix/kayenta/controllers/sample01.sse");
+
+    temp.put("nf.app,app_leo,:eq,name,CpuRawSystem,:eq,:and,(,nf.node,),:by",
+            "com/netflix/kayenta/controllers/sample02.sse");
+
+    temp.put("nf.app,app_nyassa,:eq,name,CpuRawSystem,:eq,:and,(,nf.node,),:by",
+            "com/netflix/kayenta/controllers/sample03.sse");
+
+    return temp;
   }
 
-  private String getJsonContent(String jsonFilename) throws IOException {
-    try (InputStream jsonInputStream = resourceLoader.getResource("classpath:" + jsonFilename).getInputStream()) {
+  @RequestMapping(method = RequestMethod.GET, produces = "text/event-stream")
+  public String queryMetrics(@RequestParam final String q) throws IOException {
+    return getSSEContent(queryMap.getOrDefault(q, "com/netflix/kayenta/controllers/empty.sse"));
+  }
+
+  private String getSSEContent(String sseFilename) throws IOException {
+    try (InputStream jsonInputStream = resourceLoader.getResource("classpath:" + sseFilename).getInputStream()) {
       return IOUtils.toString(jsonInputStream, Charsets.UTF_8.name());
     }
   }
