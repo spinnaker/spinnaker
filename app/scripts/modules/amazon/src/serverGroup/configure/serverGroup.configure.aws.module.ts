ab9e234a776c423c2d71481cdc8273bbb61db7e1
Justin Reynolds <jreynolds@netflix.com>
2017-05-27 12:49:44 -0700

    refactor(provider/amazon): Convert load balancer selector to TS

--- app/scripts/modules/amazon/src/serverGroup/configure/serverGroup.configure.aws.module.ts
+++ app/scripts/modules/amazon/src/serverGroup/configure/serverGroup.configure.aws.module.ts
@@ -1,21 +1,22 @@
 import { module } from 'angular';
 
 import { AZ_REBALANCE_SELECTOR } from './wizard/zones/azRebalanceSelector.component';
-import { SECURITY_GROUPS_REMOVED } from './wizard/securityGroups/securityGroupsRemoved.component';
 import { CAPACITY_SELECTOR } from './wizard/capacity/capacitySelector.component';
 import { HEALTH_PERCENT_SELECTOR } from './wizard/capacity/targetHealthyPercentageSelector.component';
+import { LOAD_BALANCER_SELECTOR } from './wizard/loadBalancers/loadBalancerSelector.component';
+import { SECURITY_GROUPS_REMOVED } from './wizard/securityGroups/securityGroupsRemoved.component';
 
 export const SERVER_GROUP_CONFIGURE_MODULE = 'spinnaker.amazon.serverGroup.configure.module';
 module(SERVER_GROUP_CONFIGURE_MODULE, [
+  AZ_REBALANCE_SELECTOR,
+  CAPACITY_SELECTOR,
+  HEALTH_PERCENT_SELECTOR,
+  LOAD_BALANCER_SELECTOR,
+  SECURITY_GROUPS_REMOVED,
   require('../serverGroup.transformer.js'),
   require('./wizard/templateSelection/deployInitializer.controller.js'),
   require('./wizard/location/ServerGroupBasicSettings.controller.js'),
   require('./wizard/securityGroups/securityGroupSelector.directive.js'),
-  SECURITY_GROUPS_REMOVED,
-  require('./wizard/loadBalancers/loadBalancerSelector.directive.js'),
-  CAPACITY_SELECTOR,
-  HEALTH_PERCENT_SELECTOR,
-  AZ_REBALANCE_SELECTOR,
   require('./wizard/zones/availabilityZoneSelector.directive.js'),
   require('./wizard/advancedSettings/advancedSettings.component.js'),
 ]);
---
+++ app/scripts/modules/amazon/src/serverGroup/configure/wizard/loadBalancers/loadBalancerSelector.component.ts
@@ -0,0 +1,39 @@
+import { IComponentController, IComponentOptions, module } from 'angular';
+
+import { INFRASTRUCTURE_CACHE_SERVICE, InfrastructureCacheService } from '@spinnaker/core';
+
+class LoadBalancerSelectorController implements IComponentController {
+  public command: any;
+
+  public refreshTime: number;
+  public refreshing = false;
+
+  constructor(private awsServerGroupConfigurationService: any, private infrastructureCaches: InfrastructureCacheService) { 'ngInject'; }
+
+  public setLoadBalancerRefreshTime(): void {
+    this.refreshTime = this.infrastructureCaches.get('loadBalancers').getStats().ageMax;
+  }
+
+  public refreshLoadBalancers(): void {
+    this.refreshing = true;
+    this.awsServerGroupConfigurationService.refreshLoadBalancers(this.command).then(() => {
+      this.refreshing = false;
+      this.setLoadBalancerRefreshTime();
+    });
+  }
+}
+
+export class LoadBalancerSelectorComponent implements IComponentOptions {
+  public bindings: any = {
+    command: '='
+  };
+  public controller: any = LoadBalancerSelectorController;
+  public templateUrl = require('./loadBalancerSelector.component.html');
+}
+
+export const LOAD_BALANCER_SELECTOR = 'spinnaker.amazon.serverGroup.configure.wizard.loadBalancers.selector.component';
+module (LOAD_BALANCER_SELECTOR, [
+  require('../../serverGroupConfiguration.service.js'),
+  INFRASTRUCTURE_CACHE_SERVICE
+])
+  .component('awsServerGroupLoadBalancerSelector', new LoadBalancerSelectorComponent());
