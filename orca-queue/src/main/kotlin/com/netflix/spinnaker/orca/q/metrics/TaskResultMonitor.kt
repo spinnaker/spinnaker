326a1ba019fc813a258c710704450ccdd57b8293
Rob Fletcher <rfletcher@netflix.com>
2017-06-06 14:40:51 -0700

    fix(orca): add a metric for task completions tagged by status and task type

---
+++ orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/metrics/TaskResultMonitor.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.orca.q.metrics
+
+import com.netflix.spectator.api.Registry
+import com.netflix.spinnaker.orca.events.TaskComplete
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationListener
+import org.springframework.stereotype.Component
+
+@Component
+open class TaskResultMonitor
+@Autowired constructor(private val registry: Registry)
+  : ApplicationListener<TaskComplete> {
+
+  private val id = registry.createId("orca.task.result")
+
+  override fun onApplicationEvent(event: TaskComplete) {
+    id
+      .withTag("status", event.status.name)
+      .withTag("task", event.taskType)
+      .let { id ->
+        registry
+          .counter(id)
+          .increment()
+      }
+  }
+}
