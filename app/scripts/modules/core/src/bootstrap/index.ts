e5041cce0af62c1be88d76621cd10008f790bdd2
Chris Thielen <github@sandgnat.com>
2017-06-06 13:46:36 -0700

    fix(core/bootstrap): Add missing imports

--- app/scripts/modules/core/src/bootstrap/index.ts
+++ app/scripts/modules/core/src/bootstrap/index.ts
@@ -2,9 +2,11 @@ export * from './bootstrap.module';
 export * from './paramChangedHelper'
 
 import './angular.config';
+import './appInit';
 import './spinnaker.component';
 import './stateChange.logger';
 import './transitionTrace.toggle';
+import './uiBootstrap.config';
 import './uiRouterRxjs';
 import './uiRouterVisualizer.toggle';
 import './uiSelect.config';
