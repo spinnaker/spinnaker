5c8bf0a464421640fc0d0514510384248f89bf3f
Gary Danko <gdanko@users.noreply.github.com>
2017-06-14 09:31:46 -0700

    add rpm support (#31)
    * add rpm support
    
    * Remove the artifact upload bits from the rpm section per Jake.
    
    * Removed an extra }

--- src/main/groovy/com/netflix/spinnaker/gradle/ospackage/OspackageBintrayPublishPlugin.groovy
+++ src/main/groovy/com/netflix/spinnaker/gradle/ospackage/OspackageBintrayPublishPlugin.groovy
@@ -22,6 +22,7 @@ import com.jfrog.bintray.gradle.BintrayPlugin
 import com.jfrog.bintray.gradle.BintrayUploadTask
 import com.jfrog.bintray.gradle.Utils
 import com.netflix.gradle.plugins.deb.Deb
+import com.netflix.gradle.plugins.rpm.Rpm
 import groovy.json.JsonBuilder
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
@@ -242,6 +243,119 @@ class OspackageBintrayPublishPlugin implements Plugin<Project> {
                 publishDeb.onlyIf {
                     graph.hasTask(':final') || graph.hasTask(':candidate') || (project.hasProperty("forcePublish$debTaskName") && project.property("forcePublish$debTaskName") as Boolean)
                 }
+            }
+        }
+        project.tasks.withType(Rpm) { Rpm rpm ->
+            rpm.release = packageExtension.buildNumber
+            String rpmTaskName = "${Character.toUpperCase(rpm.name.charAt(0))}${rpm.name.substring(1)}"
+            def publishRpm = project.tasks.create("publish$rpmTaskName")
+            publishRpm.doFirst {
+                BintrayExtension bintrayCfg = project.extensions.getByType(BintrayExtension)
+                def http = BintrayHttpClientFactory.create(bintrayCfg.apiUrl, bintrayCfg.user, bintrayCfg.key)
+                def org = bintrayCfg.pkg.userOrg ?: bintrayCfg.user
+                def repoName = packageExtension.packageRepo ?: bintrayCfg.pkg.repo
+                def repoPath = "$org/$repoName"
+                def packageName = rpm.packageName
+                def packagePath = "$repoPath/$packageName"
+                String versionName = bintrayCfg.pkg.version.name ?: project.version.toString()
+                if (versionName.endsWith('-SNAPSHOT')) {
+                    versionName = versionName.replaceAll(/SNAPSHOT/, Long.toString(System.currentTimeMillis()))
+                }
+
+                def setAttributes = { attributesPath, attributes, entity, entityName ->
+                    http.request(POST, JSON) {
+                        uri.path = attributesPath
+                        def builder = new JsonBuilder()
+                        builder.content = attributes.collect {
+                            //Support both arrays and singular values - coerce to an array of values
+                            ['name': it.key, 'values': [it.value].flatten()]
+                        }
+                        body = builder.toString()
+                        response.success = { resp ->
+                            project.logger.info("Attributes set on $entity '$entityName'.")
+                        }
+                        response.failure = { resp, reader ->
+                            throw new GradleException(
+                                    "Could not set attributes on $entity '$entityName': $resp.statusLine $reader")
+                        }
+                    }
+                }
+
+                //create package if needed
+                def createPackageIfNeeded = {
+                    def createPackage = false
+                    http.request(HEAD) {
+                        uri.path = "/packages/$packagePath"
+                        response.success = { resp ->
+                            project.logger.rpmug("Package '$packageName' exists.")
+                        }
+                        response.'404' = { resp ->
+                            project.logger.info("Package '$packageName' does not exist. Attempting to creating it...")
+                            createPackage = true
+                        }
+                    }
+                    if (createPackage) {
+                        if (bintrayCfg.dryRun) {
+                            logger.info("(Dry run) Created pakage '$packagePath'.")
+                            return
+                        }
+                        http.request(POST, JSON) {
+                            uri.path = "/packages/$repoPath"
+                            body = [name                   : packageName, desc: bintrayCfg.pkg.desc, licenses: bintrayCfg.pkg.licenses, labels: bintrayCfg.pkg.labels,
+                                    website_url            : bintrayCfg.pkg.websiteUrl, issue_tracker_url: bintrayCfg.pkg.websiteUrl, vcs_url: bintrayCfg.pkg.vcsUrl,
+                                    public_download_numbers: bintrayCfg.pkg.publicDownloadNumbers]
+
+                            response.success = { resp ->
+                                project.logger.info("Created package '$packagePath'.")
+                            }
+                            response.failure = { resp, reader ->
+                                throw new GradleException("Could not create package '$packagePath': $resp.statusLine $reader")
+                            }
+                        }
+                        if (bintrayCfg.pkg.attributes) {
+                            setAttributes "/packages/$packagePath/attributes", bintrayCfg.pkg.attributes, 'package', packageName
+                        }
+                    }
+                }
+
+                //create version if needed
+                def createVersionIfNeeded = {
+                    def createVersion
+                    http.request(HEAD) {
+                        uri.path = "/packages/$packagePath/versions/$versionName"
+                        response.success = { resp ->
+                            project.logger.rpmug("Version '$packagePath/$versionName' exists.")
+                        }
+                        response.'404' = { resp ->
+                            project.logger.info("Version '$packagePath/$versionName' does not exist. Attempting to creating it...")
+                            createVersion = true
+                        }
+                    }
+                    if (createVersion) {
+                        if (bintrayCfg.dryRun) {
+                            logger.info("(Dry run) Created verion '$packagePath/$versionName'.")
+                            return
+                        }
+                        http.request(POST, JSON) {
+                            uri.path = "/packages/$packagePath/versions"
+                            def versionReleased = Utils.toIsoDateFormat(bintrayCfg.pkg.version.released)
+                            body = [name: versionName, desc: bintrayCfg.pkg.version.desc, released: versionReleased, vcs_tag: bintrayCfg.pkg.version.vcsTag]
+                            response.success = { resp ->
+                                logger.info("Created version '$versionName'.")
+                            }
+                            response.failure = { resp, reader ->
+                                throw new GradleException("Could not create version '$versionName': $resp.statusLine $reader")
+                            }
+                        }
+                        if (bintrayCfg.pkg.version.attributes) {
+                            setAttributes "/packages/$packagePath/versions/$versionName/attributes", bintrayCfg.pkg.version.attributes,
+                                    'version', versionName
+                        }
+                    }
+                }
+
+                createPackageIfNeeded()
+                createVersionIfNeeded()
             }
         }
     }
