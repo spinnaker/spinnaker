ac77afb020bcaa8b0607beb33c7d74564440b70d
Chris Berry <chrisb@netflix.com>
2017-06-16 13:58:28 -0700

    fix(aws): change "deprecated" to "not recommended" on rolling push (#3849)

--- app/scripts/modules/amazon/src/deploymentStrategy/rollingPush.strategy.ts
+++ app/scripts/modules/amazon/src/deploymentStrategy/rollingPush.strategy.ts
@@ -1,8 +1,10 @@
 import { DeploymentStrategyRegistry } from '@spinnaker/core';
 
 DeploymentStrategyRegistry.registerStrategy({
-  label: 'Rolling Push (deprecated)',
-  description: 'Updates the launch configuration for this server group, then terminates instances incrementally, replacing them with instances launched with the updated configuration',
+  label: 'Rolling Push (not recommended)',
+  description: `Updates the launch configuration for this server group, then terminates instances incrementally,
+    replacing them with instances launched with the updated configuration. This is not a best practice - it goes against
+    the principles of immutable infrastructure - but may be necessary in some cases.`,
   key: 'rollingpush',
   providerRestricted: true,
   additionalFields: ['termination.totalRelaunches', 'termination.concurrentRelaunches', 'termination.order', 'termination.relaunchAllInstances'],
