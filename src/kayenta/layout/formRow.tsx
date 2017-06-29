2def5edb2f3b0b9bfea3b34f1e925691109f3557
Scott Vachalek <svachalek@netflix.com>
2017-06-29 14:43:50 -0700

    fix: have columns add to 12

--- src/kayenta/layout/formRow.tsx
+++ src/kayenta/layout/formRow.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function FormRow({ label, children }: { label: string, children?: any }) {
   return (
     <div className="form-group row">
-      <label className="col-sm-2 control-label">
+      <label className="col-sm-3 control-label">
         {label}
       </label>
       <div className="col-sm-9">
