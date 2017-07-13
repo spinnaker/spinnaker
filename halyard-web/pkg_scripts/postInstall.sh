874c38c34fdec915056415a3f5cbd4e46c4237af
Lars Wander <lwander@users.noreply.github.com>
2017-07-13 08:11:44 -0700

    chore(install): Don't create spinnaker in postInstall (#572)

--- halyard-web/pkg_scripts/postInstall.sh
+++ halyard-web/pkg_scripts/postInstall.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-# ubuntu
-# check that owner group exists
-if [ -z `getent group spinnaker` ]; then
-  groupadd spinnaker
-fi
-
-# check that user exists
-if [ -z `getent passwd spinnaker` ]; then
-  useradd --gid spinnaker spinnaker -m --home-dir /home/spinnaker
-fi
-
-
 echo '#!/usr/bin/env bash' | sudo tee /usr/local/bin/hal > /dev/null
 echo '/opt/halyard/bin/hal "$@"' | sudo tee /usr/local/bin/hal > /dev/null
 
