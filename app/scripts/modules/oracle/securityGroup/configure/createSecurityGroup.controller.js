7fc31948737298ac5ed13bf4174b54f0ea070b95
Owain Lewis <owain@owainlewis.com>
2017-07-21 19:53:01 +0100

    Fixes create security group controller notices for Oracle BMCS (#3936)

--- app/scripts/modules/oracle/securityGroup/configure/createSecurityGroup.controller.js
+++ app/scripts/modules/oracle/securityGroup/configure/createSecurityGroup.controller.js
@@ -2,25 +2,10 @@
 
 const angular = require('angular');
 
-import {
-  INFRASTRUCTURE_CACHE_SERVICE,
-  CACHE_INITIALIZER_SERVICE,
-} from '@spinnaker/core';
-
 module.exports = angular.module('spinnaker.oraclebmcs.securityGroup.create.controller', [
-  INFRASTRUCTURE_CACHE_SERVICE,
-  CACHE_INITIALIZER_SERVICE,
-  require('@uirouter/angularjs').default,
-  require('./configSecurityGroup.mixin.controller.js'),
 ])
-  .controller('oraclebmcsCreateSecurityGroupCtrl', function ($scope, $uibModalInstance, $state, $controller,
-                                                             cacheInitializer, infrastructureCaches,
-                                                             application, securityGroup) {
-
-    angular.extend(this, $controller('oraclebmcsConfigSecurityGroupMixin', {
-      $scope: $scope,
-      $uibModalInstance: $uibModalInstance,
-      application: application,
-      securityGroup: securityGroup
-    }));
+  .controller('oraclebmcsCreateSecurityGroupCtrl', function ($scope, $uibModalInstance) {
+    this.cancel = () => {
+      $uibModalInstance.dismiss();
+    };
   });
