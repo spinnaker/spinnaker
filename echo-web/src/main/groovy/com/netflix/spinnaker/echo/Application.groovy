922f709dc07fb48f0af93cfb31f0a6651e2b7cfe
Travis Tomsu <ttomsu@google.com>
2017-08-01 12:58:19 -0400

    chore(springBoot): Upgrade to Spring Boot 1.5.4 (#158)

--- echo-web/src/main/groovy/com/netflix/spinnaker/echo/Application.groovy
+++ echo-web/src/main/groovy/com/netflix/spinnaker/echo/Application.groovy
@@ -18,7 +18,7 @@ package com.netflix.spinnaker.echo
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.boot.context.web.SpringBootServletInitializer
+import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 /**
