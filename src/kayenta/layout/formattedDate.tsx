4b854594bea65e3040134541e363b4c1bd054d4c
Daniel Peach <dpeach@google.com>
2017-08-09 16:11:35 -0400

    feat: adds last modified labels

---
+++ src/kayenta/layout/formattedDate.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import * as moment from 'moment-timezone';
+
+import { SETTINGS } from '@spinnaker/core';
+
+interface IDateLabelProps {
+  dateIso: string;
+  format?: string;
+}
+
+const DEFAULT_FORMAT = 'MM/DD/YY @ HH:mm';
+
+export default function FormattedDate({ dateIso, format }: IDateLabelProps) {
+  if (dateIso) {
+    return <span>{moment.tz(dateIso, SETTINGS.defaultTimeZone).format(format || DEFAULT_FORMAT)}</span>;
+  } else {
+    return <span>N/A</span>;
+  }
+}
+
