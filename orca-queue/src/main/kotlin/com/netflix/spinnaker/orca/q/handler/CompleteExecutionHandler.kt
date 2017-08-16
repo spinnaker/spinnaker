1352840210f8ff1f956ef9a9d7b1facea647dfc7
Adam Jordens <adam@jordens.org>
2017-08-16 15:45:27 -0700

    fix(queue): Stop queuing CompleteExecution after 10 attempts (#1555)

--- orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/CompleteExecutionHandler.kt
+++ orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/handler/CompleteExecutionHandler.kt
@@ -77,11 +77,15 @@ open class CompleteExecutionHandler
         block.invoke(if (execution.shouldOverrideSuccess()) TERMINAL else SUCCEEDED)
       } else {
         val attempts = getAttribute<AttemptsAttribute>()?.attempts ?: 0
-        log.warn("Re-queuing $this as the execution is not yet complete (attempts: $attempts)")
+        if (attempts <= 10) {
+          log.warn("Re-queuing $this as the execution is not yet complete (attempts: $attempts)")
 
-        // no need to re-queue continuously as the RUNNING stages will also emit a `CompleteExecution` message
-        setAttribute(MaxAttemptsAttribute(10))
-        queue.push(this, retryDelay)
+          // no need to re-queue continuously as the RUNNING stages will also emit a `CompleteExecution` message
+          setAttribute(MaxAttemptsAttribute(11))
+          queue.push(this, retryDelay)
+        } else {
+          log.warn("Max attempts reached (10), $this will not be re-queued")
+        }
       }
     }
   }
