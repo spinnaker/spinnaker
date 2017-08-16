35fd7088cebb1b0eca07e8b06a1ead3a38eca439
Chris Berry <chrisb@netflix.com>
2017-08-16 15:52:53 -0700

    chore(core): fix build for styleguide (#4031)

--- app/scripts/modules/core/src/styleguide/src/styles/postcss.config.js
+++ app/scripts/modules/core/src/styleguide/src/styles/postcss.config.js
@@ -6,9 +6,9 @@ module.exports = {
     'postcss-scopeit': {scopeName: 'styleguide'},
     'postcss-style-guide': {
       project: 'Spinnaker',
-      dest: './src/styleguide/public/styleguide.html',
+      dest: './app/scripts/modules/core/src/styleguide/public/styleguide.html',
       showCode: false,
-      themePath: './src/styleguide/src/styleguide-template/'
+      themePath: './app/scripts/modules/core/src/styleguide/src/styleguide-template/'
     },
     'cssnano': {}
   }
