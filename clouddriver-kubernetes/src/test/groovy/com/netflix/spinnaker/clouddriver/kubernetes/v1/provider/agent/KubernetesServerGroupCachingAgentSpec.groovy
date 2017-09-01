0650d1c3793573f1f2b518fcb0c694c5152bd067
Lars Wander <lwander@users.noreply.github.com>
2017-09-01 11:21:07 -0400

    chore(provider/kubernetes): On demand caching tests (#1871)

--- clouddriver-kubernetes/src/test/groovy/com/netflix/spinnaker/clouddriver/kubernetes/v1/provider/agent/KubernetesServerGroupCachingAgentSpec.groovy
+++ clouddriver-kubernetes/src/test/groovy/com/netflix/spinnaker/clouddriver/kubernetes/v1/provider/agent/KubernetesServerGroupCachingAgentSpec.groovy
@@ -19,6 +19,8 @@ package com.netflix.spinnaker.clouddriver.kubernetes.v1.provider.agent
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.netflix.spectator.api.Registry
 import com.netflix.spinnaker.cats.provider.ProviderCache
+import com.netflix.spinnaker.clouddriver.cache.OnDemandAgent
+import com.netflix.spinnaker.clouddriver.kubernetes.KubernetesCloudProvider
 import com.netflix.spinnaker.clouddriver.kubernetes.api.KubernetesApiAdaptor
 import com.netflix.spinnaker.clouddriver.kubernetes.deploy.KubernetesUtil
 import com.netflix.spinnaker.clouddriver.kubernetes.security.KubernetesNamedAccountCredentials
@@ -31,6 +33,7 @@ import io.fabric8.kubernetes.api.model.ReplicationController
 import io.fabric8.kubernetes.api.model.ReplicationControllerList
 import io.fabric8.kubernetes.api.model.ReplicationControllerSpec
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class KubernetesServerGroupCachingAgentSpec extends Specification {
   static final private String NAMESPACE = "default"
@@ -124,5 +127,20 @@ class KubernetesServerGroupCachingAgentSpec extends Specification {
       result.cacheResults.instances.relationships.clusters[0][0] == clusterKey
       result.cacheResults.instances.relationships.applications[0][0] == applicationKey
       result.cacheResults.instances.relationships.serverGroups[0][0] == serverGroupKey
+  }
+
+  @Unroll
+  void "correctly reports #type/#provider is handled by the server group caching agent (#result)"() {
+    expect:
+    cachingAgent.handles(type, provider) == result
+
+    where:
+    type                                     | provider                   || result
+    OnDemandAgent.OnDemandType.ServerGroup   | KubernetesCloudProvider.ID || true
+    OnDemandAgent.OnDemandType.LoadBalancer  | KubernetesCloudProvider.ID || false
+    OnDemandAgent.OnDemandType.SecurityGroup | KubernetesCloudProvider.ID || false
+    OnDemandAgent.OnDemandType.ServerGroup   | "google "                  || false
+    OnDemandAgent.OnDemandType.LoadBalancer  | ""                         || false
+    null                                     | ""                         || false
   }
 }
