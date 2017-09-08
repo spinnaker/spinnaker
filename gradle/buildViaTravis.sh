6aad69c5582464279053bc79fe96bd373732c865
Chris Thielen <christopherthielen@users.noreply.github.com>
2017-09-08 14:40:25 -0700

    fix(quay): Un-break quay build (#4095)

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -4,7 +4,7 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Skipping build (running unit tests only) for Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
   source ~/.nvm/nvm.sh
-  NODE_JS_VERSION=`node -e 'console.log(require("./package.json").engines.node)'`;
+  NODE_JS_VERSION=`node -e 'console.log(require("./package.json").engines.node.replace(/[^\d\.]/g, ""))'`;
   nvm use $NODE_JS_VERSION
 
   ./node_modules/.bin/karma start --single-run
