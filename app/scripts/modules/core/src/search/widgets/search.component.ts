59ed290ab0cb96fe851b1060351f0bef8f9f04d9
Adam Gordon <adam@gordonizer.com>
2017-09-13 21:32:48 -0700

    chore(search): move enabled filters (#4107)
    * basically all cloud providers have the current set of filter types so
    there's no reason to tie it to the aws module.  also, because the
    new page will ultimately be feature flagged due to the necessary
    back-end clouddriver work (i.e., `KeyParser` implementation).

--- app/scripts/modules/core/src/search/widgets/search.component.ts
+++ app/scripts/modules/core/src/search/widgets/search.component.ts
@@ -8,6 +8,8 @@ export const SEARCH_COMPONENT = 'spinnaker.core.search.component';
 module(SEARCH_COMPONENT, [])
   .component('tagSearch', react2angular(Search, ['query', 'onChange']))
   .run(() => {
+    SearchFilterTypeRegistry.register({ key: 'account', modifier: 'acct', text: 'Account' });
+    SearchFilterTypeRegistry.register({ key: 'region', modifier: 'reg', text: 'Region' });
     SearchFilterTypeRegistry.register({ key: 'stack', modifier: 'stack', text: 'Stack' });
     SearchFilterTypeRegistry.register({ key: 'type', modifier: 'type', text: 'Type' });
   });
