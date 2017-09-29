d7068168b02c162a065ab3ea4f66a0d3eb719690
Justin Reynolds <justinreynolds@gmail.com>
2017-09-28 20:52:05 -0700

    fix: Make sure jarDiffs has a default to prevent calling Object.keys on null (#4179)

--- app/scripts/modules/core/src/diffs/viewChangesLink.component.ts
+++ app/scripts/modules/core/src/diffs/viewChangesLink.component.ts
@@ -86,7 +86,7 @@ class ViewChangesLinkController implements IController {
     this.loadingExecution = true;
     this.executionService.getExecution(executionId).then((details: any) => {
       const stage: any = details.stages.find((s: any) => s.id === stageId);
-      this.jarDiffs = stage.context.jarDiffs;
+      this.jarDiffs = stage.context.jarDiffs || {};
       this.commits = stage.context.commits || [];
       extend(this.changeConfig.buildInfo, stage.context.buildInfo);
       this.setJarDiffs();
