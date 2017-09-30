2ecf03506b78a5cb7a300b19228a7d0b593ddcc7
Adam Gordon <adam@gordonizer.com>
2017-09-29 22:37:50 -0700

    fix(search): fix advanced search page title (#4193)
    * just set the title and don't try to get fancy with trying to figure
    out a title based on the search filters.

--- app/scripts/modules/core/src/search/infrastructure/infrastructureV2.controller.ts
+++ app/scripts/modules/core/src/search/infrastructure/infrastructureV2.controller.ts
@@ -43,7 +43,7 @@ export class InfrastructureV2Ctrl implements IController {
               private $state: StateService, private $uibModal: IModalService,
               private infrastructureSearchService: InfrastructureSearchService,
               private cacheInitializer: CacheInitializerService, private overrideRegistry: OverrideRegistry,
-              private pageTitleService: PageTitleService) {
+              pageTitleService: PageTitleService) {
     'ngInject';
     this.$scope.categories = [];
     this.$scope.projects = [];
@@ -51,6 +51,9 @@ export class InfrastructureV2Ctrl implements IController {
     this.viewState = {
       status: SearchStatus.INITIAL
     };
+
+    // just set the page title - don't try to get fancy w/ the search terms
+    pageTitleService.handleRoutingSuccess({ pageTitleMain: { field: undefined, label: 'Infrastructure' }});
 
     this.query = UrlParser.parseLocationHash(window.location.hash);
     if (this.query.length) {
@@ -124,14 +127,6 @@ export class InfrastructureV2Ctrl implements IController {
 
       this.$scope.projects =
         result.filter((category: ISearchResultSet) => category.category === 'Projects' && category.results.length);
-      this.pageTitleService.handleRoutingSuccess(
-        {
-          pageTitleMain: {
-            field: undefined,
-            label: params ? `Search results for "${JSON.stringify(params)}"` : 'Infrastructure'
-          }
-        }
-      );
 
       if (this.$scope.categories.length || this.$scope.projects.length) {
         this.viewState.status = SearchStatus.FINISHED;
