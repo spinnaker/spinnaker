9084eb79c201746e4a9d32c10e135d359e5796bc
Lars Wander <lwander@users.noreply.github.com>
2017-10-06 12:22:07 -0400

    feat(core): Deploy manifest stage & task (#1674)

---
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/clouddriver/tasks/manifest/DeployManifestTask.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.netflix.spinnaker.orca.clouddriver.tasks.manifest;
+
+import com.google.common.collect.ImmutableMap;
+import com.netflix.spinnaker.orca.ExecutionStatus;
+import com.netflix.spinnaker.orca.Task;
+import com.netflix.spinnaker.orca.TaskResult;
+import com.netflix.spinnaker.orca.clouddriver.KatoService;
+import com.netflix.spinnaker.orca.clouddriver.model.TaskId;
+import com.netflix.spinnaker.orca.clouddriver.tasks.AbstractCloudProviderAwareTask;
+import com.netflix.spinnaker.orca.pipeline.model.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Map;
+
+@Component
+public class DeployManifestTask extends AbstractCloudProviderAwareTask implements Task {
+  @Autowired
+  KatoService kato;
+
+  public static final String TASK_NAME = "deployManifest";
+
+  @Nonnull
+  @Override
+  public TaskResult execute(@Nonnull Stage stage) {
+    String credentials = getCredentials(stage);
+    String cloudProvider = getCloudProvider(stage);
+    Map<String, Map> operation = new ImmutableMap.Builder<String, Map>()
+        .put(TASK_NAME, stage.getContext())
+        .build();
+
+    TaskId taskId = kato.requestOperations(cloudProvider, Collections.singletonList(operation)).toBlocking().first();
+
+    Map<String, Object> outputs = new ImmutableMap.Builder<String, Object>()
+        .put("kato.result.expected", true)
+        .put("kato.last.task.id", taskId)
+        .put("deploy.account.name", credentials)
+        .build();
+
+    return new TaskResult(ExecutionStatus.SUCCEEDED, outputs);
+  }
+}
