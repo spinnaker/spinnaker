10620189db7a436fbc082f79b8fc7b7781c163b6
Lars Wander <lwander@users.noreply.github.com>
2017-11-03 10:40:21 -0400

    chore(halconfig): Allow artifacts to be enabled (#4366)

--- halconfig/settings.js
+++ halconfig/settings.js
@@ -9,6 +9,7 @@ var chaosEnabled = '{%features.chaos%}' === 'true';
 var fiatEnabled = '{%features.fiat%}' === 'true';
 var jobsEnabled = '{%features.jobs%}' === 'true';
 var pipelineTemplatesEnabled = '{%features.pipelineTemplates%}' === 'true';
+var artifactsEnabled = '{%features.artifacts%}' === 'true';
 var timezone = '{%timezone%}';
 var version = '{%version%}';
 var changelogGistId = '{%changelog.gist.id%}';
@@ -137,8 +138,9 @@ window.spinnakerSettings = {
     chaosMonkey: chaosEnabled,
     jobs: jobsEnabled,
     pipelineTemplates: pipelineTemplatesEnabled,
-    pipelines: true,
     notifications: notificationsEnabled,
+    artifacts: artifactsEnabled,
+    pipelines: true,
     fastProperty: true,
     vpcMigrator: true,
     clusterDiff: false,
