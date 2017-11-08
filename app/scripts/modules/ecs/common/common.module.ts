89556a1c46923b373847a785173e7686803909b6
Bruno Carrier <brunopierrecarrier@gmail.com>
2017-11-08 12:37:42 -0500

    feat(provider/ecs): Added the ECS common footer (#4405)

---
+++ app/scripts/modules/ecs/common/common.module.ts
@@ -0,0 +1,8 @@
+import { module } from 'angular';
+
+import { ECS_FOOTER_COMPONENT } from './footer.component';
+
+export const COMMON_MODULE = 'spinnaker.ecs.common';
+module(COMMON_MODULE, [
+  ECS_FOOTER_COMPONENT,
+]);
