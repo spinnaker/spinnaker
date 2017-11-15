fd185923d223e3fc06611bb55e315a56ca78ca96
Bruno Carrier <brunopierrecarrier@gmail.com>
2017-11-15 10:04:24 -0500

    feat(provider/ecs): Added ShrinkCluster pipeline stage (#4403)
    * feat(provider/ecs): Added ShrinkCluster pipeline stage
    
    * Removed executionDetailsUrl property
    
    * Removed unused import

---
+++ app/scripts/modules/ecs/pipeline/stages/shrinkCluster/ecsShrinkClusterStage.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const angular = require('angular');
+
+import { PIPELINE_CONFIG_PROVIDER } from '@spinnaker/core';
+
+module.exports = angular.module('spinnaker.ecs.pipeline.stage.ecs.shrinkClusterStage', [
+  PIPELINE_CONFIG_PROVIDER,
+])
+  .config(function(pipelineConfigProvider) {
+    pipelineConfigProvider.registerStage({
+      provides: 'shrinkCluster',
+      cloudProvider: 'ecs',
+      templateUrl: require('./shrinkClusterStage.html'),
+      accountExtractor: (stage) => [stage.context.credentials],
+      configAccountExtractor: (stage) => [stage.credentials],
+      validators: [
+        { type: 'requiredField', fieldName: 'cluster' },
+        { type: 'requiredField', fieldName: 'shrinkToSize', fieldLabel: 'shrink to [X] Server Groups'},
+        { type: 'requiredField', fieldName: 'regions', },
+        { type: 'requiredField', fieldName: 'credentials', fieldLabel: 'account'},
+      ],
+    });
+  }).controller('ecsShrinkClusterStageCtrl', function($scope, accountService) {
+    var ctrl = this;
+
+    let stage = $scope.stage;
+
+    $scope.state = {
+      accounts: false,
+      regionsLoaded: false
+    };
+
+    accountService.listAccounts('ecs').then(function (accounts) {
+      $scope.accounts = accounts;
+      $scope.state.accounts = true;
+    });
+
+    stage.regions = stage.regions || [];
+    stage.cloudProvider = 'ecs';
+
+    if (!stage.credentials && $scope.application.defaultCredentials.ecs) {
+      stage.credentials = $scope.application.defaultCredentials.ecs;
+    }
+    if (!stage.regions.length && $scope.application.defaultRegions.ecs) {
+      stage.regions.push($scope.application.defaultRegions.ecs);
+    }
+
+    if (stage.shrinkToSize === undefined) {
+      stage.shrinkToSize = 1;
+    }
+
+    if (stage.allowDeleteActive === undefined) {
+      stage.allowDeleteActive = false;
+    }
+
+    ctrl.pluralize = function(str, val) {
+      if (val === 1) {
+        return str;
+      }
+      return str + 's';
+    };
+
+    if (stage.retainLargerOverNewer === undefined) {
+      stage.retainLargerOverNewer = 'false';
+    }
+    stage.retainLargerOverNewer = stage.retainLargerOverNewer.toString();
+  });
+
