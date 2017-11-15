3ad91c3b7e54882cb15d9a1ff863eba2cac10662
Bruno Carrier <brunopierrecarrier@gmail.com>
2017-11-15 09:53:15 -0500

    feat(provider/ecs): Added EnableAsg stage (#4400)
    * feat(provider/ecs): Added EnableAsg stage
    
    * removed executionDetailsUrl property
    
    * removed unused import

---
+++ app/scripts/modules/ecs/pipeline/stages/enableAsg/ecsEnableAsgStage.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const angular = require('angular');
+
+import { StageConstants } from '@spinnaker/core';
+
+module.exports = angular.module('spinnaker.ecs.pipeline.stage.enableAsgStage', [
+])
+  .config(function(pipelineConfigProvider) {
+    pipelineConfigProvider.registerStage({
+      provides: 'enableServerGroup',
+      alias: 'enableAsg',
+      cloudProvider: 'ecs',
+      templateUrl: require('./enableAsgStage.html'),
+      executionStepLabelUrl: require('./enableAsgStepLabel.html'),
+      validators: [
+        { type: 'requiredField', fieldName: 'cluster' },
+        { type: 'requiredField', fieldName: 'target', },
+        { type: 'requiredField', fieldName: 'regions', },
+        { type: 'requiredField', fieldName: 'credentials', fieldLabel: 'account'},
+      ],
+    });
+  }).controller('ecsEnableAsgStageCtrl', function($scope, accountService) {
+    var ctrl = this;
+
+    let stage = $scope.stage;
+
+    $scope.state = {
+      accounts: false,
+      regionsLoaded: false
+    };
+
+    accountService.listAccounts('ecs').then(function (accounts) {
+      $scope.accounts = accounts;
+      $scope.state.accounts = true;
+    });
+
+    ctrl.reset = () => {
+      ctrl.accountUpdated();
+      ctrl.resetSelectedCluster();
+    };
+
+    $scope.targets = StageConstants.TARGET_LIST;
+
+    stage.regions = stage.regions || [];
+    stage.cloudProvider = 'ecs';
+
+    if (stage.isNew && $scope.application.attributes.platformHealthOnlyShowOverride && $scope.application.attributes.platformHealthOnly) {
+      stage.interestingHealthProviderNames = ['Ecs'];
+    }
+
+    if (!stage.credentials && $scope.application.defaultCredentials.ecs) {
+      stage.credentials = $scope.application.defaultCredentials.ecs;
+    }
+    if (!stage.regions.length && $scope.application.defaultRegions.ecs) {
+      stage.regions.push($scope.application.defaultRegions.ecs);
+    }
+
+    if (!stage.target) {
+      stage.target = $scope.targets[0].val;
+    }
+
+    $scope.$watch('stage.credentials', $scope.accountUpdated);
+  });
+
