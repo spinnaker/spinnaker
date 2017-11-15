9da2c540ca9bf2d45e9e8ca9ccf69f3d4bbf85b2
Bruno Carrier <brunopierrecarrier@gmail.com>
2017-11-15 09:59:56 -0500

    feat(provider/ecs): Added ScaleDownCluster pipeline stage (#4402)
    * feat(provider/ecs): Added ScaleDownCluster pipeline stage
    
    * Corrected an error with executionDetailsTemplate
    
    * Removed unused code

---
+++ app/scripts/modules/ecs/pipeline/stages/scaleDownCluster/ecsScaleDownClusterStage.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const angular = require('angular');
+
+module.exports = angular.module('spinnaker.ecs.pipeline.stage.scaleDownClusterStage', [
+])
+  .config(function(pipelineConfigProvider) {
+    pipelineConfigProvider.registerStage({
+      provides: 'scaleDownCluster',
+      cloudProvider: 'ecs',
+      templateUrl: require('./scaleDownClusterStage.html'),
+      accountExtractor: (stage) => [stage.context.credentials],
+      configAccountExtractor: (stage) => [stage.credentials],
+      validators: [
+        { type: 'requiredField', fieldName: 'cluster' },
+        { type: 'requiredField', fieldName: 'remainingFullSizeServerGroups', fieldLabel: 'Keep [X] full size Server Groups'},
+        { type: 'requiredField', fieldName: 'regions', },
+        { type: 'requiredField', fieldName: 'credentials', fieldLabel: 'account'},
+      ],
+      strategy: true,
+    });
+  }).controller('ecsScaleDownClusterStageCtrl', function($scope, accountService) {
+    var ctrl = this;
+
+    let stage = $scope.stage;
+
+    $scope.state = {
+      accounts: false,
+      regionsLoaded: false
+    };
+
+    accountService.listAccounts('ecs').then(function (accounts) {
+      $scope.accounts = accounts;
+      $scope.state.accounts = true;
+    });
+
+    stage.regions = stage.regions || [];
+    stage.cloudProvider = 'ecs';
+
+    if (!stage.credentials && $scope.application.defaultCredentials.ecs) {
+      stage.credentials = $scope.application.defaultCredentials.ecs;
+    }
+    if (!stage.regions.length && $scope.application.defaultRegions.ecs) {
+      stage.regions.push($scope.application.defaultRegions.ecs);
+    }
+
+    if (stage.remainingFullSizeServerGroups === undefined) {
+      stage.remainingFullSizeServerGroups = 1;
+    }
+
+    if (stage.allowScaleDownActive === undefined) {
+      stage.allowScaleDownActive = false;
+    }
+
+    ctrl.pluralize = function(str, val) {
+      if (val === 1) {
+        return str;
+      }
+      return str + 's';
+    };
+
+    if (stage.preferLargerOverNewer === undefined) {
+      stage.preferLargerOverNewer = 'false';
+    }
+    stage.preferLargerOverNewer = stage.preferLargerOverNewer.toString();
+  });
+
