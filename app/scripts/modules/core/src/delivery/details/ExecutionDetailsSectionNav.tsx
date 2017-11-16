acb98bcf9aa64f8ceaf4018f5d4259a6f6d10ef8
Justin Reynolds <justinreynolds@gmail.com>
2017-11-16 12:23:32 -0800

    fix(core/delivery): Fix jumping to same type stage when clicking details sections (#4466)

--- app/scripts/modules/core/src/delivery/details/ExecutionDetailsSectionNav.tsx
+++ app/scripts/modules/core/src/delivery/details/ExecutionDetailsSectionNav.tsx
@@ -1,36 +1,61 @@
 import * as React from 'react';
 import * as ReactGA from 'react-ga';
-import { UISref, UISrefActive } from '@uirouter/react';
-import { UIRouterContext } from '@uirouter/react-hybrid';
+import { Subscription } from 'rxjs/Subscription';
 
+import { ReactInjector } from 'core/reactShims';
 import { robotToHuman } from 'core/presentation/robotToHumanFilter/robotToHuman.filter';
 
 export interface IExecutionDetailsSectionNavProps {
   sections: string[];
 }
 
-@UIRouterContext
-export class ExecutionDetailsSectionNav extends React.Component<IExecutionDetailsSectionNavProps> {
+export interface IExecutionDetailsSectionNavState {
+  activeSection: string;
+}
+
+export class ExecutionDetailsSectionNav extends React.Component<IExecutionDetailsSectionNavProps, IExecutionDetailsSectionNavState> {
+  private stateChangeSuccessSubscription: Subscription;
+
+  public constructor(props: IExecutionDetailsSectionNavProps) {
+    super(props);
+    this.state = this.getState(props);
+  }
+
+  public componentDidMount(): void {
+    this.stateChangeSuccessSubscription = ReactInjector.stateEvents.stateChangeSuccess.subscribe(() => this.setState(this.getState(this.props)));
+  }
+
+  private getState(props: IExecutionDetailsSectionNavProps): IExecutionDetailsSectionNavState {
+    const { $stateParams } = ReactInjector;
+    const activeSection = $stateParams.details || props.sections[0];
+    return { activeSection };
+  }
+
+  public componentWillReceiveProps(nextProps: IExecutionDetailsSectionNavProps): void {
+    this.setState(this.getState(nextProps));
+  }
+
+  public componentWillUnmount(): void {
+    this.stateChangeSuccessSubscription.unsubscribe();
+  }
+
   public render() {
     return (
       <ul className="nav nav-pills">
-        {this.props.sections.map((section) => <Section key={section} section={section}/>)}
+        {this.props.sections.map((section) => <Section key={section} section={section} active={this.state.activeSection === section}/>)}
       </ul>
     );
   }
 }
 
-const Section = (props: { section: string }): JSX.Element => {
+const Section = (props: { section: string, active: boolean }): JSX.Element => {
   const clicked = () => {
     ReactGA.event({ category: 'Pipeline', action: 'Execution details section selected', label: props.section });
+    ReactInjector.$state.go('.', { details: props.section });
   }
   return (
     <li>
-      <UISrefActive class="active">
-        <UISref to=".execution" params={{ details: props.section }}>
-          <a onClick={clicked}>{robotToHuman(props.section)}</a>
-        </UISref>
-      </UISrefActive>
+      <a className={`clickable ${props.active ? 'active' : ''}`} onClick={clicked}>{robotToHuman(props.section)}</a>
     </li>
   );
 }
