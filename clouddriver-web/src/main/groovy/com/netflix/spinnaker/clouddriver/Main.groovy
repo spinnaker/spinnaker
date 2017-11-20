9ec67e5e52d7d4fb019b8a56747c4107974839b9
Lars Wander <lwander@users.noreply.github.com>
2017-11-20 18:45:11 -0500

    refactor(cf): Stop building CF provider until maintainer is found (#1942)
    BREAKING CHANGE: Stops support of CF until maintainer is found

--- clouddriver-web/src/main/groovy/com/netflix/spinnaker/clouddriver/Main.groovy
+++ clouddriver-web/src/main/groovy/com/netflix/spinnaker/clouddriver/Main.groovy
@@ -19,7 +19,6 @@ package com.netflix.spinnaker.clouddriver
 import com.netflix.spinnaker.clouddriver.appengine.AppengineConfiguration
 import com.netflix.spinnaker.clouddriver.aws.AwsConfiguration
 import com.netflix.spinnaker.clouddriver.azure.AzureConfiguration
-import com.netflix.spinnaker.clouddriver.cf.config.CloudFoundryConfig
 import com.netflix.spinnaker.clouddriver.core.CloudDriverConfig
 import com.netflix.spinnaker.clouddriver.core.RetrofitConfig
 import com.netflix.spinnaker.clouddriver.dcos.DcosConfiguration
@@ -63,7 +62,6 @@ import java.security.Security
   KubernetesConfiguration,
   OpenstackConfiguration,
   DockerRegistryConfiguration,
-  CloudFoundryConfig,
   AzureConfiguration,
   SecurityConfig,
   StorageConfiguration,
