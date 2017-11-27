a1c7309151c8b1fffa7a292ecec21df01eed0c39
Chris Berry <chrisb@netflix.com>
2017-11-27 10:31:08 -0800

    fix(amazon): provide better error message when bake stage fails (#4519)

--- app/scripts/modules/amazon/src/pipeline/stages/bake/bakeExecutionDetails.controller.js
+++ app/scripts/modules/amazon/src/pipeline/stages/bake/bakeExecutionDetails.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const angular = require('angular');
+import { get } from 'lodash';
 
 import { SETTINGS } from '@spinnaker/core';
 
@@ -17,6 +18,7 @@ module.exports = angular.module('spinnaker.amazon.pipeline.stage.bake.executionD
       $scope.provider = $scope.stage.context.cloudProviderType || 'aws';
       $scope.roscoMode = SETTINGS.feature.roscoMode;
       $scope.bakeryDetailUrl = $interpolate(SETTINGS.bakeryDetailUrl);
+      $scope.bakeFailedNoError = get($scope.stage, 'context.status.result') === 'FAILURE' && !$scope.stage.failureMessage;
     };
 
     let initialize = () => executionDetailsSectionService.synchronizeSection($scope.configSections, initialized);
