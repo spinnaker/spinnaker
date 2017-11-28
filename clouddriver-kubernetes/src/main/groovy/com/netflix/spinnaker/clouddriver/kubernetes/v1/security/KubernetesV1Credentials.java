c130831158e2f1ff2689c19f4ed88f92ccf8c933
Ethan Rogers <ethanfrogers@users.noreply.github.com>
2017-11-28 15:55:49 -0800

    fix(provider/kubernetes): fix registry init 2 (#2191)
    the previous fix caused sever performance issues. this fix will only
    configure hard coded registries once, only reconfiguring failed
    registries on the next loop, if any.

--- clouddriver-kubernetes/src/main/groovy/com/netflix/spinnaker/clouddriver/kubernetes/v1/security/KubernetesV1Credentials.java
+++ clouddriver-kubernetes/src/main/groovy/com/netflix/spinnaker/clouddriver/kubernetes/v1/security/KubernetesV1Credentials.java
@@ -131,8 +131,11 @@ public class KubernetesV1Credentials implements KubernetesCredentials {
 
   public List<String> getDeclaredNamespaces() {
     if (namespaces != null && !namespaces.isEmpty()) {
+      List<String> toInitialize = new ArrayList<>(namespaces);
+      List<String> initializedNamespaces = new ArrayList<>(imagePullSecrets.keySet());
+      toInitialize.removeAll(initializedNamespaces);
+      reconfigureRegistries(toInitialize);
       // If namespaces are provided, used them
-      reconfigureRegistries(namespaces);
       return namespaces;
     } else {
       try {
