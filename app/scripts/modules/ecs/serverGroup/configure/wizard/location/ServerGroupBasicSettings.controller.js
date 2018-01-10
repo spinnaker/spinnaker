17d9d52b0793d36dad26b3316f0045ab3a423ae7
Bruno Carrier <brunopierrecarrier@gmail.com>
2018-01-10 13:32:31 -0500

    feat(provider/ecs): Added Location section to deploy modal (#4636)

---
+++ app/scripts/modules/ecs/serverGroup/configure/wizard/location/ServerGroupBasicSettings.controller.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const angular = require('angular');
+
+import { NAMING_SERVICE, V2_MODAL_WIZARD_SERVICE } from '@spinnaker/core';
+
+module.exports = angular.module('spinnaker.ecs.serverGroup.configure.basicSettings', [
+  require('@uirouter/angularjs').default,
+  require('angular-ui-bootstrap'),
+  V2_MODAL_WIZARD_SERVICE,
+  NAMING_SERVICE,
+])
+  .controller('ecsServerGroupBasicSettingsCtrl', function($scope,
+                                                          $controller,
+                                                          $uibModalStack,
+                                                          $state,
+                                                          v2modalWizardService,
+                                                          namingService
+  ) {
+
+    angular.extend(this, $controller('BasicSettingsMixin', {
+      $scope: $scope,
+      namingService: namingService,
+      $uibModalStack: $uibModalStack,
+      $state: $state,
+    }));
+
+    $scope.$watch('form.$valid', function(newVal) {
+      if (newVal) {
+        v2modalWizardService.markClean('location');
+        v2modalWizardService.markComplete('location');
+      } else {
+        v2modalWizardService.markIncomplete('location');
+      }
+    });
+
+  });
