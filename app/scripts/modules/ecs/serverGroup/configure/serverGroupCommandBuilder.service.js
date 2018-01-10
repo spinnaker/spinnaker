f08a47085209bb983d2113b04c508e1432dbef64
Bruno Carrier <brunopierrecarrier@gmail.com>
2018-01-10 15:03:51 -0500

    feat(provider/ecs): Added the ServerGroup Configuration Service (#4654)

---
+++ app/scripts/modules/ecs/serverGroup/configure/serverGroupCommandBuilder.service.js
@@ -0,0 +1,233 @@
+'use strict';
+
+const angular = require('angular');
+import _ from 'lodash';
+
+import { ACCOUNT_SERVICE, INSTANCE_TYPE_SERVICE, NAMING_SERVICE } from '@spinnaker/core';
+
+import { ECS_SERVER_GROUP_CONFIGURATION_SERVICE } from './serverGroupConfiguration.service';
+
+module.exports = angular.module('spinnaker.ecs.serverGroupCommandBuilder.service', [
+  ACCOUNT_SERVICE,
+  INSTANCE_TYPE_SERVICE,
+  NAMING_SERVICE,
+  ECS_SERVER_GROUP_CONFIGURATION_SERVICE,
+])
+  .factory('ecsServerGroupCommandBuilder', function ($q,
+                                                     accountService,
+                                                     namingService,
+                                                     instanceTypeService,
+                                                     ecsServerGroupConfigurationService) {
+
+    const CLOUD_PROVIDER = 'ecs';
+
+    function buildNewServerGroupCommand (application, defaults) {
+      defaults = defaults || {};
+      var credentialsLoader = accountService.getCredentialsKeyedByAccount('ecs');
+
+      var defaultCredentials = defaults.account || application.defaultCredentials.ecs;
+      var defaultRegion = defaults.region || application.defaultRegions.ecs;
+
+      var preferredZonesLoader = accountService.getAvailabilityZonesForAccountAndRegion('ecs', defaultCredentials, defaultRegion);
+
+
+      return $q.all({
+        preferredZones: preferredZonesLoader,
+        credentialsKeyedByAccount: credentialsLoader,
+      })
+        .then(function (asyncData) {
+          var availabilityZones = asyncData.preferredZones;
+
+          var credentials = asyncData.credentialsKeyedByAccount[defaultCredentials];
+
+          var defaultIamRole = 'None (No IAM role)';
+          defaultIamRole = defaultIamRole.replace('{{application}}', application.name);
+
+
+          var command = {
+            application: application.name,
+            credentials: defaultCredentials,
+            region: defaultRegion,
+            strategy: '',
+            capacity: {
+              min: 1,
+              max: 1,
+              desired: 1
+            },
+            healthCheckType: 'EC2',
+            selectedProvider: 'ecs',
+            iamRole: defaultIamRole,
+            availabilityZones: availabilityZones,
+            autoscalingPolicies: [],
+            placementStrategyName: '',
+            placementStrategySequence: [],
+            ecsClusterName: '',
+            targetGroup: '',
+            viewState: {
+              useAllImageSelection: false,
+              useSimpleCapacity: true,
+              usePreferredZones: true,
+              mode: defaults.mode || 'create',
+              disableStrategySelection: true,
+              dirty: {},
+            },
+          };
+
+          if (application.attributes && application.attributes.platformHealthOnlyShowOverride && application.attributes.platformHealthOnly) {
+            command.interestingHealthProviderNames = ['ecs'];
+          }
+
+          return command;
+        });
+    }
+
+    function buildServerGroupCommandFromPipeline(application, originalCluster) {
+
+      var pipelineCluster = _.cloneDeep(originalCluster);
+      var region = Object.keys(pipelineCluster.availabilityZones)[0];
+      // var instanceTypeCategoryLoader = instanceTypeService.getCategoryForInstanceType('ecs', pipelineCluster.instanceType);
+      var commandOptions = { account: pipelineCluster.account, region: region };
+      var asyncLoader = $q.all({command: buildNewServerGroupCommand(application, commandOptions)});
+
+      return asyncLoader.then(function(asyncData) {
+        var command = asyncData.command;
+        var zones = pipelineCluster.availabilityZones[region];
+        var usePreferredZones = zones.join(',') === command.availabilityZones.join(',');
+
+        var viewState = {
+          instanceProfile: asyncData.instanceProfile,
+          disableImageSelection: true,
+          useSimpleCapacity: pipelineCluster.capacity.min === pipelineCluster.capacity.max && pipelineCluster.useSourceCapacity !== true,
+          usePreferredZones: usePreferredZones,
+          mode: 'editPipeline',
+          submitButtonLabel: 'Done',
+          templatingEnabled: true,
+          existingPipelineCluster: true,
+          dirty: {},
+        };
+
+        var viewOverrides = {
+          region: region,
+          credentials: pipelineCluster.account,
+          availabilityZones: pipelineCluster.availabilityZones[region],
+          viewState: viewState,
+        };
+
+        pipelineCluster.strategy = pipelineCluster.strategy || '';
+
+        return angular.extend({}, command, pipelineCluster, viewOverrides);
+      });
+
+    }
+
+    // Only used to prepare view requiring template selecting
+    function buildNewServerGroupCommandForPipeline() {
+      return $q.when({
+        viewState: {
+          requiresTemplateSelection: true,
+        }
+      });
+    }
+
+    function buildUpdateServerGroupCommand(serverGroup) {
+      var command = {
+        type: 'modifyAsg',
+        asgs: [
+          { asgName: serverGroup.name, region: serverGroup.region }
+        ],
+        healthCheckType: serverGroup.asg.healthCheckType,
+        credentials: serverGroup.account
+      };
+      ecsServerGroupConfigurationService.configureUpdateCommand(command);
+      return command;
+    }
+
+    function buildServerGroupCommandFromExisting(application, serverGroup, mode = 'clone') {
+      var preferredZonesLoader = accountService.getPreferredZonesByAccount('ecs');
+
+      var serverGroupName = namingService.parseServerGroupName(serverGroup.asg.autoScalingGroupName);
+
+      var asyncLoader = $q.all({
+        preferredZones: preferredZonesLoader,
+      });
+
+      return asyncLoader.then(function(asyncData) {
+        var zones = serverGroup.asg.availabilityZones.sort();
+        var usePreferredZones = false;
+        var preferredZonesForAccount = asyncData.preferredZones[serverGroup.account];
+        if (preferredZonesForAccount) {
+          var preferredZones = preferredZonesForAccount[serverGroup.region].sort();
+          usePreferredZones = zones.join(',') === preferredZones.join(',');
+        }
+
+        // These processes should never be copied over, as the affect launching instances and enabling traffic
+        let enabledProcesses = ['Launch', 'Terminate', 'AddToLoadBalancer'];
+
+        var command = {
+          application: application.name,
+          strategy: '',
+          stack: serverGroupName.stack,
+          freeFormDetails: serverGroupName.freeFormDetails,
+          credentials: serverGroup.account,
+          healthCheckType: serverGroup.asg.healthCheckType,
+          loadBalancers: serverGroup.asg.loadBalancerNames,
+          region: serverGroup.region,
+          useSourceCapacity: false,
+          capacity: {
+            'min': serverGroup.asg.minSize,
+            'max': serverGroup.asg.maxSize,
+            'desired': serverGroup.asg.desiredCapacity
+          },
+          availabilityZones: zones,
+          selectedProvider: CLOUD_PROVIDER,
+          source: {
+            account: serverGroup.account,
+            region: serverGroup.region,
+            asgName: serverGroup.asg.autoScalingGroupName,
+          },
+          suspendedProcesses: (serverGroup.asg.suspendedProcesses || [])
+            .map((process) => process.processName)
+            .filter((name) => !enabledProcesses.includes(name)),
+          targetGroup: serverGroup.targetGroup,
+          viewState: {
+            instanceProfile: asyncData.instanceProfile,
+            useAllImageSelection: false,
+            useSimpleCapacity: serverGroup.asg.minSize === serverGroup.asg.maxSize,
+            usePreferredZones: usePreferredZones,
+            mode: mode,
+            isNew: false,
+            dirty: {},
+          },
+        };
+
+        if (mode === 'clone' || mode === 'editPipeline') {
+          command.useSourceCapacity = true;
+        }
+
+        if (mode === 'editPipeline') {
+          command.strategy = 'redblack';
+          command.suspendedProcesses = [];
+        }
+
+        if (serverGroup.launchConfig) {
+          angular.extend(command, {
+            iamRole: serverGroup.launchConfig.iamInstanceProfile,
+          });
+          if (serverGroup.launchConfig.userData) {
+            command.base64UserData = serverGroup.launchConfig.userData;
+          }
+          command.viewState.imageId = serverGroup.launchConfig.imageId;
+        }
+
+        return command;
+      });
+    }
+
+    return {
+      buildNewServerGroupCommand: buildNewServerGroupCommand,
+      buildServerGroupCommandFromExisting: buildServerGroupCommandFromExisting,
+      buildNewServerGroupCommandForPipeline: buildNewServerGroupCommandForPipeline,
+      buildServerGroupCommandFromPipeline: buildServerGroupCommandFromPipeline,
+      buildUpdateServerGroupCommand: buildUpdateServerGroupCommand,
+    };
+});
---
+++ app/scripts/modules/ecs/serverGroup/configure/serverGroupConfiguration.service.ts
@@ -0,0 +1,401 @@
+import { module, IPromise, IQService } from 'angular';
+import { chain, extend, find, flatten, has, intersection, keys, some, xor } from 'lodash';
+
+import {
+  ACCOUNT_SERVICE,
+  AccountService,
+  Application,
+  CACHE_INITIALIZER_SERVICE,
+  CacheInitializerService,
+  IAccountDetails,
+  IDeploymentStrategy,
+  IRegion,
+  IServerGroupCommand,
+  IServerGroupCommandBackingData,
+  IServerGroupCommandBackingDataFiltered,
+  IServerGroupCommandDirty,
+  IServerGroupCommandResult,
+  ISubnet,
+  LOAD_BALANCER_READ_SERVICE,
+  LoadBalancerReader,
+  SERVER_GROUP_COMMAND_REGISTRY_PROVIDER,
+  ServerGroupCommandRegistry,
+  SUBNET_READ_SERVICE,
+  SubnetReader,
+} from '@spinnaker/core';
+
+import { IAmazonLoadBalancer } from 'amazon/domain';
+import { IamRoleReader } from '../../iamRoles/iamRole.read.service';
+import { EscClusterReader } from '../../ecsCluster/ecsCluster.read.service';
+import { MetricAlarmReader } from '../../metricAlarm/metricAlarm.read.service';
+import { IRoleDescriptor } from '../../iamRoles/IRole';
+import { MetricAlarmDescriptor } from '../../metricAlarm/MetricAlarm';
+import { PlacementStrategyService } from '../../placementStrategy/placementStrategy.service';
+import { PlacementStrategy } from '../../placementStrategy/PlacementStrategy';
+import { EcsClusterDescriptor } from '../../ecsCluster/EcsCluster';
+
+export interface IEcsServerGroupCommandDirty extends IServerGroupCommandDirty {
+  targetGroup?: string;
+}
+
+export interface IEcsServerGroupCommandResult extends IServerGroupCommandResult {
+  dirty: IEcsServerGroupCommandDirty;
+}
+
+export interface IEcsServerGroupCommandBackingDataFiltered extends IServerGroupCommandBackingDataFiltered {
+  targetGroups: string[];
+  iamRoles: string[];
+  ecsClusters: string[];
+  metricAlarms: MetricAlarmDescriptor[];
+}
+
+export interface IEcsServerGroupCommandBackingData extends IServerGroupCommandBackingData {
+  filtered: IEcsServerGroupCommandBackingDataFiltered;
+  targetGroups: string[];
+  ecsClusters: EcsClusterDescriptor[];
+  iamRoles: IRoleDescriptor[];
+  metricAlarms: MetricAlarmDescriptor[];
+}
+
+export interface IEcsServerGroupCommand extends IServerGroupCommand {
+  backingData: IEcsServerGroupCommandBackingData;
+  targetHealthyDeployPercentage: number;
+  targetGroup: string;
+  placementStrategyName: string;
+  placementStrategySequence: PlacementStrategy[];
+
+  placementStrategyNameChanged: () => IServerGroupCommandResult;
+}
+
+export class EcsServerGroupConfigurationService {
+  // private enabledMetrics = ['GroupMinSize', 'GroupMaxSize', 'GroupDesiredCapacity', 'GroupInServiceInstances', 'GroupPendingInstances', 'GroupStandbyInstances', 'GroupTerminatingInstances', 'GroupTotalInstances'];
+  // private healthCheckTypes = ['EC2', 'ELB'];
+  // private terminationPolicies = ['OldestInstance', 'NewestInstance', 'OldestLaunchConfiguration', 'ClosestToNextInstanceHour', 'Default'];
+
+  constructor(private $q: IQService,
+              private accountService: AccountService,
+              private cacheInitializer: CacheInitializerService,
+              private subnetReader: SubnetReader,
+              private loadBalancerReader: LoadBalancerReader,
+              private serverGroupCommandRegistry: ServerGroupCommandRegistry,
+              private iamRoleReader: IamRoleReader,
+              private ecsClusterReader: EscClusterReader,
+              private metricAlarmReader: MetricAlarmReader,
+              private placementStrategyService: PlacementStrategyService,
+              ) {
+    'ngInject';
+  }
+
+  public configureUpdateCommand(command: IEcsServerGroupCommand): void {
+    command.backingData = {
+      // terminationPolicies: clone(this.terminationPolicies)
+    } as IEcsServerGroupCommandBackingData;
+  }
+
+  // TODO (Bruno Carrier): Why do we need to inject an Application into this constructor so that the app works?  This is strange, and needs investigating
+  public configureCommand(application: Application, command: IEcsServerGroupCommand): IPromise<void> {
+    this.applyOverrides('beforeConfiguration', command);
+    // The bellow console.log was added to suppress the `TS6133: 'application' is declared but its value is never read.` error.
+    console.log(typeof(application));
+
+    command.toggleSuspendedProcess = (process: string): void => {
+      command.suspendedProcesses = command.suspendedProcesses || [];
+      const processIndex = command.suspendedProcesses.indexOf(process);
+      if (processIndex === -1) {
+        command.suspendedProcesses.push(process);
+      } else {
+        command.suspendedProcesses.splice(processIndex, 1);
+      }
+    };
+
+    command.processIsSuspended = (process: string): boolean => command.suspendedProcesses.includes(process);
+
+    command.onStrategyChange = (strategy: IDeploymentStrategy): void => {
+      // Any strategy other than None or Custom should force traffic to be enabled
+      if (strategy.key !== '' && strategy.key !== 'custom') {
+        command.suspendedProcesses = (command.suspendedProcesses || []).filter(p => p !== 'AddToLoadBalancer');
+      }
+    };
+
+    command.regionIsDeprecated = (): boolean => {
+      return has(command, 'backingData.filtered.regions') &&
+        command.backingData.filtered.regions.some((region) => region.name === command.region && region.deprecated);
+    };
+
+    return this.$q.all({
+      credentialsKeyedByAccount: this.accountService.getCredentialsKeyedByAccount('ecs'),
+      loadBalancers: this.loadBalancerReader.listLoadBalancers('ecs'),
+      subnets: this.subnetReader.listSubnets(),
+      iamRoles: this.iamRoleReader.listRoles('ecs'),
+      ecsClusters: this.ecsClusterReader.listClusters(),
+      metricAlarms: this.metricAlarmReader.listMetricAlarms(),
+    }).then((backingData: Partial<IEcsServerGroupCommandBackingData>) => {
+      let loadBalancerReloader = this.$q.when(null);
+      backingData.accounts = keys(backingData.credentialsKeyedByAccount);
+      backingData.filtered = {} as IEcsServerGroupCommandBackingDataFiltered;
+      command.backingData = backingData as IEcsServerGroupCommandBackingData;
+      this.configureVpcId(command);
+      this.configureAvailableIamRoles(command);
+      this.configureAvailableMetricAlarms(command);
+      this.configureAvailableEcsClusters(command);
+
+      if (command.loadBalancers && command.loadBalancers.length) {
+        // verify all load balancers are accounted for; otherwise, try refreshing load balancers cache
+        const loadBalancerNames = this.getLoadBalancerNames(command);
+        if (intersection(loadBalancerNames, command.loadBalancers).length < command.loadBalancers.length) {
+          loadBalancerReloader = this.refreshLoadBalancers(command, true);
+        }
+      }
+
+      return this.$q.all([loadBalancerReloader]).then(() => {
+        this.applyOverrides('afterConfiguration', command);
+        this.attachEventHandlers(command);
+      });
+    });
+  }
+
+  public applyOverrides(phase: string, command: IEcsServerGroupCommand): void {
+    this.serverGroupCommandRegistry.getCommandOverrides('ecs').forEach((override: any) => {
+      if (override[phase]) {
+        override[phase](command);
+      }
+    });
+  }
+
+
+  public configureAvailabilityZones(command: IEcsServerGroupCommand): void {
+    command.backingData.filtered.availabilityZones =
+      find<IRegion>(command.backingData.credentialsKeyedByAccount[command.credentials].regions, { name: command.region }).availabilityZones;
+  }
+
+  public configureAvailableMetricAlarms(command: IEcsServerGroupCommand): void {
+    // const previouslyFiltered = command.backingData.filtered.metricAlarms;
+    command.backingData.filtered.metricAlarms = chain(command.backingData.metricAlarms)
+      .filter({
+        accountName: command.credentials,
+        region: command.region
+      })
+      .map(metricAlarm => {
+        return {
+          alarmName: metricAlarm.alarmName,
+          alarmArn: metricAlarm.alarmArn,
+        } as MetricAlarmDescriptor;
+      })
+      .value();
+
+    /* TODO: Determine if it's needed to detect which (if not all) metricAlarms/Autoscaling Policies have become invalid due to account/region change.
+    const result: IEcsServerGroupCommandResult = { dirty: {} };
+    const currentAutoscalingPolicies = command.autoscalingPolicies;
+    const newAutoscalingPolicies = command.backingData.filtered.metricAlarms;
+
+    if (currentAutoscalingPolicies) {
+      const matched = insersection(newAutoscalingPolicies, currentAutoscalingPolicies);
+      const removedAutoscalingPolicies = xor(matched, currentAutoscalingPolicies)
+      command.autoscalingPolicies = intersection(newAutoscalingPolicies, matched);
+
+      if (removedAutoscalingPolicies.length) {
+        result.dirty.autoscalingPolicies = removedAutoscalingPolicies;
+      }
+    }
+    */
+  }
+
+  public configureAvailableEcsClusters(command: IEcsServerGroupCommand): void {
+    command.backingData.filtered.ecsClusters = chain(command.backingData.ecsClusters)
+      .filter({ account: command.credentials ,
+                region: command.region })
+      .map('name')
+      .value();
+  }
+
+  public configureAvailableIamRoles(command: IEcsServerGroupCommand): void {
+    command.backingData.filtered.iamRoles = chain(command.backingData.iamRoles)
+      .filter({ accountName: command.credentials })
+      .map('name')
+      .value();
+    if (command.backingData.filtered.iamRoles.length > 0) {
+      command.backingData.filtered.iamRoles.splice(0, 0, 'None (No IAM role)');
+    }
+  }
+
+  public configureSubnetPurposes(command: IEcsServerGroupCommand): IServerGroupCommandResult {
+    const result: IEcsServerGroupCommandResult = { dirty: {} };
+    const filteredData = command.backingData.filtered;
+    if (command.region === null) {
+      return result;
+    }
+    filteredData.subnetPurposes = chain(command.backingData.subnets)
+      .filter({ account: command.credentials, region: command.region })
+      .reject({ target: 'elb' })
+      .reject({ purpose: null })
+      .uniqBy('purpose')
+      .value();
+
+    if (!chain(filteredData.subnetPurposes).some({ purpose: command.subnetType }).value()) {
+      command.subnetType = null;
+      result.dirty.subnetType = true;
+    }
+    return result;
+  }
+
+  private getLoadBalancerMap(command: IEcsServerGroupCommand): IAmazonLoadBalancer[] {
+    return chain(command.backingData.loadBalancers)
+      .map('accounts')
+      .flattenDeep()
+      .filter({ name: command.credentials })
+      .map('regions')
+      .flattenDeep()
+      .filter({ name: command.region })
+      .map<IAmazonLoadBalancer>('loadBalancers')
+      .flattenDeep<IAmazonLoadBalancer>()
+      .value()
+  }
+
+  public getLoadBalancerNames(command: IEcsServerGroupCommand): string[] {
+    const loadBalancers = this.getLoadBalancerMap(command).filter((lb) => (!lb.loadBalancerType || lb.loadBalancerType === 'classic') && lb.vpcId === command.vpcId);
+    return loadBalancers.map((lb) => lb.name).sort();
+  }
+
+  public getVpcLoadBalancerNames(command: IEcsServerGroupCommand): string[] {
+    const loadBalancersForVpc = this.getLoadBalancerMap(command).filter((lb) => (!lb.loadBalancerType || lb.loadBalancerType === 'classic') && lb.vpcId);
+    return loadBalancersForVpc.map((lb) => lb.name).sort();
+  }
+
+  public getTargetGroupNames(command: IEcsServerGroupCommand): string[] {
+    const loadBalancersV2 = this.getLoadBalancerMap(command).filter((lb) => lb.loadBalancerType !== 'classic') as any[];
+    const allTargetGroups = flatten(loadBalancersV2.map<string[]>((lb) => lb.targetGroups));
+    return allTargetGroups.sort();
+  }
+
+  public configureLoadBalancerOptions(command: IEcsServerGroupCommand): IServerGroupCommandResult {
+    const result: IEcsServerGroupCommandResult = { dirty: {} };
+    const currentLoadBalancers = (command.loadBalancers || []).concat(command.vpcLoadBalancers || []);
+    // const currentTargetGroups = command.targetGroup || [];
+    const newLoadBalancers = this.getLoadBalancerNames(command);
+    const vpcLoadBalancers = this.getVpcLoadBalancerNames(command);
+    const allTargetGroups = this.getTargetGroupNames(command);
+
+    if (currentLoadBalancers && command.loadBalancers) {
+      const valid = command.vpcId ? newLoadBalancers : newLoadBalancers.concat(vpcLoadBalancers);
+      const matched = intersection(valid, currentLoadBalancers);
+      const removedLoadBalancers = xor(matched, currentLoadBalancers);
+      command.loadBalancers = intersection(newLoadBalancers, matched);
+      if (!command.vpcId) {
+        command.vpcLoadBalancers = intersection(vpcLoadBalancers, matched);
+      } else {
+        delete command.vpcLoadBalancers;
+      }
+      if (removedLoadBalancers.length) {
+        result.dirty.loadBalancers = removedLoadBalancers;
+      }
+    }
+
+    // if (currentTargetGroups && command.targetGroup) {
+    //   const matched = intersection(allTargetGroups, currentTargetGroups);
+    //   const removedTargetGroups = xor(matched, currentTargetGroups);
+    //   command.targetGroup = intersection(allTargetGroups, matched);
+    //   if (removedTargetGroups.length) {
+    //     result.dirty.targetGroup = removedTargetGroups;
+    //   }
+    // }
+
+    command.backingData.filtered.loadBalancers = newLoadBalancers;
+    command.backingData.filtered.vpcLoadBalancers = vpcLoadBalancers;
+    command.backingData.filtered.targetGroups = allTargetGroups;
+    return result;
+  }
+
+  public refreshLoadBalancers(command: IEcsServerGroupCommand, skipCommandReconfiguration?: boolean) {
+    return this.cacheInitializer.refreshCache('loadBalancers').then(() => {
+      return this.loadBalancerReader.listLoadBalancers('ecs').then((loadBalancers) => {
+        command.backingData.loadBalancers = loadBalancers;
+        if (!skipCommandReconfiguration) {
+          this.configureLoadBalancerOptions(command);
+        }
+      });
+    });
+  }
+
+  public configureVpcId(command: IEcsServerGroupCommand): IEcsServerGroupCommandResult {
+    const result: IEcsServerGroupCommandResult = { dirty: {} };
+    if (!command.subnetType) {
+      command.vpcId = null;
+      result.dirty.vpcId = true;
+    } else {
+      const subnet = find<ISubnet>(command.backingData.subnets, {
+        purpose: command.subnetType,
+        account: command.credentials,
+        region: command.region
+      });
+      command.vpcId = subnet ? subnet.vpcId : null;
+    }
+    return result;
+  }
+
+  public attachEventHandlers(command: IEcsServerGroupCommand): void {
+    command.subnetChanged = (): IServerGroupCommandResult => {
+      const result = this.configureVpcId(command);
+      extend(result.dirty, this.configureLoadBalancerOptions(command).dirty);
+      command.viewState.dirty = command.viewState.dirty || {};
+      extend(command.viewState.dirty, result.dirty);
+      return result;
+    };
+
+    command.regionChanged = (): IServerGroupCommandResult => {
+      const result: IEcsServerGroupCommandResult = { dirty: {} };
+      const filteredData = command.backingData.filtered;
+      extend(result.dirty, this.configureSubnetPurposes(command).dirty);
+      if (command.region) {
+        extend(result.dirty, command.subnetChanged().dirty);
+        this.configureAvailabilityZones(command);
+        this.configureAvailableMetricAlarms(command);
+        this.configureAvailableEcsClusters(command);
+      } else {
+        filteredData.regionalAvailabilityZones = null;
+      }
+
+      return result;
+    };
+
+    command.credentialsChanged = (): IServerGroupCommandResult => {
+      const result: IEcsServerGroupCommandResult = { dirty: {} };
+      const backingData = command.backingData;
+      if (command.credentials) {
+        this.configureAvailableIamRoles(command);
+        this.configureAvailableMetricAlarms(command);
+        this.configureAvailableEcsClusters(command);
+
+        const regionsForAccount: IAccountDetails = backingData.credentialsKeyedByAccount[command.credentials] || { regions: [] } as IAccountDetails;
+        backingData.filtered.regions = regionsForAccount.regions;
+        if (!some(backingData.filtered.regions, { name: command.region })) {
+          command.region = null;
+          result.dirty.region = true;
+        } else {
+          extend(result.dirty, command.regionChanged().dirty);
+        }
+      } else {
+        command.region = null;
+      }
+      return result;
+    };
+
+    command.placementStrategyNameChanged = (): IServerGroupCommandResult => {
+      const result: IEcsServerGroupCommandResult = { dirty: {} };
+      command.placementStrategySequence = this.placementStrategyService.getPredefinedStrategy(command.placementStrategyName);
+      return result;
+    };
+
+    this.applyOverrides('attachEventHandlers', command);
+  }
+
+}
+
+export const ECS_SERVER_GROUP_CONFIGURATION_SERVICE = 'spinnaker.ecs.serverGroup.configure.service';
+module(ECS_SERVER_GROUP_CONFIGURATION_SERVICE, [
+  ACCOUNT_SERVICE,
+  SUBNET_READ_SERVICE,
+  LOAD_BALANCER_READ_SERVICE,
+  CACHE_INITIALIZER_SERVICE,
+  SERVER_GROUP_COMMAND_REGISTRY_PROVIDER,
+])
+  .service('ecsServerGroupConfigurationService', EcsServerGroupConfigurationService);
