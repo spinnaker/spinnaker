cc26e2b90e06d3e4eeff1eb00974f90a27c8107e
Bruno Carrier <brunopierrecarrier@gmail.com>
2018-01-10 10:34:11 -0500

    feat(provider/ecs): Added vertical scaling section to deploy modal (#4632)
    * feat(provider/ecs): Added vertical scaling section to deploy modal
    
    * minor fixes: Updated PR after Chris Berry's review

---
+++ app/scripts/modules/ecs/serverGroup/configure/wizard/verticalScaling/verticalScaling.component.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const angular = require('angular');
+
+import { V2_MODAL_WIZARD_SERVICE } from '@spinnaker/core';
+
+module.exports = angular
+  .module('spinnaker.ecs.serverGroup.configure.wizard.verticalScaling.component', [
+    V2_MODAL_WIZARD_SERVICE,
+  ])
+  .component('ecsServerGroupVerticalScaling', {
+    bindings: {
+      command: '=',
+      application: '=',
+    },
+    templateUrl: require('./verticalScaling.component.html'),
+  });
