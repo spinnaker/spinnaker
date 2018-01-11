2314523d8b8353557a284dba61d03814a71201e7
Bruno Carrier <brunopierrecarrier@gmail.com>
2018-01-11 12:32:08 -0500

    feat(provider/ecs): Updated the ECS help file (#4653)

--- app/scripts/modules/ecs/ecs.help.ts
+++ app/scripts/modules/ecs/ecs.help.ts
@@ -12,10 +12,11 @@ const helpContents: {[key: string]: string} = {
   'ecs.capacity.minimum': '<p>The minimum number of containers you can reach as a result of autoscaling.</p> <p>Typically, this represents the bare minimum you can afford to run without impacting your capacity to meet your SLA (Service Level Agreement) objectives</p>',
   'ecs.capacity.maximum': '<p>The maximal number of containers you can reach as a result of autoscaling.</p>',
   'ecs.capacity.reserved.computeUnits': '<p>The assured minimal amount of computing capacity your container will be able to use.  1024 units are equal to 1 AWS virtual CPU</p> <p>If other containers on your underlying host are not using their reserved compute capacity, this container will be able to use it.</p>',
-  'ecs.capacity.reserved.memory': '<p>The maximal amount of memory that your container can use, in megabytes.  Exceeding this amount may result in termination of your container.</p><p>1024 MB = 1 GB</p>',
+  'ecs.capacity.reserved.memory': '<p>The maximal amount of memory that your container can use, in megabytes.  Exceeding this amount may result in termination of your container.</p><p>1024 mb = 1 gb</p>',
   'ecs.loadbalancing.targetPort': '<p>The port on which your application is listening for incoming traffic</p>',
   'ecs.iamrole': '<p>The IAM role that your container (task, in AWS wording) will inherit.  </p><p>Define a role only if your application needs to access AWS APIs</p>',
-  'ecs.placementStrategy': '<p>The strategy the container scheduler will be using.  See <a href="http://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-placement-strategies.html" target="_blank">AWS documentation</a> for more details. </p><p>You should at least balance across availability zones</p>',
+  'ecs.placementStrategy': '<p>The strategy the container scheduler will be using.  See <a href="http://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-placement-strategies.html" target="_blank">AWS documentation</a> for more details. </p><p>You should at least balance across availability zones</p><p>Custom placement strategies have not been implemented yet.</p>',
+  'ecs.capacity.autoscalingPolicies': '<p>A predefined MetricAlarm and Autoscaling policy with an Action must exist.</p><p>There is a delay in MetricAlarm recognizing the Autoscaling policy.</p>',
 };
 
 export const ECS_HELP = 'spinnaker.ecs.help.contents';
