b2b150ecacde5e238c8b9a2f491b7046fc65d0b6
Bruno Carrier <brunopierrecarrier@gmail.com>
2018-01-11 12:20:22 -0500

    feat(provider/ecs): Added Disable ASG stage (#4646)

---
+++ app/scripts/modules/ecs/pipeline/stages/disableAsg/ecsDisableAsgStage.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const angular = require('angular');
+
+import { PipelineTemplates, StageConstants } from '@spinnaker/core';
+
+module.exports = angular.module('spinnaker.ecs.pipeline.stage.disableAsgStage', [
+])
+  .config(function(pipelineConfigProvider) {
+    pipelineConfigProvider.registerStage({
+      provides: 'disableServerGroup',
+      alias: 'disableAsg',
+      cloudProvider: 'ecs',
+      templateUrl: require('./disableAsgStage.html'),
+      executionDetailsUrl: PipelineTemplates.disableAsgExecutionDetails,
+      executionStepLabelUrl: require('./disableAsgStepLabel.html'),
+      validators: [
+        {
+          type: 'targetImpedance',
+          message: 'This pipeline will attempt to disable a server group without deploying a new version into the same cluster.'
+        },
+        { type: 'requiredField', fieldName: 'cluster' },
+        { type: 'requiredField', fieldName: 'target', },
+        { type: 'requiredField', fieldName: 'regions', },
+        { type: 'requiredField', fieldName: 'credentials', fieldLabel: 'account'},
+      ],
+    });
+  }).controller('ecsDisableAsgStageCtrl', function($scope, accountService) {
+
+    let stage = $scope.stage;
+
+    $scope.state = {
+      accounts: false,
+      regionsLoaded: false
+    };
+
+    accountService.listAccounts('ecs').then(function (accounts) {
+      $scope.accounts = accounts;
+      $scope.state.accounts = true;
+    });
+
+
+    $scope.targets = StageConstants.TARGET_LIST;
+
+    stage.regions = stage.regions || [];
+    stage.cloudProvider = 'ecs';
+
+    if (stage.isNew && $scope.application.attributes.platformHealthOnlyShowOverride && $scope.application.attributes.platformHealthOnly) {
+      stage.interestingHealthProviderNames = ['Ecs'];
+    }
+
+    if (!stage.credentials && $scope.application.defaultCredentials.ecs) {
+      stage.credentials = $scope.application.defaultCredentials.ecs;
+    }
+    if (!stage.regions.length && $scope.application.defaultRegions.ecs) {
+      stage.regions.push($scope.application.defaultRegions.ecs);
+    }
+
+    if (!stage.target) {
+      stage.target = $scope.targets[0].val;
+    }
+
+  });
+
