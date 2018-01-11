13ae6ef41a2bddbda264ecb8457a6053aece6c52
Bruno Carrier <brunopierrecarrier@gmail.com>
2018-01-11 12:40:30 -0500

    feat(provider/ecs): Added IAM role reader to ECS (#4631)
    * feat(provider/ecs): Added IAM role reader to ECS
    
    * Removed extra white line

---
+++ app/scripts/modules/ecs/iamRoles/IRole.ts
@@ -0,0 +1,6 @@
+export interface IRoleDescriptor {
+  name: string;
+  trustRelationships: string[];
+  id: string;
+  accountName: string;
+}
---
+++ app/scripts/modules/ecs/iamRoles/iamRole.read.service.ts
@@ -0,0 +1,21 @@
+import { IPromise, module } from 'angular';
+
+import { API_SERVICE, Api } from 'core/api/api.service';
+import { IRoleDescriptor } from './IRole';
+
+export class IamRoleReader {
+  public constructor(private API: Api) { 'ngInject'; }
+
+  public listRoles(provider: string): IPromise<IRoleDescriptor[]> {
+    return this.API
+      .all('roles')
+      .all(provider)
+      .getList();
+  }
+}
+
+export const IAM_ROLE_READ_SERVICE = 'spinnaker.ecs.iamRole.read.service';
+
+module(IAM_ROLE_READ_SERVICE, [
+  API_SERVICE
+]).service('iamRoleReader', IamRoleReader);
