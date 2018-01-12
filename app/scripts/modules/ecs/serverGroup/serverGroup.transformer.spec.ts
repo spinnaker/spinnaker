a9af189dd8bd71bffe485e576ce79f772282a9d4
Bruno Carrier <brunopierrecarrier@gmail.com>
2018-01-12 10:45:24 -0500

    feat(provider/ecs): Added serverGroup Transformer (#4650)
    * feat(provider/ecs): Added serverGroup Transformer stage
    
    * Fixed imports for @spinnaker/amazon

---
+++ app/scripts/modules/ecs/serverGroup/serverGroup.transformer.spec.ts
@@ -0,0 +1,187 @@
+import { mock, IQService, IRootScopeService, IScope } from 'angular';
+
+import {
+  ECS_SERVER_GROUP_TRANSFORMER,
+  EcsServerGroupTransformer
+} from './serverGroup.transformer';
+import { IScalingPolicyAlarmView, IAmazonServerGroup, IStepAdjustment } from '@spinnaker/amazon';
+import { VPC_READ_SERVICE, VpcReader } from '@spinnaker/amazon';
+
+describe('ecsServerGroupTransformer', () => {
+
+  let transformer: EcsServerGroupTransformer,
+      vpcReader: VpcReader,
+      $q: IQService,
+      $scope: IScope;
+
+  beforeEach(mock.module(
+    ECS_SERVER_GROUP_TRANSFORMER,
+    VPC_READ_SERVICE));
+
+  beforeEach(mock.inject((_ecsServerGroupTransformer_: EcsServerGroupTransformer,
+                          _vpcReader_: VpcReader,
+                          _$q_: IQService,
+                          $rootScope: IRootScopeService) => {
+    transformer = _ecsServerGroupTransformer_;
+    vpcReader = _vpcReader_;
+    $q = _$q_;
+    $scope = $rootScope.$new();
+  }));
+
+  describe('normalize server group', () => {
+    beforeEach(() => {
+      spyOn(vpcReader, 'listVpcs').and.returnValue($q.when([
+        { account: 'test', region: 'us-east-1', id: 'vpc-1', name: 'main' }
+      ]));
+    });
+
+    it('adds vpc name to server group', () => {
+      const serverGroup = {
+        account: 'test',
+        region: 'us-east-1',
+        vpcId: 'vpc-1',
+        instances: [],
+      } as IAmazonServerGroup;
+      transformer.normalizeServerGroup(serverGroup);
+      $scope.$digest();
+      expect(serverGroup.vpcName).toBe('main');
+    });
+
+    it('adds empty vpc name when no vpcId found on server group', () => {
+      const serverGroup = {
+        account: 'test',
+        region: 'us-east-1',
+        instances: [],
+      } as IAmazonServerGroup;
+      transformer.normalizeServerGroup(serverGroup);
+      $scope.$digest();
+      expect(serverGroup.vpcName).toBe('');
+    });
+  });
+
+  describe('command transforms', () => {
+
+    it('sets subnetType property to empty string when null', () => {
+      const command: any = {
+        viewState: {
+          mode: 'create',
+          useAllImageSelection: true,
+          allImageSelection: 'something-packagebase',
+        },
+        subnetType: null,
+        application: { name: 'theApp' }
+      };
+
+      let transformed = transformer.convertServerGroupCommandToDeployConfiguration(command);
+      expect(transformed.subnetType).toBe('');
+
+      command.subnetType = 'internal';
+      transformed = transformer.convertServerGroupCommandToDeployConfiguration(command);
+      expect(transformed.subnetType).toBe('internal');
+    });
+
+  });
+
+  describe('normalize server group details', () => {
+
+    it('adds appropriate comparator to alarm', () => {
+      const serverGroup = {
+        scalingPolicies: [{
+            alarms: [
+              { comparisonOperator: 'LessThanThreshold' },
+              { comparisonOperator: 'GreaterThanThreshold' },
+              { comparisonOperator: 'LessThanOrEqualToThreshold' },
+              { comparisonOperator: 'GreaterThanOrEqualToThreshold' },
+              { comparisonOperator: 'WhatIsThis' },
+            ]
+          }]
+      } as IAmazonServerGroup;
+      transformer.normalizeServerGroupDetails(serverGroup);
+      const alarms = serverGroup.scalingPolicies[0].alarms as IScalingPolicyAlarmView[];
+      expect(alarms.map(a => a.comparator)).toEqual(['&lt;', '&gt;', '&le;', '&ge;', undefined]);
+    });
+
+    it('adds operator, absAdjustment to simple policies', () => {
+      const serverGroup = {
+        scalingPolicies: [
+            { scalingAdjustment: 10 },
+            { scalingAdjustment: 0 },
+            { scalingAdjustment: -5 }
+          ]
+      } as IAmazonServerGroup;
+      const transformed = transformer.normalizeServerGroupDetails(serverGroup);
+      const policies = transformed.scalingPolicies;
+      expect(policies.map(a => a.absAdjustment)).toEqual([10, 0, 5]);
+      expect(policies.map(a => a.operator)).toEqual(['increase', 'increase', 'decrease']);
+    });
+
+    it('adds operator, absAdjustment to step policies', () => {
+      const serverGroup = {
+        scalingPolicies: [
+          {
+            stepAdjustments: [
+              { scalingAdjustment: 10, metricIntervalLowerBound: 0 },
+              { scalingAdjustment: 0, metricIntervalLowerBound: 6 },
+              { scalingAdjustment: -5, metricIntervalLowerBound: 11 }
+          ]}
+        ]
+      } as IAmazonServerGroup;
+      const transformed = transformer.normalizeServerGroupDetails(serverGroup);
+      const steps = transformed.scalingPolicies[0].stepAdjustments;
+      expect(steps.map(a => a.absAdjustment)).toEqual([10, 0, 5]);
+      expect(steps.map(a => a.operator)).toEqual(['increase', 'increase', 'decrease']);
+    });
+
+    describe('sorting step adjustments', () => {
+
+      beforeEach(function() {
+        this.test = (steps: IStepAdjustment[], expected: any[]) => {
+          const serverGroup = {
+            scalingPolicies: [
+              {
+                stepAdjustments: steps
+              }
+            ]
+          } as IAmazonServerGroup;
+          transformer.normalizeServerGroupDetails(serverGroup);
+          const check = serverGroup.scalingPolicies[0].stepAdjustments;
+          expect(check.map(s => s.scalingAdjustment)).toEqual(expected);
+        };
+      });
+
+      it('reverse sorts step adjustments by lower bound when none have an upper bound defined', function() {
+        this.test(
+          [
+            { scalingAdjustment: 10, metricIntervalLowerBound: 3 },
+            { scalingAdjustment: 0, metricIntervalLowerBound: 5 },
+            { scalingAdjustment: -5, metricIntervalLowerBound: 1 }
+          ],
+          [-5, 10, 0]
+        );
+      });
+
+      it('reverse sorts step adjustments by lower bound when some do not have an upper bound defined', function() {
+        this.test(
+          [
+            { id: 1, scalingAdjustment: 10, metricIntervalLowerBound: 3, metricIntervalUpperBound: 5 },
+            { id: 2, scalingAdjustment: 0, metricIntervalLowerBound: 5 },
+            { id: 3, scalingAdjustment: -5, metricIntervalLowerBound: 1, metricIntervalUpperBound: 3 }
+          ],
+          [-5, 10, 0]
+        );
+      });
+
+      it('reverse sorts step adjustments by upper bound when all have an upper bound defined', function() {
+        this.test(
+          [
+            { id: 1, scalingAdjustment: 10, metricIntervalLowerBound: 3, metricIntervalUpperBound: 5 },
+            { id: 2, scalingAdjustment: 0, metricIntervalLowerBound: 5, metricIntervalUpperBound: 9 },
+            { id: 3, scalingAdjustment: -5, metricIntervalLowerBound: 1, metricIntervalUpperBound: 0 }
+          ],
+          [0, 10, -5]
+        );
+      });
+
+    });
+  });
+});
---
+++ app/scripts/modules/ecs/serverGroup/serverGroup.transformer.ts
@@ -0,0 +1,109 @@
+import { module, IPromise } from 'angular';
+import { defaults } from 'lodash';
+
+import { IVpc } from '@spinnaker/core';
+
+import {
+  IScalingAdjustmentView, IScalingPolicyView, IScalingPolicyAlarmView, IAmazonServerGroup, IStepAdjustmentView,
+  IScalingPolicy, IAmazonServerGroupView
+} from '@spinnaker/amazon';
+import { VPC_READ_SERVICE, VpcReader } from '@spinnaker/amazon';
+
+export class EcsServerGroupTransformer {
+  public constructor(private vpcReader: VpcReader) { 'ngInject'; }
+
+  private addComparator(alarm: IScalingPolicyAlarmView): void {
+    if (!alarm.comparisonOperator) {
+      return;
+    }
+    switch (alarm.comparisonOperator) {
+      case 'LessThanThreshold':
+        alarm.comparator = '&lt;';
+        break;
+      case 'GreaterThanThreshold':
+        alarm.comparator = '&gt;';
+        break;
+      case 'LessThanOrEqualToThreshold':
+        alarm.comparator = '&le;';
+        break;
+      case 'GreaterThanOrEqualToThreshold':
+        alarm.comparator = '&ge;';
+        break;
+    }
+  }
+
+  private addAdjustmentAttributes(policyOrStepAdjustment: IScalingAdjustmentView): void {
+    policyOrStepAdjustment.operator = policyOrStepAdjustment.scalingAdjustment < 0 ? 'decrease' : 'increase';
+    policyOrStepAdjustment.absAdjustment = Math.abs(policyOrStepAdjustment.scalingAdjustment);
+  }
+
+  private transformScalingPolicy(policy: IScalingPolicy): IScalingPolicyView {
+    const view: IScalingPolicyView = Object.assign({}, policy) as IScalingPolicyView;
+    const upperBoundSorter = (a: IStepAdjustmentView, b: IStepAdjustmentView) => b.metricIntervalUpperBound - a.metricIntervalUpperBound,
+          lowerBoundSorter = (a: IStepAdjustmentView, b: IStepAdjustmentView) => a.metricIntervalLowerBound - b.metricIntervalLowerBound;
+
+    view.alarms = policy.alarms || [];
+    view.alarms.forEach((alarm) => this.addComparator(alarm));
+    this.addAdjustmentAttributes(view); // simple policies
+    if (view.stepAdjustments && view.stepAdjustments.length) {
+      view.stepAdjustments.forEach((sa) => this.addAdjustmentAttributes(sa)); // step policies
+      const sorter = policy.stepAdjustments.every(a => a.metricIntervalUpperBound !== undefined) ?
+        upperBoundSorter : lowerBoundSorter;
+      view.stepAdjustments.sort((a, b) => sorter(a, b));
+    }
+    return view;
+  };
+
+  public normalizeServerGroupDetails(serverGroup: IAmazonServerGroup): IAmazonServerGroupView {
+    const view: IAmazonServerGroupView = Object.assign({}, serverGroup) as IAmazonServerGroupView;
+    if (serverGroup.scalingPolicies) {
+      view.scalingPolicies = serverGroup.scalingPolicies.map((policy) => this.transformScalingPolicy(policy));
+    }
+    return view;
+  }
+
+  public normalizeServerGroup(serverGroup: IAmazonServerGroup): IPromise<IAmazonServerGroup> {
+    serverGroup.instances.forEach((instance) => { instance.vpcId = serverGroup.vpcId; });
+    return this.vpcReader.listVpcs().then((vpc) => this.addVpcNameToServerGroup(serverGroup)(vpc));
+  }
+
+  private addVpcNameToServerGroup(serverGroup: IAmazonServerGroup): (vpc: IVpc[]) => IAmazonServerGroup {
+    return (vpcs: IVpc[]) => {
+      const match = vpcs.find((test) => test.id === serverGroup.vpcId);
+      serverGroup.vpcName = match ? match.name : '';
+      return serverGroup;
+    };
+  }
+
+  public convertServerGroupCommandToDeployConfiguration(base: any): any {
+    // use _.defaults to avoid copying the backingData, which is huge and expensive to copy over
+    const command = defaults({ backingData: [], viewState: [] }, base);
+    command.cloudProvider = 'ecs';
+    command.availabilityZones = {};
+    command.availabilityZones[command.region] = base.availabilityZones;
+    command.loadBalancers = (base.loadBalancers || []).concat(base.vpcLoadBalancers || []);
+    command.targetGroup = base.targetGroup || '';
+    command.account = command.credentials;
+    command.subnetType = command.subnetType || '';
+
+    if (base.viewState.mode !== 'clone') {
+      delete command.source;
+    }
+
+    delete command.region;
+    delete command.viewState;
+    delete command.backingData;
+    delete command.selectedProvider;
+    delete command.instanceProfile;
+    delete command.vpcId;
+
+    return command;
+  }
+
+}
+
+export const ECS_SERVER_GROUP_TRANSFORMER = 'spinnaker.ecs.serverGroup.transformer';
+module(ECS_SERVER_GROUP_TRANSFORMER, [
+  VPC_READ_SERVICE,
+])
+  .service('ecsServerGroupTransformer', EcsServerGroupTransformer);
