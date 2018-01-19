b351c0ea8498753e25fe9a6cef51b77d31d79873
Justin Reynolds <justinreynolds@gmail.com>
2018-01-18 16:55:06 -0800

    feat(core/validation): Add react component for form validation errors

--- app/scripts/modules/core/src/index.ts
+++ app/scripts/modules/core/src/index.ts
@@ -71,6 +71,8 @@ export * from './task';
 
 export * from './utils';
 
+export * from './validation';
+
 export * from './widgets';
 
 export * from './core.module';
---
+++ app/scripts/modules/core/src/validation/ValidationError.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class ValidationError extends React.Component<{ message: string }> {
+  public render() {
+    return (
+      <div className="error-message">
+        <span className="fa fa-exclamation-circle"/> {this.props.message}
+      </div>
+    );
+  }
+}
