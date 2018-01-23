08e0f215a3947916c61f8bde0a3a6deba7a48e77
Chris Thielen <christopherthielen@users.noreply.github.com>
2018-01-22 18:29:34 -0800

    fix(core/account): Fix account tag color rendering during scroll/filter operation (#4706)

--- app/scripts/modules/core/src/account/AccountTag.tsx
+++ app/scripts/modules/core/src/account/AccountTag.tsx
@@ -1,3 +1,4 @@
+import { IPromise } from 'angular';
 import * as React from 'react';
 import { BindAll } from 'lodash-decorators';
 import { ReactInjector } from 'core/reactShims';
@@ -12,13 +13,39 @@ export interface IAccountTagState {
 
 @BindAll()
 export class AccountTag extends React.Component<IAccountTagProps, IAccountTagState> {
+  private static cache: {
+    [account: string]: boolean|IPromise<boolean>;
+  } = {};
+
   public state = { isProdAccount: false };
+  private mounted = true;
 
-  constructor(props: IAccountTagProps) {
-    super(props);
+  public componentWillUnmount(): void {
+    this.mounted = false;
+  }
 
-    ReactInjector.accountService.challengeDestructiveActions(props.account)
-      .then(isProdAccount => this.setState({ isProdAccount }));
+  public componentDidMount() {
+    this.updateAccount(this.props.account);
+  }
+
+  public componentWillReceiveProps(nextProps: IAccountTagProps): void {
+    this.updateAccount(nextProps.account);
+  }
+
+  private updateAccount(account: string) {
+    const { cache } = AccountTag;
+    if (!cache.hasOwnProperty(account)) {
+      cache[account] = ReactInjector.accountService.challengeDestructiveActions(account)
+        .then(result => cache[account] = result);
+    }
+
+    const cachedVal: boolean | IPromise<boolean> = cache[account];
+
+    if (typeof cachedVal === 'boolean') {
+      this.setState({ isProdAccount: cachedVal })
+    } else {
+      cachedVal.then(isProdAccount => this.mounted && this.setState({ isProdAccount }));
+    }
   }
 
   public render() {
