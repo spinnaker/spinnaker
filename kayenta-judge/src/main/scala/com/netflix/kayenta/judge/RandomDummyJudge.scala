b46444ae09bf33beb3114b4d97e8ea85c06a74d3
Matt Duftler <duftler@google.com>
2018-01-29 09:10:53 -0500

    fix(random-judge): Fully populate dummy judge results so report ui has data to work with. (#218)

--- kayenta-judge/src/main/scala/com/netflix/kayenta/judge/RandomDummyJudge.scala
+++ kayenta-judge/src/main/scala/com/netflix/kayenta/judge/RandomDummyJudge.scala
@@ -18,10 +18,16 @@ package com.netflix.kayenta.judge
 
 import java.util
 
-import com.netflix.kayenta.canary.results.{CanaryJudgeResult, CanaryJudgeScore}
+import com.netflix.kayenta.canary.results.{CanaryAnalysisResult, CanaryJudgeGroupScore, CanaryJudgeResult, CanaryJudgeScore}
 import com.netflix.kayenta.canary.{CanaryClassifierThresholdsConfig, CanaryConfig, CanaryJudge}
+import com.netflix.kayenta.judge.classifiers.metric.{High, Low, Pass}
+import com.netflix.kayenta.judge.classifiers.score.ThresholdScoreClassifier
+import com.netflix.kayenta.judge.scorers.WeightedSumScorer
+import com.netflix.kayenta.judge.stats.DescriptiveStatistics
 import com.netflix.kayenta.metrics.MetricSetPair
 import org.springframework.stereotype.Component
+
+import scala.collection.JavaConverters._
 
 @Component
 class RandomDummyJudge extends CanaryJudge {
@@ -34,10 +40,80 @@ class RandomDummyJudge extends CanaryJudge {
   override def judge(canaryConfig: CanaryConfig,
                      scoreThresholds: CanaryClassifierThresholdsConfig,
                      metricSetPairList: util.List[MetricSetPair]): CanaryJudgeResult = {
+    val metricResults = metricSetPairList.asScala.toList.map { metricPair =>
+      val metricConfig = canaryConfig.getMetrics.asScala.find(m => m.getName == metricPair.getName) match {
+        case Some(config) => config
+        case None => throw new IllegalArgumentException(s"Could not find metric config for ${metricPair.getName}")
+      }
 
-    val randomValue = "%.2f".format(random.nextDouble * 100).toDouble
-    val score = CanaryJudgeScore.builder.score(randomValue).build
-    CanaryJudgeResult.builder.judgeName(_judgeName).score(score).build
+      val experimentValues = metricPair.getValues.get("experiment").asScala.map(_.toDouble).toArray
+      val controlValues = metricPair.getValues.get("control").asScala.map(_.toDouble).toArray
+
+      val experimentMetric = Metric(metricPair.getName, experimentValues, label="Canary")
+      val controlMetric = Metric(metricPair.getName, controlValues, label="Baseline")
+
+      val experimentStats = DescriptiveStatistics.summary(experimentMetric)
+      val controlStats = DescriptiveStatistics.summary(controlMetric)
+
+      val randomValue = "%.2f".format(random.nextDouble * 100).toDouble
+
+      // The classification of each individual metric will be done randomly, ignoring the actual data points.
+      val classification = {
+        if (randomValue >= 66) {
+          High.toString
+        } else if (randomValue >= 33) {
+          Pass.toString
+        } else {
+          Low.toString
+        }
+      }
+
+      CanaryAnalysisResult.builder()
+        .name(metricPair.getName)
+        .id(metricPair.getId)
+        .classification(classification)
+        .groups(metricConfig.getGroups)
+        .experimentMetadata(Map("stats" -> DescriptiveStatistics.toMap(experimentStats).asJava.asInstanceOf[Object]).asJava)
+        .controlMetadata(Map("stats" -> DescriptiveStatistics.toMap(controlStats).asJava.asInstanceOf[Object]).asJava)
+        .build()
+    }
+
+    val groupWeights = Option(canaryConfig.getClassifier.getGroupWeights) match {
+      case Some(groups) => groups.asScala.mapValues(_.toDouble).toMap
+      case None => Map[String, Double]()
+    }
+
+    val weightedSumScorer = new WeightedSumScorer(groupWeights)
+    val scores = weightedSumScorer.score(metricResults)
+
+    val scoreClassifier = new ThresholdScoreClassifier(scoreThresholds.getPass, scoreThresholds.getMarginal)
+    val scoreClassification = scoreClassifier.classify(scores)
+
+    val groupScores = scores.groupScores match {
+      case None => List(CanaryJudgeGroupScore.builder().build())
+      case Some(groups) => groups.map{ group =>
+        CanaryJudgeGroupScore.builder()
+          .name(group.name)
+          .score(group.score)
+          .classification("")
+          .classificationReason("")
+          .build()
+      }
+    }
+
+    val summaryScore = CanaryJudgeScore.builder()
+      .score(scoreClassification.score)
+      .classification(scoreClassification.classification.toString)
+      .classificationReason(scoreClassification.reason.getOrElse(""))
+      .build()
+
+    val results = metricResults.asJava
+    CanaryJudgeResult.builder()
+      .judgeName(_judgeName)
+      .score(summaryScore)
+      .results(results)
+      .groupScores(groupScores.asJava)
+      .build()
   }
 
 }
