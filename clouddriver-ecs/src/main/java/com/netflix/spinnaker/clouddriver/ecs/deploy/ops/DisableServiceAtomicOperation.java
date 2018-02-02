5d8671468b81ff11decef8a4beb93c6bfd808b67
Daniel Kirillov <dkirillov25@gmail.com>
2018-02-02 14:27:57 -0500

    feat(provider/ecs): Disable Service Atomic Operation (#2345)
    * Added DisableServiceAtomicOperation.
    
    * Deleted an unnecessary validator.

--- clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/DisableServiceAtomicOperation.java
+++ clouddriver-ecs/src/main/java/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/DisableServiceAtomicOperation.java
@@ -1,24 +1,53 @@
+/*
+ * Copyright 2018 Lookout, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.netflix.spinnaker.clouddriver.ecs.deploy.ops;
 
-import com.netflix.spinnaker.clouddriver.ecs.deploy.description.DisableServiceDescription;
-import com.netflix.spinnaker.clouddriver.orchestration.AtomicOperation;
+import com.amazonaws.services.ecs.AmazonECS;
+import com.amazonaws.services.ecs.model.UpdateServiceRequest;
+import com.netflix.spinnaker.clouddriver.ecs.deploy.description.ModifyServiceDescription;
 
 import java.util.List;
 
-public class DisableServiceAtomicOperation implements AtomicOperation<Void> {
+public class DisableServiceAtomicOperation extends AbstractEcsAtomicOperation<ModifyServiceDescription, Void> {
 
-  DisableServiceDescription description;
-
-  public DisableServiceAtomicOperation(DisableServiceDescription description) {
-    this.description = description;
+  public DisableServiceAtomicOperation(ModifyServiceDescription description) {
+    super(description, "DISABLE_ECS_SERVER_GROUP");
   }
 
   @Override
   public Void operate(List priorOutputs) {
-
-    // TODO - implement this stub
-
+    updateTaskStatus("Initializing Disable Amazon ECS Server Group Operation...");
+    disableService();
     return null;
   }
 
+  private void disableService() {
+    AmazonECS ecs = getAmazonEcsClient();
+
+    String service = description.getServerGroupName();
+    String account = description.getCredentialAccount();
+    String cluster = getCluster(service, account);
+
+    updateTaskStatus(String.format("Disabling %s server group for %s.", service, account));
+    UpdateServiceRequest request = new UpdateServiceRequest()
+      .withCluster(cluster)
+      .withService(service)
+      .withDesiredCount(0);
+    ecs.updateService(request);
+    updateTaskStatus(String.format("Server group %s disabled for %s.", service, account));
+  }
 }
---
+++ clouddriver-ecs/src/test/groovy/com/netflix/spinnaker/clouddriver/ecs/deploy/ops/DisableServiceAtomicOperationSpec.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 Lookout, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.clouddriver.ecs.deploy.ops
+
+import com.netflix.spinnaker.clouddriver.ecs.TestCredential
+import com.netflix.spinnaker.clouddriver.ecs.deploy.description.ModifyServiceDescription
+
+class DisableServiceAtomicOperationSpec extends CommonAtomicOperation {
+  void 'should execute the operation'() {
+    given:
+    def operation = new DisableServiceAtomicOperation(new ModifyServiceDescription(
+      serverGroupName: "test-server-group",
+      credentials: TestCredential.named('Test', [:])
+    ))
+
+    operation.amazonClientProvider = amazonClientProvider
+    operation.accountCredentialsProvider = accountCredentialsProvider
+    operation.containerInformationService = containerInformationService
+
+    amazonClientProvider.getAmazonEcs(_, _, _) >> ecs
+    containerInformationService.getClusterName(_, _, _) >> 'cluster-name'
+    accountCredentialsProvider.getCredentials(_) >> TestCredential.named("test")
+
+    when:
+    operation.operate([])
+
+    then:
+    1 * ecs.updateService(_)
+  }
+}
