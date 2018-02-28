7174a99f08a569db1b53cf117ca21a45357d8b23
Chris Thielen <github@sandgnat.com>
2018-02-28 12:12:21 -0800

    fix(core/instance): Update InstanceDetails when props change

--- app/scripts/modules/core/src/instance/details/InstanceDetails.tsx
+++ app/scripts/modules/core/src/instance/details/InstanceDetails.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Subject } from 'rxjs';
 
 import { Overridable, IOverridableProps } from 'core/overrideRegistry';
 import { ReactInjector } from 'core/reactShims';
 import { Application } from 'core/application';
 import { Spinner } from 'core/widgets';
+import { Observable } from 'rxjs/Observable';
 
 export interface IInstanceDetailsProps extends IOverridableProps {
   $stateParams: {
@@ -12,25 +14,51 @@ export interface IInstanceDetailsProps extends IOverridableProps {
   },
   app: Application;
 }
+export interface IInstanceDetailsState {
+  accountId: string;
+  loading: boolean;
+}
 
-export class InstanceDetails extends React.Component<IInstanceDetailsProps, { accountId: string }> {
-  public constructor(props: any) {
-    super(props);
+export class InstanceDetails extends React.Component<IInstanceDetailsProps, IInstanceDetailsState> {
+  public state = {
+    accountId: null,
+    loading: false,
+  } as IInstanceDetailsState;
 
-    const { app, $stateParams: { provider, instanceId } } = this.props;
-    ReactInjector.versionedCloudProviderService.getAccountForInstance(provider, instanceId, app).then(accountId => {
-      this.setState({ accountId })
-    });
-    this.state = { accountId: null };
+  private destroy$ = new Subject();
+  private props$ = new Subject<IInstanceDetailsProps>();
+
+  public componentDidMount() {
+    const { versionedCloudProviderService } = ReactInjector;
+
+    this.props$.next(this.props);
+    this.props$
+      .do(() => this.setState({ loading: true, accountId: null }))
+      .mergeMap(({ app, $stateParams }) => {
+        const acct = versionedCloudProviderService.getAccountForInstance($stateParams.provider, $stateParams.instanceId, app);
+        return Observable.fromPromise(acct);
+      })
+      .takeUntil(this.destroy$)
+      .subscribe((accountId: string) => {
+        this.setState({ accountId, loading: false })
+      });
+  }
+
+  public componentWillReceiveProps(nextProps: IInstanceDetailsProps) {
+    this.props$.next(nextProps)
+  }
+
+  public componentWillUnmount() {
+    this.destroy$.next();
   }
 
   public render() {
-
-    const { accountId } = this.state;
-    if (accountId) {
-      return <InstanceDetailsCmp {...this.props} accountId={accountId} />;
+    const { accountId, loading } = this.state;
+    if (loading) {
+      return <Spinner/>;
     }
-    return <Spinner/>;
+
+    return <InstanceDetailsCmp {...this.props} accountId={accountId} />;
   }
 }
 
