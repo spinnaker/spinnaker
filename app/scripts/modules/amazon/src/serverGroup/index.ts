6dcedb74003c8704cb072c9722e9d1c3fec5477b
Justin Reynolds <justinreynolds@gmail.com>
2018-03-01 13:39:28 -0800

    chore(amazon/serverGroup): Export server group config interfaces

--- app/scripts/modules/amazon/src/serverGroup/index.ts
+++ app/scripts/modules/amazon/src/serverGroup/index.ts
@@ -1,3 +1,4 @@
+export * from './configure';
 export * from './details/scalingPolicy/ScalingPolicyTypeRegistry';
 export * from './details/scalingPolicy/scalingPolicy.write.service';
 export * from './details/sections';
