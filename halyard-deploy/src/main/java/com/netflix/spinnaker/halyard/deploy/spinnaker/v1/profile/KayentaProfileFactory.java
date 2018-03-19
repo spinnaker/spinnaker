23ba0f6de1733de09523d719bc3b6cb828b1cdd8
Matt Duftler <duftler@google.com>
2018-03-19 15:57:35 -0400

    fix(kayenta): Copy base profile contents. (#875)

--- halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/KayentaProfileFactory.java
+++ halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/KayentaProfileFactory.java
@@ -16,7 +16,9 @@
 
 package com.netflix.spinnaker.halyard.deploy.spinnaker.v1.profile;
 
+import com.netflix.spinnaker.halyard.config.model.v1.node.DeploymentConfiguration;
 import com.netflix.spinnaker.halyard.deploy.spinnaker.v1.SpinnakerArtifact;
+import com.netflix.spinnaker.halyard.deploy.spinnaker.v1.SpinnakerRuntimeSettings;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -25,5 +27,11 @@ public class KayentaProfileFactory extends SpringProfileFactory {
   @Override
   public SpinnakerArtifact getArtifact() {
     return SpinnakerArtifact.KAYENTA;
+  }
+
+  @Override
+  protected void setProfile(Profile profile, DeploymentConfiguration deploymentConfiguration, SpinnakerRuntimeSettings endpoints) {
+    super.setProfile(profile, deploymentConfiguration, endpoints);
+    profile.appendContents(profile.getBaseContents());
   }
 }
