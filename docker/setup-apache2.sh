f9e29241f0faa6b243608cf17d5d18e8cf175b59
Lars Wander <lwander@users.noreply.github.com>
2018-04-06 15:50:43 -0400

    chore(docker): correct permissions on lib and run (#5130)

--- docker/setup-apache2.sh
+++ docker/setup-apache2.sh
@@ -3,3 +3,5 @@ apt-get install apache2 -y
 service apache2 stop
 a2enmod proxy proxy_ajp proxy_http rewrite deflate headers proxy_balancer proxy_connect proxy_html xml2enc
 chown -R www-data:www-data /etc/apache2
+chown -R www-data:www-data /var/lib/apache2
+chown -R www-data:www-data /var/run/apache2
