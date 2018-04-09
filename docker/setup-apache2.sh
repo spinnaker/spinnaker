67e5b1266df38782d6a0045c49837bdbbd275bbe
Lars Wander <lwander@users.noreply.github.com>
2018-04-09 08:48:21 -0400

    chore(docker): grant www-data access to apache logs (#5135)

--- docker/setup-apache2.sh
+++ docker/setup-apache2.sh
@@ -9,3 +9,6 @@ chown -R www-data:www-data /var/lib/apache2
 
 mkdir -p /var/run/apache2
 chown -R www-data:www-data /var/run/apache2
+
+mkdir -p /var/log/apache2
+chown -R www-data:www-data /var/log/apache2
