34d3cadd8b4dd27212aea743867869c411226bff
Scott Bloch-Wehba-Seaward <sbws@google.com>
2018-04-10 17:01:12 -0400

    fix(caching): caching agent metricsSupport being null obscures original exception (#2501)

--- clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/cache/CatsOnDemandCacheUpdater.groovy
+++ clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/cache/CatsOnDemandCacheUpdater.groovy
@@ -104,7 +104,9 @@ class CatsOnDemandCacheUpdater implements OnDemandCacheUpdater {
           log.info("$agent.providerName/$agent?.onDemandAgentType handled $type in ${TimeUnit.NANOSECONDS.toMillis(elapsed)} millis. Payload: $data")
         }
       } catch (e) {
-        agent.metricsSupport.countError()
+        if (agent.metricsSupport != null) {
+          agent.metricsSupport.countError()
+        }
         log.warn("$agent.providerName/$agent.onDemandAgentType failed to handle on demand update for $type", e)
       }
     }
