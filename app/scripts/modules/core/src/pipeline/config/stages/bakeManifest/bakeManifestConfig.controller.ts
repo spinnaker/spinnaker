ed2e7eb8dc1c9815ed844e0770167d6439851add
Lars Wander <lwander@users.noreply.github.com>
2018-04-25 13:08:36 -0400

    feat(bake/manifest): allow ui-specified value artifacts (#5232)

--- app/scripts/modules/core/src/pipeline/config/stages/bakeManifest/bakeManifestConfig.controller.ts
+++ app/scripts/modules/core/src/pipeline/config/stages/bakeManifest/bakeManifestConfig.controller.ts
@@ -22,10 +22,13 @@ export class BakeManifestConfigCtrl implements IController {
         expectedArtifacts: [{
           matchArtifact: {
             type: 'embedded/base64',
+            kind: 'base64',
             name: ''
-          }
+          },
+          defaultArtifact: {},
+          useDefaultArtifact: false,
         }],
-        inputArtifacts: [  BakeManifestConfigCtrl.defaultInputArtifact() ]
+        inputArtifacts: [ BakeManifestConfigCtrl.defaultInputArtifact() ]
       };
 
       Object.assign(this.$scope.stage, defaultSelection);
@@ -39,6 +42,14 @@ export class BakeManifestConfigCtrl implements IController {
       this.$scope.stage,
       this.$scope.$parent.pipeline,
     );
+  }
+
+  public hasValueArtifacts(): boolean {
+    if (!this.$scope.stage.inputArtifacts) {
+      this.$scope.stage.inputArtifacts = [ BakeManifestConfigCtrl.defaultInputArtifact() ];
+    }
+
+    return this.$scope.stage.inputArtifacts.length > 1;
   }
 
   public addInputArtifact() {
