1df856a23fa273173809987d8dad6a395ef880b5
Justin Reynolds <justinreynolds@gmail.com>
2018-04-25 10:11:46 -0700

    fix: API is no longer on ReactInjector

--- src/kayenta/service/metricsServiceMetadata.service.ts
+++ src/kayenta/service/metricsServiceMetadata.service.ts
@@ -1,5 +1,6 @@
-import { ReactInjector } from '@spinnaker/core';
+import { API } from '@spinnaker/core';
+
 import { IMetricsServiceMetadata } from 'kayenta/domain/IMetricsServiceMetadata';
 
 export const listMetricsServiceMetadata = (filter?: string, metricsAccountName?: string): Promise<IMetricsServiceMetadata[]> =>
-  ReactInjector.API.one('v2/canaries/metadata/metricsService').withParams({ filter, metricsAccountName }).get();
+  API.one('v2/canaries/metadata/metricsService').withParams({ filter, metricsAccountName }).get();
