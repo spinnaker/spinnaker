32f79c1f0d209afc1963fdafbac1c444fb81ed45
Jørgen Jervidalo <jorgen.jervidalo@schibsted.com>
2018-05-02 13:15:23 +0200

    feat(checklist): Support for using Map for key/value pairs in checklist

--- app/scripts/modules/core/src/forms/checklist/checklist.directive.js
+++ app/scripts/modules/core/src/forms/checklist/checklist.directive.js
@@ -2,88 +2,108 @@
 
 const angular = require('angular');
 
-module.exports = angular
-  .module('spinnaker.core.forms.checklist.checklist.directive', [])
-  .directive('checklist', function() {
-    return {
-      restrict: 'E',
-      templateUrl: require('./checklist.directive.html'),
-      scope: {
-        items: '=',
-        model: '=',
-        onChange: '&',
-        inline: '=',
-        includeSelectAllButton: '=',
-      },
-      link: function(scope) {
-        function initializeModelHolder() {
-          scope.model = scope.model || [];
-          scope.modelHolder = {};
-          scope.model.forEach(function(val) {
-            scope.modelHolder[val] = true;
-          });
-        }
+module.exports = angular.module('spinnaker.core.forms.checklist.checklist.directive', []).directive('checklist', () => {
+  return {
+    restrict: 'E',
+    templateUrl: require('./checklist.directive.html'),
+    scope: {
+      items: '=',
+      model: '=',
+      onChange: '&',
+      inline: '=',
+      includeSelectAllButton: '=',
+    },
+    link: scope => {
+      function initializeModelHolder() {
+        scope.values = {};
+        scope.itemsNormalized = [];
+        scope.model = scope.model || [];
+        scope.modelHolder = {};
+        scope.model.forEach(val => {
+          scope.modelHolder[val] = true;
+        });
+      }
 
-        function updateModel() {
-          var updatedModel = [];
-          scope.items.forEach(function(testKey) {
-            if (scope.modelHolder[testKey]) {
-              updatedModel.push(testKey);
-            }
-          });
-
-          angular.copy(updatedModel, scope.model);
-
-          if (scope.onChange) {
-            scope.$evalAsync(scope.onChange);
+      function updateModel() {
+        const updatedModel = [];
+        scope.itemsNormalized.forEach(testKey => {
+          if (scope.modelHolder[testKey]) {
+            updatedModel.push(testKey);
           }
-        }
+        });
 
-        function allItemsSelected() {
-          var allSelected = true;
-          scope.items.forEach(function(key) {
-            if (!scope.modelHolder[key]) {
-              allSelected = false;
-            }
+        angular.copy(updatedModel, scope.model);
+
+        if (scope.onChange) {
+          scope.$evalAsync(scope.onChange);
+        }
+      }
+
+      function allItemsSelected() {
+        let allSelected = true;
+        scope.itemsNormalized.forEach(key => {
+          if (!scope.modelHolder[key]) {
+            allSelected = false;
+          }
+        });
+        return allSelected;
+      }
+
+      scope.selectAllOrNone = () => {
+        if (allItemsSelected()) {
+          scope.itemsNormalized.forEach(key => {
+            scope.modelHolder[key] = false;
           });
-          return allSelected;
+        } else {
+          scope.itemsNormalized.forEach(key => {
+            scope.modelHolder[key] = true;
+          });
         }
+        updateModel();
+      };
 
-        scope.selectAllOrNone = function() {
-          if (allItemsSelected()) {
-            scope.items.forEach(function(key) {
-              scope.modelHolder[key] = false;
+      scope.selectButtonText = () => {
+        if (allItemsSelected()) {
+          return 'Deselect All';
+        }
+        return 'Select All';
+      };
+
+      scope.allItemsSelected = allItemsSelected;
+      scope.updateModel = updateModel;
+
+      scope.$watch('model', initializeModelHolder);
+
+      scope.$watch('items', (newOptions, oldOptions) => {
+        console.log(newOptions);
+        if (newOptions) {
+          if (_.isMap(newOptions)) {
+            newOptions.forEach((value, key) => {
+              scope.values[key] = value;
             });
+            scope.itemsNormalized = Array.from(newOptions, ([key]) => key);
           } else {
-            scope.items.forEach(function(key) {
-              scope.modelHolder[key] = true;
+            newOptions.forEach(item => {
+              scope.values[item] = item;
             });
+            scope.itemsNormalized = newOptions;
           }
-          updateModel();
-        };
-
-        scope.selectButtonText = function() {
-          if (allItemsSelected()) {
-            return 'Deselect All';
-          }
-          return 'Select All';
-        };
-
-        scope.allItemsSelected = allItemsSelected;
-        scope.updateModel = updateModel;
-
-        scope.$watch('model', initializeModelHolder);
-
-        scope.$watch('items', function(newOptions, oldOptions) {
-          if (oldOptions && oldOptions !== newOptions) {
-            oldOptions.forEach(function(oldOption) {
+        }
+        if (oldOptions && oldOptions !== newOptions) {
+          oldOptions.forEach(function(oldOption, oldKey) {
+            if (_.isMap(newOptions)) {
+              if (!newOptions.has(oldKey)) {
+                delete scope.modelHolder[oldKey];
+              }
+            } else {
               if (!newOptions.includes(oldOption)) {
                 delete scope.modelHolder[oldOption];
               }
-            });
-            updateModel();
-          }
-        });
-      },
-    };
-  });
+            }
+          });
+          updateModel();
+        }
+      });
+    },
+  };
+});
