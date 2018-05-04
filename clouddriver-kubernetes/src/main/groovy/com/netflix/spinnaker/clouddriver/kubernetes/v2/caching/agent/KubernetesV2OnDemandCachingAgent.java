5c6b2a4eca9d4d9e0292ab1791ec69183fc3a958
Lars Wander <lwander@users.noreply.github.com>
2018-05-04 14:20:15 -0400

    chore(provider/kubernetes): cache refresh debug logging (#2603)

--- clouddriver-kubernetes/src/main/groovy/com/netflix/spinnaker/clouddriver/kubernetes/v2/caching/agent/KubernetesV2OnDemandCachingAgent.java
+++ clouddriver-kubernetes/src/main/groovy/com/netflix/spinnaker/clouddriver/kubernetes/v2/caching/agent/KubernetesV2OnDemandCachingAgent.java
@@ -254,6 +254,9 @@ public abstract class KubernetesV2OnDemandCachingAgent extends KubernetesV2Cachi
     String name;
     KubernetesKind kind;
 
+    // todo(lwander): this can be removed
+    log.debug("Queried for on demand cache refresh of '{}'", data);
+
     try {
       Pair<KubernetesKind, String> parsedName = KubernetesManifest.fromFullResourceName(fullName);
       kind = parsedName.getLeft();
