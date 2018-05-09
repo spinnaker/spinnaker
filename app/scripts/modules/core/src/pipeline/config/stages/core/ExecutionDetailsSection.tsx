9bcc2627c1faae3849eacd37c1c1a2727b85d8ca
Chris Berry <chrisb@netflix.com>
2018-05-09 16:45:51 -0700

    refactor(core): handle TS issue with interface extension (#5300)

--- app/scripts/modules/core/src/pipeline/config/stages/core/ExecutionDetailsSection.tsx
+++ app/scripts/modules/core/src/pipeline/config/stages/core/ExecutionDetailsSection.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
-import { IExecutionDetailsComponentProps } from 'core/domain';
 
-export interface IExecutionDetailsSectionProps
-  extends IExecutionDetailsComponentProps,
-    IExecutionDetailsSectionWrapperProps {}
+import { Application } from 'core/application/application.model';
+import { IExecution } from 'core/domain/IExecution';
+import { IExecutionStage } from 'core/domain/IExecutionStage';
+import { IExecutionDetailsSection } from 'core/domain/IStageTypeConfig';
+
+export interface IExecutionDetailsSectionProps extends IExecutionDetailsSectionWrapperProps {
+  // NOTE: these are the fields from IExecutionDetailsComponentProps, but TS is not behaving in the library build,
+  // and does not recognize fields in a separate interface that this one extends when they are in a separate file, so
+  // we are copying them here
+  application: Application;
+  detailsSections: IExecutionDetailsSection[];
+  execution: IExecution;
+  provider: string;
+  stage: IExecutionStage;
+}
 
 export interface IExecutionDetailsSectionWrapperProps {
   children?: any;
