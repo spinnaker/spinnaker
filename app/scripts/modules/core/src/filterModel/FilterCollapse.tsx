4fbf69403f3e63db574322959667ae996e2f7e9d
Justin Reynolds <justinreynolds@gmail.com>
2018-05-10 10:16:55 -0700

    refactor(core): Create a react filter collapse button

---
+++ app/scripts/modules/core/src/filterModel/FilterCollapse.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { $rootScope } from 'ngimport';
+
+import { ReactInjector } from 'core/reactShims';
+import { Tooltip } from 'core/presentation';
+
+export class FilterCollapse extends React.Component<{}> {
+  private onClick = (pin: boolean) => {
+    ReactInjector.insightFilterStateModel.pinFilters(pin);
+    $rootScope.$apply(); // insight layout needs to change
+    this.setState({}); // force re-render since we are using insight filter state model to show the collapse button
+  };
+
+  public render() {
+    return (
+      <>
+        <h3 className="filters-placeholder">
+          <Tooltip value="Show filters">
+            <a className="btn btn-xs btn-default pin clickable" onClick={() => this.onClick(true)}>
+              <i className="fa fa-forward" />
+            </a>
+          </Tooltip>
+        </h3>
+        <Tooltip value="Hide filters">
+          <a
+            className="btn btn-xs btn-default pull-right unpin clickable"
+            onClick={() => this.onClick(false)}
+            style={{ display: ReactInjector.insightFilterStateModel.filtersExpanded ? 'inherit' : 'none' }}
+          >
+            <i className="fa fa-backward" />
+          </a>
+        </Tooltip>
+      </>
+    );
+  }
+}
--- app/scripts/modules/core/src/filterModel/index.ts
+++ app/scripts/modules/core/src/filterModel/index.ts
@@ -1,4 +1,5 @@
 export * from './FilterModelService';
+export * from './FilterCollapse';
 export * from './FilterTags';
 export * from './IFilterModel';
 export * from './dependentFilter/DependentFilterService';
