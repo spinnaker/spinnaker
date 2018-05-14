d9c0f272bfd3c177c37000eb9ef0e7b3fc7fc9e5
Lars Wander <lwander@users.noreply.github.com>
2018-05-14 11:08:23 -0400

    fix(deploy/local): add version to profile override (#941)

--- halyard-core/src/main/java/com/netflix/spinnaker/halyard/core/registry/v1/LocalDiskProfileReader.java
+++ halyard-core/src/main/java/com/netflix/spinnaker/halyard/core/registry/v1/LocalDiskProfileReader.java
@@ -20,21 +20,18 @@ package com.netflix.spinnaker.halyard.core.registry.v1;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yaml.snakeyaml.Yaml;
 
-
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
-import org.apache.commons.io.IOUtils;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.FileInputStream;
-
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -58,55 +55,78 @@ public class LocalDiskProfileReader implements ProfileReader {
 
   @Override
   public InputStream readProfile(String artifactName, String version, String profileName) throws IOException {
-    String path = profilePath(artifactName, profileName);
-    return getContents(path);
+    version = version.substring("local:".length());
+    try {
+      String path = profilePath(artifactName, version, profileName);
+      return getContents(path);
+    } catch (IOException e) {
+      log.debug("Failed to get profile, retrying default location", e);
+      String path = defaultProfilePath(artifactName, profileName);
+      return getContents(path);
+    }
   }
 
   @Override
   public BillOfMaterials readBom(String version) throws IOException {
-      if (!Versions.isLocal(version)) {
-        throw new IllegalArgumentException("Versions using a local BOM must be prefixed with \"local:\"");
-      }
-      String versionName = Versions.fromLocal(version);
-      String bomName = bomPath(versionName);
-      return relaxedObjectMapper.convertValue(
+    if (!Versions.isLocal(version)) {
+      throw new IllegalArgumentException("Versions using a local BOM must be prefixed with \"local:\"");
+    }
+    String versionName = Versions.fromLocal(version);
+    String bomName = bomPath(versionName);
+    return relaxedObjectMapper.convertValue(
         yamlParser.load(getContents(bomName)),
         BillOfMaterials.class
-      );
+    );
   }
 
   @Override
   public Versions readVersions() throws IOException {
-      throw new UnsupportedOperationException();
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public InputStream readArchiveProfile(String artifactName, String version, String profileName) throws IOException {
-      Path profilePath = Paths.get(profilePath(artifactName, profileName));
+    version = version.substring("local:".length());
 
-      ByteArrayOutputStream os = new ByteArrayOutputStream();
-      TarArchiveOutputStream tarArchive = new TarArchiveOutputStream(os);
+    try {
+      Path profilePath = Paths.get(profilePath(artifactName, version, profileName));
+      return readArchiveProfileFrom(profilePath);
+    } catch (IOException e) {
+      log.debug("Failed to get archive profile, retrying default location", e);
+      Path profilePath = Paths.get(defaultProfilePath(artifactName, profileName));
+      return readArchiveProfileFrom(profilePath);
+    }
+  }
 
-      ArrayList<Path> filePathsToAdd =
+  public InputStream readArchiveProfileFrom(Path profilePath) throws IOException {
+
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    TarArchiveOutputStream tarArchive = new TarArchiveOutputStream(os);
+
+    ArrayList<Path> filePathsToAdd =
         java.nio.file.Files.walk(profilePath, Integer.MAX_VALUE, FileVisitOption.FOLLOW_LINKS)
-        .filter(path -> path.toFile().isFile())
-        .collect(Collectors.toCollection(ArrayList::new));
+            .filter(path -> path.toFile().isFile())
+            .collect(Collectors.toCollection(ArrayList::new));
 
-      for (Path path : filePathsToAdd) {
+    for (Path path : filePathsToAdd) {
       TarArchiveEntry tarEntry = new TarArchiveEntry(path.toFile(), profilePath.relativize(path).toString());
       tarArchive.putArchiveEntry(tarEntry);
       IOUtils.copy(Files.newInputStream(path), tarArchive);
       tarArchive.closeArchiveEntry();
-      }
+    }
 
-      tarArchive.finish();
-      tarArchive.close();
+    tarArchive.finish();
+    tarArchive.close();
 
-      return new ByteArrayInputStream(os.toByteArray());
+    return new ByteArrayInputStream(os.toByteArray());
   }
 
-  private String profilePath(String artifactName, String profileFileName) {
-      return Paths.get(localBomPath, artifactName, profileFileName).toString();
+  private String profilePath(String artifactName, String version, String profileFileName) {
+    return Paths.get(localBomPath, artifactName, version, profileFileName).toString();
+  }
+
+  private String defaultProfilePath(String artifactName, String profileFileName) {
+    return Paths.get(localBomPath, artifactName, profileFileName).toString();
   }
 
   String bomPath(String version) {
@@ -114,8 +134,8 @@ public class LocalDiskProfileReader implements ProfileReader {
   }
 
   private InputStream getContents(String objectName) throws IOException {
-      log.info("Getting file contents of " + objectName);
-      return new FileInputStream(objectName);
-    }
-
+    log.info("Getting file contents of " + objectName);
+    return new FileInputStream(objectName);
   }
+
+}
