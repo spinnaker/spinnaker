db4b37161622081f560b808fe93206fbb0b8d387
Scott Bloch-Wehba-Seaward <sbws@google.com>
2018-05-18 10:25:52 -0400

    feat(artifacts): type icons in artifact selectors (#5346)

--- app/scripts/modules/core/src/artifact/expectedArtifactMultiSelector.component.ts
+++ app/scripts/modules/core/src/artifact/expectedArtifactMultiSelector.component.ts
@@ -2,6 +2,9 @@ import { IComponentOptions, IController, module } from 'angular';
 
 import { IExpectedArtifact } from 'core/domain';
 import { EXPECTED_ARTIFACT_SERVICE, ExpectedArtifactService } from './expectedArtifact.service';
+import { ArtifactIconService } from './ArtifactIconService';
+
+import './artifactSelector.less';
 
 class ExpectedArtifactMultiSelectorCtrl implements IController {
   public command: any;
@@ -9,14 +12,30 @@ class ExpectedArtifactMultiSelectorCtrl implements IController {
   public label: string;
   public expectedArtifacts: IExpectedArtifact[];
   public helpFieldKey: string;
+  public showIcons: boolean;
 
   constructor(public expectedArtifactService: ExpectedArtifactService) {
     'ngInject';
   }
+
+  public iconPath(expected: IExpectedArtifact): string {
+    const artifact = expected.matchArtifact || expected.defaultArtifact;
+    if (artifact == null) {
+      return '';
+    }
+    return ArtifactIconService.getPath(artifact.type);
+  }
 }
 
 class ExpectedArtifactMultiSelectorComponent implements IComponentOptions {
-  public bindings: any = { command: '=', expectedArtifacts: '<', artifactLabel: '@', idsField: '@', helpFieldKey: '@' };
+  public bindings: any = {
+    command: '=',
+    expectedArtifacts: '<',
+    artifactLabel: '@',
+    idsField: '@',
+    helpFieldKey: '@',
+    showIcons: '<',
+  };
   public controller: any = ExpectedArtifactMultiSelectorCtrl;
   public controllerAs = 'ctrl';
   public template = `
@@ -25,9 +44,13 @@ class ExpectedArtifactMultiSelectorComponent implements IComponentOptions {
           <stage-config-field label="{{ctrl.artifactLabel}}" help-key="{{ctrl.helpFieldKey}}">
             <ui-select multiple
                        ng-model="ctrl.command[ctrl.idsField]"
-                       class="form-control input-sm">
-              <ui-select-match>{{ $item | summarizeExpectedArtifact }}</ui-select-match>
+                       class="form-control input-sm expected-artifact-multi-selector">
+              <ui-select-match>
+                <img ng-if="ctrl.showIcons" width="16" height="16" class="artifact-icon" ng-src="{{ ctrl.iconPath($item) }}" />
+                {{ $item | summarizeExpectedArtifact }}
+              </ui-select-match>
               <ui-select-choices repeat="expected.id as expected in ctrl.expectedArtifacts">
+                <img ng-if="ctrl.showIcons" width="16" height="16" class="artifact-icon" ng-src="{{ ctrl.iconPath(expected) }}" />
                 <span>{{ expected | summarizeExpectedArtifact }}</span>
               </ui-select-choices>
             </ui-select>
--- app/scripts/modules/core/src/artifact/expectedArtifactSelector.component.ts
+++ app/scripts/modules/core/src/artifact/expectedArtifactSelector.component.ts
@@ -3,6 +3,9 @@ import { IComponentOptions, IController, module } from 'angular';
 import { IExpectedArtifact } from 'core/domain';
 import { IAccount } from 'core/account';
 import { EXPECTED_ARTIFACT_SERVICE, ExpectedArtifactService } from './expectedArtifact.service';
+import { ArtifactIconService } from './ArtifactIconService';
+
+import './artifactSelector.less';
 
 class ExpectedArtifactSelectorCtrl implements IController {
   public command: any;
@@ -11,9 +14,18 @@ class ExpectedArtifactSelectorCtrl implements IController {
   public accounts: IAccount[];
   public expectedArtifacts: IExpectedArtifact[];
   public helpFieldKey: string;
+  public showIcons: boolean;
 
   constructor(public expectedArtifactService: ExpectedArtifactService) {
     'ngInject';
+  }
+
+  public iconPath(expected: IExpectedArtifact): string {
+    const artifact = expected.matchArtifact || expected.defaultArtifact;
+    if (artifact == null) {
+      return '';
+    }
+    return ArtifactIconService.getPath(artifact.type);
   }
 }
 
@@ -25,6 +37,7 @@ class ExpectedArtifactSelectorComponent implements IComponentOptions {
     account: '=',
     accounts: '<',
     helpFieldKey: '@',
+    showIcons: '<',
   };
   public controller: any = ExpectedArtifactSelectorCtrl;
   public controllerAs = 'ctrl';
@@ -33,10 +46,14 @@ class ExpectedArtifactSelectorComponent implements IComponentOptions {
       <ng-form name="artifact">
         <stage-config-field label="Expected Artifact" help-key="{{ctrl.helpFieldKey}}">
           <ui-select ng-model="ctrl.id"
-                     class="form-control input-sm" required>
-            <ui-select-match>{{ $select.selected | summarizeExpectedArtifact }}</ui-select-match>
+                     class="form-control input-sm expected-artifact-selector" required>
+            <ui-select-match>
+              <img ng-if="ctrl.showIcons" width="16" height="16" class="artifact-icon" ng-src="{{ ctrl.iconPath($select.selected) }}" />
+              {{ $select.selected | summarizeExpectedArtifact }}
+            </ui-select-match>
             <ui-select-choices repeat="expected.id as expected in ctrl.expectedArtifacts">
-               <span>{{ expected | summarizeExpectedArtifact }}</span>
+              <img ng-if="ctrl.showIcons" width="16" height="16" class="artifact-icon" ng-src="{{ ctrl.iconPath(expected) }}" />
+              <span>{{ expected | summarizeExpectedArtifact }}</span>
             </ui-select-choices>
           </ui-select>
         </stage-config-field>
@@ -51,7 +68,7 @@ class ExpectedArtifactSelectorComponent implements IComponentOptions {
           </ui-select>
         </stage-config-field>
       </ng-form>
-     </render-if-feature>
+    </render-if-feature>
   `;
 }
 
