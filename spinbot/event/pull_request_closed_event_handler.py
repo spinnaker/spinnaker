55759a36fad946248a910372d148a8f865996980
Lars Wander <lwander@google.com>
2018-05-21 13:46:12 -0400

    feat(spinbot): pull request metrics

---
+++ spinbot/event/pull_request_closed_event_handler.py
@@ -0,0 +1,43 @@
+import re
+from datetime import datetime
+from .pull_request_event import GetPullRequest
+from .handler import Handler
+
+class PullRequestClosedEventHandler(Handler):
+    def __init__(self):
+        super().__init__()
+
+    def handles(self, event):
+        return (event.type == 'PullRequestEvent'
+            and event.payload.get('action') == 'closed')
+
+    def handle(self, g, event):
+        pull_request = GetPullRequest(g, event)
+        if not pull_request:
+            log.warn('Unable to determine pull request for {}'.format(event))
+            return
+
+        merged = pull_request.merged
+        age = pull_request.closed_at - pull_request.created_at
+        change_size = pull_request.additions + pull_request.deletions
+        files_changed_count = pull_request.changed_files
+        comments = pull_request.comments
+
+        approved = next((r for r in pull_request.get_reviews() if r.state == 'APPROVED'), None) is not None
+
+        self.monitoring_db.write('pull_request_closed',
+            {
+                'count': 1,
+                'age_days': age.days,
+                'loc_delta': change_size,
+                'files_changed_count': files_changed_count,
+                'comments': comments
+            },
+            tags={
+                'repo': event.repo.name,
+                'approved': approved,
+                'merged': merged
+            }
+        )
+
+PullRequestClosedEventHandler()
--- spinbot/manifests/create.sh
+++ spinbot/manifests/create.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl create namespace spinbot
-kubectl create secret generic -n spinbot token --from-file ~/.spinbot/token
-kubectl create secret generic -n spinbot gcs.creds --from-file ~/.gcp/account.json
-kubectl apply -f deploy.yml
+kubectl --context utils-cluster create namespace spinbot
+kubectl --context utils-cluster create secret generic -n spinbot token --from-file ~/.spinbot/token
+kubectl --context utils-cluster create secret generic -n spinbot gcs.creds --from-file ~/.gcp/account.json
+kubectl --context utils-cluster apply -f deploy.yml
