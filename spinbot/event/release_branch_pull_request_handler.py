601eaa0c021d7c0ce4cae98546ed2caa155eb223
Lars Wander <lwander@users.noreply.github.com>
2018-05-21 08:41:53 -0400

    fix(spinbot): read commit messages, not PR titles (#2804)

--- spinbot/event/release_branch_pull_request_handler.py
+++ spinbot/event/release_branch_pull_request_handler.py
@@ -2,14 +2,19 @@ from .handler import Handler
 from .pull_request_event import GetBaseBranch, GetPullRequest, GetTitle
 from gh import ReleaseBranchFor, ParseCommitMessage
 
-close_message = ('Only fixes can be merged into ' + 
-    'release branches. This is currently tagged as "{}" which doesn\'t qualify.\n\nRead more ' +
-    'about [commit conventions](https://www.spinnaker.io/community/contributing/submitting/#commit-message-conventions) ' +
+format_message = ('Features cannot be merged into release branches. The following commits ' +
+    'are not tagged as one of "{}":\n\n{}\n\n' +
+    'Read more about [commit conventions](https://www.spinnaker.io/community/contributing/submitting/#commit-message-conventions) ' +
     'and [patch releases](https://www.spinnaker.io/community/releases/release-cadence/#patching-the-release-candidate) here.')
 
 class ReleaseBranchPullRequestHandler(Handler):
     def __init__(self):
         super().__init__()
+        self.omit_repos = self.config.get('omit_repos', [])
+        self.allowed_types = self.config.get(
+            'allowed_types',
+            ['fix', 'chore', 'docs', 'test']
+        )
 
     def handles(self, event):
         return (event.type == 'PullRequestEvent'
@@ -17,17 +22,28 @@ class ReleaseBranchPullRequestHandler(Handler):
             and ReleaseBranchFor(GetBaseBranch(event)) != None)
 
     def handle(self, g, event):
-        message = ParseCommitMessage(GetTitle(event))
-        if message is None:
-            message = {}
+        repo = GetRepo(event)
+        if repo in self.omit_repos:
+            self.logging.info('Skipping {} because it\'s in omitted repo {}'.format(event, repo))
+            return
 
-        if message.get('type') != 'fix':
-            pull_request = GetPullRequest(g, event)
-            if pull_request is None:
-                self.logging.warn('Unable to determine PR that created {}'.format(event))
-                return
+        pull_request = GetPullRequest(g, event)
+        if pull_request is None:
+            self.logging.warn('Unable to determine PR that created {}'.format(event))
+            return
 
-            pull_request.create_issue_comment(close_message.format(message.get('type')))
-            pull_request.edit(state='closed')
+        commits = pull_request.get_commits()
+        bad_commits = []
+
+        for commit in commits:
+            message = ParseCommitMessage(commit.commit.message)
+            if message is None or message.get('type') not in self.allowed_types:
+                bad_commits.append(commit.commit)
+
+        if len(bad_commits) > 0:
+            pull_request.create_issue_comment(format_message.format(
+                ', '.join(self.allowed_types)
+                ''.join(map(lambda c: '{}: {}'.format(c.sha, c.message), bad_commits))
+            ))
 
 ReleaseBranchPullRequestHandler()
