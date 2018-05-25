4aaeb3fb4f4430126aef19a61a928e629c7cf337
Eric Zimanyi <ezimanyi@google.com>
2018-05-25 13:13:28 -0400

    fix(caching): Fix on-demand cache result return status (#2663)
    When multiple caching agents handle an on-demand refresh, the return
    status is currently based only on the result of the last agent.  Instead
    we should return a PENDING result if *any* of the caching agents find
    results, rather than only if that last one does.

--- clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/cache/CatsOnDemandCacheUpdater.groovy
+++ clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/cache/CatsOnDemandCacheUpdater.groovy
@@ -84,12 +84,12 @@ class CatsOnDemandCacheUpdater implements OnDemandCacheUpdater {
             continue;
           }
           if (!agent.metricsSupport) {
-            hasOnDemandResults = false
             continue;
           }
           if (result.cacheResult) {
-            hasOnDemandResults = !(result.cacheResult.cacheResults ?: [:]).values().flatten().isEmpty() && !agentScheduler.atomic
-            if (hasOnDemandResults) {
+            boolean agentHasOnDemandResults = !(result.cacheResult.cacheResults ?: [:]).values().flatten().isEmpty() && !agentScheduler.atomic
+            if (agentHasOnDemandResults) {
+              hasOnDemandResults = true;
               result.cacheResult.cacheResults.each { k, v ->
                 if (v) {
                   cachedIdentifiersByType[k].addAll(v*.id)
