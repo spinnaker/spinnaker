f875ec13de04967f30a0b530d2481d689e315e8b
Lars Wander <lwander@users.noreply.github.com>
2018-06-08 12:29:19 -0400

    fix(provider/kubernetes): add cachesThreads parameter (#967)

--- halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/providers/kubernetes/KubernetesAccount.java
+++ halyard-config/src/main/java/com/netflix/spinnaker/halyard/config/model/v1/providers/kubernetes/KubernetesAccount.java
@@ -50,6 +50,7 @@ public class KubernetesAccount extends ContainerAccount implements Cloneable {
   @ValidForSpinnakerVersion(lowerBound = "1.5.0", message = "Spinnaker does not support configuring this behavior before that version.")
   Boolean configureImagePullSecrets;
   Boolean serviceAccount;
+  int cacheThreads = 1;
   List<String> namespaces = new ArrayList<>();
   List<String> omitNamespaces = new ArrayList<>();
   @ValidForSpinnakerVersion(lowerBound = "1.7.0", message = "Configuring kind caching behavior is not supported yet.")
