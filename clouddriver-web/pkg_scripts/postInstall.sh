4cedf7424d9487b93906a8cbfa48408bfcb35cb6
Lars Wander <lwander@users.noreply.github.com>
2018-06-20 12:41:15 -0400

    fix(install): install kubectl if missing (#2728)
    * fix(install): install kubectl if not already installed
    
    * fix(install): install kubectl if missing (#2727)

--- clouddriver-web/pkg_scripts/postInstall.sh
+++ clouddriver-web/pkg_scripts/postInstall.sh
@@ -11,4 +11,15 @@ if [ -z `getent passwd spinnaker` ]; then
   useradd --gid spinnaker spinnaker -m --home-dir /home/spinnaker
 fi
 
+install_kubectl() {
+  if [ -z `which kubectl` ]; then
+    wget https://storage.googleapis.com/kubernetes-release/release/stable.txt && wget https://storage.googleapis.com/kubernetes-release/release/$(cat stable.txt)/bin/linux/amd64/kubectl
+    rm stable.txt
+    chmod +x kubectl
+    mv ./kubectl /usr/local/bin/kubectl
+  fi
+}
+
+install_kubectl
+
 install --mode=755 --owner=spinnaker --group=spinnaker --directory  /var/log/spinnaker/clouddriver 
