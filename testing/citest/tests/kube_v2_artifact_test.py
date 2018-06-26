0f85366e7ea19b9cc8bab7eb30aefb1f3752980f
Lars Wander <lwander@google.com>
2018-06-26 15:09:33 -0400

    test(provider/kubernetes): v2 artifact tests
    This is just the start, will test a few more flows in subsequent PRs

---
+++ testing/citest/tests/kube_v2_artifact_test.py
@@ -0,0 +1,290 @@
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Integration test to see if the image promotion process is working for the
+Spinnaker Kubernetes V2 integration.
+"""
+
+# Standard python modules.
+import sys
+import random
+import string
+
+# citest modules.
+import citest.kube_testing as kube
+import citest.json_contract as jc
+import citest.json_predicate as jp
+import citest.service_testing as st
+
+# Spinnaker modules.
+import spinnaker_testing as sk
+import spinnaker_testing.gate as gate
+import spinnaker_testing.frigga as frigga
+import citest.base
+
+ov_factory = jc.ObservationPredicateFactory()
+
+class KubeV2ArtifactTestScenario(sk.SpinnakerTestScenario):
+  """Defines the scenario for the kube v2 artifact test.
+  """
+
+  @classmethod
+  def new_agent(cls, bindings):
+    """Implements citest.service_testing.AgentTestScenario.new_agent."""
+    agent = gate.new_agent(bindings)
+    agent.default_max_wait_secs = 180
+    return agent
+
+  @classmethod
+  def initArgumentParser(cls, parser, defaults=None):
+    """Initialize command line argument parser.
+
+    Args:
+      parser: argparse.ArgumentParser
+    """
+    super(KubeV2ArtifactTestScenario, cls).initArgumentParser(
+        parser, defaults=defaults)
+
+    defaults = defaults or {}
+    parser.add_argument(
+      '--test_namespace', default='default',
+      help='The namespace to manage within the tests.')
+
+  def __init__(self, bindings, agent=None):
+    """Constructor.
+
+    Args:
+      bindings: [dict] The data bindings to use to configure the scenario.
+      agent: [GateAgent] The agent for invoking the test operations on Gate.
+    """
+    super(KubeV2ArtifactTestScenario, self).__init__(bindings, agent)
+    bindings = self.bindings
+
+    # We'll call out the app name because it is widely used
+    # because it scopes the context of our activities.
+    # pylint: disable=invalid-name
+    self.TEST_APP = bindings['TEST_APP']
+
+    # Take just the first if there are multiple
+    # because some uses below assume just one.
+    self.TEST_NAMESPACE = bindings['TEST_NAMESPACE'].split(',')[0]
+
+  def create_app(self):
+    """Creates OperationContract that creates a new Spinnaker Application."""
+    contract = jc.Contract()
+    return st.OperationContract(
+        self.agent.make_create_app_operation(
+            bindings=self.bindings, application=self.TEST_APP,
+            account_name=self.bindings['SPINNAKER_KUBERNETES_V2_ACCOUNT']),
+        contract=contract)
+
+  def delete_app(self):
+    """Creates OperationContract that deletes a new Spinnaker Application."""
+    contract = jc.Contract()
+    return st.OperationContract(
+        self.agent.make_delete_app_operation(
+            application=self.TEST_APP,
+            account_name=self.bindings['SPINNAKER_KUBERNETES_V2_ACCOUNT']),
+        contract=contract)
+
+  def __not_found_observation_predicate(self, title='Not Found Permitted'):
+    return ov_factory.error_list_contains(st.CliAgentRunErrorPredicate(
+      title=title, error_regex='.* not found'))
+
+  def __deployment_image_predicate(self, image):
+    return ov_factory.value_list_contains(jp.DICT_MATCHES({
+         'spec': jp.DICT_MATCHES({
+             'template': jp.DICT_MATCHES({
+                 'spec': jp.DICT_MATCHES({
+                     'containers': jp.LIST_MATCHES([
+                         jp.DICT_MATCHES({ 'image': jp.STR_EQ(image) })
+                     ])
+                 })
+             })
+         })
+     }))
+
+  def __deployment_manifest(self, name, image):
+    return {
+        'apiVersion': 'apps/v1beta2',
+        'kind': 'Deployment',
+        'metadata': {
+            'name': name,
+            'namespace': self.TEST_NAMESPACE,
+            'labels': {
+                'app': self.TEST_APP,
+            }
+        },
+        'spec': {
+            'replicas': 1,
+            'selector': {
+                'matchLabels': {
+                    'app': self.TEST_APP,
+                }
+            },
+            'template': {
+                'metadata': {
+                    'labels': {
+                        'app': self.TEST_APP,
+                    }
+                },
+                'spec': {
+                    'containers': [{
+                        'name': 'primary',
+                        'image': image,
+                    }]
+                }
+            }
+        }
+    }
+
+  def __docker_image_artifact(self, name, image):
+    id_ = ''.join(random.choice(string.ascii_uppercase + string.digits) 
+                  for _ in range(10))
+    return {
+      'type': 'docker/image',
+      'name': name,
+      'reference': image,
+      'uuid': id_
+    }
+
+  def deploy_deployment_with_docker_artifact(self, image):
+    """Creates OperationContract for deploying and substituting one image into 
+    a Deployment object
+
+    To verify the operation, we just check that the deployment was created with
+    the correct image.
+    """
+    bindings = self.bindings
+    name = self.TEST_APP + '-deployment'
+    image_name = 'placeholder'
+    docker_artifact = self.__docker_image_artifact(image_name, image)
+    payload = self.agent.make_json_payload_from_kwargs(
+        job=[{
+            'cloudProvider': 'kubernetes',
+            'moniker': {
+                'app': self.TEST_APP
+            },
+            'account': bindings['SPINNAKER_KUBERNETES_V2_ACCOUNT'],
+            'source': 'text',
+            'type': 'deployManifest',
+            'user': '[anonymous]',
+            'manifests': [self.__deployment_manifest(name, image_name)],
+            'artifacts': [docker_artifact]
+        }],
+        description='Deploy manifest',
+        application=self.TEST_APP)
+
+    builder = kube.KubeContractBuilder(self.kube_v2_observer)
+    (builder.new_clause_builder('Deployment created',
+                                retryable_for_secs=15)
+     .get_resources(
+         'deploy',
+         extra_args=[name, '--namespace', self.TEST_NAMESPACE])
+     .EXPECT(self.__deployment_image_predicate(image)))
+
+    return st.OperationContract(
+        self.new_post_operation(
+            title='deploy_manifest', data=payload, path='tasks'),
+        contract=builder.build())
+
+  def delete_manifest(self):
+    """Creates OperationContract for deleteManifest
+
+    To verify the operation, we just check that the Kubernetes deployment
+    is no longer visible (or is in the process of terminating).
+    """
+    bindings = self.bindings
+    name = self.TEST_APP + '-deployment'
+    payload = self.agent.make_json_payload_from_kwargs(
+        job=[{
+            'cloudProvider': 'kubernetes',
+            'type': 'deleteManifest',
+            'account': bindings['SPINNAKER_KUBERNETES_V2_ACCOUNT'],
+            'user': '[anonymous]',
+            'kinds': [ 'deployment' ],
+            'location': self.TEST_NAMESPACE,
+            'options': { },
+            'labelSelectors': {
+                'selectors': [{
+                    'kind': 'EQUALS',
+                    'key': 'app',
+                    'values': [ self.TEST_APP ]
+                }]
+            }
+        }],
+        application=self.TEST_APP,
+        description='Destroy Manifest')
+
+    builder = kube.KubeContractBuilder(self.kube_v2_observer)
+    (builder.new_clause_builder('Replica Set Removed')
+     .get_resources(
+         'deployment',
+         extra_args=[name, '--namespace', self.TEST_NAMESPACE])
+     .EXPECT(self.__not_found_observation_predicate()))
+
+    return st.OperationContract(
+        self.new_post_operation(
+            title='delete_manifest', data=payload, path='tasks'),
+        contract=builder.build())
+
+class KubeV2ArtifactTest(st.AgentTestCase):
+  """The test fixture for the KubeV2ArtifactTest.
+
+  This is implemented using citest OperationContract instances that are
+  created by the KubeV2ArtifactTestScenario.
+  """
+  # pylint: disable=missing-docstring
+
+  @property
+  def scenario(self):
+    return citest.base.TestRunner.global_runner().get_shared_data(
+        KubeV2ArtifactTestScenario)
+
+  def test_a_create_app(self):
+    self.run_test_case(self.scenario.create_app())
+
+  def test_b_deploy_deployment_with_docker_artifact(self):
+    self.run_test_case(self.scenario.deploy_deployment_with_docker_artifact('library/nginx'))
+
+  def test_b_update_deployment_with_docker_artifact(self):
+    self.run_test_case(self.scenario.deploy_deployment_with_docker_artifact('library/redis'))
+
+  def test_y_delete_manifest(self):
+    self.run_test_case(self.scenario.delete_manifest(), max_retries=2)
+
+  def test_z_delete_app(self):
+    # Give a total of a minute because it might also need
+    # an internal cache update
+    self.run_test_case(self.scenario.delete_app(),
+                       retry_interval_secs=8, max_retries=8)
+
+
+def main():
+  """Implements the main method running this artifact test."""
+
+  defaults = {
+      'TEST_STACK': 'tst',
+      'TEST_APP': 'kubv2arti' + KubeV2ArtifactTestScenario.DEFAULT_TEST_ID
+  }
+
+  return citest.base.TestRunner.main(
+      parser_inits=[KubeV2ArtifactTestScenario.initArgumentParser],
+      default_binding_overrides=defaults,
+      test_case_list=[KubeV2ArtifactTest])
+
+
+if __name__ == '__main__':
+  sys.exit(main())
