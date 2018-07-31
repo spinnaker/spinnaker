94f2012c562054a507ce34cc742fc7d7808e2270
Justin Reynolds <justinreynolds@gmail.com>
2018-07-31 16:25:14 -0700

    refactor(core): Create react version of TaskReason

--- app/scripts/modules/core/src/task/index.ts
+++ app/scripts/modules/core/src/task/index.ts
@@ -1,5 +1,6 @@
 export * from './PlatformHealthOverrideMessage';
 export * from './monitor/TaskMonitor';
+export * from './modal/TaskReason';
 export * from './task.read.service';
 export * from './taskExecutor';
 export * from './verification';
---
+++ app/scripts/modules/core/src/task/modal/TaskReason.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+export interface ITaskReasonProps {
+  onChange: (reason: string) => void;
+  reason: string;
+}
+
+export class TaskReason extends React.Component<ITaskReasonProps> {
+  public render() {
+    return (
+      <div className="row" style={{ marginTop: '10px', marginBottom: '10px' }}>
+        <div className="col-md-3 sm-label-right">Reason</div>
+        <div className="col-md-7">
+          <textarea
+            className="form-control"
+            value={this.props.reason}
+            onChange={event => this.props.onChange(event.target.value)}
+            ng-model="vm.command.reason"
+            rows={3}
+            placeholder="(Optional) anything that might be helpful to explain the reason for this change; HTML is okay"
+          />
+        </div>
+      </div>
+    );
+  }
+}
