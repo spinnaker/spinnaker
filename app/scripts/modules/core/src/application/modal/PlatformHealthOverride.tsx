06cd9d0ad7cf25855f0107ab09f56cb4820509ec
Justin Reynolds <justinreynolds@gmail.com>
2018-07-31 16:25:36 -0700

    refactor(core): Create react version of platform health override

---
+++ app/scripts/modules/core/src/application/modal/PlatformHealthOverride.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { isEqual } from 'lodash';
+
+import { IServerGroupCommand } from 'core/serverGroup';
+import { HelpField } from 'core/help/HelpField';
+
+export interface IPlatformHealthOverrideProps {
+  command: IServerGroupCommand;
+  onChange: (healthProviderNames: string[]) => void;
+  platformHealthType: string;
+  showHelpDetails?: boolean;
+}
+
+export class PlatformHealthOverride extends React.Component<IPlatformHealthOverrideProps> {
+  private clicked = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const interestingHealthProviderNames = event.target.checked ? [this.props.platformHealthType] : null;
+    this.props.onChange(interestingHealthProviderNames);
+  };
+
+  public render() {
+    return (
+      <div className="checkbox">
+        <label>
+          <input
+            type="checkbox"
+            checked={isEqual(this.props.command.interestingHealthProviderNames, [this.props.platformHealthType])}
+            onChange={this.clicked}
+          />
+          Consider only {this.props.platformHealthType} health
+        </label>
+        <HelpField id="application.platformHealthOnly" expand={this.props.showHelpDetails} />
+      </div>
+    );
+  }
+}
