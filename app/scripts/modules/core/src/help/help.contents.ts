672e47f7893eca8e68b120d960dab5451212502b
kskewes <karl.skewes@gmail.com>
2018-08-01 10:27:04 +1200

    docs(artifacts): GitLab example URL typo (#5525)
    * 1. Add missing slash after projects in Gitlab example URL
    2. Change example URL from org/repo to Gitlab project :id to match
    upstream docs.
    
    * Update help.contents.ts
    
    Revert change to Project :ID.
    This PR is now just doc typo fix, adding a missing slash. :)
    
    * Correct typo in Gitlab example url #5525

--- app/scripts/modules/core/src/help/help.contents.ts
+++ app/scripts/modules/core/src/help/help.contents.ts
@@ -135,7 +135,7 @@ const helpContents: { [key: string]: string } = {
       <p>Either the commit or branch to checkout.</p>`,
   'pipeline.config.expectedArtifact.defaultGitlab.reference': `
       <p>The Gitlab API file url the artifact lives under. The domain name may change if you're running your own Gitlab server. The repository and path to files must be URL encoded.</p>
-      <p>An example is <code>https://gitlab.com/api/v4/projects$ORG%2F$REPO/repository/files/path%2Fto%2Ffile.yml/raw</code>. See <a href="https://www.spinnaker.io/reference/artifacts/types/gitlab-file/#fields">our docs</a> for more info.</p>`,
+      <p>An example is <code>https://gitlab.com/api/v4/projects/$ORG%2F$REPO/repository/files/path%2Fto%2Ffile.yml/raw</code>. See <a href="https://www.spinnaker.io/reference/artifacts/types/gitlab-file/#fields">our docs</a> for more info.</p>`,
   'pipeline.config.expectedArtifact.defaultBitbucket.reference': `
       <p>The Bitbucket API file url the artifact lives under. The domain name may change if you're running your own Bitbucket server. The repository and path to files must be URL encoded.</p>
       <p>An example is <code>https://api.bitbucket.org/1.0/repositories/$ORG/$REPO/raw/$VERSION/$FILEPATH</code>. See <a href="https://www.spinnaker.io/reference/artifacts/types/bitbucket-file/#fields">our docs</a> for more info.</p>`,
