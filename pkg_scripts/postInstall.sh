b7116d69fb52b2b74c12e80379f5684b6fd00450
Eric Wiseblatt <ewiseblatt@google.com>
2018-08-06 20:20:39 +0000

    chore(install): Removed obsolete deprecated scripts and supporting libraries.

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-# This doesnt belong here, but I cannot figure out where it goes
-# and how other files have it set.
-chmod +x /opt/spinnaker/install/first_google_boot.sh
-
-# Deprecated. Will be removed in the future.
-if [ `readlink -f /opt/spinnaker/scripts` != "/opt/spinnaker/bin" ]; then
-  ln -s /opt/spinnaker/bin /opt/spinnaker/scripts
-fi
-
 if [ ! -f /opt/spinnaker/config/spinnaker-local.yml ]; then
   # Create master config on original install, but leave in place on upgrades.
   cp /opt/spinnaker/config/default-spinnaker-local.yml /opt/spinnaker/config/spinnaker-local.yml
 fi
-
-# deck settings
-/opt/spinnaker/bin/reconfigure_spinnaker.sh
 
 # enable deck and reverse proxy in apache
 /usr/sbin/a2ensite spinnaker
