ff18f2ba53efafd44b48a53fdf9433dc0e19a704
Adam Jordens <adam@jordens.org>
2018-08-08 17:37:25 -0700

    fix(entitytags): Bit of logging around the `UpsertEntityTagsAtomicOperation` (#2845)

--- clouddriver-elasticsearch/src/main/java/com/netflix/spinnaker/clouddriver/elasticsearch/ops/UpsertEntityTagsAtomicOperation.java
+++ clouddriver-elasticsearch/src/main/java/com/netflix/spinnaker/clouddriver/elasticsearch/ops/UpsertEntityTagsAtomicOperation.java
@@ -17,17 +17,23 @@
 package com.netflix.spinnaker.clouddriver.elasticsearch.ops;
 
 import com.netflix.spinnaker.clouddriver.core.services.Front50Service;
+import com.netflix.spinnaker.clouddriver.data.task.Task;
+import com.netflix.spinnaker.clouddriver.data.task.TaskRepository;
 import com.netflix.spinnaker.clouddriver.elasticsearch.descriptions.BulkUpsertEntityTagsDescription;
 import com.netflix.spinnaker.clouddriver.elasticsearch.descriptions.UpsertEntityTagsDescription;
 import com.netflix.spinnaker.clouddriver.elasticsearch.model.ElasticSearchEntityTagsProvider;
+import com.netflix.spinnaker.clouddriver.model.EntityTags;
 import com.netflix.spinnaker.clouddriver.orchestration.AtomicOperation;
 import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider;
 import com.netflix.spinnaker.kork.core.RetrySupport;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UpsertEntityTagsAtomicOperation implements AtomicOperation<Void> {
+  private static final String BASE_PHASE = "ENTITY_TAGS";
+
   private final RetrySupport retrySupport;
   private final Front50Service front50Service;
   private final AccountCredentialsProvider accountCredentialsProvider;
@@ -51,6 +57,16 @@ public class UpsertEntityTagsAtomicOperation implements AtomicOperation<Void> {
     bulkDescription.isPartial = entityTagsDescription.isPartial;
     bulkDescription.entityTags = Collections.singletonList(entityTagsDescription);
 
+    getTask().updateStatus(
+      BASE_PHASE,
+      String.format(
+        "Updating entity tags for %s (isPartial: %s, tags: %s)",
+        entityTagsDescription.getId(),
+        entityTagsDescription.isPartial,
+        entityTagsDescription.getTags().stream().map(EntityTags.EntityTag::getName).collect(Collectors.joining(", "))
+      )
+    );
+
     new BulkUpsertEntityTagsAtomicOperation(
       retrySupport,
       front50Service,
@@ -62,4 +78,7 @@ public class UpsertEntityTagsAtomicOperation implements AtomicOperation<Void> {
     return null;
   }
 
+  private static Task getTask() {
+    return TaskRepository.threadLocalTask.get();
+  }
 }
