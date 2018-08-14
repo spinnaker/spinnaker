275a28bfc6640c9af03440dc10b69b13e0d5c0c3
Rob Fletcher <rfletcher@netflix.com>
2018-08-14 14:01:48 -0700

    Prove assets don't overwrite each other

--- keel-rule-engine/src/test/kotlin/com/netflix/spinnaker/keel/persistence/AssetRepositoryTests.kt
+++ keel-rule-engine/src/test/kotlin/com/netflix/spinnaker/keel/persistence/AssetRepositoryTests.kt
@@ -2,14 +2,17 @@ package com.netflix.spinnaker.keel.persistence
 
 import com.netflix.spinnaker.keel.model.Asset
 import com.netflix.spinnaker.keel.model.AssetId
+import com.nhaarman.mockito_kotlin.argumentCaptor
 import com.nhaarman.mockito_kotlin.check
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.reset
+import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.verifyZeroInteractions
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import strikt.api.expect
+import strikt.assertions.containsExactlyInAnyOrder
 import strikt.assertions.isEqualTo
 
 abstract class AssetRepositoryTests<T : AssetRepository> {
@@ -43,6 +46,34 @@ abstract class AssetRepositoryTests<T : AssetRepository> {
     subject.assets(callback)
 
     verify(callback).invoke(asset)
+  }
+
+  @Test
+  fun `assets with different ids do not overwrite each other`() {
+    val asset1 = Asset(
+      id = AssetId("SecurityGroup:ec2:test:us-west-2:fnord"),
+      apiVersion = "1.0",
+      kind = "ec2:SecurityGroup",
+      spec = ByteArray(0)
+    )
+    val asset2 = Asset(
+      id = AssetId("SecurityGroup:ec2:test:us-east-1:fnord"),
+      apiVersion = "1.0",
+      kind = "ec2:SecurityGroup",
+      spec = ByteArray(0)
+    )
+
+    subject.store(asset1)
+    subject.store(asset2)
+    subject.assets(callback)
+
+    argumentCaptor<Asset>().apply {
+      verify(callback, times(2)).invoke(capture())
+      expect(allValues) {
+        map { size }.isEqualTo(2)
+        containsExactlyInAnyOrder(asset1, asset2)
+      }
+    }
   }
 
   @Test
