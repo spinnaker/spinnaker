cc743a4eef08b71947df1757fd4f174fb20a7bc9
Scott Bloch-Wehba-Seaward <sbws@google.com>
2018-08-16 14:54:16 -0400

    fix(appengine): add server group button broken (#5627)

--- app/scripts/modules/appengine/src/serverGroup/configure/serverGroupCommandBuilder.service.ts
+++ app/scripts/modules/appengine/src/serverGroup/configure/serverGroupCommandBuilder.service.ts
@@ -141,20 +141,20 @@ export class AppengineServerGroupCommandBuilder {
   public buildNewServerGroupCommandForPipeline(
     _stage: IStage,
     pipeline: IPipeline,
-  ): {
+  ): IPromise<{
     backingData: {
       triggerOptions: Array<IAppengineGitTrigger | IAppengineJenkinsTrigger>;
       expectedArtifacts: IExpectedArtifact[];
     };
-  } {
+  }> {
     // We can't copy server group configuration for App Engine, and can't build the full command here because we don't have
     // access to the application.
-    return {
+    return this.$q.when({
       backingData: {
         triggerOptions: AppengineServerGroupCommandBuilder.getTriggerOptions(pipeline),
         expectedArtifacts: AppengineServerGroupCommandBuilder.getExpectedArtifacts(pipeline),
       },
-    };
+    });
   }
 
   public buildServerGroupCommandFromPipeline(
