efdd728e166ba07ed4ad5e5d8ae0d05b9910b893
Emily Burns <emburns@netflix.com>
2018-08-17 15:46:07 -0700

    refactor(trigger): pull link and or text from ITrigger (#5635)

--- app/scripts/modules/core/src/domain/IExecutionTrigger.ts
+++ app/scripts/modules/core/src/domain/IExecutionTrigger.ts
@@ -15,4 +15,6 @@ export interface IExecutionTrigger {
   dryRun?: boolean;
   artifacts?: IArtifact[];
   resolvedExpectedArtifacts?: IExpectedArtifact[];
+  link?: string;
+  linkText?: string;
 }
--- app/scripts/modules/core/src/pipeline/executionBuild/ExecutionBuildLink.tsx
+++ app/scripts/modules/core/src/pipeline/executionBuild/ExecutionBuildLink.tsx
@@ -40,18 +40,52 @@ export class ExecutionBuildLink extends React.Component<IExecutionBuildLinkProps
               {trigger.parentExecution.name}
             </a>
           )}
-        {this.props.execution.buildInfo &&
-          this.props.execution.buildInfo.number && (
-            <a
-              className="execution-build-number clickable"
-              onClick={this.handleBuildInfoClick}
-              href={this.props.execution.buildInfo.url}
-              target="_blank"
-            >
-              <span className="build-label">Build</span> #{this.props.execution.buildInfo.number}
-            </a>
-          )}
+        {this.getBuildLink()}
       </span>
     );
   }
+
+  private getBuildText(execution: IExecution) {
+    if (execution.trigger.linkText) {
+      return <span className="build-label">{execution.trigger.linkText}</span>;
+    } else {
+      return (
+        <>
+          <span className="build-label">Build</span> #{execution.buildInfo.number}
+        </>
+      );
+    }
+  }
+
+  private getBuildLink = () => {
+    const { execution } = this.props;
+
+    if (
+      !(execution.trigger.linkText && execution.trigger.link) &&
+      !(execution.buildInfo && execution.buildInfo.number)
+    ) {
+      return null;
+    }
+
+    if (execution.trigger.linkText && !(execution.trigger.link || execution.buildInfo.url)) {
+      // If supplying link text, must supply either link or url
+      return null;
+    }
+
+    if (execution.trigger.link && !(execution.trigger.linkText || execution.buildInfo.number)) {
+      // If supplying link, must supply either link text or build number
+      return null;
+    }
+
+    return (
+      <a
+        className="execution-build-number clickable"
+        onClick={this.handleBuildInfoClick}
+        href={execution.trigger.link ? execution.trigger.link : execution.buildInfo.url}
+        target="_blank"
+      >
+        {this.getBuildText(execution)}
+      </a>
+    );
+  };
 }
