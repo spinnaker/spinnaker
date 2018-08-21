d3a2739629feb50dcc8b1a0ede65f68a8991b8ab
Rob Fletcher <rfletcher@netflix.com>
2018-08-21 15:48:36 -0700

    More appropriate log levels

--- keel-rule-engine/src/main/kotlin/com/netflix/spinnaker/keel/processing/ValidateAssetTreeHandler.kt
+++ keel-rule-engine/src/main/kotlin/com/netflix/spinnaker/keel/processing/ValidateAssetTreeHandler.kt
@@ -23,6 +23,7 @@ class ValidateAssetTreeHandler(
   override fun handle(message: ValidateAssetTree) {
     validateSubTree(message.rootId).also { invalidAssetIds ->
       invalidAssetIds.forEach { id ->
+        log.debug("{} : Requesting convergence", id)
         queue.push(ConvergeAsset(id))
       }
     }
@@ -34,32 +35,32 @@ class ValidateAssetTreeHandler(
   private fun validateSubTree(id: AssetId): Set<AssetId> {
     val desired = repository.get(id)
     if (desired == null) {
-      log.error("{}: Not found", id)
+      log.error("{} : Not found", id)
       return emptySet()
     } else {
       val invalidAssetIds = mutableSetOf<AssetId>()
-      log.info("{}: Validating state", id)
+      log.debug("{} : Validating state", id)
       assetService
         .current(desired)
         .also { current ->
           when {
             current == null -> {
-              log.debug("{}: Does not exist", id)
+              log.info("{} : Does not exist", id)
               repository.updateState(id, Missing)
               invalidAssetIds.add(id)
             }
             desired.fingerprint == current.fingerprint -> {
-              log.debug("{}: Current state valid", id)
+              log.info("{} : Current state valid", id)
               repository.updateState(id, Ok)
             }
             else -> {
-              log.info("{}: Current state invalid", id)
+              log.info("{} : Current state invalid", id)
               repository.updateState(id, Diff)
               invalidAssetIds.add(id)
             }
           }
           repository.dependents(id).forEach { dependentId ->
-            log.debug("{}: Validating dependent asset {}", id, dependentId)
+            log.debug("{} : Validating dependent asset {}", id, dependentId)
             validateSubTree(dependentId).also { them ->
               invalidAssetIds.addAll(them)
             }
