f4a9bdebd51436d716667e44d2c6d1d0462232b1
Rob Fletcher <rfletcher@netflix.com>
2018-08-21 16:53:41 -0700

    Scheduler thingy to kick everything off

---
+++ keel-rule-engine/src/main/kotlin/com/netflix/spinnaker/keel/processing/ScheduledAssetChecker.kt
@@ -0,0 +1,22 @@
+package com.netflix.spinnaker.keel.processing
+
+import com.netflix.spinnaker.keel.persistence.AssetRepository
+import com.netflix.spinnaker.q.Queue
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class ScheduledAssetChecker(
+  private val repository: AssetRepository,
+  private val queue: Queue
+) {
+  private val log by lazy { LoggerFactory.getLogger(javaClass) }
+
+  @Scheduled(fixedDelayString = "\${check.cycle.frequency.ms:3600000}")
+  fun runCheckCycle() {
+    repository.rootAssets {
+      queue.push(ValidateAssetTree(it.id))
+    }
+  }
+}
---
+++ keel-rule-engine/src/test/kotlin/com/netflix/spinnaker/keel/processing/ScheduledAssetCheckerSpec.kt
@@ -0,0 +1,78 @@
+package com.netflix.spinnaker.keel.processing
+
+import com.netflix.spinnaker.keel.model.Asset
+import com.netflix.spinnaker.keel.model.AssetId
+import com.netflix.spinnaker.keel.persistence.InMemoryAssetRepository
+import com.netflix.spinnaker.q.Queue
+import com.nhaarman.mockito_kotlin.argThat
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.never
+import com.nhaarman.mockito_kotlin.verify
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import java.time.Clock
+
+internal object ScheduledAssetCheckerSpec : Spek({
+
+  val clock = Clock.systemDefaultZone()
+  val repository = InMemoryAssetRepository(clock)
+  val queue: Queue = mock()
+  val subject = ScheduledAssetChecker(repository, queue)
+
+  describe("checking assets") {
+    given("the repository contains some assets") {
+
+      val rootAsset1 = Asset(
+        id = AssetId("SecurityGroup:aws:prod:us-west-2:keel"),
+        kind = "SecurityGroup",
+        spec = randomBytes()
+      )
+      val rootAsset2 = Asset(
+        id = AssetId("SecurityGroup:aws:prod:us-east-1:keel"),
+        kind = "SecurityGroup",
+        spec = randomBytes()
+      )
+      val assets = listOf(rootAsset1, rootAsset2).flatMap {
+        listOf(
+          Asset(
+            id = AssetId(it.id.value.replace("SecurityGroup", "LoadBalancer")),
+            kind = "LoadBalancer",
+            dependsOn = setOf(it.id),
+            spec = randomBytes()
+          ),
+          Asset(
+            id = AssetId(it.id.value.replace("SecurityGroup", "Cluster")),
+            kind = "Cluster",
+            dependsOn = setOf(it.id),
+            spec = randomBytes()
+          )
+        )
+      }
+
+      beforeGroup {
+        (assets + rootAsset1 + rootAsset2).forEach(repository::store)
+      }
+
+      afterGroup { repository.dropAll() }
+
+      on("running the check cycle") {
+        subject.runCheckCycle()
+      }
+
+      it("requests validation for each root asset") {
+        verify(queue).push(ValidateAssetTree(rootAsset1.id))
+        verify(queue).push(ValidateAssetTree(rootAsset2.id))
+      }
+
+      it("does not request validation for any dependent assets") {
+        verify(queue, never()).push(argThat<ValidateAssetTree> {
+          rootId in assets.map(Asset::id)
+        })
+      }
+    }
+  }
+
+})
