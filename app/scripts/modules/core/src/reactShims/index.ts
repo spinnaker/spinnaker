9c9c38f44df28bae36a40e0a5f241fff70d2becb
Chris Thielen <christopherthielen@users.noreply.github.com>
2018-08-29 13:15:17 -1000

    refactor(core/reactShims): Remove spread-resolves-objects-to-props router shim now that `@uirouter/react` does this natively (#5685)

--- app/scripts/modules/core/src/reactShims/index.ts
+++ app/scripts/modules/core/src/reactShims/index.ts
@@ -3,5 +3,4 @@ export * from './modal.injector';
 export * from './ngReact';
 export * from './react.injector';
 export * from './react.module';
-export * from './react.uirouter';
 export * from './state.events';
--- app/scripts/modules/core/src/reactShims/react.module.ts
+++ app/scripts/modules/core/src/reactShims/react.module.ts
@@ -1,13 +1,13 @@
 import { module } from 'angular';
 import 'ngimport';
 import { STATE_EVENTS } from './state.events';
-import { REACT_UIROUTER } from './react.uirouter';
 import { ReactInjector } from './react.injector';
 import { ModalInjector } from './modal.injector';
 import { NgReact } from './ngReact';
+import './react.uirouter.css';
 
 export const REACT_MODULE = 'spinnaker.core.react';
-module(REACT_MODULE, ['bcherny/ngimport', STATE_EVENTS, REACT_UIROUTER]).run(function($injector: any) {
+module(REACT_MODULE, ['bcherny/ngimport', 'ui.router', STATE_EVENTS]).run(function($injector: any) {
   // Make angular services importable and Convert angular components to react
   ReactInjector.initialize($injector);
   ModalInjector.initialize($injector);
