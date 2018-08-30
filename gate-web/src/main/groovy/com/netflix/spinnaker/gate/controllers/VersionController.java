b029096b4873ac538d48a3c72f9d98910f8ee61d
Jacob Kiefer <jacobkiefer@google.com>
2018-08-30 14:07:49 -0400

    feat(version): Expose Gate's version via an endpoint. (#590)

---
+++ gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/VersionController.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.gate.controllers;
+
+import io.swagger.annotations.ApiOperation;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/version")
+public class VersionController {
+
+  @ApiOperation(value = "Fetch Gate's current version", response = String.class)
+  @RequestMapping(method = RequestMethod.GET)
+  String getVersion() {
+    return Optional.ofNullable(VersionController.class
+      .getPackage()
+      .getImplementationVersion())
+      .orElse("Unknown");
+  }
+}
