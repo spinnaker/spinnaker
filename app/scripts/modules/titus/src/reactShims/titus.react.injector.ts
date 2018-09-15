0631440b6161cf05f194aa50a9843d001bd16b1e
Justin Reynolds <justinreynolds@gmail.com>
2018-09-14 22:41:29 -0700

    refactor(titus): Expose server group command services in react injector

--- app/scripts/modules/titus/src/reactShims/titus.react.injector.ts
+++ app/scripts/modules/titus/src/reactShims/titus.react.injector.ts
@@ -5,6 +5,8 @@ import { ReactInject } from '@spinnaker/core';
 // prettier-ignore
 export class TitusReactInject extends ReactInject {
   public get titusServerGroupTransformer() { return this.$injector.get('titusServerGroupTransformer') as any; }
+  public get titusServerGroupCommandBuilder() { return this.$injector.get('titusServerGroupCommandBuilder') as any; }
+  public get titusServerGroupConfigurationService() { return this.$injector.get('titusServerGroupConfigurationService') as any; }
 
   public initialize($injector: IInjectorService) {
     this.$injector = $injector;
