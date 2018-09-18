a2ab817973adb45acf67a62f2d481300e5a6b017
Matt Duftler <duftler@google.com>
2018-09-18 13:00:48 -0400

    feat(stackdriver): Add new metric config fields.

--- src/kayenta/metricStore/stackdriver/domain/IStackdriverCanaryMetricSetQueryConfig.ts
+++ src/kayenta/metricStore/stackdriver/domain/IStackdriverCanaryMetricSetQueryConfig.ts
@@ -2,5 +2,8 @@ import { ICanaryMetricSetQueryConfig } from 'kayenta/domain';
 
 export interface IStackdriverCanaryMetricSetQueryConfig extends ICanaryMetricSetQueryConfig {
   metricType: string;
+  resourceType: string;
+  crossSeriesReducer: string;
+  perSeriesAligner: string;
   groupByFields: string[];
 }
