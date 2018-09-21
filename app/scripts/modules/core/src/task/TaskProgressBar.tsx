e8081abde4e7ce09259e51321920626acc124cc6
Alan Quach <integsrtite@gmail.com>
2018-09-20 21:46:41 -0700

    refactor(core/task): Migrated task progress bar to react (#5753)

---
+++ app/scripts/modules/core/src/task/TaskProgressBar.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import * as classNames from 'classnames';
+
+import { ITask } from 'core/domain';
+import { robotToHuman } from 'core/presentation/robotToHumanFilter/robotToHuman.filter';
+
+export interface ITaskProgressBarProps {
+  task: ITask;
+}
+
+export function TaskProgressBar(props: ITaskProgressBarProps) {
+  const { task } = props;
+  const { steps, id } = task;
+  let tooltip;
+
+  if (task.isRunning) {
+    const currentStep = steps.find(step => step.hasNotStarted || step.isRunning);
+    if (currentStep) {
+      const currentStepIndex = steps.indexOf(currentStep) + 1;
+      tooltip = (
+        <Tooltip id={id}>{`Step ${currentStepIndex} of ${steps.length}: ${robotToHuman(currentStep.name)}`}</Tooltip>
+      );
+    }
+  }
+
+  if (task.isFailed) {
+    const failedStep = steps.find(step => step.isFailed || step.isSuspended);
+    if (failedStep && task.failureMessage) {
+      const failedStepIndex = steps.indexOf(failedStep) + 1;
+      const ellipses = String.fromCharCode(8230);
+      const clipped =
+        task.failureMessage.length > 400 ? task.failureMessage.substring(0, 400) + ellipses : task.failureMessage;
+      tooltip = (
+        <Tooltip id={id}>
+          {`Failed on Step ${failedStepIndex} of ${steps.length}:`}
+          <br />
+          {robotToHuman(failedStep.name)}
+          <br />
+          <br />
+          <strong>Exception:</strong>
+          <p>{clipped}</p>
+        </Tooltip>
+      );
+    } else {
+      tooltip = <Tooltip id={id}>Task failed; sorry, no reason provided</Tooltip>;
+    }
+  }
+
+  const stepsComplete = steps.filter(step => step.isCompleted);
+
+  const progressBarClassName = classNames({
+    'progress-bar': true,
+    'progress-bar-info': task.isRunning || task.hasNotStarted,
+    'progress-bar-success': task.isCompleted,
+    'progress-bar-disabled': task.isCanceled,
+    'progress-bar-danger': task.isFailed,
+  });
+  const progressBar = (
+    <div className="progress">
+      <div className={progressBarClassName} style={{ width: `${stepsComplete.length / steps.length * 100}%` }} />
+    </div>
+  );
+
+  if (tooltip) {
+    return (
+      <OverlayTrigger placement="top" overlay={tooltip}>
+        {progressBar}
+      </OverlayTrigger>
+    );
+  }
+  return progressBar;
+}
--- app/scripts/modules/core/src/task/taskProgressBar.directive.js
+++ app/scripts/modules/core/src/task/taskProgressBar.directive.js
@@ -1,64 +1,9 @@
 'use strict';
 
 const angular = require('angular');
+import { react2angular } from 'react2angular';
+import { TaskProgressBar } from './TaskProgressBar';
 
 module.exports = angular
   .module('spinnaker.core.task.progressBar.directive', [])
-  .directive('taskProgressBar', function($filter, $sce) {
-    return {
-      restrict: 'E',
-      scope: {
-        task: '=',
-      },
-      templateUrl: require('./taskProgressBar.directive.html'),
-      link: function(scope) {
-        var task = scope.task;
-
-        var stepsComplete = task.steps.filter(function(step) {
-          return step.isCompleted;
-        });
-
-        scope.progressStyle = { width: (stepsComplete.length / task.steps.length) * 100 + '%' };
-
-        if (task.isRunning) {
-          let currentStep = task.steps.find(step => step.hasNotStarted || step.isRunning);
-          if (currentStep) {
-            var currentStepIndex = task.steps.indexOf(currentStep) + 1;
-            scope.tooltip = $sce.trustAsHtml(
-              'Step ' +
-                currentStepIndex +
-                ' of ' +
-                task.steps.length +
-                ': ' +
-                $filter('robotToHuman')(currentStep.name),
-            );
-          }
-        }
-
-        if (task.isFailed) {
-          let failedStep = task.steps.find(step => step.isFailed || step.isSuspended);
-
-          if (failedStep && task.failureMessage) {
-            var failedStepIndex = task.steps.indexOf(failedStep) + 1;
-            var clipped =
-              task.failureMessage.length > 400
-                ? task.failureMessage.substring(0, 400) + '&hellip;'
-                : task.failureMessage;
-            scope.tooltip = $sce.trustAsHtml(
-              'Failed on Step ' +
-                failedStepIndex +
-                ' of ' +
-                task.steps.length +
-                ':<br>' +
-                $filter('robotToHuman')(failedStep.name) +
-                '<br><br><strong>Exception:</strong><p>' +
-                clipped +
-                '</p>',
-            );
-          } else {
-            scope.tooltip = $sce.trustAsHtml('Task failed; sorry, no reason provided');
-          }
-        }
-      },
-    };
-  });
+  .component('taskProgressBar', react2angular(TaskProgressBar, ['task']));
