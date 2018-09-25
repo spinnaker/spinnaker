c1f90e4293e8e7bf754c574ac0af63ce13a6bd1a
Rob Zienert <rob@robzienert.com>
2018-09-25 11:08:48 -0700

    fix(metrics): Correctly handle potentially empty account tag (#2426)

--- orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/metrics/MetricsTagHelper.kt
+++ orca-queue/src/main/kotlin/com/netflix/spinnaker/orca/q/metrics/MetricsTagHelper.kt
@@ -2,8 +2,8 @@ package com.netflix.spinnaker.orca.q.metrics
 
 import com.netflix.spectator.api.BasicTag
 import com.netflix.spinnaker.orca.ExecutionStatus
-import com.netflix.spinnaker.orca.pipeline.model.Stage
 import com.netflix.spinnaker.orca.clouddriver.utils.CloudProviderAware
+import com.netflix.spinnaker.orca.pipeline.model.Stage
 
 class MetricsTagHelper : CloudProviderAware {
   companion object {
@@ -14,17 +14,26 @@ class MetricsTagHelper : CloudProviderAware {
         BasicTag("status", status.toString()),
         BasicTag("executionType", stage.execution.type.name.capitalize()),
         BasicTag("isComplete", status.isComplete.toString()),
-        BasicTag("cloudProvider", helper.getCloudProvider(stage) ?: "n_a"))
+        BasicTag("cloudProvider", helper.getCloudProvider(stage).valueOrNa())
+      )
 
     fun detailedTaskTags(stage: Stage, taskModel: com.netflix.spinnaker.orca.pipeline.model.Task, status: ExecutionStatus): Iterable<BasicTag> =
       arrayListOf(
         BasicTag("taskType", taskModel.implementingClass),
-        BasicTag("account", helper.getCredentials(stage) ?: "n_a"),
+        BasicTag("account", helper.getCredentials(stage).valueOrNa()),
 
         // sorting regions to reduce the metrics cardinality
         BasicTag("region", helper.getRegions(stage).let {
           if (it.isEmpty()) { "n_a" }
           else { it.sorted().joinToString(",") }
         }))
+
+    private fun String?.valueOrNa(): String {
+      return if (this == null || isBlank()) {
+        "n_a"
+      } else {
+        this
+      }
+    }
   }
 }
