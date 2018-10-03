04f72e5afc75b201fa79fc17d145bb5c70eae24c
Daniel Peach <dpeach@google.com>
2018-10-03 13:42:37 -0400

    fix(build): use travis's own nvm version (#356)

--- travis/installViaTravis.sh
+++ travis/installViaTravis.sh
@@ -4,14 +4,5 @@
 NODE_JS_VERSION=`node -e 'console.log(require("./package.json").engines.node.replace(/[^\d\.]/g, ""))'`;
 echo -e "Installing/activating node v$NODE_JS_VERSION"
 
-# http://austinpray.com/ops/2015/09/20/change-travis-node-version.html
-# Clear out whatever version of NVM Travis has. Their version of NVM is probably old.
-rm -rf ~/.nvm
-git clone https://github.com/creationix/nvm.git ~/.nvm
-
-# Checkout the latest stable nvm tag.
-(cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`)
-
-source ~/.nvm/nvm.sh
 nvm install $NODE_JS_VERSION
 yarn
