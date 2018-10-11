4f48a3edf2803785ac7122f20ad72dd374e06212
Martin <github@grusy.de>
2018-10-11 15:42:33 +1100

    fix(deck): Change Build Stage description (#5839)
    Change Build Stage description to be more generic

--- app/scripts/modules/google/src/pipeline/stages/bake/gceBakeStage.js
+++ app/scripts/modules/google/src/pipeline/stages/bake/gceBakeStage.js
@@ -21,7 +21,7 @@ module.exports = angular
       provides: 'bake',
       cloudProvider: 'gce',
       label: 'Bake',
-      description: 'Bakes an image in the specified region',
+      description: 'Bakes an image',
       templateUrl: require('./bakeStage.html'),
       executionDetailsUrl: require('./bakeExecutionDetails.html'),
       executionLabelComponent: BakeExecutionLabel,
