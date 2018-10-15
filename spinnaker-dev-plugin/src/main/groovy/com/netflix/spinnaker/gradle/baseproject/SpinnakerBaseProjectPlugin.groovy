58a85a26cc7d63a0fa92b6789773ea9dd6695fdd
Travis Tomsu <ttomsu@google.com>
2018-10-15 14:18:15 -0400

    fix(builds): Reorder plugins to ensure Java 1.8. Adds License report plugin back. (#51)

--- spinnaker-dev-plugin/src/main/groovy/com/netflix/spinnaker/gradle/baseproject/SpinnakerBaseProjectPlugin.groovy
+++ spinnaker-dev-plugin/src/main/groovy/com/netflix/spinnaker/gradle/baseproject/SpinnakerBaseProjectPlugin.groovy
@@ -10,9 +10,13 @@ import org.gradle.api.Project
 class SpinnakerBaseProjectPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.plugins.apply(SpinnakerBaseProjectConventionsPlugin)
         project.plugins.apply(SpinnakerIdeaConfigPlugin)
         project.plugins.apply(SpinnakerDependencyPlugin)
+        project.plugins.apply(SpinnakerLicenseReportPlugin)
         project.plugins.apply(DependencyCheckPlugin)
+
+        // This should come last to ensure the bump to Java 1.8 is applied
+        // (some Netflix OSS plugins hardcode/revert it to 1.7)
+        project.plugins.apply(SpinnakerBaseProjectConventionsPlugin)
     }
 }
