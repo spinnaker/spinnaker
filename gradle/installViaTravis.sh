e53f2995556b481d178a4158a83e07b313586d7e
Travis Tomsu <ttomsu@google.com>
2018-10-25 13:46:59 -0400

    chore(builds): Introduces composite builds via gradle plugin (#407)

--- gradle/installViaTravis.sh
+++ gradle/installViaTravis.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # This script will build the project.
 
+GRADLE="./gradlew -I gradle/init-publish.gradle"
+
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Assemble Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew -Pskip.loadtest=true assemble
+  $GRADLE -Pskip.loadtest=true assemble
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Assemble Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -Prelease.travisci=true -Pskip.loadtest=true -x test assemble
+  $GRADLE -Prelease.travisci=true -Pskip.loadtest=true -x test assemble
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Assemble Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -Pskip.loadtest=true -x test assemble
+  $GRADLE -Prelease.travisci=true -Prelease.useLastTag=true -Pskip.loadtest=true -x test assemble
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew -Pskip.loadtest=true assemble
+  $GRADLE -Pskip.loadtest=true assemble
 fi
 
