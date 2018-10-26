3960ad132f376b9b615c4cd4c1cf7a64f221e0e9
Justin Reynolds <justinreynolds@gmail.com>
2018-10-26 11:01:15 -0700

    refactor(core/deploy): Convert custom strategy config to react

--- app/scripts/modules/core/src/deploymentStrategy/strategies/custom/custom.strategy.ts
+++ app/scripts/modules/core/src/deploymentStrategy/strategies/custom/custom.strategy.ts
@@ -1,9 +1,10 @@
 import { DeploymentStrategyRegistry } from 'core/deploymentStrategy/deploymentStrategy.registry';
+import { AdditionalFields } from './AdditionalFields';
 
 DeploymentStrategyRegistry.registerStrategy({
   label: 'Custom',
   description: 'Runs a custom deployment strategy',
   key: 'custom',
   additionalFields: ['pipelineParameters', 'strategyApplication', 'strategyPipeline'],
-  additionalFieldsTemplateUrl: require('./additionalFields.html'),
+  AdditionalFieldsComponent: AdditionalFields,
 });
