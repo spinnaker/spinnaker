6ae6c516ac18da5083ccd3838af2b2ad27c40c82
Justin Reynolds <justinreynolds@gmail.com>
2018-10-26 10:57:04 -0700

    refactor(core/deploy): Convert rolling red back strategy config to react

--- app/scripts/modules/core/src/deploymentStrategy/deploymentStrategy.module.ts
+++ app/scripts/modules/core/src/deploymentStrategy/deploymentStrategy.module.ts
@@ -6,15 +6,8 @@ import './strategies/none/none.strategy';
 import './strategies/redblack/redblack.strategy';
 import './strategies/rollingredblack/rollingredblack.strategy';
 
-import { CUSTOM_STRATEGY_SELECTOR_COMPONENT } from './strategies/custom/customStrategySelector.component';
-import { PIPELINE_SELECTOR_COMPONENT } from './strategies/rollingredblack/pipelineSelector.component';
 import { DEPLOYMENT_STRATEGY_SELECTOR_COMPONENT } from './deploymentStrategySelector.component';
 import { DEPLOYMENT_STRATEGY_FIELDS_CONTROLLER } from './deploymentStrategyFieldsController';
 
 export const DEPLOYMENT_STRATEGY_MODULE = 'spinnaker.core.deploymentStrategy';
-module(DEPLOYMENT_STRATEGY_MODULE, [
-  CUSTOM_STRATEGY_SELECTOR_COMPONENT,
-  PIPELINE_SELECTOR_COMPONENT,
-  DEPLOYMENT_STRATEGY_FIELDS_CONTROLLER,
-  DEPLOYMENT_STRATEGY_SELECTOR_COMPONENT,
-]);
+module(DEPLOYMENT_STRATEGY_MODULE, [DEPLOYMENT_STRATEGY_FIELDS_CONTROLLER, DEPLOYMENT_STRATEGY_SELECTOR_COMPONENT]);
