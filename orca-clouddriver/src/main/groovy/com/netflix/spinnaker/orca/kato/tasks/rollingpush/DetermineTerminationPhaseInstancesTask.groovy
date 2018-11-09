97b54cc559208e0e448d84dc68eeb3b87328eab7
Cameron Fieber <cameron@fieber.ca>
2018-11-09 14:47:38 -0800

    fix(rollingPush): ensure context resets on task loop (#2506)

--- orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/kato/tasks/rollingpush/DetermineTerminationPhaseInstancesTask.groovy
+++ orca-clouddriver/src/main/groovy/com/netflix/spinnaker/orca/kato/tasks/rollingpush/DetermineTerminationPhaseInstancesTask.groovy
@@ -19,6 +19,7 @@ package com.netflix.spinnaker.orca.kato.tasks.rollingpush
 import com.netflix.spinnaker.orca.ExecutionStatus
 import com.netflix.spinnaker.orca.Task
 import com.netflix.spinnaker.orca.TaskResult
+import com.netflix.spinnaker.orca.clouddriver.pipeline.instance.TerminatingInstanceSupport
 import com.netflix.spinnaker.orca.pipeline.model.Stage
 import org.springframework.stereotype.Component
 
@@ -32,6 +33,15 @@ class DetermineTerminationPhaseInstancesTask implements Task {
 
     List<String> instanceIds = terms.take(concurrentRelaunches)
     List<String> terminationInstanceIds = terms.drop(concurrentRelaunches)
-    return new TaskResult(ExecutionStatus.SUCCEEDED, [instanceIds: instanceIds, terminationInstanceIds: terminationInstanceIds])
+    def outputs = [
+            instanceIds                                               : instanceIds,
+            terminationInstanceIds                                    : terminationInstanceIds,
+            //these get populated during terminate processing, ensure we are on a fresh-ish context
+            // since we've been looopin'
+            'terminate.instance.ids'                                  : [],
+            (TerminatingInstanceSupport.TERMINATE_REMAINING_INSTANCES): []
+
+    ]
+    return new TaskResult(ExecutionStatus.SUCCEEDED, outputs)
   }
 }
