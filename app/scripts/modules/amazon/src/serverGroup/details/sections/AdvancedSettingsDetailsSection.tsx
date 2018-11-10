4c80cb867188795a5ce66029e84def46dbcb0380
Kevin Woo <kevinawoo@gmail.com>
2018-11-09 16:29:05 -0800

    fix(aws/serverGroups): always show AWS sever group settings (#6003)

--- app/scripts/modules/amazon/src/serverGroup/details/sections/AdvancedSettingsDetailsSection.tsx
+++ app/scripts/modules/amazon/src/serverGroup/details/sections/AdvancedSettingsDetailsSection.tsx
@@ -21,30 +21,26 @@ export class AdvancedSettingsDetailsSection extends React.Component<IAmazonServe
 
     const asg = serverGroup.asg;
 
-    if (serverGroup.buildInfo && serverGroup.buildInfo.jenkins) {
-      return (
-        <CollapsibleSection heading="Advanced Settings">
-          <dl className="horizontal-when-filters-collapsed">
-            <dt>Cooldown</dt>
-            <dd>{asg.defaultCooldown} seconds</dd>
-            {asg.enabledMetrics.length > 0 && [
-              <dt key={'t-metrics'}>Enabled Metrics</dt>,
-              <dd key={'d-metrics'}>{asg.enabledMetrics.map(m => m.metric).join(', ')}</dd>,
-            ]}
-            <dt>Health Check Type</dt>
-            <dd>{asg.healthCheckType}</dd>
-            <dt>Grace Period</dt>
-            <dd>{asg.healthCheckGracePeriod} seconds</dd>
-            <dt>Termination Policies</dt>
-            <dd>{asg.terminationPolicies.join(', ')}</dd>
-          </dl>
-          <a className="clickable" onClick={this.editAdvancedSettings}>
-            Edit Advanced Settings
-          </a>
-        </CollapsibleSection>
-      );
-    }
-
-    return null;
+    return (
+      <CollapsibleSection heading="Advanced Settings">
+        <dl className="horizontal-when-filters-collapsed">
+          <dt>Cooldown</dt>
+          <dd>{asg.defaultCooldown} seconds</dd>
+          {asg.enabledMetrics.length > 0 && [
+            <dt key={'t-metrics'}>Enabled Metrics</dt>,
+            <dd key={'d-metrics'}>{asg.enabledMetrics.map(m => m.metric).join(', ')}</dd>,
+          ]}
+          <dt>Health Check Type</dt>
+          <dd>{asg.healthCheckType}</dd>
+          <dt>Grace Period</dt>
+          <dd>{asg.healthCheckGracePeriod} seconds</dd>
+          <dt>Termination Policies</dt>
+          <dd>{asg.terminationPolicies.join(', ')}</dd>
+        </dl>
+        <a className="clickable" onClick={this.editAdvancedSettings}>
+          Edit Advanced Settings
+        </a>
+      </CollapsibleSection>
+    );
   }
 }
