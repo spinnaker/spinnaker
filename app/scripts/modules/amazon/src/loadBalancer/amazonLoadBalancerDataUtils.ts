64fdb9027c85e4a111eeada283b66ceace5d6329
Justin Reynolds <justinreynolds@gmail.com>
2018-11-13 13:41:36 -0800

    fix(titus): Fix target group tag in clusters view (#6032)

--- app/scripts/modules/amazon/src/loadBalancer/amazonLoadBalancerDataUtils.ts
+++ app/scripts/modules/amazon/src/loadBalancer/amazonLoadBalancerDataUtils.ts
@@ -1,7 +1,8 @@
 import { IPromise } from 'angular';
+import { $q } from 'ngimport';
 import { flatten } from 'lodash';
 
-import { Application, ILoadBalancer } from '@spinnaker/core';
+import { AccountService, Application, ILoadBalancer } from '@spinnaker/core';
 
 import { IAmazonApplicationLoadBalancer, IAmazonHealth, IAmazonServerGroup, ITargetGroup } from 'amazon/domain';
 
@@ -43,10 +44,10 @@ export class AmazonLoadBalancerDataUtils {
     application: Application,
     serverGroup: IAmazonServerGroup,
   ): IPromise<ITargetGroup[]> {
-    return application
-      .getDataSource('loadBalancers')
-      .ready()
-      .then(() => {
+    return $q
+      .all([AccountService.getAccountDetails(serverGroup.account), application.getDataSource('loadBalancers').ready()])
+      .then(data => {
+        const awsAccount = (data[0] && data[0].awsAccount) || serverGroup.account;
         const loadBalancers: IAmazonApplicationLoadBalancer[] = application
           .getDataSource('loadBalancers')
           .data.filter(
@@ -56,8 +57,7 @@ export class AmazonLoadBalancerDataUtils {
           .map((targetGroupName: string) => {
             const allTargetGroups = flatten(loadBalancers.map(lb => lb.targetGroups || []));
             const targetGroup = allTargetGroups.find(
-              tg =>
-                tg.name === targetGroupName && tg.region === serverGroup.region && tg.account === serverGroup.account,
+              tg => tg.name === targetGroupName && tg.region === serverGroup.region && tg.account === awsAccount,
             );
             return this.buildTargetGroup(targetGroup, serverGroup);
           })
