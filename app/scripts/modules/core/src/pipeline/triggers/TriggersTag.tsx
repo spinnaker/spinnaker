c8cebde7c3612db006d5d95654b4f080dbb12e8f
Justin Reynolds <justinreynolds@gmail.com>
2018-11-14 10:18:00 -0800

    fix(core/pipeline): Only show quiet period tag if respect flag is true (#6038)

--- app/scripts/modules/core/src/pipeline/triggers/TriggersTag.tsx
+++ app/scripts/modules/core/src/pipeline/triggers/TriggersTag.tsx
@@ -21,18 +21,20 @@ export class TriggersTag extends React.Component<ITriggersTagProps, ITriggersTag
   constructor(props: ITriggersTagProps) {
     super(props);
 
-    let triggerCount = 0,
-      activeTriggerCount = 0;
+    let triggerCount = 0;
+    let activeTriggerCount = 0;
+    let quietPeriodEnabled = false;
 
     const pipeline = this.props.pipeline;
     if (pipeline && pipeline.triggers && pipeline.triggers.length) {
       triggerCount = pipeline.triggers.length;
       activeTriggerCount = filter(pipeline.triggers, { enabled: true }).length;
+      quietPeriodEnabled = Boolean(pipeline.respectQuietPeriod);
     }
 
     const hasQuietPeriod = SETTINGS.feature.quietPeriod && SETTINGS.quietPeriod && SETTINGS.quietPeriod.length === 2;
 
-    if (hasQuietPeriod) {
+    if (hasQuietPeriod && quietPeriodEnabled) {
       this.quietPeriodStart = new Date(SETTINGS.quietPeriod[0]);
       this.quietPeriodEnd = new Date(SETTINGS.quietPeriod[1]);
     }
