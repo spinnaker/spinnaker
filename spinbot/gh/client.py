63b6d82067c7031a92bbd9794fccfca9cb490d03
Lars Wander <lwander@users.noreply.github.com>
2018-11-16 14:08:19 -0500

    feat(spinbot): automated cherrypicks (#3614)

--- spinbot/gh/client.py
+++ spinbot/gh/client.py
@@ -6,13 +6,20 @@ import monitoring
 import heapq
 import os
 
+from .repo import CherryPick
+from .util import ReleaseBranch
+
 class Client(object):
     def __init__(self, config):
-        token = self.get_token(config)
-        self.g = github.Github(token)
+        self.token = self.get_token(config)
+        self.username = self.get_username(config)
+        self.g = github.Github(self.token)
         self._repos = config['repos']
         self.monitoring_db = monitoring.GetDatabase('spinbot')
         self.logging = logging.getLogger('github_client_wrapper')
+
+    def get_username(self, config):
+        return config.get('username', 'spinnakerbot')
 
     def get_token(self, config):
         token_path = config.get('token_path')
@@ -21,6 +28,27 @@ class Client(object):
 
         with open(os.path.expanduser(token_path), 'r') as f:
             return f.read().strip()
+
+    def cherry_pick(self, repo, release, commit):
+        branch = CherryPick(repo=repo, release=release, commit=commit, username=self.username, password=self.token)
+        r = self.g.get_repo(repo)
+        c = r.get_commit(commit)
+        og_message = c.commit.message
+        title = og_message.split('\n')[0]
+        body = '\n'.join(og_message.split('\n')[1:])
+        message = '{body}\n\n> Automated cherry pick of {commit} into {release}'.format(
+                body=body,
+                commit=commit, 
+                release=release, 
+        )
+
+        return r.create_pull(
+                title=title, 
+                base=ReleaseBranch(release), 
+                head=branch,
+                body=message,
+                maintainer_can_modify=True
+        )
 
     def rate_limit(self):
         ret = self.g.get_rate_limit()
