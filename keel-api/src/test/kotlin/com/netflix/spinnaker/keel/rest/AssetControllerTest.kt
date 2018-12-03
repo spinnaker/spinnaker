74b9c628983bcbc52d32564c13f10f2d4de26ef3
Rob Fletcher <rfletcher@netflix.com>
2018-12-03 14:36:29 -0800

    fix(travis): revert inappropriate file change

--- keel-api/src/test/kotlin/com/netflix/spinnaker/keel/rest/AssetControllerTest.kt
+++ keel-api/src/test/kotlin/com/netflix/spinnaker/keel/rest/AssetControllerTest.kt
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
@@ -22,7 +23,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
     "clouddriver.baseUrl=https://localhost:8081",
     "orca.baseUrl=https://localhost:8082"
   ],
-  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+  webEnvironment = RANDOM_PORT
 )
 @AutoConfigureMockMvc
 internal class AssetControllerTest {
