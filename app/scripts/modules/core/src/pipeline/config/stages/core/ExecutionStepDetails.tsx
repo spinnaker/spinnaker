e73b3d646a7a8b143b3d5ba1b76a2c2850630638
Chris Berry <chrisb@netflix.com>
2018-12-03 16:03:45 -0800

    fix(core): increment running task time in step details (#6132)

--- app/scripts/modules/core/src/pipeline/config/stages/core/ExecutionStepDetails.tsx
+++ app/scripts/modules/core/src/pipeline/config/stages/core/ExecutionStepDetails.tsx
@@ -5,35 +5,59 @@ import { StatusGlyph } from 'core/task/StatusGlyph';
 import { displayableTasks } from 'core/task/displayableTasks.filter';
 import { duration } from 'core/utils/timeFormatters';
 import { robotToHuman } from 'core/presentation/robotToHumanFilter/robotToHuman.filter';
+import { OrchestratedItemRunningTime } from 'core/pipeline/executions/execution/OrchestratedItemRunningTime';
 
 export interface IExecutionStepDetailsProps {
   item: IExecutionStage;
 }
 
-export function ExecutionStepDetails(props: IExecutionStepDetailsProps) {
-  return (
-    <div className="row">
-      <div className="col-md-9">
-        <div className="row">
-          <div className="col-md-9">
-            <strong>Task</strong>
-          </div>
-          <div className="col-md-3 text-right">
-            <strong>Duration</strong>
-          </div>
-        </div>
-        {displayableTasks(props.item.tasks || []).map((task, index) => (
-          <div key={index} className="row">
+export interface IExecutionStepDetailsState {
+  runningTimeInMs: number;
+}
+
+export class ExecutionStepDetails extends React.Component<IExecutionStepDetailsProps, IExecutionStepDetailsState> {
+  private runningTime: OrchestratedItemRunningTime;
+
+  public componentWillUnmount(): void {
+    this.runningTime && this.runningTime.reset();
+  }
+
+  public componentDidMount(): void {
+    const { item } = this.props;
+    const runningTask = (item.tasks || []).find(t => t.status === 'RUNNING');
+    if (runningTask) {
+      this.runningTime = new OrchestratedItemRunningTime(runningTask, (time: number) =>
+        this.setState({ runningTimeInMs: time }),
+      );
+    }
+  }
+
+  public render() {
+    const { item } = this.props;
+    return (
+      <div className="row">
+        <div className="col-md-9">
+          <div className="row">
             <div className="col-md-9">
-              <span className="small">
-                <StatusGlyph item={task} />
-              </span>{' '}
-              {robotToHuman(task.name)}
+              <strong>Task</strong>
             </div>
-            <div className="col-md-3 text-right">{duration(task.runningTimeInMs)}</div>
+            <div className="col-md-3 text-right">
+              <strong>Duration</strong>
+            </div>
           </div>
-        ))}
+          {displayableTasks(item.tasks || []).map((task, index) => (
+            <div key={index} className="row">
+              <div className="col-md-9">
+                <span className="small">
+                  <StatusGlyph item={task} />
+                </span>{' '}
+                {robotToHuman(task.name)}
+              </div>
+              <div className="col-md-3 text-right">{duration(task.runningTimeInMs)}</div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
