feaa5791d2065ff6f6bfeaf02e509b9f5869aa6d
Chris Berry <chrisb@netflix.com>
2018-12-12 11:08:00 -0800

    fix(core): do not load GA script if not configured (#6177)

--- app/scripts/modules/core/src/analytics/analytics.service.js
+++ app/scripts/modules/core/src/analytics/analytics.service.js
@@ -7,12 +7,13 @@ const angular = require('angular');
 module.exports = angular
   .module('spinnaker.core.analytics.service', [require('angulartics'), require('angulartics-google-analytics')])
   .run(function($window) {
-    if (SETTINGS.analytics.ga) {
+    if (SETTINGS.analytics.ga || SETTINGS.analytics.customConfig) {
+      $window.addEventListener('load', () => {
         if (SETTINGS.analytics.customConfig) {
-            $window.ga('create', SETTINGS.analytics.ga, SETTINGS.analytics.customConfig);
+          $window.ga('create', SETTINGS.analytics.ga, SETTINGS.analytics.customConfig);
+        } else {
+          $window.ga('create', SETTINGS.analytics.ga, 'auto');
         }
-        else {
-            $window.ga('create', SETTINGS.analytics.ga, 'auto');
-        }
+      });
     }
   });
