7d0046480ea16919095ab5cc304dd1c589aa67d9
Matt Duftler <duftler@google.com>
2018-12-17 08:56:50 -0500

    fix(localdeb/redis): Also install specific version of redis-tools. (#1134)

--- halyard-deploy/src/main/resources/services/redis/install.sh
+++ halyard-deploy/src/main/resources/services/redis/install.sh
@@ -1,2 +1,2 @@
 add-apt-repository -y ppa:chris-lea/redis-server
-apt-get -q -y --force-yes install redis-server={%version%}
+apt-get -q -y --force-yes install redis-server={%version%} redis-tools={%version%}
