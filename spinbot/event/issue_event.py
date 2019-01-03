6b821e65d66201f5113d16a06e2064ab66f70c19
Lars Wander <lwander@users.noreply.github.com>
2019-01-03 13:10:52 -0500

    feat(spinbot): assign issue functionality (#3805)

--- spinbot/event/issue_event.py
+++ spinbot/event/issue_event.py
@@ -10,3 +10,12 @@ def GetIssue(g, event):
         return None
 
     return g.get_issue(repo, number)
+
+def GetRepo(g, event):
+    url = event.payload.get('issue', {}).get('url')
+    if not url:
+        return None
+
+    repo = '/'.join(url.split('/')[-4:-2])
+    return g.get_repo(repo)
+
