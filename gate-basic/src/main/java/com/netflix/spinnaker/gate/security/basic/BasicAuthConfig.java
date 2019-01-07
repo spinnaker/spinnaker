9989cb6d05b27d995ca3bcdb9f44f366dc3c4427
German Muzquiz <35276119+german-muzquiz@users.noreply.github.com>
2019-01-07 13:28:13 -0600

    fix(authn/basic): Enable basic auth in gate (#675)
    * fix(authn/basic): Enable basic auth
    
    * fix(authn/basic): Removed multiauth support from basic authn
    
    * fix(authn/basic): Arranged module names in alphabetical order
    
    * fix(authn/basic): Spinnaker User principal object instead of a string
    
    * fix(authn/basic): Added tests for basic auth
    
    * fix(authn/basic): Refactor from groovy to java
    
    * fix(authn/basic): Renamed property for enabling basic auth
    
    * fix(authn/basic): Fixed and added tests for security.basic combinations

---
+++ gate-basic/src/main/java/com/netflix/spinnaker/gate/security/basic/BasicAuthConfig.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.gate.security.basic;
+
+import com.netflix.spinnaker.gate.config.AuthConfig;
+import com.netflix.spinnaker.gate.security.SpinnakerAuthConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@ConditionalOnExpression("${security.basicform.enabled:false}")
+@Configuration
+@SpinnakerAuthConfig
+@EnableWebSecurity
+public class BasicAuthConfig extends WebSecurityConfigurerAdapter {
+
+  private final AuthConfig authConfig;
+
+  private final BasicAuthProvider authProvider;
+
+  @Autowired
+  public BasicAuthConfig(AuthConfig authConfig, BasicAuthProvider authProvider) {
+    this.authConfig = authConfig;
+    this.authProvider = authProvider;
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) {
+    auth.authenticationProvider(authProvider);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.formLogin().and().httpBasic();
+    authConfig.configure(http);
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    authConfig.configure(web);
+  }
+}
---
+++ gate-basic/src/main/java/com/netflix/spinnaker/gate/security/basic/BasicAuthProvider.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spinnaker.gate.security.basic;
+
+import com.netflix.spinnaker.security.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+@ConditionalOnExpression("${security.basicform.enabled:false}")
+@Component
+public class BasicAuthProvider implements AuthenticationProvider {
+
+  private final SecurityProperties securityProperties;
+
+  @Autowired
+  public BasicAuthProvider(SecurityProperties securityProperties) {
+    this.securityProperties = securityProperties;
+
+    if (securityProperties.getUser() == null) {
+      throw new AuthenticationServiceException("User credentials are not configured");
+    }
+  }
+
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    String name = authentication.getName();
+    String password = authentication.getCredentials() != null ? authentication.getCredentials().toString() : null;
+
+    if (!securityProperties.getUser().getName().equals(name) ||
+        !securityProperties.getUser().getPassword().equals(password)) {
+      throw new BadCredentialsException("Invalid username/password combination");
+    }
+
+    User user = new User();
+    user.setEmail(name);
+    user.setUsername(name);
+    user.setRoles(Collections.singletonList("USER"));
+    return new UsernamePasswordAuthenticationToken(user, password, new ArrayList<>());
+  }
+
+  @Override
+  public boolean supports(Class<?> authentication) {
+    return authentication == UsernamePasswordAuthenticationToken.class;
+  }
+}
