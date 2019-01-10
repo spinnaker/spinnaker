fe944f406c4187f2b099e2eeaf0ca301de445411
Justin Reynolds <justinreynolds@gmail.com>
2019-01-10 12:04:56 -0800

    fix(core/deployment): Fix rolling red back rollback checkbox state (#6345)

--- app/scripts/modules/core/src/deploymentStrategy/strategies/rollingredblack/AdditionalFields.tsx
+++ app/scripts/modules/core/src/deploymentStrategy/strategies/rollingredblack/AdditionalFields.tsx
@@ -12,6 +12,9 @@ export interface IRollingRedBlackCommand extends IServerGroupCommand {
   pipelineBeforeCleanup: {
     application: string;
   };
+  rollback: {
+    onFailure: boolean;
+  };
   scaleDown: boolean;
   targetPercentages: number[] | string;
 }
@@ -36,6 +39,11 @@ export class AdditionalFields extends React.Component<IRollingRedBlackStrategyAd
     this.forceUpdate();
   };
 
+  private rollbackOnFailureChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.props.command.rollback.onFailure = e.target.checked;
+    this.forceUpdate();
+  };
+
   public render() {
     const { NumberList } = NgReact;
     const { command } = this.props;
@@ -43,7 +51,7 @@ export class AdditionalFields extends React.Component<IRollingRedBlackStrategyAd
       <div className="form-group">
         <div className="col-md-12 checkbox" style={{ marginTop: 0 }}>
           <label>
-            <input type="checkbox" ng-model="$ctrl.command.rollback.onFailure" />
+            <input type="checkbox" checked={command.rollback.onFailure} onChange={this.rollbackOnFailureChange} />
             Rollback to previous server group if deployment fails <HelpField id="strategy.rollingRedBlack.rollback" />
           </label>
         </div>
