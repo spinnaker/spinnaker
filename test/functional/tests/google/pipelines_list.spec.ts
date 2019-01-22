a0a7e3ec38b8a1818e027a8eb255a30579c9010f
Scott <sbws@google.com>
2019-01-22 11:01:31 -0500

    feat(tests): add pipelines_list functional test for GCE (#6405)

--- test/functional/tests/google/pipelines_list.spec.ts
+++ test/functional/tests/google/pipelines_list.spec.ts
@@ -4,22 +4,22 @@ import { InfrastructurePage } from '../core/pages/InfrastructurePage';
 import { PipelinesListPage } from '../core/pages/PipelinesListPage';
 import { HeaderNavLocators } from '../core/locators/HeaderNavLocators';
 
-describe('AppEngine Pipelines', () => {
+describe('Compute Pipelines', () => {
   describe('List', () => {
-    it('shows stored appengine pipelines with their account tag', () => {
+    it('shows stored GCE pipelines with their account tag', () => {
       const home = new HomePage();
       const apps = new ApplicationsListPage();
       const infra = new InfrastructurePage();
       const pipelines = new PipelinesListPage();
       home.open();
       home.click(HeaderNavLocators.applicationsButton);
-      apps.clickOnApplicationLink('gae');
+      apps.clickOnApplicationLink('compute');
       infra.click(HeaderNavLocators.pipelinesButton);
       pipelines.awaitLocator(PipelinesListPage.locators.executionGroup);
       const executionGroups = browser.$$(PipelinesListPage.locators.executionGroup);
       const firstAccountTag = browser.$(PipelinesListPage.locators.accountTag);
-      expect(executionGroups.length).toBe(4);
-      expect(firstAccountTag.getText()).toEqual('GAE');
+      expect(executionGroups.length).toBe(9);
+      expect(firstAccountTag.getText()).toEqual('GCE');
     });
   });
 });
