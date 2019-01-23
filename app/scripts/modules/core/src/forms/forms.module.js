abe1eea2164a5dbae04f3755273e35a46bdb185f
Chris Thielen <github@sandgnat.com>
2019-01-23 14:16:46 -0800

    Revert "chore(uiSelect): Remove decorators for uiSelectMultiple which we no longer use, AFAICT"
    This reverts commit be2fc98cd2a217d90ffd4f47c9ddf60fba8c63fd.

--- app/scripts/modules/core/src/forms/forms.module.js
+++ app/scripts/modules/core/src/forms/forms.module.js
@@ -9,6 +9,7 @@ module.exports = angular.module('spinnaker.core.forms', [
   require('./autofocus/autofocus.directive.js').name,
   require('./checklist/checklist.directive.js').name,
   require('./checkmap/checkmap.directive.js').name,
+  require('./ignoreEmptyDelete.directive.js').name,
   BUTTON_BUSY_INDICATOR_COMPONENT,
   require('./mapEditor/mapEditor.component.js').name,
   require('./validateOnSubmit/validateOnSubmit.directive.js').name,
