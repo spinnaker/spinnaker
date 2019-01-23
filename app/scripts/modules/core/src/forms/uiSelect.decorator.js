be2fc98cd2a217d90ffd4f47c9ddf60fba8c63fd
Chris Thielen <github@sandgnat.com>
2019-01-23 13:43:26 -0800

    chore(uiSelect): Remove decorators for uiSelectMultiple which we no longer use, AFAICT

--- app/scripts/modules/core/src/forms/uiSelect.decorator.js
+++ app/scripts/modules/core/src/forms/uiSelect.decorator.js
@@ -1,29 +1,6 @@
 'use strict';
 
 module.exports = function($provide) {
-  $provide.decorator('uiSelectMultipleDirective', function($delegate) {
-    const [uiSelect] = $delegate,
-      originalLink = uiSelect.link,
-      SELECT_EVENT_KEY = 'uis:select';
-
-    uiSelect.link = function(scope, element, attrs, ctrls) {
-      originalLink.apply(this, arguments);
-      const [$select] = ctrls;
-      scope.$$listeners[SELECT_EVENT_KEY] = [];
-      scope.$on(SELECT_EVENT_KEY, function(event, item) {
-        if ($select.selected.length >= $select.limit) {
-          return;
-        }
-        if (!event.defaultPrevented) {
-          $select.selected.push(item);
-          scope.$selectMultiple.updateModel();
-        }
-      });
-    };
-
-    return $delegate;
-  });
-
   // The GCP nested approach throws an exception when activating the select when there's a nested select option,
   // but it's harmless so we trap it and discard it
 
