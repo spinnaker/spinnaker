941839813c938ca774d7d49fd5dd9448e72143a5
Rob Fletcher <rfletcher@netflix.com>
2019-01-23 13:14:06 -0800

    chore(dependencies) Upgraded Gradle & Kotlin

--- keel-core/src/test/kotlin/com/netflix/spinnaker/keel/api/AssetMetadataTest.kt
+++ keel-core/src/test/kotlin/com/netflix/spinnaker/keel/api/AssetMetadataTest.kt
@@ -4,43 +4,42 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.google.gson.GsonBuilder
-import com.oneeyedmen.minutest.junit.toTestFactory
+import com.oneeyedmen.minutest.junit.JUnit5Minutests
 import com.oneeyedmen.minutest.rootContext
-import org.junit.jupiter.api.TestFactory
 import strikt.api.expectThat
 import strikt.assertions.containsKey
 import strikt.assertions.hasEntry
 import strikt.assertions.isEqualTo
 import java.util.*
 
-internal object AssetMetadataTest {
+internal object AssetMetadataTest : JUnit5Minutests {
 
   val mapper = YAMLMapper()
     .registerKotlinModule()
 
   val gson = GsonBuilder().setPrettyPrinting().serializeNulls().create()
 
-  @TestFactory
-  fun serialize() = rootContext<AssetMetadata> {
-    fixture {
-      AssetMetadata(
-        AssetName("my-new-cron-object"),
-        285,
-        UUID.fromString("9423255b-4600-11e7-af6a-28d2447dc82b"),
-        mapOf(
-          "clusterName" to "",
-          "creationTimestamp" to "2017-05-31T12:56:35Z",
-          "deletionGracePeriodSeconds" to null,
-          "deletionTimestamp" to null,
-          "namespace" to "default",
-          "selfLink" to "/apis/stable.example.com/v1/namespaces/default/crontabs/my-new-cron-object"
+  override val tests = rootContext<Unit> {
+    derivedContext<AssetMetadata>("serialization") {
+      fixture {
+        AssetMetadata(
+          AssetName("my-new-cron-object"),
+          285,
+          UUID.fromString("9423255b-4600-11e7-af6a-28d2447dc82b"),
+          mapOf(
+            "clusterName" to "",
+            "creationTimestamp" to "2017-05-31T12:56:35Z",
+            "deletionGracePeriodSeconds" to null,
+            "deletionTimestamp" to null,
+            "namespace" to "default",
+            "selfLink" to "/apis/stable.example.com/v1/namespaces/default/crontabs/my-new-cron-object"
+          )
         )
-      )
-    }
+      }
 
-    test("serializes as a hash using Jackson") {
-      expectThat(mapper.writeValueAsString(this)).isEqualTo(
-        """---
+      test("serializes as a hash using Jackson") {
+        expectThat(mapper.writeValueAsString(this)).isEqualTo(
+          """---
           |name: "my-new-cron-object"
           |resourceVersion: 285
           |uid: "9423255b-4600-11e7-af6a-28d2447dc82b"
@@ -51,11 +50,11 @@ internal object AssetMetadataTest {
           |namespace: "default"
           |selfLink: "/apis/stable.example.com/v1/namespaces/default/crontabs/my-new-cron-object"
           |""".trimMargin())
-    }
+      }
 
-    test("serializes as a hash using Gson") {
-      expectThat(gson.toJson(this)).isEqualTo(
-        """{
+      test("serializes as a hash using Gson") {
+        expectThat(gson.toJson(this)).isEqualTo(
+          """{
           |  "name": "my-new-cron-object",
           |  "uid": "9423255b-4600-11e7-af6a-28d2447dc82b",
           |  "resourceVersion": 285,
@@ -66,14 +65,12 @@ internal object AssetMetadataTest {
           |  "namespace": "default",
           |  "selfLink": "/apis/stable.example.com/v1/namespaces/default/crontabs/my-new-cron-object"
           |}""".trimMargin())
+      }
     }
-  }
-    .toTestFactory()
 
-  @TestFactory
-  fun deserializeWithJackson() = rootContext<String> {
-    fixture {
-      """---
+    derivedContext<String>("deserialization with Jackson") {
+      fixture {
+        """---
         |clusterName: ""
         |creationTimestamp: 2017-05-31T12:56:35Z
         |deletionGracePeriodSeconds: null
@@ -84,28 +81,26 @@ internal object AssetMetadataTest {
         |selfLink: /apis/stable.example.com/v1/namespaces/default/crontabs/my-new-cron-object
         |uid: 9423255b-4600-11e7-af6a-28d2447dc82b
       """.trimMargin()
-    }
+      }
 
-    test("deserializes properties") {
-      expectThat(mapper.readValue<AssetMetadata>(this)) {
-        get { name }.isEqualTo(AssetName("my-new-cron-object"))
-        get { uid }.isEqualTo(UUID.fromString("9423255b-4600-11e7-af6a-28d2447dc82b"))
-        get { resourceVersion }.isEqualTo(285L)
+      test("deserializes properties") {
+        expectThat(mapper.readValue<AssetMetadata>(this)) {
+          get { name }.isEqualTo(AssetName("my-new-cron-object"))
+          get { uid }.isEqualTo(UUID.fromString("9423255b-4600-11e7-af6a-28d2447dc82b"))
+          get { resourceVersion }.isEqualTo(285L)
+        }
+      }
+
+      test("deserializes extra data") {
+        expectThat(mapper.readValue<AssetMetadata>(this))
+          .get { data }
+          .hasEntry("namespace", "default")
       }
     }
 
-    test("deserializes extra data") {
-      expectThat(mapper.readValue<AssetMetadata>(this))
-        .get { data }
-        .hasEntry("namespace", "default")
-    }
-  }
-    .toTestFactory()
-
-  @TestFactory
-  fun deserializeWithGson() = rootContext<String> {
-    fixture {
-      """{
+    derivedContext<String>("deserialization with Gson") {
+      fixture {
+        """{
         |  "annotations" : {
         |    "kubectl.kubernetes.io/last-applied-configuration" : "{\"apiVersion\":\"file.spinnaker.netflix.com/v1\",\"kind\":\"message\",\"metadata\":{\"annotations\":{},\"name\":\"my-message\"},\"spec\":{\"text\":\"OMFG\"}}\n"
         |  },
@@ -118,21 +113,21 @@ internal object AssetMetadataTest {
         |  "selfLink" : "/apis/file.spinnaker.netflix.com/v1/messages/my-message",
         |  "uid" : "16742412-f8e0-11e8-8396-025000000001"
         |}""".trimMargin()
-    }
-    test("deserializes properties using Gson") {
-      expectThat(gson.fromJson(this, AssetMetadata::class.java)) {
-        get { name }.isEqualTo(AssetName("my-message"))
-        get { uid }.isEqualTo(UUID.fromString("16742412-f8e0-11e8-8396-025000000001"))
-        get { resourceVersion }.isEqualTo(280688L)
+      }
+      test("deserializes properties using Gson") {
+        expectThat(gson.fromJson(this, AssetMetadata::class.java)) {
+          get { name }.isEqualTo(AssetName("my-message"))
+          get { uid }.isEqualTo(UUID.fromString("16742412-f8e0-11e8-8396-025000000001"))
+          get { resourceVersion }.isEqualTo(280688L)
+        }
+      }
+
+      test("deserializes extra data using Gson") {
+        expectThat(gson.fromJson(this, AssetMetadata::class.java))
+          .get { data }
+          .hasEntry("namespace", "")
+          .containsKey("annotations")
       }
     }
-
-    test("deserializes extra data using Gson") {
-      expectThat(gson.fromJson(this, AssetMetadata::class.java))
-        .get { data }
-        .hasEntry("namespace", "")
-        .containsKey("annotations")
-    }
   }
-    .toTestFactory()
 }
