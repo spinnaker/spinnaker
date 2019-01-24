4600bbd9d1588963c6bdde31a334674ffa90d8bc
Chris Thielen <github@sandgnat.com>
2019-01-24 15:05:33 -0800

    chore(package): remove unused (hopefully) spin.js package

--- app/scripts/modules/core/src/bootstrap/appInit.ts
+++ app/scripts/modules/core/src/bootstrap/appInit.ts
@@ -1,7 +1,6 @@
 // Random deck initialization code
 import { IRootScopeService } from 'angular';
 
-const Spinner = require('spin.js');
 import { bootstrapModule } from './bootstrap.module';
 import { SETTINGS } from 'core/config';
 import { CacheInitializerService } from 'core/cache';
@@ -9,8 +8,6 @@ import { StateService } from '@uirouter/core';
 
 bootstrapModule.run(($rootScope: IRootScopeService, $state: StateService) => {
   'ngInject';
-  (window as any).Spinner = Spinner;
-
   $rootScope.feature = SETTINGS.feature;
   $rootScope.$state = $state; // TODO: Do we really need this?
 });
