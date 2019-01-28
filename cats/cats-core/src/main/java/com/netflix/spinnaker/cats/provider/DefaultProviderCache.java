bc8d9ff61e77b063fad3f2b2b33c08be1d9536ac
Asher Feldman <ccpanda@gmail.com>
2019-01-28 13:05:31 -0800

    feat(cats): improved support for incremental CacheResults (#3315)

--- cats/cats-core/src/main/java/com/netflix/spinnaker/cats/provider/DefaultProviderCache.java
+++ cats/cats-core/src/main/java/com/netflix/spinnaker/cats/provider/DefaultProviderCache.java
@@ -35,225 +35,235 @@ import java.util.Set;
 /**
  * An implementation of ProviderCache that writes through to a provided backing
  * WriteableCache.
- *
+ * <p>
  * This implementation will handle aggregating results from multiple sources, and
  * the view methods will merge relationships from all sources into a single relationship.
  */
 public class DefaultProviderCache implements ProviderCache {
 
-    private static final String ALL_ID = "_ALL_"; //dirty = true
-    private static final Map<String, Object> ALL_ATTRIBUTE = Collections.unmodifiableMap(new HashMap<String, Object>(1) {{
-        put("id", ALL_ID);
-    }});
+  private static final String ALL_ID = "_ALL_"; //dirty = true
+  private static final Map<String, Object> ALL_ATTRIBUTE = Collections.unmodifiableMap(new HashMap<String, Object>(1) {{
+    put("id", ALL_ID);
+  }});
 
-    private final WriteableCache backingStore;
+  private final WriteableCache backingStore;
 
-    public DefaultProviderCache(WriteableCache backingStore) {
-        this.backingStore = backingStore;
+  public DefaultProviderCache(WriteableCache backingStore) {
+    this.backingStore = backingStore;
+  }
+
+  @Override
+  public CacheData get(String type, String id) {
+    return get(type, id, null);
+  }
+
+  @Override
+  public CacheData get(String type, String id, CacheFilter cacheFilter) {
+    validateTypes(type);
+    if (ALL_ID.equals(id)) {
+      return null;
+    }
+    CacheData item = backingStore.get(type, id, cacheFilter);
+    if (item == null) {
+      return null;
     }
 
-    @Override
-    public CacheData get(String type, String id) {
-        return get(type, id, null);
-    }
+    return mergeRelationships(item);
+  }
 
-    @Override
-    public CacheData get(String type, String id, CacheFilter cacheFilter) {
-        validateTypes(type);
-        if (ALL_ID.equals(id)) {
-            return null;
+  @Override
+  public Collection<CacheData> getAll(String type) {
+    return getAll(type, (CacheFilter) null);
+  }
+
+  @Override
+  public Collection<CacheData> getAll(String type, CacheFilter cacheFilter) {
+    validateTypes(type);
+    Collection<CacheData> all = backingStore.getAll(type, cacheFilter);
+    return buildResponse(all);
+  }
+
+  @Override
+  public Collection<CacheData> getAll(String type, Collection<String> identifiers) {
+    return getAll(type, identifiers, null);
+  }
+
+  @Override
+  public Collection<CacheData> getAll(String type, Collection<String> identifiers, CacheFilter cacheFilter) {
+    validateTypes(type);
+    Collection<CacheData> byId = backingStore.getAll(type, identifiers, cacheFilter);
+    return buildResponse(byId);
+  }
+
+  @Override
+  public Collection<CacheData> getAll(String type, String... identifiers) {
+    return getAll(type, Arrays.asList(identifiers));
+  }
+
+  @Override
+  public Collection<String> existingIdentifiers(String type, Collection<String> identifiers) {
+    Set<String> existing = new HashSet<>(backingStore.existingIdentifiers(type, identifiers));
+    existing.remove(ALL_ID);
+    return existing;
+  }
+
+  @Override
+  public Collection<String> getIdentifiers(String type) {
+    validateTypes(type);
+    Set<String> identifiers = new HashSet<>(backingStore.getIdentifiers(type));
+    identifiers.remove(ALL_ID);
+    return identifiers;
+  }
+
+  @Override
+  public Collection<String> filterIdentifiers(String type, String glob) {
+    validateTypes(type);
+    Set<String> identifiers = new HashSet<>(backingStore.filterIdentifiers(type, glob));
+    identifiers.remove(ALL_ID);
+
+    return identifiers;
+  }
+
+  @Override
+  public void putCacheResult(String sourceAgentType, Collection<String> authoritativeTypes, CacheResult cacheResult) {
+    Set<String> allTypes = new HashSet<>(cacheResult.getCacheResults().keySet());
+    allTypes.addAll(authoritativeTypes);
+    allTypes.addAll(cacheResult.getEvictions().keySet());
+    validateTypes(allTypes);
+
+    Map<String, Collection<String>> evictions = new HashMap<>();
+
+    for (String type : allTypes) {
+      final Collection<String> previousSet;
+      if (authoritativeTypes.contains(type)) {
+        previousSet = getExistingSourceIdentifiers(type, sourceAgentType);
+      } else {
+        previousSet = new HashSet<>();
+      }
+      if (cacheResult.getCacheResults().containsKey(type)) {
+        cacheDataType(type, sourceAgentType, cacheResult.getCacheResults().get(type));
+        for (CacheData data : cacheResult.getCacheResults().get(type)) {
+          previousSet.remove(data.getId());
         }
-        CacheData item = backingStore.get(type, id, cacheFilter);
-        if (item == null) {
-            return null;
-        }
-
-        return mergeRelationships(item);
+      }
+      if (cacheResult.getEvictions().containsKey(type)) {
+        previousSet.addAll(cacheResult.getEvictions().get(type));
+      }
+      if (!previousSet.isEmpty()) {
+        evictions.put(type, previousSet);
+      }
     }
 
-    @Override
-    public Collection<CacheData> getAll(String type) {
-        return getAll(type, (CacheFilter) null);
+    for (Map.Entry<String, Collection<String>> eviction : evictions.entrySet()) {
+      evictDeletedItems(eviction.getKey(), eviction.getValue());
     }
+  }
 
-    @Override
-    public Collection<CacheData> getAll(String type, CacheFilter cacheFilter) {
-        validateTypes(type);
-        Collection<CacheData> all = backingStore.getAll(type, cacheFilter);
-        return buildResponse(all);
+  @Override
+  public void addCacheResult(String sourceAgentType, Collection<String> authoritativeTypes, CacheResult cacheResult) {
+    Set<String> allTypes = new HashSet<>(cacheResult.getCacheResults().keySet());
+    validateTypes(allTypes);
+
+    allTypes.forEach(type -> {
+      cacheDataType(type, sourceAgentType, cacheResult.getCacheResults().get(type));
+    });
+  }
+
+  @Override
+  public void putCacheData(String sourceAgentType, CacheData cacheData) {
+    backingStore.merge(sourceAgentType, cacheData);
+  }
+
+  private void validateTypes(String... types) {
+    validateTypes(Arrays.asList(types));
+  }
+
+  private void validateTypes(Collection<String> types) {
+    Set<String> invalid = new HashSet<>();
+    for (String type : types) {
+      if (!validType(type)) {
+        invalid.add(type);
+      }
     }
-
-    @Override
-    public Collection<CacheData> getAll(String type, Collection<String> identifiers) {
-        return getAll(type, identifiers, null);
+    if (!invalid.isEmpty()) {
+      throw new IllegalArgumentException("Types contain unsupported characters: " + invalid);
     }
+  }
 
-    @Override
-    public Collection<CacheData> getAll(String type, Collection<String> identifiers, CacheFilter cacheFilter) {
-        validateTypes(type);
-        Collection<CacheData> byId = backingStore.getAll(type, identifiers, cacheFilter);
-        return buildResponse(byId);
+  private boolean validType(String type) {
+    return type.indexOf(':') == -1;
+  }
+
+  private Collection<CacheData> buildResponse(Collection<CacheData> source) {
+    Collection<CacheData> response = new ArrayList<>(source.size());
+    for (CacheData item : source) {
+      if (!ALL_ID.equals(item.getId())) {
+        response.add(mergeRelationships(item));
+      }
     }
+    return Collections.unmodifiableCollection(response);
+  }
 
-    @Override
-    public Collection<CacheData> getAll(String type, String... identifiers) {
-        return getAll(type, Arrays.asList(identifiers));
+  private Collection<String> getExistingSourceIdentifiers(String type, String sourceAgentType) {
+    CacheData all = backingStore.get(type, ALL_ID, RelationshipCacheFilter.include(sourceAgentType));
+    if (all == null) {
+      return new HashSet<>();
     }
-
-    @Override
-    public Collection<String> existingIdentifiers(String type, Collection<String> identifiers) {
-        Set<String> existing = new HashSet<>(backingStore.existingIdentifiers(type, identifiers));
-        existing.remove(ALL_ID);
-        return existing;
+    Collection<String> relationship = all.getRelationships().get(sourceAgentType);
+    if (relationship == null) {
+      return new HashSet<>();
     }
+    return relationship;
+  }
 
-    @Override
-    public Collection<String> getIdentifiers(String type) {
-        validateTypes(type);
-        Set<String> identifiers = new HashSet<>(backingStore.getIdentifiers(type));
-        identifiers.remove(ALL_ID);
-        return identifiers;
+  private void cacheDataType(String type, String sourceAgentType, Collection<CacheData> items) {
+    Collection<String> idSet = new HashSet<>();
+
+    int ttlSeconds = -1;
+    Collection<CacheData> toStore = new ArrayList<>(items.size() + 1);
+    for (CacheData item : items) {
+      idSet.add(item.getId());
+      toStore.add(uniqueifyRelationships(item, sourceAgentType));
+
+      if (item.getTtlSeconds() > ttlSeconds) {
+        ttlSeconds = item.getTtlSeconds();
+      }
     }
+    Map<String, Collection<String>> allRelationship = new HashMap<>();
+    allRelationship.put(sourceAgentType, idSet);
 
-    @Override
-    public Collection<String> filterIdentifiers(String type, String glob) {
-        validateTypes(type);
-        Set<String> identifiers = new HashSet<>(backingStore.filterIdentifiers(type, glob));
-        identifiers.remove(ALL_ID);
+    toStore.add(new DefaultCacheData(ALL_ID, ttlSeconds, ALL_ATTRIBUTE, allRelationship));
+    backingStore.mergeAll(type, toStore);
+  }
 
-        return identifiers;
+  private CacheData uniqueifyRelationships(CacheData source, String sourceAgentType) {
+    Map<String, Collection<String>> relationships = new HashMap<>(source.getRelationships().size());
+    for (Map.Entry<String, Collection<String>> entry : source.getRelationships().entrySet()) {
+      relationships.put(entry.getKey() + ':' + sourceAgentType, entry.getValue());
     }
+    return new DefaultCacheData(source.getId(), source.getTtlSeconds(), source.getAttributes(), relationships);
+  }
 
-    @Override
-    public void putCacheResult(String sourceAgentType, Collection<String> authoritativeTypes, CacheResult cacheResult) {
-        Set<String> allTypes = new HashSet<>(cacheResult.getCacheResults().keySet());
-        allTypes.addAll(authoritativeTypes);
-        allTypes.addAll(cacheResult.getEvictions().keySet());
-        validateTypes(allTypes);
-
-        Map<String, Collection<String>> evictions = new HashMap<>();
-
-        for (String type : allTypes) {
-            final Collection<String> previousSet;
-            if (authoritativeTypes.contains(type)) {
-                previousSet = getExistingSourceIdentifiers(type, sourceAgentType);
-            } else {
-                previousSet = new HashSet<>();
-            }
-            if (cacheResult.getCacheResults().containsKey(type)) {
-                cacheDataType(type, sourceAgentType, cacheResult.getCacheResults().get(type));
-                for (CacheData data : cacheResult.getCacheResults().get(type)) {
-                    previousSet.remove(data.getId());
-                }
-            }
-            if (cacheResult.getEvictions().containsKey(type)) {
-              previousSet.addAll(cacheResult.getEvictions().get(type));
-            }
-            if (!previousSet.isEmpty()) {
-              evictions.put(type, previousSet);
-            }
-        }
-
-        for (Map.Entry<String, Collection<String>> eviction : evictions.entrySet()) {
-          evictDeletedItems(eviction.getKey(), eviction.getValue());
-        }
+  private CacheData mergeRelationships(CacheData source) {
+    Map<String, Collection<String>> relationships = new HashMap<>(source.getRelationships().size());
+    for (Map.Entry<String, Collection<String>> entry : source.getRelationships().entrySet()) {
+      int idx = entry.getKey().indexOf(':');
+      if (idx == -1) {
+        throw new IllegalStateException("Expected delimiter in relationship key");
+      }
+      String type = entry.getKey().substring(0, idx);
+      Collection<String> values = relationships.get(type);
+      if (values == null) {
+        values = new HashSet<>();
+        relationships.put(type, values);
+      }
+      values.addAll(entry.getValue());
     }
+    return new DefaultCacheData(source.getId(), source.getAttributes(), relationships);
+  }
 
-    @Override
-    public void putCacheData(String sourceAgentType, CacheData cacheData) {
-        backingStore.merge(sourceAgentType, cacheData);
-    }
-
-    private void validateTypes(String... types) {
-        validateTypes(Arrays.asList(types));
-    }
-
-    private void validateTypes(Collection<String> types) {
-        Set<String> invalid = new HashSet<>();
-        for (String type : types) {
-            if (!validType(type)) {
-                invalid.add(type);
-            }
-        }
-        if (!invalid.isEmpty()) {
-            throw new IllegalArgumentException("Types contain unsupported characters: " + invalid);
-        }
-    }
-
-    private boolean validType(String type) {
-        return type.indexOf(':') == -1;
-    }
-
-    private Collection<CacheData> buildResponse(Collection<CacheData> source) {
-        Collection<CacheData> response = new ArrayList<>(source.size());
-        for (CacheData item : source) {
-            if (!ALL_ID.equals(item.getId())) {
-                response.add(mergeRelationships(item));
-            }
-        }
-        return Collections.unmodifiableCollection(response);
-    }
-
-    private Collection<String> getExistingSourceIdentifiers(String type, String sourceAgentType) {
-        CacheData all = backingStore.get(type, ALL_ID, RelationshipCacheFilter.include(sourceAgentType));
-        if (all == null) {
-            return new HashSet<>();
-        }
-        Collection<String> relationship = all.getRelationships().get(sourceAgentType);
-        if (relationship == null) {
-            return new HashSet<>();
-        }
-        return relationship;
-    }
-
-    private void cacheDataType(String type, String sourceAgentType, Collection<CacheData> items) {
-        Collection<String> idSet = new HashSet<>();
-
-        int ttlSeconds = -1;
-        Collection<CacheData> toStore = new ArrayList<>(items.size() + 1);
-        for (CacheData item : items) {
-            idSet.add(item.getId());
-            toStore.add(uniqueifyRelationships(item, sourceAgentType));
-
-            if (item.getTtlSeconds() > ttlSeconds) {
-                ttlSeconds = item.getTtlSeconds();
-            }
-        }
-        Map<String, Collection<String>> allRelationship = new HashMap<>();
-        allRelationship.put(sourceAgentType, idSet);
-
-        toStore.add(new DefaultCacheData(ALL_ID, ttlSeconds, ALL_ATTRIBUTE, allRelationship));
-        backingStore.mergeAll(type, toStore);
-    }
-
-    private CacheData uniqueifyRelationships(CacheData source, String sourceAgentType) {
-        Map<String, Collection<String>> relationships = new HashMap<>(source.getRelationships().size());
-        for (Map.Entry<String, Collection<String>> entry : source.getRelationships().entrySet()) {
-            relationships.put(entry.getKey() + ':' + sourceAgentType, entry.getValue());
-        }
-        return new DefaultCacheData(source.getId(), source.getTtlSeconds(), source.getAttributes(), relationships);
-    }
-
-    private CacheData mergeRelationships(CacheData source) {
-        Map<String, Collection<String>> relationships = new HashMap<>(source.getRelationships().size());
-        for (Map.Entry<String, Collection<String>> entry : source.getRelationships().entrySet()) {
-            int idx = entry.getKey().indexOf(':');
-            if (idx == -1) {
-                throw new IllegalStateException("Expected delimiter in relationship key");
-            }
-            String type = entry.getKey().substring(0, idx);
-            Collection<String> values = relationships.get(type);
-            if (values == null) {
-                values = new HashSet<>();
-                relationships.put(type, values);
-            }
-            values.addAll(entry.getValue());
-        }
-        return new DefaultCacheData(source.getId(), source.getAttributes(), relationships);
-    }
-
-    @Override
-    public void evictDeletedItems(String type, Collection<String> ids) {
-        backingStore.evictAll(type, ids);
-    }
+  @Override
+  public void evictDeletedItems(String type, Collection<String> ids) {
+    backingStore.evictAll(type, ids);
+  }
 }
