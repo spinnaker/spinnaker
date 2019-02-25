3c2b8f10ff9db55b27c38a9d81cb4b981d45dcd2
Erik Munson <erik@ipsumcreative.com>
2019-02-25 13:52:31 -0800

    chore(build): use --frozen-lockfile for travis (#412)

--- travis/installViaTravis.sh
+++ travis/installViaTravis.sh
@@ -5,4 +5,4 @@ NODE_JS_VERSION=`node -e 'console.log(require("./package.json").engines.node.rep
 echo -e "Installing/activating node v$NODE_JS_VERSION"
 
 nvm install $NODE_JS_VERSION
-yarn
+yarn --frozen-lockfile
