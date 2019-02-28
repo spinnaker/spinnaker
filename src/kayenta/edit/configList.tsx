28bdc1deb33f580aeba0383f141ef94609e27d47
Erik Munson <erik@ipsumcreative.com>
2019-02-27 16:58:30 -0800

    fix(configs): make the config list click target humanely sized (#416)

--- src/kayenta/edit/configList.tsx
+++ src/kayenta/edit/configList.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { sortBy } from 'lodash';
-import { UISref, UISrefActive } from '@uirouter/react';
+import { UISref } from '@uirouter/react';
 
 import { ICanaryState } from 'kayenta/reducers';
 import { ICanaryConfigSummary } from 'kayenta/domain/ICanaryConfigSummary';
 import CreateConfigButton from './createConfigButton';
 import FormattedDate from 'kayenta/layout/formattedDate';
 import { OwnedBy } from './ownedBy';
+
+import './configList.less';
 
 interface IConfigListStateProps {
   configs: ICanaryConfigSummary[];
@@ -21,19 +23,19 @@ interface IConfigListStateProps {
 function ConfigList({ configs, selectedConfigId, application }: IConfigListStateProps) {
   return (
     <section className="config-list">
-      <ul className="tabs-vertical list-unstyled" style={{ wordBreak: 'break-all' }}>
+      <ul className="tabs-vertical list-unstyled " style={{ wordBreak: 'break-all' }}>
         {configs.map(config => (
           <li key={config.id} className={config.id === selectedConfigId ? 'selected' : ''}>
-            <UISrefActive class="active">
-              <UISref to=".configDetail" params={{ id: config.id, new: false, copy: false }}>
-                <a className="heading-4">{config.name}</a>
-              </UISref>
-            </UISrefActive>
-            <div className="body-small color-text-caption caption" style={{ marginTop: '5px', marginBottom: '0' }}>
-              Edited: <FormattedDate dateIso={config.updatedTimestampIso} />
-              <br />
-              <OwnedBy owningApplications={config.applications} currentApplication={application} />
-            </div>
+            <UISref to=".configDetail" params={{ id: config.id, new: false, copy: false }}>
+              <a>
+                <div className="heading-4 color-text-primary">{config.name}</div>
+                <div className="body-small color-text-caption caption" style={{ marginTop: '5px', marginBottom: '0' }}>
+                  Edited: <FormattedDate dateIso={config.updatedTimestampIso} />
+                  <br />
+                  <OwnedBy owningApplications={config.applications} currentApplication={application} />
+                </div>
+              </a>
+            </UISref>
           </li>
         ))}
       </ul>
