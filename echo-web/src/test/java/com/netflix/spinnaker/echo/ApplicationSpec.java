0f5400768c15578fa8bbd53701dfa12a1f577523
Eric Zimanyi <ezimanyi@google.com>
2019-02-28 12:21:40 -0500

    test(core): Basic startup test (#472)
    Test that echo starts up with a very basic config, with just
    the baseUrl for each dependent service defined, and with beans
    mocking these services.

---
+++ echo-web/src/test/java/com/netflix/spinnaker/echo/ApplicationSpec.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.echo;
+
+import com.netflix.spinnaker.echo.pipelinetriggers.orca.OrcaService;
+import com.netflix.spinnaker.echo.services.Front50Service;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {Application.class})
+@TestPropertySource(properties = { "spring.config.location=classpath:echo-test.yml" })
+public class ApplicationSpec {
+  @MockBean
+  Front50Service front50Service;
+
+  @MockBean
+  OrcaService orcaService;
+
+  @Test
+  public void startupTest() {
+  }
+}
