fc5bfe0ecab5e6ac5ccad6d0700d8f9739ffd410
Rob Fletcher <rfletcher@netflix.com>
2019-03-01 12:11:55 -0800

    fix(yaml): fix date/time rendering in YAML

--- keel-core/src/main/kotlin/com/netflix/spinnaker/keel/serialization/ObjectMappers.kt
+++ keel-core/src/main/kotlin/com/netflix/spinnaker/keel/serialization/ObjectMappers.kt
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import de.huxhorn.sulky.ulid.ULID
 import java.text.SimpleDateFormat
@@ -19,29 +18,33 @@ import java.util.*
  * Factory method for [ObjectMapper]s configured how we like 'em.
  */
 fun configuredObjectMapper(): ObjectMapper =
-  jacksonObjectMapper()
-    .registerModule(JavaTimeModule())
-    .registerULIDModule()
-    .enable(WRITE_DATES_AS_TIMESTAMPS)
-    .enable(WRITE_DATES_WITH_ZONE_ID)
-    .enable(WRITE_DATE_KEYS_AS_TIMESTAMPS)
-    .disable(FAIL_ON_UNKNOWN_PROPERTIES)
-    .apply {
-      dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ").apply {
-        timeZone = TimeZone.getDefault()
-      }
-    }
+  ObjectMapper().configureMe()
 
 /**
  * Factory method for [YAMLMapper]s configured how we like 'em.
  */
 fun configuredYamlMapper(): ObjectMapper =
-  YAMLMapper()
-    .registerKotlinModule()
+  YAMLMapper().configureMe()
+
+private fun ObjectMapper.configureMe() =
+  registerKotlinModule()
     .registerULIDModule()
+    .registerModule(JavaTimeModule())
+    .configureSaneDateTimeRepresentation()
+    .disable(FAIL_ON_UNKNOWN_PROPERTIES)
 
 private fun ObjectMapper.registerULIDModule() =
   registerModule(SimpleModule().apply {
     addSerializer(ULID.Value::class.java, ToStringSerializer())
     addDeserializer(ULID.Value::class.java, ULIDDeserializer())
   })
+
+private fun ObjectMapper.configureSaneDateTimeRepresentation() =
+  enable(WRITE_DATES_AS_TIMESTAMPS)
+    .enable(WRITE_DATES_WITH_ZONE_ID)
+    .enable(WRITE_DATE_KEYS_AS_TIMESTAMPS)
+    .apply {
+      dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ").apply {
+        timeZone = TimeZone.getDefault()
+      }
+    }
