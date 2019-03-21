df91608dc0c7d3fc596be54bbad341b3a2e860d7
Jon Schneider <jkschneider@gmail.com>
2019-03-21 18:29:17 -0500

    fix(concourse): Fix concourse build info type (#2779)

--- orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/model/ConcourseTrigger.kt
+++ orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/model/ConcourseTrigger.kt
@@ -16,6 +16,8 @@
 
 package com.netflix.spinnaker.orca.pipeline.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.netflix.spinnaker.kork.artifacts.model.Artifact
 import com.netflix.spinnaker.kork.artifacts.model.ExpectedArtifact
 
@@ -33,6 +35,17 @@ data class ConcourseTrigger
 ) : Trigger {
   override var other: Map<String, Any> = mutableMapOf()
   override var resolvedExpectedArtifacts: List<ExpectedArtifact> = mutableListOf()
-  var buildInfo: BuildInfo<Any>? = null
+  var buildInfo: ConcourseBuildInfo? = null
   var properties: Map<String, Any> = mutableMapOf()
 }
+
+class ConcourseBuildInfo
+@JsonCreator
+constructor(@param:JsonProperty("name") override val name: String?,
+            @param:JsonProperty("number") override val number: Int,
+            @param:JsonProperty("url") override val url: String?,
+            @param:JsonProperty("result") override val result: String?,
+            @param:JsonProperty("artifacts") override val artifacts: List<JenkinsArtifact>?,
+            @param:JsonProperty("scm") override val scm: List<SourceControl>?,
+            @param:JsonProperty("building") override var building: Boolean = false)
+  : BuildInfo<Any>(name, number, url, result, artifacts, scm, building)
