11c19b5ef61b3582920ede000241a7ea14dc8c60
Paul <dotdotdotpaul@users.noreply.github.com>
2019-04-02 14:19:34 -0700

    fix(config): Deck support for --infrastructure-stages (#1267)

--- halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/deck/DeckProfileFactory.java
+++ halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/deck/DeckProfileFactory.java
@@ -117,6 +117,7 @@ public class DeckProfileFactory extends RegistryBackedProfileFactory {
     bindings.put("features.wercker", Boolean.toString(features.getWercker() != null ? features.getWercker() : false));
     bindings.put("features.managedPipelineTemplatesV2UI", Boolean.toString(features.getManagedPipelineTemplatesV2UI() != null ? features.getManagedPipelineTemplatesV2UI() : false));
     bindings.put("features.gremlin", Boolean.toString(features.getGremlin() != null ? features.getGremlin() : false));
+    bindings.put("features.infrastructureStages", Boolean.toString(features.getInfrastructureStages() != null ? features.getInfrastructureStages() : false));
 
     // Configure Kubernetes
     KubernetesProvider kubernetesProvider = deploymentConfiguration.getProviders().getKubernetes();
