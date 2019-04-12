48ba3fcd1e23cd1c4beab18a9e19d44deb2059af
Erik Munson <erik@ipsumcreative.com>
2019-04-11 22:14:15 -0700

    fix(core/pipeline): un-shadow parameter (#6843)

--- app/scripts/modules/core/src/pipeline/config/stages/waitForCondition/SkipConditionWait.tsx
+++ app/scripts/modules/core/src/pipeline/config/stages/waitForCondition/SkipConditionWait.tsx
@@ -21,8 +21,8 @@ const skipRemainingWait = (
 ): void => {
   const { confirmationModalService, executionService } = ReactInjector;
   (event.target as HTMLElement).blur(); // forces closing of the popover when the modal opens
-  const matcher = (execution: IExecution) => {
-    const match = execution.stages.find(test => test.id === stage.id);
+  const matcher = ({ stages }: IExecution) => {
+    const match = stages.find(test => test.id === stage.id);
     return match.status !== 'RUNNING';
   };
 
