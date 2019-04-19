0ec6a7e92db5401c7420d719c0fd151b0f21f31c
maggieneterval <mneterval@google.com>
2019-04-19 16:13:40 -0400

    feat(kubernetes): validate text manifests when rollout strategies enabled

--- app/scripts/modules/kubernetes/src/v2/pipelines/stages/deployManifest/deployManifest.validator.ts
+++ app/scripts/modules/kubernetes/src/v2/pipelines/stages/deployManifest/deployManifest.validator.ts
@@ -2,6 +2,10 @@ import { get, isEmpty } from 'lodash';
 
 import { IPipeline, IStage, IValidatorConfig, ICustomValidator } from '@spinnaker/core';
 
+import { strategyRedBlack } from 'kubernetes/v2/rolloutStrategy/redblack.strategy';
+
+const MAX_VERSION_HISTORY_ANNOTATION = 'strategy.spinnaker.io/max-version-history';
+
 export const deployManifestValidators = (): IValidatorConfig[] => {
   return [
     {
@@ -11,6 +15,20 @@ export const deployManifestValidators = (): IValidatorConfig[] => {
         const services = get(stage, 'trafficManagement.options.services', []);
         if (enabled && isEmpty(services)) {
           return `Select at least one <strong>Service</strong> to enable Spinnaker-managed rollout strategy options.`;
+        }
+        if (enabled && stage.source === 'text') {
+          const manifests = get(stage, 'manifests', []);
+          if (manifests.length !== 1 || get(manifests, [0, 'kind']) !== 'ReplicaSet') {
+            return 'Spinnaker can manage traffic for ReplicaSets only. Please enter exactly one ReplicaSet manifest or disable rollout strategies.';
+          }
+          const strategy = get(stage, 'trafficManagement.options.strategy');
+          const maxVersionHistory = parseInt(
+            get(manifests, [0, 'metadata', 'annotations', MAX_VERSION_HISTORY_ANNOTATION]),
+            10,
+          );
+          if (strategy === strategyRedBlack.key && maxVersionHistory < 2) {
+            return `The max version history specified in your manifest conflicts with the behavior of the Red/Black rollout strategy. Please update your <strong>${MAX_VERSION_HISTORY_ANNOTATION}</strong> annotation to a value greater than or equal to 2.`;
+          }
         }
         return null;
       },
