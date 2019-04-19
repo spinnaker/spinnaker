21f1e766be5ed224ff5d0123d19640f3c4283c7e
Rob Fletcher <rfletcher@netflix.com>
2019-04-19 12:53:11 -0700

    refactor(artifacts): tidy up Redis artifact repository

--- keel-redis/src/main/kotlin/com/netflix/spinnaker/keel/redis/RedisArtifactRepository.kt
+++ keel-redis/src/main/kotlin/com/netflix/spinnaker/keel/redis/RedisArtifactRepository.kt
@@ -7,6 +7,7 @@ import com.netflix.spinnaker.keel.api.DeliveryArtifact
 import com.netflix.spinnaker.keel.api.DeliveryArtifactVersion
 import com.netflix.spinnaker.keel.persistence.ArtifactRepository
 import com.netflix.spinnaker.kork.jedis.RedisClientDelegate
+import redis.clients.jedis.JedisCommands
 
 class RedisArtifactRepository(
   private val redisClient: RedisClientDelegate,
@@ -14,18 +15,18 @@ class RedisArtifactRepository(
 ) : ArtifactRepository {
   override fun store(artifact: DeliveryArtifact) {
     redisClient.withCommandsClient<Unit> { redis ->
-      redis.sadd("keel.delivery_artifacts", objectMapper.writeValueAsString(artifact))
+      redis.sadd("keel.delivery_artifacts", artifact.asJson())
     }
   }
 
   override fun store(artifactVersion: DeliveryArtifactVersion) {
     redisClient.withCommandsClient<Unit> { redis ->
       with(artifactVersion) {
-        require(redis.sismember("keel.delivery_artifacts", objectMapper.writeValueAsString(artifact))) {
+        require(redis.isRegistered(artifact)) {
           "No registered artifact with name ${artifact.name} and type ${artifact.type}"
         }
         redis.sadd(
-          "{keel.delivery_artifact_versions.%s.%s}".format(artifact.type, artifact.name),
+          artifact.versionsKey,
           objectMapper.writeValueAsString(this)
         )
       }
@@ -36,20 +37,23 @@ class RedisArtifactRepository(
     redisClient.withCommandsClient<DeliveryArtifact?> { redis ->
       DeliveryArtifact(name, type)
         .let {
-          if (redis.sismember("keel.delivery_artifacts", objectMapper.writeValueAsString(it))) {
-            it
-          } else {
-            null
-          }
+          if (redis.isRegistered(it)) it else null
         }
     }
 
   override fun versions(artifact: DeliveryArtifact): List<DeliveryArtifactVersion> =
     redisClient.withCommandsClient<List<DeliveryArtifactVersion>> { redis ->
-      redis.smembers("{keel.delivery_artifact_versions.%s.%s}".format(artifact.type, artifact.name))
-        .map {
-          objectMapper.readValue<DeliveryArtifactVersion>(it)
-        }
+      redis.smembers(artifact.versionsKey)
+        .map { objectMapper.readValue<DeliveryArtifactVersion>(it) }
+        .sortedByDescending { it.version }
     }
 
+  private fun JedisCommands.isRegistered(artifact: DeliveryArtifact) =
+    sismember("keel.delivery_artifacts", artifact.asJson())
+
+  private fun DeliveryArtifact.asJson() =
+    objectMapper.writeValueAsString(this)
+
+  private val DeliveryArtifact.versionsKey: String
+    get() = "{keel.delivery_artifact_versions.%s.%s}".format(type, name)
 }
