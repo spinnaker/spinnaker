13512a4406184a37772f9429b26da6909764edcd
Eric Zimanyi <ezimanyi@google.com>
2019-05-10 15:54:50 -0400

    test(gcb): Add test for starting build with artifact (#2897)

--- orca-igor/src/test/groovy/com/netflix/spinnaker/orca/igor/tasks/StartGoogleCloudBuildTaskSpec.groovy
+++ orca-igor/src/test/groovy/com/netflix/spinnaker/orca/igor/tasks/StartGoogleCloudBuildTaskSpec.groovy
@@ -16,7 +16,8 @@
 
 package com.netflix.spinnaker.orca.igor.tasks
 
-
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.netflix.spinnaker.kork.artifacts.model.Artifact
 import com.netflix.spinnaker.orca.TaskResult
 import com.netflix.spinnaker.orca.clouddriver.OortService
 import com.netflix.spinnaker.orca.igor.IgorService
@@ -24,12 +25,17 @@ import com.netflix.spinnaker.orca.igor.model.GoogleCloudBuild
 import com.netflix.spinnaker.orca.pipeline.model.Execution
 import com.netflix.spinnaker.orca.pipeline.model.Stage
 import com.netflix.spinnaker.orca.pipeline.util.ArtifactResolver
+import retrofit.client.Response
+import retrofit.mime.TypedString
 import spock.lang.Specification
 import spock.lang.Subject
 
 class StartGoogleCloudBuildTaskSpec extends Specification {
   def ACCOUNT = "my-account"
-  def BUILD = new HashMap<String, Object>()
+  def BUILD = [
+    steps: []
+  ]
+  def objectMapper = new ObjectMapper()
 
   Execution execution = Mock(Execution)
   IgorService igorService = Mock(IgorService)
@@ -39,17 +45,45 @@ class StartGoogleCloudBuildTaskSpec extends Specification {
   @Subject
   StartGoogleCloudBuildTask task = new StartGoogleCloudBuildTask(igorService, oortService, artifactResolver)
 
-  def "starts a build"() {
+  def "starts a build defined inline"() {
     given:
     def igorResponse = GoogleCloudBuild.builder()
       .id("98edf783-162c-4047-9721-beca8bd2c275")
       .build()
+    def stage = new Stage(execution, "googleCloudBuild", [account: ACCOUNT, buildDefinition: BUILD])
 
     when:
-    def stage = new Stage(execution, "googleCloudBuild", [account: ACCOUNT, buildDefinition: BUILD])
     TaskResult result = task.execute(stage)
 
     then:
+    1 * igorService.createGoogleCloudBuild(ACCOUNT, BUILD) >> igorResponse
+    result.context.buildInfo == igorResponse
+  }
+
+  def "starts a build defined as an artifact"() {
+    given:
+    def igorResponse = GoogleCloudBuild.builder()
+      .id("810b665a-611d-4ebd-98d1-df7c939843f8")
+      .build()
+    def artifact = Artifact.builder()
+      .uuid("b4541a86-fa45-4a1f-8c55-45cb5cc9d537")
+      .artifactAccount("my-account")
+      .build()
+    def stage = new Stage(execution, "googleCloudBuild", [
+      account: ACCOUNT,
+      buildDefinitionSource: "artifact",
+      buildDefinitionArtifact: [
+        artifact: artifact
+      ]
+    ])
+
+    when:
+    TaskResult result = task.execute(stage)
+
+    then:
+    artifactResolver.getBoundArtifactForStage(stage, null, artifact) >> artifact
+    oortService.fetchArtifact(artifact) >> new Response("", 200, "", Collections.emptyList(), new TypedString(objectMapper.writeValueAsString(BUILD)))
+
     1 * igorService.createGoogleCloudBuild(ACCOUNT, BUILD) >> igorResponse
     result.context.buildInfo == igorResponse
   }
