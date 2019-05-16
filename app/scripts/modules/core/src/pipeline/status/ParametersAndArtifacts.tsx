96ac95a3c0d269c9467397ad7331a01495e2e435
Jon Schneider <jschneider@pivotal.io>
2019-05-16 09:06:48 -0500

    fix(artifacts): Artifacts are shown on pipeline execution when artifactsRewrite is enabled (#6973)

--- app/scripts/modules/core/src/pipeline/status/ParametersAndArtifacts.tsx
+++ app/scripts/modules/core/src/pipeline/status/ParametersAndArtifacts.tsx
@@ -140,7 +140,7 @@ export class ParametersAndArtifacts extends React.Component<
           pinnedDisplayableParameters={pinnedDisplayableParameters}
         />
 
-        {SETTINGS.feature.artifacts && (
+        {(SETTINGS.feature.artifacts || SETTINGS.feature.artifactsRewrite) && (
           <ResolvedArtifactList
             artifacts={artifacts}
             resolvedExpectedArtifacts={resolvedExpectedArtifacts}
