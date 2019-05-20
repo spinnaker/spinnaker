8ab7a882da9bd6b28a23399b0899fc2b03eef0f6
Rob Fletcher <rfletcher@netflix.com>
2019-05-20 10:57:52 -0700

    chore(build): auto-generate bom contents

--- keel-bom/keel-bom.gradle.kts
+++ keel-bom/keel-bom.gradle.kts
@@ -25,25 +25,9 @@ if (property("enablePublishing") == "true") {
 dependencies {
   api(platform("com.netflix.spinnaker.kork:kork-bom:${property("korkVersion")}"))
   constraints {
-    api(project(":keel-api"))
-    api(project(":keel-artifact"))
-    api(project(":keel-bakery-plugin"))
-    api(project(":keel-clouddriver"))
-    api(project(":keel-core"))
-    api(project(":keel-core-test"))
-    api(project(":keel-deliveryconfig-plugin"))
-    api(project(":keel-ec2-plugin"))
-    api(project(":keel-eureka"))
-    api(project(":keel-front50"))
-    api(project(":keel-igor"))
-    api(project(":keel-mahe"))
-    api(project(":keel-orca"))
-    api(project(":keel-plugin"))
-    api(project(":keel-redis"))
-    api(project(":keel-retrofit"))
-    api(project(":keel-retrofit-test-support"))
-    api(project(":keel-spring-test-support"))
-    api(project(":keel-sql"))
-    api(project(":keel-veto"))
+    rootProject
+      .subprojects
+      .filter { it != project }
+      .forEach { api(project(it.path)) }
   }
 }
