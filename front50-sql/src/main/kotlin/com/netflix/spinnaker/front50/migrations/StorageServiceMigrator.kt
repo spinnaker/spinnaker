0b0420a08ee54927bceb0fda626ff9c1edd2d5d0
Adam Jordens <adam@jordens.org>
2019-05-28 21:27:39 -0700

    fix(sql): Avoid migrating objects solely due to s3 precision loss
    S3 returns an RFC 1123 Last-Modified time which lacks seconds and
    in-fact is common rounded up to the next second.
    
    This PR will compare old and new w/ 1s of buffer.
    
    A modification in S3 will only be migrated if it is > 1s newer than
    whatever is in SQL.

--- front50-sql/src/main/kotlin/com/netflix/spinnaker/front50/migrations/StorageServiceMigrator.kt
+++ front50-sql/src/main/kotlin/com/netflix/spinnaker/front50/migrations/StorageServiceMigrator.kt
@@ -81,8 +81,9 @@ class StorageServiceMigrator(
        * - does not exist in 'target'
        * or
        * - has been more recently modified in 'source'
+       *   (with "some" buffer to account for precision loss on s3 due to RFC 1123 being used for last modified values)
        */
-      !targetObjectKeys.containsKey(e.key) || targetObjectKeys[e.key]!! < e.value
+      !targetObjectKeys.containsKey(e.key) || targetObjectKeys[e.key]!! < (e.value - 1000)
     }
 
     if (migratableObjectKeys.isEmpty()) {
