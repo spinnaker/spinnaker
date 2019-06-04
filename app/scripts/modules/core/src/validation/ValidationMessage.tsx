f89d97d06341b0ac09a99fc64b539b10375ee021
Chris Thielen <christopherthielen@users.noreply.github.com>
2019-06-04 11:35:51 -0700

    feat(core/presentation): Add "success" type to ValidationMessage (#7082)

--- app/scripts/modules/core/src/validation/ValidationMessage.tsx
+++ app/scripts/modules/core/src/validation/ValidationMessage.tsx
@@ -2,11 +2,27 @@ import * as React from 'react';
 
 export interface IValidationMessageProps {
   message: React.ReactNode;
-  type: 'error' | 'warning' | 'message';
+  type: 'success' | 'error' | 'warning' | 'message' | 'none';
+  // default: true
+  showIcon?: boolean;
 }
 
-export const ValidationMessage = (props: IValidationMessageProps) => (
-  <div className={`${props.type}-message`}>
-    <span className="fa fa-exclamation-circle" /> {props.message}
-  </div>
-);
+const iconClassName = {
+  success: 'far fa-check-circle',
+  error: 'fa fa-exclamation-circle',
+  warning: 'fa fa-exclamation-circle',
+  message: 'icon-view-1',
+  none: '',
+};
+
+export const ValidationMessage = (props: IValidationMessageProps) => {
+  const divClassName = `${props.type}-message`;
+  const showIcon = props.showIcon === undefined ? true : props.showIcon;
+  const spanClassName = (showIcon && iconClassName[props.type]) || '';
+
+  return (
+    <div className={divClassName}>
+      <span className={spanClassName} /> {props.message}
+    </div>
+  );
+};
