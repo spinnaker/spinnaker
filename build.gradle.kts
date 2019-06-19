c38e71736487c64bbcff847bc54d34e9d31e7f9f
Rob Fletcher <rfletcher@netflix.com>
2019-06-19 16:46:09 -0700

    chore(dependencies): new Kork BOM with new Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ buildscript {
 }
 
 plugins {
-  id("nebula.kotlin") version "1.3.31" apply false
-  id("org.jetbrains.kotlin.plugin.allopen") version "1.3.31" apply false
+  id("nebula.kotlin") version "1.3.40" apply false
+  id("org.jetbrains.kotlin.plugin.allopen") version "1.3.40" apply false
 }
 
 allprojects {
