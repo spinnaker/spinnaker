5217743b9e54d113d5fdc96ca6c5883cb11871f2
Rob Fletcher <rfletcher@netflix.com>
2019-06-26 15:14:05 -0700

    feat(environments): ensure in-memory repositories co-operate

--- keel-core/src/main/kotlin/com/netflix/spinnaker/keel/persistence/memory/InMemoryDeliveryConfigRepository.kt
+++ keel-core/src/main/kotlin/com/netflix/spinnaker/keel/persistence/memory/InMemoryDeliveryConfigRepository.kt
@@ -1,16 +1,30 @@
 package com.netflix.spinnaker.keel.persistence.memory
 
 import com.netflix.spinnaker.keel.api.DeliveryConfig
+import com.netflix.spinnaker.keel.api.Environment
+import com.netflix.spinnaker.keel.persistence.ArtifactRepository
 import com.netflix.spinnaker.keel.persistence.DeliveryConfigRepository
 import com.netflix.spinnaker.keel.persistence.NoSuchDeliveryConfigName
+import com.netflix.spinnaker.keel.persistence.ResourceRepository
 
-class InMemoryDeliveryConfigRepository() : DeliveryConfigRepository {
+class InMemoryDeliveryConfigRepository(
+  private val resourceRepository: ResourceRepository,
+  private val artifactRepository: ArtifactRepository
+) : DeliveryConfigRepository {
   private val configs = mutableMapOf<String, DeliveryConfig>()
 
   override fun get(name: String): DeliveryConfig =
     configs[name] ?: throw NoSuchDeliveryConfigName(name)
 
   override fun store(deliveryConfig: DeliveryConfig) {
-    configs[deliveryConfig.name] = deliveryConfig
+    with(deliveryConfig) {
+      configs[name] = this
+      artifacts.forEach {
+        if (!artifactRepository.isRegistered(it.name, it.type)) {
+          artifactRepository.register(it)
+        }
+      }
+      environments.flatMap(Environment::resources).forEach(resourceRepository::store)
+    }
   }
 }
--- keel-core/src/test/kotlin/com/netflix/spinnaker/keel/persistence/memory/InMemoryDeliveryConfigRepositoryTests.kt
+++ keel-core/src/test/kotlin/com/netflix/spinnaker/keel/persistence/memory/InMemoryDeliveryConfigRepositoryTests.kt
@@ -3,6 +3,10 @@ package com.netflix.spinnaker.keel.persistence.memory
 import com.netflix.spinnaker.keel.persistence.DeliveryConfigRepositoryTests
 
 internal class InMemoryDeliveryConfigRepositoryTests : DeliveryConfigRepositoryTests<InMemoryDeliveryConfigRepository>() {
+
+  val resourceRepository = InMemoryResourceRepository()
+  val artifactRepository = InMemoryArtifactRepository()
+
   override fun factory(resourceTypeIdentifier: (String) -> Class<*>): InMemoryDeliveryConfigRepository =
-    InMemoryDeliveryConfigRepository()
+    InMemoryDeliveryConfigRepository(resourceRepository, artifactRepository)
 }
