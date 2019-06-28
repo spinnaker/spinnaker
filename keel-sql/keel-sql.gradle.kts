f9ca3438f2a730e9d5249f1c3bf9ad63594f1c0e
Rob Fletcher <rfletcher@netflix.com>
2019-06-28 10:24:58 -0700

    refactor(persistence): JOOQ generation

--- keel-sql/keel-sql.gradle.kts
+++ keel-sql/keel-sql.gradle.kts
@@ -1,6 +1,30 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 plugins {
   `java-library`
   id("kotlin-spring")
+  id("ch.ayedo.jooqmodelator") version "3.5.0"
+}
+
+afterEvaluate {
+  tasks.getByName("generateJooqMetamodel") {
+    inputs.files("$projectDir/src/main/resources")
+    outputs.dirs("$projectDir/src/generated/java")
+  }
+
+  tasks.getByName("compileKotlin").dependsOn("generateJooqMetamodel")
+}
+
+sourceSets {
+  main {
+    java {
+      srcDir("$projectDir/src/generated/java")
+    }
+  }
+}
+
+tasks.getByName<Delete>("clean") {
+  delete.add("$projectDir/src/generated/java")
 }
 
 dependencies {
@@ -21,4 +45,29 @@ dependencies {
   testImplementation(project(":keel-api"))
   testImplementation("org.testcontainers:mysql")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
+  jooqModelatorRuntime(platform("com.netflix.spinnaker.kork:kork-bom:${property("korkVersion")}"))
+  jooqModelatorRuntime("mysql:mysql-connector-java")
+}
+
+jooqModelator {
+  jooqVersion = "3.11.11"
+  jooqEdition = "OSS"
+  jooqConfigPath = "$projectDir/src/main/resources/jooqConfig.xml"
+  jooqOutputPath = "$projectDir/src/generated/java"
+  migrationEngine = "LIQUIBASE"
+  migrationsPaths = listOf("$projectDir/src/main/resources/db")
+  dockerTag = "mysql/mysql-server:5.7"
+  dockerEnv = listOf("MYSQL_ROOT_PASSWORD=sa", "MYSQL_ROOT_HOST=%", "MYSQL_DATABASE=keel")
+  dockerHostPort = 3306
+  dockerContainerPort = 3306
+}
+
+// Don't enforce spotless for generated code
+afterEvaluate {
+  configure<SpotlessExtension> {
+    java {
+      targetExclude(fileTree("$projectDir/src/generated/java"))
+    }
+  }
 }
--- keel-sql/src/main/kotlin/com/netflix/spinnaker/keel/sql/SqlArtifactRepository.kt
+++ keel-sql/src/main/kotlin/com/netflix/spinnaker/keel/sql/SqlArtifactRepository.kt
@@ -6,8 +6,9 @@ import com.netflix.spinnaker.keel.api.randomUID
 import com.netflix.spinnaker.keel.persistence.ArtifactAlreadyRegistered
 import com.netflix.spinnaker.keel.persistence.ArtifactRepository
 import com.netflix.spinnaker.keel.persistence.NoSuchArtifactException
+import com.netflix.spinnaker.keel.persistence.metamodel.Tables.DELIVERY_ARTIFACT
+import com.netflix.spinnaker.keel.persistence.metamodel.Tables.DELIVERY_ARTIFACT_VERSION
 import org.jooq.DSLContext
-import org.jooq.impl.DSL.table
 import org.slf4j.LoggerFactory
 
 class SqlArtifactRepository(
@@ -16,8 +17,9 @@ class SqlArtifactRepository(
   override fun register(artifact: DeliveryArtifact) {
     jooq.inTransaction {
       insertInto(DELIVERY_ARTIFACT)
-        .columns(UID, NAME, TYPE)
-        .values(randomUID().toString(), artifact.name, artifact.type.name)
+        .set(DELIVERY_ARTIFACT.UID, randomUID().toString())
+        .set(DELIVERY_ARTIFACT.NAME, artifact.name)
+        .set(DELIVERY_ARTIFACT.TYPE, artifact.type.name)
         .onDuplicateKeyIgnore()
         .execute()
         .also { count ->
@@ -28,15 +30,16 @@ class SqlArtifactRepository(
 
   override fun store(artifact: DeliveryArtifact, version: String): Boolean =
     jooq.inTransaction {
-      val uid = select(UID)
+      val uid = select(DELIVERY_ARTIFACT.UID)
         .from(DELIVERY_ARTIFACT)
-        .where(NAME.eq(artifact.name))
-        .and(TYPE.eq(artifact.type.name))
+        .where(DELIVERY_ARTIFACT.NAME.eq(artifact.name))
+        .and(DELIVERY_ARTIFACT.TYPE.eq(artifact.type.name))
         .fetchOne()
         ?: throw NoSuchArtifactException(artifact)
 
-      insertInto(DELIVERY_ARTIFACT_VERSION, DELIVERY_ARTIFACT_UID, VERSION)
-        .values(uid.value1(), version)
+      insertInto(DELIVERY_ARTIFACT_VERSION)
+        .set(DELIVERY_ARTIFACT_VERSION.DELIVERY_ARTIFACT_UID, uid.value1())
+        .set(DELIVERY_ARTIFACT_VERSION.VERSION, version)
         .onDuplicateKeyIgnore()
         .execute() == 1
     }
@@ -45,34 +48,24 @@ class SqlArtifactRepository(
     jooq
       .selectOne()
       .from(DELIVERY_ARTIFACT)
-      .where(NAME.eq(name))
-      .and(TYPE.eq(type.name))
+      .where(DELIVERY_ARTIFACT.NAME.eq(name))
+      .and(DELIVERY_ARTIFACT.TYPE.eq(type.name))
       .fetchOne() != null
 
   override fun versions(artifact: DeliveryArtifact): List<String> =
     if (isRegistered(artifact.name, artifact.type)) {
       jooq
-        .select(VERSION)
+        .select(DELIVERY_ARTIFACT_VERSION.VERSION)
         .from(DELIVERY_ARTIFACT, DELIVERY_ARTIFACT_VERSION)
-        .where(UID.eq(DELIVERY_ARTIFACT_UID))
-        .and(NAME.eq(artifact.name))
-        .and(TYPE.eq(artifact.type.name))
-        .orderBy(VERSION.desc())
+        .where(DELIVERY_ARTIFACT.UID.eq(DELIVERY_ARTIFACT_VERSION.DELIVERY_ARTIFACT_UID))
+        .and(DELIVERY_ARTIFACT.NAME.eq(artifact.name))
+        .and(DELIVERY_ARTIFACT.TYPE.eq(artifact.type.name))
+        .orderBy(DELIVERY_ARTIFACT_VERSION.VERSION.desc())
         .fetch()
-        .getValues(VERSION)
+        .getValues(DELIVERY_ARTIFACT_VERSION.VERSION)
     } else {
       throw NoSuchArtifactException(artifact)
     }
-
-  companion object {
-    private val DELIVERY_ARTIFACT = table("delivery_artifact")
-    private val DELIVERY_ARTIFACT_VERSION = table("delivery_artifact_version")
-    private val UID = field<String>("uid")
-    private val DELIVERY_ARTIFACT_UID = field<String>("delivery_artifact_uid")
-    private val NAME = field<String>("name")
-    private val TYPE = field<String>("type")
-    private val VERSION = field<String>("version")
-  }
 
   private val log by lazy { LoggerFactory.getLogger(javaClass) }
 }
