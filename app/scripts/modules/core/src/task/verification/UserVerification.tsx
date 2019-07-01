40eb9394247966f51b1975bbbb069ebda8593a82
Chris Thielen <christopherthielen@users.noreply.github.com>
2019-07-01 16:52:48 -0700

    feat(core/task): UserVerification: Accept an 'account' prop. (#7173)
    - auto-hide when the account doesn't "challengeDestructiveActions"
    - use hooks

--- app/scripts/modules/core/src/task/verification/UserVerification.tsx
+++ app/scripts/modules/core/src/task/verification/UserVerification.tsx
@@ -1,55 +1,55 @@
 import * as React from 'react';
-
 import { HelpField } from 'core/help/HelpField';
+import { AccountService } from 'core/account';
+
+const { useEffect, useState } = React;
 
 export interface IUserVerificationProps {
   label?: string;
-  expectedValue: string;
   onValidChange: (isValid: boolean) => void;
+
+  // Provide one of:
+  expectedValue?: string;
+  account?: string;
 }
 
-export interface IUserVerificationState {
-  value: string;
-  matches: boolean;
-}
+export function UserVerification(props: IUserVerificationProps) {
+  const { label, account } = props;
+  const [value, setValue] = useState('');
 
-export class UserVerification extends React.Component<IUserVerificationProps, IUserVerificationState> {
-  public state = { value: '', matches: false };
+  if (props.account && props.expectedValue) {
+    throw new Error("Supply either the 'account' or the 'expectedValue' prop, but not both");
+  }
 
-  private handleChange = (evt: React.ChangeEvent<any>) => {
-    const { value } = evt.target;
-    const { expectedValue } = this.props;
+  const expectedValue = props.expectedValue || account;
+  const matches = value === expectedValue;
 
-    const previousMatches = this.state.value === expectedValue;
-    const matches = value === expectedValue;
-    this.setState({ value, matches });
+  const [challengeDestructiveActions, setChallengeDestructiveActions] = useState(true);
+  useEffect(() => {
+    account && AccountService.challengeDestructiveActions(account).then(setChallengeDestructiveActions);
+  }, [account]);
 
-    if (matches !== previousMatches) {
-      this.props.onValidChange(matches);
-    }
-  };
+  const hideComponent = account && !challengeDestructiveActions;
 
-  public render() {
-    const { label, expectedValue } = this.props;
-    const { value, matches } = this.state;
-    const className = `form-control input-sm highlight-pristine ${matches ? '' : 'ng-invalid'}`;
-    const defaultLabel = (
-      <>
-        Type the name of the account (<span className="verification-text">{expectedValue}</span>) to continue
-      </>
-    );
+  useEffect(() => props.onValidChange(hideComponent || matches), [matches, hideComponent]);
 
-    return (
-      <div className="row verification user-verification">
-        <div className="col-sm-12">
-          <div className="form-inline">
-            <div className="form-group">
-              <div className="form-control-static">{label || defaultLabel}</div> <HelpField id="user.verification" />
-              <input type="text" className={className} value={value} onChange={this.handleChange} />
-            </div>
+  const defaultLabel = (
+    <>
+      Type the name of the account (<span className="verification-text">{expectedValue}</span>) to continue
+    </>
+  );
+
+  const className = `form-control input-sm highlight-pristine ${matches ? '' : 'ng-invalid'}`;
+  return hideComponent ? null : (
+    <div className="row verification user-verification">
+      <div className="col-sm-12">
+        <div className="form-inline">
+          <div className="form-group">
+            <div className="form-control-static">{label || defaultLabel}</div> <HelpField id="user.verification" />
+            <input type="text" className={className} value={value} onChange={evt => setValue(evt.target.value)} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
