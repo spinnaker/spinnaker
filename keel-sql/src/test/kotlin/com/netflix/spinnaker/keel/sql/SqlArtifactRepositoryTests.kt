1327c2d0bf8c6ae5e778854146910937097f1c64
Rob Fletcher <rfletcher@netflix.com>
2019-07-15 16:24:22 -0700

    fix(tests): Configure MySQL container to use correct database name
    JOOQ's generated table models encode the schema so tests need to run using a consistent schema name.

--- keel-sql/src/test/kotlin/com/netflix/spinnaker/keel/sql/SqlArtifactRepositoryTests.kt
+++ keel-sql/src/test/kotlin/com/netflix/spinnaker/keel/sql/SqlArtifactRepositoryTests.kt
@@ -2,11 +2,25 @@ package com.netflix.spinnaker.keel.sql
 
 import com.netflix.spinnaker.keel.persistence.ArtifactRepositoryTests
 import com.netflix.spinnaker.kork.sql.test.SqlTestUtil.cleanupDb
-import com.netflix.spinnaker.kork.sql.test.SqlTestUtil.initTcMysqlDatabase
+import com.netflix.spinnaker.kork.sql.test.SqlTestUtil.initDatabase
+import org.jooq.SQLDialect.MYSQL_5_7
 import org.junit.jupiter.api.AfterAll
+import org.testcontainers.containers.JdbcDatabaseContainer
+import org.testcontainers.containers.MySQLContainerProvider
 
 internal object SqlArtifactRepositoryTests : ArtifactRepositoryTests<SqlArtifactRepository>() {
-  private val testDatabase = initTcMysqlDatabase()
+  private val testContainer = MySQLContainerProvider()
+    .newInstance("5.7.22")
+    .withDatabaseName("keel")
+    .withUsername("keel_service")
+    .withUsername("whatever")
+    .also { it.start() }
+
+  private val testDatabase = initDatabase(
+    testContainer.authenticatedJdbcUrl,
+    MYSQL_5_7
+  )
+
   private val jooq = testDatabase.context
 
   override fun factory() = SqlArtifactRepository(jooq)
@@ -19,5 +33,10 @@ internal object SqlArtifactRepositoryTests : ArtifactRepositoryTests<SqlArtifact
   @AfterAll
   fun shutdown() {
     testDatabase.dataSource.close()
+    testContainer.stop()
   }
+
+  @Suppress("UsePropertyAccessSyntax")
+  private val JdbcDatabaseContainer<*>.authenticatedJdbcUrl: String
+    get() = "${getJdbcUrl()}?user=${getUsername()}&password=${getPassword()}"
 }
