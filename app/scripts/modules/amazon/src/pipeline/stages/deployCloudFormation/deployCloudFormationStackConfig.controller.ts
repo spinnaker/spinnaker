d3249c9e2173cfd3652f6fb8a95c2fb5f2367984
Jammy Louie <jlouie@pivotal.io>
2019-07-17 11:12:20 -0400

    feat(aws): Support new artifact model for deploy cloudformation (#7180)

--- app/scripts/modules/amazon/src/pipeline/stages/deployCloudFormation/deployCloudFormationStackConfig.controller.ts
+++ app/scripts/modules/amazon/src/pipeline/stages/deployCloudFormation/deployCloudFormationStackConfig.controller.ts
@@ -1,10 +1,13 @@
 import { IController, IScope } from 'angular';
+import { extend } from 'lodash';
 import { $q } from 'ngimport';
 import {
   ExpectedArtifactSelectorViewController,
   NgGenericArtifactDelegate,
   AccountService,
   IAccountDetails,
+  IArtifact,
+  IExpectedArtifact,
 } from '@spinnaker/core';
 
 export class DeployCloudFormationStackConfigController implements IController {
@@ -56,4 +59,34 @@ export class DeployCloudFormationStackConfigController implements IController {
   public toggleChangeSet() {
     this.$scope.stage.isChangeSet = !this.$scope.stage.isChangeSet;
   }
+
+  public onStackArtifactEdited = (artifact: IArtifact) => {
+    this.$scope.$applyAsync(() => {
+      this.$scope.stage.stackArtifactId = null;
+      this.$scope.stage.stackArtifact = artifact;
+    });
+  };
+
+  public onStackArtifactSelected = (expectedArtifact: IExpectedArtifact) => {
+    this.onChangeStackArtifactId(expectedArtifact.id);
+  };
+
+  public onChangeStackArtifactId = (artifactId: string) => {
+    this.$scope.$applyAsync(() => {
+      this.$scope.stage.stackArtifactId = artifactId;
+      this.$scope.stage.stackArtifact = null;
+    });
+  };
+
+  public onStackArtifactAccountSelected = (stackArtifactAccount: string) => {
+    this.$scope.$applyAsync(() => {
+      this.$scope.stage.stackArtifactAccount = stackArtifactAccount;
+    });
+  };
+
+  public updatePipeline = (changes: any) => {
+    this.$scope.$applyAsync(() => {
+      extend(this.$scope.pipeline, changes);
+    });
+  };
 }
