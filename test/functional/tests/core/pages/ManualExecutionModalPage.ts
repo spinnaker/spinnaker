af9b5c299b287e17b2409cf7a6ac7387e74b02f8
Jammy Louie <jlouie@pivotal.io>
2019-07-30 16:57:49 -0400

    fix(tests): fix tests for the refactored manual execution (#7285)

--- test/functional/tests/core/pages/ManualExecutionModalPage.ts
+++ test/functional/tests/core/pages/ManualExecutionModalPage.ts
@@ -2,12 +2,12 @@ import { Page } from './Page';
 
 export class ManualExecutionModalPage extends Page {
   public static locators = {
-    pipelineSelectArrow: '//div[contains(@class, "modal-content")]//span[contains(@class, "ui-select-toggle")]',
+    pipelineSelectArrow: '//div[contains(@class, "modal-content")]//span[contains(@class, "Select-arrow-zone")]',
     runButton:
       '//div[contains(@class, "modal-content")]//button[contains(@class, "btn-primary") and contains(., "Run")]',
 
     factories: {
-      pipelineWithName: (name: string) => `//*[contains(@class, 'select2-result-label')]//div[contains(., '${name}')]`,
+      pipelineWithName: (name: string) => `//*[contains(@class, 'Select-menu')]//div[contains(text(), '${name}')]`,
     },
   };
 
