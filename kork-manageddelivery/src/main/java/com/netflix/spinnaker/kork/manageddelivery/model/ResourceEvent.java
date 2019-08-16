972ac56db7a92c094bfa1799a58a650c7d406101
Emily Burns <emjeburns@gmail.com>
2019-08-16 13:49:02 -0700

    chore(keel): update resource history events (#361)

--- kork-manageddelivery/src/main/java/com/netflix/spinnaker/kork/manageddelivery/model/ResourceEvent.java
+++ kork-manageddelivery/src/main/java/com/netflix/spinnaker/kork/manageddelivery/model/ResourceEvent.java
@@ -29,6 +29,7 @@ import lombok.Data;
   @JsonSubTypes.Type(value = ResourceEvent.ResourceCreated.class, name = "ResourceCreated"),
   @JsonSubTypes.Type(value = ResourceEvent.ResourceUpdated.class, name = "ResourceUpdated"),
   @JsonSubTypes.Type(value = ResourceEvent.ResourceMissing.class, name = "ResourceMissing"),
+  @JsonSubTypes.Type(value = ResourceEvent.ResourceDeleted.class, name = "ResourceDeleted"),
   @JsonSubTypes.Type(
       value = ResourceEvent.ResourceActuationLaunched.class,
       name = "ResourceActuationLaunched"),
@@ -55,37 +56,14 @@ public class ResourceEvent {
   /** A managed resource does not currently exist in the cloud. */
   @Data
   @JsonTypeName("ResourceMissing")
-  public static class ResourceMissing extends ResourceEvent {}
-
-  /**
-   * The desired and actual states of a managed resource now match where previously there was a
-   * delta (or the resource did not exist).
-   */
-  @Data
-  @JsonTypeName("ResourceDeltaResolved")
-  public static class ResourceDeltaResolved extends ResourceEvent {}
-
-  /**
-   * A difference between the desired and actual state of a managed resource was detected.
-   *
-   * <p>[delta] The difference between the "base" spec (desired) and "working" spec (actual).
-   */
-  @Data
-  @JsonTypeName("ResourceDeltaDetected")
-  public static class ResourceDeltaDetected extends ResourceEvent {
-    Map<String, Object> delta;
+  public static class ResourceMissing extends ResourceEvent {
+    ResourceState state;
   }
 
-  /**
-   * A task or tasks were launched to resolve a mismatch between desired and actual state of a
-   * managed resource.
-   */
+  /** A managed resource was deleted. */
   @Data
-  @JsonTypeName("ResourceActuationLaunched")
-  public static class ResourceActuationLaunched extends ResourceEvent {
-    String plugin;
-    List<String> tasks;
-  }
+  @JsonTypeName("ResourceDeleted")
+  public static class ResourceDeleted extends ResourceEvent {}
 
   /**
    * The desired state of a resource was updated.
@@ -98,4 +76,49 @@ public class ResourceEvent {
   public static class ResourceUpdated extends ResourceEvent {
     Map<String, Object> delta;
   }
+
+  /**
+   * The desired and actual states of a managed resource now match where previously there was a
+   * delta (or the resource did not exist).
+   */
+  @Data
+  @JsonTypeName("ResourceDeltaResolved")
+  public static class ResourceDeltaResolved extends ResourceEvent {
+    ResourceState state;
+  }
+
+  /**
+   * A difference between the desired and actual state of a managed resource was detected.
+   *
+   * <p>[delta] The difference between the "base" spec (desired) and "working" spec (actual).
+   */
+  @Data
+  @JsonTypeName("ResourceDeltaDetected")
+  public static class ResourceDeltaDetected extends ResourceEvent {
+    Map<String, Object> delta;
+    ResourceState state;
+  }
+
+  /**
+   * A task or tasks were launched to resolve a mismatch between desired and actual state of a
+   * managed resource.
+   */
+  @Data
+  @JsonTypeName("ResourceActuationLaunched")
+  public static class ResourceActuationLaunched extends ResourceEvent {
+    String plugin;
+    List<Task> tasks;
+  }
+
+  public static class Task {
+    String id;
+    String name;
+  }
+}
+
+enum ResourceState {
+  Ok,
+  Diff,
+  Missing,
+  Error
 }
