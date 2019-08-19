b6ea1de57645430b9e739c42470a568959e40d74
Maggie Neterval <mneterval@google.com>
2019-08-19 15:16:58 -0400

    fix(core): fix produces artifacts UI (#7343)

--- app/scripts/modules/core/src/pipeline/config/triggers/triggers.module.ts
+++ app/scripts/modules/core/src/pipeline/config/triggers/triggers.module.ts
@@ -2,9 +2,10 @@ import { module } from 'angular';
 import { react2angular } from 'react2angular';
 
 import { Triggers } from './Triggers';
+import { ARTIFACT_MODULE } from './artifacts/artifact.module';
 
 export const TRIGGERS = 'spinnaker.core.pipeline.config.trigger.triggersDirective';
-module(TRIGGERS, []).component(
+module(TRIGGERS, [ARTIFACT_MODULE]).component(
   'triggers',
   react2angular(Triggers, ['application', 'pipeline', 'fieldUpdated', 'updatePipelineConfig']),
 );
