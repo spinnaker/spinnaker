ad4c1447c6134b8c823d9b92f12364754a11168a
Erik Munson <emunson@netflix.com>
2019-09-04 13:20:56 -0700

    feat(core/managed): Add resource dropdown with links to history + source JSON (#7376)

--- app/scripts/modules/core/src/managed/ManagedResourceDetailsIndicator.tsx
+++ app/scripts/modules/core/src/managed/ManagedResourceDetailsIndicator.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { get } from 'lodash';
+import * as ReactGA from 'react-ga';
+import { get, flatMap } from 'lodash';
+import { Dropdown } from 'react-bootstrap';
 
+import { SETTINGS } from 'core/config/settings';
 import { IEntityTags } from 'core/domain';
 import { HoverablePopover } from 'core/presentation';
 
@@ -12,14 +15,25 @@ export interface IManagedResourceDetailsIndicatorProps {
   entityTags: IEntityTags[];
 }
 
-export const ManagedResourceDetailsIndicator = ({ entityTags }: IManagedResourceDetailsIndicatorProps) => {
-  const isManaged =
-    get(entityTags, 'length') &&
-    entityTags.some(({ tags }) => tags.some(({ name }) => name === MANAGED_BY_SPINNAKER_TAG_NAME));
+const logClick = (label: string, resourceId: string) =>
+  ReactGA.event({
+    category: 'Managed Resource Menu',
+    action: `${label} clicked`,
+    label: resourceId,
+  });
 
-  if (!isManaged) {
+export const ManagedResourceDetailsIndicator = ({ entityTags }: IManagedResourceDetailsIndicatorProps) => {
+  const managedTag =
+    get(entityTags, 'length') &&
+    flatMap(entityTags, ({ tags }) => tags).find(({ name }) => name === MANAGED_BY_SPINNAKER_TAG_NAME);
+
+  if (!managedTag) {
     return null;
   }
+
+  const {
+    value: { keelResourceId },
+  } = managedTag;
 
   const helpText = (
     <>
@@ -28,7 +42,11 @@ export const ManagedResourceDetailsIndicator = ({ entityTags }: IManagedResource
       </p>
       <p>
         Changes made in the UI will be stomped in favor of the existing declarative configuration.{' '}
-        <a href="https://www.spinnaker.io/reference/managed-delivery" target="_blank">
+        <a
+          target="_blank"
+          onClick={() => logClick('Learn More', keelResourceId)}
+          href="https://www.spinnaker.io/reference/managed-delivery"
+        >
           Learn More
         </a>
       </p>
@@ -36,11 +54,36 @@ export const ManagedResourceDetailsIndicator = ({ entityTags }: IManagedResource
   );
 
   return (
-    <HoverablePopover template={helpText} placement="left">
-      <div className="band band-info ManagedResourceDetailsIndicator">
-        <span className="icon">🌈</span>
-        Managed by Spinnaker
-      </div>
-    </HoverablePopover>
+    <div className="vertical middle center band band-info ManagedResourceDetailsIndicator">
+      <HoverablePopover template={helpText} placement="left">
+        <span className="summary-message horizontal sp-margin-s-bottom">
+          <span className="rainbow-icon">🌈</span>
+          Managed by Spinnaker
+        </span>
+      </HoverablePopover>
+      <Dropdown className="dropdown" id="server-group-managed-resource-dropdown" pullRight={true}>
+        <Dropdown.Toggle className="btn btn-sm btn-default dropdown-toggle">Resource Actions</Dropdown.Toggle>
+        <Dropdown.Menu className="dropdown-menu">
+          <li>
+            <a
+              target="_blank"
+              onClick={() => logClick('History', keelResourceId)}
+              href={`${SETTINGS.gateUrl}/history/${keelResourceId}`}
+            >
+              History
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              onClick={() => logClick('Raw Source', keelResourceId)}
+              href={`${SETTINGS.gateUrl}/managed/resources/${keelResourceId}`}
+            >
+              Raw Source
+            </a>
+          </li>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
   );
 };
