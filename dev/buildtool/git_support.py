b154bd52502ffc3451d0a0c9881e9b076cea1f57
Louis Jimenez <louisjimenez@google.com>
2019-09-17 10:06:34 -0400

    fix(build): make feat into patch bump to fix collision on clouddriver 6.3

--- dev/buildtool/git_support.py
+++ dev/buildtool/git_support.py
@@ -322,7 +322,7 @@ class CommitMessage(
       # Some tags indicate only a patch release.
       re.compile(r'^\s*'
                  r'(?:\*\s+)?'
-                 r'((?:fix|bug|chore|docs?|perf|test)[\(:].*)',
+                 r'((?:feat|fix|bug|chore|docs?|perf|test)[\(:].*)',
                  re.MULTILINE)
   ]
   DEFAULT_MINOR_REGEXS = [
@@ -331,7 +331,7 @@ class CommitMessage(
       # implementation changes that suggest a higher level of risk.
       re.compile(r'^\s*'
                  r'(?:\*\s+)?'
-                 r'((?:feat|feature|refactor|config)[\(:].*)',
+                 r'((?:feature|refactor|config)[\(:].*)',
                  re.MULTILINE)
   ]
   DEFAULT_MAJOR_REGEXS = [
