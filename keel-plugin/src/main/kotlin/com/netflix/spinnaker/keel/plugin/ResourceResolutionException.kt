cdd075a153a6b9c2d3c91db9bc79f2fcf414b053
Rob Fletcher <rfletcher@netflix.com>
2019-10-08 12:26:32 -0700

    feat(errors): try to capture some notion of cause in ResourceCheckError

--- keel-plugin/src/main/kotlin/com/netflix/spinnaker/keel/plugin/ResourceResolutionException.kt
+++ keel-plugin/src/main/kotlin/com/netflix/spinnaker/keel/plugin/ResourceResolutionException.kt
@@ -6,7 +6,7 @@ abstract class ResourceResolutionException(
   type: String,
   resourceId: ResourceId,
   cause: Throwable
-) : RuntimeException("Unable to resolve $type state of $resourceId", cause)
+) : RuntimeException("Unable to resolve $type state of $resourceId due to: ${cause.message}", cause)
 
 class CannotResolveCurrentState(
   resourceId: ResourceId,
