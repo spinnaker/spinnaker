2d6c9e783734f072f314f543db8c5e2e2ad9d858
Chris Thielen <github@sandgnat.com>
2019-10-09 15:55:16 -0700

    feat(core/utils): extract firstDefined utility function

---
+++ app/scripts/modules/core/src/utils/firstDefined.ts
@@ -0,0 +1,6 @@
+import { isNil } from 'lodash';
+
+/** returns the first value that is neither null nor undefined */
+export function firstDefined<T>(...values: T[]): T {
+  return values.find(val => !isNil(val));
+}
--- app/scripts/modules/core/src/utils/index.ts
+++ app/scripts/modules/core/src/utils/index.ts
@@ -1,13 +1,14 @@
 ///<reference path="./classnames.d.ts" />
 
+export * from './RenderWhenVisible';
 export * from './clipboard/CopyToClipboard';
 export * from './debug';
+export * from './firstDefined';
 export * from './json/JsonUtils';
 export * from './json/traverseObject';
 export * from './noop';
 export * from './q';
 export * from './renderIfFeature.component';
-export * from './RenderWhenVisible';
 export * from './scrollTo/scrollTo.service';
 export * from './timeFormatters';
 export * from './unicodeBase64';
