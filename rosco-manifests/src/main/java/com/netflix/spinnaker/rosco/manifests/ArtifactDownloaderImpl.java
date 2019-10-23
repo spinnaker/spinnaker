00dbfdedc47d68ac774af7acd84cff1762ae1728
Maggie Neterval <mneterval@google.com>
2019-10-23 11:41:52 -0400

    fix(artifact): improve error messaging for failed artifact downl… (#453)

--- rosco-manifests/src/main/java/com/netflix/spinnaker/rosco/manifests/ArtifactDownloaderImpl.java
+++ rosco-manifests/src/main/java/com/netflix/spinnaker/rosco/manifests/ArtifactDownloaderImpl.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
+import retrofit.RetrofitError;
 import retrofit.client.Response;
 
 @Component
@@ -30,8 +31,16 @@ public final class ArtifactDownloaderImpl implements ArtifactDownloader {
       if (response.getBody() != null) {
         try (InputStream inputStream = response.getBody().in()) {
           IOUtils.copy(inputStream, outputStream);
+        } catch (IOException e) {
+          throw new IOException(
+              String.format(
+                  "Failed to read input stream of downloaded artifact: %s. Error: %s",
+                  artifact, e.getMessage()));
         }
       }
+    } catch (RetrofitError e) {
+      throw new IOException(
+          String.format("Failed to download artifact: %s. Error: %s", artifact, e.getMessage()));
     }
   }
 }
