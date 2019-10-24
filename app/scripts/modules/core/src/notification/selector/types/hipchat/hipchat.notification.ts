81eaac6c753e281c9d101a328492d2d695a783a1
Chris Thielen <christopherthielen@users.noreply.github.com>
2019-10-23 23:50:07 -0700

    chore(core/notification): remove remainder of hipchat notifications (#7557)
    hipchat was removed in https://github.com/spinnaker/deck/pull/6884 but this code remained

--- app/scripts/modules/core/src/notification/selector/types/hipchat/hipchat.notification.ts
+++
@@ -1,9 +0,0 @@
-import { Registry } from 'core/registry';
-
-import { HipchatNotificationType } from './HipchatNotificationType';
-
-Registry.pipeline.registerNotification({
-  component: HipchatNotificationType,
-  key: 'hipchat',
-  label: 'HipChat',
-});
--- app/scripts/modules/core/src/notification/selector/types/index.ts
+++ app/scripts/modules/core/src/notification/selector/types/index.ts
@@ -1,7 +1,6 @@
 export * from './bearychat/BearychatNotificationType';
 export * from './email/EmailNotificationType';
 export * from './googlechat/GooglechatNotificationType';
-export * from './hipchat/HipchatNotificationType';
 export * from './pubsub/PubsubNotificationType';
 export * from './slack/SlackNotificationType';
 export * from './sms/SmsNotificationType';
