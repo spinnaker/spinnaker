629a98f62a9c8b12dad4337e34ef60b46ecfef4d
Erik Munson <emunson@netflix.com>
2019-10-25 20:09:20 -0700

    fix(core/pipeline): make revision dropdown usable, layout tweaks (#7569)

--- app/scripts/modules/core/src/pipeline/config/actions/history/ShowPipelineHistoryModal.tsx
+++ app/scripts/modules/core/src/pipeline/config/actions/history/ShowPipelineHistoryModal.tsx
@@ -118,10 +118,16 @@ export function ShowPipelineHistoryModal(props: IShowHistoryModalProps) {
           )}
           {!loading && history.length > 1 && (
             <>
-              <div className="history-header row">
+              <div className="history-header row horizontal">
                 <div className="col-md-4">
                   <FormField
                     label="Revision"
+                    layout={({ label, input }) => (
+                      <div className="flex-container-h baseline margin-between-lg">
+                        <div className="bold">{label}</div>
+                        <div className="flex-grow">{input}</div>
+                      </div>
+                    )}
                     input={inputProps => (
                       <ReactSelectInput
                         clearable={false}
@@ -133,7 +139,7 @@ export function ShowPipelineHistoryModal(props: IShowHistoryModalProps) {
                     value={version}
                   />
                 </div>
-                <div className="col-md-4">
+                <div className="diff-section col-md-4 horizontal middle">
                   <DiffSummary summary={diff.summary} />
                   {version > 0 && (
                     <button className="btn btn-sm btn-primary" onClick={restoreVersion}>
