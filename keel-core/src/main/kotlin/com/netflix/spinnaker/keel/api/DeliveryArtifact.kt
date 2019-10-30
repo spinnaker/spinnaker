076e1bc56da49f49a6784e575ad60941edfe1726
Luis Fernando Pollo <1323478+luispollo@users.noreply.github.com>
2019-10-30 16:55:46 -0700

    feat(serialization): Use friendly names for artifact types (#560)
    * feat(serialization): Use friendly names for artifact types
    
    * test(serialization): Add test for backwards compatibility with enum names

--- keel-core/src/main/kotlin/com/netflix/spinnaker/keel/api/DeliveryArtifact.kt
+++ keel-core/src/main/kotlin/com/netflix/spinnaker/keel/api/DeliveryArtifact.kt
@@ -1,12 +1,23 @@
 package com.netflix.spinnaker.keel.api
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
 data class DeliveryArtifact(
   val name: String,
   val type: ArtifactType = ArtifactType.DEB
 )
 
-enum class ArtifactType {
-  DEB, DOCKER
+enum class ArtifactType(@JsonValue val friendlyName: String) {
+  DEB("deb"),
+  DOCKER("docker");
+
+  companion object {
+    @JsonCreator @JvmStatic
+    fun fromFriendlyName(friendlyName: String): ArtifactType? {
+      return valueOf(friendlyName.toUpperCase())
+    }
+  }
 }
 
 // todo eb: add RELEASE
