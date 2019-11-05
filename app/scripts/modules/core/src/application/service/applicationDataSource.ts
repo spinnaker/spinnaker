167a1512fdc6bb2fefa0f47a1ce309e56392011e
Erik Munson <emunson@netflix.com>
2019-11-04 16:48:24 -0800

    feat(dataSources): add runtime error when defaultData isn't provided (#7591)

--- app/scripts/modules/core/src/application/service/applicationDataSource.ts
+++ app/scripts/modules/core/src/application/service/applicationDataSource.ts
@@ -343,6 +343,14 @@ export class ApplicationDataSource<T = any> implements IDataSourceConfig<T> {
   constructor(config: IDataSourceConfig<T>, private application: Application) {
     Object.assign(this, config);
 
+    if (!config.hasOwnProperty('defaultData')) {
+      console.error(
+        'The defaultData field is required when registering a data source.\n\n' +
+          'defaultData accepts the initial default value that will be used before data has been fetched.\n' +
+          'For example, if your data source holds an array of objects, you should set defaultData to an empty array.',
+      );
+    }
+
     this.data = this.data || this.defaultData;
     this.label = FirewallLabels.get(config.label || robotToHuman(config.key));
 
