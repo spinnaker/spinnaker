e0cc0d62d86ef620f95b60cea310107ae95239b5
Chris Thielen <christopherthielen@users.noreply.github.com>
2019-11-07 12:40:42 -0800

    feat(core/presentation): Mark all SpinFormik initialValues fields as 'touched' (#7604)
    Normally, Formik fields do not show validation errors until the user has tabbed away (blur event).
    This is to improve UX when a user first loads a blank form.
    It would be horrible to see the entire form rendered in red because all the fields are required.
    
    However, when a form is reloaded *with existing data*, we usually want to show validation errors immediately.
    
    When the form is first rendered, mark all fields in initialValues as "touched".

--- app/scripts/modules/core/src/presentation/forms/SpinFormik.tsx
+++ app/scripts/modules/core/src/presentation/forms/SpinFormik.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
+
+import { set } from 'lodash';
 import { Formik, FormikConfig } from 'formik';
+
+import { traverseObject } from 'core/utils';
 
 /**
  * This component wraps the <Formik/> component, applying fixes and spinnaker opinions
@@ -7,12 +11,20 @@ import { Formik, FormikConfig } from 'formik';
  */
 function SpinFormikImpl<Values extends {}>(props: FormikConfig<Values>, ref?: React.MutableRefObject<Formik<Values>>) {
   const formikRef = ref || React.useRef<Formik<Values>>();
+  const formik = formikRef.current;
   const defaultIsInitialValid = () => formikRef.current && Object.keys(formikRef.current.state.errors).length === 0;
 
-  // Run initial validation when the form is first rendered
+  // When a form is reloaded with existing data, we usually want to show validation errors immediately.
+  // When the form is first rendered, mark all fields in initialValues as "touched".
+  // Then run initial validation.
   React.useEffect(() => {
-    formikRef.current && formikRef.current.getFormikActions().validateForm();
-  }, [formikRef.current]);
+    if (formik) {
+      const initialTouched = {};
+      traverseObject(props.initialValues, (path: string) => set(initialTouched, path, true));
+      formik.setTouched(initialTouched);
+      formik.getFormikActions().validateForm();
+    }
+  }, [!!formik]);
 
   return <Formik<Values> ref={formikRef} isInitialValid={props.isInitialValid || defaultIsInitialValid} {...props} />;
 }
