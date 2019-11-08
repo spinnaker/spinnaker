e370d47c9577c605ad6415a316d69e450ac9a5ae
Chris Thielen <christopherthielen@users.noreply.github.com>
2019-11-07 18:06:05 -0800

    feat(core/presentation): Made useData hook default result behavior more reasonable (#7602)
    * feat(core/presentation): Made useData hook default value behavior more reasonable
    previously, the default value was only used if some of the deps were null or undefined.
    Now, the default value is used immediately (even while the first promise is PENDING).
    It is used until the first promise turns to RESOLVED.
    
    * Update app/scripts/modules/core/src/presentation/hooks/useData.hook.ts
    
    Co-Authored-By: Erik Munson <erik@ipsumcreative.com>

---
+++ app/scripts/modules/core/src/presentation/hooks/useData.hook.spec.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import { IUseLatestPromiseResult } from './useLatestPromise.hook';
+import { useData } from './useData.hook';
+
+describe('useData hook', () => {
+  // Remove the the refresh function for .isEqual assertions
+  function promiseState(call: IUseLatestPromiseResult<any>) {
+    const { refresh, ...state } = call;
+    return state;
+  }
+
+  function Component(props: any) {
+    const { promiseFactory, deps, onChange, defaultValue } = props;
+    const useDataResult: IUseLatestPromiseResult<any> = useData(promiseFactory, defaultValue, deps);
+    const { status, result, error, requestId } = useDataResult;
+
+    React.useEffect(() => onChange(useDataResult), [status, result, error, requestId]);
+    return <></>;
+  }
+
+  function defer() {
+    let resolve: Function, reject: Function;
+    const promise = new Promise((_resolve, _reject) => {
+      resolve = _resolve;
+      reject = _reject;
+    });
+    return { promise, resolve, reject };
+  }
+
+  it('the factory is not called when some deps are null', () => {
+    const spy = jasmine.createSpy('onChange');
+    const factory = jasmine.createSpy('factory');
+    mount(<Component promiseFactory={factory} deps={['foo', null]} defaultValue={'default'} onChange={spy} />);
+    expect(factory).toHaveBeenCalledTimes(0);
+  });
+
+  it('the factory is not called when some deps are undefined', () => {
+    const spy = jasmine.createSpy('onChange');
+    const factory = jasmine.createSpy('factory');
+    mount(<Component promiseFactory={factory} deps={['foo', undefined]} defaultValue={'default'} onChange={spy} />);
+    expect(factory).toHaveBeenCalledTimes(0);
+  });
+
+  it('the default result is returned until the promise resolves', async done => {
+    const spy = jasmine.createSpy('onChange');
+    const deferred = defer();
+    const factory = () => deferred.promise;
+    const component = mount(
+      <Component promiseFactory={factory} deps={['foo']} defaultValue={'default'} onChange={spy} />,
+    );
+
+    expect(spy).toHaveBeenCalledTimes(2);
+
+    expect(promiseState(spy.calls.argsFor(0)[0])).toEqual({
+      status: 'NONE',
+      result: 'default',
+      error: undefined,
+      requestId: 0,
+    });
+
+    expect(promiseState(spy.calls.argsFor(1)[0])).toEqual({
+      status: 'PENDING',
+      result: 'default',
+      error: undefined,
+      requestId: 0,
+    });
+
+    deferred.resolve('result');
+    await deferred.promise;
+    component.setProps({});
+    expect(spy).toHaveBeenCalledTimes(3);
+    expect(promiseState(spy.calls.argsFor(2)[0])).toEqual({
+      status: 'RESOLVED',
+      result: 'result',
+      error: undefined,
+      requestId: 0,
+    });
+
+    done();
+  });
+});
--- app/scripts/modules/core/src/presentation/hooks/useData.hook.ts
+++ app/scripts/modules/core/src/presentation/hooks/useData.hook.ts
@@ -1,23 +1,26 @@
 import { IPromise } from 'angular';
-import { $q } from 'ngimport';
 import { isNil } from 'lodash';
 
 import { useLatestPromise, IUseLatestPromiseResult } from './useLatestPromise.hook';
 
 /**
- * A react hook which invokes a promise returning callback whenever any of its dependencies changes.
- * Returns the provided default value whenever any dependency is null or undefined
+ * A react hook which invokes a promise factory callback whenever any of its dependencies changes.
+ *
+ * Returns the default result until the first promise resolves.
+ *
+ * The promise factory is not called if any of the deps are null or undefined.
  *
  * This can be useful when fetching data based on a users keyboard input, for example.
  * This behavior is similar to RxJS switchMap.
  *
  * @param callback the callback to be invoked whenever dependencies change
- * @param defaultValue the default value to return when any dependencies are null or undefined
+ * @param defaultResult the default result returned before the first promise resolves
  * @param deps array of dependencies, which (when changed) cause the callback to be invoked again
  * @returns an object with the result and current status of the promise
  */
-export function useData<T>(callback: () => IPromise<T>, defaultValue: T, deps: any[]): IUseLatestPromiseResult<T> {
+export function useData<T>(callback: () => IPromise<T>, defaultResult: T, deps: any[]): IUseLatestPromiseResult<T> {
   const anyDepsMissing = deps.some(dep => isNil(dep));
-  const defaultValueCallback = () => $q.resolve(defaultValue);
-  return useLatestPromise(anyDepsMissing ? defaultValueCallback : callback, deps);
+  const result = useLatestPromise<T>(anyDepsMissing ? () => null : callback, deps);
+  const useDefaultValue = result.requestId === 0 && result.status !== 'RESOLVED';
+  return useDefaultValue ? { ...result, result: defaultResult } : result;
 }
