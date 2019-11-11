c83f58a84af983bf3d2438a8933a9e1e0681ac9c
Emily Burns <emjeburns@gmail.com>
2019-11-11 11:48:31 -0800

    fix(md): add notifications to envs (#418)

--- kork-manageddelivery/src/main/java/com/netflix/spinnaker/kork/manageddelivery/model/Notification.java
+++ kork-manageddelivery/src/main/java/com/netflix/spinnaker/kork/manageddelivery/model/Notification.java
@@ -13,17 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.netflix.spinnaker.kork.manageddelivery.model;
 
-import java.util.Collection;
-import java.util.Map;
 import lombok.Data;
 
 @Data
-public class Environment {
-  String name;
-  Collection<Resource> resources;
-  Collection<Map<String, Object>> constraints;
-  Collection<Notification> notifications;
+public class Notification {
+  String type;
+  String address;
+  String frequency;
 }
