f513f1c8625aa1acfb9ff1aba7c5c8d2bb29b13c
Alan Quach <aquach@netflix.com>
2019-11-19 16:14:39 -0800

    test(npm): Ensuring module version changes are standalone PRs (#7637)
    * test(npm): Ensuring module version changes are standalone PRs
    
    * test(npm): Using TRAVIS_BRANCH
    
    * test(npm): Making script executable
    
    * chore(travis): This ephemeral change should cause travis to fail due to package bump PR containing other changes
    
    * chore(travis): This ephemeral change should cause travis to fail due to package.json having multiple lines changed
    
    * chore(travis): Reverting ephemeral changes
    
    * test(npm): Removing unnecessary merging

---
+++ app/scripts/modules/assert_package_bumps_standalone.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Reports if package bumps are combined with other changes (not allowed). Package bumps must be standalone.
+# cd `dirname $0`;
+
+PKGJSONCHANGED="Version change detected in package.json"
+ONLYVERSIONCHANGED="Version change must be the only line changed in package.json"
+ONLYPKGJSONCHANGED="package.json (in apps/scripts/modules) must be the only files changed in a pull request with version bumps"
+
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
+TARGET_BRANCH=${TRAVIS_BRANCH:-master}
+echo "TARGET_BRANCH=$TARGET_BRANCH"
+
+# Tests are run against an ephemeral merge commit so we don't have to merge in $TARGET_BRANCH
+
+for PKGJSON in `ls app/scripts/modules/*/package.json` ; do
+  MODULE=`basename $(dirname $PKGJSON)`
+  echo "==================================================="
+  echo "Checking $MODULE"
+  echo "==================================================="
+  HAS_PKG_BUMP=`git diff $TARGET_BRANCH -- $PKGJSON | grep '"version"' | wc -l`
+  if [ $HAS_PKG_BUMP -ne 0 ] ; then
+    echo " [ YES  ] $PKGJSONCHANGED"
+
+    # Ensuring that the version change is the only change in package.json
+    PKG_JSON_OTHER_CHANGES=`git diff --numstat $TARGET_BRANCH -- $PKGJSON | cut -f 1`
+    if [ $PKG_JSON_OTHER_CHANGES -ne 1 ] ; then
+      echo " [ FAIL ] $ONLYVERSIONCHANGED"
+      echo ""
+      echo "=========================================="
+      echo "Failure details (git diff of package.json)"
+      echo "=========================================="
+      echo ""
+      git diff $TARGET_BRANCH -- $PKGJSON
+      echo ""
+      echo "=========================================="
+      exit 2
+    else
+      echo " [ PASS ] $ONLYVERSIONCHANGED"
+    fi
+
+    # checking that the only files changed are apps/scripts/modules/*/package.json
+    OTHER_FILES_CHANGED=`git diff --name-only $TARGET_BRANCH | grep -v "apps/scripts/modules/*/package.json" | wc -l`
+    if [ $OTHER_FILES_CHANGED -ne 0 ] ; then
+      echo " [ FAIL ] $ONLYPKGJSONCHANGED"
+      echo ""
+      echo "==========================================="
+      echo "Failure details (list of all files changed)"
+      echo "==========================================="
+      echo ""
+      git diff --name-only $TARGET_BRANCH
+      echo ""
+      echo "==========================================="
+      exit 1
+    else
+      echo " [ PASS ] $ONLYPKGJSONCHANGED"
+    fi
+  else
+    echo " [  NO  ] $PKGJSONCHANGED"
+    echo " [  N/A ] $ONLYVERSIONCHANGED"
+    echo " [  N/A ] $ONLYPKGJSONCHANGED"
+  fi
+  echo ""
+done
+
