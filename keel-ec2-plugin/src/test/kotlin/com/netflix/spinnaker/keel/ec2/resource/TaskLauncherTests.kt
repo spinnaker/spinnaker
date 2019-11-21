af9e8fc08fe43862238bbc890f0c0b48378e228b
Rob Fletcher <rfletcher@netflix.com>
2019-11-21 11:36:39 -0800

    fix(tests): adapt old EnvironmentResolver tests to TaskLauncher

--- keel-ec2-plugin/src/test/kotlin/com/netflix/spinnaker/keel/ec2/resource/TaskLauncherTests.kt
+++ keel-ec2-plugin/src/test/kotlin/com/netflix/spinnaker/keel/ec2/resource/TaskLauncherTests.kt
@@ -17,58 +17,92 @@
  */
 package com.netflix.spinnaker.keel.ec2.resource
 
+import com.netflix.spinnaker.keel.api.DeliveryConfig
+import com.netflix.spinnaker.keel.api.Environment
+import com.netflix.spinnaker.keel.api.NotificationConfig
+import com.netflix.spinnaker.keel.api.NotificationFrequency.QUIET
+import com.netflix.spinnaker.keel.api.NotificationType.SLACK
 import com.netflix.spinnaker.keel.api.Resource
+import com.netflix.spinnaker.keel.api.randomUID
+import com.netflix.spinnaker.keel.model.NotificationEvent.ORCHESTRATION_FAILED
+import com.netflix.spinnaker.keel.model.OrchestrationRequest
 import com.netflix.spinnaker.keel.orca.OrcaService
+import com.netflix.spinnaker.keel.orca.TaskRefResponse
 import com.netflix.spinnaker.keel.persistence.memory.InMemoryDeliveryConfigRepository
 import com.netflix.spinnaker.keel.plugin.TaskLauncher
 import com.netflix.spinnaker.keel.test.DummyResourceSpec
 import com.netflix.spinnaker.keel.test.resource
 import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
+import io.mockk.coEvery
 import io.mockk.mockk
+import io.mockk.slot
+import kotlinx.coroutines.runBlocking
+import strikt.api.expectThat
+import strikt.assertions.first
+import strikt.assertions.hasSize
+import strikt.assertions.isEmpty
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNotEmpty
 import java.time.Clock
 
 class TaskLauncherTests : JUnit5Minutests {
-  object Fixture {
+  class Fixture {
     val clock = Clock.systemDefaultZone()
     val orcaService: OrcaService = mockk()
     val deliveryConfigRepository = InMemoryDeliveryConfigRepository(clock)
     val taskLauncher = TaskLauncher(orcaService, deliveryConfigRepository)
     val resource: Resource<DummyResourceSpec> = resource()
+    val request = slot<OrchestrationRequest>()
   }
 
   fun tests() = rootContext<Fixture> {
-//    fixture { Fixture }
-//
-//    context("no environments") {
-//      test("empty list gets returned") {
-//        expectThat(taskLauncher.getNotificationsFor(ResourceId("blah"))).isEmpty()
-//      }
-//    }
-//
-//    context("an environment exists") {
-//      before {
-//        val notification = NotificationConfig(SLACK, "#my-channel", QUIET)
-//        val env = Environment(
-//          name = "test",
-//          resources = setOf(resource),
-//          notifications = setOf(notification)
-//        )
-//        val deliveryConfig = DeliveryConfig(name = "keel", application = "keel", environments = setOf(env))
-//        deliveryConfigRepository.store(deliveryConfig)
-//      }
-//
-//      test("notifications get retrieved") {
-//        val n = taskLauncher.getNotificationsFor(resource.id)
-//        expect {
-//          that(n).isNotEmpty()
-//          that(n.size).isEqualTo(1)
-//          that(n.first().`when`).isEqualTo(listOf(ORCHESTRATION_FAILED.text()))
-//          that(n.first().message).isEqualTo(
-//            mapOf(ORCHESTRATION_FAILED.text() to ORCHESTRATION_FAILED.notificationMessage())
-//          )
-//        }
-//      }
-//    }
+    fixture { Fixture() }
+
+    before {
+      coEvery {
+        orcaService.orchestrate(any(), capture(request))
+      } returns TaskRefResponse("/tasks/${randomUID()}")
+    }
+
+    context("no environments") {
+      before {
+        runBlocking {
+          taskLauncher.submitJobToOrca(resource, "task description", "correlate this", mapOf())
+        }
+      }
+
+      test("empty list gets returned") {
+        expectThat(request.captured.trigger.notifications).isEmpty()
+      }
+    }
+
+    context("an environment exists") {
+      before {
+        val notification = NotificationConfig(SLACK, "#my-channel", QUIET)
+        val env = Environment(
+          name = "test",
+          resources = setOf(resource),
+          notifications = setOf(notification)
+        )
+        val deliveryConfig = DeliveryConfig(name = "keel", application = "keel", environments = setOf(env))
+        deliveryConfigRepository.store(deliveryConfig)
+
+        runBlocking {
+          taskLauncher.submitJobToOrca(resource, "task description", "correlate this", mapOf())
+        }
+      }
+
+      test("notifications get retrieved") {
+        expectThat(request.captured.trigger.notifications) {
+          isNotEmpty()
+          hasSize(1)
+          first().get { `when` }.isEqualTo(listOf(ORCHESTRATION_FAILED.text()))
+          first().get { message }.isEqualTo(
+            mapOf(ORCHESTRATION_FAILED.text() to ORCHESTRATION_FAILED.notificationMessage())
+          )
+        }
+      }
+    }
   }
 }
