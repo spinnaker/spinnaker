eb47d6245a64a6173e141a6612e830ad35614fd2
Erik Munson <emunson@netflix.com>
2019-11-27 14:14:02 -0800

    feat(core/presentation): expose all the Modal-related components

--- app/scripts/modules/core/src/presentation/index.ts
+++ app/scripts/modules/core/src/presentation/index.ts
@@ -3,6 +3,7 @@ export * from './FormElements';
 export * from './HoverablePopover';
 export * from './LabelComponent';
 export * from './Markdown';
+export * from './modal';
 export * from './Placement';
 export * from './ReactModal';
 export * from './RenderOutputFile';
---
+++ app/scripts/modules/core/src/presentation/modal/index.ts
@@ -0,0 +1,4 @@
+export * from './Modal';
+export * from './ModalHeader';
+export * from './ModalBody';
+export * from './ModalFooter';
