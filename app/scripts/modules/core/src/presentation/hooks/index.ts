da8047c9790b5478c36f13b22ab7e9f959101a1d
Erik Munson <emunson@netflix.com>
2019-11-27 13:53:46 -0800

    chore(core/presentation): expose some new hooks to consumers

--- app/scripts/modules/core/src/presentation/hooks/index.ts
+++ app/scripts/modules/core/src/presentation/hooks/index.ts
@@ -1,4 +1,7 @@
+export * from './useContainerClassNames.hook';
 export * from './useData.hook';
 export * from './useDebouncedValue.hook';
+export * from './useEventListener.hook';
 export * from './useIsMountedRef.hook';
+export * from './useLatestCallback.hook';
 export * from './useLatestPromise.hook';
