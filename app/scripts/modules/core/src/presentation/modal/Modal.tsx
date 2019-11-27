319a221af392d7b5b7964e7ad38a0571b694bfb9
Erik Munson <emunson@netflix.com>
2019-11-27 14:09:55 -0800

    feat(core/presentation): add <Modal/> component

---
+++ app/scripts/modules/core/src/presentation/modal/Modal.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
+
+const { useMemo } = React;
+
+import { useEventListener, useContainerClassNames, useLatestCallback } from 'core/presentation';
+
+import { ModalContext } from './ModalContext';
+import * as styles from './Modal.module.css';
+
+export interface IModalProps {
+  isOpen: boolean;
+  onRequestClose?: () => any;
+  children?: React.ReactNode;
+}
+
+export const Modal = ({ onRequestClose, isOpen, children }: IModalProps) => {
+  useContainerClassNames(isOpen ? [styles.backdropBlurEffect] : []);
+
+  const keydownCallback = ({ keyCode }: KeyboardEvent) => {
+    if (keyCode === 27 /* esc */) {
+      onRequestClose();
+    }
+  };
+  useEventListener(document, 'keydown', isOpen ? keydownCallback : null);
+
+  const memoizedRequestClose = useLatestCallback(onRequestClose);
+  const modalContext = useMemo(() => ({ onRequestClose: memoizedRequestClose }), []);
+
+  return (
+    <ModalContext.Provider value={modalContext}>
+      {ReactDOM.createPortal(
+        <>
+          <CSSTransition in={isOpen} timeout={300} mountOnEnter={true} unmountOnExit={true} classNames={styles}>
+            <div className={styles.backdrop} />
+          </CSSTransition>
+          <CSSTransition in={isOpen} timeout={300} mountOnEnter={true} unmountOnExit={true} classNames={styles}>
+            <div className={styles.dialogWrapper}>
+              <div className={styles.dialog}>{children}</div>
+            </div>
+          </CSSTransition>
+        </>,
+        document.body,
+      )}
+    </ModalContext.Provider>
+  );
+};
---
+++ app/scripts/modules/core/src/presentation/modal/ModalContext.ts
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface IModalContext {
+  onRequestClose: () => any;
+}
+
+export const ModalContext = React.createContext<IModalContext>(null);
