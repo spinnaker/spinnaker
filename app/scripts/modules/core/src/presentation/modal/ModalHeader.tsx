4932d642d900184c3407bf16ce3c72edc35cfb0b
Erik Munson <emunson@netflix.com>
2019-11-27 14:11:07 -0800

    feat(core/presentation): add <ModalHeader/> component

---
+++ app/scripts/modules/core/src/presentation/modal/ModalHeader.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+const { useContext } = React;
+
+import { ModalContext } from './ModalContext';
+import * as styles from './ModalHeader.module.css';
+
+export interface IModalHeaderProps {
+  children?: React.ReactNode;
+}
+
+export const ModalHeader = ({ children }: IModalHeaderProps) => {
+  const { onRequestClose } = useContext(ModalContext);
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.title}>{children}</div>
+      <div className={styles.close} onClick={() => onRequestClose && onRequestClose()}>
+        <i className="ico icon-close" />
+      </div>
+    </div>
+  );
+};
