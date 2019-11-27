36c8cd22730b0a0ab5cbd24c27163705199822d7
Erik Munson <emunson@netflix.com>
2019-11-27 14:12:40 -0800

    feat(core/presentation): add <ModalFooter/> component

---
+++ app/scripts/modules/core/src/presentation/modal/ModalFooter.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import * as styles from './ModalFooter.module.css';
+
+export interface IModalFooterProps {
+  primaryActions?: React.ReactNode;
+  secondaryActions?: React.ReactNode;
+}
+
+export const ModalFooter = ({ primaryActions, secondaryActions }: IModalFooterProps) => (
+  <div className={styles.footer}>
+    {secondaryActions && <div className={styles.footerLeft}>{secondaryActions}</div>}
+    {primaryActions && <div className={styles.footerRight}>{primaryActions}</div>}
+  </div>
+);
