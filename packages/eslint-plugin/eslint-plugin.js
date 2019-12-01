e4cfc091966a04aa66158e05bcdea30d1d4d1346
Chris Thielen <github@sandgnat.com>
2019-12-01 13:39:48 -0800

    feat(eslint): Create no-require-dot-name rule that prefers imports over requires for angular modules
    Prefers:
       import { ANGULAR_MODULE } from './module';
       angular.module('foo', [ANGULAR_MODULE]);
    Over:
       angular.module('foo', [require('./module').name])

--- packages/eslint-plugin/eslint-plugin.js
+++ packages/eslint-plugin/eslint-plugin.js
@@ -3,6 +3,7 @@ module.exports = {
     'component-literal': require('./rules/component-literal'),
     strictdi: require('./rules/strictdi'),
     'no-ng-module-export': require('./rules/no-ng-module-export'),
+    'no-require-angularjs-deps': require('./rules/no-require-angularjs-deps'),
   },
   configs: {
     base: require('./base.config.js'),
