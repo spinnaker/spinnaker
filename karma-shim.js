5b0f15fd9cb7f17fa856eb488e0383192e6439e5
Chris Thielen <github@sandgnat.com>
2019-12-02 10:22:22 -0800

    refactor(karma): migrate karma-shim to es6 imports

--- karma-shim.js
+++ karma-shim.js
@@ -8,15 +8,15 @@ Error.stackTraceLimit = Infinity;
 // jquery has to be first or many a test will break
 global.$ = global.jQuery = require('jquery');
 
-require('./settings');
-require('./app/scripts/app');
+import './settings';
+import './app/scripts/app';
 
 // angular 1 test harnesss
-const angular = require('angular');
-require('angular-mocks');
+import 'angular';
+import 'angular-mocks';
 beforeEach(angular.mock.module('bcherny/ngimport'));
 
-require('./test/helpers/customMatchers');
+import './test/helpers/customMatchers';
 
 const testContext = require.context('./app/scripts/', true, /\.spec\.(js|ts|tsx)$/);
 testContext.keys().forEach(testContext);
