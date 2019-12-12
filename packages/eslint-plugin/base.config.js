5db4b45ecee0a39066358ad6b293c7481a7a98a6
Chris Thielen <github@sandgnat.com>
2019-12-12 11:44:06 -0800

    chore(eslint): Turn off some more eslint rules These rules do not spark joy

--- packages/eslint-plugin/base.config.js
+++ packages/eslint-plugin/base.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  parserOptions: { sourceType: 'module' },
   plugins: ['@typescript-eslint', '@spinnaker/eslint-plugin'],
   extends: ['eslint:recommended', 'prettier', 'prettier/@typescript-eslint', 'plugin:@typescript-eslint/recommended'],
   rules: {
@@ -12,20 +13,28 @@ module.exports = {
     'member-ordering': 'off',
     'no-console': ['error', { allow: ['warn', 'error'] }],
     'no-extra-boolean-cast': 'off',
+    'no-prototype-builtins': 'off',
+    'one-var': ['error', { initialized: 'never' }],
+    'prefer-rest-params': 'off',
+    'prefer-spread': 'off',
     '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-case-declarations': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-object-literal-type-assertion': 'off',
     '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
     '@typescript-eslint/no-triple-slash-reference': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/no-var-requires': 'off', // TODO: turn on?
+    '@typescript-eslint/no-var-requires': 'off', // TODO: turn on once all code is using ES6 imports
+    '@typescript-eslint/triple-slash-reference': 'off',
   },
   overrides: [
     {
