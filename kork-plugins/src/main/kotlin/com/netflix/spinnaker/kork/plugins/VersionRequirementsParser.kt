1d723bb28f09122d74944e15e2b70f3eafac4cd5
Rob Zienert <rzienert@netflix.com>
2020-01-16 13:49:55 -0800

    feat(plugins): Moving version requirements parsing from front50 to kork (#473)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ kork-plugins/src/main/kotlin/com/netflix/spinnaker/kork/plugins/VersionRequirementsParser.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spinnaker.kork.plugins
+
+import com.netflix.spinnaker.kork.exceptions.UserException
+import java.lang.IllegalStateException
+import java.util.regex.Pattern
+
+/**
+ * Provides utility methods for parsing version requirements values from and to [VersionRequirements].
+ *
+ * Version requirements are in the format of "{service}{operator}{version}", where:
+ *
+ * - `service` is the service name that is supported by a plugin
+ * - `operator` is a version constraint operator (`>`, `<`, `>=`, `<=`)
+ * - `version` is the service version that is being constrained
+ */
+object VersionRequirementsParser {
+
+  private val SUPPORTS_PATTERN = Pattern.compile(
+    "^(?<service>[\\w\\-]+)(?<operator>[><=]{1,2})(?<version>[0-9]+\\.[0-9]+\\.[0-9]+)$")
+
+  private const val SUPPORTS_PATTERN_SERVICE_GROUP = "service"
+  private const val SUPPORTS_PATTERN_OPERATOR_GROUP = "operator"
+  private const val SUPPORTS_PATTERN_VERSION_GROUP = "version"
+
+  /**
+   * Parse a single version.
+   */
+  fun parse(version: String): VersionRequirements {
+    return SUPPORTS_PATTERN.matcher(version)
+      .also {
+        try {
+          it.matches()
+        } catch (e: IllegalStateException) {
+          throw InvalidPluginVersionRequirementException(version, e)
+        }
+      }
+      .let {
+        VersionRequirements(
+          service = it.group(SUPPORTS_PATTERN_SERVICE_GROUP),
+          operator = VersionRequirementOperator.from(it.group(SUPPORTS_PATTERN_OPERATOR_GROUP)),
+          version = it.group(SUPPORTS_PATTERN_VERSION_GROUP)
+        )
+      }
+  }
+
+  /**
+   * Parse a list of comma-delimited versions.
+   */
+  fun parseAll(version: String): List<VersionRequirements> =
+    version.split(',').map { parse(it.trim()) }
+
+  /**
+   * Convert a list of [VersionRequirements] into a string.
+   */
+  fun stringify(requirements: List<VersionRequirements>): String =
+    requirements.joinToString(",") { it.toString() }
+
+  enum class VersionRequirementOperator(val symbol: String) {
+    GT(">"),
+    LT("<"),
+    GT_EQ(">="),
+    LT_EQ("<=");
+
+    companion object {
+      fun from(symbol: String): VersionRequirementOperator =
+        values().find { it.symbol == symbol }
+          ?: throw IllegalVersionRequirementsOperator(symbol, values().map { it.symbol }.joinToString { "'$it'" })
+    }
+  }
+
+  data class VersionRequirements(
+    val service: String,
+    val operator: VersionRequirementOperator,
+    val version: String
+  ) {
+    override fun toString(): String = "$service${operator.symbol}$version"
+  }
+
+  class InvalidPluginVersionRequirementException(version: String, cause: Exception) : UserException(
+    "The provided version requirement '$version' is not valid: It must conform to '$SUPPORTS_PATTERN'",
+    cause
+  )
+
+  class IllegalVersionRequirementsOperator(symbol: String, availableOperators: String) : UserException(
+    "Illegal version requirement operator '$symbol': Must be one of $availableOperators"
+  )
+}
---
+++ kork-plugins/src/test/kotlin/com/netflix/spinnaker/kork/plugins/VersionRequirementsParserTest.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spinnaker.kork.plugins
+
+import com.netflix.spinnaker.kork.plugins.VersionRequirementsParser.VersionRequirementOperator
+import com.netflix.spinnaker.kork.plugins.VersionRequirementsParser.VersionRequirements
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import strikt.api.expectThat
+import strikt.assertions.hasSize
+import strikt.assertions.isEqualTo
+
+class VersionRequirementsParserTest : JUnit5Minutests {
+
+  fun tests() = rootContext {
+    test("parses a version string") {
+      expectThat(VersionRequirementsParser.parse("clouddriver>=1.0.0"))
+        .and {
+          get { service }.isEqualTo("clouddriver")
+          get { operator }.isEqualTo(VersionRequirementOperator.GT_EQ)
+          get { version }.isEqualTo("1.0.0")
+        }
+    }
+
+    test("parses a list of version strings") {
+      expectThat(VersionRequirementsParser.parseAll("orca<8.0.0,deck>=1.1.0"))
+        .hasSize(2)
+        .and {
+          get { this[0].service }.isEqualTo("orca")
+          get { this[1].service }.isEqualTo("deck")
+        }
+    }
+
+    test("converts a list of requirements to a string") {
+      val result = VersionRequirementsParser.stringify(listOf(
+        VersionRequirements("clouddriver", VersionRequirementOperator.GT, "1.0.0"),
+        VersionRequirements("orca", VersionRequirementOperator.GT, "3.0.0")
+      ))
+      expectThat(result).isEqualTo("clouddriver>1.0.0,orca>3.0.0")
+    }
+  }
+}
