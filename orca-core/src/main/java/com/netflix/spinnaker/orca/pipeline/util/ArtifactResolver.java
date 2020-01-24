02cd0b800f19f5bcafaf6db830e152864a0789e6
Eric Zimanyi <ezimanyi@google.com>
2020-01-24 10:42:19 -0500

    refactor(artifacts): Some minor cleanup of artifact handling (#3391)
    * refactor(artifacts): Simplify error handling of incorrect StageContext
    
    Two functions log a warning and return a default value if the input
    StageContext is not of the correct type. Instead of putting the
    entire (or bulk of the) function in an if/else block, just check
    the desired condition and return early if it fails, pulling the rest
    of the function out of the if/else block.
    
    In the case of getBoundArtifactForId, just return null early rather
    than process an empty list that will eventually return null when
    nothing matches.
    
    * refactor(artifacts): Clean up stream code in ArtifactUtils
    
    A lot of stream code is overly nested, primarily because it mixes
    Optional.map() with Stream.map() and needs to reach into the Optional.
    Instead use the simpler null-safe stream pattern:
    Optional.ofNullable(collection).map(Collection::stream).orElse(emptyStream)
    
    * refactor(artifacts): Add Nonnull and Nullable annotations
    
    ArtifactUtils is generally good about annotating parameters and return
    values with Nonnull or Nullable. Add NonnullByDefault to the class so
    that all methods, parameters, and fields are non-null by default, and
    explicitly annotate those that are nullable. (Also change a nonnull
    Boolean to a boolean).
    
    Likewise, add NonnullByDefault to ArtifactResolver and remove all of
    the explicit Nonnull annotations.  (Nothing in that class can be null.)
    
    * refactor(artifacts): Simplify getExecutionForPipelineId
    
    Let's get out of RxJava Observable land sooner, returning all found
    executions as a list, and finding the max in the stream. This has two
    advantages:
    (1) We can similify start_time_or_id to be a Comparator instead of a
    Function<?,?,?>, and use the Comparator class to generate it.
    (2) (minor) We can just get the .max() of the stream instead of sorting
    the whole thing and getting the first element
    
    Also change getExecutionForPipelineId to return an Optional instead
    of being nullable; this makes the calling code a lot cleaner as well.
    
    * refactor(artifacts): Simplify getAllArtifacts
    
    The overload of getAllArtifacts that accepts a filter is only ever
    called from within the class; make it private.
    
    It's also overkill to make the parameter be an Optional<Predicate>
    as we have another overload of the function that doesn't require
    the parameter at all; if you're calling this one, you should pass
    in an actual predicate.  This simplifes the callers by unwrapping
    some Optionals.
    
    As the includeTrigger boolean is always true; remove the
    parameter.
    
    Finally, remove the objectMapper.convertValue() converting
    the result of trigger.getArtifacts() to List<Artifact>; this
    is already the type of the return value of that function.
    
    * refactor(artifacts): ArtifactResolver should accept a Java 8 supplier
    
    ArtifactResolver was accepting a guava Supplier (as it is using
    some guava utils to create the memoized supplier). Make things
    easier on clients by accepting a Java 8 supplier, and converting
    it to a Guava supplier within the class, hiding this implementation
    detail from clients.
    
    None of the callers actually need to change because they are all
    passing in a lambda which satisfies both functional interfaces,
    but if at some point in the future a caller has a Supplier<> in
    a variable, I'd rather it be able to use it directly instead of
    converting to a guava supplier. (A caller that has a guava supplier
    will still be able to use it as the guava Supplier interface
    extends the java 8 Supplier interface.)

--- orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/util/ArtifactResolver.java
+++ orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/util/ArtifactResolver.java
@@ -19,25 +19,24 @@ package com.netflix.spinnaker.orca.pipeline.util;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.lang.String.format;
 
-import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.netflix.spinnaker.kork.annotations.NonnullByDefault;
 import com.netflix.spinnaker.kork.artifacts.model.Artifact;
 import com.netflix.spinnaker.kork.artifacts.model.ExpectedArtifact;
 import com.netflix.spinnaker.kork.web.exceptions.InvalidRequestException;
 import java.util.Optional;
-import javax.annotation.Nonnull;
-import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.function.Supplier;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /** This class handles resolving a collection of {@link ExpectedArtifact} instances. */
-@ParametersAreNonnullByDefault
+@NonnullByDefault
 public final class ArtifactResolver {
-  @Nonnull private final ImmutableList<Artifact> currentArtifacts;
-  @Nonnull private final Supplier<ImmutableList<Artifact>> priorArtifacts;
+  private final ImmutableList<Artifact> currentArtifacts;
+  private final Supplier<ImmutableList<Artifact>> priorArtifacts;
   private final boolean requireUniqueMatches;
 
   private ArtifactResolver(
@@ -46,7 +45,7 @@ public final class ArtifactResolver {
       boolean requireUniqueMatches) {
     this.currentArtifacts = ImmutableList.copyOf(currentArtifacts);
     this.priorArtifacts =
-        Suppliers.memoize(Suppliers.compose(ImmutableList::copyOf, priorArtifacts));
+        Suppliers.memoize(Suppliers.compose(ImmutableList::copyOf, priorArtifacts::get));
     this.requireUniqueMatches = requireUniqueMatches;
   }
 
@@ -178,12 +177,12 @@ public final class ArtifactResolver {
      * expected artifact. If an artifact matches more than one expected artifact, it is returned
      * only once.
      */
-    @Nonnull private final ImmutableList<Artifact> resolvedArtifacts;
+    private final ImmutableList<Artifact> resolvedArtifacts;
     /**
      * This field contains the resolved expected artifacts; each resolved expected artifact is a
      * copy of the input expected artifact with its {@link ExpectedArtifact#getBoundArtifact()} set
      * to the artifact that it matched during resolution.
      */
-    @Nonnull private final ImmutableList<ExpectedArtifact> resolvedExpectedArtifacts;
+    private final ImmutableList<ExpectedArtifact> resolvedExpectedArtifacts;
   }
 }
--- orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/util/ArtifactUtils.java
+++ orca-core/src/main/java/com/netflix/spinnaker/orca/pipeline/util/ArtifactUtils.java
@@ -19,12 +19,12 @@ package com.netflix.spinnaker.orca.pipeline.util;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static java.util.Collections.emptyList;
-import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.netflix.spinnaker.kork.annotations.NonnullByDefault;
 import com.netflix.spinnaker.kork.artifacts.model.Artifact;
 import com.netflix.spinnaker.kork.artifacts.model.ExpectedArtifact;
 import com.netflix.spinnaker.orca.pipeline.model.Execution;
@@ -33,8 +33,9 @@ import com.netflix.spinnaker.orca.pipeline.model.StageContext;
 import com.netflix.spinnaker.orca.pipeline.persistence.ExecutionRepository;
 import com.netflix.spinnaker.orca.pipeline.persistence.ExecutionRepository.ExecutionCriteria;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -42,16 +43,16 @@ import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import rx.functions.Func2;
 import rx.schedulers.Schedulers;
 
 @Component
+@NonnullByDefault
 public class ArtifactUtils {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
@@ -70,67 +71,45 @@ public class ArtifactUtils {
     this.contextParameterProcessor = contextParameterProcessor;
   }
 
-  public @Nonnull List<Artifact> getArtifacts(@Nonnull Stage stage) {
-    if (stage.getContext() instanceof StageContext) {
-      return Optional.ofNullable((List<?>) ((StageContext) stage.getContext()).getAll("artifacts"))
-          .map(
-              list ->
-                  list.stream()
-                      .filter(Objects::nonNull)
-                      .flatMap(it -> ((List<?>) it).stream())
-                      .map(
-                          a ->
-                              a instanceof Map
-                                  ? objectMapper.convertValue(a, Artifact.class)
-                                  : (Artifact) a)
-                      .collect(Collectors.toList()))
-          .orElse(emptyList());
-    } else {
+  public List<Artifact> getArtifacts(Stage stage) {
+    if (!(stage.getContext() instanceof StageContext)) {
       log.warn(
           "Unable to read artifacts from unknown context type: {} ({})",
           stage.getContext().getClass(),
           stage.getExecution().getId());
       return emptyList();
     }
+
+    return Optional.ofNullable((List<?>) ((StageContext) stage.getContext()).getAll("artifacts"))
+        .map(Collection::stream)
+        .orElse(Stream.empty())
+        .filter(Objects::nonNull)
+        .flatMap(it -> ((List<?>) it).stream())
+        .map(a -> a instanceof Map ? objectMapper.convertValue(a, Artifact.class) : (Artifact) a)
+        .collect(Collectors.toList());
   }
 
-  public @Nonnull List<Artifact> getAllArtifacts(@Nonnull Execution execution) {
-    return getAllArtifacts(execution, true, Optional.empty());
+  public List<Artifact> getAllArtifacts(Execution execution) {
+    return getAllArtifacts(execution, s -> true);
   }
 
-  public @Nonnull List<Artifact> getAllArtifacts(
-      @Nonnull Execution execution,
-      Boolean includeTrigger,
-      Optional<Predicate<Stage>> stageFilter) {
+  private List<Artifact> getAllArtifacts(Execution execution, Predicate<Stage> stageFilter) {
     // Get all artifacts emitted by the execution's stages; we'll sort the stages topologically,
     // then reverse the result so that artifacts from later stages will appear
     // earlier in the results.
     List<Artifact> emittedArtifacts =
         Stage.topologicalSort(execution.getStages())
-            .filter(stageFilter.orElse(s -> true))
+            .filter(stageFilter)
             .filter(s -> s.getOutputs().containsKey("artifacts"))
-            .flatMap(
-                s ->
-                    ((List<?>) s.getOutputs().get("artifacts"))
-                        .stream()
-                            .map(
-                                a ->
-                                    a instanceof Map
-                                        ? objectMapper.convertValue(a, Artifact.class)
-                                        : (Artifact) a))
+            .flatMap(s -> ((List<?>) s.getOutputs().get("artifacts")).stream())
+            .map(
+                a -> a instanceof Map ? objectMapper.convertValue(a, Artifact.class) : (Artifact) a)
             .collect(Collectors.toList());
     Collections.reverse(emittedArtifacts);
 
     // Get all artifacts in the parent pipeline's trigger; these artifacts go at the end of the
-    // list,
-    // after any that were emitted by the pipeline
-    if (includeTrigger) {
-      List<Artifact> triggerArtifacts =
-          objectMapper.convertValue(
-              execution.getTrigger().getArtifacts(), new TypeReference<List<Artifact>>() {});
-
-      emittedArtifacts.addAll(triggerArtifacts);
-    }
+    // list, after any that were emitted by the pipeline
+    emittedArtifacts.addAll(execution.getTrigger().getArtifacts());
 
     return emittedArtifacts;
   }
@@ -158,43 +137,38 @@ public class ArtifactUtils {
     return objectMapper.convertValue(evaluatedBoundArtifactMap, Artifact.class);
   }
 
-  public @Nullable Artifact getBoundArtifactForId(@Nonnull Stage stage, @Nullable String id) {
-    if (isEmpty(id)) {
+  public @Nullable Artifact getBoundArtifactForId(Stage stage, @Nullable String id) {
+    if (StringUtils.isEmpty(id)) {
       return null;
     }
 
-    List<ExpectedArtifact> expectedArtifacts;
-    if (stage.getContext() instanceof StageContext) {
-      expectedArtifacts =
-          Optional.ofNullable(
-                  (List<?>) ((StageContext) stage.getContext()).getAll("resolvedExpectedArtifacts"))
-              .map(
-                  list ->
-                      list.stream()
-                          .filter(Objects::nonNull)
-                          .flatMap(it -> ((List<?>) it).stream())
-                          .map(
-                              a ->
-                                  a instanceof Map
-                                      ? objectMapper.convertValue(a, ExpectedArtifact.class)
-                                      : (ExpectedArtifact) a)
-                          .collect(Collectors.toList()))
-              .orElse(emptyList());
-    } else {
+    if (!(stage.getContext() instanceof StageContext)) {
       log.warn(
           "Unable to read resolved expected artifacts from unknown context type: {} ({})",
           stage.getContext().getClass(),
           stage.getExecution().getId());
-      expectedArtifacts = new ArrayList<>();
+      return null;
     }
 
-    final Optional<ExpectedArtifact> expectedArtifactOptional =
-        expectedArtifacts.stream().filter(e -> e.getId().equals(id)).findFirst();
+    Optional<ExpectedArtifact> expectedArtifactOptional =
+        Optional.ofNullable(
+                (List<?>) ((StageContext) stage.getContext()).getAll("resolvedExpectedArtifacts"))
+            .map(Collection::stream)
+            .orElse(Stream.empty())
+            .filter(Objects::nonNull)
+            .flatMap(it -> ((List<?>) it).stream())
+            .map(
+                a ->
+                    a instanceof Map
+                        ? objectMapper.convertValue(a, ExpectedArtifact.class)
+                        : (ExpectedArtifact) a)
+            .filter(e -> e.getId().equals(id))
+            .findFirst();
 
     expectedArtifactOptional.ifPresent(
         expectedArtifact -> {
-          final Artifact boundArtifact = expectedArtifact.getBoundArtifact();
-          final Artifact matchArtifact = expectedArtifact.getMatchArtifact();
+          Artifact boundArtifact = expectedArtifact.getBoundArtifact();
+          Artifact matchArtifact = expectedArtifact.getMatchArtifact();
           if (boundArtifact != null
               && matchArtifact != null
               && boundArtifact.getArtifactAccount() == null) {
@@ -205,38 +179,36 @@ public class ArtifactUtils {
     return expectedArtifactOptional.map(ExpectedArtifact::getBoundArtifact).orElse(null);
   }
 
-  public @Nonnull List<Artifact> getArtifactsForPipelineId(
-      @Nonnull String pipelineId, @Nonnull ExecutionCriteria criteria) {
-    Execution execution = getExecutionForPipelineId(pipelineId, criteria);
-
-    return execution == null ? Collections.emptyList() : getAllArtifacts(execution);
+  public List<Artifact> getArtifactsForPipelineId(String pipelineId, ExecutionCriteria criteria) {
+    return getExecutionForPipelineId(pipelineId, criteria)
+        .map(this::getAllArtifacts)
+        .orElse(Collections.emptyList());
   }
 
-  public @Nonnull List<Artifact> getArtifactsForPipelineIdWithoutStageRef(
-      @Nonnull String pipelineId, @Nonnull String stageRef, @Nonnull ExecutionCriteria criteria) {
-    Execution execution = getExecutionForPipelineId(pipelineId, criteria);
-
-    if (execution == null) {
-      return Collections.emptyList();
-    }
-
-    return getAllArtifacts(execution, true, Optional.of(it -> !stageRef.equals(it.getRefId())));
+  public List<Artifact> getArtifactsForPipelineIdWithoutStageRef(
+      String pipelineId, String stageRef, ExecutionCriteria criteria) {
+    return getExecutionForPipelineId(pipelineId, criteria)
+        .map(e -> getAllArtifacts(e, it -> !stageRef.equals(it.getRefId())))
+        .orElse(Collections.emptyList());
   }
 
-  public void resolveArtifacts(@Nonnull Map pipeline) {
+  public void resolveArtifacts(Map pipeline) {
     Map<String, Object> trigger = (Map<String, Object>) pipeline.get("trigger");
     ImmutableList<ExpectedArtifact> expectedArtifacts =
-        Optional.ofNullable((List<?>) pipeline.get("expectedArtifacts")).orElse(emptyList())
-            .stream()
+        Optional.ofNullable((List<?>) pipeline.get("expectedArtifacts"))
+            .map(Collection::stream)
+            .orElse(Stream.empty())
             .map(it -> objectMapper.convertValue(it, ExpectedArtifact.class))
             .collect(toImmutableList());
 
     ImmutableSet<Artifact> receivedArtifacts =
         Stream.concat(
-                Optional.ofNullable((List<?>) pipeline.get("receivedArtifacts")).orElse(emptyList())
-                    .stream(),
-                Optional.ofNullable((List<?>) trigger.get("artifacts")).orElse(emptyList())
-                    .stream())
+                Optional.ofNullable((List<?>) pipeline.get("receivedArtifacts"))
+                    .map(Collection::stream)
+                    .orElse(Stream.empty()),
+                Optional.ofNullable((List<?>) trigger.get("artifacts"))
+                    .map(Collection::stream)
+                    .orElse(Stream.empty()))
             .map(it -> objectMapper.convertValue(it, Artifact.class))
             .collect(toImmutableSet());
 
@@ -273,7 +245,7 @@ public class ArtifactUtils {
     }
   }
 
-  private List<Artifact> getPriorArtifacts(final Map<String, Object> pipeline) {
+  private List<Artifact> getPriorArtifacts(Map<String, Object> pipeline) {
     // set pageSize to a single record to avoid hydrating all of the stored Executions for
     // the pipeline, since getArtifactsForPipelineId only uses the most recent Execution from the
     // returned Observable<Execution>
@@ -283,12 +255,11 @@ public class ArtifactUtils {
     return getArtifactsForPipelineId((String) pipeline.get("id"), criteria);
   }
 
-  private Execution getExecutionForPipelineId(
-      @Nonnull String pipelineId, @Nonnull ExecutionCriteria criteria) {
+  private Optional<Execution> getExecutionForPipelineId(
+      String pipelineId, ExecutionCriteria criteria) {
     return executionRepository.retrievePipelinesForPipelineConfigId(pipelineId, criteria)
-        .subscribeOn(Schedulers.io()).toSortedList(startTimeOrId).toBlocking().single().stream()
-        .findFirst()
-        .orElse(null);
+        .subscribeOn(Schedulers.io()).toList().toBlocking().single().stream()
+        .min(startTimeOrId);
   }
 
   private static class ArtifactResolutionException extends RuntimeException {
@@ -297,12 +268,12 @@ public class ArtifactUtils {
     }
   }
 
-  private static Func2<Execution, Execution, Integer> startTimeOrId =
-      (a, b) -> {
-        Long aStartTime = Optional.ofNullable(a.getStartTime()).orElse(0L);
-        Long bStartTime = Optional.ofNullable(b.getStartTime()).orElse(0L);
-
-        int startTimeCmp = bStartTime.compareTo(aStartTime);
-        return startTimeCmp != 0L ? startTimeCmp : b.getId().compareTo(a.getId());
-      };
+  // Consider moving this to ExecutionRepository.ExecutionComparator or re-using START_TIME_OR_ID
+  // from there. The only difference is that START_TIME_OR_ID sorts executions with a null start
+  // time first, followed by executions in order of recency (by start time then by id). We don't
+  // want executions with a null start time showing up first here as then a single execution with
+  // a null start time would always get selected by getExecutionForPipelineId.
+  private static Comparator<Execution> startTimeOrId =
+      Comparator.comparing(Execution::getStartTime, Comparator.nullsLast(Comparator.reverseOrder()))
+          .thenComparing(Execution::getId, Comparator.reverseOrder());
 }
