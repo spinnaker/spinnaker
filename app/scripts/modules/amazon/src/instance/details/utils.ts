607a6c46519d4079e37afc6238a57bfc33a33c54
caseyhebebrand <chebebrand@netflix.com>
2020-01-29 09:19:41 -0800

    fix(amazon): Prevent error if target group is undefined (#7819)
    * fix(amazon): Prevent error if target group is undefined
    
    * fix(amazon): Prevent error if target group is undefined
    
    * Use nullish coalescing

--- app/scripts/modules/amazon/src/instance/details/utils.ts
+++ app/scripts/modules/amazon/src/instance/details/utils.ts
@@ -16,7 +16,7 @@ export const applyHealthCheckInfoToTargetGroups = (
   healthMetrics.forEach(metric => {
     if (metric.type === 'TargetGroup') {
       metric.targetGroups.forEach((tg: ITargetGroup) => {
-        const group = targetGroups[tg.name];
+        const group = targetGroups[tg.name] ?? ({} as ITargetGroup);
         const port = group.healthCheckPort && isNumber(group.healthCheckPort) ? group.healthCheckPort : group.port;
         tg.healthCheckProtocol = group.healthCheckProtocol.toLowerCase();
         tg.healthCheckPath = `:${port}${group.healthCheckPath}`;
