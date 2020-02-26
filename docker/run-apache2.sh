ef24597d3ceca9224166cb71a13eb1ad950903da
Brandon Powell <brandon.powell@armory.io>
2020-02-26 15:35:07 -0800

    feat(plugins): copies over plugin-manifest from a halyard deploy (#7941)

--- docker/run-apache2.sh
+++ docker/run-apache2.sh
@@ -80,4 +80,9 @@ then
 	cp /opt/spinnaker/config/settings-local.js /opt/deck/html/settings-local.js
 fi
 
+if [ -e /opt/spinnaker/config/plugin-manifest.json ];
+then 
+	cp /opt/spinnaker/config/plugin-manifest.json /opt/deck/html/plugin-manifest.json
+fi
+
 apache2ctl -D FOREGROUND 
