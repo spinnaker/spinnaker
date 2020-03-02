27291f52a5a3eb6803841179c2857e379a1109cd
Gal Yardeni <55253849+gal-yardeni@users.noreply.github.com>
2020-03-02 15:06:08 -0800

    feat(ciView): creating the basis for ci view within Spinnaker (#641)
    * feat(ciView): creating the basis for ciView
    
    * removing unneeded classes
    
    * adding comments

---
+++ igor-web/src/main/java/com/netflix/spinnaker/igor/ci/CiController.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.igor.ci;
+
+import com.netflix.spinnaker.igor.build.model.GenericBuild;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ConditionalOnProperty("services.ci.enabled")
+@RequestMapping("/ci")
+public class CiController {
+
+  private Optional<CiBuildService> ciBuildService;
+
+  @Autowired
+  public CiController(Optional<CiBuildService> ciBuildService) {
+    this.ciBuildService = ciBuildService;
+  }
+
+  @GetMapping("/builds")
+  public List<GenericBuild> getBuilds(
+      @RequestParam(value = "projectKey") String projectKey,
+      @RequestParam(value = "repoSlug") String repoSlug,
+      @RequestParam(value = "completionStatus", required = false) String completionStatus) {
+    return getCiService().getBuilds(projectKey, repoSlug, completionStatus);
+  }
+
+  private CiBuildService getCiService() {
+    if (ciBuildService.isPresent()) {
+      return ciBuildService.get();
+    } else {
+      throw new UnsupportedOperationException("No CI service exists");
+    }
+  }
+}
