7d76d620637419da68ddf353c99bd126af9775ac
Chris Smalley <csmalley@netflix.com>
2020-03-02 15:42:33 -0800

    feat(plugins): Clean plugins dir when clean task runs (#103)
    Co-authored-by: Rob Zienert <rob@robzienert.com>

--- spinnaker-dev-plugin/src/main/groovy/com/netflix/spinnaker/gradle/baseproject/SpinnakerBaseProjectConventionsPlugin.groovy
+++ spinnaker-dev-plugin/src/main/groovy/com/netflix/spinnaker/gradle/baseproject/SpinnakerBaseProjectConventionsPlugin.groovy
@@ -9,11 +9,18 @@ import org.gradle.api.plugins.JavaPluginConvention
 class SpinnakerBaseProjectConventionsPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.plugins.withType(JavaPlugin) {
-            project.repositories.jcenter()
-            JavaPluginConvention convention = project.convention.getPlugin(JavaPluginConvention)
-            convention.sourceCompatibility = JavaVersion.VERSION_1_8
-            convention.targetCompatibility = JavaVersion.VERSION_1_8
-        }
+      project.plugins.withType(JavaPlugin) {
+        project.repositories.jcenter()
+        JavaPluginConvention convention = project.convention.getPlugin(JavaPluginConvention)
+        convention.sourceCompatibility = JavaVersion.VERSION_1_8
+        convention.targetCompatibility = JavaVersion.VERSION_1_8
+      }
+      project.afterEvaluate {
+        project.tasks
+          .getByName("clean")
+          .doLast {
+            project.delete(project.files("${project.projectDir}/plugins"))
+          }
+      }
     }
 }
