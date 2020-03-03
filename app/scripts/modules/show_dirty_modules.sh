0eaad8c39fb9f68a54cd49f1a97ca4f18a3a675d
Chris Thielen <christopherthielen@users.noreply.github.com>
2020-03-02 18:43:54 -0800

    feat(scripts): Add show_package_bumps script that outputs all package bumps and their commit shas. (#7983)
    * feat(scripts): Add show_package_bumps script that outputs all package bumps and their commit shas.
    Renamed show_unpublished_commits.sh to show_changelog.sh.
    Improved show_changelog to support changelogs between arbitrary package versions.
    
    * Update app/scripts/modules/show_package_bumps.js
    
    Co-Authored-By: Alan Quach <integsrtite@gmail.com>
    
    Co-authored-by: Alan Quach <integsrtite@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- app/scripts/modules/show_dirty_modules.sh
+++ app/scripts/modules/show_dirty_modules.sh
@@ -5,12 +5,12 @@ cd "$(dirname "$0")" || exit 1;
 for PKGJSON in */package.json ; do
   MODULE=$(dirname "$PKGJSON");
 
-  COUNT=$(./show_unpublished_commits.sh "$MODULE" | wc -l | sed -e 's/ //g')
+  COUNT=$(./show_changelog.sh "$MODULE/package.json" | wc -l | sed -e 's/ //g')
   if [ "$COUNT" -ne 0 ] ; then
     echo ""
     echo ""
     echo "=== $MODULE is dirty ($COUNT commits) ===";
-    ./show_unpublished_commits.sh "$MODULE"
+    ./show_changelog.sh "$MODULE/package.json"
   fi
 done
 
