abb0c437730f4b34e4f951e4735d54855fae180b
Erik Munson <emunson@netflix.com>
2020-03-12 16:10:28 -0700

    feat(core/presentation): add useObservable hook

---
+++ app/scripts/modules/core/src/presentation/hooks/useObservable.hook.ts
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { Observable } from 'rxjs';
+
+/**
+ * A react hook that subscribes to an rxjs observable on mount and calls the provided callback when
+ * a new value is emitted. Unsubscribes on unmount or when the observable parameter changes.
+ *
+ * @param observable the rxjs observable to subscribe to
+ * @param callback the function to call when the observable emits a value.
+ * Passed the new value as its only argument.
+ * @returns void
+ */
+export const useObservable = <T>(observable: Observable<T>, callback: (val: T) => void) => {
+  useEffect(() => {
+    const subscription = observable.subscribe(callback);
+    return () => subscription.unsubscribe();
+  }, [observable]);
+};
