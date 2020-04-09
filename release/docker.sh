891d84fb6106960d1c1c845597c480178efd38dc
Michael Plump <plumpy@google.com>
2020-04-09 10:37:47 -0400

    chore(containers): Make Dockerfiles work with buildtool's containers.yml (#1607)
    * chore(gradle): Upgrade gradlew to 5.6.4
    
    * chore(containers): Make Dockerfiles work with buildtool's containers.yml
    
    * chore(release): remove jenkins build scripts

--- release/docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker build . -t halyard.compile -f Dockerfile.compile
-docker build . -t halyard.local -f Dockerfile.local &
-docker build . -t halyard.ubuntu -f Dockerfile.ubuntu &
-wait
