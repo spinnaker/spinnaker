9735910047692f298225650f683b026a12c0f75b
Kevin Woo <kevinawoo@gmail.com>
2020-04-13 13:54:18 -0700

    fix(core/pipelines): typo with PIPELINE_CONFIG_ACTIONS (#8161)

--- app/scripts/modules/core/src/pipeline/config/actions/pipelineConfigActions.module.ts
+++ app/scripts/modules/core/src/pipeline/config/actions/pipelineConfigActions.module.ts
@@ -1,7 +1,7 @@
 import { module } from 'angular';
 import { react2angular } from 'react2angular';
 import { PipelineConfigActions } from './PipelineConfigActions';
-export const PIPELINE_CONFIG_ACTIONS = 'pinnaker.core.pipeline.config.actions';
+export const PIPELINE_CONFIG_ACTIONS = 'spinnaker.core.pipeline.config.actions';
 module(PIPELINE_CONFIG_ACTIONS, []).component(
   'pipelineConfigActions',
   react2angular(PipelineConfigActions, [
