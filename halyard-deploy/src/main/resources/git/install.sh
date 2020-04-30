eda2d8c514ce18892fed86e974fd206d0dc12d08
Michael Plump <plumpy@google.com>
2020-04-30 16:24:44 -0400

    fix(local-git): remove the Java version check from Halyard local-git deployment (#1658)
    I suppose I could put in something to check for Java 11, but only looking at the version of java in your $PATH seems like a pretty naive way to go about doing this check, since there are myriad ways you could have a JDK configured for Gradle.

--- halyard-deploy/src/main/resources/git/install.sh
+++ halyard-deploy/src/main/resources/git/install.sh
@@ -2,19 +2,6 @@
 
 ## auto-generated git install file written by halyard
 
-echo_err() {
-  echo "$@" 1>&2
-}
-
-set +e
-java_version=$(java -version 2>&1 head -1)
-set -e
-
-if [[ "$java_version" != *1.8* ]]; then
-  echo_err "You need the java jdk at version 1.8 to build & run Spinnaker. Please install it."
-  exit 1;
-fi
-
 STARTUP_SCRIPTS=()
 
 {%install-commands%}
