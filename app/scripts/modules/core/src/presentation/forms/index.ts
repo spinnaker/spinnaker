11b524debf8102eac6e0f7eefdaec8db620cc6b0
Chris Thielen <christopherthielen@users.noreply.github.com>
2020-05-06 16:42:04 -0700

    fix(core/presentation): Export the new save/restore formik fields hook (#8259)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- app/scripts/modules/core/src/presentation/forms/index.ts
+++ app/scripts/modules/core/src/presentation/forms/index.ts
@@ -1,6 +1,7 @@
 export * from './FormikForm';
 export * from './SpinFormik';
 export * from './fields';
+export * from './hooks';
 export * from './inputs';
 export * from './layouts';
 export * from './validation';
