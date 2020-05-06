0b82d9d288499e837ebb45025cfd1290ebd0b474
Chris Thielen <christopherthielen@users.noreply.github.com>
2020-05-06 12:29:40 -0700

    test(core/presentation): Test SpinFormik touches initial values (#8248)

---
+++ app/scripts/modules/core/src/presentation/forms/SpinFormik.spec.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { FormikProps } from 'formik';
+import { SpinFormik } from './SpinFormik';
+
+describe('SpinFormik', () => {
+  it('touches all fields in initialValues', () => {
+    let formik: FormikProps<any> = null;
+
+    mount(
+      <SpinFormik
+        initialValues={{ foo: '123', bar: '456' }}
+        onSubmit={() => null}
+        render={_formik => {
+          formik = _formik;
+          return null;
+        }}
+      />,
+    );
+
+    expect(formik.touched).toEqual({ foo: true, bar: true });
+  });
+});
