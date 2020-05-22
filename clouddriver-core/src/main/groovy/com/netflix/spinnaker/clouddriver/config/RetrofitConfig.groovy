9e24d5936cd721fa448057476aa1e272b73cb067
Sairam Rekapalli <srekapalli@netflix.com>
2020-05-21 22:33:08 -0700

    fix(misc): Moving to the new client provider implementation instead. (#4618)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/config/RetrofitConfig.groovy
+++ clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/config/RetrofitConfig.groovy
@@ -16,20 +16,19 @@
 
 package com.netflix.spinnaker.clouddriver.config
 
+import com.jakewharton.retrofit.Ok3Client
 import com.netflix.spinnaker.clouddriver.core.Front50ConfigurationProperties
 import com.netflix.spinnaker.clouddriver.core.services.Front50Service
 import com.netflix.spinnaker.clouddriver.exceptions.SpinnakerRetrofitErrorHandler
-import com.netflix.spinnaker.config.OkHttpClientConfiguration
+import com.netflix.spinnaker.config.DefaultServiceEndpoint
+import com.netflix.spinnaker.config.okhttp3.OkHttpClientProvider
 import com.netflix.spinnaker.retrofit.Slf4jRetrofitLogger
-import org.springframework.beans.factory.config.ConfigurableBeanFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Scope
 import retrofit.RequestInterceptor
 import retrofit.RestAdapter
-import retrofit.client.OkClient
 import retrofit.converter.JacksonConverter
 
 import static retrofit.Endpoints.newFixedEndpoint
@@ -39,20 +38,13 @@ import static retrofit.Endpoints.newFixedEndpoint
 class RetrofitConfig {
 
   @Bean
-  @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-  OkClient okClient(OkHttpClientConfiguration okHttpClientConfiguration) {
-    def client = okHttpClientConfiguration.create()
-    return new OkClient(client)
-  }
-
-  @Bean
   @ConditionalOnProperty(name = 'services.front50.enabled', matchIfMissing = true)
-  Front50Service front50Service(Front50ConfigurationProperties front50ConfigurationProperties, RestAdapter.LogLevel retrofitLogLevel, OkClient okClient, RequestInterceptor spinnakerRequestInterceptor) {
+  Front50Service front50Service(Front50ConfigurationProperties front50ConfigurationProperties, RestAdapter.LogLevel retrofitLogLevel, OkHttpClientProvider clientProvider, RequestInterceptor spinnakerRequestInterceptor) {
     def endpoint = newFixedEndpoint(front50ConfigurationProperties.baseUrl)
     new RestAdapter.Builder()
       .setRequestInterceptor(spinnakerRequestInterceptor)
       .setEndpoint(endpoint)
-      .setClient(okClient)
+      .setClient(new Ok3Client(clientProvider.getClient(new DefaultServiceEndpoint("front50", endpoint.getUrl()))))
       .setConverter(new JacksonConverter())
       .setLogLevel(retrofitLogLevel)
       .setLog(new Slf4jRetrofitLogger(Front50Service))
