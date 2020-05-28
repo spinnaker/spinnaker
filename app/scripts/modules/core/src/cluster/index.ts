8f9d4f4c176832ff766b8ea6678cd2f2082c4d00
caseyhebebrand <caseyhebebrand@gmail.com>
2020-05-28 14:21:38 -0700

    fix(core): Export filter search component (#8313)

--- app/scripts/modules/core/src/cluster/index.ts
+++ app/scripts/modules/core/src/cluster/index.ts
@@ -3,4 +3,5 @@ export * from './ClusterRuleMatcher';
 export * from './cluster.service';
 export * from './task.matcher';
 export * from './filter/ClusterFilterService';
+export * from './filter/FilterSearch';
 export * from './filter/FilterSection';
