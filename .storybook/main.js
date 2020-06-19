c72e22434f499e6b835fc533eb9d715590236169
Vignesh Murugesan <vmurugesan@netflix.com>
2020-06-19 12:17:58 -0700

    feat(storybook): Add storybook support (#8360)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+const deckWebpackConfigurer = require('../webpack.config');
+
+module.exports = {
+  addons: ['@storybook/preset-typescript'],
+  stories: ['../app/scripts/modules/core/src/presentation/**/*.stories.[tj]sx'],
+  webpackFinal: async config => {
+    const deckWebpackConfig = deckWebpackConfigurer({}, {});
+
+    config.resolve = {
+      ...config.resolve,
+      alias: deckWebpackConfig.resolve.alias,
+      extensions: deckWebpackConfig.resolve.extensions,
+      modules: deckWebpackConfig.resolve.modules,
+    };
+
+    config.module.rules = deckWebpackConfig.module.rules.map(rule => {
+      // There are some issues with thread-loader and storybook, so disabling it until it gets fixed.
+      rule.use = (rule.use || []).filter(({ loader }) => loader !== 'thread-loader');
+      return rule;
+    });
+
+    config.watch = true;
+
+    return config;
+  },
+};
--- app/scripts/modules/core/src/config/settings.ts
+++ app/scripts/modules/core/src/config/settings.ts
@@ -127,7 +127,7 @@ export interface ISpinnakerSettings {
   useClassicFirewallLabels: boolean;
 }
 
-export const SETTINGS: ISpinnakerSettings = (window as any).spinnakerSettings;
+export const SETTINGS: ISpinnakerSettings = (window as any).spinnakerSettings || {};
 
 // Make sure to set up some reasonable default settings fields so we do not have to keep checking if they exist everywhere
 SETTINGS.feature = SETTINGS.feature || {};
