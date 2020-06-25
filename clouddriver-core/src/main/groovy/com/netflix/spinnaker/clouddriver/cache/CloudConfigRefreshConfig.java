61a475e32f6ed3972ef2ae34d1ed9cc2432210d9
clanesf <35847974+clanesf@users.noreply.github.com>
2020-06-24 20:32:21 -0400

    feat(cloudconfig): Externalize cloud config refresh interval and decouple from cache refresh interval (#4699)
    * chore(cloudconfig): Decouple cloud config refresh interval from cache refresh interval
    
    * fix(cloudconfig): fix annotations per scott fredrick
    
    * fix(cloudconfig): add EnableConfigProperties to CloudConfigRefreshConfig class per feedback
    
    * refactor(cloud config): Adjusted based on feedback from Nicolas and Scott Frederick
    
    * refactor(cloudconfig): remove AutoConfigure annotation for RedisConfig as it's no longer used.
    
    * refactor(cloudconfig): Removed default as a default now exists in property file
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/cache/CloudConfigRefreshConfig.java
+++ clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/cache/CloudConfigRefreshConfig.java
@@ -16,40 +16,38 @@
 
 package com.netflix.spinnaker.clouddriver.cache;
 
-import com.netflix.spinnaker.cats.cluster.DefaultAgentIntervalProvider;
+import com.netflix.spinnaker.clouddriver.config.CloudConfigRefreshProperties;
 import com.netflix.spinnaker.clouddriver.refresh.CloudConfigRefreshScheduler;
 import com.netflix.spinnaker.kork.configserver.autoconfig.RemoteConfigSourceConfigured;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.refresh.ContextRefresher;
 import org.springframework.context.annotation.*;
 
 /**
  * Create a {@link CloudConfigRefreshScheduler} to refresh the Spring Cloud Config Server from an
- * environment repository backend on a schedule that matches the cache refresh schedule.
+ * environment repository backend on a schedule that can be defined in the spring profile or has a
+ * sensible default (60 seconds)..
  */
 @Configuration
-@AutoConfigureAfter({RedisCacheConfig.class})
+@EnableConfigurationProperties(CloudConfigRefreshProperties.class)
 public class CloudConfigRefreshConfig {
 
   @Configuration
   @Conditional(RemoteConfigSourceConfigured.class)
   @EnableConfigServer
   static class RemoteConfigSourceConfiguration {
-    @Bean
-    @ConditionalOnBean(DefaultAgentIntervalProvider.class)
-    public CloudConfigRefreshScheduler intervalProviderConfigRefreshScheduler(
-        ContextRefresher contextRefresher, DefaultAgentIntervalProvider agentIntervalProvider) {
-      return new CloudConfigRefreshScheduler(contextRefresher, agentIntervalProvider.getInterval());
-    }
 
     @Bean
-    @ConditionalOnMissingBean(DefaultAgentIntervalProvider.class)
-    public CloudConfigRefreshScheduler defaultIntervalConfigRefreshScheduler(
-        ContextRefresher contextRefresher) {
-      return new CloudConfigRefreshScheduler(contextRefresher, 60);
+    @ConditionalOnProperty(
+        prefix = "cloud.config",
+        value = "refreshIntervalSeconds",
+        matchIfMissing = true)
+    public CloudConfigRefreshScheduler cloudConfigIntervalRefreshScheduler(
+        ContextRefresher contextRefresher, CloudConfigRefreshProperties cloudConfigProperties) {
+      return new CloudConfigRefreshScheduler(
+          contextRefresher, cloudConfigProperties.getRefreshIntervalSeconds());
     }
   }
 }
