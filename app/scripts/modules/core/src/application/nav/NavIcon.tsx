ff4dc09bec11e873f0c9ad21b0341d23437ecc25
caseyhebebrand <caseyhebebrand@gmail.com>
2020-08-06 10:36:53 -0700

    chore(core): Remove outdated nav components (#8473)
    * chore(core): Remove outdated nav components
    
    * chore(core): Remove unused refresher css

--- app/scripts/modules/core/src/application/nav/NavIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export interface INavIconProps {
-  icon: string;
-}
-
-export const NavIcon = ({ icon }: INavIconProps) => {
-  return icon ? <i className={`nav-item-icon ${icon}`} /> : null;
-};
