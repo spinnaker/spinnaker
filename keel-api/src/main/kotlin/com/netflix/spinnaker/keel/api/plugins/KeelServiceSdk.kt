80b7e2ffdb155672db77930a0b33a73eb5a1b70e
Luis Pollo <1323478+luispollo@users.noreply.github.com>
2020-08-07 18:13:04 -0700

    fix(plugins): Add marker interface for KeelServiceSdk (#1418)

--- keel-api/src/main/kotlin/com/netflix/spinnaker/keel/api/plugins/KeelServiceSdk.kt
+++ keel-api/src/main/kotlin/com/netflix/spinnaker/keel/api/plugins/KeelServiceSdk.kt
@@ -2,11 +2,12 @@ package com.netflix.spinnaker.keel.api.plugins
 
 import com.netflix.spinnaker.keel.api.actuation.TaskLauncher
 import com.netflix.spinnaker.keel.api.persistence.KeelReadOnlyRepository
+import com.netflix.spinnaker.kork.plugins.api.servicesdk.ServiceSdk
 
 /**
  * A simple SDK that can be consumed by external plugins to access core Keel functionality.
  */
-interface KeelServiceSdk {
+interface KeelServiceSdk : ServiceSdk {
   val repository: KeelReadOnlyRepository
   val taskLauncher: TaskLauncher
 }
