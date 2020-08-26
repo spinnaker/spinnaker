9b9faac48cda1e8e34214441b779534367d593b1
Rob Zienert <rzienert@netflix.com>
2020-08-26 11:42:36 -0700

    fix(core): Brute-force fix NPE in DeployHandlerRegistry (#4838)

--- clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/deploy/DefaultDeployHandlerRegistry.java
+++ clouddriver-core/src/main/groovy/com/netflix/spinnaker/clouddriver/deploy/DefaultDeployHandlerRegistry.java
@@ -2,26 +2,30 @@ package com.netflix.spinnaker.clouddriver.deploy;
 
 import static java.lang.String.format;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class DefaultDeployHandlerRegistry implements DeployHandlerRegistry {
 
   private List<DeployHandler> deployHandlers;
 
+  @Autowired
   public DefaultDeployHandlerRegistry(List<DeployHandler> deployHandlers) {
     this.deployHandlers = deployHandlers;
   }
 
   @Override
   public DeployHandler findHandler(final DeployDescription description) {
-    return deployHandlers.stream()
-        .filter(it -> it.handles(description))
+    return Optional.ofNullable(deployHandlers).orElseGet(ArrayList::new).stream()
+        .filter(it -> it != null && it.handles(description))
         .findFirst()
         .orElseThrow(
             () ->
                 new DeployHandlerNotFoundException(
                     format(
-                        "No handler found supportign %s", description.getClass().getSimpleName())));
+                        "No handler found supporting %s", description.getClass().getSimpleName())));
   }
 
   public List<DeployHandler> getDeployHandlers() {
