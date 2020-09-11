0168e53f528b5039519afb951f5c74a23d681343
Adam Jordens <adam@jordens.org>
2020-09-10 18:06:24 -0700

    fix(proxy): Take advantage of `InsecureOkHttpClientBuilderProvider` (#1336)
    The `DefaultOkHttpClientBuilderProvider` (previous behavior) will actually
    utilize the `okHttpClient.*` config properties and unnecessarily set
    keystore/truststores.
    
    This is fine when communicating with other Spinnaker services but problematic
    externally. The `InsecureOkHttpClientBuilderProvider` loosens the certificate
    chain validations.

--- gate-proxy/src/main/kotlin/com/netflix/spinnaker/gate/controllers/Proxy.kt
+++ gate-proxy/src/main/kotlin/com/netflix/spinnaker/gate/controllers/Proxy.kt
@@ -35,7 +35,7 @@ internal class Proxy(val config: ProxyConfig) {
    */
   fun init(okHttpClientProvider: OkHttpClientProvider) : Proxy {
     val okHttpClient = okHttpClientProvider.getClient(DefaultServiceEndpoint(
-      "proxy__${config.id}", config.uri, config.additionalAttributes
+      "proxy__${config.id}", config.uri, config.additionalAttributes, false
     ))
 
     this.okHttpClient = okHttpClient
