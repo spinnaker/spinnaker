07c7a9667bcbf42a2047c4c605f6138bf94a8491
Chris Thielen <github@sandgnat.com>
2020-09-15 19:23:17 -0700

    feat(pluginsdk): Update dependencies and peerDependencies

--- packages/pluginsdk/pluginconfig/rollup.config.js
+++ packages/pluginsdk/pluginconfig/rollup.config.js
@@ -1,4 +1,4 @@
-const nodeResolve = require('@rollup/plugin-node-resolve');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const typescript = require('@rollup/plugin-typescript');
 const postCss = require('rollup-plugin-postcss');
