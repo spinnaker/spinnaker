519dfc010598de6428b833f1a63e2ee12baaf4f2
Vignesh Murugesan <vmurugesan@netflix.com>
2020-09-23 10:53:53 -0700

    feat(storybook/SpinFormik): Create a story for SpinFormik

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 const deckWebpackConfigurer = require('../webpack.config');
+const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
 
 module.exports = {
-  addons: ['@storybook/preset-typescript'],
-  stories: ['../app/scripts/modules/core/src/presentation/**/*.stories.[tj]sx'],
-  webpackFinal: async config => {
+  addons: ['@storybook/preset-typescript', '@storybook/addon-essentials'],
+  stories: [
+    '../app/scripts/modules/core/src/presentation/**/*.stories.[tj]sx',
+    '../app/scripts/modules/core/src/presentation/**/*.stories.mdx',
+  ],
+  webpackFinal: async (config) => {
     const deckWebpackConfig = deckWebpackConfigurer({}, {});
 
     config.resolve = {
@@ -13,11 +17,20 @@ module.exports = {
       modules: ['node_modules'],
     };
 
-    config.module.rules = deckWebpackConfig.module.rules.map(rule => {
-      // There are some issues with thread-loader and storybook, so disabling it until it gets fixed.
-      rule.use = (rule.use || []).filter(({ loader }) => loader !== 'thread-loader');
-      return rule;
-    });
+    config.module.rules = [
+      ...deckWebpackConfig.module.rules.map((rule) => {
+        // There are some issues with thread-loader and storybook, so disabling it until it gets fixed.
+        rule.use = (rule.use || []).filter(({ loader }) => loader !== 'thread-loader');
+        return rule;
+      }),
+      {
+        test: /\.mdx?$/,
+        use: [
+          { loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-react'] } },
+          { loader: '@mdx-js/loader', options: { compilers: [createCompiler()] } },
+        ],
+      },
+    ];
 
     config.watch = true;
 
