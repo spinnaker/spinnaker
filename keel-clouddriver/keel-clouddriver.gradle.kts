129e88f034666f6583b95f5d3a50de248355669f
Michael Plump <plumpy@google.com>
2020-10-08 12:23:06 -0400

    chore(kork): update kork dependency (now w/ moniker) (#1581)

--- keel-clouddriver/keel-clouddriver.gradle.kts
+++ keel-clouddriver/keel-clouddriver.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
   implementation(project(":keel-retrofit"))
   implementation(project(":keel-core"))
-  implementation("com.netflix.spinnaker.moniker:moniker")
+  implementation("com.netflix.spinnaker.kork:kork-moniker")
   implementation("com.fasterxml.jackson.core:jackson-databind")
   implementation("com.fasterxml.jackson.core:jackson-annotations")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
