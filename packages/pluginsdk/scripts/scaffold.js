5c84d68e281ec7694793d380274bc499b13e72d0
Chris Thielen <christopherthielen@users.noreply.github.com>
2020-10-12 09:00:47 -0700

    feat(pluginsdk): Add cli flags to scaffold.js to allow non-interactive scaffolding (#8636)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/pluginsdk/scripts/scaffold.js
+++ packages/pluginsdk/scripts/scaffold.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* @ts-check */
 /* eslint-disable no-console */
 
 const path = require('path');
@@ -6,20 +7,29 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const readlineSync = require('readline-sync');
 
+const yargs = require('yargs')
+  .option('directory', {
+    type: 'string',
+  })
+  .option('name', {
+    type: 'string',
+  }).argv;
+
 const pluginNameQuestion = 'Enter the short name for your plugin (default: myplugin): ';
-const pluginName = readlineSync.question(pluginNameQuestion, { defaultInput: 'myplugin' });
+const pluginName = yargs.name || readlineSync.question(pluginNameQuestion, { defaultInput: 'myplugin' });
 
 const dirQuestion = `Directory to scaffold into (default: ${pluginName}-deck): `;
-const scaffoldTargetDir = readlineSync.question(dirQuestion, { defaultInput: `${pluginName}-deck` });
+const scaffoldTargetDir = yargs.directory || readlineSync.question(dirQuestion, { defaultInput: `${pluginName}-deck` });
+const basename = path.basename(path.resolve(scaffoldTargetDir));
 
 // Rename these files
 const fileMapping = {
   'scaffold.tsconfig.json': 'tsconfig.json',
   'scaffold.prettierrc.js': '.prettierrc.js',
-  'scaffold-deck.gradle': `${scaffoldTargetDir}.gradle`,
+  'scaffold-deck.gradle': `${basename}.gradle`,
 };
 
-const renamedFile = file => fileMapping[file] || file;
+const renamedFile = (file) => fileMapping[file] || file;
 
 function mkdirIfNotExists(dirPath) {
   if (!fs.existsSync(dirPath)) {
@@ -30,8 +40,8 @@ function mkdirIfNotExists(dirPath) {
 function recursiveCopy(srcDir, targetDir) {
   const contents = fs.readdirSync(srcDir);
   contents
-    .filter(x => x !== '.' && x !== '..')
-    .forEach(file => {
+    .filter((x) => x !== '.' && x !== '..')
+    .forEach((file) => {
       const srcPath = path.resolve(srcDir, file);
       const targetPath = path.resolve(targetDir, renamedFile(file));
       const stat = fs.statSync(srcPath);
@@ -44,11 +54,11 @@ function recursiveCopy(srcDir, targetDir) {
     });
 }
 
-function updatePackageJson(pkgJsonPath, scaffoldPath) {
+function updatePackageJson(pkgJsonPath, basename) {
   const pluginSdkPackageContents = fs.readFileSync(path.resolve(__dirname, '..', 'package.json')).toString();
   const pluginSdkVersion = JSON.parse(pluginSdkPackageContents).version;
   const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath).toString());
-  pkgJson.name = path.basename(scaffoldPath);
+  pkgJson.name = basename;
   pkgJson.dependencies['@spinnaker/pluginsdk'] = pluginSdkVersion;
   pkgJson.files = ['build/dist'];
   fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
@@ -56,7 +66,7 @@ function updatePackageJson(pkgJsonPath, scaffoldPath) {
 
 mkdirIfNotExists(scaffoldTargetDir);
 recursiveCopy(path.resolve(__dirname, '..', 'scaffold'), scaffoldTargetDir);
-updatePackageJson(path.resolve(scaffoldTargetDir, 'package.json'), scaffoldTargetDir);
+updatePackageJson(path.resolve(scaffoldTargetDir, 'package.json'), basename);
 
 console.log(`Deck plugin scaffolded into ${scaffoldTargetDir}`);
 console.log(`Installing dependencies using 'yarn' and 'npx check-peer-dependencies --install' ...`);
