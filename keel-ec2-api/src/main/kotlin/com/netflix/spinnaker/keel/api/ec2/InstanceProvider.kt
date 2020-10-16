71b73a5139967e748674832117380999f80962f4
Rob Fletcher <robfletcher@users.noreply.github.com>
2020-10-16 09:49:14 -0700

    fix(plugins): instance provider was missing annotation (#1604)

--- keel-ec2-api/src/main/kotlin/com/netflix/spinnaker/keel/api/ec2/InstanceProvider.kt
+++ keel-ec2-api/src/main/kotlin/com/netflix/spinnaker/keel/api/ec2/InstanceProvider.kt
@@ -1,9 +1,12 @@
 package com.netflix.spinnaker.keel.api.ec2
 
+import com.netflix.spinnaker.keel.api.schema.Discriminator
+
 /**
  * Extensible mechanism allowing for abstraction of details of the VM instance (e.g.
  * [LaunchConfigurationSpec.instanceType], [LaunchConfigurationSpec.ebsOptimized], etc.)
  */
 interface InstanceProvider {
+  @Discriminator
   val type: String
 }
