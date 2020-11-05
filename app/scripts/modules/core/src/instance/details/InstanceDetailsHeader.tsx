37483823071dfbc76491b77004c5435479207cc6
caseyhebebrand <caseyhebebrand@gmail.com>
2020-11-05 09:32:27 -0800

    refactor(core/instance): Create generic header for instance details panel (#8706)
    * refactor(core/instance): Create generic header for instance details panel
    
    * refactor(core/instance): Include health state
    
    * refactor(core/instance): Add header to all cloudproviders
    
    refactor(core/instance): Add header to all cloudproviders

---
+++ app/scripts/modules/core/src/instance/details/InstanceDetailsHeader.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { UISref } from '@uirouter/react';
+import { UIRouterContextComponent } from '@uirouter/react-hybrid';
+import { Spinner } from 'core/widgets/spinners/Spinner';
+import { CopyToClipboard } from 'core/utils/clipboard/CopyToClipboard';
+import { CloudProviderLogo } from 'core/cloudProvider/CloudProviderLogo';
+
+import './InstanceDetailsHeader.less';
+
+export interface IInstanceDetailsHeaderProps {
+  cloudProvider?: string;
+  healthState: string;
+  instanceId: string;
+  loading: boolean;
+  standalone: boolean;
+}
+export const InstanceDetailsHeader = ({
+  cloudProvider,
+  healthState,
+  instanceId,
+  loading,
+  standalone,
+}: IInstanceDetailsHeaderProps) => (
+  <div className="InstanceDetailsHeader">
+    {!standalone && (
+      <div className="close-button">
+        <UIRouterContextComponent>
+          <UISref to="^">
+            <a className="btn btn-link">
+              <span className="glyphicon glyphicon-remove"></span>
+            </a>
+          </UISref>
+        </UIRouterContextComponent>
+      </div>
+    )}
+    {loading && (
+      <div className="horizontal center spinner-container">
+        <Spinner size="small" />
+      </div>
+    )}
+    {!loading && (
+      <div className="header-text horizontal middle">
+        {cloudProvider && <CloudProviderLogo provider={cloudProvider} height="36px" width="36px" />}
+        {!cloudProvider && <span className={`glyphicon glyphicon-hdd ${healthState}`}></span>}
+        <h3 className="horizontal middle space-between flex-1">{instanceId}</h3>
+        <CopyToClipboard text={instanceId} toolTip="Copy to clipboard" />
+      </div>
+    )}
+  </div>
+);
---
+++ app/scripts/modules/core/src/instance/details/instanceDetailsHeader.component.ts
@@ -0,0 +1,18 @@
+import { withErrorBoundary } from 'core/presentation/SpinErrorBoundary';
+import { module } from 'angular';
+import { react2angular } from 'react2angular';
+import { InstanceDetailsHeader } from './InstanceDetailsHeader';
+
+export const CORE_INSTANCE_DETAILS_HEADER_COMPONENT = 'spinnaker.core.instance.details.header';
+export const name = CORE_INSTANCE_DETAILS_HEADER_COMPONENT;
+
+module(name, []).component(
+  'instanceDetailsHeader',
+  react2angular(withErrorBoundary(InstanceDetailsHeader, 'instanceDetailsHeader'), [
+    'cloudProvider',
+    'healthState',
+    'instanceId',
+    'loading',
+    'standalone',
+  ]),
+);
