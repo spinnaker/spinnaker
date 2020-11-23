0cdf8369f4156e09ebcab8f097ec76e2b713936b
caseyhebebrand <caseyhebebrand@gmail.com>
2020-11-23 10:42:29 -0800

    chore(core): Upload new illustrations (#8744)

--- app/scripts/modules/core/src/presentation/illustrations/illustrationsByName.ts
+++ app/scripts/modules/core/src/presentation/illustrations/illustrationsByName.ts
@@ -1,18 +1,24 @@
+import { ReactComponent as alerts } from './vectors/alerts.svg';
 import { ReactComponent as announcement } from './vectors/announcement.svg';
 import { ReactComponent as appSynced } from './vectors/appSynced.svg';
 import { ReactComponent as appUnsynced } from './vectors/appUnsynced.svg';
+import { ReactComponent as dashboards } from './vectors/dashboards.svg';
 import { ReactComponent as disableManagement } from './vectors/disableManagement.svg';
 import { ReactComponent as markArtifactVersionAsBad } from './vectors/markArtifactVersionAsBad.svg';
+import { ReactComponent as metrics } from './vectors/metrics.svg';
 import { ReactComponent as pinArtifactVersion } from './vectors/pinArtifactVersion.svg';
 import { ReactComponent as runManagement } from './vectors/runManagement.svg';
 import { ReactComponent as unpinArtifactVersion } from './vectors/unpinArtifactVersion.svg';
 
 export const illustrationsByName = {
+  alerts,
   announcement,
   appSynced,
   appUnsynced,
+  dashboards,
   disableManagement,
   markArtifactVersionAsBad,
+  metrics,
   pinArtifactVersion,
   runManagement,
   unpinArtifactVersion,
