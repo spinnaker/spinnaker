f1029669ac91c4f2b0c859a66debdf49d79b7cc5
caseyhebebrand <caseyhebebrand@gmail.com>
2020-11-24 11:26:25 -0800

    chore(core): Export all generic instance components (#8755)

--- app/scripts/modules/core/src/instance/details/index.ts
+++ app/scripts/modules/core/src/instance/details/index.ts
@@ -1,2 +1,6 @@
+export * from './InstanceActions';
 export * from './InstanceDetails';
+export * from './InstanceDetailsHeader';
 export * from './InstanceDetailsPane';
+export * from './InstanceInsights';
+export * from './InstanceLinks';
