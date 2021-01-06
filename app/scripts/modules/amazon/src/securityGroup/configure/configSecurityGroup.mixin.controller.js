6904cb064db6733db513f44147a6344dc1cf89da
caseyhebebrand <caseyhebebrand@gmail.com>
2021-01-06 10:03:57 -0800

    fix(amazon): Security group names for standalone apps render incorrectly (#8816)

--- app/scripts/modules/amazon/src/securityGroup/configure/configSecurityGroup.mixin.controller.js
+++ app/scripts/modules/amazon/src/securityGroup/configure/configSecurityGroup.mixin.controller.js
@@ -322,7 +322,8 @@ module(AMAZON_SECURITYGROUP_CONFIGURE_CONFIGSECURITYGROUP_MIXIN_CONTROLLER, [
 
     ctrl.updateName = function () {
       const { securityGroup } = $scope;
-      const name = NameUtils.getClusterName(application.name, securityGroup.stack, securityGroup.detail);
+      const appName = application.isStandalone ? application.name.split('-')[0] : application.name;
+      const name = NameUtils.getClusterName(appName, securityGroup.stack, securityGroup.detail);
       securityGroup.name = name;
       $scope.namePreview = name;
     };
