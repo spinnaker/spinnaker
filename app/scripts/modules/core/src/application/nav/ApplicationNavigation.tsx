b3ae753aab84e0c471355d1ee5ac69f572b81808
Chris Berry <73450+anotherchrisberry@users.noreply.github.com>
2021-02-01 09:40:12 -0800

    fix(core): add vertical padding on page owner nav link (#8876)

--- app/scripts/modules/core/src/application/nav/ApplicationNavigation.tsx
+++ app/scripts/modules/core/src/application/nav/ApplicationNavigation.tsx
@@ -75,8 +75,11 @@ export const ApplicationNavigation = ({ app }: IApplicationNavigationProps) => {
           <NavSection key={`section-${i}`} dataSources={section} app={app} />
         ))}
       {SETTINGS.feature.pagerDuty && app.attributes.pdApiKey && (
-        <div className="nav-section clickable">
-          <div className="page-category flex-container-h middle text-semibold" onClick={pageApplicationOwner}>
+        <div className="nav-section sp-padding-s-yaxis">
+          <div
+            className="page-category flex-container-h middle text-semibold sp-padding-s-yaxis clickable"
+            onClick={pageApplicationOwner}
+          >
             <div className="nav-row-item sp-margin-s-right">
               {!isExpanded ? (
                 <Tooltip value="Page App Owner" placement="right">
