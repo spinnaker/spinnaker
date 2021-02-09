0a109843fd8c8f4d4ba24d9d333a5b998ee268d9
Karl <karl.skewes@gmail.com>
2021-02-09 17:53:40 +1300

    feat(packer): Update version to support AWS assume_role (#764)
    Co-authored-by: Cameron Motevasselani <cmotevasselani@gmail.com>

--- rosco-web/pkg_scripts/postInstall.sh
+++ rosco-web/pkg_scripts/postInstall.sh
@@ -12,7 +12,7 @@ if [ -z `getent passwd spinnaker` ]; then
 fi
 
 install_packer() {
-  PACKER_VERSION="1.4.5"
+  PACKER_VERSION="1.6.6"
   local packer_version=$(/usr/bin/packer --version)
   local packer_status=$?
   if [ $packer_status -ne 0 ] || [ "$packer_version" != "$PACKER_VERSION" ]; then
