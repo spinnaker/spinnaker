765499fdadec89131359bc089e4a9c090368eb14
Alan Quach <aquach@netflix.com>
2021-03-03 16:44:49 -0800

    fix(pipelines): Add deps to breadcrumbs useMemo (#8967)

--- app/scripts/modules/core/src/pipeline/executions/execution/ExecutionBreadcrumbs.tsx
+++ app/scripts/modules/core/src/pipeline/executions/execution/ExecutionBreadcrumbs.tsx
@@ -16,7 +16,7 @@ export const ExecutionBreadcrumbs = ({ execution }: IExecutionBreadcrumbsProps) 
       .getAllParentExecutions(execution)
       .filter((x) => x !== execution)
       .reverse();
-  }, []);
+  }, [execution]);
 
   const label = parentExecutions.length === 1 ? 'Parent Execution' : 'Parent Executions';
 
