c3af1842fbef73d3075a281520d2b832fe69b2ef
Chris Thielen <christopherthielen@users.noreply.github.com>
2021-03-05 09:42:03 -1000

    feat(core/application): Export the ApplicationContext values (#8977)

--- app/scripts/modules/core/src/application/ApplicationContext.tsx
+++ app/scripts/modules/core/src/application/ApplicationContext.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Application } from '.';
+import { Application } from './application.model';
 
 export const ApplicationContext = React.createContext<Application | undefined>(undefined);
 
