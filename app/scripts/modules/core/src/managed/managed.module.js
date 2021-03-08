b175773778f1777aba1f05bdaed5b054e3de8fcf
Rani <ranihorev@gmail.com>
2021-03-08 12:46:40 -0800

    fix(pr): remove config call
    Co-authored-by: Chris Thielen <christopherthielen@users.noreply.github.com>

--- app/scripts/modules/core/src/managed/managed.module.js
+++ app/scripts/modules/core/src/managed/managed.module.js
@@ -5,4 +5,4 @@ import { module } from 'angular';
 import { MANAGED_STATES } from './managed.states';
 
 export const CORE_MANAGED_MANAGED_MODULE = 'spinnaker.managed';
-module(CORE_MANAGED_MANAGED_MODULE, [MANAGED_STATES]).config(() => {});
+module(CORE_MANAGED_MANAGED_MODULE, [MANAGED_STATES]);
