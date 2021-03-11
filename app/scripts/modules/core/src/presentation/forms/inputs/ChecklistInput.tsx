e03cf672fec668d9b3b36a74cb23f64bc7f73d46
Chris Thielen <christopherthielen@users.noreply.github.com>
2021-03-11 09:38:37 -1000

    fix(core/presentation): Avoid remounting CheckListInput on every render (#8971)
    * fix(core/presentation): Avoid remounting CheckListInput on every render
    
    This creates a stable callback for each nested component which has
    access to the latest closure variables.
    I don't like this solution, but it "works".
    
    * refactor(core/presentation): remove nested components in ChecklistInput
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- app/scripts/modules/core/src/presentation/forms/inputs/ChecklistInput.tsx
+++ app/scripts/modules/core/src/presentation/forms/inputs/ChecklistInput.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { IFormInputProps, OmitControlledInputPropsFrom } from './interface';
 
+import { CheckboxInput } from './CheckboxInput';
+import { IFormInputProps, OmitControlledInputPropsFrom } from './interface';
 import { createFakeReactSyntheticEvent, isStringArray, orEmptyString, validationClassName } from './utils';
+
+import './ChecklistInput.less';
 
 const { useEffect } = React;
 
@@ -18,108 +21,61 @@ export interface IChecklistInputOption {
 }
 
 export function ChecklistInput(props: IChecklistInputProps) {
-  const {
-    inline,
-    showSelectAll,
-    value,
-    validation,
-    inputClassName,
-    options,
-    stringOptions,
-    onChange,
-    ...otherProps
-  } = props;
+  const { inline, showSelectAll, value, validation, inputClassName, options, stringOptions, onChange } = props;
 
   // Naively call the the field's onBlur handler
   // This is what Formik uses to mark the field as touched
   function touchField() {
     props.onBlur && props.onBlur(createFakeReactSyntheticEvent({ name: props.name, value }));
   }
+
   useEffect(touchField, []);
-
-  const className = `${orEmptyString(inputClassName)} ${validationClassName(validation)}`;
-
-  const selectedValues = value || [];
-  const isChecked = (checkboxValue: any) => selectedValues.includes(checkboxValue);
 
   const checkListOptions = isStringArray(stringOptions)
     ? stringOptions.map((s) => ({ label: s, value: s }))
     : options || [];
 
-  const labelClassName = !!inline ? 'clickable checkbox-inline' : 'clickable';
+  const selectedValues: string[] = value || [];
+  const updateSelectedValues = (selectedValues: string[]) => {
+    onChange(createFakeReactSyntheticEvent({ value: selectedValues, name: props.name }));
+  };
 
-  function CheckBox(checkboxProps: { option: IChecklistInputOption }) {
-    const { option } = checkboxProps;
+  const isChecked = (checkboxValue: any) => selectedValues.includes(checkboxValue);
 
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-      const selected = e.target.value;
-      const alreadyHasValue = isChecked(selected);
-      const newValue = !alreadyHasValue ? selectedValues.concat(selected) : value.filter((v: string) => v !== selected);
-      onChange(createFakeReactSyntheticEvent({ value: newValue, name: e.target.name }));
-    }
+  const toggleValue = (value: string) => {
+    const newValues = isChecked(value) ? selectedValues.filter((x) => x !== value) : selectedValues.concat(value);
+    onChange(createFakeReactSyntheticEvent({ value: newValues, name: props.name }));
+  };
 
-    return (
-      <label className={labelClassName} key={option.value}>
-        <input
-          className={className}
-          type="checkbox"
-          value={option.value}
-          onChange={handleChange}
-          checked={isChecked(option.value)}
-          {...otherProps}
-        />
-        {option.label}
-      </label>
-    );
-  }
+  const allSelected = checkListOptions.every((option) => isChecked(option.value));
+  const selectNone = () => updateSelectedValues([]);
+  const selectAll = () => updateSelectedValues(checkListOptions.map((o) => o.value));
 
-  function SelectAllButton() {
-    const allSelected = checkListOptions.every((option) => isChecked(option.value));
-    const anchorClassName = `btn btn-default btn-xs ${inline ? '' : 'push-left'}`;
-    const style = inline ? { margin: '8px 0 0 10px' } : {};
+  const checklistClassName = `ChecklistInput ${inline ? 'ChecklistInput_inline' : ''}`;
+  const className = `${checklistClassName} }${orEmptyString(inputClassName)} ${validationClassName(validation)}`;
 
-    const selectValue = (selected: string[]) => {
-      return onChange(createFakeReactSyntheticEvent({ name: props.name, value: selected }));
-    };
-    const selectNone = () => selectValue([]);
-    const selectAll = () => selectValue(checkListOptions.map((o) => o.value));
-
-    return (
-      <a className={anchorClassName} style={style} type="button" onClick={allSelected ? selectNone : selectAll}>
-        {allSelected ? 'Deselect All' : 'Select All'}
-      </a>
-    );
-  }
-
-  function InlineOptions() {
-    return (
-      <>
-        {checkListOptions.map((option) => (
-          <CheckBox key={option.label} option={option} />
-        ))}
-
-        {showSelectAll && checkListOptions.length > 1 && <SelectAllButton />}
-      </>
-    );
-  }
-
-  function VerticalOptions() {
-    return (
+  return (
+    <div className={className}>
       <ul className="checklist">
         {checkListOptions.map((option) => (
           <li key={option.label}>
-            <CheckBox option={option} />
+            <CheckboxInput
+              text={option.label}
+              value={option.value}
+              onChange={() => toggleValue(option.value)}
+              checked={isChecked(option.value)}
+            />
           </li>
         ))}
 
         {showSelectAll && checkListOptions.length > 1 && (
           <li key={'select_all_button'}>
-            <SelectAllButton />
+            <a type="button" onClick={allSelected ? selectNone : selectAll}>
+              {allSelected ? 'Deselect All' : 'Select All'}
+            </a>
           </li>
         )}
       </ul>
-    );
-  }
-
-  return <div className="checkbox">{inline ? <InlineOptions /> : <VerticalOptions />}</div>;
+    </div>
+  );
 }
