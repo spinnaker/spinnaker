47d9a38bcdff916bf85059038345ce4481d8b54a
Chris Thielen <github@sandgnat.com>
2021-03-30 15:21:49 -1000

    chore(pluginize): fix CSS imports

--- src/kayenta/layout/styleguide.tsx
+++ src/kayenta/layout/styleguide.tsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import * as React from 'react';
 
-import 'kayenta/canary.less';
+import '../canary.less';
 
 export const STYLEGUIDE_CLASS = 'styleguide';
 export const KAYENTA_CLASS = 'kayenta';
