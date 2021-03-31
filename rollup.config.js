5ecda9bb03a620faf91f05c079cc37fec026972c
Chris Thielen <github@sandgnat.com>
2021-03-30 15:25:16 -1000

    chore(pluginize): Add rollup plugins for bundle visualizer and json import

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
+const json = require('@rollup/plugin-json');
+const visualizer = require('rollup-plugin-visualizer');
 const basePluginConfig = require('@spinnaker/pluginsdk/pluginconfig/rollup.config');
+basePluginConfig.plugins.push(json(), visualizer());
 
 export default {
   ...basePluginConfig,
