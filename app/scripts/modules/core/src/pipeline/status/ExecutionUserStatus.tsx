9b08e8d192e29c7a34aa9330f819043a439d2904
Alan Quach <1633736+alanmquach@users.noreply.github.com>
2021-04-20 10:45:49 -0700

    fix(pipeline): Do not infer user from parent (#9095)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- app/scripts/modules/core/src/pipeline/status/ExecutionUserStatus.tsx
+++ app/scripts/modules/core/src/pipeline/status/ExecutionUserStatus.tsx
@@ -1,39 +1,12 @@
-import { has } from 'lodash';
 import React from 'react';
 
-import { IExecution, IExecutionTriggerStatusComponentProps, IPipelineTrigger, ITrigger } from 'core/domain';
-import { IAuthentication } from 'core/domain/IAuthentication';
+import { IExecutionTriggerStatusComponentProps } from 'core/domain';
 
 export class ExecutionUserStatus extends React.Component<IExecutionTriggerStatusComponentProps> {
   public render() {
     const { authentication, trigger } = this.props;
-    const parentExecution = has(trigger, 'parentExecution') ? (trigger as IPipelineTrigger).parentExecution : null;
-    const authenticatedUser = getAuthenticatedUser(authentication, parentExecution);
-    const triggerUser = getTriggerUser(trigger);
+    const authenticatedUser = authentication.user ?? '[anonymous]';
+    const triggerUser = trigger.user ?? 'unknown user';
     return triggerUser === authenticatedUser ? authenticatedUser : `${triggerUser} (${authenticatedUser})`;
   }
 }
-
-const getTriggerUser = (trigger: ITrigger): string => {
-  if (!trigger.user) {
-    return 'unknown user';
-  }
-  let user: string = trigger.user;
-  if (user === '[anonymous]' && has(trigger, 'parentExecution.trigger.user')) {
-    user = (trigger as IPipelineTrigger).parentExecution.trigger.user;
-  }
-  return user;
-};
-
-const getAuthenticatedUser = (authentication: IAuthentication, parentExecution?: IExecution): string => {
-  const user: string = authentication.user;
-  if (user && user !== '[anonymous]') {
-    return user;
-  } else {
-    if (has(parentExecution, 'authentication.user')) {
-      return parentExecution.authentication.user;
-    } else {
-      return 'unknown user';
-    }
-  }
-};
