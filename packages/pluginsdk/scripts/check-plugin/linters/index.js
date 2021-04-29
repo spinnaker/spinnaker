3fd03c1ed09b6ecd75677cae498bf56b3bc2f81d
Chris Thielen <github@sandgnat.com>
2021-04-28 14:56:44 -1000

    fix(pluginsdk): add lintstaged lint rule to check-plugin script

--- packages/pluginsdk/scripts/check-plugin/linters/index.js
+++ packages/pluginsdk/scripts/check-plugin/linters/index.js
@@ -1,8 +1,9 @@
-const { checkPackageJson } = require('./lint.package.json');
-const { checkTsconfig } = require('./lint.tsconfig.json');
-const { checkRollupConfig } = require('./lint.rollup.config');
-const { checkPrettierRc } = require('./lint.prettierrc');
 const { checkEslintRc } = require('./lint.eslintrc');
+const { checkLintStagedRc } = require('./lint.lintstagedrc.json');
+const { checkPackageJson } = require('./lint.package.json');
+const { checkPrettierRc } = require('./lint.prettierrc');
+const { checkRollupConfig } = require('./lint.rollup.config');
+const { checkTsconfig } = require('./lint.tsconfig.json');
 
-const linters = [checkPackageJson, checkTsconfig, checkRollupConfig, checkPrettierRc, checkEslintRc];
+const linters = [checkEslintRc, checkLintStagedRc, checkPackageJson, checkPrettierRc, checkRollupConfig, checkTsconfig];
 module.exports = { linters };
