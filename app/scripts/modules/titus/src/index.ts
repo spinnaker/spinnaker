15f4e529bb2cbae501e41a8e28c1b1eba4dfeea7
Vignesh Murugesan <vmurugesan@netflix.com>
2021-04-30 08:57:39 -0700

    refactor(titus): Make titus package independent
    This is the start of a new structure for all spinnaker packages such as `titus`, `core`, `amazon`.
    
    The overall idea is to make each package independent by making them specify their own npm dependencies
    and use `rollup` for building the package distribution. This helps us in migrating towards a
    npm-workspace-enabled mono-repo and use tools such as lerna to manage local development and package
    updates.
    
    We're introducing a new `tsconfig.base.json` file to define the base configuration. Once all the packages
    are migrated, then we can get rid of the top-level `tsconfig.json` file completely.

--- app/scripts/modules/titus/src/index.ts
+++ app/scripts/modules/titus/src/index.ts
@@ -1,3 +1,5 @@
+/// <reference path="./index.d.ts" />
+
 export { TITUS_MODULE } from './titus.module';
 
 export * from './reactShims';
--- app/scripts/modules/titus/src/titus.module.ts
+++ app/scripts/modules/titus/src/titus.module.ts
@@ -5,6 +5,7 @@ import { CloudProviderRegistry, DeploymentStrategyRegistry } from '@spinnaker/co
 
 import './help/titus.help';
 import { TitusInstanceDetails } from './instance/details/TitusInstanceDetails';
+import logo from './logo/titus.logo.png';
 import { TITUS_PIPELINE_STAGES_BAKE_TITUSBAKESTAGE } from './pipeline/stages/bake/titusBakeStage';
 import { TITUS_PIPELINE_STAGES_CLONESERVERGROUP_TITUSCLONESERVERGROUPSTAGE } from './pipeline/stages/cloneServerGroup/titusCloneServerGroupStage';
 import { TITUS_PIPELINE_STAGES_DESTROYASG_TITUSDESTROYASGSTAGE } from './pipeline/stages/destroyAsg/titusDestroyAsgStage';
@@ -28,12 +29,6 @@ import { TITUS_SERVERGROUP_SERVERGROUP_TRANSFORMER } from './serverGroup/serverG
 import './validation/ApplicationNameValidator';
 
 import './logo/titus.logo.less';
-
-// load all templates into the $templateCache
-const templates = require.context('./', true, /\.html$/);
-templates.keys().forEach(function (key) {
-  templates(key);
-});
 
 export const TITUS_MODULE = 'spinnaker.titus';
 module(TITUS_MODULE, [
@@ -59,7 +54,7 @@ module(TITUS_MODULE, [
   CloudProviderRegistry.registerProvider('titus', {
     name: 'Titus',
     logo: {
-      path: require('./logo/titus.logo.png'),
+      path: logo,
     },
     serverGroup: {
       transformer: 'titusServerGroupTransformer',
