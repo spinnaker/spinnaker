2ae834515af2f2a1128b820e61af21d68a9bd1e2
Alan Quach <1633736+alanmquach@users.noreply.github.com>
2021-05-17 14:31:53 -0700

    feat(pipeline): Change back button to breadcrumbs (#9020)
    * feat(pipeline): Change back button to breadcrumbs
    
    * feat(pipeline): Replace chevron with dash

--- app/scripts/modules/core/src/pipeline/details/SingleExecutionDetails.tsx
+++ app/scripts/modules/core/src/pipeline/details/SingleExecutionDetails.tsx
@@ -5,7 +5,7 @@ import ReactGA from 'react-ga';
 
 import { Application } from 'core/application/application.model';
 import { IExecution, IPipeline } from 'core/domain';
-import { Tooltip, useData, useLatestPromise } from 'core/presentation';
+import { useData, useLatestPromise } from 'core/presentation';
 import { IStateChange, ReactInjector } from 'core/reactShims';
 import { SchedulerFactory } from 'core/scheduler';
 import { ExecutionState } from 'core/state';
@@ -140,9 +140,6 @@ export function SingleExecutionDetails(props: ISingleExecutionDetailsProps) {
     });
   };
 
-  const defaultExecutionParams = { application: app.name, executionId: execution?.id || '' };
-  const executionParams = ReactInjector.$state.params.executionParams || defaultExecutionParams;
-
   let truncateAncestry = ancestry.length - 1;
   if (executionId && execution && executionId !== execution.id) {
     // We are on the eager end of a transition to a different executionId
@@ -169,14 +166,13 @@ export function SingleExecutionDetails(props: ISingleExecutionDetailsProps) {
             <div className="single-execution-details">
               <div className="flex-container-h baseline">
                 <h3>
-                  <Tooltip value="Back to Executions">
-                    <UISref to="^.executions.execution" params={executionParams}>
-                      <a className="btn btn-configure">
-                        <span className="glyphicon glyphicon glyphicon-circle-arrow-left" />
-                      </a>
-                    </UISref>
-                  </Tooltip>
-                  {execution.name}
+                  <UISref to="^.executions">
+                    <a className="clickable">{app.name}</a>
+                  </UISref>
+                  {' - '}
+                  <UISref to="^.executions" params={{ pipeline: execution.name }}>
+                    <a className="clickable">{execution.name}</a>
+                  </UISref>
                 </h3>
 
                 <div className="form-group checkbox flex-pull-right">
