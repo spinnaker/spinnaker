494add310c89f2cfbb88b3b8bf9380e2d6c82c7a
Chris Thielen <christopherthielen@users.noreply.github.com>
2021-05-21 08:00:59 -1000

    feat(amazon/serverGroup): export datelinechart and MetricAlarmChart (#9211)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- app/scripts/modules/amazon/src/serverGroup/index.ts
+++ app/scripts/modules/amazon/src/serverGroup/index.ts
@@ -4,5 +4,7 @@ export * from './details/scalingPolicy/ScalingPolicyTypeRegistry';
 export * from './details/scalingPolicy/ScalingPolicyWriter';
 export * from './details/scalingPolicy/upsert/PolicyTypeSelectionModal';
 export * from './details/scalingPolicy/CreateScalingPolicyButton';
+export * from './details/scalingPolicy/chart/DateLineChart';
+export * from './details/scalingPolicy/chart/MetricAlarmChart';
 export * from './details/sections';
 export * from './details/utils';
