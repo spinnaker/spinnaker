285c19428b631318f51435f9972cafa71830dab4
Vignesh Murugesan <vmurugesan@netflix.com>
2021-05-24 15:50:04 -0700

    chore(packages): Upgrade @spinnaker/scripts

--- app/scripts/modules/docker/src/docker.module.ts
+++ app/scripts/modules/docker/src/docker.module.ts
@@ -3,11 +3,5 @@ import { module } from 'angular';
 import { DOCKER_PIPELINE_STAGES_BAKE_DOCKERBAKESTAGE } from './pipeline/stages/bake/dockerBakeStage';
 import './pipeline/trigger/DockerTrigger';
 
-// load all templates into the $templateCache
-const templates = require.context('./', true, /\.html$/);
-templates.keys().forEach(function (key) {
-  templates(key);
-});
-
 export const DOCKER_MODULE = 'spinnaker.docker';
 module(DOCKER_MODULE, [DOCKER_PIPELINE_STAGES_BAKE_DOCKERBAKESTAGE]);
