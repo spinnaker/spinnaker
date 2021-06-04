34f5eb31c52ba12c6a28c01bf8ae0d05377cb15c
Vignesh Murugesan <vmurugesan@netflix.com>
2021-06-04 14:28:47 -0700

    feat(scripts): Add custom auto-external configurer for rollup
    The existing rollup plugin `rollup-plugin-auto-external` does not handle imports with path in them
    such as `bootstrap/dist/js/bootstrap`. So creating a simple external configurer that handles such
    scenarios along with handling scoped packages. This configurer uses the passed in dependencies map
    and accurately marks npm imports as externals.

--- packages/scripts/config/rollup.config.base.module.js
+++ packages/scripts/config/rollup.config.base.module.js
@@ -1,12 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 const baseRollupConfig = require('./rollup.config.base');
 const angularJsTemplateLoader = require('../helpers/rollup-plugin-angularjs-template-loader');
-const autoExternal = require('rollup-plugin-auto-external');
+const externalConfigurer = require('../helpers/rollup-node-auto-external-configurer');
+
+const packageJSON = JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf8'));
 
 module.exports = {
   ...baseRollupConfig,
   input: 'src/index.ts',
   output: [{ dir: 'dist', format: 'es', sourcemap: true }],
-  plugins: [...baseRollupConfig.plugins, angularJsTemplateLoader({ sourceMap: true }), autoExternal()],
-  // `autoExternal` cannot handle paths in import statements, so we need to explicitly add it in `external`.
-  external: ['rxjs/operators'],
+  plugins: [...baseRollupConfig.plugins, angularJsTemplateLoader({ sourceMap: true })],
+  external: externalConfigurer(packageJSON.dependencies),
 };
