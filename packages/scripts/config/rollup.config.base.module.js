d3baa94e44659003364dc18564255c6c7fca0fb5
Vignesh Murugesan <vmurugesan@netflix.com>
2021-06-07 18:41:52 -0700

    feat(build): Add support for stripping during bundling
    There's some webpack related code in spinnaker packages that are still needed as
    the codebase is being migrated, but needs to be stripped when bundled through
    rollup, so adding support for that.

--- packages/scripts/config/rollup.config.base.module.js
+++ packages/scripts/config/rollup.config.base.module.js
@@ -3,6 +3,7 @@ const path = require('path');
 const baseRollupConfig = require('./rollup.config.base');
 const angularJsTemplateLoader = require('../helpers/rollup-plugin-angularjs-template-loader');
 const externalConfigurer = require('../helpers/rollup-node-auto-external-configurer');
+const stripCode = require('rollup-plugin-strip-code');
 
 const packageJSON = JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf8'));
 
@@ -10,6 +11,13 @@ module.exports = {
   ...baseRollupConfig,
   input: 'src/index.ts',
   output: [{ dir: 'dist', format: 'es', sourcemap: true }],
-  plugins: [...baseRollupConfig.plugins, angularJsTemplateLoader({ sourceMap: true })],
+  plugins: [
+    ...baseRollupConfig.plugins,
+    angularJsTemplateLoader({ sourceMap: true }),
+    stripCode({
+      start_comment: 'Start - Rollup Remove',
+      end_comment: 'End - Rollup Remove',
+    }),
+  ],
   external: externalConfigurer(packageJSON.dependencies),
 };
