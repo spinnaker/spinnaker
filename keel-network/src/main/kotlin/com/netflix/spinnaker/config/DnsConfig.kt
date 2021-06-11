b7317d2daa40885f863579b9cc3d1abd05db7215
Luis Pollo <lpollo@netflix.com>
2021-06-11 16:12:05 -0700

    fix(network): Fix DnsConfig
    (cherry picked from commit efb2028c6a72f5c0f6149dbb940af6efa0db3592)

--- keel-network/src/main/kotlin/com/netflix/spinnaker/config/DnsConfig.kt
+++ keel-network/src/main/kotlin/com/netflix/spinnaker/config/DnsConfig.kt
@@ -3,6 +3,6 @@ package com.netflix.spinnaker.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("keel.dns")
-data class DnsConfig(
-  val defaultDomain: String
+class DnsConfig(
+  var defaultDomain: String
 )
