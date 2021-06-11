67ab6e2d0fa95c042981a13c1c7d1ae2a5553e44
Luis Pollo <lpollo@netflix.com>
2021-06-11 22:03:47 +0000

    fix(network): Add default config for keel.dns.defaultDomain

--- keel-network/src/main/kotlin/com/netflix/spinnaker/config/DnsConfig.kt
+++ keel-network/src/main/kotlin/com/netflix/spinnaker/config/DnsConfig.kt
@@ -4,5 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("keel.dns")
 class DnsConfig(
-  var defaultDomain: String
+  var defaultDomain: String = "keel.io"
 )
