ca80a2ea48ef1a6946de8e3842b914727e2ea495
Alan Quach <1633736+alanmquach@users.noreply.github.com>
2021-06-28 11:31:40 -0700

    feat(tasks): Use stage context over variables (#9361)
    * feat(tasks): Use stage context over variables
    
    * feat(tasks): Use as fallback and memoize
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- app/scripts/modules/core/src/orchestratedItem/orchestratedItem.transformer.ts
+++ app/scripts/modules/core/src/orchestratedItem/orchestratedItem.transformer.ts
@@ -1,4 +1,5 @@
 import { formatDistance } from 'date-fns';
+import { get, isNil } from 'lodash';
 import { $log } from 'ngimport';
 
 import { IOrchestratedItem, IOrchestratedItemVariable, ITask, ITaskStep } from 'core/domain';
@@ -18,6 +19,10 @@ export class OrchestratedItemTransformer {
     });
   }
 
+  private static shouldReplace(previousValue: any, currentValue: any): boolean {
+    return (isNil(previousValue) || previousValue === '') && !(isNil(currentValue) || currentValue === '');
+  }
+
   public static defineProperties(item: any): void {
     // Don't try to add properties more than once - but also don't blow up if something tries to do so
     const testDescriptor: PropertyDescriptor = Object.getOwnPropertyDescriptor(item, 'runningTime');
@@ -29,12 +34,32 @@ export class OrchestratedItemTransformer {
       if (item.context) {
         return item.context[key];
       }
+
       if (!item.variables) {
         return null;
       }
+
       const match: IOrchestratedItemVariable = item.variables.find(
         (variable: IOrchestratedItemVariable) => variable.key === key,
       );
+      if (match) {
+        return match.value;
+      }
+
+      // Fallback to stage context if not found in variables
+      const stages = item.execution?.stages;
+      if (stages && Array.isArray(stages)) {
+        const maybeValue = (stages as any[])
+          .map((stage) => stage.context && get(stage.context, key))
+          .reduce((prev, curr) => (OrchestratedItemTransformer.shouldReplace(prev, curr) ? curr : prev));
+
+        if (!isNil(maybeValue) && maybeValue !== '') {
+          // Memoize back into variables
+          item.variables.push({ key, value: maybeValue } as IOrchestratedItemVariable);
+          return maybeValue;
+        }
+      }
+
       return match ? match.value : null;
     };
 
