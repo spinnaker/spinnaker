9deeaa8bec6ec35b053ccf6cb2ea763acce91b94
Lorin Hochstein <lhochstein@netflix.com>
2021-07-15 17:55:42 -0700

    fix(pr): update tests
    (cherry picked from commit 1c0aef17ea6f503b19e84fc146e3934c8fdd5e7d)

--- keel-titus-plugin/src/test/kotlin/com/netflix/spinnaker/keel/titus/postdeploy/TagAmiHandlerTests.kt
+++ keel-titus-plugin/src/test/kotlin/com/netflix/spinnaker/keel/titus/postdeploy/TagAmiHandlerTests.kt
@@ -4,31 +4,114 @@ import com.netflix.spectator.api.NoopRegistry
 import com.netflix.spinnaker.keel.api.ArtifactInEnvironmentContext
 import com.netflix.spinnaker.keel.api.DeliveryConfig
 import com.netflix.spinnaker.keel.api.Environment
+import com.netflix.spinnaker.keel.api.TaskStatus
 import com.netflix.spinnaker.keel.api.action.ActionRepository
+import com.netflix.spinnaker.keel.api.action.ActionState
 import com.netflix.spinnaker.keel.api.action.ActionType
 import com.netflix.spinnaker.keel.api.actuation.TaskLauncher
-import com.netflix.spinnaker.keel.api.artifacts.DeliveryArtifact
+import com.netflix.spinnaker.keel.api.constraints.ConstraintStatus
 import com.netflix.spinnaker.keel.core.api.TagAmiPostDeployAction
 import com.netflix.spinnaker.keel.orca.OrcaService
 import com.netflix.spinnaker.keel.test.DummyArtifact
+import com.netflix.spinnaker.keel.titus.verification.TASKS
 import com.netflix.spinnaker.keel.verification.ImageFinder
+import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import java.time.Instant.now
 
 internal class TagAmiHandlerTests {
-
   private val orca : OrcaService = mockk()
   private val launcher : TaskLauncher = mockk()
   private val repo : ActionRepository = mockk()
   private val finder : ImageFinder = mockk()
+
+  @Test
+  fun getSupportedType() {
+    expectThat(handler.supportedType.name).isEqualTo("tag-ami")
+  }
+
+  @Test
+  fun `running the action launches a task, no verifications`() {
+
+    every { repo.getStates(context, ActionType.VERIFICATION) } returns emptyMap()
+
+    every { finder.getImages(deliveryConfig, "testing") } returns listOf(
+      mockk {
+        every { kind } returns mockk {
+          every { group } returns "ec2"
+        }
+        every { images } returns emptyList()
+      }
+    )
+
+    coEvery {
+        launcher.submitJob(
+          user=any(),
+          application=any(),
+          notifications = any(),
+          subject=any(),
+          description=any(),
+          correlationId=any(),
+          stages=any())
+    } returns mockk() {
+      every { id } returns "01FAP29KG410CVJHKXTEW5CA20"
+    }
+
+    runBlocking {
+      handler.start(context, TagAmiPostDeployAction())
+    }
+
+    coVerify {
+      launcher.submitJob(
+        user=any(),
+        application=any(),
+        notifications = any(),
+        subject=any(),
+        description=any(),
+        correlationId=any(),
+        stages=any())
+    }
+  }
+
+  @Test
+  fun `task succeeded`() {
+    val taskId = "01FAP9T72MWQEF49QY2JNJHPWA"
+    val oldState = ActionState(ConstraintStatus.PENDING, now(), null, metadata = mapOf(TASKS to listOf(taskId)))
+    coEvery { orca.getOrchestrationExecution(taskId, any() ) } returns mockk() {
+      every { id } returns taskId
+      every { application } returns "fnord"
+      every { status } returns TaskStatus.SUCCEEDED
+    }
+
+    val newState = runBlocking {
+      handler.evaluate(context, mockk(), oldState)
+    }
+
+    expectThat(newState.status).isEqualTo(ConstraintStatus.PASS)
+  }
+
+  @Test
+  fun `task terminal`() {
+    val taskId = "01FAP9T72MWQEF49QY2JNJHPWA"
+    val oldState = ActionState(ConstraintStatus.PENDING, now(), null, metadata = mapOf(TASKS to listOf(taskId)))
+    coEvery { orca.getOrchestrationExecution(taskId, any() ) } returns mockk() {
+      every { id } returns taskId
+      every { application } returns "fnord"
+      every { status } returns TaskStatus.TERMINAL
+    }
+
+    val newState = runBlocking {
+      handler.evaluate(context, mockk(), oldState)
+    }
+
+    expectThat(newState.status).isEqualTo(ConstraintStatus.FAIL)
+  }
 
   private val handler = TagAmiHandler(
     eventPublisher = mockk(),
@@ -37,7 +120,7 @@ internal class TagAmiHandlerTests {
     orca = orca,
     actionRepository = repo,
     spectator = NoopRegistry(),
-    baseUrlConfig = mockk(),
+    baseUrlConfig = mockk() { every { baseUrl } returns "https://spin" },
     imageFinder = finder
   )
   private val deliveryConfig = DeliveryConfig(
@@ -54,43 +137,4 @@ internal class TagAmiHandlerTests {
     artifactReference = "fnord",
     version = "1.2.3"
   )
-
-  @Test
-  fun getSupportedType() {
-    expectThat(handler.supportedType.name).isEqualTo("tag-ami")
-  }
-
-  @Test
-  fun `running the action launches a task`() {
-    every { repo.getStates(context, ActionType.VERIFICATION) } returns emptyMap()
-    every { finder.getImages(deliveryConfig, "testing") } returns emptyList()
-
-    runBlocking {
-      handler.start(context, TagAmiPostDeployAction())
-    }
-
-    coVerify {
-      launcher.submitJob(
-        user=any(),
-        application=any(),
-        notifications = any(),
-        subject=any(),
-        description=any(),
-        correlationId=any(),
-        stages=any())
-    }
-
-  }
-
-  @Test
-  fun evaluate() {
-  }
-
-  @Test
-  fun toJob() {
-  }
-
-  @Test
-  fun getEventPublisher() {
-  }
 }
