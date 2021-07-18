e088b8fa059a7fe8a15d7134814d5376e9fc9ec5
Chris Thielen <github@sandgnat.com>
2021-07-17 15:21:09 -1000

    chore(publishing): fix if/then syntax in scripts/gha_output_* scripts

--- scripts/gha_output_changelog.sh
+++ scripts/gha_output_changelog.sh
@@ -17,7 +17,7 @@ function changelog() {
 }
 
 # If the last commit was a package bump, emit the changelog as a GHA output
-if [ git log -1 --pretty=%B | head -n 1 | grep "chore(publish): publish packages" ] ; then
+if git log -1 --pretty=%B | head -n 1 | grep "chore(publish): publish packages" ; then
   CHANGELOG=$(changelog)
   # Quote newlines so the entire thing can be emitted as a single line output
   CHANGELOG="${CHANGELOG//'%'/'%25'}"
