36f588997d15b04136c3b293639a8b8f71b934ae
Chris Thielen <github@sandgnat.com>
2021-07-19 12:58:24 -1000

    fix(publishing): Get publish git-tag from ${package}/package.json

--- scripts/gha_publish.sh
+++ scripts/gha_publish.sh
@@ -17,7 +17,7 @@ for PACKAGEJSON in */package.json ; do
   HAS_PKG_BUMP=$(git diff HEAD^ -- "${PACKAGEJSON}" | grep -c '"version"')
   if [ "$HAS_PKG_BUMP" -ne 0 ] ; then
     echo "Deck package publisher ---> Version bump detected in $PACKAGEJSON"
-    TAG=$(jq -r '.name + "@" + .version' < package.json)
+    TAG=$(jq -r '.name + "@" + .version' < $PACKAGEJSON)
     git tag -a $TAG -m "Publish $TAG to NPM"
     git push origin $TAG
     BUMPS=yes
