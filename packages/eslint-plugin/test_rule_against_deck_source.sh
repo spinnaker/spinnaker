26bff0be0cc6b12dfde7ade2851b30534ce23671
Chris Thielen <github@sandgnat.com>
2021-08-11 14:21:42 -1000

    fix(eslint-plugin): update test_rule_against_deck_source.sh to allow colocated tests and checking packages/*

--- packages/eslint-plugin/test_rule_against_deck_source.sh
+++ packages/eslint-plugin/test_rule_against_deck_source.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+RULESDIR=`dirname $0`/rules;
+RULESDIRTEMP=$RULESDIR/temp
 if [[ -z "$1" ]] ; then
-  echo "please provide the rule to run against ../../app:\n";
-  ls `dirname $0`/rules | sed -e 's/\.js$//'
+  echo "please provide the rule to run against ../../app and ../../packages:\n";
+  ls $RULESDIR | grep -v '\.spec\.' | sed -e 's/\.js$//'
 else
-  RULE={\"$1\":2}
+  RULEFILE=$(basename $(ls $RULESDIR/$1.[tj]s));
+  RULECONFIG={\"$1\":2}
+  echo RULE=$RULE
+  echo RULECONFIG=$RULECONFIG
+  mkdir -p $RULESDIRTEMP
+  ln -s ../$RULEFILE $RULESDIRTEMP
   shift;
-  echo "node ${NODE_OPTS} ../../node_modules/.bin/eslint --ext js,ts,jsx,tsx --no-eslintrc -c test.eslintrc --rulesdir rules --rule $RULE ../../app $*"
-  node ${NODE_OPTS} ../../node_modules/.bin/eslint --ext js,ts,jsx,tsx --no-eslintrc -c test.eslintrc --rulesdir rules --rule $RULE ../../app $*
+  echo "node ${NODE_OPTS} ../../node_modules/.bin/eslint --ext js,ts,jsx,tsx --no-eslintrc -c test.eslintrc --rulesdir $RULESDIRTEMP --rule $RULECONFIG ../../app ../../packages $*"
+  node ${NODE_OPTS} ../../node_modules/.bin/eslint --ext js,ts,jsx,tsx --no-eslintrc -c test.eslintrc --rulesdir $RULESDIRTEMP --rule $RULECONFIG ../../app ../../packages $*
+  rm -rf $RULESDIRTEMP
 fi
--- packages/eslint-plugin/utils/mockModule.js
+++ packages/eslint-plugin/utils/mockModule.js
@@ -2,7 +2,7 @@ function mockModule(moduleName) {
   jest.mock(moduleName);
   const mockedModule = require(moduleName);
   const actualModule = jest.requireActual(moduleName);
-  for ([key, value] of Object.entries(actualModule)) {
+  for (const [key, value] of Object.entries(actualModule)) {
     if (typeof value === 'function') {
       mockedModule[key].mockImplementation(value);
     }
