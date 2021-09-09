add89c3abffb44aeea599d631e65db6fffbc570e
caseyhebebrand <caseyhebebrand@gmail.com>
2021-09-09 11:46:18 -0700

    chore(amazon): Export shared scaling policy components (#9649)

--- packages/amazon/src/serverGroup/details/scalingPolicy/index.ts
+++ packages/amazon/src/serverGroup/details/scalingPolicy/index.ts
@@ -1,1 +1,8 @@
+export { DateLineChart } from './chart/DateLineChart';
+export { MetricAlarmChart } from './chart/MetricAlarmChart';
+export * from './ScalingPolicyTypeRegistry';
+export * from './CreateScalingPolicyButton';
+export * from './ScalingPolicyWriter';
+export * from './targetTracking/TargetMetricFields';
+export * from './targetTracking/TargetTrackingChart';
 export * from './upsert';
--- packages/amazon/src/serverGroup/details/scalingPolicy/upsert/index.ts
+++ packages/amazon/src/serverGroup/details/scalingPolicy/upsert/index.ts
@@ -1,2 +1,6 @@
+export * from './alarm/AlarmConfigurer';
+export * from './alarm/DimensionsEditor';
+export * from './alarm/MetricSelector';
 export * from './PolicyTypeSelectionModal';
+export * from './ScalingPolicyAdditionalSettings';
 export * from './step/StepPolicyAction';
