b238b9c2357ed33e44bbcd48cffe6255c923dd5c
Chris Thielen <github@sandgnat.com>
2021-09-30 10:09:12 -1000

    fix(core/presentation): Remove return value from useEffect in useMountStatusRef

--- packages/core/src/presentation/hooks/useMountStatusRef.hook.ts
+++ packages/core/src/presentation/hooks/useMountStatusRef.hook.ts
@@ -4,7 +4,9 @@ export function useMountStatusRef() {
   const mountStatusRef = useRef<'FIRST_RENDER' | 'MOUNTED' | 'UNMOUNTED'>('FIRST_RENDER');
   useEffect(() => {
     mountStatusRef.current = 'MOUNTED';
-    return () => (mountStatusRef.current = 'UNMOUNTED');
+    return () => {
+      mountStatusRef.current = 'UNMOUNTED';
+    };
   });
   return mountStatusRef;
 }
