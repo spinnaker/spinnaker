deee318cc857742bc27aed2e40d51d5bb1fce531
mattgogerly <matthewgogerly@gmail.com>
2021-10-27 20:44:18 +0100

    chore(deps): use select2 version from NPM

--- packages/core/src/core.module.ts
+++ packages/core/src/core.module.ts
@@ -12,7 +12,7 @@ import 'react-virtualized/styles.css';
 import 'react-virtualized-select/styles.css';
 import 'ui-select/dist/select.css';
 import '@spinnaker/styleguide/public/styleguide.min.css';
-import 'Select2/select2.css';
+import 'select2/select2.css';
 import 'select2-bootstrap-css/select2-bootstrap.css';
 import 'source-sans/source-sans-3.css';
 import './fonts/icons.css';
