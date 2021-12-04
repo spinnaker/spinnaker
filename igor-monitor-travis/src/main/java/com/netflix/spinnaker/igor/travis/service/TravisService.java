34ef01ecca82bc1f4af6c9457430966adba5b922
Jørgen Jervidalo <jorgen.jervidalo@schibsted.com>
2021-12-04 18:00:25 +0100

    Revert "feat(travis): Adds more info to triggered Travis builds by (#932)
    This reverts commit f8b365683dfd152481d42fb96ac36ef64a9a96f1.
    
    Reverted in favour of https://github.com/spinnaker/orca/pull/4093
    default (#891)"
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- igor-monitor-travis/src/main/java/com/netflix/spinnaker/igor/travis/service/TravisService.java
+++ igor-monitor-travis/src/main/java/com/netflix/spinnaker/igor/travis/service/TravisService.java
@@ -179,15 +179,11 @@ public class TravisService implements BuildOperations, BuildProperties {
     String repoSlug = cleanRepoSlug(inputRepoSlug);
     String branch = branchFromRepoSlug(inputRepoSlug);
     RepoRequest repoRequest = new RepoRequest(branch.isEmpty() ? "master" : branch);
-    String buildMessage;
     if (buildMessageKey != null && queryParameters.containsKey(buildMessageKey)) {
-      buildMessage = queryParameters.get(buildMessageKey);
+      String buildMessage = queryParameters.get(buildMessageKey);
       queryParameters.remove(buildMessageKey);
-    } else {
-      buildMessage =
-          "Application: '${execution.application}', pipeline: '${execution.name}', execution: '${execution.id}'";
+      repoRequest.setMessage(repoRequest.getMessage() + ": " + buildMessage);
     }
-    repoRequest.setMessage(repoRequest.getMessage() + ": " + buildMessage);
     repoRequest.setConfig(new Config(queryParameters));
     final TriggerResponse triggerResponse =
         travisClient.triggerBuild(getAccessToken(), repoSlug, repoRequest);
