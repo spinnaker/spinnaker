a8d9831dd56a1af6d9bb0fcad7267a81688b101b
Shaun Phillips <sphillips@salesforce.com>
2022-03-10 07:23:58 -0800

    refactor(gate/web): Expand star imports in ExecutionsController

--- gate/gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/ExecutionsController.java
+++ gate/gate-web/src/main/groovy/com/netflix/spinnaker/gate/controllers/ExecutionsController.java
@@ -22,7 +22,11 @@ import io.swagger.v3.oas.annotations.Parameter;
 import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExecutionsController {
