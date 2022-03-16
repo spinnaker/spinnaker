3e68011bcce2600eba14021b47a7a6d22951e04a
kskewes-sf <96093759+kskewes-sf@users.noreply.github.com>
2022-03-17 09:47:35 +1300

    chore(ci): GHA - simplify build versioning (#4238)
    - collapse version info gathering steps into single `build_variables` step
    - collapse version info parts into single string and use everywhere.
    - use short git sha: `(git rev-parse --short HEAD)`
    
    Note `build.yml` versioning is not compatible with Debian package
    building as gradle plugin enforces `^[0-9]+`.
    We don't publish master branch or release-* branches to GAR apt
    repository though.
    Prefixing the version with `<tag>-dev-` or something and publishing Debian
    packages is possible but may pollute `apt-cache policy spinnaker-rosco` output
    and overall be unnecessary with regular releases.

--- .github/workflows/release_info.sh
+++ .github/workflows/release_info.sh
@@ -3,7 +3,6 @@
 # Only look to the latest release to determine the previous tag -- this allows us to skip unsupported tag formats (like `version-1.0.0`)
 export PREVIOUS_TAG=`curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
 echo "PREVIOUS_TAG=$PREVIOUS_TAG"
-
 export NEW_TAG=${GITHUB_REF/refs\/tags\//}
 echo "NEW_TAG=$NEW_TAG"
 export CHANGELOG=`git log $NEW_TAG...$PREVIOUS_TAG --oneline`
