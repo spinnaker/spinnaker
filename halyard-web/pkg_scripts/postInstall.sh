e958d02b892a202d8de44a3bf0c003da36b2ec6d
Ashley Kleynhans <ashleykleynhans@users.noreply.github.com>
2022-05-23 18:14:14 +0200

    fix(install): Fixed bugs in systemd service preventing halyard daemon from starting (#1953)

--- halyard-web/pkg_scripts/postInstall.sh
+++ halyard-web/pkg_scripts/postInstall.sh
@@ -5,13 +5,21 @@ echo '/opt/halyard/bin/hal "$@"' | sudo tee -a /usr/local/bin/hal > /dev/null
 
 chmod +x /usr/local/bin/hal
 
-HAL_USER=""
-if [ -f "/opt/spinnaker/config/halyard-user" ]; then
+if [ -f "/opt/spinnaker/config/halyard-user" ];
+then
   HAL_USER=$(cat /opt/spinnaker/config/halyard-user)
-fi
-
-if [ -z "$HAL_USER" ]; then
-  HAL_USER="ubuntu"
+else
+  HAL_USER=spinnaker
+  getent passwd spinnaker > /dev/null
+  if [ $? -gt 0 ]; then
+    useradd -s /bin/bash $HAL_USER
+  fi
+  if [ ! -f "/opt/spinnaker/config/halyard-user" ];
+  then
+    mkdir -p /opt/spinnaker/config
+    chown -R spinnaker:spinnaker /opt/spinnaker
+    echo ${HAL_USER} > /opt/spinnaker/config/halyard-user
+  fi
 fi
 
 install --mode=755 --owner=$HAL_USER --group=$HAL_USER --directory /var/log/spinnaker/halyard
