351eb222bb6dc4d4decbdf50e385b55e1c87285c
Jason <jason.mcintosh@armory.io>
2022-06-22 03:49:53 -0500

    fix(redis): Updates redis to version 5 and non custom version (#1952)
    * fix(redis): Updates redis to version 5 and non custom version
    
    * feat(redis): Update redis to 6.2
    
    Bumps from 5 version

--- halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/service/distributed/kubernetes/v2/KubernetesV2RedisService.java
+++ halyard-deploy/src/main/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/service/distributed/kubernetes/v2/KubernetesV2RedisService.java
@@ -43,7 +43,7 @@ public class KubernetesV2RedisService extends RedisService implements Kubernetes
   }
 
   public String getArtifactId(DeploymentConfiguration deploymentConfiguration) {
-    return "us-docker.pkg.dev/spinnaker-community/redis/redis-cluster:v2";
+    return "redis:6.2";
   }
 
   @Override
