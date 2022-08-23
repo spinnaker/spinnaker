a088e784dcbfc6bd55b90b6b632c7f716d91be0f
Ashley Kleynhans <ashleykleynhans@users.noreply.github.com>
2022-08-23 03:07:29 +0200

    fix(install): Fixed bugs in postInstall script that causes installation to fail on Ubuntu 20.04 and 22.04 LTS (#899)

--- rosco-web/pkg_scripts/postInstall.sh
+++ rosco-web/pkg_scripts/postInstall.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Add /usr/local/bin to path so helm and helm3 can be found,
+# otherwise their installation returns a non-zero exit code
+# causing this script to fail,
+PATH=$PATH:/usr/local/bin
+
 # ubuntu
 # check that owner group exists
 if [ -z `getent group spinnaker` ]; then
@@ -11,17 +16,23 @@ if [ -z `getent passwd spinnaker` ]; then
   useradd --gid spinnaker spinnaker -m --home-dir /home/spinnaker
 fi
 
+create_temp_dir() {
+  TEMPDIR=$(mktemp -d /tmp/installrosco.XXXX)
+  cd $TEMPDIR
+}
+
+remove_temp_dir() {
+  cd ..
+  rm -rf $TEMPDIR
+}
+
 install_packer() {
   PACKER_VERSION="1.8.1"
   local packer_version=$(/usr/bin/packer --version)
   local packer_status=$?
   if [ $packer_status -ne 0 ] || [ "$packer_version" != "$PACKER_VERSION" ]; then
-    TEMPDIR=$(mktemp -d installrosco.XXXX)
-    cd $TEMPDIR
     wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
     unzip -o "packer_${PACKER_VERSION}_linux_amd64.zip" -d /usr/bin
-    cd ..
-    rm -rf $TEMPDIR
   fi
 }
 
@@ -40,7 +51,9 @@ install_helm3() {
   mv /usr/local/bin/helm /usr/local/bin/helm3
 }
 
+create_temp_dir
 install_packer
 install_helm3
 install_helm
+remove_temp_dir
 install --mode=755 --owner=spinnaker --group=spinnaker --directory  /var/log/spinnaker/rosco
