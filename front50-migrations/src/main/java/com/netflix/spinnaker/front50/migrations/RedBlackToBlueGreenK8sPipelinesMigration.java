7342d04105f1c4bbe937b613738927e6c1d6b31b
ovidiupopa07 <105648914+ovidiupopa07@users.noreply.github.com>
2023-01-17 17:12:54 +0200

    feat(pipeline/kubernetes): Migrate existing K8s deploy manifest traffic management from redblack to bluegreen (#1197)
    * feat(pipeline/kubernetes): Introduce a migration class for kubernetes red/black traffic management strategy
    
    The migration loads all the k8s pipelines that are currently using red/black traffic management and it is updating all pipelines to the new terminology blue/green
    
    * feat(pipeline/kubernetes): Add more unit tests
    
    * feat(pipeline/kubernetes): Remove validity condition since we do now know when this migration will run in the users environment

---
+++ front50-migrations/src/main/java/com/netflix/spinnaker/front50/migrations/RedBlackToBlueGreenK8sPipelinesMigration.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2023 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netflix.spinnaker.front50.migrations;
+
+import com.netflix.spinnaker.front50.api.model.pipeline.Pipeline;
+import com.netflix.spinnaker.front50.model.pipeline.PipelineDAO;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Predicate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class RedBlackToBlueGreenK8sPipelinesMigration implements Migration {
+
+  private final PipelineDAO pipelineDAO;
+
+  public RedBlackToBlueGreenK8sPipelinesMigration(PipelineDAO pipelineDAO) {
+    this.pipelineDAO = pipelineDAO;
+  }
+
+  @Override
+  public boolean isValid() {
+    return true;
+  }
+
+  @Override
+  public void run() {
+    log.info(
+        "Starting the migration of K8s pipelines using RED/BLACK traffic management strategy to BLUE/GREEN");
+    pipelineDAO.all().stream()
+        .filter(RedBlackToBlueGreenK8sPipelinesMigration.pipelineWithRedBlackStrategyPredicate())
+        .forEach(this::migrate);
+  }
+
+  private void migrate(Pipeline pipeline) {
+    List<Map<String, Object>> stages = new ArrayList<>(pipeline.getStages());
+
+    stages.stream()
+        .filter(RedBlackToBlueGreenK8sPipelinesMigration::kubernetesProvider)
+        .filter(RedBlackToBlueGreenK8sPipelinesMigration::deployManifestStage)
+        .map(RedBlackToBlueGreenK8sPipelinesMigration::getTrafficManagement)
+        .filter(Objects::nonNull)
+        .filter(RedBlackToBlueGreenK8sPipelinesMigration::trafficManagementEnabled)
+        .map(RedBlackToBlueGreenK8sPipelinesMigration::getTrafficManagementOptions)
+        .forEach(
+            trafficManagementOptions -> {
+              Object strategy = trafficManagementOptions.get("strategy");
+              if ("redblack".equals(strategy)) {
+                trafficManagementOptions.put("strategy", "bluegreen");
+              }
+            });
+
+    this.pipelineDAO.update(pipeline.getId(), pipeline);
+    log.info(
+        "Migrated pipeline (application: {}, pipelineId: {}, name: {})",
+        pipeline.getApplication(),
+        pipeline.getId(),
+        pipeline.getName());
+  }
+
+  private static Predicate<Pipeline> pipelineWithRedBlackStrategyPredicate() {
+    return pipeline ->
+        pipeline.getStages().stream()
+            .filter(RedBlackToBlueGreenK8sPipelinesMigration::kubernetesProvider)
+            .filter(RedBlackToBlueGreenK8sPipelinesMigration::deployManifestStage)
+            .map(RedBlackToBlueGreenK8sPipelinesMigration::getTrafficManagement)
+            .filter(Objects::nonNull)
+            .filter(RedBlackToBlueGreenK8sPipelinesMigration::trafficManagementEnabled)
+            .map(RedBlackToBlueGreenK8sPipelinesMigration::getTrafficManagementOptions)
+            .anyMatch(RedBlackToBlueGreenK8sPipelinesMigration::redBlackStrategy);
+  }
+
+  private static Map<String, Object> getTrafficManagementOptions(
+      Map<String, Object> trafficManagement) {
+    return (Map<String, Object>) trafficManagement.get("options");
+  }
+
+  private static boolean trafficManagementEnabled(Map<String, Object> trafficManagement) {
+    return Boolean.TRUE.equals(trafficManagement.get("enabled"));
+  }
+
+  private static Map<String, Object> getTrafficManagement(Map<String, Object> stage) {
+    return (Map<String, Object>) stage.get("trafficManagement");
+  }
+
+  private static boolean kubernetesProvider(Map<String, Object> stage) {
+    return "kubernetes".equals(stage.get("cloudProvider"));
+  }
+
+  private static boolean deployManifestStage(Map<String, Object> stage) {
+    return "deployManifest".equals(stage.get("type"));
+  }
+
+  private static boolean redBlackStrategy(Map<String, Object> stage) {
+    return "redblack".equals(stage.get("strategy"));
+  }
+}
