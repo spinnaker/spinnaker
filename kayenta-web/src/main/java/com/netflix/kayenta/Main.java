eaaadcbd8490e247f86fd56b5faf9293d303e894
Sandesh <30489233+j-sandy@users.noreply.github.com>
2023-01-23 22:45:20 +0530

    chore(web): clean up for spring property setup (#928)
    Using `com.netflix.spinnaker.kork.boot.DefaultPropertiesBuilder` class to setup spring and other related properties required for the spring boot application to start.

--- kayenta-web/src/main/java/com/netflix/kayenta/Main.java
+++ kayenta-web/src/main/java/com/netflix/kayenta/Main.java
@@ -17,8 +17,7 @@
 package com.netflix.kayenta;
 
 import com.netflix.kayenta.config.ApplicationConfiguration;
-import java.util.Collections;
-import java.util.HashMap;
+import com.netflix.spinnaker.kork.boot.DefaultPropertiesBuilder;
 import java.util.Map;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
@@ -27,21 +26,12 @@ import org.springframework.context.annotation.Import;
 @Import(ApplicationConfiguration.class)
 @Configuration
 public class Main {
-  private static final Map<String, Object> DEFAULT_PROPS = buildDefaults();
-
-  private static Map<String, Object> buildDefaults() {
-    Map<String, String> defaults = new HashMap<>();
-    defaults.put("netflix.environment", "test");
-    defaults.put("netflix.account", "${netflix.environment}");
-    defaults.put("netflix.stack", "test");
-    defaults.put("spring.config.additional-location", "${user.home}/.spinnaker/");
-    defaults.put("spring.application.name", "kayenta");
-    defaults.put("spring.config.name", "spinnaker,${spring.application.name}");
-    defaults.put("spring.profiles.active", "${netflix.environment},local");
-    defaults.put("spring.jackson.serialization.WRITE_DATES_AS_TIMESTAMPS", "false");
-    defaults.put("spring.jackson.default-property-inclusion", "non_null");
-    return Collections.unmodifiableMap(defaults);
-  }
+  private static final Map<String, Object> DEFAULT_PROPS =
+      new DefaultPropertiesBuilder()
+          .property("spring.application.name", "kayenta")
+          .property("spring.jackson.serialization.WRITE_DATES_AS_TIMESTAMPS", "false")
+          .property("spring.jackson.default-property-inclusion", "non_null")
+          .build();
 
   public static void main(String... args) {
     new SpringApplicationBuilder().properties(DEFAULT_PROPS).sources(Main.class).run(args);
