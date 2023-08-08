e5682dfa47fa327cb61d637859701cf505781e73
Abe Garcia <44239562+abe21412@users.noreply.github.com>
2023-08-08 13:11:20 -0400

    feat(integration-tests): increase kubernetes integration test coverage (#5990)
    Co-authored-by: abe garcia <abraham_garcia-ortiz1@homedepot.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- clouddriver-kubernetes/src/integration/java/com/netflix/spinnaker/clouddriver/kubernetes/it/containers/KubernetesCluster.java
+++ clouddriver-kubernetes/src/integration/java/com/netflix/spinnaker/clouddriver/kubernetes/it/containers/KubernetesCluster.java
@@ -35,6 +35,7 @@ import org.springframework.util.FileCopyUtils;
 public class KubernetesCluster {
 
   private static KubernetesCluster INSTANCE;
+  private static final String IMAGE = System.getenv("IMAGE");
   private static final String KIND_VERSION = "0.11.1";
   private static final String KUBECTL_VERSION = "1.20.6";
   private static final Path IT_BUILD_HOME = Paths.get(System.getenv("IT_BUILD_HOME"));
@@ -171,7 +172,11 @@ public class KubernetesCluster {
       System.out.println("Deleting old test cluster");
       runKindCmd("delete cluster --name=kube-int-tests");
     }
-    runKindCmd("create cluster --name=kube-int-tests --kubeconfig=" + KUBECFG_PATH + " --wait=10m");
+    runKindCmd(
+        "create cluster --name=kube-int-tests --kubeconfig="
+            + KUBECFG_PATH
+            + " --wait=10m --image="
+            + IMAGE);
   }
 
   private String runKindCmd(String args) throws IOException, InterruptedException {
