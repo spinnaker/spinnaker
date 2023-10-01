c19f9ce9ca411fe16e865d3d3121d497bce93575
Matt Gogerly <6519811+mattgogerly@users.noreply.github.com>
2023-10-01 21:33:13 +0100

    fix(kubernetes): export rollout restart stage so it's actually available for use (#10037)

--- packages/kubernetes/src/pipelines/stages/index.ts
+++ packages/kubernetes/src/pipelines/stages/index.ts
@@ -1,4 +1,5 @@
 export * from './deleteManifest/deleteManifestStage';
 export * from './deployManifest/deployManifestStage';
 export * from './patchManifest/patchManifestStage';
+export * from './rolloutRestartManifest/rolloutRestartManifestStage';
 export * from './runJob/runJobStage';
