c0e6d3b5ff7926ae51dc081628abb99c1c7ea7c5
Jørgen Jervidalo <jorgen.jervidalo@schibsted.com>
2024-02-09 16:36:10 +0100

    feat(aws): Upgrade packer and add SSM support (Amazon) (#1066)
    * feat(aws): Upgrade packer and add SSM support (Amazon)
    
    This PR upgrades Packer to version v1.10.1. All the previously default providers were moved to plugins in v1.10 [1], so we also need to manually install plugins. I've opted to install Amazon, Azure, Docker and Google plugins by default, but the list can be overridden by Docker build args (or an environment variable in the case of the deb package).
    
    This PR also installs AWS CLI 2 and the Session Manager Plugin in the Ubuntu images and in the deb package. **I opted to not install it in the Alpine images**, as the session-manager-plugin is not officially supported, and you'd need to either copy the binary or compile it from source to get it to work. I'm very open to reconsider this.
    
    After applying this PR, you can use SSM instead of SSH to connect to instances when baking using the `amazon-ebs` provider (or friends) [2].
    
    [1]: https://www.hashicorp.com/blog/announcing-the-removal-of-bundled-plugins-in-hashicorp-packer
    [2]: https://developer.hashicorp.com/packer/integrations/hashicorp/amazon/latest/components/builder/ebs#session-manager-plugin
    
    * Fix for arm builds
    
    * And another fix
    
    * Removed the docker packer plugin
    
    * Remove the rest of the docker references and make the deb postinstall script work on arm64
    
    * Fix unzip command
    
    * Fix indenting
    
    * Fix name of arg PACKER_PLUGINS

--- rosco-web/pkg_scripts/postInstall.sh
+++ rosco-web/pkg_scripts/postInstall.sh
@@ -16,6 +16,14 @@ if [ -z `getent passwd spinnaker` ]; then
   useradd --gid spinnaker spinnaker -m --home-dir /home/spinnaker
 fi
 
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH="amd64"
+fi
+
+# Get the PACKER_PLUGINS environment variable or set a default value
+PACKER_PLUGINS=${PACKER_PLUGINS:-"amazon azure googlecompute"}
+
 create_temp_dir() {
   TEMPDIR=$(mktemp -d /tmp/installrosco.XXXX)
   cd $TEMPDIR
@@ -27,13 +35,18 @@ remove_temp_dir() {
 }
 
 install_packer() {
-  PACKER_VERSION="1.8.1"
-  local packer_version=$(/usr/bin/packer --version)
+  PACKER_VERSION="1.10.1"
+  local packer_version="$(/usr/bin/packer --version)"
   local packer_status=$?
   if [ $packer_status -ne 0 ] || [ "$packer_version" != "$PACKER_VERSION" ]; then
-    wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
-    unzip -o "packer_${PACKER_VERSION}_linux_amd64.zip" -d /usr/bin
+    wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_${ARCH}.zip
+    unzip -o "packer_${PACKER_VERSION}_linux_${ARCH}.zip" -d /usr/bin
   fi
+
+  # Install plugins as the "spinnaker" user
+  for plugin in $PACKER_PLUGINS; do
+    su - spinnaker -c "/usr/bin/packer plugins install \"github.com/hashicorp/$plugin\""
+  done
 }
 
 install_helm() {
@@ -51,9 +64,25 @@ install_helm3() {
   mv /usr/local/bin/helm /usr/local/bin/helm3
 }
 
+install_aws_cli2() {
+  if [ "$ARCH" = "amd64" ]; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+  elif [ "$ARCH" = "arm64" ]; then
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+    curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_arm64/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+  fi
+  unzip awscliv2.zip
+  ./aws/install
+  rm -rf awscliv2.zip ./aws
+  dpkg -i session-manager-plugin.deb
+  rm session-manager-plugin.deb
+}
+
 create_temp_dir
 install_packer
 install_helm3
 install_helm
+install_aws_cli2
 remove_temp_dir
 install --mode=755 --owner=spinnaker --group=spinnaker --directory  /var/log/spinnaker/rosco
