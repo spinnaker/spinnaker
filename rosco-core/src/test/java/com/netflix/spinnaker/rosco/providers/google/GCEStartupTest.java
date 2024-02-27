71072186c1962521cea126d8d52daedc4f9f4afe
Sandesh <30489233+j-sandy@users.noreply.github.com>
2024-02-27 11:13:30 +0530

    test(core): add tests to verify the rosco startup with various cloud providers enabled (#1073)
    Adding tests for AWS, Azure, Docker, Google and Oracle cloud providers to verify the rosco startup with these providers enabled.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ rosco-core/src/test/java/com/netflix/spinnaker/rosco/providers/google/GCEStartupTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024 OpsMx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.rosco.providers.google;
+
+import com.netflix.spinnaker.rosco.Main;
+import com.netflix.spinnaker.rosco.providers.google.config.RoscoGoogleConfiguration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {Main.class})
+@TestPropertySource(
+    properties = {
+      "spring.application.name=rosco",
+      "google.enabled=true",
+      "rosco.config-dir=/some/path"
+    })
+public class GCEStartupTest {
+
+  @Autowired GCEBakeHandler gceBakeHandler;
+
+  @Autowired RoscoGoogleConfiguration.GCEBakeryDefaults gceBakeryDefaults;
+
+  @Autowired RoscoGoogleConfiguration.GoogleConfigurationProperties googleConfigurationProperties;
+
+  @Test
+  public void startupTest() {}
+}
