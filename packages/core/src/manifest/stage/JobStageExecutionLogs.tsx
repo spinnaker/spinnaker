fd55cf1fb3bca08b931a10bb4c10d65393a20c73
Christos Arvanitis <christos.arvanitis@armory.io>
2024-04-16 11:50:52 +0300

    fix(runJobs): Persist External Log links after the deletion of the pods (#10081)
    * fix(runJobs): Persist External Log links after the deletion of the pods
    
    * fix(runJobs): Persist External Log links after the deletion of the pods
    
    ---------
    
    Co-authored-by: ovidiupopa07 <105648914+ovidiupopa07@users.noreply.github.com>

--- packages/core/src/manifest/stage/JobStageExecutionLogs.tsx
+++ packages/core/src/manifest/stage/JobStageExecutionLogs.tsx
@@ -1,4 +1,4 @@
-import { isEmpty, template } from 'lodash';
+import { template } from 'lodash';
 import React from 'react';
 import { from as observableFrom, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -52,7 +52,7 @@ export class JobStageExecutionLogs extends React.Component<IJobStageExecutionLog
     const { manifest } = this.state;
     const { externalLink, podNamesProviders, location, account } = this.props;
     // prefer links to external logging platforms
-    if (!isEmpty(manifest) && externalLink) {
+    if (externalLink) {
       return (
         <a target="_blank" href={this.renderExternalLink(externalLink, manifest)}>
           Console Output (External)
