291a87258ebbac1961db6a3201bf164702948b8f
Jørgen Jervidalo <jorgen.jervidalo@schibsted.com>
2024-04-29 22:13:35 +0200

    feat(aws): Install AWS CLI v2, upgrade aws-iam-authenticator, remove s3cmd (#6156)
    * feat(aws): Install AWS CLI v2 and upgrade aws-iam-authenticator
    
    * Switch to wget (curl isn't available in the slim images)

--- clouddriver-web/pkg_scripts/postInstall.sh
+++ clouddriver-web/pkg_scripts/postInstall.sh
@@ -3,6 +3,8 @@
 # Remember to also update Dockerfile.*
 KUBECTL_DEFAULT_RELEASE=1.22.17
 KUBECTL_RELEASES="${KUBECTL_DEFAULT_RELEASE} 1.26.12 1.27.9 1.28.5 1.29.0"
+AWS_CLI_VERSION=2.15.22
+AWS_AIM_AUTHENTICATOR_VERSION=0.6.14
 
 # ubuntu
 # check that owner group exists
@@ -26,6 +28,18 @@ install_kubectl() {
   fi
 }
 
+install_awscli2() {
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${AWS_CLI_VERSION}.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  ./aws/install
+  rm -rf ./awscliv2.zip ./aws
+
+  curl "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v${AWS_AIM_AUTHENTICATOR_VERSION}/aws-iam-authenticator_${AWS_AIM_AUTHENTICATOR_VERSION}_linux_amd64" -O aws-iam-authenticator
+  chmod +x ./aws-iam-authenticator \
+  mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+}
+
 install_kubectl
+install_awscli2
 
 install --mode=755 --owner=spinnaker --group=spinnaker --directory /var/log/spinnaker/clouddriver
