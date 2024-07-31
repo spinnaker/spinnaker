e8535ee84afcff90eeea75f472f1f29c87b59f5e
David Byron <dbyron@salesforce.com>
2024-07-31 16:54:15 -0700

    test(orca/web): demonstrate failure of failedStages endpoint
    Yes, the GET /pipelines/running and GET /pipelines/waiting endpoints work as expected
    since they're hard coded to return an empty list.

---
+++ orca/orca-web/src/test/java/com/netflix/spinnaker/orca/controllers/TaskControllerWebTest.java
@@ -0,0 +1,304 @@
+/*
+ * Copyright 2024 Salesforce, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.netflix.spinnaker.orca.controllers;
+
+import static com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType.PIPELINE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import com.netflix.spinnaker.orca.Main;
+import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionStatus;
+import com.netflix.spinnaker.orca.api.pipeline.models.ExecutionType;
+import com.netflix.spinnaker.orca.api.pipeline.models.PipelineExecution;
+import com.netflix.spinnaker.orca.api.pipeline.models.StageExecution;
+import com.netflix.spinnaker.orca.notifications.NotificationClusterLock;
+import com.netflix.spinnaker.orca.pipeline.model.PipelineExecutionImpl;
+import com.netflix.spinnaker.orca.pipeline.model.StageExecutionImpl;
+import com.netflix.spinnaker.orca.pipeline.persistence.ExecutionRepository;
+import com.netflix.spinnaker.orca.q.pending.PendingExecutionService;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest(classes = Main.class)
+@TestPropertySource(
+    properties = {
+      "spring.config.location=classpath:orca-test.yml",
+      "keiko.queue.redis.enabled=false"
+    })
+class TaskControllerWebTest {
+
+  private static final String TEST_EXECUTION_ID = "12345";
+
+  private static final String TEST_STAGE_ID = "stageId";
+
+  private static final String ERROR_MESSAGE_EXECUTION_ID_READ =
+      "Failed to evaluate expression 'hasPermission(this.getPipeline(#executionId)?.application, 'APPLICATION', 'READ')'";
+
+  private static final String ERROR_MESSAGE_ID_READ =
+      "Failed to evaluate expression 'hasPermission(this.getPipeline(#id)?.application, 'APPLICATION', 'READ')'";
+
+  private static final String ERROR_MESSAGE_ID_WRITE =
+      "Failed to evaluate expression 'hasPermission(this.getPipeline(#id)?.application, 'APPLICATION', 'WRITE')'";
+
+  private static final String ERROR_MESSAGE_ID_EXECUTE =
+      "Failed to evaluate expression 'hasPermission(this.getPipeline(#id)?.application, 'APPLICATION', 'EXECUTE')'";
+
+  @Autowired private WebApplicationContext webApplicationContext;
+
+  @MockBean private ExecutionRepository executionRepository;
+
+  @MockBean private PendingExecutionService pendingExecutionService;
+
+  @MockBean private NotificationClusterLock notificationClusterLock;
+
+  private MockMvc webAppMockMvc;
+
+  @BeforeEach
+  void init(TestInfo testInfo) {
+    System.out.println("--------------- Test " + testInfo.getDisplayName());
+
+    webAppMockMvc = webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+  }
+
+  @Test
+  void testFailedStages() throws Exception {
+    webAppMockMvc
+        .perform(
+            get("/pipelines/failedStages")
+                .queryParam("executionId", TEST_EXECUTION_ID)
+                .characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_EXECUTION_ID_READ))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be ok
+  }
+
+  @Test
+  void testDeletePipeline() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    execution.setStatus(ExecutionStatus.SUCCEEDED); // arbitrary
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            delete("/pipelines/" + TEST_EXECUTION_ID)
+                .characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_WRITE))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be ok
+  }
+
+  @Test
+  void testCancel() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            put("/pipelines/" + TEST_EXECUTION_ID + "/cancel")
+                .characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_EXECUTE))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be accepted
+  }
+
+  @Test
+  void testPause() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            put("/pipelines/" + TEST_EXECUTION_ID + "/pause")
+                .characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_EXECUTE))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be accepted
+  }
+
+  @Test
+  void testResume() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            put("/pipelines/" + TEST_EXECUTION_ID + "/resume")
+                .characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_EXECUTE))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be accepted
+  }
+
+  @Test
+  void testRunning() throws Exception {
+    webAppMockMvc
+        .perform(get("/pipelines/running").characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(status().isGone());
+  }
+
+  @Test
+  void testWaiting() throws Exception {
+    webAppMockMvc
+        .perform(get("/pipelines/waiting").characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(status().isGone());
+  }
+
+  @Test
+  void testUpdatePipelineStage() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    StageExecution stage = new StageExecutionImpl(execution, "evaluateVariables"); // arbitrary
+    stage.setId(TEST_STAGE_ID);
+    execution.getStages().add(stage);
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            patch("/pipelines/" + TEST_EXECUTION_ID + "/stages/" + TEST_STAGE_ID)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .characterEncoding(StandardCharsets.UTF_8.toString())
+                .content("{}"))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_EXECUTE))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be ok
+  }
+
+  @Test
+  void testRetryPipelineStage() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            put("/pipelines/" + TEST_EXECUTION_ID + "/stages/" + TEST_STAGE_ID + "/restart")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .characterEncoding(StandardCharsets.UTF_8.toString())
+                .content("{}"))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_EXECUTE))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be ok
+  }
+
+  @Test
+  void testEvaluateExpressionForExecution() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            get("/pipelines/" + TEST_EXECUTION_ID + "/evaluateExpression")
+                .queryParam("expression", "")
+                .characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_READ))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be ok
+  }
+
+  @Test
+  void testEvaluateExpressionForExecutionAtStage() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    StageExecution stage = new StageExecutionImpl(execution, "evaluateVariables"); // arbitrary
+    stage.setId(TEST_STAGE_ID);
+    execution.getStages().add(stage);
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            get("/pipelines/" + TEST_EXECUTION_ID + "/" + TEST_STAGE_ID + "/evaluateExpression")
+                .queryParam("expression", "")
+                .characterEncoding(StandardCharsets.UTF_8.toString()))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_READ))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be ok
+  }
+
+  @Test
+  void testEvaluateVariables() throws Exception {
+    PipelineExecution execution = new PipelineExecutionImpl(ExecutionType.PIPELINE, "test");
+    when(executionRepository.retrieve(PIPELINE, TEST_EXECUTION_ID)).thenReturn(execution);
+
+    webAppMockMvc
+        .perform(
+            post("/pipelines/" + TEST_EXECUTION_ID + "/evaluateVariables")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .characterEncoding(StandardCharsets.UTF_8.toString())
+                .content("[]"))
+        .andDo(print())
+        .andExpect(
+            result ->
+                assertThat(result.getResolvedException())
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(ERROR_MESSAGE_ID_READ))
+        .andExpect(status().isBadRequest()); // FIXME: this needs to be ok
+  }
+}
