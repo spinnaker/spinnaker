1922381b00019e2936e60d68d08624544a2881eb
j-sandy <30489233+j-sandy@users.noreply.github.com>
2024-12-26 11:28:12 +0530

    chore(dependency): upgrade spring boot from 2.7.x to 3.0.x and spring cloud from 2021.0.x to 2022.0.x
    Upgrading spring boot 2.7.18 to 3.0.13 and spring cloud 2021.0.8 to 2022.0.5.
    Spring cloud release 2022.0.x is compatible with spring boot 3.0.x.
    https://github.com/spring-cloud/spring-cloud-release/wiki/Supported-Versions#supported-releases
    ======================================================
    refactor(dependency): replace javax with jakarta during upgrade of spring boot 3.0.x
    
    Spring Boot 3.0 has migrated from Java EE to Jakarta EE APIs for all dependencies.
    https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.0-Release-Notes#third-party-library-upgrades
    So, replacing the imports with `javax` to `jakarta`.

--- fiat/fiat-github/src/main/java/com/netflix/spinnaker/fiat/roles/github/GitHubProperties.java
+++ fiat/fiat-github/src/main/java/com/netflix/spinnaker/fiat/roles/github/GitHubProperties.java
@@ -1,9 +1,9 @@
 package com.netflix.spinnaker.fiat.roles.github;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
