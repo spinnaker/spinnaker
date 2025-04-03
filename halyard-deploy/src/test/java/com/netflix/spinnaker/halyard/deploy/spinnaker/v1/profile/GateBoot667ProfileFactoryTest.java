db1a929b96e0d968b11ab75d3f655b3e58e94c65
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2025-04-03 14:05:27 +0530

    Tests: Add GateServiceTest and GateBoot667ProfileFactoryTest. Also add redirect-uri to the new config.

---
+++ halyard-deploy/src/test/java/com/netflix/spinnaker/halyard/deploy/spinnaker/v1/profile/GateBoot667ProfileFactoryTest.java
@@ -0,0 +1,55 @@
+package com.netflix.spinnaker.halyard.deploy.spinnaker.v1.profile;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.netflix.spinnaker.halyard.config.model.v1.security.ApiSecurity;
+import com.netflix.spinnaker.halyard.config.model.v1.security.Authn;
+import com.netflix.spinnaker.halyard.config.model.v1.security.OAuth2;
+import com.netflix.spinnaker.halyard.config.model.v1.security.Security;
+import com.netflix.spinnaker.halyard.config.model.v1.security.SpringSsl;
+import com.netflix.spinnaker.halyard.config.model.v1.security.X509;
+import com.netflix.spinnaker.halyard.deploy.spinnaker.v1.service.ServiceSettings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class GateBoot667ProfileFactoryTest {
+
+  private GateBoot667ProfileFactory factory;
+  private ServiceSettings mockServiceSettings;
+  private Security mockSecurity;
+  private Authn mockAuthn;
+  private OAuth2 oAuth2;
+
+  @BeforeEach
+  void setUp() {
+    factory = new GateBoot667ProfileFactory();
+    oAuth2 = new OAuth2();
+    oAuth2.setEnabled(true);
+    oAuth2.setProvider(OAuth2.Provider.GOOGLE);
+    mockServiceSettings = mock(ServiceSettings.class);
+    mockSecurity = mock(Security.class);
+    mockAuthn = mock(Authn.class);
+    ApiSecurity mockApiSecurity = mock(ApiSecurity.class);
+    when(mockSecurity.getAuthn()).thenReturn(mockAuthn);
+    when(mockAuthn.getOauth2()).thenReturn(oAuth2);
+    X509 x509 = new X509();
+    when(mockAuthn.getX509()).thenReturn(x509);
+
+    when(mockSecurity.getApiSecurity()).thenReturn(mockApiSecurity);
+    SpringSsl springSsl = mock(SpringSsl.class);
+    when(mockApiSecurity.getSsl()).thenReturn(springSsl);
+  }
+
+  @Test
+  void testGetGateConfigWithOAuth2Enabled() {
+    GateProfileFactory.GateConfig config = factory.getGateConfig(mockServiceSettings, mockSecurity);
+    assertNotNull(config);
+    assertNotNull(config.getSpring());
+    assertNotNull(config.getSpring().getSecurity());
+  }
+}
