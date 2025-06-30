5a2bfff77f852299be57f668b9f021b31321bbf8
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2025-06-30 10:10:24 +0530

    refactor(security): Replace the use of WebSecurityConfigurerAdapter with a configuration that defines SecurityFilterChain bean. The error occurs because WebSecurityConfigurerAdapter has been deprecated and removed in Spring Security 6.x, which is used by Spring Boot 3.x.
    Error details for reference:
    ```
    gate/gate-core/src/main/java/com/netflix/spinnaker/gate/security/anonymous/AnonymousConfig.java:52: error: cannot find symbol
    public class AnonymousConfig extends WebSecurityConfigurerAdapter {
                                         ^
      symbol: class WebSecurityConfigurerAdapter
    ```
    Reference:
    https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

--- gate/gate-iap/src/main/java/com/netflix/spinnaker/gate/security/iap/IapSsoConfig.java
+++ gate/gate-iap/src/main/java/com/netflix/spinnaker/gate/security/iap/IapSsoConfig.java
@@ -33,7 +33,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 /**
@@ -46,7 +46,7 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableWebSecurity
 @ConditionalOnExpression("${google.iap.enabled:false}")
 @EnableConfigurationProperties(IapSecurityConfigProperties.class)
-public class IapSsoConfig extends WebSecurityConfigurerAdapter {
+public class IapSsoConfig {
 
   @Autowired AuthConfig authConfig;
 
@@ -80,8 +80,8 @@ public class IapSsoConfig extends WebSecurityConfigurerAdapter {
     long expirationTimeAllowedSkew = 30000L;
   }
 
-  @Override
-  public void configure(HttpSecurity http) throws Exception {
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     log.info("IAP JWT token verification is enabled.");
 
     Preconditions.checkNotNull(
@@ -98,6 +98,6 @@ public class IapSsoConfig extends WebSecurityConfigurerAdapter {
         "IAP security expirationTimeAllowedSkew value must be >= 0.");
 
     authConfig.configure(http);
-    http.addFilterBefore(iapAuthenticationFilter(), BasicAuthenticationFilter.class);
+    return http.addFilterBefore(iapAuthenticationFilter(), BasicAuthenticationFilter.class).build();
   }
 }
