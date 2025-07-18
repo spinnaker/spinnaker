293a28efedb38e41657c665510c2b875c092a8d1
kirangodishala <kiran@opsmx.io>
2025-07-18 17:26:57 +0530

    test(rosco/retrofit2): add test to demonstrate the issue of having more than one slash in the base url.

---
+++ rosco/rosco-core/src/test/java/com/netflix/spinnaker/rosco/services/ClouddriverServiceTest.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2025 OpsMx, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.rosco.services;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.netflix.spectator.api.NoopRegistry;
+import com.netflix.spinnaker.config.OkHttp3ClientConfiguration;
+import com.netflix.spinnaker.config.OkHttpMetricsInterceptorProperties;
+import com.netflix.spinnaker.config.RetrofitConfiguration;
+import com.netflix.spinnaker.config.okhttp3.RawOkHttpClientFactory;
+import com.netflix.spinnaker.kork.artifacts.model.Artifact;
+import com.netflix.spinnaker.kork.dynamicconfig.DynamicConfigService;
+import com.netflix.spinnaker.kork.retrofit.Retrofit2SyncCall;
+import com.netflix.spinnaker.kork.retrofit.exceptions.SpinnakerHttpException;
+import com.netflix.spinnaker.okhttp.OkHttp3MetricsInterceptor;
+import com.netflix.spinnaker.okhttp.OkHttpClientConfigurationProperties;
+import com.netflix.spinnaker.okhttp.Retrofit2EncodeCorrectionInterceptor;
+import com.netflix.spinnaker.okhttp.SpinnakerRequestHeaderInterceptor;
+import com.netflix.spinnaker.okhttp.SpinnakerRequestInterceptor;
+import com.netflix.spinnaker.retrofit.Retrofit2ConfigurationProperties;
+import com.netflix.spinnaker.retrofit.RetrofitConfigurationProperties;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import retrofit.RequestInterceptor;
+
+@SpringBootTest(
+    classes = {
+      ServiceConfig.class,
+      ClouddriverServiceTest.TestConfig.class,
+      DynamicConfigService.NoopDynamicConfig.class,
+      NoopRegistry.class,
+      Retrofit2EncodeCorrectionInterceptor.class,
+      RetrofitConfiguration.class,
+      Retrofit2ConfigurationProperties.class,
+      OkHttp3MetricsInterceptor.class,
+      RawOkHttpClientFactory.class,
+      RetrofitConfigurationProperties.class,
+      OkHttpClientConfigurationProperties.class,
+      OkHttpMetricsInterceptorProperties.class,
+      ObjectMapper.class,
+      OkHttp3ClientConfiguration.class
+    })
+public class ClouddriverServiceTest {
+  @RegisterExtension
+  static WireMockExtension wmClouddriver =
+      WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
+
+  @Autowired ClouddriverService clouddriverService;
+
+  static String WM_CORRECT_ENDPOINT =
+      "/api/artifacts/fetch/"; // since the baseUrl has /api/ at the end.
+  static String WM_INCORRECT_ENDPOINT = "/artifacts/fetch/";
+  static String WM_CORRECT_URL;
+  static String WM_INCORRECT_URL;
+
+  @DynamicPropertySource
+  static void registerUrls(DynamicPropertyRegistry registry) {
+    String baseUrl = wmClouddriver.baseUrl() + "/api/";
+    System.out.println("wiremock clouddriver url: " + baseUrl);
+    registry.add("services.clouddriver.base-url", () -> baseUrl);
+  }
+
+  @Test
+  void testBaseUrlWithMultipleSlashes() {
+    WM_CORRECT_URL = wmClouddriver.baseUrl() + WM_CORRECT_ENDPOINT;
+    WM_INCORRECT_URL = wmClouddriver.baseUrl() + WM_INCORRECT_ENDPOINT;
+    wmClouddriver.stubFor(
+        WireMock.put(urlEqualTo(WM_CORRECT_ENDPOINT))
+            .willReturn(aResponse().withStatus(200).withBody("{\"message\": \"success\"}")));
+
+    wmClouddriver.stubFor(
+        WireMock.put(urlEqualTo(WM_INCORRECT_ENDPOINT))
+            .willReturn(aResponse().withStatus(400).withBody("bad request")));
+    Artifact artifact = Artifact.builder().build();
+
+    // FIXME: Fix this issue occurring when base url has multiple slashes like http://.../api/
+    // instead of single slash at the end.
+    Throwable thrown =
+        catchThrowable(() -> Retrofit2SyncCall.execute(clouddriverService.fetchArtifact(artifact)));
+    assertThat(thrown).isInstanceOf(SpinnakerHttpException.class);
+    assertThat(thrown.getMessage())
+        .contains("Status: 400, Method: PUT, URL: " + WM_INCORRECT_URL + ", Message: Bad Request");
+
+    wmClouddriver.verify(0, WireMock.putRequestedFor(urlEqualTo(WM_CORRECT_ENDPOINT)));
+    wmClouddriver.verify(1, WireMock.putRequestedFor(urlEqualTo(WM_INCORRECT_ENDPOINT)));
+  }
+
+  @Configuration
+  static class TestConfig {
+    @Bean
+    public SpinnakerRequestHeaderInterceptor spinnakerRequestHeaderInterceptor() {
+      return new SpinnakerRequestHeaderInterceptor(false);
+    }
+    // SpinnakerRequestInterceptor is not needed for retrofit2 client but due to the way retrofit1
+    // and retrofit2 specific beans are mixed up in kork, this is needed for now. Once the beans
+    // are separated this can be removed
+    @Bean
+    public RequestInterceptor spinnakerRequestInterceptor() {
+      return new SpinnakerRequestInterceptor(false);
+    }
+  }
+}
