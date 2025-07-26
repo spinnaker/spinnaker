46a0a5f307727d6dbdf4aa5a4a6c61aa7ede8f64
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2025-07-26 08:01:16 +0530

    tests(front50): remove temporary security override used during migration to Spring Boot 3.0

--- front50/front50-integration/src/test/java/com/netflix/spinnaker/front50/CompositeStorageContainerSqlToProgressTest.java
+++ front50/front50-integration/src/test/java/com/netflix/spinnaker/front50/CompositeStorageContainerSqlToProgressTest.java
@@ -95,8 +95,7 @@ class CompositeStorageContainerSqlToProgressTest {
             .dependsOn(mysql)
             .dependsOn(postgres)
             .waitingFor(Wait.forHealthcheck().withStartupTimeout(Duration.ofSeconds(90)))
-            .withEnv("SPRING_APPLICATION_JSON", getSpringApplicationJson())
-            .withEnv("SPRING_PROFILES_ACTIVE", "noauth-test");
+            .withEnv("SPRING_APPLICATION_JSON", getSpringApplicationJson());
 
     Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
     front50Container.start();
