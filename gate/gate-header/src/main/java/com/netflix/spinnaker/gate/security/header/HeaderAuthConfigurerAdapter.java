997e5bf4a5e326b8166660a54760182000537b0b
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2025-08-18 21:01:46 +0530

    refactor(security): remove explicit sessionManagement stateless config as part of springboot 3 upgrade

--- gate/gate-header/src/main/java/com/netflix/spinnaker/gate/security/header/HeaderAuthConfigurerAdapter.java
+++ gate/gate-header/src/main/java/com/netflix/spinnaker/gate/security/header/HeaderAuthConfigurerAdapter.java
@@ -24,7 +24,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 
@@ -45,20 +44,6 @@ public class HeaderAuthConfigurerAdapter {
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     authConfig.configure(http);
     http.addFilter(requestHeaderAuthenticationFilter);
-
-    // Save the work to read and write session information.  Each request
-    // provides X-SPINNAKER-USER, and gate caches information from fiat, so
-    // there's no need for callers to support session cookies, and dealing with
-    // expiration, etc.
-    //
-    // With this, when services.fiat.legacyFallback is false, FiatSessionFilter
-    // doesn't ever do meaningful work because request.getSession() always returns
-    // null, so save some cycles by setting fiat.session-filter.enabled to false.
-    //
-    // When services.fiat.legacyFallback is true, FiatSessionFilter still
-    // invalidates the cache for the user.
-    http.sessionManagement(
-        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     return http.build();
   }
 }
