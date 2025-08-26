601593d3f3247dd5720764c9f8519239c2e5d72b
Cristhian Castaneda <cristhian_rivera@apple.com>
2025-08-26 16:18:40 -0600

    refactor(kubernetes): yarn prettier

--- deck/packages/kubernetes/src/rawResource/rawResource.dataSource.ts
+++ deck/packages/kubernetes/src/rawResource/rawResource.dataSource.ts
@@ -15,22 +15,21 @@ const fetchK8sResources = (application: Application): PromiseLike<ApiK8sResource
 const formatK8sResources = (_: Application, result: ApiK8sResource): PromiseLike<ApiK8sResource> =>
   Promise.resolve(result);
 
-module(KUBERNETS_RAW_RESOURCE_DATA_SOURCE, [])
-  .run(() => {
-    ApplicationDataSourceRegistry.registerDataSource({
-      key: KUBERNETS_RAW_RESOURCE_DATA_SOURCE_KEY,
-      label: 'Kubernetes',
-      category: INFRASTRUCTURE_KEY,
-      sref: KUBERNETS_RAW_RESOURCE_DATA_SOURCE_SREF,
-      primary: true,
-      icon: 'fas fa-xs fa-fw fa-th-large',
-      iconName: 'spMenuK8s',
-      loader: fetchK8sResources,
-      onLoad: formatK8sResources,
-      providerField: 'cloudProvider',
-      credentialsField: 'account',
-      regionField: 'region',
-      description: 'Collections of kubernetes resources',
-      defaultData: [],
-    });
+module(KUBERNETS_RAW_RESOURCE_DATA_SOURCE, []).run(() => {
+  ApplicationDataSourceRegistry.registerDataSource({
+    key: KUBERNETS_RAW_RESOURCE_DATA_SOURCE_KEY,
+    label: 'Kubernetes',
+    category: INFRASTRUCTURE_KEY,
+    sref: KUBERNETS_RAW_RESOURCE_DATA_SOURCE_SREF,
+    primary: true,
+    icon: 'fas fa-xs fa-fw fa-th-large',
+    iconName: 'spMenuK8s',
+    loader: fetchK8sResources,
+    onLoad: formatK8sResources,
+    providerField: 'cloudProvider',
+    credentialsField: 'account',
+    regionField: 'region',
+    description: 'Collections of kubernetes resources',
+    defaultData: [],
   });
+});
