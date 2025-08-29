bd04696265bbc1a6c45d2d10b1e9b78dc29b073c
kirangodishala <kiran@opsmx.io>
2025-08-29 22:03:13 +0530

    test(halyard/retrofit2): demonstrate the issue caused due to passing a concrete class object to @Body parameter whose type is an abstract class.

--- halyard/halyard-cli/src/test/java/com/netflix/spinnaker/halyard/cli/services/v1/DaemonServiceTest.java
+++ halyard/halyard-cli/src/test/java/com/netflix/spinnaker/halyard/cli/services/v1/DaemonServiceTest.java
@@ -18,7 +18,10 @@
 package com.netflix.spinnaker.halyard.cli.services.v1;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.put;
+import static com.github.tomakehurst.wiremock.client.WireMock.putRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,6 +30,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.netflix.spinnaker.halyard.config.model.v1.security.OAuth2;
 import com.netflix.spinnaker.halyard.core.DaemonResponse;
 import com.netflix.spinnaker.halyard.core.problem.v1.Problem;
 import com.netflix.spinnaker.halyard.core.problem.v1.ProblemBuilder;
@@ -84,6 +88,36 @@ public class DaemonServiceTest {
     assertThat(retrievedTask.getResponse().getProblemSet().getProblems().get(0))
         .usingComparator(compareBySeverityAndMessage)
         .isEqualTo(problem);
+  }
+
+  @Test
+  public void testSetAuthnMethod() throws JsonProcessingException {
+    DaemonTask task = new DaemonTask<>("testTask", 1000);
+    wmDaemon.stubFor(
+        put(urlEqualTo("/v1/config/deployments/default/security/authn/oauth2/?validate=true"))
+            .willReturn(
+                aResponse().withStatus(200).withBody(objectMapper.writeValueAsString(task))));
+
+    OAuth2 oAuth2 = new OAuth2();
+    oAuth2.setEnabled(true);
+    OAuth2.Client client = new OAuth2.Client();
+    client.setClientId("client-id");
+    client.setClientSecret("cf86e306218cf86e306218cf86e306218");
+    client.setScope("user:email");
+    oAuth2.setClient(client);
+    oAuth2.setProvider(OAuth2.Provider.GITHUB);
+
+    daemonService.setAuthnMethod("default", "oauth2", true, oAuth2);
+
+    String expectedBody =
+        "{\"enabled\":true,\"client\":{\"clientId\":\"client-id\",\"clientSecret\":\"cf86e306218cf86e306218cf86e306218\",\"accessTokenUri\":\"https://github.com/login/oauth/access_token\",\"userAuthorizationUri\":\"https://github.com/login/oauth/authorize\",\"clientAuthenticationScheme\":null,\"scope\":\"user:email\",\"preEstablishedRedirectUri\":null,\"useCurrentUri\":null},\"userInfoRequirements\":null,\"resource\":{\"userInfoUri\":\"https://api.github.com/user\"},\"userInfoMapping\":{\"email\":\"email\",\"firstName\":\"\",\"lastName\":\"name\",\"username\":\"login\"},\"provider\":\"GITHUB\"}";
+
+    // FIXME: most of the data is missing in the actualBody when the object is serialized
+    String actualBody = "{\"enabled\":true}";
+    wmDaemon.verify(
+        putRequestedFor(
+                urlEqualTo("/v1/config/deployments/default/security/authn/oauth2/?validate=true"))
+            .withRequestBody(equalTo(actualBody)));
   }
 
   private <C, T> DaemonTask<C, T> getTask(ResponseBody body) {
