8052291918f05d994ac26e17e0d95470e5fc6239
rahul-chekuri <rahulgandhi.chekuri@opsmx.io>
2025-09-23 17:34:22 +0530

    fix: Address spotlessJavaCheck format issues

--- gate/gate-oauth2/src/main/java/com/netflix/spinnaker/gate/security/oauth2/OAuth2SsoConfig.java
+++ gate/gate-oauth2/src/main/java/com/netflix/spinnaker/gate/security/oauth2/OAuth2SsoConfig.java
@@ -27,10 +27,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
 import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.stereotype.Component;
 
@@ -44,54 +44,58 @@ public class OAuth2SsoConfig {
   @Autowired private SpinnakerOAuth2UserInfoService customOAuth2UserService;
   @Autowired private SpinnakerOIDCUserInfoService oidcUserInfoService;
   @Autowired private DefaultCookieSerializer defaultCookieSerializer;
-  @Autowired private OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> tokenResponseClient;
+
+  @Autowired
+  private OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> tokenResponseClient;
 
   @Bean
   // ManagedDeliverySchemaEndpointConfiguration#schemaSecurityFilterChain should go first
   @Order(2)
   SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-      defaultCookieSerializer.setSameSite(null);
-      authConfig.configure(httpSecurity);
-      httpSecurity
+    defaultCookieSerializer.setSameSite(null);
+    authConfig.configure(httpSecurity);
+    httpSecurity
         .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
         .oauth2Login(
-          oauth2 -> oauth2
-            .userInfoEndpoint(
-              userInfo ->
-                userInfo
-                  .userService(customOAuth2UserService)
-                  .oidcUserService(oidcUserInfoService))
-            // Using same token response client that get sets by default this is to allows
-            // injection of a mock or test implementation
-            // for unit/integration tests, so we don't need to call GitHub (or any real
-            // OAuth2 provider)
-            .tokenEndpoint()
-            .accessTokenResponseClient(tokenResponseClient));
-      return httpSecurity.build();
-    }
-
-    @Bean
-    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> tokenResponseClient() {
-      return new DefaultAuthorizationCodeTokenResponseClient();
-    }
-
-    /**
-     * Use this class to specify how to map fields from the userInfoUri response to what's expected to
-     * be in the User.
-     */
-    @Component
-    @ConfigurationProperties("spring.security.oauth2.client.registration.user-info-mapping")
-    @Data
-    public static class UserInfoMapping {
-      private String email = "email";
-      private String firstName = "given_name";
-      private String lastName = "family_name";
-      private String username = "email";
-      private String serviceAccountEmail = "client_email";
-      private String roles = null;
-    }
-
-    @Component
-    @ConfigurationProperties("spring.security.oauth2.client.registration.user-info-requirements")
-    public static class UserInfoRequirements extends HashMap<String, String> {}
+            oauth2 ->
+                oauth2
+                    .userInfoEndpoint(
+                        userInfo ->
+                            userInfo
+                                .userService(customOAuth2UserService)
+                                .oidcUserService(oidcUserInfoService))
+                    // Using same token response client that get sets by default this is to allows
+                    // injection of a mock or test implementation
+                    // for unit/integration tests, so we don't need to call GitHub (or any real
+                    // OAuth2 provider)
+                    .tokenEndpoint()
+                    .accessTokenResponseClient(tokenResponseClient));
+    return httpSecurity.build();
   }
+
+  @Bean
+  public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest>
+      tokenResponseClient() {
+    return new DefaultAuthorizationCodeTokenResponseClient();
+  }
+
+  /**
+   * Use this class to specify how to map fields from the userInfoUri response to what's expected to
+   * be in the User.
+   */
+  @Component
+  @ConfigurationProperties("spring.security.oauth2.client.registration.user-info-mapping")
+  @Data
+  public static class UserInfoMapping {
+    private String email = "email";
+    private String firstName = "given_name";
+    private String lastName = "family_name";
+    private String username = "email";
+    private String serviceAccountEmail = "client_email";
+    private String roles = null;
+  }
+
+  @Component
+  @ConfigurationProperties("spring.security.oauth2.client.registration.user-info-requirements")
+  public static class UserInfoRequirements extends HashMap<String, String> {}
+}
