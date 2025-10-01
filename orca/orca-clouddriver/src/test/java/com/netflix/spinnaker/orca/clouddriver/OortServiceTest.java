bcef2816fb0372d65a4f7aec9a2f7dcd28ccbbc2
kirangodishala <kiran@opsmx.io>
2025-10-01 18:04:14 +0530

    test(orca/retrofit): add a test to demonstrate an issue when a path parameter contains colon character and API expects it to be not encoded

--- orca/orca-clouddriver/src/test/java/com/netflix/spinnaker/orca/clouddriver/OortServiceTest.java
+++ orca/orca-clouddriver/src/test/java/com/netflix/spinnaker/orca/clouddriver/OortServiceTest.java
@@ -21,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.netflix.spinnaker.kork.retrofit.Retrofit2SyncCall;
+import com.netflix.spinnaker.okhttp.Retrofit2EncodeCorrectionInterceptor;
 import java.util.Map;
 import okhttp3.ResponseBody;
 import org.junit.jupiter.api.AfterEach;
@@ -41,6 +42,10 @@ class OortServiceTest {
     Retrofit retrofit =
         new Retrofit.Builder()
             .baseUrl(mockServer.baseUrl())
+            .client(
+                new okhttp3.OkHttpClient.Builder()
+                    .addInterceptor(new Retrofit2EncodeCorrectionInterceptor())
+                    .build())
             .addConverterFactory(JacksonConverterFactory.create())
             .build();
 
@@ -80,7 +85,15 @@ class OortServiceTest {
                     .withBody("{\"message\": \"success\"}")));
 
     Map map = Retrofit2SyncCall.execute(oortService.getCloudFormationStack(stackId));
+    // FIXME: map is supposed to receive a response body if the request matches the stub above.
+    //
+    //                                                Request was not matched
+    //                                               =======================
+    // Closest stub:
+    // /aws/cloudFormation/stacks/arn:aws:cloudformation:us-west-2:123456789012:stack/my-stack/50d6f6c0-e4a3-11e4-8f3c-500c217fbb7a
+    // Request:
+    // /aws/cloudFormation/stacks/arn%3Aaws%3Acloudformation%3Aus-west-2%3A123456789012%3Astack/my-stack/50d6f6c0-e4a3-11e4-8f3c-500c217fbb7a
 
-    assertThat(map).isNotNull();
+    assertThat(map).isNull();
   }
 }
