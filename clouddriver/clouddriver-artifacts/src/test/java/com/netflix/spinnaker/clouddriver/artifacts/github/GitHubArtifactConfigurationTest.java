a5d975a2cac25b9b3e44cae61ffaa9ea3acf53ab
Jason McIntosh <jason.mcintosh@harness.io>
2025-10-17 10:38:12 -0500

    fix(config): Enable not having to set ALL fields on a configuration property for URL restrictions

---
+++ clouddriver/clouddriver-artifacts/src/test/java/com/netflix/spinnaker/clouddriver/artifacts/github/GitHubArtifactConfigurationTest.java
@@ -0,0 +1,38 @@
+package com.netflix.spinnaker.clouddriver.artifacts.github;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.netflix.spinnaker.clouddriver.artifacts.ArtifactCredentialsRepository;
+import com.netflix.spinnaker.config.ArtifactConfiguration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@EnableConfigurationProperties(value = GitHubArtifactProviderProperties.class)
+@Import({
+  GitHubArtifactConfiguration.class,
+  ArtifactConfiguration.class,
+  TestConfigurationForArtifacts.class
+})
+@TestPropertySource(locations = "classpath:github-account-test.properties")
+class GitHubArtifactConfigurationTest {
+  @Autowired ArtifactCredentialsRepository credentialsRepository;
+
+  @Test
+  public void verifyDefaultsGetSet() {
+    GitHubArtifactCredentials credentialsForType =
+        (GitHubArtifactCredentials)
+            credentialsRepository.getCredentialsForType("test", "github/file");
+    System.out.println(credentialsForType.getClass().getCanonicalName());
+    assertThat(credentialsForType).isNotNull();
+    assertThat(credentialsForType.getAccount().getUrlRestrictions().getAllowedSchemes())
+        .contains("http", "https");
+    assertThat(credentialsForType.getAccount().getUrlRestrictions().getAllowedDomains())
+        .contains("github.com");
+  }
+}
