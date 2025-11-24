a2d49be033d790d0fcdd565c58fb2e49504b4fbe
kirangodishala <kiran@opsmx.io>
2025-11-24 21:38:21 +0530

    refactor(artifacts): stop sharing an OkHttpClient bean between artifact types to pave the way to make artifact support configurable (e.g. only including support for some subset of artifact types).
    As well, although it's not implemented today, it seems reasonable to configure different
    OkHttpClient properties for different artifact types

--- clouddriver/clouddriver-artifacts/src/main/java/com/netflix/spinnaker/config/ArtifactConfiguration.java
+++ clouddriver/clouddriver-artifacts/src/main/java/com/netflix/spinnaker/config/ArtifactConfiguration.java
@@ -24,7 +24,6 @@ import com.netflix.spinnaker.credentials.CredentialsTypeBaseConfiguration;
 import com.netflix.spinnaker.credentials.CredentialsTypeProperties;
 import java.util.List;
 import java.util.stream.Collectors;
-import okhttp3.OkHttpClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -35,11 +34,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @ComponentScan("com.netflix.spinnaker.clouddriver.artifacts")
 public class ArtifactConfiguration {
-  @Bean
-  OkHttpClient okHttpClient() {
-    return new OkHttpClient();
-  }
-
   @Bean
   public ArtifactCredentialsRepository artifactCredentialsRepository(
       ApplicationContext applicationContext,
