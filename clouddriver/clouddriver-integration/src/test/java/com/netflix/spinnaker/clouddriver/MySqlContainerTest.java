512aa39f8368cd094b604a6b9f4e3a14641cc6e7
kirangodishala <kiran@opsmx.io>
2025-12-23 13:52:45 +0530

    test(clouddriver-integration): update MySqlContainerTest to demonstrate the issue of multiple OkHttpClient beans when artifact types - http, helm and github etc - that use an okHttpClient bean are enabled.

--- clouddriver/clouddriver-integration/src/test/java/com/netflix/spinnaker/clouddriver/MySqlContainerTest.java
+++ clouddriver/clouddriver-integration/src/test/java/com/netflix/spinnaker/clouddriver/MySqlContainerTest.java
@@ -16,8 +16,12 @@
 
 package com.netflix.spinnaker.clouddriver;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,8 +29,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.ContainerLaunchException;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -57,11 +61,12 @@ public class MySqlContainerTest extends BaseContainerTest {
     jdbcUrl = String.format("jdbc:mysql://%s:%d/clouddriver", MYSQL_NETWORK_ALIAS, MYSQL_PORT);
     clouddriverContainer
         .dependsOn(mysql)
-        .withEnv("SPRING_APPLICATION_JSON", getSpringApplicationJson())
-        .start();
+        .withEnv("SPRING_APPLICATION_JSON", getSpringApplicationJson());
+    // .start();
 
-    Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
-    clouddriverContainer.followOutput(logConsumer);
+    // Only valid once the container has started
+    // Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
+    // clouddriverContainer.followOutput(logConsumer);
   }
 
   private String getSpringApplicationJson() throws JsonProcessingException {
@@ -83,8 +88,50 @@ public class MySqlContainerTest extends BaseContainerTest {
             "false",
             "sql.migration",
             migration);
+    Map<String, Object> allProperties = new HashMap<>();
+    String artifactTypes = System.getProperty("artifactTypes", "");
+    if (artifactTypes.contains("bitbucket")) {
+      allProperties.put("artifacts.bitbucket.enabled", "true");
+    }
+    if (artifactTypes.contains("docker") || artifactTypes.contains("kubernetes")) {
+      allProperties.put("kubernetes.enabled", "true");
+    }
+    if (artifactTypes.contains("gcs")) {
+      allProperties.put("artifacts.gcs.enabled", "true");
+    }
+    if (artifactTypes.contains("github")) {
+      allProperties.put("artifacts.github.enabled", "true");
+    }
+    if (artifactTypes.contains("gitlab")) {
+      allProperties.put("artifacts.gitlab.enabled", "true");
+    }
+    if (artifactTypes.contains("git-repo")) {
+      allProperties.put("artifacts.git-repo.enabled", "true");
+    }
+    if (artifactTypes.contains("helm")) {
+      allProperties.put("artifacts.helm.enabled", "true");
+    }
+    if (artifactTypes.contains("http")) {
+      allProperties.put("artifacts.http.enabled", "true");
+    }
+    if (artifactTypes.contains("ivy")) {
+      allProperties.put("artifacts.ivy.enabled", "true");
+    }
+    if (artifactTypes.contains("jenkins")) {
+      allProperties.put("jenkins.enabled", "true");
+    }
+    if (artifactTypes.contains("maven")) {
+      allProperties.put("artifacts.maven.enabled", "true");
+    }
+    if (artifactTypes.contains("oracle")) {
+      allProperties.put("artifacts.oracle.enabled", "true");
+    }
+    if (artifactTypes.contains("s3")) {
+      allProperties.put("artifacts.s3.enabled", "true");
+    }
+    allProperties.putAll(properties);
     ObjectMapper mapper = new ObjectMapper();
-    return mapper.writeValueAsString(properties);
+    return mapper.writeValueAsString(allProperties);
   }
 
   @AfterAll
@@ -98,8 +145,32 @@ public class MySqlContainerTest extends BaseContainerTest {
     }
   }
 
+  // FIXME: expect the container to start
+  //
+  // @Test
+  // void testHealthCheckWithMySql() throws Exception {
+  //   super.testHealthCheck();
+  // }
+
   @Test
-  void testHealthCheckWithMySql() throws Exception {
-    super.testHealthCheck();
+  void testFailToStart() {
+    assertThatThrownBy(() -> clouddriverContainer.start())
+        .isInstanceOf(ContainerLaunchException.class)
+        .hasMessage("Container startup failed for image " + dockerImageName.toString());
+    assertThat(clouddriverContainer.getLogs())
+        .contains(
+            """
+***************************
+APPLICATION FAILED TO START
+***************************
+
+Description:
+
+Parameter 0 of constructor in com.netflix.spinnaker.config.okhttp3.InsecureOkHttpClientBuilderProvider required a single bean, but 5 were found:
+\t- bitbucketOkHttpClient: defined by method 'bitbucketOkHttpClient' in class path resource [com/netflix/spinnaker/clouddriver/artifacts/bitbucket/BitbucketArtifactConfiguration.class]
+\t- gitHubOkHttpClient: defined by method 'gitHubOkHttpClient' in class path resource [com/netflix/spinnaker/clouddriver/artifacts/github/GitHubArtifactConfiguration.class]
+\t- gitlabOkHttpClient: defined by method 'gitlabOkHttpClient' in class path resource [com/netflix/spinnaker/clouddriver/artifacts/gitlab/GitlabArtifactConfiguration.class]
+\t- helmOkHttpClient: defined by method 'helmOkHttpClient' in class path resource [com/netflix/spinnaker/clouddriver/artifacts/helm/HelmArtifactConfiguration.class]
+\t- httpOkHttpClient: defined by method 'httpOkHttpClient' in class path resource [com/netflix/spinnaker/clouddriver/artifacts/http/HttpArtifactConfiguration.class]""");
   }
 }
