f365b9a1c148991b349e80837f60dcd9c1c805a0
Jason McIntosh <jason.mcintosh@harness.io>
2026-01-06 17:10:48 -0600

    fix(gcp): Fiat batch queries on rate limit would randomly fail the entire operation and not retry appropriately.  Fixes how groups are pulled from GCP to really use rate limits AND retries appropriately at the expense of moving from batch logic

--- fiat/fiat-google-groups/src/main/java/com/netflix/spinnaker/fiat/roles/google/GoogleDirectoryUserRolesProvider.java
+++ fiat/fiat-google-groups/src/main/java/com/netflix/spinnaker/fiat/roles/google/GoogleDirectoryUserRolesProvider.java
@@ -16,13 +16,7 @@
 
 package com.netflix.spinnaker.fiat.roles.google;
 
-import com.google.api.client.googleapis.batch.BatchRequest;
-import com.google.api.client.googleapis.batch.json.JsonBatchCallback;
-import com.google.api.client.googleapis.json.GoogleJsonError;
-import com.google.api.client.googleapis.json.GoogleJsonErrorContainer;
 import com.google.api.client.http.HttpBackOffUnsuccessfulResponseHandler;
-import com.google.api.client.http.HttpHeaders;
-import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
@@ -34,6 +28,7 @@ import com.google.api.services.directory.model.Groups;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.common.annotations.VisibleForTesting;
 import com.netflix.spinnaker.fiat.model.resources.Role;
 import com.netflix.spinnaker.fiat.permissions.ExternalUser;
 import com.netflix.spinnaker.fiat.roles.UserRolesProvider;
@@ -50,11 +45,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -65,6 +60,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import org.springframework.util.ObjectUtils;
 
 @Slf4j
 @Component
@@ -75,6 +71,9 @@ public class GoogleDirectoryUserRolesProvider implements UserRolesProvider, Init
   // Used to denote a shared service account, used by service account shared between pipelines by
   // unique role
   public static final String SHARED_SERVICE_ACCOUNT_SUFFIX = "@shared-managed-service-account";
+  // https://docs.cloud.google.com/java/docs/reference/google-http-client/latest/com.google.api.client.http.javanet.NetHttpTransport  - use a single globally shared one of these.  AKA
+  // DO NOT recreate per run.
+  private static final HttpTransport httpTransport = new NetHttpTransport();
 
   @Autowired @Setter private Config config;
 
@@ -87,86 +86,69 @@ public class GoogleDirectoryUserRolesProvider implements UserRolesProvider, Init
     Assert.state(config.getAdminUsername() != null, "Supply an admin username");
   }
 
-  @Data
-  @EqualsAndHashCode(callSuper = true)
-  private class GroupBatchCallback extends JsonBatchCallback<Groups> {
-
-    Map<String, Collection<Role>> emailGroupsMap;
-
-    String email;
-
-    @Override
-    public void onFailure(GoogleJsonError e, HttpHeaders responseHeaders) throws IOException {
-      log.warn("Failed to fetch groups for user " + email + ": " + e.getMessage());
-    }
-
-    @Override
-    public void onSuccess(Groups groups, HttpHeaders responseHeaders) throws IOException {
-      if (groups == null || groups.getGroups() == null || groups.getGroups().isEmpty()) {
-        log.debug("No groups found for user " + email);
-        return;
-      }
-
-      Set<Role> groupSet =
-          groups.getGroups().stream().flatMap(toRoleFn()).collect(Collectors.toSet());
-      emailGroupsMap.put(email, groupSet);
-    }
-  }
-
   @Override
   public Map<String, Collection<Role>> multiLoadRoles(Collection<ExternalUser> users) {
     if (users == null || users.isEmpty()) {
       return new HashMap<>();
     }
 
-    Collection<String> userEmails =
-        users.stream().map(ExternalUser::getId).collect(Collectors.toList());
+    Collection<String> userEmails = users.stream().map(ExternalUser::getId).toList();
     HashMap<String, Collection<Role>> emailGroupsMap = new HashMap<>();
-    Directory service = getDirectoryService();
-    BatchRequest batch = service.batch();
-    userEmails.forEach(
-        email -> {
-          try {
-            // Check if this is a managed service account, we should never check google groups for
-            // these
-            if (email.endsWith(SERVICE_ACCOUNT_SUFFIX)
-                || email.endsWith(SHARED_SERVICE_ACCOUNT_SUFFIX)) {
-              // Skip over this in the batch
-              return;
-            }
-
-            GroupBatchCallback callback =
-                new GroupBatchCallback().setEmailGroupsMap(emailGroupsMap).setEmail(email);
-            HttpRequest request =
-                service
-                    .groups()
-                    .list()
-                    .setDomain(config.getDomain())
-                    .setUserKey(email)
-                    .buildHttpRequest();
-            HttpBackOffUnsuccessfulResponseHandler handler =
-                new HttpBackOffUnsuccessfulResponseHandler(new ExponentialBackOff());
-            handler.setBackOffRequired(
-                response -> {
-                  int code = response.getStatusCode();
-                  // 403 is Google's Rate limit exceeded response.
-                  return code == 403 || code / 100 == 5;
-                });
-            request.setUnsuccessfulResponseHandler(handler);
-            batch.queue(request, Groups.class, GoogleJsonErrorContainer.class, callback);
-
-          } catch (IOException ioe) {
-            throw new RuntimeException(ioe);
-          }
-        });
-
+    // Per some discussion... the google batch APIs have challenges.  Eg. they do an
+    // "all-or-nothing" type operation
+    // and may randomly fail.  So we use a custom thread pool to parallelize the requests and use a
+    // backoff for EACH
+    // request.  See
+    // https://stackoverflow.com/a/78388542
+    // and
+    // https://docs.cloud.google.com/java/docs/reference/google-api-client/latest/com.google.api.client.googleapis.batch
+    // AS A RESULT:  THIS WILL run slower than a batched operation which does it all at once... WITH
+    // upside this should be "safer"
+    ForkJoinPool customPool = new ForkJoinPool(config.groupLookupConcurrency);
     try {
-      batch.execute();
-    } catch (IOException ioe) {
+      log.info("Fetching group information for " + userEmails.size() + " users");
+      customPool
+          .submit(
+              () ->
+                  userEmails.parallelStream()
+                      .forEach(
+                          email -> {
+                            log.debug("Getting roles for email " + email);
+                            try {
+                              emailGroupsMap.put(email, getRolesForEmail(email));
+                            } catch (IOException ioe) {
+                              log.warn(
+                                  "Failed to fetch groups for user "
+                                      + email
+                                      + ": "
+                                      + ioe.getMessage());
+                            }
+                          }))
+          .get();
+      log.info(
+          "Actually got "
+              + emailGroupsMap.size()
+              + " sets of groups - which excludes any service accounts");
+    } catch (Exception ioe) {
       throw new RuntimeException(ioe);
+    } finally {
+      customPool.shutdown();
     }
 
     return emailGroupsMap;
+  }
+
+  private Collection<Role> getRolesForEmail(String email) throws IOException {
+    // Check if this is a managed service account, we should never check google groups for
+    // these
+    if (email.endsWith(SERVICE_ACCOUNT_SUFFIX) || email.endsWith(SHARED_SERVICE_ACCOUNT_SUFFIX)) {
+      // Skip over this in the batch
+      return new HashSet<>();
+    }
+    log.debug("Fetching group information for " + email);
+    return getGroupsFromEmailRecursively(email).getGroups().stream()
+        .flatMap(toRoleFn())
+        .collect(Collectors.toSet());
   }
 
   @Override
@@ -210,8 +192,11 @@ public class GoogleDirectoryUserRolesProvider implements UserRolesProvider, Init
    *     memberships.
    * @throws IOException If an error occurs while retrieving group information.
    */
+  @VisibleForTesting
   protected Groups getGroupsFromEmailRecursively(String email) throws IOException {
+    log.debug("Starting group fetch");
     final Groups groups = getGroupsFromEmail(email);
+    log.debug("Completed fetching groups... found " + groups.size());
     if (groups == null
         || groups.getGroups() == null
         || groups.getGroups().isEmpty()
@@ -221,16 +206,28 @@ public class GoogleDirectoryUserRolesProvider implements UserRolesProvider, Init
     final Set<String> collectedGroup = new HashSet<>();
     final Deque<String> stack = new ArrayDeque<>();
     for (Group g : groups.getGroups()) {
+      // Skip the group recursion IF no email is there....
+      if (ObjectUtils.isEmpty(g.getEmail())) {
+        continue;
+      }
       stack.push(g.getEmail());
       collectedGroup.add(g.getEmail());
     }
+    log.debug("Starting group recursion");
     while (!stack.isEmpty()) {
       String nextEmail = stack.pop();
       Groups subGroups = getGroupsFromEmail(nextEmail);
       if (subGroups == null || subGroups.getGroups() == null || subGroups.getGroups().isEmpty()) {
+        log.debug(
+            "No groups found for email " + nextEmail + " while looking for recursive sub groups");
         continue;
       }
+      log.debug("Found groups for email " + nextEmail + " of " + subGroups.getGroups());
       for (Group g : subGroups.getGroups()) {
+        if (ObjectUtils.isEmpty(g.getEmail())) {
+          groups.getGroups().add(g);
+          continue;
+        }
         if (collectedGroup.contains(g.getEmail())) {
           continue;
         }
@@ -239,15 +236,27 @@ public class GoogleDirectoryUserRolesProvider implements UserRolesProvider, Init
         collectedGroup.add(g.getEmail());
       }
     }
+    log.debug("Completed group recursion");
     return groups;
   }
 
   protected Groups getGroupsFromEmail(String email) throws IOException {
     final Directory service = getDirectoryService();
+
     final Groups groups =
-        service.groups().list().setDomain(config.getDomain()).setUserKey(email).execute();
+        service
+            .groups()
+            .list()
+            .setDomain(config.getDomain())
+            .setUserKey(email)
+            .buildHttpRequest()
+            .setUnsuccessfulResponseHandler(getHttpBackOffUnsuccessfulResponseHandler())
+            .execute()
+            .parseAs(Groups.class);
+
     String nextPageToken = groups.getNextPageToken();
     while (nextPageToken != null) {
+
       final Groups nextPage =
           service
               .groups()
@@ -255,11 +264,27 @@ public class GoogleDirectoryUserRolesProvider implements UserRolesProvider, Init
               .setDomain(config.getDomain())
               .setUserKey(email)
               .setPageToken(nextPageToken)
-              .execute();
+              .buildHttpRequest()
+              .setUnsuccessfulResponseHandler(getHttpBackOffUnsuccessfulResponseHandler())
+              .execute()
+              .parseAs(Groups.class);
       groups.getGroups().addAll(nextPage.getGroups());
       nextPageToken = nextPage.getNextPageToken();
     }
     return groups;
+  }
+
+  private static HttpBackOffUnsuccessfulResponseHandler
+      getHttpBackOffUnsuccessfulResponseHandler() {
+    HttpBackOffUnsuccessfulResponseHandler handler =
+        new HttpBackOffUnsuccessfulResponseHandler(new ExponentialBackOff());
+    handler.setBackOffRequired(
+        response -> {
+          int code = response.getStatusCode();
+          // 403 is Google's Rate limit exceeded response.
+          return code == 403 || code / 100 == 5;
+        });
+    return handler;
   }
 
   private GoogleCredentials getGoogleCredential() {
@@ -278,10 +303,8 @@ public class GoogleDirectoryUserRolesProvider implements UserRolesProvider, Init
   }
 
   private Directory getDirectoryService() {
-    HttpTransport httpTransport = new NetHttpTransport();
     GsonFactory jacksonFactory = new GsonFactory();
     GoogleCredentials credentials = getGoogleCredential();
-
     return new Directory.Builder(
             httpTransport, jacksonFactory, new HttpCredentialsAdapter(credentials))
         .setApplicationName("Spinnaker-Fiat")
@@ -331,6 +354,8 @@ public class GoogleDirectoryUserRolesProvider implements UserRolesProvider, Init
      * backwards compatibility
      */
     private RoleSource[] roleSources = new RoleSource[] {Config.RoleSource.NAME};
+
+    private int groupLookupConcurrency = 10;
 
     /** RoleSource maps to metadata on the Group metadata, NAME = Group Name, Email = Group Email */
     private enum RoleSource {
--- fiat/fiat-google-groups/src/test/groovy/com/netflix/spinnaker/fiat/roles/google/GoogleDirectoryUserRolesProviderSpec.groovy
+++ fiat/fiat-google-groups/src/test/groovy/com/netflix/spinnaker/fiat/roles/google/GoogleDirectoryUserRolesProviderSpec.groovy
@@ -1,5 +1,6 @@
 package com.netflix.spinnaker.fiat.roles.google
 
+import com.netflix.spinnaker.fiat.model.resources.Role
 import com.netflix.spinnaker.fiat.permissions.ExternalUser
 import com.google.api.services.directory.model.Group;
 import com.google.api.services.directory.model.Groups;
@@ -7,113 +8,164 @@ import spock.lang.Specification
 import spock.lang.Unroll
 
 class GoogleDirectoryUserRolesProviderSpec extends Specification {
-    GoogleDirectoryUserRolesProvider.Config config = new GoogleDirectoryUserRolesProvider.Config()
+  GoogleDirectoryUserRolesProvider.Config config = new GoogleDirectoryUserRolesProvider.Config()
 
-    def "should read google groups"() {
-        setup:
-        config.domain = "test.com"
-        def group = new Group()
-        group.set("email", "test@test.com")
-        group.set("name", "test")
-        List<Group> groupList = new ArrayList<>()
-        groupList.add(group)
-        def groups = new Groups()
-        groups.setGroups(groupList)
+  def "should read google groups"() {
+    setup:
+    config.domain = "test.com"
+    def group = new Group()
+    group.set("email", "test@test.com")
+    group.set("name", "test")
+    List<Group> groupList = new ArrayList<>()
+    groupList.add(group)
+    def groups = new Groups()
+    groups.setGroups(groupList)
 
-        GoogleDirectoryUserRolesProvider provider = new GoogleDirectoryUserRolesProvider() {
-            @Override
-            Groups getGroupsFromEmailRecursively(String email) {
-                return groups
-            }
+    GoogleDirectoryUserRolesProvider provider = new GoogleDirectoryUserRolesProvider() {
+      @Override
+      Groups getGroupsFromEmailRecursively(String email) {
+        return groups
+      }
+    }
+
+    provider.setProperty("config", config)
+
+    when:
+    def result1 = provider.loadRoles(ExternalUser.forId("testuser"))
+
+    then:
+    result1.name.containsAll(["test"])
+    result1.name.size() == 1
+
+    when:
+    config.roleSources = [GoogleDirectoryUserRolesProvider.Config.RoleSource.EMAIL, GoogleDirectoryUserRolesProvider.Config.RoleSource.NAME]
+    def result2 = provider.loadRoles(ExternalUser.forId("testuser"))
+
+    then:
+    result2.name.containsAll(["test@test.com", "test"])
+    result2.name.size() == 2
+
+    when:
+    config.roleSources = [GoogleDirectoryUserRolesProvider.Config.RoleSource.EMAIL]
+    def result3 = provider.loadRoles(ExternalUser.forId("testuser"))
+
+    then:
+    result3.name.containsAll(["test@test.com"])
+    result3.name.size() == 1
+
+    when:
+    //test that a null name does not break the resolution
+    group.setName(null)
+    groupList.clear()
+    groupList.add(group)
+    def result4 = provider.loadRoles(ExternalUser.forId("testuser"))
+
+    then:
+    result4.name.containsAll(["test@test.com"])
+    result4.name.size() == 1
+
+    when:
+    //test that a service account does not get groups returned
+    def result5 = provider.loadRoles(ExternalUser.forId("testuser@managed-service-account"))
+
+    then:
+    result5.name.size() == 0
+
+    when:
+    //test that a shared service account does not get groups returned
+    def result6 = provider.loadRoles(ExternalUser.forId("testuser@shared-managed-service-account"))
+
+    then:
+    result6.name.size() == 0
+  }
+
+  @Unroll
+  def "should recursively collect all nested groups if expandIndirectGroups is #expandIndirectGroups"() {
+    given:
+    config.expandIndirectGroups = expandIndirectGroups
+    def provider = Spy(GoogleDirectoryUserRolesProvider) {
+      getGroupsFromEmail("root@example.com") >> new Groups(groups: [
+        new Group(email: "child1@example.com"),
+        new Group(email: "child2@example.com")
+      ])
+      getGroupsFromEmail("child1@example.com") >> new Groups(groups: [
+        new Group(email: "grandchild1@example.com")
+      ])
+      getGroupsFromEmail("child2@example.com") >> new Groups(groups: [
+        new Group(email: "grandchild2@example.com"),
+        new Group(email: "child1@example.com")
+      ])
+      getGroupsFromEmail("grandchild1@example.com") >> new Groups(groups: [])
+      getGroupsFromEmail("grandchild2@example.com") >> null
+    }
+    provider.setConfig(config)
+
+    when:
+    def result = provider.getGroupsFromEmailRecursively("root@example.com")
+
+    then:
+    result.groups*.email.containsAll(groupsContent)
+    result.groups.size() == totalEmails
+
+    where:
+    expandIndirectGroups | totalEmails | groupsContent
+    true                 | 4           | ["child1@example.com", "child2@example.com", "grandchild1@example.com", "grandchild2@example.com"]
+    false                | 2           | ["child1@example.com", "child2@example.com"]
+
+  }
+
+
+  @Unroll
+  def "should parallel load groups as needed"() {
+    given:
+    config.expandIndirectGroups = expandIndirectGroups
+    config.groupLookupConcurrency = 4
+    def provider = new GoogleDirectoryUserRolesProvider() {
+      // Move from spy to  a real overload due to thread hnadling.
+      @Override
+      protected Groups getGroupsFromEmail(String email) throws IOException {
+        switch (email) {
+          case "root@example.com":
+            return  new Groups(groups : [
+              new Group(email: "child1@example.com"),
+              new Group(email: "child2@example.com")
+            ])
+          case "child1@example.com":
+            return new Groups(groups: [
+              new Group(email: "grandchild1@example.com"),
+              new Group(name: "grandchild22")
+            ])
+          case "child2@example.com":
+            return new Groups(groups: [
+              new Group(email: "grandchild2@example.com"),
+              new Group(email: "child1@example.com")
+            ])
+          case "grandchild1@example.com":
+            return new Groups(groups: [new Group(name: "test1")])
+          case "user2@example.com":
+            return new Groups(groups: [new Group(name: "test2")])
+          case "user3@example.com":
+            return new Groups(groups: [new Group(name: "test3")])
+          default:
+            return null;
         }
-
-        provider.setProperty("config", config)
-
-        when:
-        def result1 = provider.loadRoles(externalUser("testuser"))
-
-        then:
-        result1.name.containsAll(["test"])
-        result1.name.size() == 1
-
-        when:
-        config.roleSources = [GoogleDirectoryUserRolesProvider.Config.RoleSource.EMAIL, GoogleDirectoryUserRolesProvider.Config.RoleSource.NAME]
-        def result2 = provider.loadRoles(externalUser("testuser"))
-
-        then:
-        result2.name.containsAll(["test@test.com", "test"])
-        result2.name.size() == 2
-
-        when:
-        config.roleSources = [GoogleDirectoryUserRolesProvider.Config.RoleSource.EMAIL]
-        def result3 = provider.loadRoles(externalUser("testuser"))
-
-        then:
-        result3.name.containsAll(["test@test.com"])
-        result3.name.size() == 1
-
-        when:
-        //test that a null name does not break the resolution
-        group.setName(null)
-        groupList.clear()
-        groupList.add(group)
-        def result4 = provider.loadRoles(externalUser("testuser"))
-
-        then:
-        result4.name.containsAll(["test@test.com"])
-        result4.name.size() == 1
-       
-        when:
-        //test that a service account does not get groups returned
-        def result5 = provider.loadRoles(externalUser("testuser@managed-service-account"))
-
-        then:
-        result5.name.size() == 0
-        
-        when:
-        //test that a shared service account does not get groups returned
-        def result6 = provider.loadRoles(externalUser("testuser@shared-managed-service-account"))
-
-        then:
-        result6.name.size() == 0
+      }
     }
+    provider.setConfig(config)
 
-    @Unroll
-    def "should recursively collect all nested groups if expandIndirectGroups is #expandIndirectGroups"() {
-        given:
-        config.expandIndirectGroups = expandIndirectGroups
-        def provider = Spy(GoogleDirectoryUserRolesProvider) {
-            getGroupsFromEmail("root@example.com") >> new Groups(groups: [
-                new Group(email: "child1@example.com"),
-                new Group(email: "child2@example.com")
-            ])
-            getGroupsFromEmail("child1@example.com") >> new Groups(groups: [
-                new Group(email: "grandchild1@example.com")
-            ])
-            getGroupsFromEmail("child2@example.com") >> new Groups(groups: [
-                new Group(email: "grandchild2@example.com"),
-                new Group(email: "child1@example.com")
-            ])
-            getGroupsFromEmail("grandchild1@example.com") >> new Groups(groups: [])
-            getGroupsFromEmail("grandchild2@example.com") >> null
-        }
-        provider.setConfig(config)
+    when:
+    def result = provider.multiLoadRoles(Set.of(ExternalUser.forId("root@example.com"), ExternalUser.forId("user2@example.com"), ExternalUser.forId("user3@example.com")));
 
-        when:
-        def result = provider.getGroupsFromEmailRecursively("root@example.com")
+    then:
+    result.get(emailAddress).containsAll(groupsContent)
 
-        then:
-        result.groups*.email.containsAll(groupsContent)
-        result.groups.size() == totalEmails
+    where:
+    expandIndirectGroups | emailAddress        | groupsContent
+    true                 | "root@example.com"  | [new Role("test1"), new Role("grandchild22")]
+    true                 | "user2@example.com" | [new Role("test2")]
+    true                 | "user3@example.com" | [new Role("test3")]
 
-        where:
-        expandIndirectGroups | totalEmails | groupsContent
-        true                 | 4           | ["child1@example.com", "child2@example.com", "grandchild1@example.com", "grandchild2@example.com"]
-        false                | 2           | ["child1@example.com", "child2@example.com"]
-        
-    }
+  }
 
-    private static ExternalUser externalUser(String id) {
-        return new ExternalUser().setId(id)
-    }
+
 }
