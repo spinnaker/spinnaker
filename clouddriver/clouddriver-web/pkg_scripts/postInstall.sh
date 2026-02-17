330dd8f0ed6759532f3fdb6e6865d49a3af68c0f
Ashley Kleynhans <ashleykleynhans@users.noreply.github.com>
2026-02-17 04:46:50 +0200

    fix(clouddriver): update kubectl download URL (#7470)

--- clouddriver/clouddriver-web/pkg_scripts/postInstall.sh
+++ clouddriver/clouddriver-web/pkg_scripts/postInstall.sh
@@ -21,7 +21,7 @@ install_kubectl() {
   if [ -z "$(which kubectl)" ]; then
     for version in $KUBECTL_RELEASES; do
       release_version=$(echo "${version}" | cut -d. -f1,2); \
-      wget -nv "https://cdn.dl.k8s.io/release/v${version}/bin/linux/amd64/kubectl" -O "/usr/local/bin/kubectl-${release_version}";
+      wget -nv "https://dl.k8s.io/release/v${version}/bin/linux/amd64/kubectl" -O "/usr/local/bin/kubectl-${release_version}";
       chmod +x "/usr/local/bin/kubectl-${release_version}";
     done
     ln -sf "/usr/local/bin/kubectl-$(echo ${KUBECTL_DEFAULT_RELEASE} | cut -d. -f1,2)" /usr/local/bin/kubectl
