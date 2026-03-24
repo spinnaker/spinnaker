4c84509cf5f06c769a6ef2c953cc3cf5f1b9d19a
Jason McIntosh <jason.mcintosh@harness.io>
2026-03-23 19:50:06 -0500

    fix(api-auth): Remove copied saml stuff from API Auth stuff

--- gate/gate-auth-apitoken/src/main/java/com/netflix/spinnaker/gate/security/apitoken/APIAuthTokenConfiguration.java
+++ gate/gate-auth-apitoken/src/main/java/com/netflix/spinnaker/gate/security/apitoken/APIAuthTokenConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Apple, Inc.
+ * Copyright 2023 McIntosh.farm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
