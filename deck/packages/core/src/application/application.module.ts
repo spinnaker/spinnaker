afadf5f78a96e2d6b6e8ade4dc53643b0b9f7c3d
Matt Gogerly <6519811+mattgogerly@users.noreply.github.com>
2026-06-16 15:41:42 +0100

    refactor(deck): remove shared core Angular wrappers
    * refactor(deck): remove shared core Angular wrappers
    
    * fix(deck): keep app config state registered
    
    * fix(deck): avoid duplicate nav categories
    
    * fix(deck): isolate navigation category tests
    
    * chore(deck): rename replacement parameter
    
    (cherry picked from commit 871b0a673ad075028964ac99ea68c045bc315632)

--- deck/packages/core/src/application/application.module.ts
+++ deck/packages/core/src/application/application.module.ts
@@ -4,7 +4,8 @@ import { APPLICATION_INITIALIZERS_MODULE } from './application.initializers';
 import { APPLICATION_STATE_PROVIDER } from './application.state.provider';
 import './applicationSearchResultType';
 import { APPLICATIONS_STATE_PROVIDER } from './applications.state.provider';
-import { CORE_APPLICATION_CONFIG_APPCONFIG_DATASOURCE } from './config/appConfig.dataSource';
+import './config/appConfig.dataSource';
+import { APP_CONFIG_STATES } from './config/appConfig.states';
 import { CORE_APPLICATION_CONFIG_APPLICATIONCONFIG_CONTROLLER } from './config/applicationConfig.controller';
 import { CORE_APPLICATION_MODAL_CREATEAPPLICATION_MODAL_CONTROLLER } from './modal/createApplication.modal.controller';
 import { PERMISSIONS_CONFIGURER_COMPONENT } from './modal/permissionsConfigurer.component';
@@ -17,9 +18,9 @@ module(APPLICATION_MODULE, [
   APPLICATION_STATE_PROVIDER,
   APPLICATION_INITIALIZERS_MODULE,
   APPLICATIONS_STATE_PROVIDER,
+  APP_CONFIG_STATES,
   CORE_APPLICATION_CONFIG_APPLICATIONCONFIG_CONTROLLER,
   CORE_APPLICATION_MODAL_CREATEAPPLICATION_MODAL_CONTROLLER,
   CORE_APPLICATION_MODAL_PLATFORMHEALTHOVERRIDE_DIRECTIVE,
-  CORE_APPLICATION_CONFIG_APPCONFIG_DATASOURCE,
   PERMISSIONS_CONFIGURER_COMPONENT,
 ]);
