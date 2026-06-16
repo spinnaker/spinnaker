81afec5ba16f74e9127d1004473c74c01b02bc49
Johannes Grumboeck <johannes.grumboeck@redbull.com>
2026-06-16 22:42:37 +0200

    feat(ecs): migrate IamRoleCachingAgent to AWS SDK v2
    Replace v1 AmazonIdentityManagement with v2 IamClient.
    Uses getIamV2() from bilingual AmazonClientProvider.
    Remove v1 com.amazonaws.regions.Regions usage (hardcode us-east-1 default).
    Update tests to mock v2 types.
    
    Part of spinnaker/spinnaker#7699.

--- clouddriver/clouddriver-ecs/src/test/java/com/netflix/spinnaker/clouddriver/ecs/provider/agent/IamRoleCachingAgentTest.java
+++ clouddriver/clouddriver-ecs/src/test/java/com/netflix/spinnaker/clouddriver/ecs/provider/agent/IamRoleCachingAgentTest.java
@@ -20,16 +20,10 @@ import static com.netflix.spinnaker.clouddriver.ecs.cache.Keys.Namespace.IAM_ROL
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
-import com.amazonaws.services.identitymanagement.model.ListRolesRequest;
-import com.amazonaws.services.identitymanagement.model.ListRolesResult;
-import com.amazonaws.services.identitymanagement.model.Role;
 import com.netflix.spinnaker.cats.cache.CacheData;
 import com.netflix.spinnaker.clouddriver.aws.security.AmazonCredentials;
 import com.netflix.spinnaker.clouddriver.aws.security.NetflixAmazonCredentials;
@@ -37,10 +31,14 @@ import com.netflix.spinnaker.clouddriver.ecs.cache.Keys;
 import com.netflix.spinnaker.clouddriver.ecs.cache.model.IamRole;
 import java.util.*;
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.ListRolesRequest;
+import software.amazon.awssdk.services.iam.model.ListRolesResponse;
+import software.amazon.awssdk.services.iam.model.Role;
 import spock.lang.Subject;
 
 public class IamRoleCachingAgentTest extends CommonCachingAgent {
-  private final AmazonIdentityManagement iam = mock(AmazonIdentityManagement.class);
+  private final IamClient iam = mock(IamClient.class);
   private final IamPolicyReader iamPolicyReader = mock(IamPolicyReader.class);
 
   @Subject
@@ -55,13 +53,13 @@ public class IamRoleCachingAgentTest extends CommonCachingAgent {
     iamTrustRelationship.setType("Service");
     iamTrustRelationship.setValue("ecs-tasks.amazonaws.com");
 
-    Set<Role> roles = new HashSet<>();
+    List<Role> roles = new ArrayList<>();
     Set<IamRole> iamRoles = new HashSet<>();
     for (int x = 0; x < numberOfRoles; x++) {
       String arn = "iam-role-arn-" + x;
       String name = "iam-role-name-" + x;
 
-      roles.add(new Role().withArn(arn).withRoleName(name));
+      roles.add(Role.builder().arn(arn).roleName(name).build());
 
       IamRole iamRole = new IamRole();
       iamRole.setAccountName(ACCOUNT);
@@ -71,10 +69,9 @@ public class IamRoleCachingAgentTest extends CommonCachingAgent {
       iamRoles.add(iamRole);
     }
 
-    when(clientProvider.getIam(any(NetflixAmazonCredentials.class), anyString(), anyBoolean()))
-        .thenReturn(iam);
+    when(clientProvider.getIamV2(any(NetflixAmazonCredentials.class), anyString())).thenReturn(iam);
     when(iam.listRoles(any(ListRolesRequest.class)))
-        .thenReturn(new ListRolesResult().withRoles(roles).withIsTruncated(false));
+        .thenReturn(ListRolesResponse.builder().roles(roles).isTruncated(false).build());
     when(iamPolicyReader.getTrustedEntities(any()))
         .thenReturn(Collections.singleton(iamTrustRelationship));
 
@@ -159,7 +156,7 @@ public class IamRoleCachingAgentTest extends CommonCachingAgent {
   @Test
   public void shouldGetDefaultRegion() {
     // given
-    String defaultRegionName = Regions.DEFAULT_REGION.getName();
+    String defaultRegionName = "us-east-1";
     when(netflixAmazonCredentials.getRegions())
         .thenReturn(Collections.singletonList(new AmazonCredentials.AWSRegion("us-east-1", null)));
 
