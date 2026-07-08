bd166c83e9c8a0b34a0fe4e3d8331f5f10ac8775
Matt Gogerly <matt.gogerly@wise.com>
2026-07-08 12:54:39 +0100

    feat(deck/huawei): add registration test

--- deck/packages/huaweicloud/src/huaweicloud.spec.ts
+++ deck/packages/huaweicloud/src/huaweicloud.spec.ts
@@ -1,6 +1,18 @@
+import { CloudProviderRegistry, SETTINGS } from '@spinnaker/core';
+
 describe('HuaweiCloud package entrypoint', () => {
+  beforeAll(() => {
+    SETTINGS.providers.huaweicloud = { defaults: {} };
+  });
+
   it('loads successfully', () => {
     expect(() => require('./index')).not.toThrow();
+  });
+
+  it('registers the provider configuration', () => {
+    require('./index');
+
+    expect(CloudProviderRegistry.getProvider('huaweicloud')).toEqual({ name: 'huaweicloud' });
   });
 
   it('does not expose an Angular module token', () => {
