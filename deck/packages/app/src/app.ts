7847d79f1baf08d17bc9b06e876fca5f01e880cc
Matt Gogerly <matt.gogerly@wise.com>
2026-07-16 10:40:23 +0100

    refactor(deck): remove Angular from app canary

--- deck/packages/app/src/app.ts
+++ deck/packages/app/src/app.ts
@@ -9,7 +9,7 @@ import { AMAZON_MODULE } from '@spinnaker/amazon';
 import '@spinnaker/appengine';
 import { AZURE_MODULE } from '@spinnaker/azure';
 import { GOOGLE_MODULE } from '@spinnaker/google';
-import { CANARY_MODULE } from './canary/canary.module';
+import './canary/canary.module';
 import '@spinnaker/kubernetes';
 import '@spinnaker/oracle';
 import '@spinnaker/kayenta';
@@ -18,12 +18,4 @@ import { ECS_MODULE } from '@spinnaker/ecs';
 import '@spinnaker/cloudrun';
 import '@spinnaker/cloudfoundry';
 
-module('netflix.spinnaker', [
-  CORE_MODULE,
-  AMAZON_MODULE,
-  AZURE_MODULE,
-  GOOGLE_MODULE,
-  ECS_MODULE,
-  CANARY_MODULE,
-  TITUS_MODULE,
-]);
+module('netflix.spinnaker', [CORE_MODULE, AMAZON_MODULE, AZURE_MODULE, GOOGLE_MODULE, ECS_MODULE, TITUS_MODULE]);
