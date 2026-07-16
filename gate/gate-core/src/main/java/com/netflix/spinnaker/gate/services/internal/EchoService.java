3f222eabab0f0722bdd3589da398c45e8f42049c
Jason McIntosh <jason.mcintosh@harness.io>
2026-07-16 16:45:32 -0500

    fix(spotless): Formatting

--- gate/gate-core/src/main/java/com/netflix/spinnaker/gate/services/internal/EchoService.java
+++ gate/gate-core/src/main/java/com/netflix/spinnaker/gate/services/internal/EchoService.java
@@ -4,7 +4,6 @@ import com.netflix.spinnaker.kork.plugins.SpinnakerPluginDescriptor;
 import io.cloudevents.CloudEvent;
 import java.util.List;
 import java.util.Map;
-import org.springframework.http.ResponseEntity;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
