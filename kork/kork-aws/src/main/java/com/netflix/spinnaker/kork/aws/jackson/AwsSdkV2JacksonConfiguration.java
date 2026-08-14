816b47672accbe10689005dc8a109ebf399dad2e
Jason McIntosh <jason.mcintosh@harness.io>
2026-08-14 10:45:14 -0500

    chore(aws): Move orca and kork from sdk v1 to v2

---
+++ kork/kork-aws/src/main/java/com/netflix/spinnaker/kork/aws/jackson/AwsSdkV2JacksonConfiguration.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2026 Harness, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kork.aws.jackson;
+
+import com.fasterxml.jackson.databind.Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Boot auto-configuration that registers {@link AwsSdkV2Module} as a bean. Any {@code
+ * ObjectMapper} that is auto-configured by Spring Boot will pick this module up automatically.
+ */
+@Configuration
+public class AwsSdkV2JacksonConfiguration {
+
+  @Bean
+  Module awsSdkV2Module() {
+    return new AwsSdkV2Module();
+  }
+}
