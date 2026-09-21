75fc8b8de178951abc8a7caf4f5b9157062b80d6
Jashan Singh <js4683@drexel.edu>
2026-09-21 12:54:34 -0400

    style: spotless import grouping

--- front50/front50-core/src/main/java/com/netflix/spinnaker/front50/config/Front50CoreConfiguration.java
+++ front50/front50-core/src/main/java/com/netflix/spinnaker/front50/config/Front50CoreConfiguration.java
@@ -22,7 +22,6 @@ import java.util.List;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
